e70b463841c7fc99f7e9fdc3103e2a4c67de0d81
etwum <33198751+etwum@users.noreply.github.com>
2018-01-28 13:57:51 -0800

    added new grid printer python file

---
+++ students/etwum/lesson02/grid_printer.py
@@ -0,0 +1,12 @@
+def print_grid(size, units):
+    return
+
+
+
+
+
+
+while(True):
+
+    size = int(input('Please enter a number to determine the rows and column in the grid'))
+    units = int(input('Please enter the unit size'))
