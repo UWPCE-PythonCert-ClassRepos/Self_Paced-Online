803363dfc7a59b2e9503e77f0f4f524867f5cedb
Dustin L <r.dustin.lane@gmail.com>
2018-01-28 10:43:32 -0800

    Add print_fixed_grid() function

--- students/Dustin_L/lesson02/grid_printer.py
+++ students/Dustin_L/lesson02/grid_printer.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 
+def print_fixed_grid():
+    '''
+    This function prints a 2 x 2 cell grid where length = width = 8
+    '''
+
+    tmb = '+ - - - - + - - - - +\n'
+    bars = '|         |         |\n'
+
+    print(tmb + (4 * bars) + tmb + (4 * bars) + tmb)
+
+
+if __name__ == '__main__':
+    print_fixed_grid()
