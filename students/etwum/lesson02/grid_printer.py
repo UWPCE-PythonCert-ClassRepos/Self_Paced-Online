fdc712dfd8f3ed6474c463e15561aa0bde1554b6
etwum <33198751+etwum@users.noreply.github.com>
2018-01-28 15:57:35 -0800

    changes
    added an if statement for a size input of 1 vs a size input greater than 1

--- students/etwum/lesson02/grid_printer.py
+++ students/etwum/lesson02/grid_printer.py
@@ -1,12 +1,22 @@
 def print_grid(size, units):
-    #columns
+    # this function is used to print a grid based on size and unit parameters
+
     if size == 1:
-        print('+' + '-'*units + '+')
+        print('+' + ' - ' * units + '+')
+        print(((("|" + "   " * units + "|") * (size) + "\n") * (units) +
+               ('+' + ' - ' * units + '+') * (size) + "\n") * (size))
+
     else:
-        print(('+' + '-' * units + '+') +('-' * units + '+')* (size-1))
 
-    #rows
+        # columns
 
+        print(('+' + ' - ' * units + '+') +(' - ' * units + '+')* (size-1))
+
+        print(((("|" + "   "*units + "|") + ("   " * units + '|')*(size-1)+"\n")*(units) +
+        ('+' + ' - ' * units + '+') + (' - ' * units + '+') * (size - 1) + "\n")*(size-1))
+
+        print(((("|" + "   " * units + "|") + ("   " * units + '|') * (size - 1) + "\n") * (units) +
+               ('+' + ' - ' * units + '+') + (' - ' * units + '+') * (size - 1) + "\n") * (size - 2))
 
 while(True):
     boolInput = False
@@ -15,14 +25,15 @@ while(True):
             x = int(input('Please enter a number to determine the rows and column in the grid: '))
             y = int(input('Please enter a number to determine the the unit size: '))
         except (ValueError) as b:
-            print('Please numbers only')
+            print('Please input numbers only')
             print()
         else:
             boolInput = True
 
     print_grid(x,y)
-    exit = input('Would you like exit ')
-    if exit.lower() == 'y':
+    exit_game = input('Would you like to exit (y or n)')
+    if exit_game.lower() == 'y':
         break
     else:
         continue
+        print()
