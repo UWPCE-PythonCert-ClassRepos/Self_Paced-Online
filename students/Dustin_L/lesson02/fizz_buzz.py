ab3daaf407238bd080cda0e7fed7c519fbdd93f9
Dustin L <r.dustin.lane@gmail.com>
2018-01-28 11:29:45 -0800

    Add fizz_buzz() function

--- students/Dustin_L/lesson02/fizz_buzz.py
+++ students/Dustin_L/lesson02/fizz_buzz.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
 
+def fizz_buzz(n):
+    '''
+    This function prints all integers from 1 to n inclusive with the following
+    rules:
+    * Integers that are multiples of 3: print "Fizz"
+    * Integers that are multiples of 5: print "Buzz"
+    * Integers that are multiples of both 3 and 5: print "FizzBuzz"
+    * Print all other integers
+    '''
+
+    for i in range(1, n + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
 if __name__ == '__main__':
-    pass
+    fizz_buzz(100)
