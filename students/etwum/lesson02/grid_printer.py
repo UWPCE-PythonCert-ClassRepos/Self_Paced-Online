0e7bb9e2b5af1ca98985917cc2f651952f8946c2
etwum <33198751+etwum@users.noreply.github.com>
2018-01-28 14:34:17 -0800

    building code for rows and columns

--- students/etwum/lesson02/grid_printer.py
+++ students/etwum/lesson02/grid_printer.py
@@ -1,11 +1,17 @@
 def print_grid(size, units):
-    print('+' + '-'*units)
+    #columns
+    if size == 1:
+        print('+' + '-'*units + '+')
+    else:
+        print(('+' + '-' * units + '+') +('-' * units + '+')* (size-1))
+
+    #rows
 
 
 while(True):
-
-    x = int(input('Please enter a number to determine the rows and column in the grid'))
-    y = int(input('Please enter the unit size'))
+    try:
+    x = int(input('Please enter a number to determine the rows and column in the grid: '))
+    y = int(input('Please enter a number to determine the the unit size: '))
 
     print_grid(x,y)
     exit = input('Would you like exit ')
