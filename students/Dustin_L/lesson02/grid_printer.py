9df15f4e42866dbd6dacbd90027313f865a05f75
Dustin L <r.dustin.lane@gmail.com>
2018-01-28 10:44:33 -0800

    Add print_row() function

--- students/Dustin_L/lesson02/grid_printer.py
+++ students/Dustin_L/lesson02/grid_printer.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 
+def print_row(n, sz, bottom):
+    '''
+    This function prints a single row of n cells where each cell is of size sz.
+    The bottom line of the row is only printed if 'bottom' is True.
+    '''
+
+    if n < 0 or sz < 0:
+        return None
+
+    # Top and bottom line definition
+    tb = ('+ ' + ('- ' * sz)) * n + '+' + '\n'
+    # Inner line definition
+    inner = ('| ' + (' ' * 2 * sz)) * n + '|' + '\n'
+
+    row = tb + (sz * inner)
+    if bottom:
+        row += tb
+
+    print(row, end='')
+
+
 def print_fixed_grid():
     '''
     This function prints a 2 x 2 cell grid where length = width = 8
