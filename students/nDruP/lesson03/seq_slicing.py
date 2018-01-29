29bf2b400c199742e6fe7aac2bba6ca2fa576c5b
Andrew Park <apark46.work@gmail.com>
2018-01-29 01:03:42 -0800

    Lesson03|seq_slicing.py rm_every_other removes every other element in a sequence through slicing.

--- students/nDruP/lesson03/seq_slicing.py
+++ students/nDruP/lesson03/seq_slicing.py
@@ -3,6 +3,8 @@ def swap_first_last(seq):
         return seq[len(seq)-1:len(seq)]+seq[1:len(seq)-1]+seq[0:1]
     return seq
 
+def rm_every_other(seq):
+    return seq[0:len(seq):2]
 
 one_element = [51]
 string_a = 'Love Galore'
@@ -13,3 +15,8 @@ assert swap_first_last(string_a) == 'eove GalorL'
 assert swap_first_last(seq_a) == [6,3,1,2,4,5]
 assert swap_first_last(tuple_a)==(6,3,1,2,4,5)
 assert swap_first_last(one_element) == one_element
+
+assert rm_every_other(string_a) == 'Lv aoe'
+assert rm_every_other(seq_a) == [5,1,4]
+assert rm_every_other(tuple_a) == (5,1,4)
+assert rm_every_other(one_element) == [51]
