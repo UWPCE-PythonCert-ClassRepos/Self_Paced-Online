be174bfa39db018aeb4b01d653c103851ca95fbd
Andrew Park <apark46.work@gmail.com>
2018-01-28 18:56:26 -0800

    commented the fibonacci_loop function

--- students/nDruP/lesson02/series.py
+++ students/nDruP/lesson02/series.py
@@ -1,10 +1,9 @@
 def fibonacci_recursive(n):
     """
-    Recursively compute the nth Fibonacci number.
+    Recursively compute the nth Fibonacci number (where n>1).
     The 0th fibonacci number is 0
     The 1st fibonacci number is 1
-    The nth fibonacci number (where n>1) is the (n-2)th - (n-1)th fibonacci
-    number.
+    The nth fibonacci number is the (n-2)th - (n-1)th fibonacci number.
     """
     if n==0:
         return 0
@@ -13,6 +12,12 @@ def fibonacci_recursive(n):
     return fibonacci_recursive(n-2)+fibonacci_recursive(n-1)
 
 def fibonacci_loop(n):
+    """
+    Compute the nth fibonacci number with a loop (where n>1).
+    The 0th fibonacci number is 0
+    The 1st fibonacci number is 1
+    The nth fibonacci number is the (n-2)th - (n-1)th fibonacci number
+    """
     fibo = [0,1]
     lastFibo = 0
     if n > 1:
