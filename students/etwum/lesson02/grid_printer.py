ca47689a6b5cc2fcdc1914f13fa6999663ef6d7d
etwum <33198751+etwum@users.noreply.github.com>
2018-01-28 16:35:33 -0800

    added comments
    added comments to the code, updated parameter and variable names

--- students/etwum/lesson02/grid_printer.py
+++ students/etwum/lesson02/grid_printer.py
@@ -1,39 +1,51 @@
-def print_grid(size, units):
+def print_grid(dimensions, units):
     # this function is used to print a grid based on size and unit parameters
 
-
-    if size == 1:
+    # if/else used to create a 1x1 grid or any grid larger
+    if dimensions == 1:
+        # creates 1x1 grid
 
         # column
         print('+' + ' - ' * units + '+')
+
         #row
-        print(((("|" + "   " * units + "|") * (size) + "\n") * (units) +
-               ('+' + ' - ' * units + '+') * (size) + "\n") * (size))
+        print(((("|" + "   " * units + "|") * (dimensions) + "\n") * (units) +
+               ('+' + ' - ' * units + '+') * (dimensions) + "\n") * (dimensions))
 
     else:
+        # creates a grid bigger than 1x1
 
         # columns
-        print(('+' + ' - ' * units + '+') +(' - ' * units + '+')* (size-1))
+        print(('+' + ' - ' * units + '+') +(' - ' * units + '+')* (dimensions-1))
 
-        #rows
-        print(((("|" + "   "*units + "|") + ("   " * units + '|')*(size-1)+"\n")*(units) +
-        ('+' + ' - ' * units + '+') + (' - ' * units + '+') * (size - 1) + "\n")*(size))
+        # rows
+        print(((("|" + "   "*units + "|") + ("   " * units + '|')*(dimensions-1)+"\n")*(units) +
+        ('+' + ' - ' * units + '+') + (' - ' * units + '+') * (dimensions - 1) + "\n")*(dimensions))
 
 
 while(True):
     # get user input for the size and units of the grid or exit the program
+
+    # boolean used to continue or exit the while loop based on user input
     boolInput = False
+
+    # while loop to get user input
     while boolInput != True:
+
+        # try/except for catch user inputs other than a number
         try:
-            x = int(input('Please enter a number to determine the rows and column in the grid: '))
-            y = int(input('Please enter a number to determine the the unit size: '))
-        except (ValueError) as b:
+            grid_size = int(input('Please enter a number to determine the rows and column in the grid: '))
+            unit_size = int(input('Please enter a number to determine the the unit size: '))
+        except ValueError as b:
             print('Please input numbers only')
             print()
         else:
             boolInput = True
 
-    print_grid(x,y)
+    # run the print grid function
+    print_grid(grid_size,unit_size)
+
+    # exit the program or continue the loop
     exit_program = input('Would you like to exit (y or n)')
     if exit_program.lower() == 'y':
         break
