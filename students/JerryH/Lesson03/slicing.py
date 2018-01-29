8d47edb75ed58304e8558c61ed5ac93822b7bf84
Jerry.H <doridoriae86@gmail.com>
2018-01-28 20:36:31 -0800

    Fixed the mid_last_first function.

--- students/JerryH/Lesson03/slicing.py
+++ students/JerryH/Lesson03/slicing.py
@@ -11,4 +11,4 @@ def reverse_elements(seq):
     return seq[-1::-1]
 
 def mid_last_first(seq):
-    return seq[(len(seq)//2)+2:(len(seq)//2)+3] + seq[-3:-2] + seq[2:3]
+    return seq[(len(seq)//2)-1:(len(seq)//2)+2] + seq[-3:] + seq[:3]
