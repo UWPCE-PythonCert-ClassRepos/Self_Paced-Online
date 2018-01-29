23663393767d63a16ce398db2c4cb46087e57848
Jerry.H <doridoriae86@gmail.com>
2018-01-28 20:23:28 -0800

    Change function name to fizz_buzz()

--- students/JerryH/Lesson02/fizz_buzz.py
+++ students/JerryH/Lesson02/fizz_buzz.py
@@ -1,4 +1,4 @@
-def FizzBuzz():
+def fizz_buzz():
   for i in range(1,101): #Looing from 1 to 100 inclusive
     if i % 3 == 0 and i % 5 == 0:
       print("FizzBuzz")
--- students/JerryH/Lesson03/list_lab.py
+++ students/JerryH/Lesson03/list_lab.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
 
-#Series 1
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
-print(fruits)
-addFruit = input("What fruit do you want to add?\n")
-fruits.append(addFruit)
-print(fruits)
-num = int(input("Enter a number:\n"))
-print(num)
-print("The fruit is {}".format(fruits[num-1]))
-someFruit = input("Add another fruit:\n")
-fruits = [someFruit] + fruits
-print(fruits)
-anotherFruit = input("Add another fruit again:\n")
-fruits.insert(0,anotherFruit)
-print(fruits)
-for i in range(len(fruits)):
-    if fruits[i][0] == 'P':
-        print(fruits[i], end=" ")
+#Series 1
+def series1():
+    print(fruits)
+    addFruit = input("What fruit do you want to add?\n")
+    fruits.append(addFruit)
+    print(fruits)
+    num = int(input("Enter a number:\n"))
+    print(num)
+    print("The fruit is {}".format(fruits[num-1]))
+    someFruit = input("Add another fruit:\n")
+    fruits = [someFruit] + fruits
+    print(fruits)
+    anotherFruit = input("Add another fruit again:\n")
+    fruits.insert(0,anotherFruit)
+    print(fruits)
+    for i in range(len(fruits)):
+        if fruits[i][0] == 'P':
+            print(fruits[i], end=" ")
+
+#Series 2
+def series2():
+    print(fruits)
+    fruits.pop()
+    print(fruits)
+    toDelete = input("Which fruit do you want to delete? /n")
+    for f in fruits:
+        if f == toDelete:
+            fruits.remove(toDelete)
+    print(fruits)
+
+#Series #3
+def series3():
+    print([f.lower() for f in fruits])
+    ans = input("Do you like apples?\n")
+    ans = ans.lower()
+    while ans != 'yes' and ans != 'no':
+        ans = input("Please answer yes or no.\n")
+
+    if ans == 'no':
+        fruits.remove('Apples')
+    print(fruits)
