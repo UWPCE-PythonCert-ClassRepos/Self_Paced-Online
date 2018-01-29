1e55da5aad67f2c1c4778e00556e6546e3db0290
Andrew Park <apark46.work@gmail.com>
2018-01-28 17:47:42 -0800

    Commented/renamed functions of Part 2&3 of grid printer scripts

--- students/nDruP/lesson02/GridPrinterPt02.py
+++ students/nDruP/lesson02/GridPrinterPt02.py
@@ -1,15 +1,27 @@
 """
 Print grid based on given parameter.
 print_grid(3) would print:
++ - + - +
+|   |   |
++ - + - +
+|   |   |
++ - + - +
 
-+ - + - +
-|   |   |
-+ - + - +
-|   |   |
-+ - + - +
+print_grid(8):
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+
 
 print_grid(15) prints:
-
 + - - - - - - - + - - - - - - - +
 |               |               |
 |               |               |
@@ -30,14 +42,15 @@ print_grid(15) prints:
 """
 
 def print_grid(n):
-    printPlusMinus(n)
+    #Print 2x2 grid of size n
+    printTopBottomBorder(n)
     for y in range(2):
-        for x in range(n//2):
-            printBarSpace(n)
-        printPlusMinus(n)
+        printSideBorder(n)
+        printTopBottomBorder(n)
     return
 
-def printPlusMinus(n):
+def printTopBottomBorder(n):
+    #Print 2*(n//2)+3 length Top/Bottom borders of grid spaces 
     print('+', end='')
     for plus in range(2):
         for minus in range(n//2):
@@ -46,12 +59,14 @@ def printPlusMinus(n):
     print()
     return
 
-def printBarSpace(n):
-    print('|',end='')
-    for bar in range(2):
-        print(' '*(n+((n+1)%2)),end='')
+def printSideBorder(n):
+    #Print n//2 length side borders of grid spaces
+    for x in range(n//2):
         print('|',end='')
-    print()
+        for bar in range(2):
+            print(' '*(n+(1-n%2)),end='')
+            print('|',end='')
+        print()
     return
 
 print('print_grid(3)')
--- students/nDruP/lesson02/GridPrinterPt03.py
+++ students/nDruP/lesson02/GridPrinterPt03.py
@@ -24,14 +24,20 @@ For example, print_grid2(3,4) results in:
 """
 
 def print_grid(space, size):
-    printPlusMinus(space,size)
+    """
+    Print a (space x space) grid with (size x size) grid spaces
+    """
+    printTopBottomBorder(space,size)
     for y in range(space):
-        for x in range(size):
-            printBarSpace(space,size)
-        printPlusMinus(space,size)
+        printSideBorder(space,size)
+        printTopBottomBorder(space,size)
     return
 
-def printPlusMinus(space,size):
+def printTopBottomBorder(space,size):
+    """
+    Print a Top/Bottom border(s) for (space) amount of 
+    (size x size) large grid spaces.
+    """
     print('+', end='')
     for plus in range(space):
         for minus in range(size):
@@ -40,12 +46,17 @@ def printPlusMinus(space,size):
     print()
     return
 
-def printBarSpace(space,size):
-    print('|',end='')
-    for bar in range(space):
-        print('  '*size,end='')
-        print(' |',end='')
-    print()
+def printSideBorder(space,size):
+    """
+    Print side border(s) for (space) amount of 
+    (size x size) large grid spaces
+    """
+    for x in range(size):
+        print('|',end='')
+        for bar in range(space):
+            print('  '*size,end='')
+            print(' |',end='')
+        print()
     return
 
 print('print_grid(3.4)')
