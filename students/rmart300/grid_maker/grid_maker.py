9fb78248803184438b964fccb6f1aee7db2009a6
rmart300 <stephen.ross.martin@gmail.com>
2018-01-28 20:12:39 -0800

    updated variable and function names to use underscore delimiters instead of camelcase

--- students/rmart300/grid_maker/grid_maker.py
+++ students/rmart300/grid_maker/grid_maker.py
@@ -1,10 +1,10 @@
 """
-Gridmaker by Ross Martin
+grid_maker by Ross Martin
 UW Python210 Lesson 2 Assignment
 contains three methods to generate custom sized grids
 """
 
-def makeFirstGrid():
+def make_first_grid():
     size = 8
     halfsize = int(size/2)
     print_grid2(2,halfsize)
@@ -15,20 +15,20 @@ def print_grid(size):
     halfsize = int(size/2)
     print_grid2(2,halfsize)
 
-def print_grid2(rowcol,size):
+def print_grid2(rowcol, size):
     """function to print square grid: number of rows and columns and size of each cell of grid are configurable"""
-    lineToPrint = ('+'+'-'*size)*rowcol+'+\n'
+    line_to_print = ('+'+'-'*size)*rowcol+'+\n'
     for i in range(0,rowcol):
         for j in range(0,size):
-            lineToPrint += (('|'+' '*size))*rowcol + '|\n'
-        lineToPrint += ('+'+'-'*size)*rowcol+'+\n'                
+            line_to_print += (('|'+' '*size))*rowcol + '|\n'
+        line_to_print += ('+'+'-'*size)*rowcol+'+\n'                
         
-    print(lineToPrint)
+    print(line_to_print)
 
 
 if __name__ == '__main__':
-    print('make first grid') 
-    makeFirstGrid()
+    print('make_first_grid') 
+    make_first_grid()
     print('\nprint_grid(3)')
     print_grid(3)
     print('\nprint_grid(15)')
