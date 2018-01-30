1d3d7341808966b27172be2a125f34d7d559d0b1
Jerry.H <doridoriae86@gmail.com>
2018-01-29 19:52:00 -0800

    Fixed naming error and changed the indexing for each series so they will match with the solution

--- students/JerryH/Lesson02/series.py
+++ students/JerryH/Lesson02/series.py
@@ -2,7 +2,7 @@ def fibo_series(n):
     """This function prints out the nth digit in the Fibonacci series."""
 
     fibo = [0,1]
-    for i in range(2,n):
+    for i in range(2,n+1):
         x = fibo[i-2] + fibo[i-1]
         fibo.append(x)
     return fibo[n]
@@ -11,12 +11,11 @@ def fibo_series(n):
 def lucas_series(n):
     """This function prints out the nth digit in the Lucas series."""
 
-
     lu = [2,1]
-    for i in range(2,n):
+    for i in range(2,n+1):
         x = lu[i-2] + lu[i-1]
         lu.append(x)
-    return lucas[n]
+    return lu[n]
     #print(lu) #prints the whole series
 
 def sum_series(n, arg1 = 0, arg2 = 1):
@@ -26,16 +25,16 @@ def sum_series(n, arg1 = 0, arg2 = 1):
     produced."""
 
     series = [arg1, arg2]
-    for i in range(2,n):
+    for i in range(2,n+1):
         x = series[i-2] + series[i-1]
         series.append(x)
     return series[n]
 
-# Test FiboSeries(n) is returning the expected value [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+# Test fibo_series(n) is returning the expected value [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 assert fibo_series(5) == 5
 assert fibo_series(10) == 55
 
-# Test Lucas(n) is returning the expected value [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+# Test lucas_series(n) is returning the expected value [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
 assert lucas_series(3) == 4
 assert lucas_series(9) == 76
 
