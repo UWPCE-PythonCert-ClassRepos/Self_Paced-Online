e34bec4310c19f105327eee9a92b26864a10fbaa
Andrew Park <apark46.work@gmail.com>
2018-01-31 03:58:13 -0800

    Lesson03|list_lab.py added documentation to the fruit_series functions.

--- students/nDruP/lesson03/list_lab.py
+++ students/nDruP/lesson03/list_lab.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 def fruit_series1():
+"""
+Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+Display the list (plain old print() is fine…).
+Ask the user for another fruit and add it to the end of the list.
+Display the list.
+Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis). Remember that Python uses zero-based indexing, so you will need to correct.
+Add another fruit to the beginning of the list using “+” and display the list.
+Add another fruit to the beginning of the list using insert() and display the list.
+Display all the fruits that begin with “P”, using a for loop.
+"""
     fruits = ["Apples","Pears","Oranges","Peaches"]
     print(fruits)
     new_fruit = input("I'm hungry. Give me another fruit >")
@@ -22,7 +32,15 @@ def fruit_series1():
     print()
     return fruits
 
+
 def fruit_series2(fruits):
+"""
+Display the list.
+Remove the last fruit from the list.
+Display the list.
+Ask the user for a fruit to delete, find it and delete it.
+(Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+"""
     print(fruits)
     fruits.pop(len(fruits)-1)
     fruits*=2
@@ -40,7 +58,14 @@ def fruit_series2(fruits):
     print(fruits)
     return
 
+
 def fruit_series3(fruits):
+"""
+Ask the user for input displaying a line like “Do you like apples?” for each fruit in the list (making the fruit all lowercase).
+For each “no”, delete that fruit from the list.
+For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values (a while loop is good here)
+Display the list.
+"""
     no_fruits = []
     for like_fruit in fruits:
         yes_no = input("Do you like "+str(like_fruit.lower())+
@@ -56,7 +81,12 @@ def fruit_series3(fruits):
             fruits.remove(rm_fruit)
     print(fruits)
 
+    
 def fruit_series4(fruits):
+"""
+Make a copy of the list and reverse the letters in each fruit in the copy.
+Delete the last item of the original list. Display the original list and the copy.
+"""
     rev_fruits = []
     for tiurf in fruits:
         rev_fruits+=[tiurf[::-1]]
