1a43c3d465da7c21d1006361ca196a454068cf3c
etwum <33198751+etwum@users.noreply.github.com>
2018-01-31 20:06:29 -0800

    fibonacci function complete
    figured out how to do the fibonacci function

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -2,31 +2,36 @@
 def fibonacci(n):
     # fib(n) = fib(n-2) + fib(n-1)
     # 0, 1, 1, 2, 3, 5, 8, 13
+    # starting two numbers of the fibonacci series
     x = 0
     y = 1
+
+    #add numbers to a list
     list = [x,y]
 
-    while len(list) < n:
+    # variable used to keep count of the nth value
+    count = 1
 
-        x1 = list[0] + list[1]
-        #add item to list
-        list.append(x1)
+    while count < n:
 
-        x2 = len(list)
+        x2 = len(list) -2
+        x3 = len(list) - 1
 
-        
+        x4 = list[x2] + list[x3]
+        list.append(x4)
+
+        count += 1
+
+    return print(list[-2])
+
+fibonacci(5)
 
 
-    #calculate nth value in the fibonacci series
 
-
-    return #nth value of the fibonacci series
-
-
-def lucas(n):
+"""def lucas(n):
     return n
 
 
 
 def sum_series(n):
-    return n
+    return n"""
