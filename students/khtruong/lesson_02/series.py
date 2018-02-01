8dfc343e698a36da62f342410b46cdc14f13ff56
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-01-31 23:56:05 -0800

    commit for lesson02 along with user settings for vscode

---
+++ students/khtruong/lesson_02/series.py
@@ -0,0 +1,33 @@
+"""This modules contain three functions that calculates the fibonacci,
+lucas series, and sum series.
+
+"""
+
+
+def fibonacci(nth_element):
+    """Return the nth value in the fibonacci series."""
+
+    return
+
+def lucas(nth_elemnent):
+    """Return the nth value in the lucas series."""
+
+    return
+
+def sum_series(nth_element, _1st=0, _2nd=1):
+    """Return the nth value in the sum series.
+
+    Calling this function with no optional parameters will produce
+    numbers from the fibonacci series. Calling it with the optional
+    arguments 2 and 1 will produce values from the lucas numbers.
+    Other values for the optional parameters will produce other
+    series.
+
+    """
+    if nth_element == 1:
+        sum_series = _1st
+    elif nth_element == 2:
+        sum_series = _2nd
+    else:
+        sum_series = 
+    return
