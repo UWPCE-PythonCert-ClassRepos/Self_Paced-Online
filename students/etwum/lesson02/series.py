c4de52669cf1fb4a4aa3f725cbb31b57ea53361b
etwum <33198751+etwum@users.noreply.github.com>
2018-01-31 19:25:42 -0800

    thinking
    trying to figure out the best way to get the two previous items listed in a list before the current value of a variable

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -2,10 +2,20 @@
 def fibonacci(n):
     # fib(n) = fib(n-2) + fib(n-1)
     # 0, 1, 1, 2, 3, 5, 8, 13
-    list = [0,1]
+    x = 0
+    y = 1
+    list = [x,y]
 
-    x = list[0] + list[1]
-    #add item to list
+    while len(list) < n:
+
+        x1 = list[0] + list[1]
+        #add item to list
+        list.append(x1)
+
+        x2 = len(list)
+
+        
+
 
     #calculate nth value in the fibonacci series
 
