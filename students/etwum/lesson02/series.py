d9c3b63c0880b6b755b13b52253311a73831e56e
etwum <33198751+etwum@users.noreply.github.com>
2018-01-31 20:24:10 -0800

    annotated fibonacci function
    added annotations to the fibonacci series function

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -1,28 +1,37 @@
 
 def fibonacci(n):
-    # fib(n) = fib(n-2) + fib(n-1)
-    # 0, 1, 1, 2, 3, 5, 8, 13
-    # starting two numbers of the fibonacci series
+    # this function finds the nth integer in the fibonacci series
+    # fibonacci series ....0, 1, 1, 2, 3, 5, 8, 13
+    # the next integer is determined by summing the previous two
+
+    # starting two numbers of the fibonacci series (0 and 1)
     x = 0
     y = 1
 
-    #add numbers to a list
-    list = [x,y]
+    # adds the starting two numbers to a list
+    fibonacci_list = [x,y]
 
     # variable used to keep count of the nth value
     count = 1
 
     while count < n:
+        # loops while the variable count is less than the parameter n
 
-        x2 = len(list) -2
-        x3 = len(list) - 1
+        # find the length of the list and substracts 1 and 2
+        # in order to find the previous two numbers in the fibonacci series
 
+        x2 = len(fibonacci_list) -2
+        x3 = len(fibonacci_list) - 1
         x4 = list[x2] + list[x3]
+
+        # add the next number of the fibonacci series to the list
         list.append(x4)
 
+        #iterate the count variable
         count += 1
 
-    return print(list[-2])
+    # return the nth fibonacci integer
+    return print(fibonacci_list[-2])
 
 fibonacci(5)
 
