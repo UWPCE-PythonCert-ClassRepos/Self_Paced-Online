825576c64012a918551d525d510d4cc7cdc3a9d6
johnpharmd <jhumphre@uw.edu>
2018-01-31 18:48:37 -0800

    modified grid_draw.py, added fizz_buzz.py

---
+++ students/johnpharmd/lesson02/fizz_buzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    n = 1
+    while n < 101:
+        if n % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz')
+        elif n % 3 == 0:
+            print('Fizz')
+        elif n % 5 == 0:
+            print('Buzz')
+        else:
+            print(n)
+        n += 1
+
