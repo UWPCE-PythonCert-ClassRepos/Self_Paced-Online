92395c5dbd04fd3a2fae4fcf549715bed5d94c5b
etwum <33198751+etwum@users.noreply.github.com>
2018-01-31 18:53:20 -0800

    thinking
    thinking

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -1,1 +1,22 @@
 
+def fibonacci(n):
+    # fib(n) = fib(n-2) + fib(n-1)
+    # 0, 1, 1, 2, 3, 5, 8, 13
+    list = [0,1]
+
+    x = list[0] + list[1]
+    #add item to list
+
+    #calculate nth value in the fibonacci series
+
+
+    return #nth value of the fibonacci series
+
+
+def lucas(n):
+    return n
+
+
+
+def sum_series(n):
+    return n
