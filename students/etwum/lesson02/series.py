663fe6ff6dfcb937267321dfa3e26e416104317c
etwum <33198751+etwum@users.noreply.github.com>
2018-02-01 18:12:09 -0800

    fibonacci function update and lucas function creation
    updated the list names to fibonacci_list
    
    copied code from the fibonacci series function to the lucas series function..edited it where needed

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -22,10 +22,10 @@ def fibonacci(n):
 
         x2 = len(fibonacci_list) -2
         x3 = len(fibonacci_list) - 1
-        x4 = list[x2] + list[x3]
+        x4 = fibonacci_list[x2] + fibonacci_list[x3]
 
         # add the next number of the fibonacci series to the list
-        list.append(x4)
+        fibonacci_list.append(x4)
 
         #iterate the count variable
         count += 1
@@ -37,10 +37,43 @@ fibonacci(5)
 
 
 
-"""def lucas(n):
-    return n
+def lucas(n):
+    # this function finds the nth integer in the lucas series
+    # lucas series ....2, 1, 3, 4, 7, 11, 18, 29
+    # the next integer is determined by summing the previous two
+
+    # starting two numbers of the fibonacci series (0 and 1)
+    x = 2
+    y = 1
+
+    # adds the starting two numbers to a list
+    lucas_list = [x,y]
+
+    # variable used to keep count of the nth value
+    count = 1
+
+    while count < n:
+        # loops while the variable count is less than the parameter n
+
+        # find the length of the list and substracts 1 and 2
+        # in order to find the previous two numbers in the fibonacci series
+
+        x2 = len(lucas_list) -2
+        x3 = len(lucas_list) - 1
+        x4 = lucas_list[x2] + lucas_list[x3]
+
+        # add the next number of the fibonacci series to the list
+        lucas_list.append(x4)
+
+        #iterate the count variable
+        count += 1
+
+    # return the nth fibonacci integer
+    return print(lucas_list[-2])
+
+lucas(5)
 
 
 
-def sum_series(n):
+"""def sum_series(n):
     return n"""
