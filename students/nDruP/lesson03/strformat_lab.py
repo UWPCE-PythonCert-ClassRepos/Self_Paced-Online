11faaf1d3340a3fc98514f61a1615dbc31ed9594
Andrew Park <apark46.work@gmail.com>
2018-02-02 01:40:43 -0800

    Lesson03|strformat_lab.py changed format_quadruple to take a tuple as an input. Made the test statements cleaner. Tried making format_quadruple_alt w/ f-strings; so far unsuccessful

--- students/nDruP/lesson03/strformat_lab.py
+++ students/nDruP/lesson03/strformat_lab.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
-
-def format_quadruple(file_num, float_2dec, sci_int, sci_3float):
+def format_quadruple(quadruple):
     """
     The first element is used to generate a filename that can help with file sorting.
     The second element is a floating point number. You should display it with 2 decimal places shown.
     The third value is an integer, but could be any number. You should display it in scientific notation, with 2 decimal places shown.
     The fourth value is a float with a lot of digits – display it in scientific notation with 3 significant figures.
     """
-    return "file_{:03} :   {:.2f}, {:.2e}, {:.2e}".format(file_num, float_2dec, sci_int, sci_3float)
+    return "file_{0[0]:03} :   {0[1]:.2f}, {0[2]:.2e}, {0[3]:.2e}".format(quadruple)
 
+def format_quadruple_alt(quadruple):
+    """
+    Use an alternate type of format string to replicate results of format_quadruple. (keywords/f-strings)
+    """
+    return f"file_{file_num:03} :   {floats_2dec:.2f}, {int_sci_2dec:.2e}, {float_sci_3sig:.2e}"
 
-assert format_quadruple(2,123.4567,10000,12345.67) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-assert format_quadruple(12,3.141592654,-1234,.123456) == 'file_012 :   3.14, -1.23e+03, 1.23e-01' 
-assert format_quadruple(100,13,0,0) == 'file_100 :   13.00, 0.00e+00, 0.00e+00'
+quadruple1 = (2,123.4567,10000,12345.67)
+quadruple2 = (12,3.141592654,-1234,.123456)
+quadruple3 = (100,13,0,0)
+
+assert format_quadruple(quadruple1) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+assert format_quadruple(quadruple2) == 'file_012 :   3.14, -1.23e+03, 1.23e-01' 
+assert format_quadruple(quadruple3) == 'file_100 :   13.00, 0.00e+00, 0.00e+00'
+
+assert format_quadruple_alt(quadruple1) == format_quadruple(quadruple1)
+assert format_quadruple_alt(quadruple2) == format_quadruple(quadruple2)
+assert format_quadruple_alt(quadruple3) == format_quadruple(quadruple3)
