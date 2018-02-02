9fead9abc9c37fa0b8c7a14941cbae8aecd4615f
etwum <33198751+etwum@users.noreply.github.com>
2018-02-01 19:42:40 -0800

    added assert tests
    added asserts statements to the end of the program to test the functions

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -33,7 +33,6 @@ def fibonacci(n):
     # return the nth fibonacci integer
     return print(fibonacci_list[-2])
 
-fibonacci(5)
 
 
 
@@ -71,15 +70,12 @@ def lucas(n):
     # return the nth lucas integer
     return print(lucas_list[-2])
 
-lucas(5)
 
 
 
 def sum_series(n ,y = 0, z = 1):
-    # this function finds the nth integer in the lucas series
-    # lucas series ....2, 1, 3, 4, 7, 11, 18, 29
+    # this function finds the nth integer in any series based on the input parameters
     # the next integer is determined by summing the previous two
-
 
     # adds the starting two numbers to a list
     series_list = [y,z]
@@ -91,20 +87,44 @@ def sum_series(n ,y = 0, z = 1):
         # loops while the variable count is less than the parameter n
 
         # find the length of the list and substracts 1 and 2
-        # in order to find the previous two numbers in the lucas series
+        # in order to find the previous two numbers in the series
 
         x2 = len(series_list) -2
         x3 = len(series_list) - 1
         x4 = series_list[x2] + series_list[x3]
 
-        # add the next number of the lucas series to the list
+        # add the next number of the series to the list
         series_list.append(x4)
 
         # iterate the count variable
         count += 1
 
-    # return the nth lucas integer
+    # return the nth integer
     return print(series_list[-2])
 
 
-sum_series(3,5,6)
+while True:
+    #Test functions
+    x = int(input("input a number"))
+    y = int(input("input a number"))
+    z = int(input("input a number"))
+
+    # test to make sure the value of x is greater than zero
+    assert x > 0
+
+    # test to make sure the values of y + z are greater than zero for the sum_series function
+    assert y + z > 0
+
+    fibonacci(x)
+    lucas(x)
+    sum_series(x,y,z)
+
+    break
+
+
+
+
+
+
+
+
