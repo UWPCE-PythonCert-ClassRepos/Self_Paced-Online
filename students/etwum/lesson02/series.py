03db4d4616f8bd66072f0e2b4f61b26b2a65bc92
etwum <33198751+etwum@users.noreply.github.com>
2018-02-01 18:25:45 -0800

    create sum_series function
    copied previous code used but generalized it with three parameters

--- students/etwum/lesson02/series.py
+++ students/etwum/lesson02/series.py
@@ -42,7 +42,7 @@ def lucas(n):
     # lucas series ....2, 1, 3, 4, 7, 11, 18, 29
     # the next integer is determined by summing the previous two
 
-    # starting two numbers of the fibonacci series (0 and 1)
+    # starting two numbers of the lucas series (0 and 1)
     x = 2
     y = 1
 
@@ -56,24 +56,55 @@ def lucas(n):
         # loops while the variable count is less than the parameter n
 
         # find the length of the list and substracts 1 and 2
-        # in order to find the previous two numbers in the fibonacci series
+        # in order to find the previous two numbers in the lucas series
 
         x2 = len(lucas_list) -2
         x3 = len(lucas_list) - 1
         x4 = lucas_list[x2] + lucas_list[x3]
 
-        # add the next number of the fibonacci series to the list
+        # add the next number of the lucas series to the list
         lucas_list.append(x4)
 
-        #iterate the count variable
+        # iterate the count variable
         count += 1
 
-    # return the nth fibonacci integer
+    # return the nth lucas integer
     return print(lucas_list[-2])
 
 lucas(5)
 
 
 
-"""def sum_series(n):
-    return n"""
+def sum_series(n ,y = 0, z = 1):
+    # this function finds the nth integer in the lucas series
+    # lucas series ....2, 1, 3, 4, 7, 11, 18, 29
+    # the next integer is determined by summing the previous two
+
+
+    # adds the starting two numbers to a list
+    series_list = [y,z]
+
+    # variable used to keep count of the nth value
+    count = 1
+
+    while count < n:
+        # loops while the variable count is less than the parameter n
+
+        # find the length of the list and substracts 1 and 2
+        # in order to find the previous two numbers in the lucas series
+
+        x2 = len(series_list) -2
+        x3 = len(series_list) - 1
+        x4 = series_list[x2] + series_list[x3]
+
+        # add the next number of the lucas series to the list
+        series_list.append(x4)
+
+        # iterate the count variable
+        count += 1
+
+    # return the nth lucas integer
+    return print(series_list[-2])
+
+
+sum_series(3,5,6)
