bef70cb4ad651ed52be7fbf031d526c17a720406
22e88 <22e88@mailbox.org>
2018-02-02 05:36:40 +0100

    added grid_printer.py

---
+++ students/elmar_m/lesson02/grid_printer.py
@@ -0,0 +1,11 @@
+def printer():
+    print('hallo printer')
+
+def testfunc():
+    print('hallo ich bin die testfunc')
+
+def zwei():
+    print('2222222')
+
+if __name__ == '__main__':
+    print('i wanna be a module, please import me!')
