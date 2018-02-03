8597043ed759ed0e9c50586b2fa60fcae5cdfaec
Dustin L <r.dustin.lane@gmail.com>
2018-02-03 14:35:37 -0800

    Add series_two() function

--- students/Dustin_L/lesson03/list_lab.py
+++ students/Dustin_L/lesson03/list_lab.py
@@ -52,7 +52,45 @@ def series_one(lst):
     return l
 
 
+def series_two(lst):
+    """Performs a set of operations on list of fruit.
+
+    A deepy copy is made of the passed list. The last item is removed from this
+    copy. The user is then prompted to selet an item to be removed from list and
+    all specified items are removed. If none are found, the user is continually
+    prompted until the item is found in the list.
+
+    Args:
+        lst (list): List to be operated upon.
+
+    Returns:
+        list: Modified list.
+    """
+    l = copy.deepcopy(lst)
+    print(l)
+
+    # Remove last item
+    l.pop()
+    print(l)
+
+    # Prompt user for fruit and then delete all occurences from list
+    l *= 2
+    while True:
+        usr_in      = input('Enter fruit to remove from list' + PROMPT)
+        removed_lst = [i for i in l if i.lower() != usr_in.lower()]
+
+        if removed_lst != l:
+            l = removed_lst
+            print(l)
+            break
+        else:
+            print('Specified fruit is NOT in list!')
+
+    return l
+
+
 if __name__ == '__main__':
     fruit_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
     series_one(fruit_list)
+    series_two(fruit_list)
