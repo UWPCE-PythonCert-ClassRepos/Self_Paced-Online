bd33f6eea98ca2e7fd27dd6a9f1a65c86e40acaa
Dustin L <r.dustin.lane@gmail.com>
2018-02-03 11:50:56 -0800

    Add first_last_swap() function

--- students/Dustin_L/lesson03/slicing.py
+++ students/Dustin_L/lesson03/slicing.py
@@ -5,3 +5,20 @@ This module contains various slicing functions.
 """
 
 
+def first_and_last_swap(seq):
+    """Swap first and last item of a sequence
+
+    Args:
+        seq (list): Sequence to be operated upon
+
+    Returns:
+        list: Copy of sequence with first and last items swapped
+    """
+    return seq[-1:] + seq[1:-1] + seq[:1]
+
+
+if __name__ == '__main__':
+    test_string = 'This is my test string'
+
+    assert (first_and_last_swap(test_string) == 'ghis is my test strinT'), \
+        "first_and_last_swap() failed"
