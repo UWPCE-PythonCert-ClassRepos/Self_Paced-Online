444ee2e035e26e6456345016ef3367c32b07899c
Dustin L <r.dustin.lane@gmail.com>
2018-02-03 12:28:55 -0800

    Updated function docstrings

--- students/Dustin_L/lesson03/slicing.py
+++ students/Dustin_L/lesson03/slicing.py
@@ -9,10 +9,10 @@ def first_and_last_swap(seq):
     """Swap first and last item of a sequence
 
     Args:
-        seq (list): Sequence to be operated upon
+        seq (sequence): Sequence to be operated upon
 
     Returns:
-        list: Copy of sequence with first and last items swapped
+        sequence: Copy of sequence with first and last items swapped
     """
     return seq[-1:] + seq[1:-1] + seq[:1]
 
@@ -21,10 +21,10 @@ def remove_every_other(seq):
     """Remove every other item from sequence
 
     Args:
-        seq (list): Sequence to be operated upon
+        seq (sequence): Sequence to be operated upon
 
     Returns:
-        list: Copy of sequence with every other item removed
+        sequence: Copy of sequence with every other item removed
     """
     return seq[::2]
 
@@ -33,10 +33,10 @@ def first_last_four_every_other(seq):
     """Remove first and last four items and return every other item from the remaining items.
 
     Args:
-        seq (list): Sequence to be operated upon.
+        seq (sequence): Sequence to be operated upon.
 
     Returns:
-        list: Copy of sequence with first and last four items removed and every
+        sequence: Copy of sequence with first and last four items removed and every
         other remaining item selected.
     """
     return seq[4:-4:2]
@@ -46,10 +46,10 @@ def reverse(seq):
     """Reverse the sequence.
 
     Args:
-        seq (list): Sequence to be operated upon.
+        seq (sequence): Sequence to be operated upon.
 
     Returns:
-        list: Copy of reversed sequence.
+        sequence: Copy of reversed sequence.
     """
     return seq[::-1]
 
