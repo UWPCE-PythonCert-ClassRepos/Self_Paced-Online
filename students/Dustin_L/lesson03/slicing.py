234dd2a5ce6fa853f8e33e705b9a4757c0f69710
Dustin L <r.dustin.lane@gmail.com>
2018-02-03 12:15:51 -0800

    Add first_last_four_every_other() function

--- students/Dustin_L/lesson03/slicing.py
+++ students/Dustin_L/lesson03/slicing.py
@@ -29,10 +29,25 @@ def remove_every_other(seq):
     return seq[::2]
 
 
+def first_last_four_every_other(seq):
+    """Remove first and last four items and return every other item from the remaining items.
+
+    Args:
+        seq (list): Sequence to be operated upon.
+
+    Returns:
+        list: Copy of sequence with first and last four items removed and every
+        other remaining item selected.
+    """
+    return seq[4:-4:2]
+
+
 if __name__ == '__main__':
     test_string = 'This is my test string'
 
-    assert (first_and_last_swap(test_string) == 'ghis is my test strinT'), \
+    assert(first_and_last_swap(test_string) == 'ghis is my test strinT'), \
         'first_and_last_swap() failed'
-    assert (remove_every_other(test_string) == 'Ti sm etsrn'), \
+    assert(remove_every_other(test_string) == 'Ti sm etsrn'), \
         'remove_every_other() failed'
+    assert(first_last_four_every_other(test_string) == ' sm ets'), \
+        'first_last_four_every_other() failed'
