85528564fefeb5d21deab1f5d661ee8c63f5efeb
22e88 <22e88@mailbox.org>
2018-02-04 15:41:18 +0100

    added lesson02/fizz_buzz.py

---
+++ students/elmar_m/lesson02/fizz_buzz.py
@@ -0,0 +1,9 @@
+'''
+elmar_m / 22e88@mailbox.org
+-------------------------------
+Lesson02:  Fizz Buzz Exercise
+'''
+
+
+if __name__ == '__main__':
+    print('i wanna be a module, please import me!')
