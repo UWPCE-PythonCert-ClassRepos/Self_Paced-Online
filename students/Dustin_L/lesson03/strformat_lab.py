a73af1170bcec4fc0b6528ac086cdfbe9f590815
Dustin L <r.dustin.lane@gmail.com>
2018-02-04 11:44:13 -0800

    Add task_one() function

--- students/Dustin_L/lesson03/strformat_lab.py
+++ students/Dustin_L/lesson03/strformat_lab.py
@@ -4,5 +4,19 @@
 This module contains all of the functions for the String Format Lab.
 """
 
+
+def task_one(quad):
+    """Return formatted string.
+
+    Args:
+        quad (tuple): Tuple of 4 elements.
+
+    Returns:
+        str: Formatted string.
+    """
+    fmt_str = 'file_{:0>3d}: {:.2f}, {:.2e}, {:.3e}'
+    return fmt_str.format(*quad)
+
+
 if __name__ == '__main__':
-    pass
+    print(task_one((2, 123.4567, 10000, 12345.67)))
