ed70803da815c3e315536c28de07dc045fdac9ff
ZhuojingXie <llxllxzj@gmail.com>
2018-02-04 10:33:28 -0800

    update print grid exercise

--- students/ZhuojingXie/lesson02/printer_exercise.py
+++ students/ZhuojingXie/lesson02/printer_exercise.py
@@ -1,24 +1,24 @@
-def draw_line1(x=5, y =2):
+def draw_line1(x=3, y =2):
     i = 0
     while i <y:
         print('+',end = ' ')
         print('- '*x, end = ' ')
         i +=1
     print ('+')
-# draw first line, and set default size =5 and unit =2
+# draw first line, and set default size =3 and unit =2
 
 
-def draw_line2(x=5, y=2):
+def draw_line2(x=3, y=2):
     i=0
     while i<y:
         print('|', end = ' ')
         print('  '*x, end =' ')
         i +=1
     print ('|')
-# draw second line, and set default size =5 and unit =2
+# draw second line, and set default size =3 and unit =2
 
 
-def print_grid(n):
+def print_grid(n=3):
     for y in range (0, 2):
         draw_line1(n)
         for x in range(0, n):
@@ -31,3 +31,13 @@ def print_grid2(m, n):
         for x in range(0, n):
             draw_line2(n,m)
     draw_line1(n,m)
+
+
+
+print_grid()  #print 2*2 unit with size 3 by using print_grid() at default setting
+
+print_grid(3)
+print_grid(8) #check out the print_grid() functiong
+
+print_grid2(3,4)
+print_grid2(6,3) #check out the prin_grid() function
