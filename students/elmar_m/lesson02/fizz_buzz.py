cd666096bafd8ffa32c2f9704bf4976e9523efde
22e88 <22e88@mailbox.org>
2018-02-04 16:03:24 +0100

    fizz_buzz.py: added modulo check for both 3 and 5

--- students/elmar_m/lesson02/fizz_buzz.py
+++ students/elmar_m/lesson02/fizz_buzz.py
@@ -7,13 +7,15 @@ Lesson02:  Fizz Buzz Exercise
 
 for i in range(1,101):
     # print(i)
-    if i % 3 == 0:
-        print('fizz')
+    # if i % 3 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
     elif i % 5 == 0:
-        print('buzz')
+        print('Buzz')
     else:
         print(i)
-
 
 if __name__ == '__main__':
     print('i wanna be a module, please import me!')
