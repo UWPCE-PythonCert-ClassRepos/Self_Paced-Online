525a1b0ce8621bbbfac8bcef35f4efda0b7b97b7
Travis Brackney <tbrackney@gmail.com>
2018-02-03 21:01:17 -0800

    Added Lesson 2 assignment

---
+++ students/tbrackney/Lesson2/lesson2.py
@@ -0,0 +1,34 @@
+"""
+File Name: Lesson2.py
+Author: Travis Brackney
+Class: Python 201 - Self paced online
+Date Created 2/3/2018
+Python Version: 3.6.4
+"""
+
+
+def print_grid(n):
+    """ Prints a simple grid"""
+    spaces = n // 2
+    segment = '+ ' + '- ' * spaces
+    border = 2 * segment + '+'
+    mid_segment = '| ' + (spaces * '  ')
+    mid_line = 2 * mid_segment + '|'
+    for i in range(2):
+        print(border)
+        for j in range(spaces):
+            print(mid_line)
+    print(border)
+
+
+def print_grid2(cell_count, cell_length):
+    spaces = cell_length
+    segment = '+ ' + '- ' * spaces
+    border = cell_count * segment + '+'
+    mid_segment = '| ' + (spaces * '  ')
+    mid_line = cell_count * mid_segment + '|'
+    for i in range(cell_count):
+        print(border)
+        for j in range(spaces):
+            print(mid_line)
+    print(border)
---
+++ students/tbrackney/lesson01/break_me.py
@@ -0,0 +1,7 @@
+def name_error():
+    print(b)
+
+def type_error():
+    a = "string"
+    b = a / 5
+    return b
