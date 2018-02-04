1029eb500897e8c7d2822482d0343b6fba6d6fe5
rmart300 <stephen.ross.martin@gmail.com>
2018-02-04 09:45:23 -0800

    added task3

--- students/rmart300/lesson03/strformat_lab.py
+++ students/rmart300/lesson03/strformat_lab.py
@@ -27,9 +27,17 @@ def task1(a):
     print(out_string)
     return out_string
 
+#same as task1, but using fstring
 def task2(a):
     (lead_zero, float_value, scientific1, scientific2) = format_numbers(a)
     out_string = f"file_{lead_zero} :   {float_value}, {scientific1}, {scientific2}"
+    print(out_string)
+    return out_string
+
+#build dynamic list
+def task3(a):
+    num_string = ', '.join(str(num) for num in a)
+    out_string = "the numbers are: {}".format(num_string)
     print(out_string)
     return out_string
 
@@ -37,4 +45,9 @@ if __name__ == '__main__':
     a = ( 2, 123.4567, 10000, 12345.67 )
     assert task1(a) == 'file_002 :   123.46, 1.00e+04, 1.235e+04'
     assert task2(a) == 'file_002 :   123.46, 1.00e+04, 1.235e+04'    
-    print('all tests passed'
+    b = [ 1, 2, 5, 7 ]
+    assert task3(b) == 'the numbers are: 1, 2, 5, 7'
+
+    print('all tests passed')
+
+
