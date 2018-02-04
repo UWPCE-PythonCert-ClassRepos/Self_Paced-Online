82fd9f8568ebdd5f16282001c16e065efd3dee08
Dustin L <r.dustin.lane@gmail.com>
2018-02-04 12:00:01 -0800

    Add task_three() function

--- students/Dustin_L/lesson03/strformat_lab.py
+++ students/Dustin_L/lesson03/strformat_lab.py
@@ -30,6 +30,25 @@ def task_two(quad):
     return f'file_{quad[0]:0>3d}: {quad[1]:.2f}, {quad[2]:.2e}, {quad[3]:.3e}'
 
 
+def task_three(input_tup):
+    """Return formatted string containing all values from the passed tuple.
+
+    The string format is: 'the x numbers are: a, b, c...'
+
+    Args:
+        input_tup (tuple): Tuple containing values to be entered in string.
+
+    Returns:
+        str: Formatted string.
+    """
+    l = len(input_tup)
+    num_fmt = ('{:d}, ' * (l - 1)) + '{:d}'
+    num_str = num_fmt.format(*input_tup)
+
+    return f'the {l} numbers are: {num_str}'
+
+
 if __name__ == '__main__':
     print(task_one((2, 123.4567, 10000, 12345.67)))
     print(task_two((2, 123.4567, 10000, 12345.67)))
+    print(task_three((1, 2, 3, 4, 5, 6, 7, 3, 1, 0, 100)))
