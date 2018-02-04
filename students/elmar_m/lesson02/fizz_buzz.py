56255607818d7905e5bdb71bca2580bb7fbba2ae
22e88 <22e88@mailbox.org>
2018-02-04 15:56:46 +0100

    fizz_buzz.py: added modulo check 3 and 5

--- students/elmar_m/lesson02/fizz_buzz.py
+++ students/elmar_m/lesson02/fizz_buzz.py
@@ -6,7 +6,13 @@ Lesson02:  Fizz Buzz Exercise
 
 
 for i in range(1,101):
-    print(i)
+    # print(i)
+    if i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
 
 
 if __name__ == '__main__':
