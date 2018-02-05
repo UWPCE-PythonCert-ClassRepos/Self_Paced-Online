9a51d286de41888913692a584a5d64e251afa9cd
csdotson <csdotson1@gmail.com>
2018-02-05 07:59:54 -0800

    Add skeleton for series.py

--- students/csdotson/lesson02/series.py
+++ students/csdotson/lesson02/series.py
@@ -1,1 +1,23 @@
 # Lesson 2 - Computing Fibonacci and Lucas Series
+
+def fibonacci(n):
+    """Compute Fibonacci Series, return the nth value"""
+    first_num = 0
+    second_num = 1
+    # Return the nth value of Fib series
+
+
+def lucas(n):
+    """Compute Lucas Series, return the nth value"""
+    first_num = 2
+    second_num = 1
+    # Returns nth value of the lucas number Series
+
+
+def sum_series(n, first_num = 0, second_num = 1):
+    # Add docstring
+    # Required parameter, n, specifies which element to return
+    # Optional parameters, first_num and second_num, sepcify the first two numbers of the series - whether we're looking at a Fib or Lucas series
+
+
+# Add block of code to test your functions - write a series of 'assert' statements to accomplish this
