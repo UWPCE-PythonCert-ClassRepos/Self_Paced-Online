8e2d6ced28efeba38dcdd6440270192746b09cf2
Tri Nguyen <tricaonguyen.nguyen@gmail.com>
2018-02-05 01:41:46 -0800

    upload updates and series.py

--- students/tri_nguyen/lesson02/fizz_buzz.py
+++ students/tri_nguyen/lesson02/fizz_buzz.py
@@ -10,10 +10,7 @@
 # For numbers which are multiple of both three and five print
 # 'FizzBuzz' instead
 
-# Declare variables
-num_range = range(1, 101)
-
-for number in num_range:
+for number in range(1, 101):
     if (number % 3 == 0) and (number % 5 == 0):
         print('FizzBuzz')
     elif (number % 3 == 0):
