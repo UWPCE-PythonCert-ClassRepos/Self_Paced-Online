b20d95552006455c0188e60721cc61e80bb83256
22e88 <22e88@mailbox.org>
2018-02-06 04:16:30 +0100

    series.py: first working fib function

--- students/elmar_m/lesson02/series.py
+++ students/elmar_m/lesson02/series.py
@@ -4,39 +4,25 @@ elmar_m / 22e88@mailbox.org
 Lesson02:  Fibonacci Series Exercise
 '''
 
-'''
-def fibonacci(n):
+def fib(n):
+    if n == 0:
+        print('value 0 in Fibonacci series: 0')
+    elif n == 1:
+        print('value 1 in Fibonacci series: 1')
+    else:
+        counter = 0
+        valuelist= [0, 1]
+        c = None
 
-def series(n, v1=1, v2=2):
-    value_1 = 0
-    value_2  = 1
-    value_3 = value_1 + value_2
-    print(value_3)
-    value_1 = value_2
-    value_2 = value_3
+        while counter <= n - 2:
+            a = valuelist[0]
+            b = valuelist[1]
+            c = a + b
+            valuelist[0] = b
+            valuelist[1] = c
+            counter += 1
 
-# def calc(a, b, c=(a + b)):
-#     # c = a + b
-#     return(c)
-
-def test():
-    print('hello world')
-'''
-def create_series(n):
-    counter = 0
-    mylist = [0, 1]
-    c = None
-
-    while counter <= n:
-        a = mylist[0]
-        b = mylist[1]
-        c = a + b
-        print(c)
-        mylist[0] = b
-        mylist[1] = c
-        counter += 1
-
-    print('result:', c)
+        print('value', n,  'in Fibonacci series:', c)
 
 
 
