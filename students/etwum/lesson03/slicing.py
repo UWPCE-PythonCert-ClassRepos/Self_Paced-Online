90b86c4f99a9f2e7c71617afe7fe5c98b6e2532b
etwum <33198751+etwum@users.noreply.github.com>
2018-02-05 18:47:01 -0800

    created slicing file
    created slicing file and began coding by adding functions

---
+++ students/etwum/lesson03/slicing.py
@@ -0,0 +1,31 @@
+
+def exchange_first_last(seq):
+
+    if seq == type(str):
+        x = len(seq)
+        y = seq[1:x] + seq[0:1]
+
+    """x = (1,2,3,4)
+    z = "Hello World"
+    y = list(x)"""
+    return print(y)
+
+
+def remove_every_other():
+    return
+
+
+def first_last_four_every_other_remove():
+    return
+
+
+
+def reverse():
+    return
+
+
+def middle_last_first():
+    return
+
+
+exchange_first_last("hello world")
