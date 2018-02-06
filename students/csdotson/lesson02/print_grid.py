35f95023f9942176abf54b6b23c0399996d4291d
csdotson <csdotson1@gmail.com>
2018-02-05 20:57:31 -0800

    Add print_grid function to print 2 x 2 grid

--- students/csdotson/lesson02/print_grid.py
+++ students/csdotson/lesson02/print_grid.py
@@ -1,1 +1,18 @@
 # Lesson 2 Assignment - Grid Printer
+
+def print_grid(n):
+    # Define rows to print
+    horizontal = ("+ " + ("- " * n)) * 2 + "+"
+    vertical = ("|" + (" " * (2*n + 1))) * 2 + "|"
+
+    # Do the printing
+    print(horizontal)
+    for i in range(n):
+        print(vertical)
+    print(horizontal)
+    for i in range(n):
+        print(vertical)
+    print(horizontal)
+
+
+def print_grid2():
