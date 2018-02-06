22aa1b78fdc834aa60ec72d6289b9e63fee336c0
etwum <33198751+etwum@users.noreply.github.com>
2018-02-05 20:08:53 -0800

    first function complete
    finished coding and testing exchange_first_last function

--- students/etwum/lesson03/slicing.py
+++ students/etwum/lesson03/slicing.py
@@ -2,15 +2,24 @@
 def exchange_first_last(seq):
     if type(seq) is str:
         y = seq[-1] + seq[1:-1] + seq[0]
-        new_sequence = y
-        return print(new_sequence)
+        new_string = y
+        return print(new_string)
 
     elif type(seq) is tuple:
         x = list(seq)
-        y = x[1]
-        z = x[-1]
+        last_item = [x[-1]]
+        middle_items = x[1:-1]
+        first_item = [x[0]]
+        new_list = last_item + middle_items + first_item
+        new_tuple = tuple(new_list)
+        return print(new_tuple)
 
-
+    else:
+        last_item2 = [seq[-1]]
+        middle_items2 = seq[1:-1]
+        first_item2 = [seq[0]]
+        new_list2 = last_item2 + middle_items2 + first_item2
+        return print(new_list2)
 
 
 def remove_every_other():
@@ -29,6 +38,6 @@ def reverse():
 def middle_last_first():
     return
 
-hello = "hello world"
+hello = [12,1,4,5,7,6,10]
 
 exchange_first_last(hello)
