8c7068d19dfe43137235dfb93705d8869cee42de
csdotson <csdotson1@gmail.com>
2018-02-06 07:39:57 -0800

    Add docstring and streamline print_grid logic to be consistent with print_grid2

--- students/csdotson/lesson02/print_grid.py
+++ students/csdotson/lesson02/print_grid.py
@@ -1,22 +1,23 @@
 # Lesson 2 Assignment - Grid Printer
 
 def print_grid(n):
-    # Define rows to print
+    """Print 2 x 2 grid with cell size n"""
     horizontal = ("+ " + ("- " * n)) * 2 + "+"
     vertical = ("|" + (" " * (2 * n + 1))) * 2 + "|"
 
     # Do the printing
     print(horizontal)
-    for i in range(n):
-        print(vertical)
-    print(horizontal)
-    for i in range(n):
-        print(vertical)
-    print(horizontal)
+    for j in range(2):
+        for i in range(n):
+            print(vertical)
+            if i == (n - 1):
+                print(horizontal)
+            else:
+                continue
 
 
 def print_grid2(grid_dim, cell_size):
-    # Define rows to print
+    """Print grid_dim x grid_dim grid with given cell_size"""
     horizontal = ("+ " + ("- " * cell_size)) * grid_dim + "+"
     vertical = ("|" + (" " * (2 * cell_size + 1))) * grid_dim + "|"
 
