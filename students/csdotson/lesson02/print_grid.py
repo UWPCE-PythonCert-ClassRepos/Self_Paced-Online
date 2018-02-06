7c0c0478ccd2effaae8bb8d734e233e32ae70963
csdotson <csdotson1@gmail.com>
2018-02-06 07:29:31 -0800

    Complete print_grid2 function

--- students/csdotson/lesson02/print_grid.py
+++ students/csdotson/lesson02/print_grid.py
@@ -3,7 +3,7 @@
 def print_grid(n):
     # Define rows to print
     horizontal = ("+ " + ("- " * n)) * 2 + "+"
-    vertical = ("|" + (" " * (2*n + 1))) * 2 + "|"
+    vertical = ("|" + (" " * (2 * n + 1))) * 2 + "|"
 
     # Do the printing
     print(horizontal)
@@ -15,4 +15,17 @@ def print_grid(n):
     print(horizontal)
 
 
-def print_grid2():
+def print_grid2(grid_dim, cell_size):
+    # Define rows to print
+    horizontal = ("+ " + ("- " * cell_size)) * grid_dim + "+"
+    vertical = ("|" + (" " * (2 * cell_size + 1))) * grid_dim + "|"
+
+    # Do the printing
+    print(horizontal)
+    for j in range(grid_dim):
+        for i in range(cell_size):
+            print(vertical)
+            if i == (cell_size - 1):
+                print(horizontal)
+            else:
+                continue
