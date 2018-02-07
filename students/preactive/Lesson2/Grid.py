5326bdf1c9d060687666d43cc138f81fc539917d
Michael Messmer <michael.messmer@oracle.com>
2018-02-07 10:27:01 -0800

    lesson 2 update

--- students/preactive/Lesson2/Grid.py
+++ students/preactive/Lesson2/Grid.py
@@ -1,2 +1,22 @@
 def printgrid(gridSize,numOfCubes):
-    for xy in gridSize:
+    for x in range(0, numOfCubes):
+        for y in range(0,gridSize):
+            if y == 0:
+                print("+ " + "- " * gridSize + "+ ", end="")
+
+            elif y >= 1 and y != gridSize - 1:
+                print("- " * gridSize + "+ ", end="")
+            else:
+                print("- " * gridSize + "+")
+        #for y in range(0,numOfCubes):
+        #    if x > 0:
+        #        if y == 0 or y == gridSize:
+        #            print("- " * gridSize + "+", end="")
+        #        else:
+        #            print(" " * gridSize + "|", end="")
+        #if x == gridSize - 1:
+        #    print("+ " + "- " * gridSize + "+")
+        #if x == gridSize - 1:
+        #    print("- " * gridSize + "+")
+
+printgrid(3,3)
