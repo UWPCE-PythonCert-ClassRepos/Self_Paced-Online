39d9445109d1e644f852741336a966aa24107344
Michael Messmer <preactive@gmail.com>
2018-02-07 14:29:38 -0800

    more work on lesson two

--- students/preactive/Lesson2/Grid.py
+++ students/preactive/Lesson2/Grid.py
@@ -1,22 +1,31 @@
-def printgrid(gridSize,numOfCubes):
-    for x in range(0, numOfCubes):
+def printgrid(gridSize,cubesXY):
+    for x in range(0, cubesXY):
         for y in range(0,gridSize):
             if y == 0:
-                print("+ " + "- " * gridSize + "+ ", end="")
+                if cubesXY == 1:
+                    print("+ " + "- " * gridSize + "+ ")
+                if cubesXY > 1:
+                    if x == 0:
+                        print("+ " + "- " * gridSize + "+ ", end="")
+                    elif x < cubesXY - 1:
+                        print("- " * gridSize + "+", end="")
+                    else:
+                        print("- " * gridSize + "+")
 
-            elif y >= 1 and y != gridSize - 1:
-                print("- " * gridSize + "+ ", end="")
-            else:
-                print("- " * gridSize + "+")
-        #for y in range(0,numOfCubes):
-        #    if x > 0:
-        #        if y == 0 or y == gridSize:
-        #            print("- " * gridSize + "+", end="")
-        #        else:
-        #            print(" " * gridSize + "|", end="")
-        #if x == gridSize - 1:
-        #    print("+ " + "- " * gridSize + "+")
-        #if x == gridSize - 1:
-        #    print("- " * gridSize + "+")
+            if y != 0 or y != gridSize -1 or gridSize == 1:
+                if cubesXY == 1:
+                    print("| " + "  " * gridSize + "| ")
+                if cubesXY > 1:
+                    print(" " * gridSize + "| ", end="")
 
-printgrid(3,3)
+
+            if y == gridSize - 1:
+                if cubesXY == 1:
+                    print("+ " + "- " * gridSize + "+ ")
+                if cubesXY > 1:
+                    print("- " * gridSize + "+ ", end="")
+
+
+
+
+printgrid(1,2)
