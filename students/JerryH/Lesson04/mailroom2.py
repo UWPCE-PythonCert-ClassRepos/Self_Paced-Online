b302ddf900510c6335d69d9c95d38b6e250d7f16
Jerry.H <doridoriae86@gmail.com>
2018-02-06 17:51:06 -0800

    More changes

--- students/JerryH/Lesson04/mailroom2.py
+++ students/JerryH/Lesson04/mailroom2.py
@@ -21,7 +21,8 @@ menu = {
 QUIT_OPT = '4'
 
 def prompt():
-    return input("Please choose the following options:\n1) {op1}.\n2) {op2}.\n3) {op3}.\n4) {op4}\n".format(**menu))
+    return input("Please choose the following options:\n1) {op1}.\n2) {op2}.\n3)"
+        " {op3}.\n4) {op4}\n".format(**menu))
 
 
 def donor_name_prompt():
@@ -49,34 +50,30 @@ def send_thank_you():
     donation = float(input("Please enter the donation amount:\n"))
 
     # If the donnor doesn't exist in the donor dictionary - add his info
-    if donor_name not in get_all_donor_names():
-        # print(donors)
-        donors[donor_name] = [donation]
-    # else append the donation into the existing donors
-    else:
-        donors[donor_name].append(donation)
+    # Using defaultdict
+    donors[donor_name].append(donation)
     print("Thank You Email:  Thansk for the donation!\n\n")
 
 
 def create_report():
-	print("\nDonor Name           |  Total Given | Num Gifts | Average Gift")
-	print("---------------------------------------------------------------\n")
-	report = []  # initialize report
+    print("\nDonor Name           |  Total Given | Num Gifts | Average Gift")
+    print("---------------------------------------------------------------\n")
+    report = []  # initialize report
 
-	# for each_name in get_all_donor_names(donors):
-	for each_donor in donors.keys():
-	    report.append([each_donor, sum(donors[each_donor]), len(donors[each_donor])])
+    # for each_name in get_all_donor_names(donors):
+    for each_donor in donors.keys():
+        report.append([each_donor, sum(donors[each_donor]), len(donors[each_donor])])
 
-	# sorting the report based on donations
-	r_sorted = sorted(report, key=lambda r: r[1], reverse=True)
+    # sorting the report based on donations
+    r_sorted = sorted(report, key=lambda r: r[1], reverse=True)
 
-	# Create the report
-	for d in range(len(r_sorted)):
-	    print("{:23}${:12.2f}{:10}   ${:12.2f}".format(r_sorted[d][0],
-	        r_sorted[d][1],
-	        r_sorted[d][2],
-	        r_sorted[d][1]/r_sorted[d][2]))
-	print("\n")
+    # Create the report
+    for donor_report in r_sorted:
+        print("{:23}${:12.2f}{:10}   ${:12.2f}".format(donor_report[0],
+            donor_report[1],
+            donor_report[2],
+            donor_report[1] / donor_report[2]))
+    print("\n")
 
 
 def send_letters():
@@ -102,10 +99,13 @@ selection_map = {
 
 
 def main():
-	option_value = 0
-	while option_value != QUIT_OPT:
-		option_value = prompt()
-		selection_map[option_value]()
+    option_value = 0
+    while option_value != QUIT_OPT:
+        try:
+            option_value = prompt()
+            selection_map[option_value]()
+        except KeyError:
+            print("%s is not a valid option! Please try again.\n" % option_value)
 
 
 # start the script
