d576700a8be592ac8bf74e611e743292a68008bc
Andrew Park <apark46.work@gmail.com>
2018-02-07 18:06:41 -0800

    Lesson04|dict_lab.py dictionary1 and 2 do what the lab asks. Experimenting w/ sets now

--- students/nDruP/lesson04/dict_lab.py
+++ students/nDruP/lesson04/dict_lab.py
@@ -3,10 +3,21 @@ def dictionary1():
     print(dating_profile)
     dating_profile.pop('cake')
     print(dating_profile)
-    dating_profile['fruit']='Mango'
+    dating_profile['fruit'] = 'Mango'
     print(dating_profile.keys())
     print(dating_profile.values())
     print('cake' in dating_profile)
     print('Mango' in dating_profile.values())
+    return dating_profile
 
+def dictionary2():
+    t_profile = {'name':'Chris', 'city':'Seattle', 'cake':'Chocolate'}
+    for x,y in t_profile.items():
+        t_profile[x] = (y.lower()).count('t')
+    print(t_profile)
+
+def set1():
+    s2 = set([0:20:2])
+    
 dictionary1()
+dictionary2()
