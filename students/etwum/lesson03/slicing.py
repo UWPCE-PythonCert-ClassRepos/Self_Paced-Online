57198128b51dee0bcec51f5612ed7f07b3694dc3
etwum <33198751+etwum@users.noreply.github.com>
2018-02-07 19:22:07 -0800

    adding assertion test
    adding the assertion test to test the functions

--- students/etwum/lesson03/slicing.py
+++ students/etwum/lesson03/slicing.py
@@ -3,7 +3,7 @@ def exchange_first_last(seq):
     if type(seq) is str:
         y = seq[-1] + seq[1:-1] + seq[0]
         new_string = y
-        return print(new_string)
+        return new_string
 
     elif type(seq) is tuple:
         x = list(seq)
@@ -12,27 +12,27 @@ def exchange_first_last(seq):
         first_item = [x[0]]
         new_list = last_item + middle_items + first_item
         new_tuple = tuple(new_list)
-        return print(new_tuple)
+        return new_tuple
 
     else:
         last_item2 = [seq[-1]]
         middle_items2 = seq[1:-1]
         first_item2 = [seq[0]]
         new_list2 = last_item2 + middle_items2 + first_item2
-        return print(new_list2)
+        return new_list2
 
 
 def remove_every_other(seq):
     if type(seq) is str:
         y = seq[0::2]
         new_string = y
-        return print(new_string)
+        return new_string
 
     elif type(seq) is tuple:
         x = list(seq)
         new_list = x[0::2]
         new_tuple = tuple(new_list)
-        return print(new_tuple)
+        return new_tuple
 
     else:
         new_list2 = seq[0::2]
@@ -76,6 +76,20 @@ def reverse(seq):
 def middle_last_first():
     return
 
-hello = [1,2,3,4,5,5,6,6,74,54,342,45,66343,4]
+hello = "hello world"
 
-first_last_four_every_other_remove(hello)
+remove_every_other(hello)
+
+
+# Assertion Tests
+string_test = "hello world"
+tuple_test = (1,2,3,4,5,6,7,8,9,10,11)
+
+assert exchange_first_last(string_test) == "dello worlh"
+assert exchange_first_last(tuple_test) == (11,2,3,4,5,6,7,8,9,10,1)
+assert remove_every_other(string_test) == "hlowrd"
+assert remove_every_other(tuple_test) == (1,3,5,7,9,11)
+first_last_four_every_other_remove()
+reverse()
+
+
