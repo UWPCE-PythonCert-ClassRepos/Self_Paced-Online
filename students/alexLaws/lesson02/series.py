050f884da586a804a4c3319a3b1b648d22186363
Alex Laws <tal1286@gmail.com>
2018-02-08 14:00:08 -0800

    series assignment

---
+++ students/alexLaws/lesson02/series.py
@@ -0,0 +1,24 @@
+def fibonacci(n):
+    """Return the nth value in the fibonacci sequence"""
+    return sum_series(n)
+
+
+def lucas(n):
+    """Return the nth value in the lucas sequence"""
+    return sum_series(n, 2, 1)
+
+
+def sum_series(n, a=0, b=1):
+    """Return the nth value in a fibonacci-type series; 1st values are a & b"""
+    if n == 1:
+        return (a)
+    elif n == 2:
+        return (b)
+    else:
+        return (sum_series(n-1, a, b) + sum_series(n-2, a, b))
+
+"""assert statements to test the program"""
+assert fibonacci(8) == 13
+assert sum_series(8) == 13
+assert lucas(8) == 29
+assert sum_series(8, 2, 1) == 29
