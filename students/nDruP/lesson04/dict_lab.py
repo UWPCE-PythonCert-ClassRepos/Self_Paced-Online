83c11225d8f3882fbe3d1a8d9b59695c794fc565
Andrew Park <apark46.work@gmail.com>
2018-02-07 18:18:44 -0800

    Lesson04|dict_lab.py Finished Set exercises. Still don't really know how I'd use them, but we'll see

--- students/nDruP/lesson04/dict_lab.py
+++ students/nDruP/lesson04/dict_lab.py
@@ -10,14 +10,37 @@ def dictionary1():
     print('Mango' in dating_profile.values())
     return dating_profile
 
+
 def dictionary2():
     t_profile = {'name':'Chris', 'city':'Seattle', 'cake':'Chocolate'}
     for x,y in t_profile.items():
         t_profile[x] = (y.lower()).count('t')
     print(t_profile)
+    return
+
 
 def set1():
-    s2 = set([0:20:2])
-    
+    s2 = set((list(range(21)))[::2])
+    s3 = set((list(range(21)))[::3])
+    s4 = set((list(range(21)))[::4])
+    print(s2)
+    print(s3)
+    print(s4)
+    print(s3.issubset(s2))
+    print(s4.issubset(s2))
+    return
+
+
+def set2():
+    pythoni = set('Python')
+    pythoni.update('i')
+    marathon = frozenset('marathon')
+    print(pythoni.union(marathon))
+    print(pythoni.intersection(marathon))
+    return
+
+
 dictionary1()
 dictionary2()
+set1()
+set2()
