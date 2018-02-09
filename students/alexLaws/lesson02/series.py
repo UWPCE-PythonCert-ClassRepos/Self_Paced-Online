dd5f8069e1708fd022b42f4960704d49321417b1
Alex Laws <tal1286@gmail.com>
2018-02-08 17:40:00 -0800

    added assert statements

--- students/alexLaws/lesson02/series.py
+++ students/alexLaws/lesson02/series.py
@@ -18,7 +18,7 @@ def sum_series(n, a=0, b=1):
         return (sum_series(n-1, a, b) + sum_series(n-2, a, b))
 
 """assert statements to test the program"""
-assert fibonacci(8) == 13
-assert sum_series(8) == 13
-assert lucas(8) == 29
-assert sum_series(8, 2, 1) == 29
+assert fibonacci(8) == 13  # The 8th num in my fibonacci sequence is 13
+assert sum_series(8) == 13  # The series function w/1 input returns fibonacci
+assert lucas(8) == 29  # The 8th num in my Lucas sequence is 29
+assert sum_series(8, 2, 1) == 29  # The series output w/3 inputs is Lucas
