73b312028277ef95a824e646f181568693ac9569
Jerry.H <doridoriae86@gmail.com>
2018-02-08 20:43:21 -0800

    more changes to trigrams.py

--- students/JerryH/Lesson04/trigrams.py
+++ students/JerryH/Lesson04/trigrams.py
@@ -6,10 +6,9 @@ from collections import defaultdict
 
 
 def get_text():
-    file = open("sherlock.txt", "r")
-    text = file.read()
-    file.close()
-    return text
+    with open("sherlock_small.txt", "r") as fh: # fh: file handle
+        return fh.read()
+
 
 
 def get_word_list():
@@ -17,22 +16,21 @@ def get_word_list():
     punc = string.punctuation.replace("-", "") #keep "-" in the punctuation
     table = str.maketrans({key: None for key in punc})
     new_text = text.translate(table)
+    return new_text.split()
 
-    word = new_text.split()
-    return word
-
-# create a default list dictionary
-word_dict = defaultdict(list, {})
-words = get_word_list()
-
-new_wordings = [] # Beginning of the new wordings
-r = random.randint(0, len(words))
-
-# Create trigrams in dictionary 'word_dict'
-for i in range(len(words)-2):
-    word_dict[words[i] + ' ' + words[i+1]].append(words[i+2])
 
 def main():
+    # create a default list dictionary
+    word_dict = defaultdict(list, {})
+    words = get_word_list()
+
+    new_wordings = [] # Beginning of the new wordings
+    r = random.randint(0, len(words))
+
+    # Create trigrams in dictionary 'word_dict'
+    for i in range(len(words)-2):
+        word_dict[words[i] + ' ' + words[i+1]].append(words[i+2])
+
     # Get the next word after the random picked word pairs
     next_word = word_dict[words[r] + ' ' + words[r+1]]
 
@@ -41,15 +39,12 @@ def main():
 
     # Putting all words together starting with initial new_wordings defined above.
     # Stops the loop if there's no more next word after the chosen word pair
-    while next_word != None:
-        next_word = word_dict[new_wordings[-2] + ' ' + new_wordings[-1]] # Find the next word
+    while next_word:
+        next_word = word_dict[" ".join(new_wordings[-2:])] # Find the next word
 
-        if next_word != []:
-            new_wordings.append(next_word[0])
-            # Delete that word from the list so it doesn't get used again
-            word_dict[new_wordings[-2] + ' ' + new_wordings[-1]].pop(0)
-        else:
-            next_word = None
+        if next_word:
+            new_wordings.append(random.choice(next_word))
+
     # Print out the final wordings
     print(' '.join(new_wordings))
 
