53867d10b8cc79b9cf4cb8a6f706526b99412fc2
Dustin L <r.dustin.lane@gmail.com>
2018-02-08 20:20:24 -0800

    Dict Lab - Add dictionaries_one() fxn

--- students/Dustin_L/lesson04/dict_lab.py
+++ students/Dustin_L/lesson04/dict_lab.py
@@ -5,5 +5,29 @@ Contains all functions for the Dictionary Lab Module.
 """
 
 
+def dictionaries_one():
+    """Test basic dictionary operations"""
+    d1 = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+    print(d1)
+
+    # del d1['cake']
+    d1.pop('cake')
+    print(d1)
+
+    d1['fruit'] = 'Mango'
+    print(d1)
+
+    print(d1.keys())
+    print(d1.values())
+    if 'cake' in d1:
+        print(True)
+    else:
+        print(False)
+    if 'Mango' in d1.values():
+        print(True)
+    else:
+        print(False)
+
 
 if __name__ == '__main__':
+    dictionaries_one()
