dc64da778764ddc3c6c647e8d8865a6d6eee4eeb
csdotson <csdotson1@gmail.com>
2018-02-11 13:27:16 -0800

    Add README, list_lab, slicing_lab outlines

---
+++ students/csdotson/lesson03/list_lab.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Lesson 3 - List Lab Excercise
+
+response = input("a prompt for the user > ")
+print(response)
---
+++ students/csdotson/lesson03/slicing_lab.py
@@ -0,0 +1,18 @@
+# Lesson 3 - Slicing Lab Excercise
+
+def exchange_first_last(seq):
+    # Takes seq as argument, returns copy of sequence where first and last items exchanged
+    return (seq[-1] + seq[1:-2] + seq[0])
+
+
+def every_other_removed(seq):
+    # Takes seq as argument, returns copy of sequence where every other item removed
+
+def first_4_last_4(seq):
+    # Takes seq as argument, returns copy of sequence where the first 4 and the last 4 items removed, and then every other item in between
+
+def reverse_seq(seq):
+    # Takes seq as argument, returns copy of sequence with the elements reversed
+
+def shuffle_thirds(seq):
+    # Takes seq as argument, returns copy of sequence with the middle third, then last third, then the first third in the new order
