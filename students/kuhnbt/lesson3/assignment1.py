516291802e01f4d8a3174793fdff4f788caa8a23
Ben Kuhn <kuhnbt@gmail.com>
2018-02-11 11:07:36 -0500

    initial commit

---
+++ students/kuhnbt/lesson3/assignment1.py
@@ -0,0 +1,12 @@
+def exchange_first_last(seq):
+    """Exchange first and last elements of input sequence"""
+    return seq[-1] + seq[1:-2] + seq[0]
+
+
+def remove_every_other(seq):
+
+def rm4_and_every_other(seq):
+
+def reverse_seq(seq):
+
+def reorder_by_thirds(seq):
