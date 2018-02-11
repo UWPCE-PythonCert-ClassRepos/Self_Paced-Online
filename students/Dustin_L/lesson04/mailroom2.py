1a32f021f047000922db9fa2c8e608448b7815f8
Dustin L <r.dustin.lane@gmail.com>
2018-02-11 10:38:40 -0800

    Mailroom 2 - Add mailroom2.py file

---
+++ students/Dustin_L/lesson04/mailroom2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Mail Room 2 Module
+
+This module contains all of the functions for the updated Mail Room 2 module.
+"""
+
+
+def main():
+    """Main function"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
