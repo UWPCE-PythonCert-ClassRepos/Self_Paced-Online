a8d60477ac255c927876d20f56b2d060745b87d6
Andrew Park <apark46.work@gmail.com>
2018-02-10 16:44:43 -0800

    Lesson04|mailroom2.py renamed some functions for clarity. The whole script is PEP8 compliant. Added exit option in create_letters.

--- students/nDruP/lesson04/mailroom2.py
+++ students/nDruP/lesson04/mailroom2.py
@@ -8,7 +8,9 @@ to the original prompt
 """
 
 
-import sys, os, datetime
+import sys
+import os
+import datetime
 
 
 donor_dict = {"Sleve McDichael": [86457.89, 2346.43, 9099.09],
@@ -20,11 +22,14 @@ donor_dict = {"Sleve McDichael": [86457.89, 2346.43, 9099.09],
 
 
 def main_menu():
+    """
+    Prompt user to send a Thank You, Create a report, create letters, or quit.
+    """
     user_prompt = None
-    valid_prompts = {"1":send_thanks,
-                     "2":create_report,
-                     "3":send_all,
-                     "4":sys.exit}
+    valid_prompts = {"1": send_thanks,
+                     "2": create_report,
+                     "3": send_all,
+                     "4": sys.exit}
     while True:
         print_divider()
         print("We're a Pyramid Scheme & So Are You! E-Mailroom")
@@ -74,15 +79,14 @@ def send_thanks():
         if donor_name not in donor_dict:
             donor_list[donor_name] = []
         donor_dict[donor_name].append(donation_amt)
-
         print_divider()
         message = f"Dearest {donor_name},\n"
         message += f"Thank you so much for donation of ${donation_amt:.2f}!\n"
         message += "We will use your donation to create a real living Pokemon."
-        message += f"\nSincerely,\n We're a Pyramid Scheme & so is {donor_name}"
+        message += f"\nSincerely,\nWe're a Pyramid Scheme & so is {donor_name}"
         print(message)
         print_divider()
-        
+
         break
     return
 
@@ -91,18 +95,18 @@ def create_report():
     """
     Print a list of donors sorted by total historical donation amount.
     Donor Name, Total Given, Num Gifts, Average Gift
-    """    
+    """
     sort_donors = sorted(donor_dict.items(), key=sum_2tuple_by2, reverse=True)
     col_name_list = ["Donor Name", "Total Given", "Gifts", "Avg Donation"]
     billi_len = 12
     donor_col_len = max([len(name) for name, donations in sort_donors])
     donor_col = "{:<" + f"{donor_col_len}" + "}"
     money_col = "{:>" + f"{billi_len}" + "}"
-    
+
     header_row = donor_col + "\t" + money_col + "\t{:}" + "\t" + money_col
     print(header_row.format(*col_name_list))
     money_col = money_col[:len(money_col)-1] + ".2f}"
-    
+
     for name, donations in sort_donors:
         row = donor_col.format(name)
         row += "\t$ " + money_col.format(sum(donations))
@@ -115,28 +119,32 @@ def create_report():
 def send_all():
     """
     Write a full set of letters to each donor to individual files on disk.
-    Go through all donors in donor_dict, generate a thank you letter, write to disk as a text file.
+    Go through all donors in donor_dict, generate a thank you letter,
+    write to disk as a text file.
     """
-    curdate = (datetime.datetime.now()).strftime("%Y_%m_%d")
-    print("Enter an existing directory to place the letters.")
-    print("Current working directory will be used if given directory is " +
-          "invalid")
-    write_dir = input(">")
-    if not os.path.isdir(write_dir):
-        write_dir = os.getcwd()
-    print(write_dir+curdate)
-    for donor, donations in donor_dict.items():
-        thank_you = "Dear " + donor + ",\n\t"
-        thank_you += "We are so grateful for your" + " immense generosity of "  
-        thank_you += f"${sum(donations):.2f}!"
-        thank_you += " You now have our eternal loyalty. Use it wisely."
-        thank_you += "\nSincerely, \n"
-        thank_you += "We're a pyramid scheme and so is " + donor
-        file_name = donor.replace(' ', '_') + "_" + curdate + ".txt"
-        letter = open(write_dir+"/"+file_name, 'w+')
-        letter.write(thank_you)
-        letter.close()
-    print("Finished writing the letters")
+    while True:
+        curdate = (datetime.datetime.now()).strftime("%Y_%m_%d")
+        print("Enter an existing directory to place the letters.")
+        print("Current working directory will be used if given directory is " +
+              "invalid")
+        print("Enter exit to return to main menu.")
+        write_dir = input(">")
+        if write_dir.lower() == 'exit':
+            break
+        if not os.path.isdir(write_dir):
+            write_dir = os.getcwd()
+        for donor, donations in donor_dict.items():
+            thank_you = "Dear " + donor + ",\n\t"
+            thank_you += "We are so grateful for your lifetime generosity of "
+            thank_you += f"${sum(donations):.2f}!"
+            thank_you += " You now have our eternal loyalty. Use it wisely."
+            thank_you += "\nSincerely, \n"
+            thank_you += "We're a pyramid scheme and so is " + donor
+            file_name = donor.replace(' ', '_') + "_" + curdate + ".txt"
+            letter = open(write_dir+"/"+file_name, 'w+')
+            letter.write(thank_you)
+            letter.close()
+        print("Finished writing the letters")
     return
 
 
@@ -148,13 +156,11 @@ def sum_2tuple_by2(idx_set):
     return sum(idx_set[1])
 
 
-
-
 def print_divider():
     """
     Prints a divider so user has better idea of when they enter a new screen.
     """
     print("\n"+"*"*50+"\n")
 
-                      
+
 main_menu()
