87d1389d2055fa0daabcbc4955bf74acbafe7d2f
Ben Kuhn <kuhnbt@gmail.com>
2018-02-11 12:09:47 -0500

    added tests

--- students/kuhnbt/lesson3/assignment1.py
+++ students/kuhnbt/lesson3/assignment1.py
@@ -1,7 +1,7 @@
 def exchange_first_last(seq):
     """Exchange first and last elements of input sequence with 
     length>1"""
-    return seq[-1:] + seq[1:-2] + seq[:1]
+    return seq[-1:] + seq[1:-1] + seq[:1]
 
 
 def remove_every_other(seq):
@@ -19,7 +19,31 @@ def reverse_seq(seq):
     return seq[::-1]
 
 
-def reorder_by_thirds(seq):
+def reorder_thirds(seq):
     """Return middle third, then last third, then first third"""
-    slice_index = len(seq)//3
-    return seq[slice_index:] + seq[:slice_index]
+    seq_slice = len(seq)//3
+    return seq[seq_slice:] + seq[:seq_slice]
+
+test_string = 'this is a test string'
+short_string = 'abc'
+test_list = ['this', 'is', 'my', 'test', 'list']
+
+assert exchange_first_last(test_string) == 'ghis is a test strint'
+assert exchange_first_last(short_string) == 'cba'
+assert exchange_first_last(test_list) == ['list', 'is', 'my', 'test', 'this']
+
+assert remove_every_other(test_string) == 'ti sats tig'
+assert remove_every_other(short_string) == 'ac'
+assert remove_every_other(test_list) == ['this', 'my', 'list']
+
+assert rm4_and_every_other(test_string) == ' sats t'
+assert rm4_and_every_other(short_string) == ''
+assert rm4_and_every_other(test_list) == []
+
+assert reverse_seq(test_string) == 'gnirts tset a si siht'
+assert reverse_seq(short_string) == 'cba'
+assert reverse_seq(test_list) == ['list', 'test', 'my', 'is', 'this']
+
+assert reorder_thirds(test_string) == ' a test stringthis is'
+assert reorder_thirds(short_string) == 'bca'
+assert reorder_thirds(test_list) == ['is', 'my', 'test', 'list', 'this']
