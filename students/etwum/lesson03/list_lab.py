4828e4793dacd1c7fd9fb79c5e2f24a6c65463ae
etwum <33198751+etwum@users.noreply.github.com>
2018-02-13 18:33:47 -0800

    Series 1
    working on the Series 1 code

--- students/etwum/lesson03/list_lab.py
+++ students/etwum/lesson03/list_lab.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+
+# Series 1
+fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
+
+print(fruit_list)
+
+new_fruit = input("Please input a fruit to add to the list. ")
+
+fruit_list.append(new_fruit.capitalize())
+
+print(fruit_list)
+
+boolInput_Check = False
+while boolInput_Check == False:
+
+    try:
+        user_number = int(input("Please input a number: "))
+        if user_number > len(fruit_list):
+            raise Exception
+
+    except ValueError as x:
+        print("Please input a whole number")
+        print(x)
+
+    except Exception:
+        print("The number you entered is too high")
+
+    else:
+        boolInput_Check = True
+
+x = user_number - 1
+print("Here is your fruit selection: %d." % user_number, fruit_list[x])
+
+
+
+
+
+
