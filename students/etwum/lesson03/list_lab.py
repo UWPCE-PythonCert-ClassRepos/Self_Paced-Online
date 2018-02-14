8a3f5f7b2a5ee5e61a7df5644133452162856857
etwum <33198751+etwum@users.noreply.github.com>
2018-02-13 20:07:54 -0800

    Series 2 complete and other updates
    -completes series 2 code
    -updated user input
    -added separation lines to the code

--- students/etwum/lesson03/list_lab.py
+++ students/etwum/lesson03/list_lab.py
@@ -10,12 +10,12 @@ new_fruit = input("Please input a fruit to add to the list. ")
 fruit_list.append(new_fruit.capitalize())
 
 print(fruit_list)
-
+print("----------------------------------------------------------------" + "\n")
 boolInput_Check = False
 while boolInput_Check == False:
 
     try:
-        user_number = int(input("Please input a number: "))
+        user_number = int(input("Please input a number to display a fruit: "))
         if user_number > len(fruit_list):
             raise Exception
 
@@ -31,33 +31,61 @@ while boolInput_Check == False:
 
 x = user_number - 1
 print("Here is your fruit selection: %d." % user_number, fruit_list[x])
+print("----------------------------------------------------------------" + "\n")
 
 new_fruit_list = []
-new_fruit2 = str(input("Please input a second fruit to the list. "))
+new_fruit2 = str(input("Please input a second fruit to add to the beginning of the list. "))
 
 new_fruit_list.append(new_fruit2.capitalize())
 
 fruit_list2 = new_fruit_list + fruit_list
 
 print(fruit_list2)
+print("----------------------------------------------------------------" + "\n")
 
-new_fruit3 = str(input("Please input a third fruit to the list. "))
+new_fruit3 = input("Please input a third fruit to add to the beginning of the list. ")
 
 fruit_list2.insert(0,new_fruit3.capitalize())
 
 print(fruit_list2)
+print("----------------------------------------------------------------" + "\n")
 
+
+print("Here are the fruits from the list which begin with a P:")
 for x in fruit_list2:
     for y in x:
         if y[0] == "P":
+
             print(x)
+print("----------------------------------------------------------------" + "\n")
 
 # Series 2
 print(fruit_list2)
-
+print("Removing the last item from the list")
 fruit_list2.pop(-1)
 
 print(fruit_list2)
+print("----------------------------------------------------------------" + "\n")
+
+
+boolDelete = False
+while boolDelete == False:
+    delete_fruit = input("What fruit would you like to delete? ")
+
+    for x in fruit_list2:
+        if x == delete_fruit.capitalize():
+            fruit_list2.remove(delete_fruit.capitalize())
+            print("Fruit deleted from list!")
+            print(fruit_list2)
+            boolDelete = True
+            print("----------------------------------------------------------------" + "\n")
+
+    if boolDelete == False:
+        print("Fruit not found in list")
+
+
+
+
 
 
 
