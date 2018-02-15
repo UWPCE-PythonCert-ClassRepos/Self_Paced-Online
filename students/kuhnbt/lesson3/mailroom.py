9a42371d1c370ede01dd7ee5a42e080ff1347640
Ben Kuhn <kuhnbt@gmail.com>
2018-02-14 20:13:01 -0500

    mailroom initial ommit

---
+++ students/kuhnbt/lesson3/mailroom.py
@@ -0,0 +1,7 @@
+donor_info = {}
+
+selection = input('Please choose an action:\n1. Send a thank you\n2.', 
+                  'Create a report\n3. Quit')
+
+def send_thankyou():
+    name = input('Please enter full name:\n')
