d8ffe76e6de9c5f1390e04ff0fda258e8acc8c7e
csdotson <csdotson1@gmail.com>
2018-02-15 20:46:21 -0800

    Complete functions required to handle logic

--- students/csdotson/lesson03/mailroom.py
+++ students/csdotson/lesson03/mailroom.py
@@ -1,55 +1,98 @@
 #!/usr/bin/env python3
 # Lesson 3 - Mailroom... part 1
 
-Donations = [ ["Bill Gates", 10.50], ["Bill Gates", 123.45], ["Bill Gates",    1111.11], ["Jeff Bezos", 7.65], ["Jeff Bezos", 1000], ["Paul Allen", 145.90], ["John Nordstrom", 45.67], ["John Nordstrom", 6519.65], ["Mark Zuck", 789.12] ]
-
-### Prompts and Flow Control ###
-response = input("Welcome Chris! What would you like to do?\n(S)end a Thank You / (C)reate a Report / (Q)uit\n--> ")
-
-while True:
-    if response == "S":
-        send_thank_you()
-        break
-    elif response == "C":
-        print_report()
-        break
-    else:
-        print("Goodbye Sir!")
-        break
+donations = [ ["Bill Gates", 10.50], ["Bill Gates", 123.45], ["Bill Gates",    1111.11], ["Jeff Bezos", 7.65], ["Jeff Bezos", 1000], ["Paul Allen", 145.90], ["John Nordstrom", 45.67], ["John Nordstrom", 6519.65], ["Mark Zuck", 789.12] ]
 
 
 ### Function Definitions ###
+def prompt(which = 0):
+    if which == 0:
+        response = input("Welcome Chris! What would you like to do?\n(S)end a Thank You / (C)reate a Report / (Q)uit\n--> ")
+    else:
+        response = input("What would you like to do next?\n(S)end a Thank You / (C)reate a Report / (Q)uit\n--> ")
+
+    while True:
+        if response == "S":
+            send_thank_you()
+            break
+        elif response == "C":
+            print_report()
+            break
+        else:
+            print("Goodbye Sir!")
+            break
+
 
 def send_thank_you():
-    # Prompt for a full name
-    response = input("Please enter full name of who you'd like to thank:\nBy the way, you can also type 'list' to get a list of names\n-->")
+    response = input("Please enter full name of person you'd like to thank:\nBy the way, you can also type 'list' to get a list of donors or 'Q' to quit\n--> ")
 
     if response == "list":
-        donor_list()
-        # Re-prompt for a name
-    elif response # Response is name not in list
+        print()
+        list_donors()
+        send_thank_you()
+    elif response not in donor_names():
+        new_donor = response
+        donation_response = input(f"Please enter a donation amount for {new_donor}: ")
+        donations.append([new_donor, float(donation_response)])
+        print_email(new_donor, donation_response)
+    else:
+        donor = response
+        donation_response = input(f"Please enter a donation amount for {donor}: ")
+        donations.append([donor, float(donation_response)])
+        print_email(donor, donation_response)
 
 
+def print_email(donor, amount):
+    print()
+    print(f"Dear {donor},\nThank you so very much for you kind donation of ${amount}. We can assure you that it will be put to great use.\nBest,\nChris")
+    print()
+    prompt(1)
 
 
+def list_donors():
+    donor_list = donor_names()
+
+    print("List of donors:")
+    for i in donor_list:
+        print(i)
+
+    print()
 
 
-def donor_list():
-    ...
+def donor_names():
+    name_list = []
+    for i, j in donations:
+        if i not in name_list:
+            name_list.append(i)
+
+    return name_list
+
 
 def print_report():
-    header = '{:20}{:^15}{:^15}{:>15}'.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
+    header = '{:20}|{:^15}|{:^15}|{:>15}'.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
     print(header)
-    print("-" * 65)
+    print("-" * 68)
 
-def total_given():
-    ...
+    for name in donor_names():
+        print('{:20}${:>15.2f}{:>15}{:>15.2f}'.format(name, total_given(name), num_gifts(name), avg_gift(name)))
 
-def num_gifts():
-    ...
+    print()
+    prompt(1)
 
-def avg_gift():
-    ...
 
-def prompt():
-    ...
+def total_given(donor):
+    sum = 0
+    for i, j in donations:
+        if i == donor:
+            sum += j
+    return sum
+
+def num_gifts(donor):
+    count = 0
+    for i, j in donations:
+        if i == donor:
+            count += 1
+    return count
+
+def avg_gift(donor):
+    return (total_given(donor)/num_gifts(donor))
