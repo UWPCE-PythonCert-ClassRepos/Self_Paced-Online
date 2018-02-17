c39cb3d72cd790124854d35863239ee9ea8471aa
Andrew Park <apark46.work@gmail.com>
2018-02-16 16:53:08 -0800

    Lesson04|trigram.py Unable to figure out an elegant way to make sure the last word of the text makes its way into the trigram dictionary.

--- students/nDruP/lesson04/trigram.py
+++ students/nDruP/lesson04/trigram.py
@@ -7,21 +7,80 @@ Write a new text file based on the trigrams created from the text file.
 
 
 import os
+import random
+random.seed(51)
 
 
-def build_trigram_dict(read_dir, text_file):
+def pull_text(read_dir, bof = None, eof = None):
     """
-    Build trigrams from text file.
-    1. read text file
-    2. Take 3 words, unless 3rd starts with [' " ( ] start new key from
-    or 2nd ends with [, . ? ! )]
-    3. First 2 words are key, 3rd is value. No repeat values.
-    4. Return dict of trigrams.
+    Parse valid text from text file.
+    """
+    orig_text = open(read_dir, 'r')
+    all_text = ''
+    end = orig_text.tell()
+
+    if bof:
+        while bof not in orig_text.readline():
+            continue
+
+    while True:
+        line = orig_text.readline()
+        if (orig_text.tell() == end) or (eof and eof in line):
+            break
+        if line == '\n':
+            all_text += line
+        else:
+            all_text += line[:len(line)-1] + ' '
+        print(line)
+        end = orig_text.tell()
+        print(end)
+    orig_text.close()
+    return all_text
+
+
+def build_trigram_dict(parsed_text):
+    """
+    Build trigrams from parsedtext.
+    1. Take 3 words
+    2. First 2 words are key, 3rd is value.
+    3. Return dict of trigrams.
+    Ignore multiple spaces.
     """
     trigram_dict = {}
+    key_word = [None, None, None]
+    ignore_char = [' ', '-']
+    word = ''
+    word_index = 0
+    #pull 3 words from text, ignore multiple spaces.
+    #Set first 2 words space-separated as key.
+    #Add 3rd as value
+    #pull next word, make current 2nd and 3rd words the key
+    for x in parsed_text:
+        print(x)
+        if x in ignore_char:
+            if word != '':
+                if None in key_word:
+                    key_word[word_index] = word
+                    word_index = word_index + 1
+                else:
+                    bigram = key_word[0] + ' ' + key_word[1]
+                    if not trigram_dict.get(bigram):
+                        trigram_dict[bigram] = [key_word[2]]
+                    else:
+                        trigram_dict[bigram] += [key_word[2]]
+                    key_word[0] = key_word[1]
+                    key_word[1] = key_word[2]
+                    key_word[2] = word
+                print(word)
+                print(key_word)
+                word = ''
+        else:
+            word += x
+        print(key_word)
+    print(trigram_dict.items())
+    return trigram_dict
 
-
-def make_story(trigram_dict):
+def make_story(story_dict):
     """
     Generate a story based on trigram dict.
     1. Begin at random key.
@@ -32,24 +91,34 @@ def make_story(trigram_dict):
     If there is no value at new key, start at step 1.
     6. Return story
     """
+    keys = story_dict.keys()
+    start = round(random.random()*len(keys))
     story = ''
     return story
 
-def write_story(story):
+def write_story(story, orig, directory):
     """
     Write generated story to text file.
     """
+    print("New story saved in " + directory)
+    return
 
 
-
-read_dir = os.getcwd()+'/'
-text_file = 'something.txt'
+start_dir = os.getcwd()
+orig_story = 'TaoTeChing.txt'
+gtb_bof = "***"
+gtb_eof = "End of the Project Gutenberg"
 enjoy_story = None
 
-trigram_dict = build_trigram_dict(read_dir, text_file)
+sample_story = pull_text(os.path.join(start_dir, 'sample.txt'))
+#guten_story = pull_text(os.path.join(start_dir, orig_story), gtb_bof, gtb_eof)
+#short_story = pull_text(os.path.join(start_dir, 'sherlock_small.txt'))
+parsed_story = sample_story
+trigram_dict = build_trigram_dict(parsed_story)
 while enjoy_story != 'y':
-    story = make_story(trigram_dict)
-    print(story)
+    enjoy_story = None
+    built_story = make_story(trigram_dict)
+    print(built_story)
     while enjoy_story not in ['y', 'n']:
         enjoy_story = input("Did you enjoy this story? (y/n)").lower()
-write_story(story)
+write_story(built_story, orig_story, start_dir)
