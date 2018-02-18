ce17e497770d96cf9f1c04fda9bce4289674efff
johnpharmd <jhumphre@uw.edu>
2018-02-18 08:50:49 -0800

    added more to, further re-factored, mailroom.py

--- students/johnpharmd/lesson03/mailroom.py
+++ students/johnpharmd/lesson03/mailroom.py
@@ -5,29 +5,39 @@ donors_amts = [['Gates', 150000, 3], ['Brin', 150000, 3],
               ['Cerf', 50000, 2], ['Musk', 100000, 1],
               ['Berners-Lee', 50000, 2],
               ['Wojcicki', 125000, 1], ['Avey', 200000, 2]]
-
+donors = []
 
 # Processing
+for donor in donors_amts:
+	donors.append(donor[0])
+
+
 def send_ty():
-	global donors_amts
+	global donors_amts, donors
 	while True:
 		print()
 		response = input('Enter full last name of Donor,'
 			+ ' "list" for List of Donors'
 			+ ', or "e" to Exit back to Main Menu: ')
+		print()
 		if response == 'e':
 			break	
 		if response.isalpha():
 			if response == 'list':
 				print('Here is the list of Donors: ')
-				donors_amts.sort()
-				for donor in donors_amts:
-					print(donor[0])
+				donors.sort()
+				for donor in donors:
+					print(donor)
 			elif response != 'list':
 				response = response.capitalize()
-				for donor in donors_amts:
-					if response in donor:
-						print('Donor found:', response)
+				if response in donors:
+					print('Donor found:', response)
+				elif response not in donors:
+					print('Added to list of Donors:', response)
+					donors.append(response)
+					donors_amts.append([response, 0, 0])
+				new_response = input('Enter a Donation amount' +
+					' (in USD): ')
 
 
 def get_report():
