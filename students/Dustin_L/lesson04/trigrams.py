a86f4d258c66f45bb901bc0e45a900151327edcd
Dustin L <r.dustin.lane@gmail.com>
2018-02-17 22:08:08 -0800

    Trigrams - Add trigrams.py file

---
+++ students/Dustin_L/lesson04/trigrams.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Trigrams
+
+This module contains all the functions for the Trigrams module.
+"""
+
+
+def main():
+    """Main function."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
