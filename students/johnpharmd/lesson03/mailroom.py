7eda7904ffd01136dab5239b443604d317c5c821
johnpharmd <jhumphre@uw.edu>
2018-02-18 12:53:11 -0800

    refactored 'send_ty()' function

--- students/johnpharmd/lesson03/mailroom.py
+++ students/johnpharmd/lesson03/mailroom.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
 # Data
-donors_amts = [['Gates', 150000, 3], ['Brin', 150000, 3],
-              ['Cerf', 50000, 2], ['Musk', 100000, 1],
-              ['Berners-Lee', 50000, 2],
-              ['Wojcicki', 125000, 1], ['Avey', 200000, 2]]
-donors = []
+donors_amts = [['Gates', 'Mr.', 150000, 3], ['Brin', 'Mr.', 150000, 3],
+              ['Cerf', 'Mr.', 50000, 2], ['Musk', 'Mr.', 100000, 1],
+              ['Berners-Lee', 'Mr.', 50000, 2],
+              ['Wojcicki', 'Ms.', 125000, 1], ['Avey', 'Ms.', 200000, 2]]
+
 
 # Processing
-for donor in donors_amts:
-	donors.append(donor[0])
-
-
 def send_ty():
-	global donors_amts, donors
+	global donors_amts
 	while True:
 		print()
 		response = input('Enter full last name of Donor,'
@@ -25,19 +21,35 @@ def send_ty():
 		if response.isalpha():
 			if response == 'list':
 				print('Here is the list of Donors: ')
-				donors.sort()
-				for donor in donors:
-					print(donor)
+				donors_amts.sort()
+				for donor in donors_amts:
+					print(donor[0])
 			elif response != 'list':
 				response = response.capitalize()
-				if response in donors:
-					print('Donor found:', response)
-				elif response not in donors:
-					print('Added to list of Donors:', response)
-					donors.append(response)
-					donors_amts.append([response, 0, 0])
-				new_response = input('Enter a Donation amount' +
-					' (in USD): ')
+				for donor in donors_amts:
+					if donor[0] == response:
+						print('Donor found:', response)
+					else:
+						salutation = input('Salutation: "Ms." or "Mr."?: ')
+						print('Added to list of Donors:', salutation,
+							response)
+						donors_amts.append([response, salutation,
+							0, 0])
+				new_response = int(input('Enter a Donation amount' +
+					' (in USD): '))
+				print('Added to', response, '\'s Donations:',
+					new_response)
+				for donor in donors_amts:
+					if donor[0] == response:
+						donor[2] += new_response
+						donor[3] += 1
+				print('Here is the full List:')
+				donors_amts.sort()
+				for donor in donors_amts:
+					print(donor[0])
+				form_st = 'Dear {} {}, Thank you for your generous donation in the amount of {} USD.'
+				print(form_st.format('Donor'
+					, response, new_response))
 
 
 def get_report():
