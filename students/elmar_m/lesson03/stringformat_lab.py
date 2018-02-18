489ac652e4b10711a5e0f99fbebcd040a915adda
22e88 <22e88@mailbox.org>
2018-02-18 21:00:20 +0100

    stringformat_lab.py: initial version

---
+++ students/elmar_m/lesson03/stringformat_lab.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+'''
+file: stringformat_lab.py
+elmar_m / 22e88@mailbox.org
+Lesson03: String Formatting Lab Exercise
+'''
+
+a_tuple = ( 2, 123.4567, 10000, 12345.67)
+t_tuple = ( 999, 123.4567, 10000, 12345.67)
+
+
+
+'''
+Task 1
+'''
+def task1(t):
+    # print('file_{} {} {} {}'.format(*t))  # OK
+    # print('file_{:0>3d} {:.2f} {:.2e} {:.3e}'.format(*t))
+    return 'file_{:0>3d} {:.2f} {:.2e} {:.3e}'.format(*t)
+
+
+
+'''
+Task 2
+'''
+
+
+
+'''
+Task 3
+'''
+
+
+
+
+'''
+Task 4
+'''
+
+
+
+
+'''
+Task 5
+'''
+
+
+
+
+'''
+Task 6
+'''
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('This file contains functions for the stringformatting lab exercises, please import it to use them')
