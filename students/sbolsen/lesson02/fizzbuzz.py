4acac7bd00470881ceb648eb0502cb1fb1c8a831
Scott B Olsen <scott.b.olsen@oracle.com>
2018-02-18 17:39:49 -0800

    Adding fizzbuzz.py assignment without commented lines

--- students/sbolsen/lesson02/fizzbuzz.py
+++ students/sbolsen/lesson02/fizzbuzz.py
@@ -1,13 +1,3 @@
-#for i in range(1, 101):
-#    if i % 3 == 0:
-#        print('fizz')
-#        if i % 5 == 0:
-#            print('buzz')
-#            if i % 3 == 0 and i % 5 == 0:
-#                print('fizzbuzz')
-#    else:
-#        print(i)
-
 for i in range(1, 101):
     if i % 3 ==0 and i % 5 == 0:
         print('FizzBuzz')
