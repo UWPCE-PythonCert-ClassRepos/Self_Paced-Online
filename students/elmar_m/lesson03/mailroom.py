e2c9c03fe32dba88ee976950bff3d17951426e0d
22e88 <22e88@mailbox.org>
2018-02-21 05:41:20 +0100

    mailroom.py: main structure, first functions working...

--- students/elmar_m/lesson03/mailroom.py
+++ students/elmar_m/lesson03/mailroom.py
@@ -6,9 +6,29 @@ elmar_m / 22e88@mailbox.org
 Lesson03: Mailroom Exercise Part 1
 '''
 
+donors = {
+    'bill' : ['2000', '7.5', '950000'],
+    'steve' : ['5.5', '234000', '928'],
+    'donald' : ['657', '234', '28.57', '90456'],
+    'angie' : ['2', '99', '297765', '47', '28346'],
+    'kim' : ['38982', '66.23', '9856', '0.1'],
+    }
+
+
+
 def thankyou():
-    print('thanks...')
-    pass
+    name = None
+    while name != 'x':
+        print('>> give me a donor name, or type "l" to see a list. Type "x" to exit.')
+        name = input('>> ')
+        if name == 'l':
+            print(' '.join(donors.keys()))
+        elif name in donors.keys():
+            print('>> ', name, 'donated:', donors[name])
+            # print(name, 'found in keys')
+        else:
+            break
+
 
 def report():
     print('generating report...')
