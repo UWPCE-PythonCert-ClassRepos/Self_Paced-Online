ad5efede301daf55bcdb480e306f19f8cf6287e4
Thomas Horn <hortw1991@gmail.com>
2018-02-21 11:50:46 -0600

    Typo fix.

--- students/thorn/lesson02/fizzbuzz.py
+++ students/thorn/lesson02/fizzbuzz.py
@@ -10,7 +10,7 @@ def fizz_buzz():
     """ Loops from 1 to 100 checking for above conditions. """
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
-            print("FizozBuzz")
+            print("FizzBuzz")
         elif x % 3 == 0:
             print("Fizz")
         elif x % 5 == 0:
