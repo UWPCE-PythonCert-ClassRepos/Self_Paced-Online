e26bb82705743a833a5ee3dfdf1b00b0d7bd4976
22e88 <22e88@mailbox.org>
2018-02-22 05:20:41 +0100

    mailroom.py: pre-final version

--- students/elmar_m/lesson03/mailroom.py
+++ students/elmar_m/lesson03/mailroom.py
@@ -15,10 +15,15 @@ donors = {
     }
  
 def mail(n, m):
-    # return('Dear', n, 'thank you very much for your donation of', m, 'dollars')
-    print('Dear {}, thank you very much for your donation of {} dollars.'.format(n, m))
+    '''
+    Create mail text.
+    '''
+    print('Dear {}, thank you very much for your donation of {} dollars.\n'.format(n, m))
 
 def thankyou():
+    '''
+    Show list of known donors, add new donor to list, add new donation to donor 
+    '''
     name = None
     while name != 'x':
         print('>> give me a donor name, or type "l" to see a list. Type "x" to exit.')
@@ -28,23 +33,30 @@ def thankyou():
         elif name == 'x':
             break
         elif name in donors.keys():
-            # print('>>', name, 'donated:', donors[name])
             print('>>', name, 'already in list')
-            # donors[name].append('10')
+
             donation = input('>> please add current donation:\n>> ')
             donors[name].append(int(donation))
             print('>>', donation, 'added to donation list of', name, 'thank you.\n')
+
             mail(name, donation)
+
         elif not name in donors.keys():
             print('>>', name, 'not in list, adding it ')
             donors[name] = [] 
-            
+
+            donation = input('>> please add current donation:\n>> ')
+            donors[name].append(int(donation))
+            print('>>', donation, 'added to donation list of', name, 'thank you.\n')
+
+            mail(name, donation)
+           
+    print('> Options: 1 == quit | 2 == thankyou | 3 == report')
 
 def report():
-    print('>> generating report...')
-    print('>> donors so far:', ' '.join(donors.keys()))
-
-    print('Donor Name   | Total Given   | Num Gifts | Average Gift')
+    '''
+    Show an overview of current donors and donations
+    '''
 
     # get the highest number:
     sumlist = []
@@ -52,21 +64,13 @@ def report():
         sumlist.append(sum(donors[i]))
     maxn = len(str(max(sumlist)))
     maxn += 2
-    print('maxwert', maxn)
 
-    # fstring = '{:<20} {:=$20} {:>' + str(dcount) + '.2f}'
-    # fstring = '{:<20} $ {:=20} {:>20} {:=20}'
-    # fstring = '{} ' + '{:>' + str(maxn) + '} ' + '{:>5} {:>20}' #  not OK
-    # fstring = '{:<20} ' + '{:>' + str(maxn) + '} ' + '{:>9} {:>20}' # OK
-    # fstring = '{:<20} ' + '|' + '{:>' + str(maxn) + '} ' + '|' + '{:>9}' + '|' + '{:>20}' # OK
-    fstring = '{:<14} ' + '|' + '{:>' + str(maxn) + '} ' + '|' + '{:>9}' + '|' + '{:>' + str(maxn) + '}'
-    print(fstring)
+    fstring = '{:<20} ' + '|' + '{:>' + str(maxn) + '} ' + '|' + '{:>9}' + '|' + '{:>20}' # OK
     
     # print header line:
     # print(fstring.format('Donor Name', 'Total Given', 'Num Gifts', 'Average Gift'))  # ~OK
     print(fstring.format('Donor Name', 'Total', 'Num Gifts', 'Average Gift'))  
-    # print('-' * (maxn + 54)) # ~OK
-    print('-' * (2 * maxn + 26))
+    print('-' * (maxn + 54)) # ~OK
 
     for i in donors.keys():
         # print('>>', i, 'donated:', donors[i])
@@ -79,7 +83,7 @@ def report():
 def main():
     answer = None
     print(
-        'options: 1 == quit | 2 == thankyou | 3 == report'
+        '> Options: 1 == quit | 2 == thankyou | 3 == report'
         )
         
     while answer != 'quit' and answer != '1':
