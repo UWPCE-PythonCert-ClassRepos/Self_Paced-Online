1ec7c43bb7918eeb1cb45816c6112c9da8b5f1bc
Ben Kuhn <kuhnbt@gmail.com>
2018-02-21 21:27:32 -0500

    Final changes to mailroom and kata_script

--- students/kuhnbt/lesson4/kata_script.py
+++ students/kuhnbt/lesson4/kata_script.py
@@ -1,4 +1,4 @@
-# My book is Dante's inferno, the first of the three books of the divine comedy
+# My book is Dante's inferno (technically an epic poem)
 import requests
 import string
 import random
@@ -9,33 +9,35 @@ full_text = requests.get(url).text
 start = 'Inferno: Canto I'
 end = 'End of the Project Gutenberg EBook'
 
+# Remove start and end material, newlines and punctuation
+
 full_text = full_text[full_text.find(start):full_text.find(end)]
 
-full_text = full_text.replace('\n',' ').replace('\r',' ')
+full_text = full_text.replace('\n', ' ').replace('\r', ' ')
 for s in string.punctuation:
     full_text = full_text.replace(s, '')
 
 full_text = full_text.split()
 
+
 def generate_dict(txt):
-    #find other dict methd to do this
+    """Create triplets dictionary in format (wd_one, wd_two):wd_three"""
     d = {}
     for idx in range(len(txt)-2):
         wd_one, wd_two, wd_three = txt[idx], txt[idx+1], txt[idx+2]
-        if d.get((wd_one, wd_two)):
-            d[(wd_one, wd_two)].append(wd_three)
-        else:
-            d[(wd_one, wd_two)] = [wd_three]
+        d.setdefault((wd_one, wd_two), []).append(wd_three)
     return d
 
+
 def generate_text(trigrams_dict, length):
-    """Generate text with given number of words using trigrams_dict"""
+    """Generate text from trigrams dict with given number of words"""
     start_loc = random.randint(0, len(trigrams_dict))
     start_key = list(trigrams_dict.keys())[start_loc]
     results = [start_key[0], start_key[1]]
-    for _ in range(length):
+    for _ in range(length-2):
         next_word_choices = trigrams_dict[start_key]
-        next_word = next_word_choices[random.randint(0, len(next_word_choices))]
+        next_word = next_word_choices[random.randint(0,
+                                      len(next_word_choices)-1)]
         start_key = (start_key[1], next_word)
         results.append(next_word)
-    return results
+    return ' '.join(results)
--- students/kuhnbt/lesson4/mailroom_v2.py
+++ students/kuhnbt/lesson4/mailroom_v2.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-
 import os
+import datetime
 
 donor_info = {
-             'Ernest Hemingway':[35, 65.25, 40],
-             'John Steinbeck':[25.50, 20, 10], 
-             'Joseph Heller':[45, 20, 105.50],
+             'Ernest Hemingway': [35, 65.25, 40],
+             'John Steinbeck': [25.50, 20, 10],
+             'Joseph Heller': [45, 20, 105.50],
              'Kurt Vonnegut': [60, 40, 400],
              'Michael Chabon': [100, 200, 300, 400]
              }
 
+
 def start_program():
     """Prompt user for desired donor action and fulfill the request"""
     exit_program = False
-    selection_dict = {'1':send_thankyou, '2':create_report,
-                      '3':send_to_everyone, '4':quit_program}
+    selection_dict = {'1': send_thankyou, '2': create_report,
+                      '3': send_to_everyone, '4': quit_program}
     while not exit_program:
         selection = input('''Please enter a selection (1-4):
                            1. Send a thank you
@@ -30,6 +31,7 @@ def start_program():
 
 def quit_program():
     return True
+
 
 def send_thankyou():
     """Send a thank you note to person designated by user; add person to
@@ -55,15 +57,16 @@ def create_report():
     """Print summary report of donor info"""
     max_donor_width = 0
     for name in donor_info:
-        if len(name)>max_donor_width:
+        if len(name) > max_donor_width:
             max_donor_width = len(name)
-    print('{:{}}|{:12}|{:10}|{:8}'.format('Donor Name', max_donor_width, 
+    print('{:{}}|{:12}|{:10}|{:8}'.format('Donor Name', max_donor_width,
           'Total Given', 'Num Gifts', 'Average Gift'))
     donor_list = list(donor_info.items())
     for donor in sorted(donor_list, key=lambda x: sum(x[1])):
-        print('{:{}}|${:^11.2f}|{:^10}|${:^8.2f}'.format(donor[0], 
-            max_donor_width, sum(donor[1]), len(donor[1]), 
-            sum(donor[1])/len(donor[1])))
+        print('{:{}}|${:^11.2f}|{:^10}|${:^8.2f}'.format(donor[0],
+              max_donor_width, sum(donor[1]), len(donor[1]),
+              sum(donor[1])/len(donor[1])))
+
 
 def send_to_everyone(directory=os.getcwd()):
     """Write thank you notes to specified directory for each donor in
@@ -71,8 +74,9 @@ def send_to_everyone(directory=os.getcwd()):
     user_dir = input('Change output directory (y/n)?')
     if user_dir.lower() == 'y':
         directory = input('Please enter desired directory:')
+    today = str(datetime.datetime.today())[:10]
     for donor in donor_info:
-        filename = '/'.join([directory, donor])+'.txt'
+        filename = '/'.join([directory, donor])+'_' + today + '.txt'
         with open(filename, 'w') as f:
             print('Writing to ' + filename + '...')
             f.write(get_thank_you(donor))
@@ -81,14 +85,21 @@ def send_to_everyone(directory=os.getcwd()):
 
 def get_thank_you(donor):
     """Return text of thank you letter for given donor"""
-    thankyou = f'Dear {donor}:\nThank you for your generous donation of'\
-               f' ${donor_info[donor][-1]:.2f}.\n'
-    if len(donor_info[donor])>1:
-        thankyou += f'I also appreciate your {len(donor_info[donor])}'\
-        ' previous donations to our organization.\n'   
-    thankyou += 'I assure you that your contributions will be put to'\
-                ' good use!\n\nRegards,\nBen'
+    donor_dict = {'name': donor, 'donation': donor_info[donor][-1],
+                  'num_donations': len(donor_info[donor])}
+    donor_dict['multiple'] = 's' if donor_dict['num_donations'] > 1 else ''
+    thankyou = '''
+    Dear {name}:
+    Thank you for your generous donation of ${donation:.2f}.
+    I really appreciate your {num_donations}
+    donation{multiple} to our organization.
+    I assure you that your contributions will be put to
+    good use!
+    Regards,
+    Ben'''.format(**donor_dict)
+
     return thankyou
+
 
 if __name__ == '__main__':
     start_program()
