8706c55380079f1ad66731125d8115b953e86f99
Dennis Lee <dennis271lee@yahoo.com>
2018-02-22 15:47:12 -0800

    [DennisLee][Lesson 02] Added the sum_series function.

--- students/DennisLee/lesson02/series.py
+++ students/DennisLee/lesson02/series.py
@@ -50,6 +50,36 @@ def lucas(n: int) -> int:
             x, y = y, x + y
         return y
 
+def sum_series(index_num: int,
+               index_0_value: int = 0,
+               index_1_value: int = 1) -> int:
+    """
+    Retrieve a particular number within a series that contains the sum
+    of the series's previous two values.
+
+    :index_num:  The position number to retrieve (zero-based index).
+
+    :index_0_value:  The value of the first value within the series.
+                     Default is the fibonacci series's first value (0).
+
+    :index_1_value:  The value of the second value within the series.
+                     Default is the fibonacci series's second value (0).
+
+    :return: The value of the specified index number within the series.
+    """
+
+    if index_num < 0:
+        return -1  # error code - indexes can't be negative
+    elif index_num == 0:
+        return index_0_value
+    elif index_num == 1:
+        return index_1_value
+    else:
+        x, y = index_0_value, index_1_value
+        for i in range(index_num - 1):
+            x, y = y, x + y
+        return y    
+
 if __name__ == '__main__':
     print('The first 50 fibonacci numbers:')
     for a in range(50):
@@ -58,3 +88,15 @@ if __name__ == '__main__':
     print('\nThe first 50 lucas numbers:')
     for a in range(50):
         print(lucas(a))
+
+    print('\nThe first 50 numbers of the sum_series function [#0==1,#1==2]:')
+    for a in range(50):
+        print(sum_series(a, 1, 2))
+
+    print('\nThe first 50 numbers of the sum_series function [defaults/fibonacci]:')
+    for a in range(50):
+        print(sum_series(a))
+
+    print('\nThe first 50 numbers of the sum_series function [series=lucas]:')
+    for a in range(50):
+        print(sum_series(a, 2))
