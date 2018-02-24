fbfeff5e6b8f0610b4e706f3e3ff9d7f1cc72465
Dennis Lee <dennis271lee@yahoo.com>
2018-02-23 19:08:08 -0800

    [DennisLee][Lesson 03][Slicing Lab] Added param desc to docstrings.

--- students/DennisLee/lesson03/slicing_functions.py
+++ students/DennisLee/lesson03/slicing_functions.py
@@ -2,6 +2,8 @@ def first_last_exchange(seq):
     """
     Exchange the first and last items of a sequence.
 
+    :seq:  The input sequence (can be a tuple or a list).
+
     :return:  The updated sequence.
     """
     return seq[-1:] + seq[1:-1] + seq[:1]
@@ -9,6 +11,8 @@ def first_last_exchange(seq):
 def skip_every_other_item(seq):
     """
     Skip every other item in a sequence.
+
+    :seq:  The input sequence (can be a tuple or a list).
 
     :return:  A sequence containing only the even-numbered position
               values of the input sequence.
@@ -20,6 +24,8 @@ def remove_leading_and_trailing_4_items_and_take_every_other_item(seq):
     Strip the first 4 and last 4 items of a sequence, and then skip
     every other item from the remaining items in the sequence.
 
+    :seq:  The input sequence (can be a tuple or a list).
+
     :return:  The updated sequence.
     """
     return seq[4:-4:2]
@@ -27,6 +33,8 @@ def remove_leading_and_trailing_4_items_and_take_every_other_item(seq):
 def reverse_items(seq):
     """
     Reverse the ordering of a sequence.
+
+    :seq:  The input sequence (can be a tuple or a list).
 
     :return:  The sequence of reversed items.
     """
@@ -37,6 +45,8 @@ def move_first_third_of_items_to_end(seq):
     Create a new sequence with the middle third items of a given 
     sequence at the beginning, followed by the last third items, and
     finally the first third items of the original sequence.
+
+    :seq:  The input sequence (can be a tuple or a list).
 
     :return:  The updated sequence.
     """
