8dfbdd60acf2780e465e06d1b7ffa2e3f5e88429
Alex Laws <tal1286@gmail.com>
2018-02-25 13:26:27 -0800

    Changed dictionary structure

--- students/alexLaws/lesson04/mailroom.py
+++ students/alexLaws/lesson04/mailroom.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
-donor_list = []
-donor_list.append({'Name': 'Ryan Moore', 'Donation': 500})
-donor_list.append({'Name': 'Ryan Moore', 'Donation': 250})
-donor_list.append({'Name': 'Ted Laws', 'Donation': 1000})
-donor_list.append({'Name': 'Ted Laws', 'Donation': 100})
-donor_list.append({'Name': 'Ben Snell', 'Donation': 150})
-donor_list.append({'Name': 'Andrew Crawford', 'Donation': 2000})
-donor_list.append({'Name': 'Andrew Crawford', 'Donation': 2000})
-donor_list.append({'Name': 'Andrew Crawford', 'Donation': 4000})
-donor_list.append({'Name': 'Beth Ross', 'Donation': 400})
+donor_dict = {
+    'Ryan Moore': [500, 250], 'Ted Laws': [1000, 100], 'Ben Snell': [150],
+    'Andrew Crawford': [2000, 2000, 4000], 'Beth Ross': [400]}
 
 
 def user_action():
@@ -26,76 +19,35 @@ def user_action():
             print("That is not an option. Choose again.")
 
 
-def dnrs(x):
-    names = []
-    for dnr in x:
-        person = dnr.get("Name")
-        if person in names:
-            pass
-        else:
-            names.append(person)
-    return names
-
-
-def send_to(x):
-    donors = dnrs(x)
+def send_to(donor_list):
     recipient = input("Who is the thank you note for? Enter a full name or 'list' to see a list: ")
     if recipient.lower() == 'list':
         print("Here are all the donors:")
-        for name in donors:
-            print(name)
+        for i in donor_list:
+            print(i)
         recipient = input("Who is the thank you note for? ")
         return recipient
     else:
         return recipient
 
 
-def name_sum(person, x):
-    donor_total = 0
-    for i in x:
-        if i.get('Name') == person:
-            donor_total += int(i.get('Donation'))
-    return donor_total
-
-
-def name_cnt(person, x):
-    donor_count = 0
-    for i in x:
-        if i.get('Name') == person:
-            donor_count += 1
-    return donor_count
-
-
-def thank_you(recip, x):
+def thank_you(recip, donor_dictionary):
     print("OK, here is a thank you note for {}".format(recip))
-    total = name_sum(recip, x)
+    total = sum(donor_dictionary[recip])
     note = "Dear {},\nThank you for your recent donation. You have now donated ${}.\nThank you so much!\n- Alex Laws"
     print(note.format(recip, total))
 
 
-def total_sort(x):
-    donors = dnrs(x)
-    to_sort = []
-    for i in donors:
-        to_sort.append({'Name': i, 'Total': name_sum(i, x)})
-    amounts = []
-    for n in to_sort:
-        amounts.append(n.get('Total'))
-    amounts.sort(reverse=True)
-    ordered = []
-    for num in amounts:
-        for i in to_sort:
-            if i.get('Total') == num and i.get('Name') not in ordered:
-                ordered.append(i.get('Name'))
-    return ordered
-
-
-def report(x):
+def report(donor_dictionary):
     print("Donor Name       |  Total Given  |  Num Gifts  |  Average Gift")
-    donors = total_sort(x)
-    for donor in donors:
-        total = name_sum(donor, x)
-        number = name_cnt(donor, x)
+    to_sort = {}
+    for donor in donor_dictionary:
+        total = sum(donor_dictionary[donor])
+        to_sort[donor] = total
+    in_order = sorted(to_sort, key=to_sort.__getitem__, reverse=True)
+    for donor in in_order:
+        total = to_sort[donor]
+        number = len(donor_dictionary[donor])
         average = total / number
         print("{:17} ${:14,.2f} {:13} ${:13,.2f}".format(donor, total, number, average))
 
@@ -104,15 +56,15 @@ if __name__ == '__main__':
     while True:
         act = user_action().lower()
         if act == "send a thank you":
-            name = send_to(donor_list)
-            if name in dnrs(donor_list):
-                thank_you(name, donor_list)
+            name = send_to(donor_dict)
+            if name in donor_dict:
+                thank_you(name, donor_dict)
             else:
                 amt = int(input("Wait, A new donation! How much: "))
-                donor_list.append({'Name': name, 'Donation': amt})
-                thank_you(name, donor_list)
+                donor_dict[name] = [amt]
+                thank_you(name, donor_dict)
         if act == "create a report":
-            report(donor_list)
+            report(donor_dict)
         if act == "quit":
             break
         print("Would you like to do something else...")
