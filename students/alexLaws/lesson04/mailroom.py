c226dafb059c55cef827ff9ffa6d401791f83174
Alex Laws <tal1286@gmail.com>
2018-02-25 16:05:40 -0800

    Added switch menu functionality

--- students/alexLaws/lesson04/mailroom.py
+++ students/alexLaws/lesson04/mailroom.py
@@ -5,25 +5,29 @@ donor_dict = {
     'Andrew Crawford': [2000, 2000, 4000], 'Beth Ross': [400]}
 
 
-def user_action():
-    print("Choose an action from this list:")
-    print("Send a Thank You")
-    print("Create a Report")
-    print("Quit")
+prompt = ("\nWhat would you like to do\n"
+          "Choose an action from this list:\n"
+          "1 - Send a Thank You\n"
+          "2 - Create a Report\n"
+          "3 - Quit\n")
+
+
+def menu_selection(prompt, dispatch_dict, donor_dictionary):
     while True:
-        option = input("What is your choice: ")
-        if option.lower() in ["send a thank you", "create a report", "quit"]:
-            return option
+        response = input(prompt)
+        if dispatch_dict[response](donor_dictionary) == "Exit Menu":
             break
-        else:
-            print("That is not an option. Choose again.")
 
 
-def send_to(donor_list):
+def exit(a):
+    return "Exit Menu"
+
+
+def send_to(donor_dictionary):
     recipient = input("Who is the thank you note for? Enter a full name or 'list' to see a list: ")
     if recipient.lower() == 'list':
         print("Here are all the donors:")
-        for i in donor_list:
+        for i in donor_dictionary:
             print(i)
         recipient = input("Who is the thank you note for? ")
         return recipient
@@ -31,11 +35,16 @@ def send_to(donor_list):
         return recipient
 
 
-def thank_you(recip, donor_dictionary):
-    print("OK, here is a thank you note for {}".format(recip))
-    total = sum(donor_dictionary[recip])
+def thank_you(donor_dictionary):
+    name = send_to(donor_dictionary)
+    if name in donor_dictionary:
+        amt = sum(donor_dictionary[name])
+    else:
+        amt = int(input("Wait, A new donation! How much: "))
+        donor_dict[name] = [amt]
+    print("OK, here is a thank you note for {}".format(name))
     note = "Dear {},\nThank you for your recent donation. You have now donated ${}.\nThank you so much!\n- Alex Laws"
-    print(note.format(recip, total))
+    print(note.format(name, amt))
 
 
 def report(donor_dictionary):
@@ -52,19 +61,7 @@ def report(donor_dictionary):
         print("{:17} ${:14,.2f} {:13} ${:13,.2f}".format(donor, total, number, average))
 
 
+menu_dict = {"1": thank_you, "2": report, "3": exit}
+
 if __name__ == '__main__':
-    while True:
-        act = user_action().lower()
-        if act == "send a thank you":
-            name = send_to(donor_dict)
-            if name in donor_dict:
-                thank_you(name, donor_dict)
-            else:
-                amt = int(input("Wait, A new donation! How much: "))
-                donor_dict[name] = [amt]
-                thank_you(name, donor_dict)
-        if act == "create a report":
-            report(donor_dict)
-        if act == "quit":
-            break
-        print("Would you like to do something else...")
+    menu_selection(prompt, menu_dict, donor_dict)
