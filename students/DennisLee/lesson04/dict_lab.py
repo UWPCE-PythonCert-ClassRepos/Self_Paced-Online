89d91aff85bbe2a8ef43602ac9113a1e03ceb749
Dennis Lee <dennis271lee@yahoo.com>
2018-03-01 01:27:52 -0800

    L4 A1 Ungraded: Added sets 1 and 2, and simplified dictionary 2.

--- students/DennisLee/lesson04/dict_lab.py
+++ students/DennisLee/lesson04/dict_lab.py
@@ -13,12 +13,31 @@ def dict1():
 
 def dict2():
     d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
-    for x in d.keys():
-        d[x] = d[x].lower().count('t')
+    for item in d:
+        d[item] = d[item].lower().count('t')
     print(d)
+
+def set1():
+    s2 = set(range(0, 21, 2))
+    s3 = set(range(0, 21, 3))
+    s4 = set(range(0, 21, 4))
+    print(s2, s3, s4, sep='\n')
+    print(s3.issubset(s2))
+    print(s4.issubset(s2))
+
+def set2():
+    s = set('Python')
+    s.update('i')
+    f = frozenset('marathon')
+    print(s.union(f))
+    print(s.intersection(f))
 
 if __name__ == "__main__":
     print('\n\nDictionary 1')
     dict1()
     print('\n\nDictionary 2')
     dict2()
+    print('\n\nSet 1')
+    set1()
+    print('\n\nSet 2')
+    set2()
