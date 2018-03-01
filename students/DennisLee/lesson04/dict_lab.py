b1782c640384331df71c745534bf1f6347ecbaf6
Dennis Lee <dennis271lee@yahoo.com>
2018-03-01 01:13:18 -0800

    L4 A1 Ungraded: Added 2nd set of dictionaries

--- students/DennisLee/lesson04/dict_lab.py
+++ students/DennisLee/lesson04/dict_lab.py
@@ -11,5 +11,14 @@ def dict1():
     print('cake' in d.keys())
     print('Mango' in d.values())
 
+def dict2():
+    d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+    for x in d.keys():
+        d[x] = d[x].lower().count('t')
+    print(d)
+
 if __name__ == "__main__":
+    print('\n\nDictionary 1')
     dict1()
+    print('\n\nDictionary 2')
+    dict2()
