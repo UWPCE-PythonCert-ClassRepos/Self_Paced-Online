004c5492c6570e23885c0dfed52bb87b2334b3b1
Thomas Horn <hortw1991@gmail.com>
2018-03-01 22:21:43 -0600

    1st commit - functions set up.

---
+++ students/thorn/lesson03/slicing_basics.py
@@ -0,0 +1,34 @@
+"""
+Thomas Horn
+Series of functions that take a sequence as an argument and return a copy of
+that sequence with various manipulations.
+"""
+
+def first_last_exchange(og_sequence):
+    """ Exchanges the first and last items of the sequence. """
+    pass
+
+
+def every_other_remove(og_sequence):
+    """ Removes ever other items of the sequence, beginning with index 1. """
+    pass
+
+def first_4_last_4_every_other_removed(og_sequence):
+    """ 
+    Removes the first and last 4 items and every other item beginning with
+    index 1.
+    """
+    pass
+
+def reverse_slice(og_sequence):
+    """ Reverses the elements of the sequence via slicing. """
+    pass
+
+def middlelast_lastfirst_firstmiddle(og_sequence):
+    """ 
+    Moves the first third to the middle third's position, middle third to last
+    third, and last third to first third.
+    """
+    pass
+
+
