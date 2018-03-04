bf5c9e43dfe8aad7a10bbb97206bb8fc581a1aa6
22e88 <22e88@mailbox.org>
2018-03-04 06:36:45 +0100

    kata14.py: work in progress

--- students/elmar_m/lesson04/kata14.py
+++ students/elmar_m/lesson04/kata14.py
@@ -2,24 +2,39 @@
 
 infile = './sherlock_small.txt'
 
-f = open(infile, 'r')
-
-lines = f.readlines()
-print('\n', type(lines), '\n')
-print(lines)
-print('\nnumber of elements:', len(lines), '\n')
-
 wordlist = [] 
 
-for i in lines:
-    print('--- beginning ---')
-    # wordlist = i.split()
+with open(infile, 'r') as f:
+    # lines = f.readlines()
+    lines = (l.strip('\n') for l in f)
+    print('\n', type(lines), '\n')
+
+    # for i in lines:
+    #     print(i)
+
+    # for i in lines:
+    #     wordlist.append(i.split())
+    
     wordlist.append(i.split())
-    print('--- end ---')
 
 for w in wordlist:
     print(w)
 
+#print('\n', type(lines), '\n')
+#print(lines)
+#print('\nnumber of elements:', len(lines), '\n')
+#
+#wordlist = [] 
+#
+#for i in lines:
+#    print('--- beginning ---')
+#    # wordlist = i.split()
+#    wordlist.append(i.split())
+#    print('--- end ---')
+#
+#for w in wordlist:
+#    print(w)
+#
     
 
 # line = f.readline()
