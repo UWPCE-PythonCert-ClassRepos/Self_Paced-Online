ccc532a219c0315f1ca674f1a891454e952e9f43
Dustin L <r.dustin.lane@gmail.com>
2018-03-04 11:10:20 -0800

    Test Mailroom - Add test stubs

---
+++ students/Dustin_L/lesson06/test_mailroom3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""This is a test module that tests mailroom3.py"""
+
+import unittest
+import mailroom3 as mr
+
+
+class TestMailRoom(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init_donor_data(self):
+        pass
+
+    def test_get_usr_input(self):
+        pass
+
+    def test_get_donor_names(self):
+        pass
+
+    def test_prompt_for_donor(self):
+        pass
+
+    def test_prompt_for_donation(self):
+        pass
+
+    def test_add_donation(self):
+        pass
+
+    def test_send_thank_you(self):
+        pass
+
+    def test_create_report(self):
+        pass
+
+    def test_quit_mailroom(self):
+        pass
+
+    def test_send_letters(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
