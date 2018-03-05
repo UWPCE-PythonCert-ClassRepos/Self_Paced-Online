d560ac4df80276fe43efbd1631c6f9f968bc9f10
Dennis Lee <dennis271lee@yahoo.com>
2018-03-04 19:12:23 -0800

    [DennisLee][Lesson 04][Trigram] Constructed paragraphs, created dicts.

--- students/DennisLee/lesson04/trigram.py
+++ students/DennisLee/lesson04/trigram.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 
+def process_text(file):
+    with open(file, 'r') as f:
+        # Build paragraph list
+        pg, paragraphs = '', list()
+        for line in f:
+            if line.strip() != '':
+                pg += line.strip() + ' '
+                continue
+            paragraphs.append(pg)
+            pg = ''
+
+        word_pairs = dict()
+        for pg in paragraphs:
+            words = pg.split(' ')
+            for i in range(len(words[:-3])):
+                key = f'{words[i]:s} {words[i+1]:s}'
+                word_pairs.setdefault(key, set())
+                word_pairs[key].add(words[i+2])
+        for k, v in word_pairs.items():
+            print(f'Key: {k:30s}  Value: {v}')
+
+if __name__ == '__main__':
+    process_text('sherlock_small.txt')
