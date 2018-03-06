c0e18cc90e9c59fde201435558575361cecc4469
idcrickmore <idcrickmore@gmail.com>
2018-03-05 17:30:51 -0800

    commited series.py with pep8 edits

--- students/idcrickmore/lesson02/series.py
+++ students/idcrickmore/lesson02/series.py
@@ -1,10 +1,12 @@
 def fibonacci(n):
     return sum_series(n, 0, 1)
 
+
 def lucas(n):
     return sum_series(n, 2, 1)
 
-def sum_series(n, first = 0, second = 1):
+
+def sum_series(n, first=0, second=1):
     # function returns the sum of the two numbers preceding the
     # 'nth' position in the series, where the first two numbers
     # are 0 and 1 by default
@@ -23,24 +25,25 @@ def sum_series(n, first = 0, second = 1):
         # two values. Stop the loop when the counter reaches the 2nd
         # position in the sequence
         count = n
-        while count > 2:          
+        while count > 2:
             first, second = second, first + second
             count -= 1
         return first + second
 
-#assertion test for fibonacci
+
+# assertion test for fibonacci
 assert fibonacci(0) == 0
 assert fibonacci(1) == 1
 assert fibonacci(2) == 1
 assert fibonacci(3) == 2
 
-#assertion test for lucas
+# assertion test for lucas
 assert lucas(0) == 2
 assert lucas(1) == 1
 assert lucas(2) == 3
 assert lucas(3) == 4
 
-#assertion test for sum_series
+# assertion test for sum_series
 assert sum_series(0, 2, 2) == 2
 assert sum_series(1, 2, 2) == 2
 assert sum_series(2, 2, 2) == 4
