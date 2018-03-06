3de54f1277d72df273367b997108ca36ffbe0362
22e88 <22e88@mailbox.org>
2018-03-06 04:26:28 +0100

    kata14.py: work in progress

--- students/elmar_m/lesson04/kata14.py
+++ students/elmar_m/lesson04/kata14.py
@@ -14,37 +14,52 @@ def create_oneline():
         with open(oneline, 'w') as o:
             o.write(a)
 
+def get_stuff(n, l):
+    # if n > 8:
+    if n > (len(l) - 3):
+        # print('n over 8, exiting')
+        print('n reached ', len(l) - 3, 'exiting')
+        return
+    else:
+        # e = n + 3
+        e = n + 2
+        # part = l[n:e]
+        k = l[n:e]
+        v = l[e] 
+        # print(part)
+        # print(k, v)
+        print(' '.join(k), v)
+        get_stuff(n + 1, l)
+)
 
 def create_dict():
     with open(oneline, 'r') as ofile:
         wordlist = ofile.readline().split()
-        wordlist.reverse() 
         # while wordlist: 
-        while wordlist[:]: 
-            
-            s = 0
-            e = s + 2
-            n = s + 2
-            # key = str(' '.join(wordlist[0:2]))
-            key = str(' '.join(wordlist[s:e]))
-            print(key)
-            # struct[key] = str(wordlist[n])
-            struct[key].append(str(wordlist[n]))
-            s += 1
-            wordlist.pop()
+        # while wordlist[:]: 
+        get_stuff(0, wordlist)
+            # key = str(' '.join(wordlist[s:e]))
+            # print(key)
+            # struct[key].append(str(wordlist[n]))
+            # # s += 1
+            # wordlist.pop()
+            # print('bla')
+            # get_stuff(s + 1)
+                
+    print('ende create_dict')
 
 
 def show_dict():
     for i, v in struct:
         print(i, v)
-        
+        print('ende show_dict') 
 
     # print(struct)
 
 def main():
     create_oneline()
     create_dict()
-    show_dict()
+    # show_dict()
 
 
 if __name__ == '__main__':
