ba4b4a115f5d12f39d007dd9fd672fb9c6520405
22e88 <22e88@mailbox.org>
2018-03-06 08:36:22 +0100

    Add work in progress kata14.py, add pg1661.txt
        pg1661.txt should be the same as sherlock.txt, just downloaded
        from another source

--- students/elmar_m/lesson04/kata14.py
+++ students/elmar_m/lesson04/kata14.py
@@ -2,8 +2,9 @@
 
 import collections
 
-infile = './sherlock_small.txt'
+# infile = './sherlock_small.txt'
 # infile = './sherlock.txt'
+infile = './pg1661.txt'
 oneline = './oneline.txt'
 the_dict = collections.defaultdict(list)
 
@@ -19,23 +20,38 @@ def create_biglist():
     with open(oneline, 'r') as ofile:
         biglist = ofile.readline().split()
         # get_stuff(0, biglist)
-    get_stuff(0, biglist)
+    # get_stuff(0, biglist)
+
+    for i in get_stuff_generator(0, biglist):
+        print(i)
 
 
-def get_stuff(n, l):
-    if n > (len(l) - 3):
-        print('n reached ', len(l) - 3, ', exiting')
-        return
-    else:
+# def get_stuff(n, l):
+#     if n > (len(l) - 3):
+#         print('n reached ', len(l) - 3, ', exiting')
+#         return
+#     else:
+#         e = n + 2
+#         k = l[n:e]
+#         y = l[e] 
+#         x = ' '.join(k)
+#         print('|=', x, '=|', y)
+#         create_dict(x, y)
+#         # return(x, y)
+#         get_stuff(n + 1, l)     # start recursion / do it again...
+
+def get_stuff_generator(n, l):
+    while n >= 0:
         e = n + 2
         k = l[n:e]
         y = l[e] 
         x = ' '.join(k)
-        print('|=', x, '=|', y)
-        create_dict(x, y)
-        # return(x, y)
-        get_stuff(n + 1, l)     # start recursion / do it again...
+        # print('|=', x, '=|', y)
+        yield x
+        n += 1
 
+# for i in get_stuff_generator(0, biglist):
+#     print(i)
 
 def create_dict(k, v):
         # the_dict[k].append(str(the_dict[v]))    # put key and value in dictionary
