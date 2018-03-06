3a5da568be9e4441b1699e40c1af1a73cff47e0d
22e88 <22e88@mailbox.org>
2018-03-06 06:00:55 +0100

    kata14.py: work in progress

--- students/elmar_m/lesson04/kata14.py
+++ students/elmar_m/lesson04/kata14.py
@@ -5,62 +5,55 @@ import collections
 infile = './sherlock_small.txt'
 # infile = './sherlock.txt'
 oneline = './oneline.txt'
-struct = collections.defaultdict(list)
+the_dict = collections.defaultdict(list)
 
 
-def create_oneline():
-    with open(infile, 'r') as f:
-        a = ' '.join(l.strip() for l in f)
+def create_oneline(f):
+    with open(f, 'r') as myfile:
+        a = ' '.join(l.strip() for l in myfile)
         with open(oneline, 'w') as o:
             o.write(a)
 
+
+def create_biglist():
+    with open(oneline, 'r') as ofile:
+        biglist = ofile.readline().split()
+        # get_stuff(0, biglist)
+    get_stuff(0, biglist)
+
+
 def get_stuff(n, l):
-    # if n > 8:
     if n > (len(l) - 3):
-        # print('n over 8, exiting')
-        print('n reached ', len(l) - 3, 'exiting')
+        print('n reached ', len(l) - 3, ', exiting')
         return
     else:
-        # e = n + 3
         e = n + 2
-        # part = l[n:e]
         k = l[n:e]
-        v = l[e] 
-        # print(part)
-        # print(k, v)
-        print(' '.join(k), v)
-        get_stuff(n + 1, l)
-)
+        y = l[e] 
+        x = ' '.join(k)
+        print('|=', x, '=|', y)
+        create_dict(x, y)
+        # return(x, y)
+        get_stuff(n + 1, l)     # start recursion / do it again...
 
-def create_dict():
-    with open(oneline, 'r') as ofile:
-        wordlist = ofile.readline().split()
-        # while wordlist: 
-        # while wordlist[:]: 
-        get_stuff(0, wordlist)
-            # key = str(' '.join(wordlist[s:e]))
-            # print(key)
-            # struct[key].append(str(wordlist[n]))
-            # # s += 1
-            # wordlist.pop()
-            # print('bla')
-            # get_stuff(s + 1)
-                
-    print('ende create_dict')
+
+def create_dict(k, v):
+        # the_dict[k].append(str(the_dict[v]))    # put key and value in dictionary
+        the_dict[k].append(str(v))    # put key and value in dictionary
 
 
 def show_dict():
-    for i, v in struct:
-        print(i, v)
-        print('ende show_dict') 
+    for i in the_dict:
+        print('key:', i, 'value:', the_dict[i])
+    print('ende show_dict') 
 
-    # print(struct)
 
 def main():
-    create_oneline()
-    create_dict()
-    # show_dict()
-
+    create_oneline(infile)
+    create_biglist()
+    # get_stuff()
+    # create_dict()
+    show_dict()
 
 if __name__ == '__main__':
     main()
