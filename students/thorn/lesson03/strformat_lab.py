dcb521c02e799b9c37053366d35d0078932ea06f
Thomas Horn <hortw1991@gmail.com>
2018-03-07 15:52:13 -0600

    Brief update.

--- students/thorn/lesson03/strformat_lab.py
+++ students/thorn/lesson03/strformat_lab.py
@@ -1,7 +1,19 @@
-#!/usr/bin/env python3
+""" #!/usr/bin/env python3 """
 
 """
 Thomas Horn
 - String Formatting Lab
 """
 
+def task_one(in_tuple = (2, 123.4567, 10000, 12345.67)):
+    """ Takes a tuple as a parameter and formats its members as follows:
+    1: Generates a filename.
+    2: Float displayed with 2 decimal places.
+    3: Integer displayed in scientific notation with 2 decimal places.
+    4: Float displayed in scientific notation to 3 sig figs.
+    """
+    
+
+
+
+task_one()
