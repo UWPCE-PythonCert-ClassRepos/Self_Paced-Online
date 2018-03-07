7e4a90cedb03e2fb42ea13c887d820a9fbe813ed
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-03-07 09:21:38 -0800

    lesson04 - fixed the join method in while loop

--- students/ghassan/lesson04/trig.py
+++ students/ghassan/lesson04/trig.py
@@ -2,9 +2,10 @@
 
 from pprint import pprint
 
-with open('data.txt', 'r') as datafh:
-    data = datafh.read().split()
+''' with open('data.txt', 'r') as datafh:
+    data = datafh.read().split() '''
 
+data = "I wish I may I wish I might".split()
 z = {}
 
 
@@ -13,7 +14,7 @@ for item in range(0, len(data)-2):
     z['{} {}'.format(
         data[item], data[item+1])].append('{}'.format(data[item+2]))
 
-key = 'I should'
+key = 'I may'
 
 new_file = [x for x in key.split()]
 
@@ -21,9 +22,8 @@ new_file = [x for x in key.split()]
 try:
     while True:
         for word in range(0, len(new_file)-1):
-            for y in z[' '.join(new_file[:])]:
+            for y in z[' '.join(new_file[-2:])]:
                 new_file.append(y)
-            print(new_file)
 except KeyError:
     with open('new_book.txt', 'w') as newfh:
         newfh.write(' '.join(new_file))
