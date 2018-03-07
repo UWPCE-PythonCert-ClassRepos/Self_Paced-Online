a20420c64b23968bc20a2289669be39858f8835d
Chris Kenyon <cbkenyon89@gmail.com>
2018-03-07 19:43:14 +0000

    Lesson 3: Activity 2

--- students/Chris_Kenyon/Lesson_3/list_lab.py
+++ students/Chris_Kenyon/Lesson_3/list_lab.py
@@ -6,32 +6,32 @@ series_1 = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print(series_1)
 
 # get user input for new fruit and add it to the end of the list
-add_fruit = raw_input('Enter an additional fruit name: ')
+add_fruit = input('Enter an additional fruit name: ')
 series_1.append(add_fruit)
 print(series_1)
 
 # get a number from user and diplay that number and the fruit in that position
 while True:
     try:
-        user_num = int(raw_input('Pick a fruit in the list between 1 and {}: '
+        user_num = int(input('Pick a fruit in the list between 1 and {}: '
                        .format(len(series_1))))
     except ValueError:
-        print "you have made an invalid choice, try again."
+        print("you have made an invalid choice, try again.")
         continue
     if user_num > 0 and user_num < len(series_1)+1:
         print('The fruit that is number {} in the list is {}'
               .format(user_num, series_1[user_num-1]))
         break
-    print "you have made an invalid choice, try again."
+    print("you have made an invalid choice, try again.")
 
 # add string to font of list using +
-add_fruit_2 = raw_input('Enter a fruit to add to the front of the list: ')
+add_fruit_2 = input('Enter a fruit to add to the front of the list: ')
 series_1 = [add_fruit_2] + series_1
 print(series_1)
 
 # add string to fruont of list using insert
-add_fruit_3 = raw_input('Enter another fruit to add
-                        to the front of the list: ')
+add_fruit_3 = input("Enter another fruit to add"
+					" to the front of the list: ")
 series_1.insert(0, add_fruit_3)
 print(series_1)
 
@@ -58,13 +58,13 @@ print("The fruits had babies \n {}".format(series_2))
 
 # While loop to check for proper input and remove fruit from list
 while True:
-    rem_fruit = raw_input('Please select a fruit from the list to remove: ')
+    rem_fruit = input('Please select a fruit from the list to remove: ')
     if rem_fruit in series_2:
         while rem_fruit in series_2:
             series_2.remove(rem_fruit)
         break
-    print("That fruit is not on the list, please
-          check spelling and capitalization and try again")
+    print("That fruit is not on the list, please"
+          " check spelling and capitalization and try again")
 print(series_2)
 
 
@@ -77,7 +77,7 @@ series_3 = ['Apples', 'Pears', 'Oranges', 'Peaches']
 gross_fruit = []
 for f in series_3:
     while True:
-        yorn = raw_input('Do you like {}? (y/n): '.format(f.lower()))
+        yorn = input('Do you like {}? (y/n): '.format(f.lower()))
         if yorn[0].lower() == 'n':
             gross_fruit += [f]
             print("me neither they are gross af")
