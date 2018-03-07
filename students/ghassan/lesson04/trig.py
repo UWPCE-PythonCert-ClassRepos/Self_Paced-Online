2fc1fba18d7150b263a3012160d3b2df4a5595d7
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-03-06 16:52:16 -0800

    Lesson04 - Finished the second part of the assignment

--- students/ghassan/lesson04/trig.py
+++ students/ghassan/lesson04/trig.py
@@ -10,8 +10,20 @@ z = {}
 
 for item in range(0, len(data)-2):
     z.setdefault('{} {}'.format(data[item], data[item+1]), [])
-    z['{} {}'.format(data[item], data[item+1])].append('{}'.format(data[item+2]))
+    z['{} {}'.format(
+        data[item], data[item+1])].append('{}'.format(data[item+2]))
 
-pprint(z)
+key = 'I should'
+
+new_file = [x for x in key.split()]
 
 
+try:
+    while True:
+        for word in range(0, len(new_file)-1):
+            for y in z[' '.join(new_file[:])]:
+                new_file.append(y)
+            print(new_file)
+except KeyError:
+    with open('new_book.txt', 'w') as newfh:
+        newfh.write(' '.join(new_file))
