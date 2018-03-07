178221d96a6d7d90dadc30d8978fd6f773698bf4
Jerry.H <doridoriae86@gmail.com>
2018-03-07 14:24:22 -0800

    lesson7

--- students/JerryH/Lesson07/html_render.py
+++ students/JerryH/Lesson07/html_render.py
@@ -4,7 +4,6 @@ class Element:
     tag = ""
     indent = "  "
 
-
     def __init__(self, content=None, **style):
         self.content = [content] if content else []
         self.styles = style
@@ -15,25 +14,29 @@ class Element:
 
 
     def render(self, fileout, cur_ind=""):
-        #print(self, "-cur_ind:{}end".format(cur_ind))
-
         fileout.write(cur_ind + "<{}".format(self.tag))
+
         for key, value in self.styles.items():
             fileout.write(' {}="{}"'.format(key,value))
         fileout.write(">\n")
+
         for item in self.content:
             if isinstance(item, Element):
                 item.render(fileout, cur_ind + self.indent)
             else: # output the content
                 fileout.write(cur_ind + self.indent + item)
                 fileout.write("\n")
-
-           #print(fileout.getvalue())
+                
         fileout.write(cur_ind + "</{}>\n".format(self.tag))
 
 
 class Html(Element):
     tag = 'html'
+
+
+    def render(self, fileout, cur_ind=""):
+        fileout.write("<!DOCTYPE html>\n")
+        Element.render(self, fileout, cur_ind="")
 
 
 class Body(Element):
@@ -43,26 +46,75 @@ class Body(Element):
 class P(Element):
     tag = 'p'
 
+
 class Head(Element):
     tag = 'head'
 
-class Title(Element):
-    tag = 'title'
+
+class SelfClosingTag(Element):
+
+    def __init__(self, **style):
+        ''' SelfClosingTag no content required '''
+        self.styles = style
 
     def render(self, fileout, cur_ind=""):
-        fileout.write(cur_ind + "<{}>".format(self.tag))
+        fileout.write(cur_ind + "<{}".format(self.tag))
+        for key, value in self.styles.items():
+            fileout.write(' {}="{}"'.format(key,value))
+        fileout.write(" />\n")
 
+
+class OneLineTag(Element):
+
+    def render(self, fileout, cur_ind=""):
+        ''' Makes this Element all in one line '''
+        fileout.write(cur_ind + "<{}".format(self.tag))
+        for key, value in self.styles.items():
+            fileout.write(' {}="{}"'.format(key,value))
+        fileout.write(">")
         for item in self.content:
             if isinstance(item, Element):
-                item.render(fileout)
-            else:
+                item.render(fileout, cur_ind + self.indent)
+            else: # output the content
                 fileout.write(item)
+
         fileout.write("</{}>\n".format(self.tag))
 
 
-    '''
-    el = Element("First content here")
-    print(el.content)
-    el.append("second sentence")
-    print(el.content)
-    '''
+class Title(OneLineTag):
+    tag = 'title'
+
+
+class Meta(SelfClosingTag):
+    tag = 'meta'
+
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+
+
+class Br(SelfClosingTag):
+    tag = 'br'
+
+
+class A(Element):
+    tag = 'a'
+
+    def __init__(self, link, content):
+        Element.__init__(self, content, href=link)
+
+
+class Ul(Element):
+    tag = 'ul'
+
+
+class Li(Element):
+    tag = 'li'
+
+
+class H(OneLineTag):
+
+    def __init__(self, size, content, **style):
+        OneLineTag.__init__(self, content, **style)
+        self.size = size
+        self.tag = "h{}".format(size)
