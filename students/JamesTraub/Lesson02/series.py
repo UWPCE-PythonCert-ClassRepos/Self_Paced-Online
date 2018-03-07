8aedf501d8f6d39fed71a7da312a21fd9d9cc59a
James Traub <traub01-devtrain@outlook.com>
2018-03-07 02:35:11 -0800

    Submitting Fibonacci for feedback

--- students/JamesTraub/Lesson02/series.py
+++ students/JamesTraub/Lesson02/series.py
@@ -8,25 +8,23 @@
 # fib(n) = fib(n-2) + fib(n-1)
 
 
-def fibinacci(n):
-	fibi = [0,1]
-	if n == 0:
-		print(fibi[0])
-		return(0)
-	elif n == 1:
-		print(fibi[1])
-		return(1)
-	else:
-		for i in range(n):
-			small_fib = fibi[i]
-			range_plus1 = i + 1
-			big_fib = fibi[range_plus1]
-			next_fib = small_fib + big_fib
-			fibi.append(next_fib)
-		fib_minus2 = (n-2)
-		fib_minus1 = (n-1)
-		fib_nth = fibi[fib_minus2] + fibi[fib_minus1]
-		print(fib_nth)
+def fibonacci(n):
+    """Return the nth value in the Fibonacci series"""
+    fibi = [0, 1]
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        for i in range(n):
+            # create Fibonacci series
+            small_fib = fibi[i]
+            range_plus1 = i + 1
+            big_fib = fibi[range_plus1]
+            next_fib = small_fib + big_fib
+            fibi.append(next_fib)
+        fib_nth = fibi[n-2] + fibi[n-1]
+        print(fib_nth)
 
 
-fibinacci(2)
+fibonacci(7)
