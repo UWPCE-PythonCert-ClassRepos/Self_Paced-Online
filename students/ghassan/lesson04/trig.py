b87288a481056ff334de124375075aeff3851045
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-03-07 18:45:33 -0800

    lesson04 - Third try

--- students/ghassan/lesson04/trig.py
+++ students/ghassan/lesson04/trig.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import random
 from pprint import pprint
 
-''' with open('data.txt', 'r') as datafh:
-    data = datafh.read().split() '''
+with open('data.txt', 'r') as datafh:
+    data = datafh.read().split()
 
-data = "I wish I may I wish I might".split()
+
 z = {}
 
 
@@ -14,16 +15,16 @@ for item in range(0, len(data)-2):
     z['{} {}'.format(
         data[item], data[item+1])].append('{}'.format(data[item+2]))
 
-key = 'I may'
+key = random.choice(list(z))
 
-new_file = [x for x in key.split()]
+
+new_file = key.split()
 
 
 try:
     while True:
         for word in range(0, len(new_file)-1):
-            for y in z[' '.join(new_file[-2:])]:
-                new_file.append(y)
+            new_file.append(random.choice(z[' '.join(new_file[-2:])]))
 except KeyError:
     with open('new_book.txt', 'w') as newfh:
         newfh.write(' '.join(new_file))
