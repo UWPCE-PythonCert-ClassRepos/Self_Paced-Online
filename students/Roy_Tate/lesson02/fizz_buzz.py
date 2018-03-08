ceae44d9bf37d9975e884a9f0e3ba67ecf15cd6d
tater <roytate@hotmail.com>
2018-03-07 20:18:40 -0800

    added docstrings

--- students/Roy_Tate/lesson02/fizz_buzz.py
+++ students/Roy_Tate/lesson02/fizz_buzz.py
@@ -1,6 +1,7 @@
 # author: githubtater
 
 def fizz_buzz():
+    '''A classic, simple computer science problem'''
     current_number = 1
     while current_number < 101:
         if (current_number % 3 == 0) and (current_number % 5 == 0):
--- students/Roy_Tate/lesson02/series.py
+++ students/Roy_Tate/lesson02/series.py
@@ -1,6 +1,7 @@
 # author: githubtater
 
 def fibonacci(n):
+    '''Return the nth value in the fibonacci series.'''
     if n == 1:
         return 1
     elif n == 0:
@@ -8,10 +9,11 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)  # formula from example: fib(n) = fib(n-2) + fib(n-1)
 
-print(fibonacci(5))
+print(fibonacci(7))
 
 
 def lucas(n):
+    '''Fibonacci() with a twist'''
     if n == 0:
         return 2
     elif n == 1:
