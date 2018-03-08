23b4e37e65c8625f1780c4f226ad9ce79b73deb8
tater <roytate@hotmail.com>
2018-03-07 21:33:40 -0800

    Working series.py with 'cheat code'. Needs revamped

--- students/Roy_Tate/lesson02/series.py
+++ students/Roy_Tate/lesson02/series.py
@@ -7,9 +7,7 @@ def fibonacci(n):
     elif n == 0:
         return 0
     else:
-        return fibonacci(n-1) + fibonacci(n-2)  # formula from example: fib(n) = fib(n-2) + fib(n-1)
-
-print(fibonacci(7))
+        return fibonacci(n-1) + fibonacci(n-2) # formula from example: fib(n) = fib(n-2) + fib(n-1)
 
 
 def lucas(n):
@@ -21,9 +19,28 @@ def lucas(n):
     else:
         return lucas(n-1) + lucas(n-2)
 
-print(lucas(6))
 
 
+def sum_series(x, y=0, z=1):
+    '''Return the nth value in the fibonacci or lucas series, depending on input values'''
+    if y == 2 and z == 1: ## This needs revamped (instructions clearly said not to do this)
+        return lucas(x)
+    else:
+        return fibonacci(x)
+
+
+if __name__ == "__main__":
+    '''Call each function above individually. Also includes assert statements to test for expected results'''
+    x = 5
+
+    print(fibonacci(x))
+    print(lucas(x))
+    print(sum_series(x)) # Call function with only required parameter >> Expect return fibonacci
+    print(sum_series(x, 2, 1)) # Provide optional variables >> Expect return lucas
+
+    assert fibonacci(x) == sum_series(x)  # based on the assignment guidelines, this should return nothing
+    assert lucas(x) == sum_series(x, 2, 1) # based on the assignment guidelines, this should return nothing
+    assert fibonacci(x) == lucas(x)       # This test should always fail, unless x == 1
 
 
 
