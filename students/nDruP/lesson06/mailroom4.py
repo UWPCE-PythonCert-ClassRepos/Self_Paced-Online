794f3bf9e858f7eec0727997a50e2984c36fe42c
Andrew Park <apark46.work@gmail.com>
2018-03-08 22:27:49 -0800

    Lesson06|mailroom4.py Refactored some more code so some parts of different functions are now helper functions that I can test. Gonna try to write tests in test_mailroom4.py to try writing tests and seeing if there's any bits I've missed/can test.

--- students/nDruP/lesson06/mailroom4.py
+++ students/nDruP/lesson06/mailroom4.py
@@ -21,35 +21,30 @@ donor_dict = defaultdict(list,
                           "Mike Truk": [70935.30, 12546.70, 312.00],
                           "Bobson Dugnutt": [1234.56, 789.00],
                           "Todd Bonzalez": [715867.83, 10352.07]})
-exit_reminder = "Return to the main menu at any time by entering 'exit'"
+exit_reminder = "Return to the main menu by entering 'exit'"
+divider = "\n" + "*" * 50 + "\n"
 
-
-def main_menu():
+def main_menu(user_prompt = None):
     """
     Prompt user to send a Thank You, Create a report, create letters, or quit.
     """
-    user_prompt = None
     valid_prompts = {"1": craft_thank_u,
                      "2": create_report,
                      "3": create_letters,
                      "4": sys.exit}
     options = list(valid_prompts.keys())
-    while True:
-        print_divider()
-        print("We're a Pyramid Scheme & So Are You! E-Mailroom")
-        print_divider()
-        while user_prompt not in valid_prompts:
-            options_str = ("{}" + (", {}") * (len(options)-1)).format(*options)
-            print(f"Please choose from the following options ({options_str}):")
-            print("1. Send a Thank you")
-            print("2. Create Donor Report")
-            print("3. Send letters to everyone")
-            print("4. Quit")
-            user_prompt = input(">")
-        print_divider()
-        valid_prompts.get(user_prompt)()
-        input("\nPress enter to continue...")
-        user_prompt = None
+    print(divider + "We're a Pyramid Scheme & So Are You! E-Mailroom"
+          + divider)
+    while user_prompt not in valid_prompts:
+        options_str = ("{}" + (", {}") * (len(options)-1)).format(*options)
+        print(f"Please choose from the following options ({options_str}):")
+        print("1. Send a Thank you")
+        print("2. Create Donor Report")
+        print("3. Send letters to everyone")
+        print("4. Quit")
+        user_prompt = input(">")
+        print(divider)
+    return valid_prompts.get(user_prompt)
 
 
 def craft_thank_u():
@@ -61,32 +56,33 @@ def craft_thank_u():
     Compose and print an email thanking the donor for their donation.
     Return to main
     """
-    donor_name = "list"
-    donation_amt = None
-    message = ''
     print("Let's craft a very personal thank you note for our donor!")
-    print(exit_reminder)
 
-    print("Pull up a list of donor names by entering 'list'")
     while donor_name == "list":
-        donor_name = input("Enter Donor Name >")
+        print(divider)
+        print("Enter Donor Name:")
+        print("Pull up a list of donor names by entering 'list'")
+        donor_name = user_input()
+        if not donor_name:
+            return
         if donor_name.lower() == "list":
-            print(("{}\n"*len(donor_dict)).format(*donor_dict))
-        check_exit(donor_name)
+            print(divider)
+            print(list_of_keys())
 
     while True:
-        donation_amt = input("Enter their Donation Amount >")
-        check_exit(donation_amt)
+        print("\nEnter Donation Amount:")
+        donation_amt = user_input()
         try:
             donation_amt = round(float(donation_amt), 2)
             break
         except ValueError:
             print("Invalid input")
+        except TypeError:
+            return
 
-    donor_dict[donor_name].append(donation_amt)
-    print_divider()
+    add_new_donation(donor_name, donation_amt)
+    
     print(thank_u_str(donor_name, donation_amt))
-    print_divider()
 
 
 def create_report():
@@ -95,45 +91,60 @@ def create_report():
     Donor Name, Total Given, Num Gifts, Average Gift
     """
     sort_donors = sorted(donor_dict.items(), key=sum_2tuple_by2, reverse=True)
-    col_name_list = ["Donor Name", "Total Given", "Gifts", "Avg Donation"]
-    billi_len = 12
-    donor_col_len = max([len(name) for name, _ in sort_donors])
-    donor_col = "{:<" + f"{donor_col_len}" + "}"
-    money_col = "{:>" + f"{billi_len}" + "}"
 
-    header_row = donor_col + "\t" + money_col + "\t{:}" + "\t" + money_col
-    print(header_row.format(*col_name_list))
-    money_col = money_col[:len(money_col)-1] + ".2f}"
+    col_name_list = ["Donor Name", "Total Given", "Gifts", "Avg Donation"]
+
+    sum_col_len = max(max([len(str(round(sum(donations), 2)))
+                   for _, donations in sort_donors]), len(col_name_list[1]))
+    avg_col_len = max(max([len(str(round(sum(donations)/len(donations),2)))
+                   for _, donations in sort_donors]), len(col_name_list[3]))
+    donor_col_len = max([len(name) for name, _ in sort_donors])
+
+    donor_col = "{:<" + f"{donor_col_len}" + "}"
+    header_sum_col = "{:>" + f"{sum_col_len}" + "}"
+    header_avg_col = "{:>" + f"{avg_col_len}" + "}"
+    row = (donor_col + "\t" + header_sum_col + "\t{:}" + "\t" + header_avg_col)
+
+    report = row.format(*col_name_list)
+
+    sum_col = header_sum_col[:len(header_sum_col)-1] + ".2f}"
+    avg_col = header_avg_col[:len(header_avg_col)-1] + ".2f}"
+    row = (donor_col + "\t$" + sum_col + "\t{:}" + "\t$" + avg_col)
 
     for name, donations in sort_donors:
-        row = donor_col.format(name)
-        row += "\t$ " + money_col.format(sum(donations))
-        row += f"\t {len(donations):>}"
-        row += "\t$ " + money_col.format(sum(donations)/len(donations))
-        print(row)
+        hist_amt = sum(donations)
+        num_gifts = len(donations)
+        row_data = [name, hist_amt, num_gifts, hist_amt/num_gifts]
+        report += "\n" + row.format(*row_data)
+    print(report)
 
 
-def create_letters():
+def create_letters(write_dir = ""):
     """
     Write a full set of letters to each donor to individual files on disk.
     Go through all donors in donor_dict, generate a thank you letter,
     write to disk as a text file.
     """
-    curdate = (datetime.datetime.now()).strftime("%Y_%m_%d")
-    print("Enter an existing directory to place the letters.")
-    print("Current working directory will be used if input is invalid")
-    print(exit_reminder)
-    write_dir = input(">")
-    check_exit(write_dir)
-    if not os.path.isdir(write_dir):
-        write_dir = os.getcwd()
+    while not os.path.isdir(write_dir):
+        print("Enter an existing directory to place the letters.")
+        print("Press Enter or input 'cwd' to use Current Working Directory")
+        write_dir = user_input()
+        if not write_dir:
+            return
+        if write_dir == '' or write_dir.lower() == "cwd":
+            write_dir = os.getcwd()
     for donor, donations in donor_dict.items():
-        file_name = donor.replace(' ', '_') + "_" + curdate + ".txt"
-        file_path = os.path.join(write_dir, file_name)
-        with open(file_path, 'w+') as letter:
-            letter.write(thank_u_str(donor, sum(donations)))
-        print("Wrote to " + file_path)
+        print(write_letter_to_dir(donor, donations, write_dir))
     print("Finished writing the letters")
+
+
+def write_letter_to_dir(name, gifts, letter_dir=os.getcwd()):
+    curdate = (datetime.datetime.now()).strftime("%Y_%m_%d")
+    file_name = name.replace(' ', '_') + "_" + curdate + ".txt"
+    file_path = os.path.join(letter_dir, file_name)
+    with open(file_path, 'w+') as letter:
+        letter.write(thank_u_str(letter_name, sum(gifts)))
+    return "Wrote to " + file_path
 
 
 def sum_2tuple_by2(idx_set):
@@ -144,30 +155,33 @@ def sum_2tuple_by2(idx_set):
     return sum(idx_set[1])
 
 
-def print_divider():
-    """
-    Prints a divider so user has better idea of when they enter a new screen.
-    """
-    print("\n"+"*"*50+"\n")
+def add_new_donation(add_name, add_donation):
+    donor_dict[add_name].append(add_donation)
 
 
-def check_exit(user_input):
-    """
-    Check if user_input is exit. If true, brings them back to main menu.
-    """
-    if user_input.lower() == "exit":
-        main_menu()
-
-
-def thank_u_str(thank_name, thank_amt):
-    return (f"Dearest {thank_name},\n"
-            f"\tThank you for donation(s) of ${thank_amt:.2f}!\n"
+def thank_u_str(thank_name, thank_gift):
+    return (divider + f"Dearest {thank_name},\n"
+            f"\tThank you for donation(s) of ${thank_gift:.2f}!\n"
             "We will use your donation(s) to create real living Pokemon.\n"
             "You now have our eternal loyalty. Use it wisely.\n"
             "Sincerely,\n"
-            "*" * 50 + "\n"
             f"We're a Pyramid Scheme & so is {thank_name}"
-            "*" * 50 + "\n")
+            + divider)
 
 
-main_menu()
+def user_input():
+    print(exit_reminder)
+    something = input(">")
+    if something.lower() == "exit":
+        return None
+    return something
+
+
+def list_of_keys(list_dict = donor_dict):
+    return ("{}\n"*len(list_dict)).format(*list_dict)
+
+
+while True:
+    main_menu()()
+    print("\nReturning to main menu..........")
+    input("Press enter to continue...")
