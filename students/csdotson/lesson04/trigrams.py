71d03ac84e2d9b31b9b13c08f9372b766c1c8e3c
csdotson <csdotson1@gmail.com>
2018-03-10 15:30:05 -0800

    Finish logic to print trigram stroy, and fix punctuation

--- students/csdotson/lesson04/trigrams.py
+++ students/csdotson/lesson04/trigrams.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
-# Lesson 4 - Trigrams # Be warned that these files have DOS line endings (carriage return followed by newline)
+# Lesson 4 - Trigrams
 import random
 
-ending_punctuation = [',', '.', ';', '?', '!']
-other_punctuation = []
-punctuation_remove = ['\n', '(', ')']  # text = text.replace('\n', ' ')
 
-input_file_switch = {
-    1: 'wish.txt',
-    2: 'sherlock_small.txt'
-}
+ending_punctuation = [".", "?", "!"]
+other_punctuation = [",", ";", ":"]
+punctuation_remove = ["(", ")", "[", "]", '"', "#", "'"]
+replace_with_space = ["\n", "--"]
 
-input_file = input_file_switch[1]
+
+input_file = input("Please enter the name of a file (with extension):\n")
+
 
 with open(input_file, 'r') as f:
     text = f.read()
     for entry in punctuation_remove:
         text = text.replace(entry, '')
-    for entry in ending_punctuation or other_punctuation:
-        text = text.replace(entry, f' {entry}')
-    text = text.split(' ')
+    for entry in replace_with_space:
+        text = text.replace(entry, ' ')
+    for entry in other_punctuation:
+        text = text.replace(entry, f" {entry}")
+    for entry in ending_punctuation:
+        text = text.replace(entry, f" {entry}")
+    text = text.split()
 
 
 text_hash = {}
@@ -33,17 +36,30 @@ for index in range(len(text)-2):
     text_hash[' '.join(pattern)].append(text[index+2]) # {'I wish': ['I', 'I']..
 
 
-def create_trigram():
-    sentence = 'wish I'
-    count = 0
-    while True:
-        next_key = ' '.join(sentence.split(' ')[-2:])
-        if text_hash[next_key] == [] or count > 100:
-            break
-        else:
-            sentence += ' '
-            sentence += text_hash[next_key][random.randint(0,len(text_hash[next_key])-1)]
-            count += 1
-    return(sentence)
 
-# create_trigram() --> 'wish I may I wish I may I wish I might'
+random_starting_point = random.randint(0,len(text)-1)
+story = ' '.join(text[random_starting_point:random_starting_point+2])
+while True:
+    next_key = ' '.join(story.split(' ')[-2:])
+    if text_hash[next_key] == [] or len(story) > 500:
+        story += '.'
+        break
+    else:
+        story += ' '
+        story += text_hash[next_key][random.randint(0,len(text_hash[next_key])-1)]
+
+
+story = story.capitalize()
+for entry in other_punctuation:
+    story = story.replace(f' {entry}', entry)
+for entry in ending_punctuation:
+    story = story.replace(f' {entry}', entry)
+
+
+word_array = story.split()
+for index in range(len(word_array)-1):
+    if '.' in word_array[index]:
+        word_array[index+1] = word_array[index+1].capitalize()
+story = ' '.join(word_array)
+
+print("\nAnd, here's your story:\n\n", story)
