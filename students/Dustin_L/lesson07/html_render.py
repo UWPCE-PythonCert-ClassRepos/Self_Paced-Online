61bdc21bdf6dd9f5658b057d7adce91d10904d7e
Dustin L <r.dustin.lane@gmail.com>
2018-03-11 11:19:56 -0700

    HTML Render - Add initial files

---
+++ students/Dustin_L/lesson07/html_render.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""HTML Render Module
+
+This module contains all of the functions for the HTML Render module
+"""
+
+
+def main():
+    """Main function"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ students/Dustin_L/lesson07/test_html_render.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Unit tests for HTML Render"""
+import unittest
+
+
+class TestHtmlRender(unittest.TestCase):
+    """Test class containing all unit tests for HTML Render"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
