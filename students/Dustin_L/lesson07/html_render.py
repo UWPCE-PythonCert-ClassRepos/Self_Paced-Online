8ce43fd6c6ac3cce5d2f5fc727724967e840a369
Dustin L <r.dustin.lane@gmail.com>
2018-03-11 11:40:52 -0700

    HTML Render - Add Element.append() method

--- students/Dustin_L/lesson07/html_render.py
+++ students/Dustin_L/lesson07/html_render.py
@@ -5,6 +5,23 @@ This module contains all of the functions for the HTML Render module
 """
 
 
+class Element(object):
+    """Element class for rendering an HTML element"""
+    tag = ''
+    indent = '    '
+
+    def __init__(self, content=None):
+        self.content = [content] if content else []
+
+    def append(self, s):
+        """Adds a string to the element content"""
+        self.content.append(s)
+
+    def render(self, file_out, cur_ind=''):
+        """Renders the tag and strings in the element content"""
+        pass
+
+
 def main():
     """Main function"""
     pass
--- students/Dustin_L/lesson07/test_html_render.py
+++ students/Dustin_L/lesson07/test_html_render.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 """Unit tests for HTML Render"""
 import unittest
+import html_render as hr
 
 
-class TestHtmlRender(unittest.TestCase):
-    """Test class containing all unit tests for HTML Render"""
+class TestElement(unittest.TestCase):
+    """Test class containing all unit tests for the Element class"""
     def setUp(self):
-        pass
+        self.element = hr.Element()
 
     def tearDown(self):
         pass
+
+    def test_append(self):
+        self.element.append('Testing the append method...')
+        self.assertTrue(self.element.content == [
+                        'Testing the append method...'],
+                        msg=self.element.content)
 
 
 if __name__ == '__main__':
