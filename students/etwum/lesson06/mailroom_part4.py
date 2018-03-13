10d9b36b887cbb90316a1ac6b260437a37e159a2
etwum <33198751+etwum@users.noreply.github.com>
2018-03-12 18:39:23 -0700

    updated donor list
    updated the donor list into type dictionary
    
    updated other code to read the dictionary

--- students/etwum/lesson06/mailroom_part4.py
+++ students/etwum/lesson06/mailroom_part4.py
@@ -2,15 +2,20 @@
 
 
 # initial list of donors
-lst_donors = [{"donor name": "Lionel Messi", "total donations": 1000000.00, "number donations": 5, "avg donation": 20000.00},
-              {"donor name": "Thierry Henry", "total donations": 500, "number donations":1, "avg donation": 500},
-              {"donor name": "Michael Jordan", "total donations": 45000, "number donations": 3, "avg donation": 15000},
-              {"donor name": "Kobe Bryant", "total donations": 8000, "number donations": 2, "avg donation": 4000}]
+dict_donors = {1: {"donor name": "Lionel Messi", "total donations": 1000000.00, "number donations": 5,
+                   "avg donation": 20000.00},
+              2: {"donor name": "Thierry Henry", "total donations": 500, "number donations":1, "avg donation": 500},
+              3: {"donor name": "Michael Jordan", "total donations": 45000, "number donations": 3,
+                  "avg donation": 15000},
+              4: {"donor name": "Kobe Bryant", "total donations": 8000, "number donations": 2, "avg donation": 4000}}
 
 
 def send_thank_you():
     # used to add new donors
-    new_donor = []
+    new_donor = {}
+
+    # donor number
+    donor_number = len(dict_donors) + 1
 
     # function creates a thank you email to current and new donors added to the list
     # add new donors and donations
@@ -20,7 +25,7 @@ def send_thank_you():
     if view_donors == 'list':
         print()
         print("Below are the list of donors:")
-        for x in lst_donors:
+        for x in dict_donors.values():
             print(x["donor name"])
         print('----------------------------')
 
@@ -44,7 +49,7 @@ def send_thank_you():
     print()
 
     # prints an email if the donors name is currently in the list
-    for x in lst_donors:
+    for x in dict_donors.values():
         if x["donor name"] == donor_full_name:
 
             while True:
@@ -93,11 +98,12 @@ def send_thank_you():
                 print("Please input a valid number")
             else:
                 break
-        new_donor.append({"donor name": donor_full_name, "total donations": new_donation_amount, "number donations": 1,
-                          "avg donation": new_donation_amount})
-        lst_donors.extend(new_donor)
+        len(dict_donors)
+        new_donor = {donor_number: {"donor name": donor_full_name, "total donations": new_donation_amount,
+                                    "number donations": 1, "avg donation": new_donation_amount}}
+        dict_donors.update(new_donor)
         print('-----------------------------------------------------')
-        for x in lst_donors:
+        for x in dict_donors.values():
             if x["donor name"] == donor_full_name:
                 email = "Dear {donor_name},\n\nThank you for your generous donations of ${donations:.2f} " \
                         "to our charity.\n".format(donor_name=x["donor name"],donations=x["total donations"])
@@ -105,29 +111,23 @@ def send_thank_you():
     print('-----------------------------------------------------')
 
 
-def sort_list(lst_donors):
-    # used to sort the donor list by the total donations column
-
-    return lst_donors["total donations"]
-
-
 def create_report():
     # creates a report of the the donors
     # headers used in table
     print()
-    lst_headers = [["Donor Name", "| Total Donation(s)", "| # of Donations", "| Avg Donation"]]
 
+    lst_headers = [["Donor Name", "| Total Donation(s)", "| # of Donations", "| Avg Donation"]]
     for x in lst_headers:
         print('{:<25}{:<20}{:<17}{:<15}'.format(*x))
     print("----------------------------------------------------------------------------")
-    for x in sorted(lst_donors,key=sort_list, reverse= True):
-        print('{:<25} $ {:<20}{:^14} $ {:<15}'.format(*x.values()))
+    for x,y in sorted(dict_donors.items(),key=lambda x: x[1]['total donations'], reverse= True):
+        print('{:<25} $ {:<20}{:^14} $ {:<15}'.format(*y.values()))
 
 
 def send_letter_all():
     print('\n----------------------------------------------')
     # creates a letter to every donor by writing them to separate text files
-    for x in lst_donors:
+    for x in dict_donors.values():
 
         # opens or creates a new text file for writing based on the donor name
         with open('{file_name}.txt'.format(file_name=x['donor name']), 'w') as f1:
