9495fe31cabe87402f6117d6998527725b964ca2
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-14 08:46:37 -0700

    Task two complete

--- students/stuartkershaw/lesson03/strformat_lab.py
+++ students/stuartkershaw/lesson03/strformat_lab.py
@@ -13,3 +13,14 @@ out_msg = '{}: {}, {}, {}'.format(file_out, float_dec_out, sci_out, sci_sig_out)
 
 print(out_msg)
 
+#Task Two
+
+file_out_alt = f"file_{seq_in[0]:03}"
+float_dec_out_alt = f"{seq_in[1]:.2f}"
+sci_out_alt = f"{seq_in[2]:.2e}"
+sci_sig_out_alt = f"{seq_in[3]:.2e}"
+
+out_msg_alt = f"{file_out_alt}: {float_dec_out_alt}, {sci_out_alt}, {sci_sig_out_alt}"
+
+print(out_msg_alt)
+
