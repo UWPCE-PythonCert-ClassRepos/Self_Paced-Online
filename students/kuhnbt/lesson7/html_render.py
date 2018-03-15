24a48edf14032b354e72908e7e6ae0a9920ec5a8
Ben Kuhn <kuhnbt@gmail.com>
2018-03-14 20:50:05 -0400

    more updates to html renderer

--- students/kuhnbt/lesson7/html_render.py
+++ students/kuhnbt/lesson7/html_render.py
@@ -4,13 +4,13 @@ class Element:
     tag_name = ''
     indentation = 4
 
-    def __init__(self, content = None, **kwargs):
+    def __init__(self, content = None, keywords = None):
         if content:
             self.content = [content]
         else:
             self.content = []
         self.text = None
-        self.keywords = kwargs
+        self.keywords = keywords
 
     def append(self, new_content):
         self.content.append(new_content)
@@ -20,7 +20,7 @@ class Element:
         if self.keywords:
             for tag, value in self.keywords.items():
                 self.text += ' {}="{}"'.format(tag, value)
-        + '>\n'
+        self.text += '>\n'
         for element in self.content:
             self.text += (cur_ind + self.indentation) * ' ' 
             if hasattr(element, 'render'):
@@ -52,7 +52,7 @@ class Head(Element):
 class OneLineTag(Element):
     def render(self, file_out, cur_ind=4):
         self.text = (cur_ind + self.indentation) * ' '
-        self.text = '<' + self.tag_name + '>'
+        self.text += '<' + self.tag_name + '>'
         for element in self.content:
             if hasattr(element, 'render'):
                 f = StringIO()
@@ -65,3 +65,32 @@ class OneLineTag(Element):
 
 class Title(OneLineTag):
     tag_name = 'title'
+
+class SelfClosingTag(Element):
+    def __init__(self, content = None, keywords = None):
+        Element.__init__(self, content, keywords)
+        if self.content:
+            raise TypeError('Self closing tags cannot contain content')
+    def render(self, file_out, cur_ind=4):
+        self.text = (cur_ind + self.indentation) * ' '
+        self.text += '<' 
+        if self.keywords:
+            for tag, value in self.keywords.items():
+                self.text += ' {}="{}"'.format(tag, value)
+        self.text += ' ' + self.tag_name + '/ >'
+
+class A(Element):
+    tag_name = 'a'
+    def __init__(self, link, content):
+        Element.__init__(self, content = content, keywords = {'href':link})
+
+class Ul(SelfClosingTag):
+    tag_name = 'ul'
+
+class Li(OneLineTag):
+    tag_name = 'li'
+
+class Header(OneLineTag):
+    def__init__(self, level, content):
+        self.tag_name = 'h' + str(level)
+        OneLineTag.init(self, content)
