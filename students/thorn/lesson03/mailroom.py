30351252dac18344a14413cb45f0523bfaa8937b
Thomas Horn <hortw1991@gmail.com>
2018-03-16 14:18:02 -0500

    Completed working mailroom.py.

--- students/thorn/lesson03/mailroom.py
+++ students/thorn/lesson03/mailroom.py
@@ -8,11 +8,11 @@ Tracks donor information and automates the sending of thank you notes.
 
 # Global that holds donor history and the amounts donated.
 DONOR_HISTORY = [
-    ['Tom Horn',        599.23, 1000.00],
-    ['Theo Hartwell',   0.01, 0.01, 0.1],
-    ['Bailey Kimmitt',  8723.22, 27167.22, 91817.66],
-    ['Paul Hubbell',    90012.32, 2312.24],
-    ['David Beckham',   1817266.11, 123123.66, 111335.112]
+    ['Tom Horn',        [599.23, 1000.00]],
+    ['Theo Hartwell',   [0.01, 0.01, 0.1]],
+    ['Bailey Kimmitt',  [8723.22, 27167.22, 91817.66]],
+    ['Paul Hubbell',    [90012.32, 2312.24]],
+    ['David Beckham',   [1817266.11, 123123.66, 111335.112]]
 ]
 
 def send_thanks():
@@ -35,31 +35,45 @@ def send_thanks():
     if name_request.lower() == "return":
         return
 
-    # List request.
+    # List request - only donors returned.
     if name_request.lower() == 'list':
         for donors in DONOR_HISTORY:
             print(donors[0])
+        return
 
     name_request = name_request.title()
-    donation_amount = float("Please enter a donation amount for {}".format(name_request))
+    donation_amount = float(input(("Please enter a donation amount for {}: ".format(name_request.title()))))
 
-    # Add to existing donor.
+    # Add to existing donor.  i is the donor location in the nested list.
+    for i, donors in enumerate(DONOR_HISTORY):
+        if donors[0].title() == name_request.title():
+            DONOR_HISTORY[i][1].append(donation_amount)
+            # Break to stop it from creating i numbers of the person.
+            break
+        # Create new donor.
+        else:
+            DONOR_HISTORY.append([name_request.title(), [donation_amount]])
+            break
 
-    # Create new donor and add amount.
-
-    # Print thank you note.
-        
-        
-        
+    print(f"Thank you {name_request.title()} for your donation of ${donation_amount:.2f}.")
+    
 
 def create_report():
     """
     Prints a list of donors sorted by total historical donation amount.
     - Includes donor name, total donated, number of donations, average donation
       amount.
-    - Returns to original prompt or quit to prompt
+    - $ Total - 27 spaces in, right justified
     """
-    pass
+    print("Donor:                    |    $ Total     |   Donations   |   $ Average   |")
+    print("-"*76)    
+    for item in DONOR_HISTORY:
+        amt_total = float(sum(item[1]))
+        num_total = int(len(item[1]))
+        # Thousand separator as default. Careful with the space if we get some big donors.
+        print("{:<26}|${:>15,.2f}|{:>15}|{:>15,.2f}".format(item[0], amt_total, num_total, amt_total/num_total))
+    return
+
 
 if __name__ == "__main__":
     # Select from options.  No checks to sanatize input.
