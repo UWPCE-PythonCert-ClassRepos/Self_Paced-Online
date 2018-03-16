8889a6535354cad5d8f80e9b7e2582c943a68ecd
Natalie Rodriguez <natrod8@outlook.com>
2018-03-16 12:52:58 -0500

    list lab with all 4 series complete

--- students/NatalieRodriguez/Lesson03/list_lab.py
+++ students/NatalieRodriguez/Lesson03/list_lab.py
@@ -5,6 +5,10 @@
 #she-bang line
 #!/usr/bin/env python3
 
+
+#_______________________________________
+#Series 1
+
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
 print(fruits)
@@ -16,7 +20,7 @@ fruits.append(more_fruit2)
 
 print(fruits)
 
-which_fruit = int(input("What number fruit would you like to see?" '\n'))
+which_fruit = int(input("What number fruit from the list would you like to see?" '\n'))
 if 0 <= which_fruit < len(fruits):
     print(fruits[which_fruit -1])
 else:
@@ -35,3 +39,65 @@ for item in fruits_longlist:
     elif item[0] == 'P':
         print(item)
     else: pass
+
+#input("Press enter to exit this fruity list.")
+
+#___________________________________________
+#Series 2
+
+print("Here is your fruit list!", fruits_longlist)
+
+fruits_longlist = fruits_longlist[:-1]
+
+print("Your fruit list, without the last item, is:", fruits_longlist)
+
+delete_fruit = input("Which fruit would you like to remove from the list?"'\n')
+
+if delete_fruit in fruits_longlist:
+    fruits_longlist.remove(delete_fruit)
+    print(delete_fruit, "have been removed from the list."'\n')
+else:
+    print(delete_fruit, "cannot be deleted because it is not in the list."'\n')
+
+print("Your fruit list now has", len(fruits_longlist), "fruits:", fruits_longlist, '\n')
+
+
+#___________________________________________
+#Series 3
+
+yes = []
+no = []
+
+for item in fruits_longlist:
+    if not item in yes and not item in no:
+        prompt = input('Do you like {}? '.format(item.lower()))
+        while not prompt == 'yes' and not prompt == 'no':
+            prompt = input('Do you like {}? (yes/no) '.format(el.lower()))
+        if prompt == 'yes':
+            yes.append(item)
+        else:
+            no.append(item)
+
+print("The fruits you enjoy are:", yes,'\n')
+
+#___________________________________________
+#Series 4
+
+fruit_copy = fruits_longlist[:]
+fruit_reversed = []
+
+for fruit in fruit_copy:
+    fruit_reversed.append(fruit[::-1])
+
+del yes[-1]
+
+print("Your full fruit list:", fruit_copy)
+print("Your fruit list, backwards!", fruit_reversed)
+
+
+
+
+
+
+
+
