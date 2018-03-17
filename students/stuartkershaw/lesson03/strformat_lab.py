f054deb7681037298ceb94958d4f132f33af3fec
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-17 14:12:33 -0700

    Task 4 complete

--- students/stuartkershaw/lesson03/strformat_lab.py
+++ students/stuartkershaw/lesson03/strformat_lab.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Task One
+# Task One
 
 seq_in = (2, 123.4567, 10000, 12345.67)
 
@@ -9,22 +9,25 @@ float_dec_out = format(seq_in[1], '.2f')
 sci_out = format(seq_in[2], '.2e')
 sci_sig_out = format(seq_in[3], '.2e')
 
-out_msg = '{}: {}, {}, {}'.format(file_out, float_dec_out, sci_out, sci_sig_out)
+out_msg = '{}: {}, {}, {}'.format(file_out, float_dec_out, sci_out,
+                                  sci_sig_out)
 
 print(out_msg)
 
-#Task Two
+# Task Two
 
 file_out_alt = f"file_{seq_in[0]:03}"
 float_dec_out_alt = f"{seq_in[1]:.2f}"
 sci_out_alt = f"{seq_in[2]:.2e}"
 sci_sig_out_alt = f"{seq_in[3]:.2e}"
 
-out_msg_alt = f"{file_out_alt}: {float_dec_out_alt}, {sci_out_alt}, {sci_sig_out_alt}"
+out_msg_alt = f"{file_out_alt}: {float_dec_out_alt}, {sci_out_alt}" \
+               "{sci_sig_out_alt}"
 
 print(out_msg_alt)
 
-#Task Three
+# Task Three
+
 
 def formatter(tuple_in):
     t = tuple_in
@@ -34,5 +37,21 @@ def formatter(tuple_in):
     out_msg = 'the {} numbers are: '.format(len(t))
     return out_msg + ', '.join(fstring).format(*t)
 
-print(formatter((2,3,5,7,9)))
 
+print(formatter((2, 3, 5, 7, 9)))
+
+# Task Four
+
+
+def formatter_four(tuple_in):
+    t = tuple_in
+    collect = []
+    collect.append(format(t[3], '02'))
+    collect.append(str(t[4]))
+    collect.append(str(t[2]))
+    collect.append(format(t[0], '02'))
+    collect.append(str(t[1]))
+    return ' '.join(collect)
+
+
+print(formatter_four((4, 30, 2017, 2, 27)))
