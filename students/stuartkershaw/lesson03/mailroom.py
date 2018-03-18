93d380d3e463dba400fdc82eaa8cb8af751adfa5
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-18 13:25:17 -0700

    Thank you complete

--- students/stuartkershaw/lesson03/mailroom.py
+++ students/stuartkershaw/lesson03/mailroom.py
@@ -8,6 +8,14 @@ donors = [
 ]
 
 
+def compose_email(donor_list, index):
+    i = index
+    dl = donor_list
+    message = f"Dear {dl[i]}, thanks so much \
+for your generous donation in the amount of: ${dl[-1][i][-1]}."
+    print(message)
+
+
 def get_selection():
     options = 'Please select a, b, or c:\n\
 a) send a thank you\nb) create a report\nc) quit\n'
@@ -21,28 +29,44 @@ a) send a thank you\nb) create a report\nc) quit\n'
             print(options)
 
 
-def send_thank_you():
+def set_donation(donor_list, index):
+    i = index
+    dl = donor_list
+    while True:
+        try:
+            donation = int(input('Please enter a donation amount: '))
+            while donation < 1:
+                donation = int(input('Please provide a whole \
+number greater than zero: '))
+            dl[-1][i].append(donation)
+        except ValueError:
+            print('Please provide a whole number greater than zero.')
+            set_donation(dl, i)
+        break
+    compose_email(dl, i)
+
+
+def get_donor_name():
     donor_names = donors[:-1]
     instruction = 'Please enter a full name or type \'list\' to see donors:\n'
     name_input = input(instruction)
     if name_input == 'list':
         for donor in donor_names:
             print(donor)
-        send_thank_you()
+        get_donor_name()
     elif name_input in donor_names:
-        index = donor_names.index(name_input)
-        print(index)
+        set_donation(donors, donors.index(name_input))
     else:
         donors[-1].append([])
         donors.insert(len(donors)-1, name_input)
-    print(donors)
+        set_donation(donors, donors.index(name_input))
 
 
 def main():
     selection = get_selection()
     if selection == 'a':
-        send_thank_you()
-    if selection == 'c':
+        get_donor_name()
+    else:
         quit()
 
 
