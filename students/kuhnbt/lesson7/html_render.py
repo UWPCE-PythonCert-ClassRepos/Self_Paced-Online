a762054e176f8faa5488a64b87b91755db518e4a
Ben Kuhn <kuhnbt@gmail.com>
2018-03-18 11:00:33 -0400

    more updates to html renderer

--- students/kuhnbt/lesson7/html_render.py
+++ students/kuhnbt/lesson7/html_render.py
@@ -15,26 +15,30 @@ class Element:
     def append(self, new_content):
         self.content.append(new_content)
 
-    def render(self, file_out, cur_ind=4):
-        self.text = '<' + self.tag_name 
+    def render(self, file_out, cur_ind=0):
+        self.text = cur_ind * ' ' + '<' + self.tag_name 
         if self.keywords:
             for tag, value in self.keywords.items():
                 self.text += ' {}="{}"'.format(tag, value)
         self.text += '>\n'
         for element in self.content:
-            self.text += (cur_ind + self.indentation) * ' ' 
             if hasattr(element, 'render'):
                 f = StringIO()
-                element.render(f, cur_ind + element.indentation)
+                element.render(f, cur_ind + self.indentation)
                 self.text += f.getvalue() + '\n'
             else:
-                self.text += element + '\n'
+                self.text += (self.indentation + cur_ind) * ' ' + element + '\n'
         self.text += (cur_ind) * ' ' 
         self.text += '</' + self.tag_name + '>'
         file_out.write(self.text)
 
 class Html(Element):
     tag_name = 'html'
+
+    def render(self, file_out, cur_indent=0):
+        self.text = '<!DOCTYPE html>\n'
+        file_out.write(self.text)
+        Element.render(self, file_out, 0)
 
 
 class Body(Element):
@@ -47,12 +51,18 @@ class P(Element):
 
 class Head(Element):
     tag_name = 'head'
-
+    def __init__(self, content, keywords):
+        self.content = [Charset()]
+        Element.__init__(self, self.content)
 
 class OneLineTag(Element):
-    def render(self, file_out, cur_ind=4):
-        self.text = (cur_ind + self.indentation) * ' '
-        self.text += '<' + self.tag_name + '>'
+    def render(self, file_out, cur_ind=0):
+        self.text = cur_ind * ' '
+        self.text += '<' + self.tag_name
+        if self.keywords:
+            for tag, value in self.keywords.items():
+                self.text += ' {}="{}"'.format(tag, value)
+        self.text += '>'
         for element in self.content:
             if hasattr(element, 'render'):
                 f = StringIO()
@@ -71,26 +81,36 @@ class SelfClosingTag(Element):
         Element.__init__(self, content, keywords)
         if self.content:
             raise TypeError('Self closing tags cannot contain content')
-    def render(self, file_out, cur_ind=4):
+    def render(self, file_out, cur_ind=0):
         self.text = (cur_ind + self.indentation) * ' '
-        self.text += '<' 
+        self.text += '<' + self.tag_name
         if self.keywords:
             for tag, value in self.keywords.items():
                 self.text += ' {}="{}"'.format(tag, value)
-        self.text += ' ' + self.tag_name + '/ >'
+        self.text += ' />'
+        file_out.write(self.text)
 
-class A(Element):
+class A(OneLineTag):
     tag_name = 'a'
     def __init__(self, link, content):
-        Element.__init__(self, content = content, keywords = {'href':link})
+        OneLineTag.__init__(self, content = content, keywords = {'href':link})
 
-class Ul(SelfClosingTag):
+class Ul(Element):
     tag_name = 'ul'
 
 class Li(OneLineTag):
     tag_name = 'li'
 
 class Header(OneLineTag):
-    def__init__(self, level, content):
+    def __init__(self, level, content):
         self.tag_name = 'h' + str(level)
-        OneLineTag.init(self, content)
+        OneLineTag.__init__(self, content)
+
+class Charset(SelfClosingTag):
+    def __init__(self, content = None, keywords = {'charset':'UTF-8'}):
+        self.tag_name = 'meta'
+        self.keywords = keywords
+        self.indentation = 0
+        SelfClosingTag.__init__(self, content, keywords)
+
+
