0a1fb97252260533cd3b37ca8cd4dd6eebf604b6
Ben Kuhn <kuhnbt@gmail.com>
2018-03-19 19:45:41 -0400

    Changed html_render and tests

--- students/kuhnbt/lesson7/html_render.py
+++ students/kuhnbt/lesson7/html_render.py
@@ -1,6 +1,16 @@
 from io import StringIO
 
 class Element:
+    """Base class for Html elements
+
+    Attributes:
+        tag_name(str): type of Html tag
+        indentation(int): number of spaces the content of a tag should
+            be indented
+        content(list containing str and Element objects): content of
+            the tag 
+        keywords(dict): pairs of attributes/values for the tag
+    """                    
     tag_name = ''
     indentation = 4
 
@@ -13,24 +23,31 @@ class Element:
         self.keywords = keywords
 
     def append(self, new_content):
+        """Add content (str or Element) to an element"""
         self.content.append(new_content)
 
     def render(self, file_out, cur_ind=0):
         self.text = cur_ind * ' ' + '<' + self.tag_name 
-        if self.keywords:
-            for tag, value in self.keywords.items():
-                self.text += ' {}="{}"'.format(tag, value)
+        self.add_keywords()
         self.text += '>\n'
         for element in self.content:
-            if hasattr(element, 'render'):
-                f = StringIO()
-                element.render(f, cur_ind + self.indentation)
-                self.text += f.getvalue() + '\n'
-            else:
-                self.text += (self.indentation + cur_ind) * ' ' + element + '\n'
+            self.get_text(element, cur_ind)
         self.text += (cur_ind) * ' ' 
         self.text += '</' + self.tag_name + '>'
         file_out.write(self.text)
+
+    def get_text(self, element, cur_ind):
+        if hasattr(element, 'render'):
+            f = StringIO()
+            element.render(f, cur_ind + self.indentation)
+            self.text += f.getvalue() + '\n'
+        else:
+            self.text += (self.indentation + cur_ind) * ' ' + element + '\n'
+    
+    def add_keywords(self):
+        if self.keywords:
+            for tag, value in self.keywords.items():
+                self.text += ' {}="{}"'.format(tag, value)
 
 class Html(Element):
     tag_name = 'html'
@@ -51,17 +68,15 @@ class P(Element):
 
 class Head(Element):
     tag_name = 'head'
-    def __init__(self, content, keywords):
-        self.content = [Charset()]
+    def __init__(self, content = None, keywords = None):
+        self.content = Charset()
         Element.__init__(self, self.content)
 
 class OneLineTag(Element):
     def render(self, file_out, cur_ind=0):
         self.text = cur_ind * ' '
         self.text += '<' + self.tag_name
-        if self.keywords:
-            for tag, value in self.keywords.items():
-                self.text += ' {}="{}"'.format(tag, value)
+        self.add_keywords()
         self.text += '>'
         for element in self.content:
             if hasattr(element, 'render'):
@@ -84,9 +99,7 @@ class SelfClosingTag(Element):
     def render(self, file_out, cur_ind=0):
         self.text = (cur_ind + self.indentation) * ' '
         self.text += '<' + self.tag_name
-        if self.keywords:
-            for tag, value in self.keywords.items():
-                self.text += ' {}="{}"'.format(tag, value)
+        self.add_keywords()        
         self.text += ' />'
         file_out.write(self.text)
 
