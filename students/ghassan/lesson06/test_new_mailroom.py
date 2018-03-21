4d5db181600c67df145e2a9f90e8837925451e7b
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-03-21 15:39:43 -0700

    lesson06 - fixed code to give chance for more tests

--- students/ghassan/lesson06/test_new_mailroom.py
+++ students/ghassan/lesson06/test_new_mailroom.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-import unittest
-from new_mailroom import main_menu, send_thankyou, donors
+import os
+from new_mailroom import add_donor, donors, thank_you_msg, save_report
 
 
-class TestMailroom(unittest.TestCase):
-    def test_main_menu(self):
-        ''' this tests if the function is actually
-        returning the user's input '''
-        users_choice = main_menu()
-        self.assertEqual(users_choice, users_choice)
+def test_add_donor():
+    add_donor('User1', 300)
+    assert 'User1' in donors.keys()
+    assert 300 in donors['User1']
 
-    def test_send_thankyou_new_donor(self):
-        ''' tests whether a new donor was actually added
-        to the donors list'''
-        # I did a list comprehention here to get
-        # a fresh copy of the dictionary keys as of
-        # a reference or a pointer to the same dictionary
-        old_donors = [donor for donor in donors.keys()]
-        send_thankyou()
-        self.assertGreater([
-            new_donor for new_donor in donors.keys()],
-            old_donors)
 
+def test_thank_you_msg():
+    msg = thank_you_msg('Ford')
+    assert msg == 'Thank you Ford for your generous donation of 3400'
+
+
+def test_save_report():
+    save_report()
+    os.chdir('/Users/galmamar/git_repo/Self_Paced-Online/students/ghassan/lesson06')  # noqa: E501
+    assert os.path.isfile('Jessie.txt')
+    assert os.path.isfile('James.txt')
+    assert os.path.isfile('Ford.txt')
