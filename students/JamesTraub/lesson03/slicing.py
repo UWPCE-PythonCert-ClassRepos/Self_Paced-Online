1680169e6dc08875574b44725f6113c487402466
James Traub <traub01-devtrain@outlook.com>
2018-03-21 00:50:36 -0700

    Completed sequence functions 1 to 4

--- students/JamesTraub/lesson03/slicing.py
+++ students/JamesTraub/lesson03/slicing.py
@@ -6,42 +6,50 @@
 # with the elements reversed (just with slicing).
 # with the middle third, then last third, then the first third in the new order.
 
-seq = 10,20,30,40,50,60,70,80,90
+tuple_seq = (10,20,30,40,50,60,70,80,90,100,110,120,130,140,150)
+string_seq = "Larger than life Jack Pepsi."
 
-def exchange_first_last(seq):
-    new_0 = seq[-1:]
-    new_last_item = seq[0:1]
-    middle_seq = seq[1:-1]
-    a_new_seq1 = new_0 + middle_seq + new_last_item
-    return a_new_seq1
-
-
-exchange_first_last(seq)
-
+# def exchange_first_last(string_seq):
+#     new_0 = string_seq[-1:]
+#     new_last_item = string_seq[0:1]
+#     middle_seq = string_seq[1:-1]
+#     a_new_seq1 = new_0 + middle_seq + new_last_item
+#     print(a_new_seq1)
+#     return a_new_seq1
 
 
-#def remove_every_other(seq):
-
- #   return a_new_seq2
+# exchange_first_last(string_seq)
 
 
 
+# def remove_every_other(tuple_seq):
+#     tuple_seq = tuple_seq[1::2]
+#     print(tuple_seq)
+#     #return a_new_seq2
 
 
-#def remove_firstLast4_everyOther(seq):
-
- #   return a_new_seq3
+# remove_every_other(tuple_seq)
 
 
+def remove_firstLast4_everyOther(string_seq):
+    first_last_removed = string_seq[4:-4]
+    print(first_last_removed)
+    a_new_seq3 = first_last_removed[1::2]
+    print(a_new_seq3)
+    return a_new_seq3
+
+
+remove_firstLast4_everyOther(string_seq)
 
 
 
-#def reversed_elements(seq):
+# def reversed_elements(string_seq):
+#     a_new_seq4 = string_seq[::-1]
+#     print(a_new_seq4)
+#     return a_new_seq4
 
-#    return a_new_seq4
 
-
-
+# reversed_elements(string_seq)
 
 
 #def middle_last_first_third(seq):
