3c6d3d5b2ca605632925cadba0adf8ba3f69ba68
tater <roytate@hotmail.com>
2018-03-21 23:48:56 -0700

    adding new user to dict is crashing reports. all else working

--- students/Roy_Tate/lesson04/mailroom2.py
+++ students/Roy_Tate/lesson04/mailroom2.py
@@ -1,49 +1,20 @@
 # author/student: Roy Tate (githubtater)
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import sys
-
-donor_dict = [
-    {'name':'Bill', 'donations': [400.254, 100, 200.232, 300]},
-    {'name':'Jeff', 'donations': [250, 450.355]},
-    {'name':'Bud', 'donations': [1000, 300000.23, 2000]},
-    {'name':'Female CEO 1', 'donations': [1030, 20030.171, 30, 30]},
-    {'name':'Female CEO 2', 'donations': [1030, 20030.355, 30, 30]},
-]
+import os
+import os.path
 
 
+# Create the donor_dict
+donor_dict = {}
+donor_dict['Bill'] = [1100, 2100, 3100]
+donor_dict['Mark'] = [1000, 300000.23, 2000]
+donor_dict['Jeff'] = [50, 450.355]
+donor_dict['Roy'] = [4500, 7500, 11221, 30232, 323]
 
-# Extract the names of donors from the list
-def donor_names():
-    names = []
-    for donor in donor_dict:
-        names.append(donor['name'])
-    return names
-
-
-# Prints the list of donor names preceded with a formatted line
-def print_list():
-    print('{:*^50}'.format('DONOR LIST'))
-    for donor in donor_names():
-        print(donor)
-
-
-#
-def add_donation(donor_name, new_donation):
-    for donor in donor_names():
-        if donor['name'] == donor_name:
-            donor['donations'].append(new_donation)
-
-
-def print_email(email_str, donor_name, donation_amount):
-    print(email_str.format(donor_name.title(), float(donation_amount)))
-    print('done')
-
-def send_thank_you():
-    # Obtain the name of the donor, the contribution amount, add the donation to the list, and generate
-    # a thank you email.
-    email_str = '''\n
+email_str = '''\n
         From:     A Worthwhile Cause
         To:       {0}
         Subject:  Thank you
@@ -57,6 +28,41 @@ def send_thank_you():
 
         The Boss
         '''
+
+
+# Prints the list of keys from donor_dict (in this case, the donor names)
+def print_list():
+    print('{:*^50}'.format('DONOR LIST'))
+    for donor in donor_dict.keys():
+        print(donor)
+
+
+# Helper function to add donations to appropriate user account
+def add_donation(name, new_donation):
+    if name in donor_dict.keys():
+        donor_dict[name].append(new_donation)
+    else:
+        donor_dict[name] = new_donation
+
+
+# Send formatted email to donor
+def print_email(email_str, donor_name, donation_amount):
+    print(email_str.format(donor_name.title(), float(donation_amount)))
+    print('Email sent to: ' + donor_name)
+
+
+def email_all():
+    print('Generating emails for all donors...')
+    for donor, donations in donor_dict.items():
+        filename = donor + '.txt'
+        with open(filename, 'w') as f:
+            f.write(email_str.format(donor.title(), sum(donations)))
+            print('[SUCCESS] Output file -->  ' + str(os.path.abspath(filename)))
+
+def send_thank_you():
+    # Obtain the name of the donor, the contribution amount, add the donation to the list, and generate
+    # a thank you email.
+
     name = input("Enter the full name of the recipient (Type 'list' to view donors. Type '1' for Main Menu):\n")
     if name == 'list':
         print_list()
@@ -64,13 +70,13 @@ def send_thank_you():
     elif name == '1':
         main()
 
-    elif name in donor_names():
+    elif name in donor_dict.keys():
         donation_amount = float(input('EXISTING: Enter the donation amount: '))
         add_donation(name, donation_amount)
         print_email(email_str, name, donation_amount)
     else:  # name not found in list
-        donation_amount = input('NEW: Enter the donation amount: ')
-        print('NEED TO ADD CODE TO APPEND NEW USERS TO DONOR_DICT')
+        donation_amount = (input('NEW: Enter the donation amount: '))
+        add_donation(name, donation_amount)
         print_email(email_str, name, donation_amount)
 
     # print(email_str.format(name.title(), donation_amount))
@@ -84,10 +90,10 @@ def create_report():
     print(header + dotted_line)
     donor_str_fmt = '{:<20} ${:>13.2f}{:>11}    ${:>11.2f}'
 
-    for donor in donor_dict:
-        name = donor['name']
-        num_gifts = len(donor['donations'])
-        total_given = sum(donor['donations'])
+    c = Counter()
+    for name, donations in donor_dict.items():
+        num_gifts = len(Counter(donations))
+        total_given = sum(donations)
         avg_gift = total_given / num_gifts
         print(donor_str_fmt.format(name, total_given, num_gifts, avg_gift))
 
@@ -105,20 +111,22 @@ def die():
 
 def main():
     # main function contains the original donor list and calls other functions.
+    # print(donor_dict)
+    # print(donor_dict.items())
+    # print(donor_dict.values())
+    email_all()
+    arg_dict = {
+        '1':send_thank_you,
+        '2':create_report,
+        '3':die,
+    }
+    prompt = '\nSelect an option:\n'\
+                 '[1] Send a Thank You\n'\
+                 '[2] Create a Report\n'\
+                 '[3] Quit\n'\
+                 '--> '
+    menu_selection(prompt, arg_dict)
 
-        arg_dict = {
-            '1':send_thank_you,
-            '2':create_report,
-            '3':die,
-        }
-
-        prompt = '\nSelect an option:\n'\
-                     '[1] Send a Thank You\n'\
-                     '[2] Create a Report\n'\
-                     '[3] Quit\n'\
-                     '--> '
-
-        menu_selection(prompt, arg_dict)
 
 if __name__ == "__main__":
     main()
