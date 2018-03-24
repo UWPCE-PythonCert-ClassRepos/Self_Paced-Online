568ca00ce5888261be5347f90e061f4be5ede90b
johnpharmd <jhumphre@uw.edu>
2018-03-24 10:34:36 -0700

    added initial mailroomtest.py (lesson06) commit

---
+++ students/johnpharmd/lesson06/mailroomtest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class MailroomTest(unittest.TestCase):
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+    def test_3(self):
+        pass
+
+    def test_4(self):
+        pass
+
+    def test_5(self):
+        pass
