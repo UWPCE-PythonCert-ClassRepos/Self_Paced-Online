730b0d786f46e9d28c6d126f166bdf854fae8a65
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-25 15:36:05 -0700

    Use object data struct

--- students/stuartkershaw/lesson04/mailroom.py
+++ students/stuartkershaw/lesson04/mailroom.py
@@ -1,25 +1,58 @@
 #!/usr/bin/env python3
 
-donors = [
-    ['Anne Hathaway', [4000, 6250]],
-    ['Geronimo Jackson', [150, 400, 1250]],
-    ['Dingo Dogson', [150, 300, 100]],
-    ['Leo Kottke', [75, 125]],
-    ['Flask McCreole', [1000]],
-    ['Piglet Norquist', [20, 15, 30]]
-]
+donors = {
+    'Anne Hathaway': {
+        'donations': [
+            4000,
+            6250
+        ],
+    },
+    'Geronimo Jackson': {
+        'donations': [
+            150,
+            400,
+            1250
+        ]
+    },
+    'Dingo Dogson': {
+        'donations': [
+            150,
+            300,
+            100
+        ]
+    },
+    'Leo Kottke': {
+        'donations': [
+            75,
+            125
+        ]
+    },
+    'Flask McCreole': {
+        'donations': [
+            1000
+        ]
+    },
+    'Piglet Norquist': {
+        'donations': [
+            20,
+            15,
+            30
+        ]
+    }
+}
 
 
-def compose_email(index):
-    message = f"Dear {donors[index][0]}, thanks so much \
-for your generous donation in the amount of: ${donors[index][1][-1]}."
+def compose_email(donor):
+    message = f"Dear {donor}, thanks so much "\
+              f"for your generous donation in the amount of: "\
+              f"${donors[donor]['donations'][-1]}."
     print(message)
     get_selection()
 
 
 def get_selection():
-    options = 'Please select a, b, or c:\n\
-a) send a thank you\nb) create a report\nc) quit\n'
+    options = 'Please select a, b, or c:\n'\
+              'a) send a thank you\nb) create a report\nc) quit\n'
     while True:
         try:
             selection = input(options)
@@ -35,57 +68,62 @@ a) send a thank you\nb) create a report\nc) quit\n'
             input(options)
 
 
-def set_donation(index):
+def set_donation(donor):
     while True:
         try:
             donation = int(input('Please enter a donation amount: '))
             while donation < 1:
-                donation = int(input('Please provide a whole \
-number greater than zero: '))
-            donors[index][1].append(donation)
+                donation = int(input('Please provide a whole '
+                                     'number greater than zero: '))
+            if 'donations' in donors[donor]:
+                donors[donor]['donations'].append(donation)
+            else:
+                donors[donor]['donations'] = [donation]
         except ValueError:
             print('Please provide a whole number greater than zero.')
-            set_donation(index)
+            set_donation(donor)
         break
-    compose_email(index)
+    compose_email(donor)
 
 
 def get_donor_name():
-    donor_names = []
-    for donor in donors:
-        donor_names.append(donor[0])
     instruction = 'Please enter a full name or type \'list\' to see donors:\n'
     name_input = input(instruction)
     if name_input == 'list':
-        for name in donor_names:
+        for name in donors:
             print(name)
         get_donor_name()
-    elif name_input in donor_names:
-        set_donation(donor_names.index(name_input))
+    elif name_input in donors:
+        set_donation(name_input)
     else:
-        donors.append([name_input, []])
-        donor_names.append(name_input)
-        set_donation(donor_names.index(name_input))
+        donors[name_input] = {}
+        set_donation(name_input)
 
 
 def show_donor_table():
     for donor in donors:
-        num = str(len(donor[1]))
-        total = str(sum(donor[1]))
-        avg = format(sum(donor[1]) / len(donor[1]), '.2f')
-        if (len(donor) < 3):
-            donor.append([total, num, avg])
-        else:
-            donor[2] = [total, num, avg]
-
-    print(donors)
+        cur_donor = donors[donor]
+        if 'rollup' not in cur_donor:
+            cur_donor['rollup'] = {}
+        cur_rollup = cur_donor['rollup']
+        cur_rollup['number'] = len(cur_donor['donations'])
+        cur_rollup['total'] = sum(cur_donor['donations'])
+        cur_rollup['average'] = float(
+            format(
+                sum(
+                    cur_donor['donations']) / len(
+                        cur_donor['donations']
+                    ), '.2f'
+                )
+            )
 
     headings = ('Donor Name', 'Total Given', 'Num Gifts', 'Average Gift')
     print('{:20}{:<15}{:<15}{:<15}'.format(*headings))
     print('{:_<65}'.format(''))
     for donor in donors:
-        print('{:<20}'.format(donor[0]),
-                             ('{:<15}' * len(donor[2])).format(*donor[2]))
+        cur_donor = donors[donor]
+        print('{:<20}'.format(donor), ('{:<15}' * len(cur_donor['rollup']))
+              .format(*cur_donor['rollup'].values()))
     get_selection()
 
 
