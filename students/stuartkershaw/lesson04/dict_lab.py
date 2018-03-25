f80dfd344a46cc4a672183718ea8da63fd7e956d
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-25 13:44:58 -0700

    Dictionary activities

--- students/stuartkershaw/lesson04/dict_lab.py
+++ students/stuartkershaw/lesson04/dict_lab.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Activity One
-
 d = {
     "name": "Chris",
     "city": "Seattle",
@@ -26,3 +24,34 @@ for key, value in d.items():
 
 print('cake' in d)
 print('Mango' in d.values())
+
+d_with_ts = {}
+
+for key, value in d.items():
+    d_with_ts[key] = value.lower().count('t')
+
+print(d_with_ts)
+
+s2 = set(range(0, 21, 2))
+s3 = set(range(0, 21, 3))
+s4 = set(range(0, 21, 4))
+
+print(s2, s3, s4)
+
+print(s3.issubset(s4))
+print(s4.issubset(s2))
+
+py_set = set(list('Python'))
+
+print(py_set)
+
+py_set.add('i')
+
+print(py_set)
+
+m_set = frozenset(list('marathon'))
+
+print(m_set)
+
+print(py_set.union(m_set))
+print(py_set.intersection(m_set))
