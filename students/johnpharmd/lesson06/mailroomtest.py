b4b163ae02110a060f5e614515bfc9a879df0b34
johnpharmd <jhumphre@uw.edu>
2018-03-25 14:18:17 -0700

    added another mailroomtest.py (lesson06) commit

--- students/johnpharmd/lesson06/mailroomtest.py
+++ students/johnpharmd/lesson06/mailroomtest.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
 import unittest
+import mailroom4
 
 
 class MailroomTest(unittest.TestCase):
-    def test_1(self):
-        pass
+    def test_get_donor_list(self, test_list):
+        for s in test_list:
+            assert type(s) == str
 
     def test_2(self):
         pass
@@ -18,3 +19,7 @@ class MailroomTest(unittest.TestCase):
 
     def test_5(self):
         pass
+
+
+a_test = MailroomTest()
+a_test.test_get_donor_list(mailroom4.get_donor_list())
