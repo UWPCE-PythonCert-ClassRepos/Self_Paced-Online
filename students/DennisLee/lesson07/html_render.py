b58b3aec7fb40b0c0a711d40b40be41d7504b89b
Dennis Lee <dennis271lee@yahoo.com>
2018-03-27 00:59:20 -0700

    [DennisLee][L7][HTML Render] Implemented the append method.

--- students/DennisLee/lesson07/html_render.py
+++ students/DennisLee/lesson07/html_render.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
-class Element(object):
-	indent, contents = None, []
-	def __init__(self, content=None): pass
-	def append(self, string): pass
-	def render(self, file_out, cur_ind=""): pass
+class Element():
+	def __init__(self, content=None):
+		self.contents = []
+		if content:
+			if isinstance(content, str):
+				self.append(str)
+			elif isinstance(content, list) or isinstance(content, tuple):
+				for i in content:
+					self.append(str(i))
+	def append(self, string):
+		try:  # Remove leading/trailing whitespace & multi-whitespaces
+			clean_str = ' '.join(string.split())
+		except AttributeError:
+			print(f"{string} is not a string - not added to element.")
+		else:
+			self.contents.append(clean_str)
+	def render(self, file_out, cur_ind=""):
+		pass
 
-def render_page(element, file_name): pass
+def render_page(element, file_name):
+	pass
 	
--- students/DennisLee/lesson07/test_html_render.py
+++ students/DennisLee/lesson07/test_html_render.py
@@ -4,8 +4,6 @@ import unittest
 import html_render as hr
 
 class ElementTestCase(unittest.TestCase):
-    def __init__(self):
-        pass
     def setUp(self):
         self.e = hr.Element()
         self.strs_before = (
@@ -21,24 +19,25 @@ class ElementTestCase(unittest.TestCase):
     def tearDown(self):
         self.e = None
     def test_append(self):
+        self.assertRaises(AttributeError, self.e.append, 50)
         self.e.append(self.strs_before[0])
         self.assertEqual(self.e.contents, [self.strs_after[0]])
         self.e.append(self.strs_before[1])
-        self.assertEqual(self.e.contents, list(self.strs_after[0:1]))
+        self.assertEqual(self.e.contents, list(self.strs_after[0:2]))
         self.e.append(self.strs_before[2])
         self.assertEqual(self.e.contents, list(self.strs_after))
     def test_render(self):
+        ind = 11
         filename = 'test_html_file.html'
-        for self.str in self.strs_before:
-            self.e.append(self.str)
-        self.e.render(filename, 11)
+        for str in self.strs_before:
+            self.e.append(str)
+        self.e.render(filename, ind)
         with open(filename, 'r') as f:
             self.strs_out = f.readline()
         self.assertEqual(len(self.strs_out), 3)
         self.assertEqual(self.strs_out[0], "<html>")
         self.assertEqual(self.strs_out[2], "</html>")
-        self.assertEqual(self.strs_out[1], 
-                ' '*self.e.indent + ' '.join(self.strs_after))
+        self.assertEqual(self.strs_out[1], ' '*ind + ' '.join(self.strs_after))
 
 if __name__ == '__main__':
     unittest.main()
