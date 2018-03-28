00928986684412340783c13fda8f1b9058311dec
= <=>
2018-03-27 20:14:55 -0500

    updated gridSize.py to change function and variable names to snake case

--- students/TrkCat/Lesson02/drawGrid.py
+++ students/TrkCat/Lesson02/drawGrid.py
@@ -1,4 +1,4 @@
-def gridPart1():
+def grid_part1():
     """Print a specific center divided 9 x 9 grid"""
     for line in range(21):
         if not line % 10:
@@ -9,26 +9,26 @@ def gridPart1():
             print('|' + ' ' * 9 + '|' + ' ' * 9 + '|')
             
             
-def gridPart2(n):
+def grid_part2(n):
     """Print a center divided n x n grid"""
     n = (n // 2) * 2 + 1
-    gridSize = 2 * n + 2
-    for line in range(gridSize + 1):
+    grid_size = 2 * n + 2
+    for line in range(grid_size + 1):
         if not line % (n + 1):
             print('+','- ' * (n // 2) + '+','- ' * (n // 2) + '+')
         elif line % 2:
-            print(' ' * gridSize)
+            print(' ' * grid_size)
         else:
             print('|' + ' ' * n + '|' + ' ' * n + '|')    
     
     
-def gridPart3(b,n):
+def grid_part3(b,n):
     """Print an n x n grid repeated b x b times"""
-    gridSize = 2 * n + 2
-    for line in range(b * gridSize + 1):
-        if not line % gridSize:
+    grid_size = 2 * n + 2
+    for line in range(b * grid_size + 1):
+        if not line % grid_size:
             print('+' + (' -' * n + ' +') * b)
         elif line % 2:
-            print(' ' * (b * gridSize))
+            print(' ' * (b * grid_size))
         else:
             print('|' + ('  ' * n + ' |') * b)   
