fa39ddc7edbceae494f6cfd5c0cea1bdbbdbe373
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-03-30 09:16:16 -0700

    Progress

--- students/stuartkershaw/lesson06/test_mailroom.py
+++ students/stuartkershaw/lesson06/test_mailroom.py
@@ -1,3 +1,23 @@
 from mailroom import donors
+from mailroom import compose_email
+from mailroom import list_donors
 
-print(donors)
+
+def test_donors():
+    assert isinstance(donors, dict) is True
+
+
+def test_compose_email():
+    assert compose_email('Anne Hathaway') == 'Dear Anne Hathaway, '\
+        'thanks so much for your generous donation in the amount of: $6250.'
+
+
+def test_list_donors(capsys):
+    list_donors()
+    captured = capsys.readouterr()
+    assert captured.out == 'Anne Hathaway\n'\
+        'Geronimo Jackson\n'\
+        'Dingo Dogson\n'\
+        'Leo Kottke\n'\
+        'Flask McCreole\n'\
+        'Piglet Norquist\n'
