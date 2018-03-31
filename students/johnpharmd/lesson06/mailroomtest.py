652be45fd773c6135730f9a57424a283619b8eeb
johnpharmd <jhumphre@uw.edu>
2018-03-31 07:45:08 -0700

    added mailroomtest.py (lesson06) commit after writing several tests

--- students/johnpharmd/lesson06/mailroomtest.py
+++ students/johnpharmd/lesson06/mailroomtest.py
@@ -4,22 +4,47 @@ import mailroom4
 
 
 class MailroomTest(unittest.TestCase):
-    def test_get_donor_list(self, test_list):
-        for s in test_list:
-            assert type(s) == str
+    def make_test_list(self):
+        self.test_list = mailroom4.get_donor_list()
+        return self.test_list
 
-    def test_2(self):
-        pass
+    def test_get_donor_list(self):
+        self.make_test_list()
+        assert type(self.test_list) == list
 
-    def test_3(self):
-        pass
+    def test_donor_list_strings(self):
+        self.make_test_list()
+        for s in self.test_list:
+            if '-' not in s:
+                assert s.isalpha()
+            else:
+                assert s.replace('-', '').isalpha()
 
-    def test_4(self):
-        pass
+    def test_prompt_title(self):
+        self.test_title = mailroom4.prompt_title()
+        assert self.test_title in ('Prof.', 'Dr.', 'Ms.', 'Mr.')
+
+    def test_prompt_donation(self):
+        self.test_donation = mailroom4.prompt_donation()
+        assert str(self.test_donation).isdigit()
 
     def test_5(self):
         pass
 
+    def test_6(self):
+        pass
 
-a_test = MailroomTest()
-a_test.test_get_donor_list(mailroom4.get_donor_list())
+    def test_7(self):
+        pass
+
+    def test_8(self):
+        pass
+
+    def test_9(self):
+        pass
+
+    def test_10(self):
+        pass
+
+    def test_11(self):
+        pass
