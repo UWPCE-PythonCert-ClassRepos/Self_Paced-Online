2039fcaaf76f24203f4c350d22312f2e1649e5aa
rmart300 <stephen.ross.martin@gmail.com>
2018-03-31 11:06:13 -0700

    added getters and setters for diameter

--- students/rmart300/lesson08/circle.py
+++ students/rmart300/lesson08/circle.py
@@ -13,3 +13,15 @@ class Circle(object):
             self._radius = 0
         else:
             self._radius = radius
+
+    @property
+    def diameter(self):
+        return self._radius*2
+
+    @diameter.setter
+    def diameter(self, diameter):
+        if diameter < 0:
+            self._radius = 0
+        else:
+            self._radius = diameter / 2
+
--- students/rmart300/lesson08/test_circle.py
+++ students/rmart300/lesson08/test_circle.py
@@ -3,15 +3,24 @@ from circle import Circle
 
 class TestCircle(unittest.TestCase):
 
-    def test_get(self):
+    def test_get_radius(self):
         c = Circle(50)
         assert c.radius == 50
 
-    def test_set(self):
+    def test_set_radius(self):
         c = Circle(50)
         c.radius = 80
         assert c.radius == 80
 
+    def test_get_diameter(self):
+        c = Circle(50)
+        assert c.diameter == 100
+
+    def test_set_diameter(self):
+        c = Circle(50)
+        c.diameter = 100
+        assert c.diameter == 100
+        assert c.radius == 50
 
 if __name__ == '__main__':
     unittest.main() 
