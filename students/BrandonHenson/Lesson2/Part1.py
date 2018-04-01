6b9a7678121fbdaad52ad2aa2fed013e9653f170
Brandon_Henson <Bigbrandonh@gmail.com>
2018-04-01 14:29:07 -0700

    added comments to 1,2, and 3

--- students/BrandonHenson/Lesson2/Part1.py
+++ students/BrandonHenson/Lesson2/Part1.py
@@ -1,21 +1,36 @@
-def FunGrid():
-    plus = '+'
-    minus = '-'
-    pipe = '|'
-    space = ' '
-    horiz = plus + minus*4 + plus + minus*4 + plus
-    vert = pipe + space*4 + pipe + space*4 + pipe
-    print(horiz)
-    print(vert)
-    print(vert)
-    print(vert)
-    print(vert)
-    print(horiz)
-    print(vert)
-    print(vert)
-    print(vert)
-    print(vert)
-    print(horiz)
-    input()
+'''
+Brandon Henson
+3/31/18
+Lesson 2 Part 1
+Making a grid
+Brandon ChangedFunGrid to fun_grid 4/1/18
+'''
 
-FunGrid()
+def fun_grid():
+################################      define a bunch of characters that are used          ##########################
+ #plus symbol used in the tops    
+	plus = '+'
+ #minus symbol used in the tops
+	minus = '-'
+ #line symbol used for the columns
+	pipe = '|'
+ #space used everywhere   
+	space = ' '
+ #concatenated symbols to make the horizontal "tops" as I call them   
+	horiz = plus + minus*4 + plus + minus*4 + plus
+ #concatenated symbols to make the sides   
+	vert = pipe + space*4 + pipe + space*4 + pipe
+ #print our grid
+	print(horiz)
+    print(vert)
+    print(vert)
+    print(vert)
+    print(vert)
+    print(horiz)
+    print(vert)
+    print(vert)
+    print(vert)
+    print(vert)
+    print(horiz)
+#call the function
+fun_grid()
