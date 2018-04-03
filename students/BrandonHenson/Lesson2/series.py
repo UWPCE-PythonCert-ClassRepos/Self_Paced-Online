9938c6aa567874461fd1c754efa95af97396e13d
Brandon_Henson <Bigbrandonh@gmail.com>
2018-04-02 18:57:57 -0700

    I finished the assertions and added some comments

--- students/BrandonHenson/Lesson2/series.py
+++ students/BrandonHenson/Lesson2/series.py
@@ -1,3 +1,10 @@
+'''
+Brandon Henson
+4/2/18
+Lesson 2 series.py
+using index and math to make functions and assertions
+'''
+
 def fibonacci(n):
     """This function prints out the digit in the Fibonacci series that is in the index number defined by the parameter."""
     fibo_list = [0,1]
@@ -8,9 +15,6 @@ def fibonacci(n):
     #print(fibo_list[n])
     return fibo_list[n]
 
-
-
-
 def lucas(n):
     """This function prints out the digit in the lucas series that is in the index number defined by the parameter."""
     luc_list = [2,1]
@@ -18,7 +22,7 @@ def lucas(n):
         x = luc_list[i-2] + luc_list[i-1]
         luc_list.append(x)
     #printed to test if I'm getting the index number
-    print(luc_list[n])
+    #print(luc_list[n])
     return luc_list[n]
 
 def sum_series (n,a = 0,b = 1):
@@ -29,7 +33,14 @@ def sum_series (n,a = 0,b = 1):
     #print (sum_list[n])
     return sum_list[n]
 
-
+#a few assert statements to test the fibonacci function
 assert fibonacci(12) == 144
-assert lucas(4) == 7
-assert sum_series(6) == 8
+assert fibonacci(11) == 89
+assert fibonacci(10) == 55
+#a few assert statements to test the lucas function
+assert lucas(12) == 322
+assert lucas(11) == 199
+assert lucas(10) == 123
+#a few assert statements to test the sum_series function
+assert sum_series(12,4,5) == 1076
+assert sum_series(11,3,5) == 610
