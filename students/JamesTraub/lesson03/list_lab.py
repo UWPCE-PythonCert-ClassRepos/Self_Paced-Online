74d0bff7cbc5f05e31b0244a02bfba9973f8b4cf
James Traub <traub01-devtrain@outlook.com>
2018-04-03 21:54:23 -0700

    Final adjustments after testing list_lab.py

--- students/JamesTraub/lesson03/list_lab.py
+++ students/JamesTraub/lesson03/list_lab.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 
-# Series 1
+# Lesson 3 - Series 1
 
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-add_fruit = input('Please enter the name of a fruit to add to the end of list. ')
-fruit.append(add_fruit)
+fruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
+print("Create and display a list of fruit.")
 print(fruit)
+print()
 
+inp_fruit = input('Please enter a name of a fruit to add to the end of list. ')
+fruit.append(inp_fruit)
+print(fruit)
+print()
 
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 fruit_number = int(input('Please enter the number that correspends with one fruit in the list. '))
-for i, numfruit in enumerate(fruit):
+for i, nfruit in enumerate(fruit):
     if fruit_number == (i + 1):
-        print(fruit_number, numfruit)
+        print(fruit_number, nfruit)
+print()
 
-
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-add_fruit = []
-beg_fruit = input('Please enter a fruit to add to the beginning of the list. ')
-add_fruit.insert(0, beg_fruit)
-new_list = add_fruit + fruit
-print(new_list)
-
-
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-add_fruit = input('Please enter a fruit to add to the beginning of the list. ')
-fruit.insert(0, add_fruit)
+add_fruit2 = []
+inp_fruit2 = input('Please enter a fruit to add to the beginning of the list. ')
+fruit = [inp_fruit2,] + fruit
 print(fruit)
+print()
 
+add_fruit3 = []
+add_fruit = input('Please enter a fruit to add to the beginning of the list. ')
+add_fruit3.insert(0, add_fruit)
+fruit = add_fruit3 + fruit
+print(fruit)
+print()
 
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 first_letter = [i[0] for i in fruit]
 for x in first_letter:
     if x.title() == "P":
@@ -37,21 +38,22 @@ for x in first_letter:
 print()
 
 
-# Series 2
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+# Lesson 3 - Series 2
+print(fruit)
+print()
 
 fruit.pop()
 print(fruit)
+print()
 
-del_fruit = input('Enter the name of a fruit to delete. ')
+del_fruit = input('Please enter the name of a fruit in the list to delete. ')
 if del_fruit in fruit:
     fruit.remove(del_fruit)
 print(fruit)
+print()
 
 
-# Series 3
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-
+# Lesson 3 - Series 3
 copy_fruit = fruit[:]
 for i in fruit:
     prompt_y_n = input("Do you like {}? ".format(i))
@@ -62,12 +64,10 @@ for i in fruit:
     if prompt_y_n == "no":
         copy_fruit.remove(i)
 print(copy_fruit)
-print(fruit)
+print()
 
 
-# Series 4
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-
+# Lesson 3 - Series 4
 new_fruit = fruit.copy()
 reversed_list = []
 for tree in new_fruit:
