4f899d246f46c8df529fa0163dffb001ac47e4a0
Dustin L <r.dustin.lane@gmail.com>
2018-04-07 10:22:33 -0700

    Circle - Add radius and diameter properties

--- students/Dustin_L/lesson08/circle.py
+++ students/Dustin_L/lesson08/circle.py
@@ -5,5 +5,29 @@
 class Circle:
     """Circle Class"""
     def __init__(self, radius):
-        self.radius = radius
-        self.diameter = 2 * radius
+        self._radius = radius
+        self._diameter = 2 * radius
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        if value < 0:
+            raise ValueError
+
+        self._diameter = value
+            self._radius = value / 2
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            raise ValueError
+
+        self._radius = value
+            self._diameter = value * 2
--- students/Dustin_L/lesson08/test_circle.py
+++ students/Dustin_L/lesson08/test_circle.py
@@ -2,13 +2,16 @@
 """Unit Tests for Circle"""
 
 import unittest
+import math
 import circle
 
 
 class TestCircle(unittest.TestCase):
     """Test class containing all unit tests for Circle"""
-    test_radius = 4
-    test_diameter = 2 * test_radius
+    def __init__(self, *args, **kwargs):
+        super(TestCircle, self).__init__(*args, **kwargs)
+        self.test_radius = 4
+        self.test_diameter = 2 * self.test_radius
 
     def setUp(self):
         self.c = circle.Circle(self.test_radius)
@@ -22,6 +25,58 @@ class TestCircle(unittest.TestCase):
 
     def test_get_diameter(self):
         self.assertTrue(self.c.diameter == self.test_diameter,
+                        msg=self.c.diameter)
+
+    def test_radius_property_positive(self):
+        new_radius = 3
+        self.c.radius = new_radius
+
+        self.assertTrue(self.c.radius == new_radius,
+                        msg=self.c.radius)
+        self.assertTrue(self.c.diameter == 2 * new_radius,
+                        msg=self.c.diameter)
+
+    def test_diameter_property_positive(self):
+        new_diameter = 5
+        self.c.diameter = new_diameter
+
+        self.assertTrue(self.c.diameter == new_diameter,
+                        msg=self.c.diameter)
+        self.assertTrue(self.c.radius == new_diameter / 2,
+                        msg=self.c.radius)
+
+    def test_radius_property_negative(self):
+        with self.assertRaises(ValueError):
+            self.c.radius = -4
+
+        self.assertTrue(self.c.radius == self.test_radius,
+                        msg=self.c.radius)
+        self.assertTrue(self.c.diameter == self.test_diameter,
+                        msg=self.c.diameter)
+
+    def test_diameter_property_negative(self):
+        with self.assertRaises(ValueError):
+            self.c.diameter = -4
+
+        self.assertTrue(self.c.radius == self.test_radius,
+                        msg=self.c.radius)
+        self.assertTrue(self.c.diameter == self.test_diameter,
+                        msg=self.c.diameter)
+
+    def test_radius_property_zero(self):
+        self.c.radius = 0
+
+        self.assertTrue(self.c.radius == 0,
+                        msg=self.c.radius)
+        self.assertTrue(self.c.diameter == 0,
+                        msg=self.c.diameter)
+
+    def test_diameter_property_zero(self):
+        self.c.diameter = 0
+
+        self.assertTrue(self.c.radius == 0,
+                        msg=self.c.radius)
+        self.assertTrue(self.c.diameter == 0,
                         msg=self.c.diameter)
 
 
