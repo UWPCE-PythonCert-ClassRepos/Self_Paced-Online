458844ed0e8ed4b4e9f840858b574a70437143ed
Dustin L <r.dustin.lane@gmail.com>
2018-04-07 09:43:01 -0700

    Circle - Add initial files

---
+++ students/Dustin_L/lesson08/circle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Circle Module"""
+
+
+class Circle:
+    """Circle Class"""
+    def __init__(self):
+        pass
---
+++ students/Dustin_L/lesson08/test_circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Unit Tests for Circle"""
+
+import unittest
+import circle
+
+
+class TestCircle(unittest.TestCase):
+    """Test class containing all unit tests for Circle"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
