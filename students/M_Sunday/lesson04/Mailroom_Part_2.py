2a0b641d37b4055de3fbc573627e9c3e7113b75a
mmsunday1 <matt.m.sunday@gmail.com>
2018-04-07 00:22:33 -0700

    1) Converted code to handle a dictionary 2) fixed error where multiple calls to print a report added the donation amount each time

--- students/M_Sunday/lesson04/Mailroom_Part_2.py
+++ students/M_Sunday/lesson04/Mailroom_Part_2.py
@@ -5,33 +5,32 @@ from operator import itemgetter
 
 
 # ------- Global Assignments -------
-donor_data = [['John', 'Marks', 5000.34, 2500, 1267],
-              ['Matt', 'Smith', 750],
-              ['Kelsey', 'Rodgers', 55.76],
-              ['John', 'Williams', 500, 1898],
-              ['Morgan', 'Stanley', 12000.98, 8670],
-              ['Roger', 'Johnson', 264, 942.23],
-              ['Sherry', 'Wilson', 4000, 2324.1, 1352],
-              ['Alex', 'Grant', 8739, 4312]]
-
+donor_data = {'John Marks': [5000.34, 2500, 1267],
+              'Matt Smith': [750],
+              'Kelsey Rodgers': [55.76],
+              'John Williams': [500, 1898],
+              'Morgan Stanley': [12000.98, 8670],
+              'Roger Johnson': [264, 942.23],
+              'Sherry Wilson': [4000, 2324.1, 1352],
+              'Alex Grant': [8739, 4312]}
 # ----------------------------------
 
 
-def menu_disp():
-    print("\n\n----- MENU -----")
-    print("1: Send a Thank You")
-    print("2: Create a Report")
-    print("3: Send letters to everyone")
-    print("4: Quit")
+def menu_display():
+    print("\n\n----- MENU -----\n"
+          "1: Send a Thank You\n"
+          "2: Create a Report\n"
+          "3: Send letters to everyone\n"
+          "4: Quit\n")
     menu_sel_ = input("------> "
                       "Select an option from the menu: ")
     return menu_sel_
 
 
-def list_display(data):
+def list_display():
     print("\n---- List of all Donors ----")
-    for item in data:
-        print("- {} {}".format(item[0], item[1]))
+    for item in donor_data:
+        print("- {}".format(item))
     print("----------------------------")
     thank_you()
 
@@ -44,9 +43,7 @@ def list_details(data):
     print(header)
     print(buffer)
     for item in sorted(data, key=itemgetter(-2), reverse=True):
-        print("{:<18} $  {:>9.2f}  {:>10}  $   {:>9.2f}".format(item[0] + ' ' + item[1],
-                                                                item[-2], item[-3],
-                                                                item[-1]))
+        print("{:<18} $  {:>9.2f}  {:>10}  $   {:>9.2f}".format(item[0], item[2], item[1], item[3]))
 
 
 def compose_email(donor_name, donation_amount):
@@ -55,39 +52,42 @@ def compose_email(donor_name, donation_amount):
     print(greeting_email, body_email)
 
 
-def add_donation(donor_name, i):
+def add_donation(donor_name):
     donation_amount = input("\nEnter the donation amount for {} ".format(donor_name))
-    donor_data[i].append(float(donation_amount))
+    donor_data[donor_name].append(float(donation_amount))
     compose_email(donor_name, donation_amount)
 
 
-def thank_you(data=donor_data):
+def thank_you():
     donor_name = input("\nPlease enter the donor's full name: ")
     if donor_name == 'list':
-        list_display(donor_data)
+        list_display()
     else:
-        donor_first_name = donor_name.split()[0]
-        donor_last_name = donor_name.split()[1]
-        for i, item in enumerate(data):
-            if item[0] == donor_first_name and item[1] == donor_last_name:
-                add_donation(donor_name, i)
-                break
+        if donor_name in donor_data:
+            add_donation(donor_name)
         else:
-            donor_data.append(donor_name.split(' '))
-            add_donation(donor_name, -1)
+            donor_data[donor_name] = []
+            add_donation(donor_name)
 
 
-def calc_data(data=donor_data):
-    for i, donor in enumerate(data):
-        donations_quantity = len(donor) - 2
-        donor_data[i].append(donations_quantity)
-        donor_data[i].append(sum(donor[2:-1]))
-        donor_data[i].append(float(donor[-1]) / donations_quantity)
+def calc_data():
+    summary_data = []
+    for i, item in enumerate(donor_data):
+        donations_quantity = len(donor_data[item])
+        donations_value = sum(donor_data[item])
+        donations_average = (float(donations_value / donations_quantity))
+        summary_data.append([])
+        summary_data[i].extend([item, donations_quantity, donations_value, donations_average])
+    return summary_data
 
 
-def report(data=donor_data):
-    calc_data(data)
-    list_details(data)
+def report():
+    summary_data = calc_data()
+    list_details(summary_data)
+
+
+def send_all_letters():
+    return
 
 
 if __name__ == "__main__":
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     menu_map = {'1': thank_you, '2': report, '3': send_all_letters, '4': 'exit'}
 
     while True:
-        menu_sel = menu_disp()
+        menu_sel = menu_display()
         if menu_map[menu_sel] == 'exit':
             break
         menu_map[menu_sel]()
