7663ce6974670bd6aa4fdde05e6240f4d9a33da5
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-04-07 16:15:25 -0700

    Wrap up circle.py, and test_circle.py

--- students/khtruong/lesson_08/circle.py
+++ students/khtruong/lesson_08/circle.py
@@ -2,13 +2,22 @@
 
 """This module create some really cool circles."""
 import math
+import functools
 
-
+@functools.total_ordering
 class Circle:
     """The circle class"""
 
     def __init__(self, radius):
         self.radius = radius
+
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__qualname__, self.radius)
+
+    def __str__(self):
+        return '{} with radius: {}'.format(
+            self.__class__.__qualname__, self.radius)
 
     @property
     def diameter(self):
@@ -26,6 +35,20 @@ class Circle:
     def from_diameter(cls, diameter):
         return cls(diameter / 2)
 
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, val):
+        return Circle(self.radius * val)
+
+    def __rmul__(self, val):
+        return Circle(self.radius * val)
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
 
 if __name__ == "__main__":
     pass
