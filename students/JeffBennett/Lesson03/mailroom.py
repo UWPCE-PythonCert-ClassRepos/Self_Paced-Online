ae26352dbded33d89e5600cbe36bfbd1b1fa9102
JeffBennett <jeff.bennett1920@gmail.com>
2018-04-09 15:18:54 -0700

    combined and simplified processing functions, finished main interaction

--- students/JeffBennett/Lesson03/mailroom.py
+++ students/JeffBennett/Lesson03/mailroom.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Data
+
 donors = [
     ['Lorenzo Currie', 3000, 250, 5000],
     ['Marie Kane', 1000, 195, 50.50],
@@ -13,17 +15,20 @@ donors = [
 
 
 def print_menu():
+    """print menu of user choices."""
     print('Main Menu\n' + '-'*25 +
           '\n(1)\tSend a Thank You\n(2)\tCreate a Report\n(3)\tQuit')
 
 
 def print_donor_list(donors):
+    """print list of donor names."""
     print('Our Current List of Donors:\n' + '-' * 25)
     for item in donors:
         print(item[0])
 
 
 def print_email(donor, gift):
+    """print email thanking donor for recent donation."""
     print(f'Dear {donor}\n')
     print('We are delighted to acknowledge your generous')
     print(f'gift of ${gift:,.2f} to Northwest Lifeboats.')
@@ -32,17 +37,20 @@ def print_email(donor, gift):
 
 
 def print_sorted_donors(ranked_list):
+    """print list of donors sorted by total donation history."""
     print('{0:32}{1:13}{2:14}{3:13}'.format
           ('Donor Name', '| Total Gifts', '| Num Gifts', '| Average Gift'))
     print('-' * 73)
     for row in ranked_list:
-        print('{1:30s}  ${0:12.2f}{2:12d}  ${3:12.2f}'.format(*row))
+        print('{0:30s}  ${1:12.2f}{2:12d}  ${3:12.2f}'.format(*row))
 
 
 # Processing
 
 
 def update_donors(list, name, donation):
+    """update the list of donors and their gift history.
+    Function iterates the copy and mutates the original list. """
     count = 0
     for donor in donors[:]:
         count += 1
@@ -51,22 +59,18 @@ def update_donors(list, name, donation):
             break
         elif count == len(donors[:]):
             donors.append([name, donation])
-            break
         else:
             continue
     return donors
 
 
 def sort_donors(donors):
-    data_table = []
-    for donor in donors:
-        data_table = data_table.append
-        ([sum(donor[1:]), donor[0],
-          len(donor[1:]), sum(donor[1:]) / len(donor[1:])])
-    print(data_table)
-    sorted_donors = sorted(data_table, ascending=False)
+    """sort donors from largest to smallest aggregate givers, and calculate
+    average gift and number of gifts per donor. """
+    data_table = [[d[0], sum(d[1:]), len(d[1:]), sum(d[1:]) / len(d[1:])]
+                  for d in donors]
+    sorted_donors = sorted(data_table, key=lambda x: x[1], reverse=True)
     return sorted_donors
-
 
 # Main
 
@@ -92,4 +96,4 @@ if __name__ == "__main__":
             continue
         if menu_choice == '3':
             break
-print("Exiting the program.  Changes made during this session not saved.")
+print("Exiting the program.  Changes made during session are not saved.")
