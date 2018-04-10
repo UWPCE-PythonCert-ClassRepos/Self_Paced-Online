2639a4e28507695bb0360d42c02499fdff4dab1f
JeroenSweerts <jeroensweerts@hotmail.com>
2018-04-11 00:13:04 +0200

    Step5 : second commit

--- students/JeroenSweerts/Lesson7/html_render.py
+++ students/JeroenSweerts/Lesson7/html_render.py
@@ -14,9 +14,7 @@ class Element():
         '''Method that can add another string to the content.'''
         self.content.append(string)
 
-    def render(self, file_out, cur_ind=0):
-        '''Method that renders the tag and the strings in the content.'''
-        self.cur_ind = cur_ind
+    def create_open_tag(self):
         if self.attributes is None:
             output = (self.cur_ind * " ") + "<" + self.tagname + ">\n"
         else:
@@ -24,17 +22,33 @@ class Element():
             for key, value in self.attributes.items():
                 output = output + " " + key + "=" + '"' + value + '"'
             output = output + ">\n"
-        if len(self.content) > 0:
-            for element in self.content:
-                if isinstance(element, str):
-                    output = output + (2 * self.cur_ind * " ") + element + "\n"
-                else:
-                    f = StringIO()
-                    element.cur_ind = self.cur_ind + element.indentation
-                    element.render(f, element.cur_ind)
-                    output = output + f.getvalue()
+        return output
+
+    def write_tag_content(self, output):
+        for element in self.content:
+            if isinstance(element, str):
+                output = output + (2 * self.cur_ind * " ") + element + "\n"
+            else:
+                f = StringIO()
+                element.cur_ind = self.cur_ind + element.indentation
+                element.render(f, element.cur_ind)
+                output = output + f.getvalue()
+        return output
+
+    def create_closing_tag(self, output):
         output = output + (self.cur_ind * " ") + "</" + self.tagname + ">\n"
+        return output
+
+    def render(self, file_out, cur_ind=0):
+        '''Method that renders the tag and the strings in the content.'''
+        self.cur_ind = cur_ind
+        output = self.create_open_tag()
+        if len(self.content) > 0:
+            output = self.write_tag_content(output)
+        output = self.create_closing_tag(output)
         file_out.write(output)
+
+
 
 class Html(Element):
     tagname = 'html'
@@ -50,9 +64,7 @@ class Head(Element):
 
 
 class OneLineTag(Element):
-    def render(self, file_out, cur_ind=0):
-        '''Method that renders the tag and the strings in the content.'''
-        self.cur_ind = cur_ind
+    def create_open_tag(self):
         if self.attributes is None:
             output = (self.cur_ind * " ") + "<" + self.tagname + ">"
         else:
@@ -60,26 +72,29 @@ class OneLineTag(Element):
             for key, value in self.attributes.items():
                 output = output + " " + key + "=" + '"' + value + '"'
             output = output + ">"
-        if len(self.content) > 0:
-            for element in self.content:
-                if isinstance(element, str):
-                    output = output + element
-                else:
-                    f = StringIO()
-                    element.cur_ind = self.cur_ind + element.indentation
-                    element.render(f, element.cur_ind)
-                    output = output + f.getvalue()
+        return output
+
+    def write_tag_content(self, output):
+        for element in self.content:
+            if isinstance(element, str):
+                output = output + element
+            else:
+                f = StringIO()
+                element.cur_ind = self.cur_ind + element.indentation
+                element.render(f, element.cur_ind)
+                output = output + f.getvalue()
+        return output
+
+    def create_closing_tag(self, output):
         output = output + "</" + self.tagname + ">\n"
-        file_out.write(output)
+        return output
 
 class Title(OneLineTag):
     tagname = 'title'
 
 
 class SelfClosingTag(Element):
-    def render(self, file_out, cur_ind=0):
-        '''Method that renders the tag and the strings in the content.'''
-        self.cur_ind = cur_ind
+    def create_open_tag(self):
         if self.attributes is None:
             output = (self.cur_ind * " ") + "<" + self.tagname + " />\n"
         else:
@@ -87,9 +102,16 @@ class SelfClosingTag(Element):
             for key, value in self.attributes.items():
                 output = output + " " + key + "=" + '"' + value + '"'
             output = output + " />\n"
-        if len(self.content) > 0:
-            raise TypeError('You are not allowed to put content in a self closing tag')
-        file_out.write(output)
+        return output
+
+    def write_tag_content(self, output):
+        raise TypeError('You are not allowed to put content in a self closing tag')
+
+    def create_closing_tag(self, output):
+        return output
+
+
+
 
 class Hr(SelfClosingTag):
     tagname = 'hr'
