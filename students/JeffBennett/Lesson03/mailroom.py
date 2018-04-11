1d626348be233f28ba722d66e424e1f932c4e8ff
JeffBennett <jeff.bennett1920@gmail.com>
2018-04-11 10:40:57 -0700

    restructured while loop to enable return to main menu after all current tasks

--- students/JeffBennett/Lesson03/mailroom.py
+++ students/JeffBennett/Lesson03/mailroom.py
@@ -16,7 +16,7 @@ donors = [
 
 def print_menu():
     """print menu of user choices."""
-    print('Main Menu\n' + '-'*25 +
+    print('\nMain Menu\n' + '-'*25 +
           '\n(1)\tSend a Thank You\n(2)\tCreate a Report\n(3)\tQuit')
 
 
@@ -29,19 +29,19 @@ def print_donor_list(donors):
 
 def print_email(donor, gift):
     """print email thanking donor for recent donation."""
-    print(f'Dear {donor}\n')
+    print(f'\nDear {donor}\n')
     print('We are delighted to acknowledge your generous')
     print(f'gift of ${gift:,.2f} to Northwest Lifeboats.')
     print('Your gift supports fishermen all along the Washington coast.\n')
     print('Sincerely yours\n\nJ. A. Bennett\nDirector')
 
 
-def print_sorted_donors(ranked_list):
+def print_sorted_donors(sorted_donors):
     """print list of donors sorted by total donation history."""
     print('{0:32}{1:13}{2:14}{3:13}'.format
           ('Donor Name', '| Total Gifts', '| Num Gifts', '| Average Gift'))
     print('-' * 73)
-    for row in ranked_list:
+    for row in sorted_donors:
         print('{0:30s}  ${1:12.2f}{2:12d}  ${3:12.2f}'.format(*row))
 
 
@@ -80,19 +80,25 @@ if __name__ == "__main__":
         print_menu()
         menu_choice = input('Please enter the number of your choice: ')
         if menu_choice == '1':
-            donor_name = input("Enter donor first and last name,\n or " +
-                               "'list' for a list of current donors: ")
-            if donor_name == 'list':
-                print_donor_list()
+            donor_name = input("\nEnter donor first and last name,\n " +
+                               "'list' for a list of current donors: \n" +
+                               "  or 'menu' to return to the main menu: ")
+            if donor_name == 'menu':
+                continue
+            elif donor_name == 'list':
+                print_donor_list(donors)
                 continue
             else:
-                gift = float(input("Enter amount of gift: "))
+                gift = float(input("\nEnter amount of gift: " +
+                                   " or '0' to return to the main menu: "))
+                if gift == 0:
+                    continue
                 print_email(donor_name, gift)
                 update_donors(donors, donor_name, gift)
                 continue
         if menu_choice == '2':
-            sort_donors(donors)
-            print_sorted_donors()
+            sorted_donors = sort_donors(donors)
+            print_sorted_donors(sorted_donors)
             continue
         if menu_choice == '3':
             break
