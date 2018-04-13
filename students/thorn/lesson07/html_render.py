3d5c1e9b9cb7c9e3df88276313fc68c386f3aab2
Thomas Horn <hortw1991@gmail.com>
2018-04-13 11:56:08 -0500

    All working except the <a>

--- students/thorn/lesson07/html_render.py
+++ students/thorn/lesson07/html_render.py
@@ -5,19 +5,21 @@ A class-based system for rendering html.
 """
 
 # This is the framework for the base class
-class Element(object):
+class Element:
     # Class attributes
     tag = ''  # tag name -> to be overridden
     indent = '    '  # indention level -> <html> is first so it starts at 4
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         ''' Init '''
         # Set content list if any exists
         if content:
             self.content = [content]
         else:
             self.content = []
-
+        # Various attributes
+        self.attributes = kwargs  # dict
+        
     def append(self, new_content):
         '''
         Appends a string to content.
@@ -35,7 +37,14 @@ class Element(object):
                  entire tag should be indented for printing
         '''
         # Start with html at our current ident --> add content 1 tab extra in
-        file_out.write(f"{cur_ind}<{self.tag}>\n")
+
+        # Attribute kwargs dictionary rendering
+        # format: <tag key="value">\n
+        file_out.write(f"{cur_ind}<{self.tag}")
+
+        for key, value in self.attributes.items():
+            file_out.write(f' {key}="{value}"')
+        file_out.write(">\n")  # Close tag and newline outside of loop
 
         for contents in self.content:
             # if hasattr(contents, 'render'):
@@ -53,6 +62,8 @@ class Html(Element):
 
     # Pass render with indent.
     def render(self, file_out, cur_ind=""):
+        # Doctype
+        file_out.write("<!DOCTYPE html>\n")
         Element.render(self, file_out, cur_ind='')
 
 
@@ -89,3 +100,70 @@ class OneLineTag(Element):
 class Title(OneLineTag):
     ''' <title> tag subclass of OneLineTag (puts it all on one line). '''
     tag = 'title'
+
+
+class SelfClosingTag(Element):
+    ''' Renders self closing tags e.g. <hr /> <br />
+    
+    Overrides render to only render one tag and any attributes.
+    Raises TypeError if content attempted.
+
+    Refactor of render coming when the entire thing works.
+    '''
+    def render(self, file_out, cur_ind=''):
+        file_out.write(f"{cur_ind}<{self.tag}")
+        # Attributes
+        for key, value in self.attributes.items():
+            file_out.write(f' {key}="{value}"')
+        # Close SINGLE LINE tag
+        # format: /> + newline
+        file_out.write(' />\n')
+        if self.content:
+            print("No content allowed")
+            raise TypeError
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+
+class Br(SelfClosingTag):
+    tag = "br"
+
+
+class Ul(Element):
+    ''' unordered list '''
+    tag = 'ul'
+
+
+class Li(Element):
+    ''' list element ''' 
+    tag = 'li'
+
+
+class H(OneLineTag):
+    ''' Header element.  Overrides onelinetag by taking one int arg for the header
+    level.
+    '''
+    def __init__(self, level, content, **kwargs):
+        super().__init__(content, **kwargs)
+        self.level = level
+        self.tag = f'h{level}'
+
+
+class Meta(SelfClosingTag):
+    ''' Meta Tag - self closing '''
+    tag = "meta"
+
+
+
+''' Not working quite right. '''
+
+class A(Element):
+    ''' Link element.  Overrides Element __init__ call. '''
+    tag = 'a'
+
+    def __init__(self, link, content):
+        # Call superclass init -> no self necessary
+        super().__init__(content, href=link)
+
+        
--- students/thorn/lesson07/html_render_unit_tests.py
+++ students/thorn/lesson07/html_render_unit_tests.py
@@ -1,18 +1,24 @@
+'''
+Thomas Horn
+4.13.2018
+Lesson 7 - HTML Render and Subclassing
+'''
+
 import unittest
 import html_render as hr
 from io import StringIO
 
-class TagTests(unittest.TestCase):
-    def test_html(self):
-        ''' Test that it starts with <html> and ends with </html> '''
-        e = hr.Element()
-        test = hr.Html()
-        e.render(hello)
-        f = StringIO()
-        test.render(f)
-        lines = f.getvalue()
-        lines.strip()
-        print(lines)
+# class TagTests(unittest.TestCase):
+#     def test_html(self):
+#         ''' Test that it starts with <html> and ends with </html> '''
+#         e = hr.Element()
+#         test = hr.Html()
+#         e.render(hello)
+#         f = StringIO()
+#         test.render(f)
+#         lines = f.getvalue()
+#         lines.strip()
+#         print(lines)
 
 
 class Step3Tests(unittest.TestCase):
@@ -21,7 +27,7 @@ class Step3Tests(unittest.TestCase):
         correct_tag = "head"
         self.assertEqual(hr.Head.tag, correct_tag)
 
-    def test_title(self):
+    def test_OneLineTag(self):
         ''' Test to make sure <title> tags are printed on the same line as their content as their super class OneLineTag dictates. '''
         test = hr.OneLineTag("One Line Test")
         test.tag = "title"
@@ -30,6 +36,55 @@ class Step3Tests(unittest.TestCase):
         # Check for start and end with title
         self.assertEqual(f.getvalue()[:7], "<title>")        
         self.assertEqual(f.getvalue()[-9:], "</title>\n")
+
+
+class Step4Tests(unittest.TestCase):
+    def test_element_accepts_kwargs(self):
+        ''' Test to just make sure element accepts different # kwargs. '''
+        test = hr.Element("some text content", id="TheList", style="line-height:200%")
+        # print(type(test.attributes))
+        # print(test.attributes)
+
+class SelfClosingTests(unittest.TestCase):
+    def test_self_closing_tag(self):
+        ''' Tests if a Br self closing tag is properly formatted. '''
+        test = hr.Br()
+        test.tag_name = 'br'
+        test_correct = '<br />\n' # need the newline
+        f = StringIO()
+        test.render(f)
+        self.assertEqual(f.getvalue(), test_correct)
+
+# class LinkTest(unittest.TestCase):
+#     def test_link(self):
+#         ''' Test to make sure a test link equals <a href="http://google.com">link</a> '''
+#         test = hr.A('http://google.com', 'link')
+#         test_correct = '<a href="http://gooogle.com">link</a>'
+#         f = StringIO()
+#         test.render(f)
+#         self.assertEqual(f.getvalue(), test_correct)
+        
+class HTest(unittest.TestCase):
+    def test_H(self):
+        ''' Tests for a proper H1level '''
+        test = hr.H(1, "Test")
+        test_correct = "<h1>Test</h1>\n"
+        f = StringIO()
+        test.render(f)
+        print(f.getvalue())
+        self.assertEqual(f.getvalue(), test_correct)
+
+
+class MetaTest(unittest.TestCase):
+    def test_Meta(self):
+        ''' Tests for a <meta charset="UTF-8" /> '''
+        test = hr.Meta(charset="UTF-8")
+        test_correct = '<meta charset="UTF-8" />\n'
+        f = StringIO()
+        test.render(f)
+        print(f.getvalue())
+        self.assertEqual(f.getvalue(), test_correct)
+
 
 
 if __name__ == "__main__":
