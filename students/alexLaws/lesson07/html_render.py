546714553b7aee327beabae5f71e774e4e8ede3e
Alex Laws <tal1286@gmail.com>
2018-04-13 15:31:45 -0700

    HTML Render Project

--- students/alexLaws/lesson07/html_render.py
+++ students/alexLaws/lesson07/html_render.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
 
 
+def style_tags(keyword_dict):
+    style = []
+    if keyword_dict is not None:
+        for key, value in keyword_dict.items():
+            style.append(' {}=\"{}\"'.format(key, value))
+    return "".join(style)
+
+
 class Element:
     '''A class to render an HTML element'''
 
     tag_name = ''
     indents = 4
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.content = []
         if content:
             self.content.append(content)
+        self.kwargs = kwargs
 
     def append(self, content):
         '''Appends strings to content'''
         self.content.append(content)
 
     def render(self, file_out, cur_ind=""):
+        '''Render the HTML File'''
+
         next_ind = cur_ind + (" " * self.indents)
-        if self.tag_name == 'html':
-            file_out.write("{}<!DOCTYPE html>\n".format(cur_ind))
-        file_out.write("{}<{}>\n".format(cur_ind, self.tag_name))
+        element_format = style_tags(self.kwargs)
+        file_out.write("{}<{}{}>\n".format(cur_ind, self.tag_name, element_format))
         for item in self.content:
             if isinstance(item, str):
                 file_out.write('{}{}\n'.format(next_ind, item))
@@ -34,11 +44,12 @@ class Html(Element):
 
     tag_name = 'html'
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+    def render(self, file_out, cur_ind=""):
+        file_out.write("{}<!DOCTYPE html>\n".format(cur_ind))
+        super().render(file_out, cur_ind)
 
 
 class Head(Element):
@@ -46,11 +57,8 @@ class Head(Element):
 
     tag_name = 'head'
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
 
 
 class Body(Element):
@@ -58,11 +66,8 @@ class Body(Element):
 
     tag_name = 'body'
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
 
 
 class P(Element):
@@ -70,11 +75,8 @@ class P(Element):
 
     tag_name = 'p'
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
 
 
 class OneLineTag(Element):
@@ -82,14 +84,12 @@ class OneLineTag(Element):
 
     tag_name = ''
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
 
     def render(self, file_out, cur_ind=""):
-        file_out.write("{}<{}>".format(cur_ind, self.tag_name))
+        element_format = style_tags(self.kwargs)
+        file_out.write("{}<{}{}>".format(cur_ind, self.tag_name, element_format))
         for item in self.content:
             if isinstance(item, str):
                 file_out.write('{}'.format(item))
@@ -103,8 +103,81 @@ class Title(OneLineTag):
 
     tag_name = 'title'
 
-    def __init__(self, content=None):
-        Element.__init__(self, content)
-        self.content = []
-        if content:
-            self.content.append(content)
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+
+class A(OneLineTag):
+    '''A class for A tags and links'''
+
+    tag_name = 'a'
+
+    def __init__(self, link, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.kwargs['href'] = link
+
+
+class H(OneLineTag):
+    '''A class for A tags and links'''
+
+    def __init__(self, h_level, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+        self.tag_name = 'h{}'.format(h_level)
+
+
+class SelfClosingTag(Element):
+    '''A class for tags that appear individually'''
+
+    tag_name = ''
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+    def render(self, file_out, cur_ind=""):
+        element_format = style_tags(self.kwargs)
+        file_out.write("{}<{}{} />\n".format(cur_ind, self.tag_name, element_format))
+
+
+class Hr(SelfClosingTag):
+    '''A class for <hr /> tags'''
+
+    tag_name = 'hr'
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+
+class Br(SelfClosingTag):
+    '''A class for <hr /> tags'''
+
+    tag_name = 'br'
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+
+class Meta(SelfClosingTag):
+    '''A class for <hr /> tags'''
+
+    tag_name = 'meta'
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+
+class Ul(Element):
+    '''A class for an unordered list tag'''
+
+    tag_name = 'ul'
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
+
+
+class Li(Ul):
+    '''A class for a list item tag'''
+
+    tag_name = 'li'
+
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, **kwargs)
