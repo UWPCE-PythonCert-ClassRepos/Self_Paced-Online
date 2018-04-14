73ec64f8192708e33f34c8911f89a5c266739914
Dustin L <r.dustin.lane@gmail.com>
2018-04-13 22:25:13 -0700

    Mailroom - Add Donor class

--- students/Dustin_L/lesson09/mailroom4.py
+++ students/Dustin_L/lesson09/mailroom4.py
@@ -7,6 +7,49 @@ This module contains all of the functions for the updated Mail Room 4 module.
 import datetime
 from collections import defaultdict
 
+
+class Donor:
+    """Contains all information for a single donor"""
+    def __init__(self, name, donations=None):
+        if not donations:
+            donations = []
+
+        self.name = str(name)
+        self._donations = list(donations)
+        self._total = sum(self._donations)
+        self._ave = self._total / len(self._donations) if donations else 0
+
+    @property
+    def donations(self):
+        """Get list of donations for this donor"""
+        return self._donations
+
+    @property
+    def num_donations(self):
+        """Get number of donations given by this donor"""
+        return len(self._donations)
+
+    @property
+    def total_donations(self):
+        """Get total amount of donations given by this donor"""
+        return self._total
+
+    @property
+    def average_donations(self):
+        """Get average donation amount for this donor"""
+        return self._ave
+
+    def add_donation(self, amount):
+        """Add donation to this donor's donation history
+
+        Args:
+            donation (int): donation amount
+        """
+        self._donations.append(amount)
+        self._total += amount
+        self._ave = self._total / self.num_donations
+
+
 THANK_YOU_FMT = ('\nDear {:s},\n'
                  'Thank you for your generous donation of ${:.2f}.\n'
                  '\t\tSincerely,\n'
