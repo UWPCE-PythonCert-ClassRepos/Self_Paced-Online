79c55cfc884f6c59bc2dc2606f696d8cd7e049ce
Dustin L <r.dustin.lane@gmail.com>
2018-04-14 13:07:03 -0700

    Mailroom - Update unit tests to use Donor and DonorDatabase classes

--- students/Dustin_L/lesson09/test_mailroom4.py
+++ students/Dustin_L/lesson09/test_mailroom4.py
@@ -4,11 +4,12 @@
 import os
 import sys
 import datetime
+import random
 import unittest
+import mailroom4 as mr
 from unittest import mock
 from io import StringIO
-from collections import defaultdict
-import mailroom4 as mr
+from mailroom4 import Donor, DonorDatabase, THANK_YOU_FMT, PROMPT_OPTS
 
 
 def redirect_stdout():
@@ -28,14 +29,56 @@ def reset_stdout():
     sys.stdout = sys.__stdout__
 
 
-class TestMailRoom(unittest.TestCase):
+class TestDonor(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_name = 'Test Donor'
+        self.test_dons = [100, 500, 50.05]
+        self.test_total = sum(self.test_dons)
+        self.test_ave = self.test_total / len(self.test_dons)
 
     def setUp(self):
-        # Reset global donation database after each test case
-        mr.donor_db = defaultdict(lambda: mr.init_donor_data(),
-                                  {'Test Donor 1': mr.init_donor_data([1000, 5000, 10000]),
-                                   'Test Donor 2': mr.init_donor_data([12000, 5000, 27000]),
-                                   "Test Donor 3": mr.init_donor_data([38734, 6273, 67520])})
+        self.donor = Donor(self.test_name, self.test_dons)
+
+    def tearDown(self):
+        pass
+
+    def test_get_donations(self):
+        self.assertTrue(self.donor.donations == self.test_dons)
+
+    def test_get_num_donations(self):
+        self.assertTrue(self.donor.num_donations == len(self.test_dons))
+
+    def test_get_total_donations(self):
+        self.assertTrue(self.donor.total_donations == self.test_total)
+
+    def test_get_ave_donations(self):
+        self.assertTrue(self.donor.average_donations == self.test_ave)
+
+    def test_add_donation(self):
+        donation = random.randint(0, 10000.0)
+        self.donor.add_donation(donation)
+        self.assertTrue(self.donor.donations == self.test_dons + [donation])
+        self.assertTrue(self.donor.num_donations == len(self.test_dons) + 1)
+        self.assertTrue(self.donor.total_donations == self.test_total + donation)
+        self.assertTrue(self.donor.average_donations ==
+                        (self.test_total + donation) / (len(self.test_dons) + 1))
+
+
+class TestDonorDatabase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d1_name = 'Test Donor 1'
+        self.d2_name = 'Test Donor 2'
+        self.d3_name = 'Test Donor 3'
+        self.d1_dons = [1000, 5000, 10000]
+        self.d2_dons = [12000, 5000, 27000]
+        self.d3_dons = [38734, 6273, 67520]
+
+    def setUp(self):
+        self.db = DonorDatabase(*[Donor(self.d1_name, self.d1_dons),
+                                  Donor(self.d2_name, self.d2_dons),
+                                  Donor(self.d3_name, self.d3_dons)])
 
     def tearDown(self):
         # Remove any added thank you letters
@@ -44,38 +87,79 @@ class TestMailRoom(unittest.TestCase):
             if f.startswith('Test_Donor'):
                 os.remove(os.path.join(os.getcwd(), f))
 
-    def test_init_donor_data(self):
-        """Tests the init_donor_data() fxn"""
+    def test_add_donor(self):
+        """Test adding donor not currently in database"""
+        d4 = 'Test Donor 4'
+        self.assertTrue(self.db[d4].name == d4)
 
-        # Test with default parameter
-        ret_dict = mr.init_donor_data()
-        self.assertTrue(ret_dict[mr.GIFTS_KEY] == [],
-                        ret_dict[mr.GIFTS_KEY])
-        self.assertTrue(ret_dict[mr.NUM_GIFTS_KEY] == 0,
-                        ret_dict[mr.NUM_GIFTS_KEY])
-        self.assertTrue(ret_dict[mr.TOTAL_KEY] == 0,
-                        ret_dict[mr.TOTAL_KEY])
-        self.assertTrue(ret_dict[mr.AVE_KEY] == 0,
-                        ret_dict[mr.AVE_KEY])
+    def test_create_report(self):
+        """Test create_report() fxn"""
+        report_lines = self.db.create_report().split('\n')
 
-        # Test with argument
-        gifts = [5, 10, 20, 15]
-        ret_dict = mr.init_donor_data(gifts=gifts)
-        self.assertTrue(ret_dict[mr.GIFTS_KEY] == gifts,
-                        ret_dict[mr.GIFTS_KEY])
-        self.assertTrue(ret_dict[mr.NUM_GIFTS_KEY] == 4,
-                        ret_dict[mr.NUM_GIFTS_KEY])
-        self.assertTrue(ret_dict[mr.TOTAL_KEY] == 50,
-                        ret_dict[mr.TOTAL_KEY])
-        self.assertTrue(ret_dict[mr.AVE_KEY] == 12.5,
-                        ret_dict[mr.AVE_KEY])
+        self.assertTrue(report_lines[0] == '')
+        self.assertTrue(report_lines[1] == '   Donor Name     | Total Given  |  Num Gifts   | Average Gift')
+        self.assertTrue(report_lines[2] == '--------------------------------------------------------------')
+        self.assertTrue(report_lines[3] == 'Test Donor 3      | $  112527.00 |            3 | $   37509.00')
+        self.assertTrue(report_lines[4] == 'Test Donor 2      | $   44000.00 |            3 | $   14666.67')
+        self.assertTrue(report_lines[5] == 'Test Donor 1      | $   16000.00 |            3 | $    5333.33')
+
+    def test_send_letters(self):
+        """Test send_letters() fxn"""
+        now = datetime.datetime.today().strftime('%m-%d-%Y')
+        self.db.send_letters()
+        files = os.listdir()
+
+        # Check the file names are correct
+        self.assertTrue(f'Test_Donor_1_{now}.txt' in files)
+        self.assertTrue(f'Test_Donor_2_{now}.txt' in files)
+        self.assertTrue(f'Test_Donor_3_{now}.txt' in files)
+
+        # Check the file contents are correct
+        with open(f'Test_Donor_1_{now}.txt', 'r') as f:
+            contents = f.read(105)
+            self.assertTrue(contents == THANK_YOU_FMT.format(
+                'Test Donor 1', 16000))
+
+        with open(f'Test_Donor_2_{now}.txt', 'r') as f:
+            contents = f.read(105)
+            self.assertTrue(contents == THANK_YOU_FMT.format(
+                'Test Donor 2', 44000))
+
+        with open(f'Test_Donor_3_{now}.txt', 'r') as f:
+            contents = f.read(106)
+            self.assertTrue(contents == THANK_YOU_FMT.format(
+                'Test Donor 3', 112527))
+
+    def test_get_donor_names(self):
+        """Test reading all donor names from database"""
+        for i, name in enumerate(self.db):
+            self.assertTrue(name == f'Test Donor {i+1}')
+
+
+class TestUserInteraction(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d1_name = 'Test Donor 1'
+        self.d2_name = 'Test Donor 2'
+        self.d3_name = 'Test Donor 3'
+        self.d1_dons = [1000, 5000, 10000]
+        self.d2_dons = [12000, 5000, 27000]
+        self.d3_dons = [38734, 6273, 67520]
+
+    def setUp(self):
+        self.db = DonorDatabase(*[Donor(self.d1_name, self.d1_dons),
+                                  Donor(self.d2_name, self.d2_dons),
+                                  Donor(self.d3_name, self.d3_dons)])
+
+    def tearDown(self):
+        pass
 
     def test_get_usr_input(self):
-        """Tests the get_usr_input() fxn"""
+        """Test the get_usr_input() fxn"""
 
         # Test all valid user input prompts
         with mock.patch('builtins.input') as mock_input:
-            for prompt in mr.PROMPT_OPTS:
+            for prompt in PROMPT_OPTS:
                 mock_input.return_value = prompt
                 self.assertTrue(mr.get_usr_input() == prompt)
 
@@ -88,7 +172,7 @@ class TestMailRoom(unittest.TestCase):
 
             self.assertTrue(captured_print.getvalue() == (f'\nPlease try again. '
                                                           f'Valid options are: '
-                                                          f'{mr.PROMPT_OPTS}\n'))
+                                                          f'{PROMPT_OPTS}\n'))
 
         # Test valid input type, but not a valid option
         se = [-5, 1]
@@ -99,18 +183,9 @@ class TestMailRoom(unittest.TestCase):
 
             self.assertTrue(captured_print.getvalue() == (f'\nPlease select a '
                                                           f'number between '
-                                                          f'{mr.PROMPT_OPTS[0]}'
+                                                          f'{PROMPT_OPTS[0]}'
                                                           f' and '
-                                                          f'{mr.PROMPT_OPTS[-1]}\n'))
-
-    def test_get_donor_names(self):
-        """Test the get_donor_names() fxn"""
-        test_donor_db = {'Test Donor 1': 'na',
-                         'Test Donor 2': 'na',
-                         'Test Donor 3': 'na'}
-        with mock.patch.dict(mr.donor_db, test_donor_db, clear=True):
-            for i, name in enumerate(mr.get_donor_names()):
-                self.assertTrue(name == f'test donor {i+1}', name)
+                                                          f'{PROMPT_OPTS[-1]}\n'))
 
     def test_prompt_for_donor(self):
         """Test the prompt_for_donor() fxn"""
@@ -118,7 +193,7 @@ class TestMailRoom(unittest.TestCase):
         # Test when user enters a name
         with mock.patch('builtins.input') as mock_input:
             mock_input.return_value = 'mr. test donor'
-            result = mr.prompt_for_donor('')
+            result = mr.prompt_for_donor('', self.db)
 
             self.assertTrue(result == 'Mr. Test Donor')
 
@@ -126,27 +201,23 @@ class TestMailRoom(unittest.TestCase):
         with mock.patch('builtins.input') as mock_input:
             mock_input.return_value = 'quit'
             captured_print = redirect_stdout()
-            result = mr.prompt_for_donor('')
+            result = mr.prompt_for_donor('', self.db)
             reset_stdout()
 
             self.assertTrue(result is None)
             self.assertTrue(captured_print.getvalue() == '')
 
         # Test when user enters "list"
-        test_donor_db = {'Test Donor 1': 'na',
-                         'Test Donor 2': 'na',
-                         'Test Donor 3': 'na'}
         se = ['list', 'quit']
-        with mock.patch.dict(mr.donor_db, test_donor_db, clear=True):
-            with mock.patch('builtins.input', side_effect=se) as mock_input:
-                captured_print = redirect_stdout()
-                result = mr.prompt_for_donor('')
-                reset_stdout()
+        with mock.patch('builtins.input', side_effect=se) as mock_input:
+            captured_print = redirect_stdout()
+            result = mr.prompt_for_donor('', self.db)
+            reset_stdout()
 
-                self.assertTrue(result is None)
-                self.assertTrue(captured_print.getvalue() == ('\nTest Donor 1\n'
-                                                              'Test Donor 2\n'
-                                                              'Test Donor 3\n'))
+            self.assertTrue(result is None)
+            self.assertTrue(captured_print.getvalue() == (f'\n{self.d1_name}\n'
+                                                          f'{self.d2_name}\n'
+                                                          f'{self.d3_name}\n'))
 
     def test_prompt_for_donation(self):
         """Test the prompt_for_donation() fxn"""
@@ -172,30 +243,6 @@ class TestMailRoom(unittest.TestCase):
             self.assertTrue(result is None, msg=result)
             self.assertTrue(captured_print.getvalue() == '')
 
-    def test_add_donation(self):
-        """Test the add_donation() fxn"""
-        mr.donor_db = defaultdict(lambda: mr.init_donor_data(),
-                                  {'Test Donor 1': {},
-                                   'Test Donor 2': {}})
-
-        # Test adding new donor
-        test_results = {mr.GIFTS_KEY: [500],
-                        mr.NUM_GIFTS_KEY: 1,
-                        mr.TOTAL_KEY: 500,
-                        mr.AVE_KEY: 500}
-        mr.add_donation('Test Donor 3', 500)
-        self.assertTrue(mr.donor_db['Test Donor 3'] == test_results,
-                        msg=mr.donor_db['Test Donor 3'])
-
-        # Test adding to existing donor
-        test_results = {mr.GIFTS_KEY: [500, 1000],
-                        mr.NUM_GIFTS_KEY: 2,
-                        mr.TOTAL_KEY: 1500,
-                        mr.AVE_KEY: 750}
-        mr.add_donation('Test Donor 3', 1000)
-        self.assertTrue(mr.donor_db['Test Donor 3'] == test_results,
-                        mr.donor_db['Test Donor 3'])
-
     def test_send_thank_you(self):
         """Test send_thank_you() fxn"""
 
@@ -203,61 +250,19 @@ class TestMailRoom(unittest.TestCase):
         se = ['Bert', '1000']
         with mock.patch('builtins.input', side_effect=se):
             captured_print = redirect_stdout()
-            mr.send_thank_you()
+            mr.send_thank_you(self.db)
             reset_stdout()
 
             self.assertTrue(captured_print.getvalue() ==
-                            mr.THANK_YOU_FMT.format('Bert', 1000) + '\n')
-
-    def test_create_report(self):
-        """Test create_report() fxn"""
-        captured_print = redirect_stdout()
-        mr.create_report()
-        reset_stdout()
-        report_lines = captured_print.getvalue().split('\n')
-
-        self.assertTrue(report_lines[0] == '')
-        self.assertTrue(report_lines[1] == '   Donor Name     | Total Given  |  Num Gifts   | Average Gift')
-        self.assertTrue(report_lines[2] == '--------------------------------------------------------------')
-        self.assertTrue(report_lines[3] == 'Test Donor 3      | $  112527.00 |            3 | $   37509.00')
-        self.assertTrue(report_lines[4] == 'Test Donor 2      | $   44000.00 |            3 | $   14666.67')
-        self.assertTrue(report_lines[5] == 'Test Donor 1      | $   16000.00 |            3 | $    5333.33')
-        self.assertTrue(report_lines[6] == '')
+                            THANK_YOU_FMT.format('Bert', 1000) + '\n')
 
     def test_quit_mailroom(self):
         """Test quit_mailroom() fxn"""
         captured_print = redirect_stdout()
-        mr.quit_mailroom()
+        mr.quit_mailroom(self.db)
         reset_stdout()
 
         self.assertTrue(captured_print.getvalue() == 'Quitting mailroom...\n')
-
-    def test_send_letters(self):
-        """Test send_letters() fxn"""
-        now = datetime.datetime.today().strftime('%m-%d-%Y')
-        mr.send_letters()
-        files = os.listdir()
-
-        # Check the file names are correct
-        self.assertTrue(f'Test_Donor_1_{now}.txt' in files)
-        self.assertTrue(f'Test_Donor_2_{now}.txt' in files)
-        self.assertTrue(f'Test_Donor_3_{now}.txt' in files)
-
-        # Check the file contents are correct
-        with open(f'Test_Donor_1_{now}.txt', 'r') as f:
-            contents = f.read(105)
-            self.assertTrue(contents == mr.THANK_YOU_FMT.format(
-                'Test Donor 1', 16000))
-
-        with open(f'Test_Donor_2_{now}.txt', 'r') as f:
-            contents = f.read(105)
-            self.assertTrue(contents == mr.THANK_YOU_FMT.format(
-                'Test Donor 2', 44000))
-
-        with open(f'Test_Donor_3_{now}.txt', 'r') as f:
-            contents = f.read(106)
-            self.assertTrue(contents == mr.THANK_YOU_FMT.format(
-                'Test Donor 3', 112527))
 
 
 if __name__ == '__main__':
