2aae5d74a3e46ef0be64af4def4f04c33d4d2463
JeroenSweerts <jeroensweerts@hotmail.com>
2018-04-15 20:29:34 +0200

    Step1 to 5

--- students/JeroenSweerts/Lesson8/circle_class.py
+++ students/JeroenSweerts/Lesson8/circle_class.py
@@ -1,5 +1,23 @@
-
+import math
 
 class Circle():
-    def __init__(self, the_radius):
+    def __init__(self, the_radius=None):
         self.radius = the_radius
+
+    @property
+    def diameter(self):
+        return 2*self.radius
+
+    @diameter.setter
+    def diameter(self, val):
+        self.radius = val/2
+
+    @property
+    def area(self):
+        return self.radius*self.radius*math.pi
+
+    @classmethod
+    def from_diameter(circle, diameter):
+        self = circle()
+        self.diameter = diameter
+        return self
--- students/JeroenSweerts/Lesson8/test_circle_class.py
+++ students/JeroenSweerts/Lesson8/test_circle_class.py
@@ -7,7 +7,34 @@ class TestCircleClass(unittest.TestCase):
     def test_Step1(self):
         circle = cc.Circle(4)
         self.assertEqual(4, circle.radius)
-        
+
+    def test_Step2a(self):
+        circle = cc.Circle(4)
+        self.assertEqual(8, circle.diameter)
+
+    def test_Step2b(self):
+        circle = cc.Circle(4)
+        circle.radius = 8
+        self.assertEqual(16, circle.diameter)
+
+    def test_Step3a(self):
+        circle = cc.Circle(4)
+        circle.diameter = 2
+        self.assertEqual(1, circle.radius)
+
+    def test_Step4a(self):
+        circle = cc.Circle(2)
+        self.assertEqual(12, int(circle.area))
+
+    def test_Step4b(self):
+        circle = cc.Circle(2)
+        with self.assertRaises(AttributeError):
+            circle.area = 42
+
+    def test_Step5a(self):
+        circle = cc.Circle.from_diameter(8)
+        self.assertEqual(8, circle.diameter)
+        self.assertEqual(4, circle.radius)
 
 
 if __name__ == '__main__':
