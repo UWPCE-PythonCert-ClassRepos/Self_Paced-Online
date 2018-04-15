7995baaf4c89444d4bb8f4f74d963fc70b1247df
johnpharmd <jhumphre@uw.edu>
2018-04-15 11:45:33 -0700

    added render(cur_ind) to html_render.py

--- students/johnpharmd/lesson07/html_render.py
+++ students/johnpharmd/lesson07/html_render.py
@@ -2,7 +2,19 @@ class Element(object):
     """renders an html element"""
 
     tag = ''
-    indent = 0
+    indent = ''
 
     def __init__(self, content=None):
         self.content = content
+        self.content_list = [content]
+
+    def render(self, cur_ind=''):
+        return ('<{}>\n' + ' ' * int(cur_ind) + '{}\n'
+                + '<\\{}>').format(self.tag,
+                                   self.content, self.tag).strip('\'')
+
+
+if __name__ == '__main__':
+    e1 = Element()
+    e1.tag = 'html'
+    print(e1.render(cur_ind='4'))
