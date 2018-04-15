7d47b20b402e7e7b2c219e5479c2b6be83789e06
22e88 <22e88@mailbox.org>
2018-04-15 12:41:59 +0200

    Add initial set of files

---
+++ students/elmar_m/lesson07/test_html_render.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# file: test_html_render.py
+
+import unittest as ut
+import html_render as hr
+
+class mytests(ut.TestCase):
+    ''' Test if html_render provides a class named 'element': '''   
+    def test_element(self):
+        pass
+
+    ''' Test if class 'element' provides an attribute named 'tagname': '''
+    def test_tagname(self):
+        pass
+
+    ''' Test if class 'element' provides an attribute named 'indentation': '''
+    def test_indent(self):
+        pass
+    
+    ''' Test if class 'element' provides a method named 'append': '''
+    def test_append(self):
+        pass
+
+    ''' Test if class 'element' provides a method named 'render': '''
+    def test_render(self):
+        pass
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ut.main()
+
+
