64873f125516dfe22c6ce4c0cd8ae19eb2d065aa
johnpharmd <jhumphre@uw.edu>
2018-04-15 11:46:24 -0700

    added test_element_render() to htmlrendertest.py

--- students/johnpharmd/lesson07/htmlrendertest.py
+++ students/johnpharmd/lesson07/htmlrendertest.py
@@ -5,7 +5,21 @@ import html_render as hr
 class HTMLRenderTest(unittest.TestCase):
     """tests html_render output"""
 
+    def setUp(self):
+        self.test = hr.Element(content='')
+
     def test_element(self):
-        test = hr.Element(content=None)
-        self.assertEqual(test.tag, '')
-        self.assertEqual(test.indent, 0)
+        self.assertEqual(self.test.tag, '')
+        self.assertEqual(self.test.indent, '')
+        self.assertEqual(self.test.content, '')
+
+    def test_element_content(self):
+        self.test.content_list.append('new content')
+        self.assertEqual(self.test.content_list, ['', 'new content'])
+
+    def test_element_render(self):
+        self.test.content = 'None'
+        self.test.tag = 'html'
+        self.assertEqual(self.test.render(cur_ind='4'),
+                         ('<html>\n' + ' ' * int('4') + 'None\n'
+                          + '<\html>').strip('\''))
