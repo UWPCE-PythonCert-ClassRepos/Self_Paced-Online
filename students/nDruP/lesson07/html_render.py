416992db00f689db87dca97b9a1ee9d2e713da7c
Andrew Park <apark46.work@gmail.com>
2018-04-15 17:56:41 -0700

    Lesson07|html_render.py Finished Steps 6-8 and the output matches the output given on the site. I'll run it through the PEP8 checker and have another once over to check if there's anything I can refactor.

--- students/nDruP/lesson07/html_render.py
+++ students/nDruP/lesson07/html_render.py
@@ -1,18 +1,20 @@
 class Element:
     tag = "html"
-    indent = 0
+    indent = 1
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.substance = () if content is None else (content,)
+        self.attributes = kwargs
 
     def render(self, file_out, cur_ind=""):
-        file_out.write((self.indent*cur_ind)+'<'+self.tag+">\n")
+        file_out.write(self.open_tag_str(cur_ind)+'\n')
         for x in self.substance:
             if issubclass(type(x), Element):
                 x.indent = self.indent + 1
                 x.render(file_out, cur_ind)
-            elif type(x) is str:
+            elif x:
                 file_out.write((cur_ind*(self.indent+1))+str(x)+'\n')
+                
         file_out.write((self.indent*cur_ind)+"</"+self.tag+">\n")
 
     def append(self, text):
@@ -20,8 +22,48 @@ class Element:
         mutable_content.append(text)
         self.substance = tuple(mutable_content)
 
+    def open_tag_str(self, cur_ind=""):
+        token = (self.indent*cur_ind)+'<'+self.tag
+        if self.attributes:
+            for attr, value in self.attributes.items():
+                token += ' '+attr+'="'+value+'"'
+        token += ">"
+        return token
+
+
+class OneLineTag(Element):
+    def render(self, file_out, cur_ind=""):
+        file_out.write(self.open_tag_str(cur_ind) + ' ')
+        for x in self.substance:
+            file_out.write(str(x)+' ')
+        file_out.write("</"+self.tag+">\n")
+
+
+class SelfClosingTag(Element):
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+
+    def render(self, file_out, cur_ind=""):
+        file_out.write(self.open_tag_str(cur_ind)+'\n')
+
+    def open_tag_str(self, cur_ind=""):
+        token = (self.indent*cur_ind)+'<'+self.tag
+        if self.attributes:
+            for attr, value in self.attributes.items():
+                token += ' '+attr+'="'+value+'"'
+        token += " \>"
+        return token
+
 
 class Html(Element):
+    def render(self, file_out, cur_ind=""):
+        file_out.write(cur_ind*self.indent+"<!DOCTYPE html>\n")
+        Element.render(self, file_out, cur_ind)
+    pass
+
+
+class Head(Element):
+    tag = "head"
     pass
 
 
@@ -30,6 +72,48 @@ class Body(Element):
     pass
 
 
+class H(OneLineTag):
+    def __init__(self, size, content=None, **kwargs):
+        self.tag = 'h'+str(size)
+        OneLineTag.__init__(self, content, **kwargs)
+
+    
 class P(Element):
     tag = "p"
+    pass
+
+
+class Ul(Element):
+    tag = "ul"
+    pass
+
+
+class Li(Element):
+    tag = "li"
+    pass
+
+
+class Title(OneLineTag):
+    tag = "title"
+    pass
+
+
+class A(OneLineTag):
+    tag = "a"
+    def __init__(self, link, content):
+        self.attributes = {"href":link}
+        self.substance = () if content is None else (content,)        
+    pass
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+    pass
+
+class Br(SelfClosingTag):
+    tag = "br"
+    pass
+
+
+class Meta(SelfClosingTag):
+    tag = "meta"
     pass
