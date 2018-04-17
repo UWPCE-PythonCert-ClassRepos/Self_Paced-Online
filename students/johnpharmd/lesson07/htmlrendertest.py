8dce651c0b50ebe202942a516fc0b4728dca5cfe
johnpharmd <jhumphre@uw.edu>
2018-04-17 04:48:13 -0700

    refactored with html_render

--- students/johnpharmd/lesson07/htmlrendertest.py
+++ students/johnpharmd/lesson07/htmlrendertest.py
@@ -3,23 +3,23 @@ import html_render as hr
 
 
 class HTMLRenderTest(unittest.TestCase):
-    """tests html_render output"""
+    """tests html_render attributes"""
 
     def setUp(self):
         self.test = hr.Element(content='')
 
     def test_element(self):
         self.assertEqual(self.test.tag, '')
-        self.assertEqual(self.test.indent, '')
+        self.assertEqual(self.test.indent, 1)
         self.assertEqual(self.test.content, '')
 
-    def test_element_content(self):
-        self.test.content_list.append('new content')
-        self.assertEqual(self.test.content_list, ['', 'new content'])
+    def test_element_append(self):
+        self.test.append('new content')
+        self.assertEqual(self.test.content, ' new content')
 
     def test_element_render(self):
         self.test.content = 'None'
         self.test.tag = 'html'
-        self.assertEqual(self.test.render(cur_ind='4'),
-                         ('<html>\n' + ' ' * int('4') + 'None\n'
-                          + '<\html>').strip('\''))
+        self.assertEqual(self.test.render('file.txt', cur_ind=''),
+                         ('<html>\n' + self.test.indent * '' +
+                          'None\n' + '<\html>').strip('\''))
