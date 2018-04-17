e764a64932c53562ba82f26f7150e4fcdd9addad
James Traub <traub01-devtrain@outlook.com>
2018-04-17 01:17:39 -0700

    Completed task 5 of 6 for Lesson 3 Lab 3

--- students/JamesTraub/lesson03/strformat_lab.py
+++ students/JamesTraub/lesson03/strformat_lab.py
@@ -27,5 +27,18 @@
 # arb_val(1,2,3,4,5)
 
 
-# Task 4 - Given a 5 element tuple: (4, 30, 2017, 2, 27)
-#          use string formatting to print: '02 27 2017 04 30'
+""" Task 4 - Given a 5 element tuple: ( 4, 30, 2017, 2, 27)
+    use string formatting to print: '02 27 2017 04 30' """
+# cinco = ( 4, 30, 2017, 2, 27)
+# print("{c3:0>2d} {c4:0>2d} {c2:0>2d} {c0:0>2d} {c1:0>2d}".format(c0=cinco[0], c1=cinco[1], c2=cinco[2], c3=cinco[3], c4=cinco[4]))
+
+
+""" Task 5 - Given a 4 element tuple: ['oranges', 1.3, 'lemons', 1.1]
+    use string formatting to print: The weight of an orange is 1.3 and the weight of a lemon is 1.1 """
+# orange = ("orange", 1.3)
+# lemon = ("lemon", 1.1)
+# print(f"The weight of an {orange[0]} is {orange[1]} and the weight of a {lemon[0]} is {lemon[1]}")
+# print(f"The weight of an {orange[0].upper()} is {orange[1] * 1.2} and the weight of a {lemon[0].upper()} is {lemon[1] * 1.2}")
+
+
+""" Task 6 - 
