8daf6167985475e236a219ba8aad4392d5670653
johnpharmd <jhumphre@uw.edu>
2018-04-21 13:53:47 -0700

    made several changes, committed prior to requesting instructor feedback

--- students/johnpharmd/lesson07/html_render.py
+++ students/johnpharmd/lesson07/html_render.py
@@ -2,20 +2,49 @@ class Element(object):
     """renders an html element"""
 
     tag = ''
-    indent = 1
+    indent = '    '
 
     def __init__(self, content=''):
-        self.content = content
-        self.content_list = ['<' + self.tag + '>', self.content,
-                             '<\\' + self.tag + '>']
+        self.open_tag = '<' + self.tag + '>'
+        self.close_tag = '</' + self.tag + '>'
+        self.content = [content]
 
     def append(self, new_string):
-        self.content = ' '.join([self.content, new_string])
-        return self.content
+        # self.content = '<br>'.join([self.content, new_string])
+        return self.content.append(new_string)
 
     def render(self, file_out, cur_ind=''):
-        output = (('<{}>\n' + self.indent * cur_ind + '{}\n'
-                  + '<\\{}>').format(self.tag, self.content,
-                                     self.tag).strip('\''))
-        # https://stackoverflow.com/questions/4369159/replace-n-with-br
-        file_out.write(output.replace('\n', '<br />'))
+        file_out.write(self.open_tag)
+        print('open_tag ==', self.open_tag)
+        print('self.content ==', self.content)
+        for l in self.content:
+            for s in l:
+                file_out.write(cur_ind + s + '\n')
+        file_out.write(self.close_tag)
+
+
+class Html(Element):
+    """renders html"""
+
+    def __init__(self, content=''):
+        self.open_tag = '<html>'
+        self.close_tag = '</html>'
+        self.content = [content]
+
+
+class Body(Element):
+    """renders body"""
+
+    def __init__(self, content=''):
+        self.open_tag = '<body>'
+        self.close_tag = '</body>'
+        self.content = [content]
+
+
+class P(Element):
+    """renders p"""
+
+    def __init__(self, content=''):
+        self.open_tag = '<p>'
+        self.close_tag = '</p>'
+        self.content = [content]
