25210c7045f1cef5f23de5c0ac33a8d0fb973e69
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-04-21 13:13:47 -0700

    Test default content and append values.

--- students/stuartkershaw/lesson07/html_render.py
+++ students/stuartkershaw/lesson07/html_render.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
 
-class Element:
-    pass
+class Element():
+    tag_name = 'html'
+    indentation = 2
+
+    def __init__(self, content):
+        self.content = [] if content is None else [content]
+
+    def append(self, new_content):
+        self.content.append(new_content)
--- students/stuartkershaw/lesson07/test_html_render.py
+++ students/stuartkershaw/lesson07/test_html_render.py
@@ -2,5 +2,22 @@ from html_render import Element
 
 
 def test_type_Element():
-    test_element = Element()
+    test_element = Element(content=None)
     assert isinstance(test_element, Element) is True
+
+
+def test_class_attributes_Element():
+    test_element = Element(content=None)
+    assert test_element.tag_name == 'html'
+    assert test_element.indentation == 2
+
+
+def test_instance_attributes_Element():
+    test_element = Element(content=None)
+    assert test_element.content == []
+
+
+def test_append_content_Element():
+    test_element = Element(content=None)
+    test_element.append('New content')
+    assert test_element.content == ['New content']
