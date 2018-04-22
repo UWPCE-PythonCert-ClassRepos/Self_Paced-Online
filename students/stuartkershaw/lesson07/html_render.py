7b3c1b756b06e602d923b88f57873460027e1bb7
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-04-21 18:45:30 -0700

    Test Step 1 creating root Element.

--- students/stuartkershaw/lesson07/html_render.py
+++ students/stuartkershaw/lesson07/html_render.py
@@ -5,8 +5,19 @@ class Element():
     tag_name = 'html'
     indentation = 2
 
-    def __init__(self, content):
+    def __init__(self, content=None):
         self.content = [] if content is None else [content]
+        self.children = []
 
     def append(self, new_content):
         self.content.append(new_content)
+
+    def render(self, file_out, cur_ind=""):
+        self.file_out = file_out
+        self.cur_ind = cur_ind
+
+        self.file_out.write(
+            f"<{self.tag_name}>\n"
+            f"{' '.join(self.content)}\n"
+            f"</{self.tag_name}>"
+        )
--- students/stuartkershaw/lesson07/test_html_render.py
+++ students/stuartkershaw/lesson07/test_html_render.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from html_render import Element
 
 
@@ -21,3 +22,13 @@ def test_append_content_Element():
     test_element = Element(content=None)
     test_element.append('New content')
     assert test_element.content == ['New content']
+
+
+def test_render_page_Element():
+    test_element = Element(content="Some content.")
+    test_element.append('Some more content.')
+
+    f = StringIO()
+    test_element.render(f, "    ")
+
+    assert f.getvalue() == '<html>\nSome content. Some more content.\n</html>'
