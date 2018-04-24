78067e9fa81146e0b695b9d22358314656f39427
csdotson <csdotson1@gmail.com>
2018-04-24 15:39:09 -0700

    Redo step1 using a list to collect content

--- students/csdotson/lesson07/html_render.py
+++ students/csdotson/lesson07/html_render.py
@@ -1,35 +1,46 @@
 class Element:
     """Create an Element class for rendering an html element"""
-    tag_name = 'html'
-    indentation = "    "
+    tag_name = ''
+    indentation = ""
 
     def __init__(self, content=None):
-        self.content = content
+        self.content = [content] if content else []
 
     def append(self, content_to_add):
-        if self.content == None:
-            self.content = content_to_add
-        else:
-            self.content += (" " + content_to_add)
+        self.content.append(content_to_add)
 
     def render(self, file_out, cur_ind=""):
-        tag_details = {'opening': "<"+self.tag_name+">", 'closing': "</"+self.tag_name+">", 'content': self.content}
-        html_output = "{opening}\n{content}\n{closing}".format(**tag_details)
-        file_out.write(html_output)
+        opening_tag = "<"+self.tag_name+">\n"
+        closing_tag = "</"+self.tag_name+">"
+        output = opening_tag
+        for elem in self.content:
+            output += (elem + "\n")
+        output += closing_tag
+        file_out.write(output)
 
 
-class HtmlElement(Element):
+class Html(Element):
     tag_name = 'html'
 
 
-class BodyElement(Element):
+
+class Body(Element):
     tag_name = 'body'
 
 
-class ParaElement(Element):
+
+class P(Element):
     tag_name = 'p'
 
 
-
-# An HTML element usually consists of a start tag and end tag, with the content inserted in between
-# <tagname>Content goes here...</tagname>
+# class TextWrapper:
+#     """
+#     A simple wrapper that creates a class with a render method
+#     for simple text
+#     """
+#     def __init__(self,text):
+#         self.text = text
+#
+#      def render(self, file_out, current_ind=""):
+#         file_out.write(current_ind)
+#         file_out.write(self.text)
