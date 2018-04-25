996739005f29e4cae859157a492ab97dfd735a7e
22e88 <22e88@mailbox.org>
2018-04-25 09:37:20 +0200

    Add tests for Html, Body, P

--- students/elmar_m/lesson07/test_html_render.py
+++ students/elmar_m/lesson07/test_html_render.py
@@ -9,37 +9,38 @@ class mytests(ut.TestCase):
 
     '''
         Test if html_render provides a class named 'Element'    
-        provides an attribute named 'tag' 
+        with certain attributes
     '''
     def test_element(self):
-        # obj = hr.Element('html', 0)
+        self.assertTrue(issubclass(hr.Element, object))
         obj = hr.Element()
         self.assertIsInstance(obj, hr.Element)
         self.assertTrue(hasattr(obj, 'tag'))
-        self.assertTrue(hasattr(obj, 'indent'))
+        self.assertTrue(hasattr(obj, 'i_level'))
         self.assertTrue(hasattr(obj, 'content'))
         self.assertTrue(hasattr(obj, 'append'))
         self.assertTrue(hasattr(obj, 'render'))
 
-   # ''' Test if class 'element' provides an attribute named 'tag': '''
-   # def test_tagname(self):
-   #     # pass
-   #     obj = hr.Element()
-   #     self.assertTrue(hasattr(obj, 'tag'))
-
-   #  ''' Test if class 'element' provides an attribute named 'indentation': '''
-   #  def test_indent(self):
-   #      pass
-   #  
-   #  ''' Test if class 'element' provides a method named 'append': '''
-   #  def test_append(self):
-   #      pass
-
-   #  ''' Test if class 'element' provides a method named 'render': '''
-   #  def test_render(self):
-   #      pass
     
+    def test_Html(self):
+        self.assertTrue(issubclass(hr.Html, hr.Element))
+        obj = hr.Html()
+        self.assertIsInstance(obj, hr.Html)
+        self.assertIsInstance(obj, hr.Element)
 
+    
+    def test_Body(self):
+        self.assertTrue(issubclass(hr.Body, hr.Element))
+        obj = hr.Body()
+        self.assertIsInstance(obj, hr.Body)
+        self.assertIsInstance(obj, hr.Element)
+
+
+    def test_P(self):
+        self.assertTrue(issubclass(hr.P, hr.Element))
+        obj = hr.P()
+        self.assertIsInstance(obj, hr.P)
+        self.assertIsInstance(obj, hr.Element)
 
 if __name__ == '__main__':
     ut.main()
