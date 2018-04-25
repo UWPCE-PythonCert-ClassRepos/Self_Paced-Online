31d38e95459aa9906426ac32bbf8699110b9dff5
22e88 <22e88@mailbox.org>
2018-04-25 10:00:15 +0200

    Add printing of <!DOCTYPE html>

--- students/elmar_m/lesson07/html_render.py
+++ students/elmar_m/lesson07/html_render.py
@@ -5,10 +5,10 @@
 # define class 'Element':
 class Element:
 
+    # define attributes:
     tag = 'element'
     i_level = 0
 
-    # define attributes:
     def __init__(self, content=None):
         self.content = [content] if content else []
     
@@ -17,13 +17,16 @@ class Element:
         self.content.append(stuff)
 
     # define method render:
-    # def render(self, io, cur_ind=''):
     def render(self, io, ind):
-        io.write('<{}>\n'.format(self.tag))
+        # io.write('<{}>\n'.format(self.tag))
+        if self.tag == 'html':
+            io.write('<!DOCTYPE html>\n')
+            io.write('<{}>\n'.format(self.tag))
+        else:
+            io.write('<{}>\n'.format(self.tag))
+            
         for i in self.content:
-            # if isinstance(i, Element):
             if hasattr(i, 'render'):
-                # i.render(io)
                 i.render(io, ind)
             else:
                 line = '{}{}\n'.format(ind, i)
@@ -32,28 +35,13 @@ class Element:
         
 
 class Html(Element):
-    # self.tag = 'html'
     tag = 'html'
-    #def __init__(self, tagtype='html', i_level=1, content=None):
-    #    self.tag = tagtype
-    #    self.indent = i_level
-    #    self.content = [content] if content else []
 
 
 class Body(Element):
-    # self.tag = 'body'
     tag = 'body'
-    #def __init__(self, tagtype='body', i_level=2, content=None):
-    #    self.tag = tagtype
-    #    self.indent = i_level
-    #    self.content = [content] if content else []
 
 
 class P(Element):
-    # self.tag = 'p'
     tag = 'p'
-    #def __init__(self, tagtype='p', i_level=3, content=None):
-    #    self.tag = tagtype
-    #    self.indent = i_level
-    #    self.content = [content] if content else []
 
