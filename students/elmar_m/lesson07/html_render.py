e38187e63a1fbbdd048b5c18d3c51611f38492d5
22e88 <22e88@mailbox.org>
2018-04-26 10:16:39 +0200

    Add working version step 4 html_render.py

--- students/elmar_m/lesson07/html_render.py
+++ students/elmar_m/lesson07/html_render.py
@@ -4,24 +4,15 @@
 
 class Element:
     tag = 'element'
-
-    # def __init__(self, content=None):       # wird erzeugt und kann schon content uebergeben 
-    #                                         # bekommen, siehe Step2 body.append(hr.P("blabla...")
-    def __init__(self, content=None, **kwargs):
+    def __init__(self, content=None):       # wird erzeugt und kann schon content uebergeben 
+                                            # bekommen, siehe Step2 body.append(hr.P("blabla...")
         self.content = [content] if content else []
-        # self.look = look
-        if 'style' in kwargs.keys():
-            self.look = kwargs['style']
-        else:
-            self.look = None
-    
+
     def append(self, stuff):
         self.content.append(stuff)
 
     def render(self, io, ind):
-        # io.write('<{}>\n'.format(self.tag))
-        io.write('<{} style={}>\n'.format(self.tag, self.look))
-            
+        io.write('<{}>\n'.format(self.tag))
         for i in self.content:
             if hasattr(i, 'render'):
                 i.render(io, ind)
@@ -34,23 +25,30 @@ class Element:
 class Html(Element):
     tag = 'html'
 
-
 class Body(Element):
     tag = 'body'
 
-
 class P(Element):
     tag = 'p'
+    def __init__(self, content=None, **kwargs):   
+        self.content = [content] if content else []
+        if 'style' in kwargs.keys():
+            s = kwargs['style']
+            self.style = str('style="' + s + '"')
+
+    def render(self, io, ind):
+        io.write('<{} {}>\n'.format(self.tag, self.style))
+        for i in self.content:
+                line = '{}{}\n'.format(ind, i)
+                io.write(line)
+        io.write('</{}>\n'.format(self.tag))
 
 
 class Head(Element):
     tag = 'head'
 
-
 class Title(Element):
     tag = 'title'
-
-
 
 class OneLineTag(Element):
     pass 
