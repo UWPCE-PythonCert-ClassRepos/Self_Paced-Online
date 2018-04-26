84538adafbe5b99d03675b063978b3152a7decbc
csdotson <csdotson1@gmail.com>
2018-04-26 07:25:00 -0700

    Finish core logic for step2 - handling reder of text, etc

--- students/csdotson/lesson07/html_render.py
+++ students/csdotson/lesson07/html_render.py
@@ -1,46 +1,49 @@
 class Element:
     """Create an Element class for rendering an html element"""
-    tag_name = ''
+    tag_name = "html"
     indentation = ""
 
     def __init__(self, content=None):
         self.content = [content] if content else []
 
     def append(self, content_to_add):
-        self.content.append(content_to_add)
+        if isinstance(content_to_add, str):
+            self.content.append(TextWrapper(content_to_add))
+        else:
+            self.content.append(content_to_add)
 
-    def render(self, file_out, cur_ind=""):
-        opening_tag = "<"+self.tag_name+">\n"
-        closing_tag = "</"+self.tag_name+">"
-        output = opening_tag
+    def render(self, file_out="f", cur_ind=""):
+        print("<"+self.tag_name+">")
         for elem in self.content:
-            output += (elem + "\n")
-        output += closing_tag
-        file_out.write(output)
+            if isinstance(elem, str):
+                elem = TextWrapper(elem)
+            elem.render()
+        print("</"+self.tag_name+">")
+        # file_out.write(output)
+        # print(output)
 
 
 class Html(Element):
     tag_name = 'html'
 
 
-
 class Body(Element):
     tag_name = 'body'
-
 
 
 class P(Element):
     tag_name = 'p'
 
 
-# class TextWrapper:
-#     """
-#     A simple wrapper that creates a class with a render method
-#     for simple text
-#     """
-#     def __init__(self,text):
-#         self.text = text
-#
-#      def render(self, file_out, current_ind=""):
-#         file_out.write(current_ind)
-#         file_out.write(self.text)
+class TextWrapper:
+    """
+    A simple wrapper that creates a class with a render method
+    for simple text
+    """
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, file_out="f", cur_ind=""):
+        print(self.text)
+        # file_out.write(cur_ind)
+        # file_out.write(self.text)
