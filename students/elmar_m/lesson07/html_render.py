b64df88253f956e311d7ea3c79a30e56ac71735f
22e88 <22e88@mailbox.org>
2018-04-26 18:58:32 +0200

    Add working version Step 7 html_render.py

--- students/elmar_m/lesson07/html_render.py
+++ students/elmar_m/lesson07/html_render.py
@@ -8,9 +8,9 @@ Lesson07: HTML renderer
 
 class Element:
     tag = 'element'
-    def __init__(self, content=None):       # wird erzeugt und kann schon content uebergeben 
-                                            # bekommen, siehe Step2 body.append(hr.P("blabla...")
-        self.content = [content] if content else []
+
+    def __init__(self, content=None):       # wird erzeugt und kann schon content uebergeben bekommen, 
+        self.content = [content] if content else []  #  siehe Step2 body.append(hr.P("blabla...")
 
     def append(self, stuff):
         self.content.append(stuff)
@@ -32,8 +32,10 @@ class Html(Element):
         io.write('<!DOCTYPE html>\n')
         Element.render(self, io, ind)
 
+
 class Body(Element):
     tag = 'body'
+
 
 class P(Element):
     tag = 'p'
@@ -42,7 +44,6 @@ class P(Element):
         if 'style' in kwargs.keys():
             s = kwargs['style']
             self.style = str('style="' + s + '"')
-
     def render(self, io, ind):
         io.write('<{} {}>\n'.format(self.tag, self.style))
         for i in self.content:
@@ -54,19 +55,26 @@ class P(Element):
 class Head(Element):
     tag = 'head'
 
+
 class Title(Element):
     tag = 'title'
+    def __init__(self, text):
+        self.text = text
+    def render(self, io, ind):
+        io.write('<{}>{}</{}>\n'.format(self.tag, self.text, self.tag))
+
 
 class SelfClosingTag(Element):
     def __init__(self):     # if erroneously given a second argument ('content'), 
         pass                # a TypeError will be raised
-
     def render(self, io, ind):
         closing = '/'
         io.write('<{} {}>\n'.format(self.tag, closing))
 
+
 class Hr(SelfClosingTag):
     tag = 'hr'
+
 
 class A(Element):
     tag = 'a'
@@ -77,4 +85,51 @@ class A(Element):
 
     def render(self, io, ind):
         io.write('{}<{} {}>{}</{}>\n'.format(ind, self.tag, self.targetstring, self.link, self.tag))
+
+
+# class OneLineTag(Element):
+#     def render(self, io, ind):
+#         io.write('{}<{} {}>{}</{}>\n'.format(ind, self.tag, self.targetstring, self.link, self.tag))
+#         io.write('<{}>\n'.format(self.tag))
+#                 line = '{}{}\n'.format(ind, i)
+#                 io.write(line)
+#         io.write('</{}>\n'.format(self.tag))
+
+class H(Element):
+    tag = 'h'
+    def __init__(self, size, text):
+        self.size = size
+        self.text = text
+    def render(self, io, ind):
+        io.write('<{}{}>{}</{}{}>\n'.format(self.tag, self.size, self.text, self.tag, self.size))
+
+
+class Ul(Element):
+    tag = 'ul'
+    def __init__(self, content=None, **kwargs):       
+        self.content = [content] if content else []
+        props = []
+        for key, value in kwargs.items():
+            props.append('{}="{}"'.format(key,value))
+        self.propstring = ' '.join(props)
+
+    def append(self, stuff):
+        self.content.append(stuff)
+
+    def render(self, io, ind):
+        io.write('<{} {}>\n'.format(self.tag, self.propstring))
+        for i in self.content:
+            if hasattr(i, 'render'):
+                i.render(io, ind)
+            else:
+                line = '{}{}\n'.format(ind, i)
+                io.write(line)
+        io.write('</{}>\n'.format(self.tag))
+ 
+
+# class Li(Element):
+class Li(Ul):
+    tag = 'li'
+    # def __init__(self, content=None, **kwargs):       
+    #     self.content = [content] if content else []
 
