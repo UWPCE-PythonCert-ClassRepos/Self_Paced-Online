db8c1bc2f4c416138963072f4a656bb9a3095ba6
22e88 <22e88@mailbox.org>
2018-04-27 14:23:50 +0200

    Add tests in test_html_render.py

--- students/elmar_m/lesson07/test_html_render.py
+++ students/elmar_m/lesson07/test_html_render.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-# file: test_html_render.py
+'''
+file: test_html_render.py
+elmar_m / 22e88@mailbox.org
+Lesson07: unittests for HTML renderer
+'''
 
 import unittest as ut
 import html_render as hr
@@ -16,10 +20,16 @@ class mytests(ut.TestCase):
         obj = hr.Element()
         self.assertIsInstance(obj, hr.Element)
         self.assertTrue(hasattr(obj, 'tag'))
-        self.assertTrue(hasattr(obj, 'i_level'))
+        self.assertTrue(obj.tag == 'element')
         self.assertTrue(hasattr(obj, 'content'))
         self.assertTrue(hasattr(obj, 'append'))
         self.assertTrue(hasattr(obj, 'render'))
+        self.assertTrue(hasattr(obj, 'props'))
+        self.assertTrue(type(obj.props) == list)
+        self.assertTrue(len(obj.props) == 0)
+        self.assertTrue(hasattr(obj, 'propstring'))
+        self.assertTrue(type(obj.propstring) == str)
+        self.assertTrue(len(obj.propstring) == 0)
 
     
     def test_Html(self):
@@ -27,6 +37,7 @@ class mytests(ut.TestCase):
         obj = hr.Html()
         self.assertIsInstance(obj, hr.Html)
         self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'html')
 
     
     def test_Body(self):
@@ -34,6 +45,7 @@ class mytests(ut.TestCase):
         obj = hr.Body()
         self.assertIsInstance(obj, hr.Body)
         self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'body')
 
 
     def test_P(self):
@@ -41,6 +53,111 @@ class mytests(ut.TestCase):
         obj = hr.P()
         self.assertIsInstance(obj, hr.P)
         self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'p')
+        self.assertTrue(hasattr(obj, 'props'))
+        self.assertTrue(type(obj.props) == list)
+        self.assertTrue(len(obj.props) == 0)
+        self.assertTrue(hasattr(obj, 'propstring'))
+        self.assertTrue(type(obj.propstring) == str)
+        self.assertTrue(len(obj.propstring) == 0)
+
+
+    def test_Head(self):
+        self.assertTrue(issubclass(hr.Head, hr.Element))
+        obj = hr.Head()
+        self.assertIsInstance(obj, hr.Head)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'head')
+
+    def test_Title(self):
+        self.assertTrue(issubclass(hr.Title, hr.Element))
+        obj = hr.Title('this is a test')
+        self.assertIsInstance(obj, hr.Title)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'title')
+        self.assertTrue(hasattr(obj, 'render'))
+
+
+    def test_SelfClosingTag(self):
+        self.assertTrue(issubclass(hr.SelfClosingTag, hr.Element))
+        obj = hr.SelfClosingTag()
+        self.assertIsInstance(obj, hr.SelfClosingTag)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(hasattr(obj, 'render'))
+    
+
+    def test_Meta(self):
+        self.assertTrue(issubclass(hr.Meta, hr.SelfClosingTag))
+        obj = hr.Meta()
+        self.assertIsInstance(obj, hr.Meta)
+        self.assertIsInstance(obj, hr.SelfClosingTag)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'meta')
+        self.assertTrue(hasattr(obj, 'props'))
+        self.assertTrue(type(obj.props) == list)
+        self.assertTrue(len(obj.props) == 0)
+        self.assertTrue(hasattr(obj, 'propstring'))
+        self.assertTrue(type(obj.propstring) == str)
+        self.assertTrue(len(obj.propstring) == 0)
+
+
+    def test_Hr(self):
+        self.assertTrue(issubclass(hr.Hr, hr.SelfClosingTag))
+        obj = hr.Hr()
+        self.assertIsInstance(obj, hr.Hr)
+        self.assertIsInstance(obj, hr.SelfClosingTag)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'hr')
+
+
+    def test_A(self):
+        self.assertTrue(issubclass(hr.A, hr.Element))
+        obj = hr.A('https://www.uw.edu', 'university')
+        self.assertIsInstance(obj, hr.A)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'a')
+        self.assertTrue(hasattr(obj, 'render'))
+
+
+    def test_H(self):
+        self.assertTrue(issubclass(hr.H, hr.Element))
+        obj = hr.H(2, 'headertext')
+        self.assertIsInstance(obj, hr.H)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'h')
+        self.assertTrue(type(obj.size) == int)
+        self.assertTrue(type(obj.text) == str)
+        self.assertTrue(hasattr(obj, 'render'))
+
+
+    def test_Ul(self):
+        self.assertTrue(issubclass(hr.Ul, hr.Element))
+        obj = hr.Ul()
+        self.assertIsInstance(obj, hr.Ul)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'ul')
+        self.assertTrue(hasattr(obj, 'props'))
+        self.assertTrue(type(obj.props) == list)
+        self.assertTrue(len(obj.props) == 0)
+        self.assertTrue(hasattr(obj, 'propstring'))
+        self.assertTrue(type(obj.propstring) == str)
+        self.assertTrue(len(obj.propstring) == 0)
+
+
+    def test_Li(self):
+        self.assertTrue(issubclass(hr.Li, hr.Ul))
+        obj = hr.Li()
+        self.assertIsInstance(obj, hr.Li)
+        self.assertIsInstance(obj, hr.Ul)
+        self.assertIsInstance(obj, hr.Element)
+        self.assertTrue(obj.tag == 'li')
+        self.assertTrue(hasattr(obj, 'props'))
+        self.assertTrue(type(obj.props) == list)
+        self.assertTrue(len(obj.props) == 0)
+        self.assertTrue(hasattr(obj, 'propstring'))
+        self.assertTrue(type(obj.propstring) == str)
+        self.assertTrue(len(obj.propstring) == 0)
+
 
 if __name__ == '__main__':
     ut.main()
