c619bbef8605b708e666ee07557246701a7f131b
22e88 <22e88@mailbox.org>
2018-04-28 07:18:08 +0200

    Add comments, try things in test_circle.py

--- students/elmar_m/lesson08/test_circle.py
+++ students/elmar_m/lesson08/test_circle.py
@@ -11,12 +11,23 @@ import circle as ci
 
 class mytests(ut.TestCase):
     
-    def test_Circle(self):
+    def test_Circle_basic(self):
+        '''
+        Tests the following:
+        - possibility to create an object from class ci.Circle
+        - if the resulting object has an attribute 'radius'
+        - is the value of 'radius' according to the argument given at initialization
+        TODO:
+        - test if initialization with something else than a numerical value (int, float)
+          raises an exception
+        '''
         obj = ci.Circle(4)
         self.assertIsInstance(obj, ci.Circle)
         self.assertTrue(hasattr(obj, 'radius'))
-        self.assertTrue(obj.radius 
         self.assertEqual(obj.radius, 4)
+
+        # obj = ci.Circle('abc')
+        
 
 
 if __name__ == '__main__':
