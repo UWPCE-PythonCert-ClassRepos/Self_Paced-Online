5241249d24656c57cbec72a9ae8eafac1c535d57
johnpharmd <jhumphre@uw.edu>
2018-04-28 13:27:53 -0700

    began refactoring render() for Step 5

--- students/johnpharmd/lesson07/html_render.py
+++ students/johnpharmd/lesson07/html_render.py
@@ -1,9 +1,10 @@
+
+
 class Element():
     """renders an html element"""
 
     tag = ''
     indent = '    '
-    attrs = {'text-align': '', 'font-style': ''}
 
     def __init__(self, content=None, **kwargs):
         self.content = [content]
@@ -16,17 +17,9 @@ class Element():
         if self.kwargs:
             kwargs_string = ' '
             self.open_tag = '<' + self.tag
-            # kwargs_keys = [key for key in self.kwargs]
-            # kwargs_values = [self.kwargs[key] for key in self.kwargs]
-            
-            # for tup in zip(kwargs_keys, kwargs_values):
-            #     for item in tup:
-            #         kwargs_string += str(item)
-            
             for k, v in self.kwargs.items():
-                kwargs_string += k + '=' +  '\'' + v + '\''
-
-            self.open_tag = self.open_tag + kwargs_string + '>'
+                kwargs_string += k + '=' + '\'' + v + '\''
+            self.open_tag += kwargs_string + '>'
         else:
             self.open_tag = '<' + self.tag + '>'
         self.close_tag = '</' + self.tag + '>'
@@ -38,15 +31,16 @@ class Element():
             if item is None:
                 file_out.write('')
             elif type(item) == str:
-                file_out.write(cur_ind * 3 + self.open_tag + '\n')
-                file_out.write(cur_ind * 4 + item + '\n')
-                file_out.write(cur_ind * 3 + self.close_tag + '\n')
+                file_out.write(cur_ind * 3 + self.open_tag + '\n' +
+                               cur_ind * 4 + item + '\n' +
+                               cur_ind * 3 + self.close_tag + '\n')
             else:
                 item.render(file_out, cur_ind)
         if self.tag != 'html' and self.tag != 'p':
             file_out.write(cur_ind * 2 + self.close_tag + '\n')
         elif self.tag == 'html':
             file_out.write(cur_ind + self.close_tag)
+        return self.open_tag, self.content, self.close_tag
 
 
 class Html(Element):
@@ -69,6 +63,38 @@ class Head(Element):
     tag = 'head'
 
 
+class A(Element):
+    """renders an anchor"""
+    tag = 'a'
+
+    def __init__(self, link='', content=None):
+        self.link = link
+        self.content = [content]
+        super().__init__()
+
+
+class SelfClosingTag(Element):
+    """renders a selfclosingtag"""
+    tag = 'sct'
+
+    def append(self):
+        print('TypeError! SelfClosingTag cannot have content.')
+
+    def render(self, file_out, cur_ind):
+        file_out.write(cur_ind * 3 + '<' + self.tag + ' />\n')
+        return '<' + self.tag + ' />'
+
+
+class Hr(SelfClosingTag):
+    """renders hr tag"""
+    tag = 'hr'
+
+
+class Br(SelfClosingTag):
+    """renders br tag"""
+    tag = 'br'
+
+
 class OneLineTag(Element):
     """renders onelinetag"""
     tag = 'olt'
@@ -81,6 +107,7 @@ class OneLineTag(Element):
             if type(item) == str:
                 file_out.write(item)
         file_out.write(self.close_tag + '\n')
+        return self.open_tag, self.content, self.close_tag
 
 
 class Title(OneLineTag):
