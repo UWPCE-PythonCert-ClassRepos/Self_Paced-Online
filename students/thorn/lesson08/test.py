4a1ea89101ffe1ab327ad860bb5c1f8718c5e9d9
Thomas Horn <hortw1991@gmail.com>
2018-04-27 21:32:23 -0500

    Deleted test files

--- students/thorn/lesson08/test.py
+++
@@ -1,8 +0,0 @@
-def michael_do_you_see():
-    '''how much the colors change'''
-
-    print("When you do dumb stuff like meeeeee")
-
-class YouWish:z
-    def __init__(self):
-        self.words = "YOU WISH THAT YOU WERE DEAAAAAAD (not an actual cry for help)"z
