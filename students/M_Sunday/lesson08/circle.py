14df36e1b4fe779c54ee3622a481724c2da8123b
mmsunday1 <matt.m.sunday@gmail.com>
2018-04-27 23:34:36 -0700

    step 1 complete

--- students/M_Sunday/lesson08/circle.py
+++ students/M_Sunday/lesson08/circle.py
@@ -3,20 +3,25 @@
 
 class Circle(object):
 
+    @staticmethod
+    def entry_check(the__radius):
+        if isinstance(the__radius, (str, list, tuple, dict)):
+            raise TypeError("Radius entry must be a single, positive, and "
+                            "non-string value")
+        else:
+            if the__radius < 0:
+                raise ValueError("Radius must be greater than 0")
+            else:
+                return the__radius
+
     def __init__(self, the_radius):
-        self._radius = the_radius
+        self._radius = self.entry_check(the_radius)
 
     def get_radius(self):
         return self._radius
 
     def set_radius(self, the_radius):
-        if type(the_radius) in ['str', 'list', 'tuple', 'dict']:
-            raise TypeError("Radius entry must be a positive, non-string value")
-        else:
-            if the_radius < 0:
-                raise ValueError("Radius must be greater than 0")
-            else:
-                self._radius = the_radius
+        self._radius = self.entry_check(the_radius)
 
     radius = property(get_radius, set_radius)
 
