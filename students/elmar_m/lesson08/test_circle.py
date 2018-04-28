8cc51d1cd25d3bf61eacb4f547ac460819f1538e
22e88 <22e88@mailbox.org>
2018-04-28 06:21:48 +0200

    Add test_circle.py initial version, being on branch LESSON08

---
+++ students/elmar_m/lesson08/test_circle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+file: test_circle.py
+elmar_m / 22e88@mailbox.org
+Lesson08: unittests for circle class
+'''
+
+import unittest as ut
+import circle as ci 
+
+class mytests(ut.TestCase):
+
+
+if __name__ == '__main__':
+    ut.main()
+
