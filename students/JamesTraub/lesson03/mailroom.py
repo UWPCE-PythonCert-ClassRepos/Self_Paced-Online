902451ebb7a3e281110fa4db757cb66e4eed30df
James Traub <traub01-devtrain@outlook.com>
2018-04-29 14:36:02 -0700

    Finishing the append to donor history function

--- students/JamesTraub/lesson03/mailroom.py
+++ students/JamesTraub/lesson03/mailroom.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 # James Traub 04-17-18
 # Lesson 3 - Mailroom Lab Exercise, Part 1
 
@@ -7,49 +7,62 @@ from datetime import date
 today = str(date.today())
 
 if __name__ == "__main__":
-    print()
+    print('')
 
 
 donors_history = [["Thomas Doyle", [1.00, 2.00, 4.00]], ["Kim Thayill", [200.00, 34999.00]], [
     "Rex Tillerson", [18000000.00, 1.00]], ["Judy Noise", [1000.00]], ["April Brinno", [20000.00, 3000.00, 400.00]]]
 
 
-mailroom_menu = "    \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\\n        Mailroom Main Menu    \\ \\ \\ \\ \\ \\ \\      \\ \\ \\ \\ \\ \\ \\\n    Option 1: \'Send a Thank You\'\n    Option 2: \'Create a Report\'\n    Option 3: \'Quit\'\n"
-
+mailroom_menu = """
+    \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\\n
+        Mailroom Main Menu\n
+    \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\\n
+    Enter a 1 to - \'Send a Thank You\'\n
+    Enter a 2 to - \'Create a Report\'\n
+    Enter a 3 to - \'Quit\'\n
+"""
 
 
 def donor_name_list():
-    print("This is the complete list of charitable donors as of " + today + ". ")
+    print("This is the complete list of charitable donors as of " + today + ".\n")
     for i in donors_history:
         print(i[0])
+        print('')
     thanking()
 
 
 def thanking():
-    thanks_for = input("Please enter the first and last name of the donor to thank. \nIf you would like to a list of the donor names enter the word 'list'. ")
-    if thanks_for.lowercase() = 'list':
+    name_or_list = input("Enter donor first and last name or the word donors to see a list of names. ")
+    donors = ''
+    if name_or_list == 'donors':
         donor_name_list()
     else:
-        if thanks_for in donors_history:
-            input("please eter a donation amount: ")
-        else:
-
+        donation_amount = float(input("Please enter a donation amount. No dollar sign needed, thank you. "'\n'))
+        for a, b in enumerate(donors_history):
+            if name_or_list == b[0]:
+                donors_history[a][1].append(donation_amount)
+                break
+        print(donors_history)
+        print("Thank you for your donation!")
 
 
 def mailing_options():
-    menu_input = ''
+    menu_input = 0
     while menu_input != '3':
         print(mailroom_menu)
-        menu_input = input("Please enter an option from the Mailroom Main Menu: ")
-        if menu_input == '1':
+        menu_input = int(input("Please enter an option from the Mailroom Main Menu: "))
+        print('')
+        if menu_input == 1:
             thanking()
-        elif menu_input == '2':
+        elif menu_input == 2:
             print("Create a Report")
-        elif menu_input == '3':
+        elif menu_input == 3:
             print("Good Bye.")
+            break
         else:
             print("Entry not recognized. Please try again.")
-        break
+            break
 
 
 mailing_options()
