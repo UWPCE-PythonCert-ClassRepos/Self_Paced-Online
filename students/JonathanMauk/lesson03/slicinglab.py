398b5c3ed954b1a0a37d0d6be388da6b370f5091
Jonathan Mauk <jorauk@uw.edu>
2018-04-30 12:42:35 -0700

    Function to exchange first and last items in slicing lab.

--- students/JonathanMauk/lesson03/slicinglab.py
+++ students/JonathanMauk/lesson03/slicinglab.py
@@ -1,2 +1,8 @@
-def exchange_fist_last(seq):
-	return seq[len(seq) - 1) +
+def exchange_first_last(seq):
+    return seq[-1:] + seq[1:-1] + seq[:1]
+
+a_string = "abcdef"
+a_tuple = (1, 2, 3, 4, 5, 6)
+
+assert exchange_first_last(a_string) == "fbcdea"
+assert exchange_first_last(a_tuple) == (6, 2, 3, 4, 5, 1)
