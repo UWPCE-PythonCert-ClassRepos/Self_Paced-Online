6af6e12d69f550f041449f39f2d1e7cc6341c4a1
Jonathan Mauk <jorauk@uw.edu>
2018-04-30 12:49:02 -0700

    Function to skip every other in slicing lab.

--- students/JonathanMauk/lesson03/slicinglab.py
+++ students/JonathanMauk/lesson03/slicinglab.py
@@ -1,8 +1,14 @@
 def exchange_first_last(seq):
     return seq[-1:] + seq[1:-1] + seq[:1]
 
-a_string = "abcdef"
-a_tuple = (1, 2, 3, 4, 5, 6)
+def remove_every_other(seq):
+    return seq[::2]
 
-assert exchange_first_last(a_string) == "fbcdea"
-assert exchange_first_last(a_tuple) == (6, 2, 3, 4, 5, 1)
+a_string = "abcdefghij"
+a_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+assert exchange_first_last(a_string) == "jbcdefghia"
+assert exchange_first_last(a_tuple) == (9, 1, 2, 3, 4, 5, 6, 7, 8, 0)
+
+assert remove_every_other(a_string) == "acegi"
+assert remove_every_other(a_tuple) == (0, 2, 4, 6, 8)
