ed4cffcaf3ac4fe5f0cab42f075d20c9093a4470
Jonathan Mauk <jorauk@uw.edu>
2018-04-30 13:19:28 -0700

    Function to reverse elements for slicing lab.

--- students/JonathanMauk/lesson03/slicinglab.py
+++ students/JonathanMauk/lesson03/slicinglab.py
@@ -10,6 +10,10 @@ def remove_firstlast_four(seq):
     return seq[4:(len(a_tuple) - 4):2]
 
 
+def reverse_slicing(seq):
+    return seq[::-1]
+
+
 a_string = "abcdefghij"
 a_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 
@@ -21,3 +25,6 @@ assert remove_every_other(a_tuple) == (0, 2, 4, 6, 8)
 
 assert remove_firstlast_four(a_string) == "e"
 assert remove_firstlast_four(a_tuple) == (4,)
+
+assert reverse_slicing(a_string) == "jihgfedcba"
+assert reverse_slicing(a_tuple) == (9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
