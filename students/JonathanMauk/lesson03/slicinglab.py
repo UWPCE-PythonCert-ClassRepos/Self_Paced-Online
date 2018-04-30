6cebb7878637295a21b46fd9e08e1a28a3865a8c
Jonathan Mauk <jorauk@uw.edu>
2018-04-30 13:09:28 -0700

    Function to remove first and last four in slicing lab.

--- students/JonathanMauk/lesson03/slicinglab.py
+++ students/JonathanMauk/lesson03/slicinglab.py
@@ -1,8 +1,14 @@
 def exchange_first_last(seq):
     return seq[-1:] + seq[1:-1] + seq[:1]
 
+
 def remove_every_other(seq):
     return seq[::2]
+
+
+def remove_firstlast_four(seq):
+    return seq[4:(len(a_tuple) - 4):2]
+
 
 a_string = "abcdefghij"
 a_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
@@ -12,3 +18,6 @@ assert exchange_first_last(a_tuple) == (9, 1, 2, 3, 4, 5, 6, 7, 8, 0)
 
 assert remove_every_other(a_string) == "acegi"
 assert remove_every_other(a_tuple) == (0, 2, 4, 6, 8)
+
+assert remove_firstlast_four(a_string) == "e"
+assert remove_firstlast_four(a_tuple) == (4,)
