430fa6bad36af2cab0070567b2254c816b8a7197
johnpharmd <jhumphre@uw.edu>
2018-04-30 14:34:36 -0700

    refactored __init__ and make_array()

--- students/johnpharmd/lesson08/sparse_array.py
+++ students/johnpharmd/lesson08/sparse_array.py
@@ -1,7 +1,15 @@
 
 
-class SparseArray:
-    """generates a 2D array with only non-zero values"""
+class SA:
+    """generates a 2D array with only non-zero numeric values"""
 
     def __init__(self, seq):
-        pass
+        self.seq = seq
+        self.seq_dictionary = {seq.index(item)-1: str(item) for item in seq
+                               if item != 0}
+        # self.seq_dictionary[0] = 'nil'
+
+    def make_array(self):
+        print('self.seq is', self.seq)
+        print('self.seq_dictionary is', self.seq_dictionary)
+        return '[' + ', '.join(self.seq_dictionary.values()) + ']'
