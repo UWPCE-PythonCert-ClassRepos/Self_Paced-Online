c0fbc72b71f13af5f4f8c6d85422e85f8bc806b6
Jonathan Mauk <jorauk@uw.edu>
2018-05-01 08:01:57 -0700

    Finished Task Three for string formatting lab.

--- students/JonathanMauk/lesson03/strformat_lab.py
+++ students/JonathanMauk/lesson03/strformat_lab.py
@@ -10,3 +10,16 @@ three = 10000
 four = 12345.67
 
 print(f'file_{one:0>3}: {two:0.2f}, {three:.2E}, {four:.2E}')
+
+# Task Three
+
+
+def formatter(in_tuple):
+    form_string = "the " + str(len(in_tuple)) + " numbers are: " + ((len(in_tuple) - 1) * '{:d}, ') + '{:d}'
+    return form_string.format(*in_tuple)
+
+tuple_1 = (1, 2, 3, 4, 5)
+tuple_2 = (49, 48, 47)
+print(formatter(tuple_1))
+print(formatter(tuple_2))
+
