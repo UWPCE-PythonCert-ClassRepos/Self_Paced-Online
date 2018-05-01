b785676c132fee5ef4c8739301ceceeb5ce8ef2b
idcrickmore <idcrickmore@gmail.com>
2018-04-30 19:31:12 -0700

    fixed some tab/spacing issues in html_render

--- students/idcrickmore/lesson07/html_render.py
+++ students/idcrickmore/lesson07/html_render.py
@@ -1,13 +1,17 @@
 """renders some simple HTML code"""
 
-#content=None is supposed to be the 'initializer signature'
 class Element():
 
-	def __init__(self):
-		self.content = list()
-	
-	def append(self, some_string):
-		self.content.append(some_string)
+    tag_name = ""
+    indent = "    "
+    
+    #content=None is supposed to be the 'initializer signature' ???
+    
+    def __init__(self):
+        self.content = list()
+
+    def append(self, some_string):
+        self.content.append(some_string)
 		
-	def render():
-		pass
+    def render():
+        pass
--- students/idcrickmore/lesson07/test_html_render.py
+++ students/idcrickmore/lesson07/test_html_render.py
@@ -3,12 +3,10 @@
 from html_render import Element
 
 
-
 def test_append():
 	if Element.append:
 		return True
-	
+
 def test_render():
 	if Element.render:
 		return True
-	
