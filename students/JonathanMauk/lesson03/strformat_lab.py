44669fcd16c9bbec11d62416fecaf394801fe260
Jonathan Mauk <jorauk@uw.edu>
2018-04-30 21:29:57 -0700

    Wrote Task Two for string formatting lab, using f-strings.

--- students/JonathanMauk/lesson03/strformat_lab.py
+++ students/JonathanMauk/lesson03/strformat_lab.py
@@ -1,3 +1,12 @@
 # Task One
 
 print('file_{0:0>3}: {1:0.2f}, {2:.2E}, {3:.2E}'.format(2, 123.4567, 10000, 12345.67))
+
+# Task Two
+
+one = 2
+two = 123.4567
+three = 10000
+four = 12345.67
+
+print(f'file_{one:0>3}: {two:0.2f}, {three:.2E}, {four:.2E}')
