c3d0e0d1d24a5a0649ca555692e78f6bd40c67fe
johnpharmd <jhumphre@uw.edu>
2018-05-02 05:41:58 -0700

    rediscovered enumerate() and used it to refactor sparse_array.py

--- students/johnpharmd/lesson08/sparse_array.py
+++ students/johnpharmd/lesson08/sparse_array.py
@@ -1,15 +1,24 @@
 
 
 class SA:
-    """generates a 2D array with only non-zero numeric values"""
+    """generates an array with only non-zero numeric values"""
 
     def __init__(self, seq):
         self.seq = seq
-        self.seq_dictionary = {seq.index(item)-1: str(item) for item in seq
-                               if item != 0}
-        # self.seq_dictionary[0] = 'nil'
+        self.seq_d = {i: j for i, j in enumerate(seq)}
+        self._sparse_arr = ''
 
-    def make_array(self):
-        print('self.seq is', self.seq)
-        print('self.seq_dictionary is', self.seq_dictionary)
-        return '[' + ', '.join(self.seq_dictionary.values()) + ']'
+    @property
+    def sparse_arr(self):
+        """getter"""
+        return self._sparse_arr
+
+    @sparse_arr.setter
+    def sparse_arr(self, value):
+        self._sparse_arr = value
+
+    def make_arr(self):
+        return (item for item in self.seq_d.values() if item != 0)
+
+    def get_len_arr(self):
+        pass
