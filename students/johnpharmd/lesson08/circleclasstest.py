f7fe08d9d977fe5c220b3dc25eeaa7d7cdbe35ec
johnpharmd <jhumphre@uw.edu>
2018-05-04 05:53:26 -0700

    unittests written through Step 2

---
+++ students/johnpharmd/lesson08/circleclasstest.py
@@ -0,0 +1,15 @@
+import circle_class as cc
+import unittest
+
+
+class CircleTest(unittest.TestCase):
+    """tests circle attributes"""
+
+    def setUp(self):
+        self.test = cc.Circle(5)
+
+    def test_circle_radius(self):
+        self.assertEqual(self.test.radius, 5)
+
+    def test_circle_diameter(self):
+        self.assertEqual(self.test.diameter, 10)
