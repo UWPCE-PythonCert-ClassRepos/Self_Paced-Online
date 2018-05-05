854cdb094d183b7918028da009f6638f13dc6907
johnpharmd <jhumphre@uw.edu>
2018-05-04 20:30:48 -0700

    added __str__() method

--- students/johnpharmd/lesson08/circle_class.py
+++ students/johnpharmd/lesson08/circle_class.py
@@ -1,12 +1,16 @@
+import math
 
 
 class Circle:
     """represents a simple circle, using properties, a class method, and
     special methods"""
-    
+
     def __init__(self, radius):
         self.radius = radius
-    
+
+    def __str__(self):
+        return 'Circle with radius: ' + str(self.radius)
+
     @property
     def diameter(self):
         return self.radius * 2
@@ -14,3 +18,10 @@ class Circle:
     @diameter.setter
     def diameter(self, value):
         self.radius = value / 2
+
+    @property
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def from_diameter(self):
+        return self.diameter
