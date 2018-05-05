3aff8f17bda22ab09720e3330cdea5d87697fce5
johnpharmd <jhumphre@uw.edu>
2018-05-04 19:42:50 -0700

    refactored circle_class after viewing Prof Barker's video

--- students/johnpharmd/lesson08/circle_class.py
+++ students/johnpharmd/lesson08/circle_class.py
@@ -6,7 +6,11 @@ class Circle:
     
     def __init__(self, radius):
         self.radius = radius
-
+    
     @property
     def diameter(self):
         return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
