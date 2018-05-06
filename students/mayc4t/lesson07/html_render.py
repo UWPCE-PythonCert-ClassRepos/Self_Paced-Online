02e5d746dd495ac0d157fa06c4b1c166de22fbf9
mayc4t <lily12A@gmail.com>
2018-05-06 11:53:55 -0700

    Step4:  Add code for step 4. Files: the same old set of files

--- students/mayc4t/lesson07/html_render.py
+++ students/mayc4t/lesson07/html_render.py
@@ -7,25 +7,33 @@ class Element:
     leaf_node=False
 
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.sub_elements = []
         if content:
             self.sub_elements = [content]
+        
+        self.style=''
+        for key, value in kwargs.items():
+            #print("The value of {} is {}".format(key, value))
+            if 'style' in kwargs: self.stl = ' "style={}"'.format(kwargs['style'])
+            #print (self.stl)
 
 
     def append( self, content ):
         self.sub_elements.append(content)
 
     def render( self, file_out, cur_ind='', new_line=True):
-        file_out.write("<{}>".format(self.tag))
-        for idx_elm in self.sub_elements: 
-            if self.leaf_node:
-                if new_line: 
-                    file_out.write("\n{}\n".format(idx_elm))
-                else:
-                    file_out.write(idx_elm)
-            else:
-                idx_elm.render(file_out, cur_ind)
+        if new_line :
+            file_out.write("<{}{}>\n".format(self.tag, self.stl))
+            for idx_elm in self.sub_elements: 
+                if self.leaf_node: file_out.write("{}\n".format(idx_elm))
+                else: idx_elm.render(file_out, cur_ind)
+        else: 
+            file_out.write("<{}{}>".format(self.tag, self.stl))
+            for idx_elm in self.sub_elements:
+
+                if self.leaf_node: file_out.write("{}".format(idx_elm))
+                else: idx_elm.render(file_out, cur_ind)
         file_out.write("</{}>\n".format(self.tag))
 
     def dump(self):
@@ -34,7 +42,7 @@ class Element:
 
 
 class Html(Element):
-    tag = "Html"
+    tag = "html"
     leaf_node=False
 
 class Body(Element):
@@ -42,7 +50,7 @@ class Body(Element):
     leaf_node=False
 
 class P(Element):
-    tag = 'P'
+    tag = 'p'
     leaf_node=True
 
 class Head(Element):
@@ -54,7 +62,6 @@ class OneLineTag(Element):
     leaf_node=True
     tag = ""
     def render( self, file_out, cur_ind):
-        #file_out.write("<{}>{}</{}>".format(self.tag,self.sub_elements, self.tag))
         super().render(file_out, cur_ind, False)
 
 class Title(OneLineTag):
