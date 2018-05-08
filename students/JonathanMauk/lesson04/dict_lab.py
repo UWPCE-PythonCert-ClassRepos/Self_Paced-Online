b74b77366b6a5b9a28e739f25a0e82a65fa84628
Jonathan Mauk <jorauk@uw.edu>
2018-05-08 12:15:14 -0700

    Finished Sets 1 & 2.

--- students/JonathanMauk/lesson04/dict_lab.py
+++ students/JonathanMauk/lesson04/dict_lab.py
@@ -33,3 +33,48 @@ for k, v in cake_dict.items():
 
 print("Printing the number of times 't' occurs in values...")
 print(t_dict)
+
+# ----- Sets 1 -----
+
+s2 = set()
+s3 = set()
+s4 = set()
+
+for n in range(21):
+    if n % 2 == 0:
+        s2.update([n])
+
+
+for n in range(21):
+    if n % 3 == 0:
+        s3.update([n])
+
+for n in range(21):
+    if n % 4 == 0:
+        s4.update([n])
+
+print(s2)
+print(s3)
+print(s4)
+
+print(s3.issubset(s2))
+print(s4.issubset(s2))
+
+# ----- Sets 2 -----
+
+s5 = set()
+
+for l in 'Python':
+    s5.update(l)
+
+print(s5)
+
+s6 = frozenset(('m', 'a', 'r', 'a', 't', 'h', 'o', 'n'))
+
+print(s6)
+
+print("Displaying union...")
+print(s5.union(s6))
+
+print("Displaying intersection...")
+print(s5.intersection(s6))
