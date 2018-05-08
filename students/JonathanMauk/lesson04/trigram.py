1bd25ca25769af994e10a715d61e5d6faac467a3
Jonathan Mauk <jorauk@uw.edu>
2018-05-08 15:50:37 -0700

    Started Trigrams assignment.

---
+++ students/JonathanMauk/lesson04/trigram.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+trigram_dict = {}
+word_list = []
+
+
+def create_trigram_dict(filename):
+
+
+print(create_trigram_dict('sherlock_small.txt'))
+
+
+
+# def mutate_text():
