8b4bee07b5a505336dcd32617b4077137c123b11
Jonathan Mauk <jorauk@uw.edu>
2018-05-08 16:27:31 -0700

    Added function to extract words from text sample.

--- students/JonathanMauk/lesson04/trigram.py
+++ students/JonathanMauk/lesson04/trigram.py
@@ -3,14 +3,26 @@ import string
 
 
 trigram_dict = {}
-word_list = []
 
 
 def create_trigram_dict(filename):
+    word_list = []
 
+    with open(filename) as f:
+        for line in f:
+            split = line.split()
+            word_list += split
+    return word_list
 
 print(create_trigram_dict('sherlock_small.txt'))
 
 
-
 # def mutate_text():
+
+
+# if __name__ == '__main__':
+#     try:
+#         get_user_input = input("Enter the name of a file to generate 300 words of text from trigrams: ")
+#         create_trigram_dict(get_user_input)
+#     except:
+#         print("Error. Please restart the program to try again.")
