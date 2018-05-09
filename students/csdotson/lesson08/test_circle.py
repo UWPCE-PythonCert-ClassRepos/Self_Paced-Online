96d98bfa4182102c495e8aeb2efa0ff2d8949226
csdotson <csdotson1@gmail.com>
2018-05-09 12:24:30 -0700

    Step 3: Set diamater property so user can set diam of circle. Note - test doesn't pass.

--- students/csdotson/lesson08/test_circle.py
+++ students/csdotson/lesson08/test_circle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Test file used to develop (TDD) and test 'html_render.py'
+Test file used to develop (TDD) and test 'circle.py'
 """
 
 import circle as c
@@ -12,3 +12,18 @@ def test_radius():
 def test_diameter():
     circle = c.Circle(4)
     assert circle.diameter == 8
+
+def test_diameter_change():
+    circle = c.Circle(4)
+    circle.diameter = 3
+    assert circle.diameter == 3
+
+def test_radius_changes_diameter():
+    circle = c.Circle(4)
+    circle.radius = 5
+    assert circle.diameter == 10
+
+def test_diameter_changes_radius():
+    circle = c.Circle(4)
+    circle.diameter = 4
+    assert circle.radius == 2
