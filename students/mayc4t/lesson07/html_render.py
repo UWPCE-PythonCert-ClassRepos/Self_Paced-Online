e547a5022b86c3f6fbb0607dc6be450279fe1e1a
mayc4t <lily12A@gmail.com>
2018-05-08 21:35:40 -0700

    step:5,6,7,8 - also went back to fix problem from step 2. - files: the same old set

--- students/mayc4t/lesson07/html_render.py
+++ students/mayc4t/lesson07/html_render.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python
 
+class TextWrapper:
+    """
+    A simple wrapper that creates a class with a render method
+    for simple text
+    """
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, file_out, cur_ind='', new_line=True):
+        out_str = self.text
+        if new_line:
+            out_str = "%s\n" % out_str
+        file_out.write(out_str)
+
 class Element:
     tag = 'html'
     ind = 4
     content =""
-    leaf_node=False
 
 
     def __init__(self, content=None, **kwargs):
         self.sub_elements = []
-        if content:
+        if content and isinstance(content, str):
+            self.sub_elements = [TextWrapper(content)]
+        elif content:
             self.sub_elements = [content]
         
-        self.style=''
+        self.attributes = ''
         for key, value in kwargs.items():
-            #print("The value of {} is {}".format(key, value))
-            if 'style' in kwargs: self.stl = ' "style={}"'.format(kwargs['style'])
-            #print (self.stl)
+            self.attributes = '%s %s="%s"' % (self.attributes, key, value)
 
 
     def append( self, content ):
-        self.sub_elements.append(content)
+        if isinstance(content, str):
+            self.sub_elements.append(TextWrapper(content))
+        else:
+            self.sub_elements.append(content)
 
-    def render( self, file_out, cur_ind='', new_line=True):
+    def render(self, file_out, cur_ind='', new_line=True):
         if new_line :
-            file_out.write("<{}{}>\n".format(self.tag, self.style))
-            for idx_elm in self.sub_elements: 
-                if self.leaf_node: file_out.write("{}\n".format(idx_elm))
-                else: idx_elm.render(file_out, cur_ind)
+            file_out.write("<{}{}>\n".format(self.tag, self.attributes))
         else: 
-            file_out.write("<{}{}>".format(self.tag, self.style))
-            for idx_elm in self.sub_elements:
-
-                if self.leaf_node: file_out.write("{}".format(idx_elm))
-                else: idx_elm.render(file_out, cur_ind)
+            file_out.write("<{}{}>".format(self.tag, self.attributes))
+        for idx_elm in self.sub_elements: 
+            idx_elm.render(file_out, cur_ind, new_line)
         file_out.write("</{}>\n".format(self.tag))
 
     def dump(self):
@@ -43,25 +54,23 @@ class Element:
 
 class Html(Element):
     tag = "html"
-    leaf_node=False
+    def render(self, file_out, cur_ind='', new_line=True):
+        file_out.write("<!DOCTYPE html>\n")
+        super().render(file_out, cur_ind, new_line)
 
 class Body(Element):
     tag = "body"
-    leaf_node=False
 
 class P(Element):
-    tag = 'p'
-    leaf_node=True
+    tag = "p"
 
 class Head(Element):
     tag = "head"
-    leaf_node = False
 
 class OneLineTag(Element):
     onelinetag = True
-    leaf_node=True
     tag = ""
-    def render( self, file_out, cur_ind):
+    def render(self, file_out, cur_ind, parent_new_line):
         super().render(file_out, cur_ind, False)
 
 class Title(OneLineTag):
@@ -69,17 +78,46 @@ class Title(OneLineTag):
 
 
 class SelfClosingTag(Element):
-    def __init__self( self, content=None, **kwargs ):
-
-        if self.content is not None:
+    def __init__(self, content=None, **kwargs ):
+        super().__init__()
+        if content is not None:
             raise TypeError
     
     def render( self, file_out, cur_ind='', new_line=True):
-        file_out.write("< /{}>\n".format(self.tag))
+        file_out.write("<{}{} />\n".format(self.tag, self.attributes))
 
 class Hr(SelfClosingTag):
     tag = "hr"
 
+class Br(SelfClosingTag):
+    tag = "hr"
 
+class A(Element):
+    tag = 'a'
+    def __init__(self, link, content, **kwargs):
+        super().__init__(content)
+        self.link = link 
 
+    def render( self, file_out, cur_ind='', new_line=True):
+        file_out.write('<{} href="{}">{}</{}>\n'.format(
+            self.tag, self.link, self.sub_elements[0].text,
+            self.tag))
+
+class Ul(Element):
+    tag = 'ul'
+
+class Li(Element):
+    tag = 'li'
+
+class H(OneLineTag):
+    tag = 'h'
+    def __init__(self, level, content):
+        super().__init__(content)
+        self.tag += str(level)
+
+class Meta(SelfClosingTag):
+    tag='meta'
+    def __init__(self, charset):
+        super().__init__()
+        self.tag += "charset={}".format(charset)
 
