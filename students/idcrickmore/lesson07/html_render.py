19e9ea8f3fd54c2926f3f52eb9d5b2d05d4fcb5b
idcrickmore <idcrickmore@gmail.com>
2018-05-10 20:16:03 -0700

    added some functionality to step 4 of lesson07

--- students/idcrickmore/lesson07/html_render.py
+++ students/idcrickmore/lesson07/html_render.py
@@ -4,27 +4,36 @@ class Element():
 
     tag_name = "html"
 
-    #inster '**kwargs" as optional argument
-
-    def __init__(self, content: str=None):
+    def __init__(self, content: str=None, **kwargs):
         self.content = [content] if content else []
-        #self.kwargs = kwargs
+        self.attributes = kwargs
 
     def append(self, content):
         #append method that can add another string to the content
         self.content.append(content)
 
     def render(self, file_out, cur_ind=""):
-        
+        if self.attributes != {}:
+            attributes = " ".join(["%s=%s" % (k, v) for k, v in self.attributes.items()])
+            open_tag = f"<{self.tag_name}{attributes}>"
+            close_tag = f"</{self.tag_name}>"
+            print(open_tag)
+            print(close_tag)
+
+        else:
+            open_tag = f"<{self.tag_name}>"
+            close_tag = f"</{self.tag_name}>"
+        # print(open_tag)
+        # print(close_tag)
         for item in self.content:
             if isinstance(item, Element):
                 item.render(file_out, "    ")
             else:
-                file_out.write(f"<{self.tag_name}>")
+                file_out.write(open_tag)
                 file_out.write(item + "\n")
-                file_out.write(f"</{self.tag_name}>")
+                file_out.write(close_tag)
 
-                
+
 class OneLineTag(Element):
     def render(self, file_out, cur_ind=""):
         for item in self.content:
@@ -55,9 +64,3 @@ class Head(Element):
 class Title(OneLineTag):
     tag_name = "title"
 
-    
-# from io import StringIO
-# el = Element()
-# el.append("Some content.")
-# el.append("Some more content.")
-# el.render(file_out)
