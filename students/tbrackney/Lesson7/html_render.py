74f55389bb714ca382243c584d056d93ae36b0cd
Travis Brackney <tbrackney@gmail.com>
2018-05-12 11:44:55 -0700

    Initial files

---
+++ students/tbrackney/Lesson7/html_render.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+File Name: html_render.py
+Author: Travis Brackney
+Class: Python 201 - Self paced online
+Date Created 5/12/2018
+Python Version: 3.6.4
+"""
+
+''' Class for rendering html page'''
+
+
+class Element():
+    pass
---
+++ students/tbrackney/Lesson7/test_html_render.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+File Name: test_html_render.py
+Author: Travis Brackney
+Class: Python 201 - Self paced online
+Date Created 5/12/2018
+Python Version: 3.6.4
+"""
+
+''' Class for rendering html page'''
