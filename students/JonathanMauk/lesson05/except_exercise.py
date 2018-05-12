b03218b9b26fa145bec79e37d6d3c36ef6382e23
Jonathan Mauk <jorauk@uw.edu>
2018-05-12 11:39:21 -0700

    Realized I did not understand the assignment parameters so I started over.

--- students/JonathanMauk/lesson05/except_exercise.py
+++ students/JonathanMauk/lesson05/except_exercise.py
@@ -14,13 +14,13 @@ from except_test import fun, more_fun, last_fun
 # in that catch block, try again with the second item in the list
 first_try = ['spam', 'cheese', 'mr death']
 
+joke = fun(first_try[0])
+
 # Here is a try/except block. Add an else that prints not_joke
 try:
-    joke = fun(first_try[0])
-except NameError:
+    not_joke = fun(first_try[2])
+except SyntaxError:
     print('Run Away!')
-else:
-    print(joke)
 
 # What did that do? You can think of else in this context, as well as in
 # loops as meaning: "else if nothing went wrong"
@@ -40,6 +40,4 @@ else:
 
 langs = ['java', 'c', 'python']
 
-try:
-    more_joke = more_fun(langs[0])
-except IndexError:
+more_joke = more_fun(langs[0])
