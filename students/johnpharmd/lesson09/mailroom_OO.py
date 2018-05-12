fea41f6f9c04b8b95f5b3d796ae56ee8e84c27e6
johnpharmd <jhumphre@uw.edu>
2018-05-12 16:06:58 -0700

    added new donation property

--- students/johnpharmd/lesson09/mailroom_OO.py
+++ students/johnpharmd/lesson09/mailroom_OO.py
@@ -2,14 +2,31 @@
 
 
 class Donor:
-    """creates donor objects"""
-    def __init__(self, title, last_name, donation_amt):
+    """creates objects for individual donors"""
+    def __init__(self, title, last_name, donation):
         self.title = title
         self.last_name = last_name
-        self.donations = []
-        self.donation_amt = self.donations.append(donation_amt)
+        self.donations = [donation]
 
     @property
     def donor(self):
         return {self.last_name: {'title': self.title, 'donations':
-                sum(self.donations), 'num_donations': len(self.donations)}}
+        sum(self.donations), 'num_donations': len(self.donations)}}
+    
+    @property
+    def title(self):
+        return self.new_title
+
+    @title.setter
+    def title(self, new_title):
+        """enables title change for donor"""
+        self.new_title = new_title
+
+    @property
+    def donation(self):
+        return self.donation
+
+    @donation.setter
+    def donation(self, donation):
+        """enables addition of new donations"""
+        self.donations.append(donation)
