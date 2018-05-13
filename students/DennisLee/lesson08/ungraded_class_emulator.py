3a845050db2d624a79ca7b5708af5e01d1eee20a
Dennis Lee <dennis271lee@yahoo.com>
2018-05-12 18:35:52 -0700

    [L8 ungraded] Got rid of attempt 2-D implementation, fixed code, added 'if name=main' tests at the end of the file.

--- students/DennisLee/lesson08/ungraded_class_emulator.py
+++ students/DennisLee/lesson08/ungraded_class_emulator.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 
+# import numpy
+# import operator
+
 class SparseArray():
 
-    val_dict, length = {}, 0
+    def __init__(self, seq=None):
+        self.val_dict, self.length = {}, 0
+        if seq:
+            self.append(seq)
 
-    def __init__(self, seq):
-        self.append(seq)
+    def __str__(self):
+        return str(self.__iter__())
 
     def __len__(self):
         return self.length
@@ -17,7 +23,7 @@ class SparseArray():
             (start, stop, step) = key.indices(self.length)
             for i in range(start, stop, step):
                 final_slice.append(self.__getitem__(i))
-            return final_slice
+            return final_slice[:]
         elif isinstance(key, tuple):
             pass
         else:
@@ -38,27 +44,31 @@ class SparseArray():
             key = self.test_key(key)
             if value:
                 self.val_dict[key] = value
-            else:
+            elif key in self.val_dict.keys():
                 del self.val_dict[key]
 
     def __delitem__(self, key):
         if isinstance(key, slice):
             (start, stop, step) = key.indices(self.length)
-            if start < stop:  # Make sure we delete going backward
-                start, stop, step = stop - 1, start - 1, -step
-            for i in range(start, stop, step):
+            items_to_delete = [i for i in range(start, stop, step)]
+            # Make sure we delete going backward, or else subsequent
+            # deletion indices will be incorrect
+            if items_to_delete[0] < items_to_delete[-1]:
+                direction = -1
+            else:
+                direction = 1
+            for i in items_to_delete[::direction]:
                 self.__delitem__(i)
         elif isinstance(key, tuple):
             pass
         else:
             key = self.test_key(key)
-            if key == self.length - 1:
+            if key in self.val_dict.keys():
                 del self.val_dict[key]
-            else:
-                for i in range(key, self.length-1):
-                    if key in self.val_dict.keys():
-                        del self.val_dict[i]
-                        self.__setitem__(i, self.val_dict[i+1])
+            for i in range(key, self.length-1):
+                if i in self.val_dict.keys():
+                    del self.val_dict[i]
+                self.__setitem__(i, self.__getitem__(i+1))
             self.length -= 1
             
     def __iter__(self):
@@ -68,7 +78,7 @@ class SparseArray():
         return self.__iter__()[::-1]
 
     def __contains__(self, item):
-        return (item in self.val_dict.values())
+        return (item in self.__iter__())
 
     def append(self, val):
         if isinstance(val, list):
@@ -88,3 +98,40 @@ class SparseArray():
         if key < 0:
             key += self.length
         return key
+
+
+    
+
+
+if __name__ == "__main__":
+    array_to_add = [0,0,0,32,56,0,423,0,0,0,0,0,0,46,0,54,66,0]
+    sa = SparseArray(array_to_add)
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    print("Array sa reversed is:", reversed(sa))
+    sa[2] = 88
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    print("7th member of sa array is:", sa[6])
+    print("10th member of sa array is:", sa[9])
+    print("Last four members of sa are:", sa[-4:])
+    sa.append(0)
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    sa.append(3)
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    sa.append([0,0,35,97,0,0,68,0,0])
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    del sa[4]
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    del sa[15:19]
+    print("Array sa now has {0} numbers: {1}".format(len(sa), sa))
+    print("Array sa contains 5? ", 5 in sa)
+    print("Array sa contains 423? ", 423 in sa)
+    print("Array sa contains 0? ", 0 in sa)
+
+    sa2 = SparseArray()
+    print("Array sa2 now has {0} numbers: {1}".format(len(sa2), sa2))
+    sa2.append([0,0,35,58,0,0])
+    print("Array sa2 now has {0} numbers: {1}".format(len(sa2), sa2))
+    del sa2[0:]
+    print("Array sa2 now has {0} numbers: {1}".format(len(sa2), sa2))
+    sa2.append(5)
+    print("Array sa2 now has {0} numbers: {1}".format(len(sa2), sa2))
