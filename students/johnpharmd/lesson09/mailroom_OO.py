64403a5d5acf917bf1d3ccb07f24a1e9123d57a2
johnpharmd <jhumphre@uw.edu>
2018-05-13 12:29:24 -0700

    wrote get_list() method

--- students/johnpharmd/lesson09/mailroom_OO.py
+++ students/johnpharmd/lesson09/mailroom_OO.py
@@ -58,6 +58,33 @@ class DonorGroup(Donor):
             if last_name in d.donor and d.donor[last_name]['title'] == title:
                 self.donors.remove(d)
 
+    def get_list(self):
+        self.donor_list = []
+        for d in self.donors:
+            for last_name in d.donor:
+                self.donor_list.append([d.donor[last_name]['donations'], last_name,
+                                        d.donor[last_name]['num_donations']])
+        return sorted(self.donor_list, reverse=True)
+
+    def get_report(self):
+        print()
+        psv = ['Donor Name', '| Total Given', '| Num Gifts',
+               '| Average Gift']
+        print('{:<15}{:>12}{:>12}{:>12}'.format(psv[0], psv[1],
+              psv[2], psv[3]))
+        for i in range(55):
+            print('-', end='')
+        print()
+        new_list = self.get_list()
+        new_list.sort(reverse=True)
+        for donor_list in new_list:
+            formatted_donor = ('{:<15}'.format(donor_list[1])
+                               + '{}{:>10}'.format(' $', donor_list[0])
+                               + '{:>12}'.format(donor_list[2])
+                               + '{}{:>11}'.format(' $',
+                               donor_list[0] // donor_list[2]))
+
     def save_data(self):
         with open('test.txt', 'w') as outfile:
-            outfile.write('This is current DonorGroup data:\n' + str(self.donorgroup))
+            outfile.write('This is current DonorGroup data:\n' +
+                          str(self.donorgroup))
