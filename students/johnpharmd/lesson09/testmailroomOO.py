c1afa53e0abcf435975d3baceb430c3324c5ebb9
johnpharmd <jhumphre@uw.edu>
2018-05-13 05:04:15 -0700

    refactored DonorGroupTest initialization

--- students/johnpharmd/lesson09/testmailroomOO.py
+++ students/johnpharmd/lesson09/testmailroomOO.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import unittest
-import mailroom_OO
+import mailroom_OO as mrOO
 
 
 class DonorTest(unittest.TestCase):
     def setUp(self):
-        self.test = mailroom_OO.Donor('Mr.', 'Brin', 100000)
+        self.test = mrOO.Donor('Mr.', 'Brin', 100000)
 
     def test_donor(self):
         self.assertEqual(self.test.donor, {'Brin': {'title': 'Mr.',
@@ -20,12 +20,15 @@ class DonorTest(unittest.TestCase):
 
 class DonorGroupTest(unittest.TestCase):
     def setUp(self):
-        self.test = mailroom_OO.DonorGroup(('Mr.', 'Brin', 100000),
-                    ('Ms.', 'Wojcicki', 200000), ('Ms.', 'Avey', 150000))
+        self.test = mrOO.DonorGroup(mrOO.Donor('Mr.', 'Brin', 100000),
+                                    mrOO.Donor('Ms.', 'Wojcicki', 200000),
+                                    mrOO.Donor('Ms.', 'Avey', 150000))
 
     def test_donor_group(self):
-        self.assertEqual(self.test.donor_group, {'Brin': {'title': 'Mr.',
-                         'donations': 100000, 'num_donations': 1},
-                         'Wojcicki': {'title': 'Ms.', 'donations': 200000,
-                         'num_donations': 1}, 'Avey': {'title': 'Ms.',
-                         'donations': 150000, 'num_donations': 1}})
+        self.assertEqual(self.test.donor_group,
+                         [{'Brin': {'title': 'Mr.', 'donations': 100000,
+                          'num_donations': 1}},
+                          {'Wojcicki': {'title': 'Ms.', 'donations': 200000,
+                           'num_donations': 1}},
+                          {'Avey': {'title': 'Ms.', 'donations': 150000,
+                                    'num_donations': 1}}])
