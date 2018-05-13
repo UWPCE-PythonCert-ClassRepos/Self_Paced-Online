c2951ced840897ea1877bced0ef0851fedfbd7d1
johnpharmd <jhumphre@uw.edu>
2018-05-13 06:19:06 -0700

    wrote test for donor_group.setter

--- students/johnpharmd/lesson09/testmailroomOO.py
+++ students/johnpharmd/lesson09/testmailroomOO.py
@@ -32,3 +32,21 @@ class DonorGroupTest(unittest.TestCase):
                            'num_donations': 1}},
                           {'Avey': {'title': 'Ms.', 'donations': 150000,
                                     'num_donations': 1}}])
+        self.test.donor_group_add = mrOO.Donor('Mr.', 'Page', 50000)
+        self.assertEqual(self.test.donor_group,
+                         [{'Brin': {'title': 'Mr.', 'donations': 100000,
+                          'num_donations': 1}},
+                          {'Wojcicki': {'title': 'Ms.', 'donations': 200000,
+                           'num_donations': 1}},
+                          {'Avey': {'title': 'Ms.', 'donations': 150000,
+                                    'num_donations': 1}},
+                          {'Page': {'title': 'Mr.', 'donations': 50000,
+                                    'num_donations': 1}}])
+        self.test.donor_group_remove = 'Brin'
+        self.assertEqual(self.test.donor_group,
+                         [{'Wojcicki': {'title': 'Ms.', 'donations': 200000,
+                           'num_donations': 1}},
+                          {'Avey': {'title': 'Ms.', 'donations': 150000,
+                                    'num_donations': 1}},
+                          {'Page': {'title': 'Ms.', 'donations': 150000,
+                                    'num_donations': 1}}])
