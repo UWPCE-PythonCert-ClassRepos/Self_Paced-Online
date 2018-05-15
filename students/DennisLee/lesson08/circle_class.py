300cac947b192abb3b589fdb7720d61ffec2549e
Dennis Lee <dennis271lee@yahoo.com>
2018-05-14 20:59:47 -0700

    [DennisLee][L8][Circle class] Completed step 7, added comments to code file.

--- students/DennisLee/lesson08/circle_class.py
+++ students/DennisLee/lesson08/circle_class.py
@@ -1,24 +1,99 @@
 #!/usr/bin/env python3
 
-from math import pi
+from math import pi  # Need pi for the circle's area calculation.
 
 class Circle():
+    """Represent a circle, its radius, diameter, and area."""
 
     def __init__(self, value):
-        self.radius = value
+        """Create a circle using the specified radius value."""
+        self.radius = value  # Use the setter to do error checking
 
     def __repr__(self):
+        """Show the formal, evaluateable class representation."""
         return f"Circle({self._radius})" 
 
     def __str__(self):
+        """Show an informal string representation of the class."""
         return f"Circle with radius: {self._radius}"
+
+    def __add__(self, other):
+        """
+        Add two circles together, with the resulting circle having a
+        radius equal to the sum of the individual circle radii.
+
+        :other:  A number or Circle object representing the radius of
+                 the second circle.
+
+        :return:  The resulting summed Circle object.
+        """
+        new_radius = self._radius
+        if isinstance(other, (int, float)):
+            new_radius += other
+        elif isinstance(other, Circle):  
+            new_radius += other.radius
+        else:
+            raise TypeError(f"Cannot add {other} to a circle because it "
+                    f"is a '{type(other)}' type, not an 'int' or 'Circle'.")
+        return Circle(new_radius)
+
+    def __radd__(self, other):
+        """
+        Add two circles together, with the resulting circle having a
+        radius equal to the sum of the individual circle radii.
+
+        :other:  A number or Circle object representing the radius of
+                 the first circle.
+
+        :return:  The resulting summed Circle object.
+        """
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        """
+        Multiply two circles together, with the resulting circle having
+        a radius equal to the product of the individual circle radii.
+
+        :other:  A number or Circle object representing the radius of
+                 the second circle.
+
+        :return:  The resulting multiplied Circle object.
+        """
+        new_radius = self._radius
+        if isinstance(other, (int, float)):
+            new_radius *= other
+        elif isinstance(other, Circle):  
+            new_radius *= other.radius
+        else:
+            raise TypeError(f"Cannot multiply {other} to a circle because it "
+                    f"is a '{type(other)}' type, not an 'int' or 'Circle'.")
+        return Circle(new_radius)
+
+    def __rmul__(self, other):
+        """
+        Multiply two circles together, with the resulting circle having
+        a radius equal to the product of the individual circle radii.
+
+        :other:  A number or Circle object representing the radius of
+                 the first circle.
+
+        :return:  The resulting multiplied Circle object.
+        """
+        return self.__mul__(other)
 
     @property
     def radius(self):
+        """Get the radius of the circle."""
         return self._radius
 
     @radius.setter
     def radius(self, value):
+        """
+        Set the radius of the circle. Only nonnegative numbers are
+        allowed.
+
+        :value:  The new radius value.
+        """
         if value < 0:
             raise ValueError(f"Negative radius of '{value}' not allowed.")
         else:
@@ -26,16 +101,27 @@ class Circle():
 
     @property
     def diameter(self):
+        """Get the diameter of the circle (i.e., twice the radius)."""
         return self._radius * 2
 
     @diameter.setter
     def diameter(self, value):
+        """
+        Set the diameter of the circle. Only nonnegative numbers are
+        allowed.
+
+        :value:  The new diameter value.
+        """
+        # Use the radius setter (instead of setting the internal member)
+        # to do error checking
         self.radius = value / 2
 
     @property
     def area(self):
+        """Get the area of the circle (pi times radius squared)."""
         return pi * self._radius ** 2
 
     @classmethod
     def from_diameter(self, value):
+        """Create a circle using the specified diameter value."""
         return Circle(value / 2)
--- students/DennisLee/lesson08/test_circle_class.py
+++ students/DennisLee/lesson08/test_circle_class.py
@@ -18,6 +18,7 @@ class CircleTestCase(unittest.TestCase):
     def test_radius_102(self):
         with self.assertRaises(ValueError):
             c = Circle(-4)
+            del c
 
     def test_diameter_100(self):
         c = Circle(20)
@@ -65,6 +66,7 @@ class CircleTestCase(unittest.TestCase):
     def test_diameter_constructor_101(self):
         with self.assertRaises(ValueError):
             c = Circle.from_diameter(-6)
+            del c
         
     def test_repr_100(self):
         c = Circle(200)
@@ -85,6 +87,97 @@ class CircleTestCase(unittest.TestCase):
     def test_str_100(self):
         c = Circle(4.555)
         self.assertTrue("Circle with radius: 4.55" in str(c))
+
+    def test_add_100(self):
+        c1 = Circle(4)
+        c2 = Circle.from_diameter(10)
+        c3 = c1 + c2
+        self.assertIsInstance(c3, Circle)
+        self.assertEqual(c3.radius, 9)
+        self.assertEqual(c3.diameter, 18)
+        self.assertLess(abs(c3.area-254.469), 0.1)
+
+    def test_add_101(self):
+        c1 = Circle(6)
+        c2 = c1 + 2
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 8)
+        self.assertEqual(c2.diameter, 16)
+        self.assertLess(abs(c2.area-201.062), 0.1)
+
+    def test_add_102(self):
+        c1 = Circle.from_diameter(14)
+        c2 = 3 + c1
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 10)
+        self.assertEqual(c2.diameter, 20)
+        self.assertLess(abs(c2.area-314.159), 0.1)
+
+    def test_add_103(self):
+        c1 = Circle.from_diameter(8)
+        c2 = c1 + 0.75
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 4.75)
+        self.assertEqual(c2.diameter, 9.5)
+        self.assertLess(abs(c2.area-70.882), 0.1)
+
+    def test_add_110(self):
+        c1 = Circle(5)
+        with self.assertRaises(ValueError):
+            c2 = c1 + (-6)
+            del c2
+
+    def test_add_111(self):
+        c1 = Circle(5)
+        with self.assertRaises(TypeError):
+            c2 = c1 + "86"
+            del c2
+
+    def test_multiply_100(self):
+        c1 = Circle(5)
+        c2 = Circle.from_diameter(6)
+        c3 = c1 * c2
+        self.assertIsInstance(c3, Circle)
+        self.assertEqual(c3.radius, 15)
+        self.assertEqual(c3.diameter, 30)
+        self.assertLess(abs(c3.area-706.858), 0.1)
+
+    def test_multiply_101(self):
+        c1 = Circle(6)
+        c2 = c1 * 4
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 24)
+        self.assertEqual(c2.diameter, 48)
+        self.assertLess(abs(c2.area-1809.557), 0.1)
+
+    def test_multiply_102(self):
+        c1 = Circle.from_diameter(14)
+        c2 = 3 * c1
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 21)
+        self.assertEqual(c2.diameter, 42)
+        self.assertLess(abs(c2.area-1385.442), 0.1)
+
+    def test_multiply_103(self):
+        c1 = Circle.from_diameter(8)
+        c2 = c1 * 0.75
+        self.assertIsInstance(c2, Circle)
+        self.assertEqual(c2.radius, 3.0)
+        self.assertEqual(c2.diameter, 6.0)
+        self.assertLess(abs(c2.area-28.274), 0.1)
+
+    def test_multiply_110(self):
+        c1 = Circle(6)
+        with self.assertRaises(ValueError):
+            c2 = c1 * -4
+            del c2
+
+    def test_multiply_111(self):
+        c1 = Circle(6)
+        with self.assertRaises(TypeError):
+            c2 = c1 * "54"
+            del c2
+        
 
 
 if __name__ == "__main__":
