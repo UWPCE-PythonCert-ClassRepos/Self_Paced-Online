3a047c89f370b85ff00776d3e40f7262fcc05509
Thomas Horn <hortw1991@gmail.com>
2018-05-15 16:22:04 -0500

    initial setup

--- students/thorn/lesson09/mailroom_oo.py
+++ students/thorn/lesson09/mailroom_oo.py
@@ -8,8 +8,17 @@ import os
 
 class Donor:
     """
-    Contains all donor information.
+    hold all the information about the donor, and have attributes and methods to provide access to the donor specific information that is needed
 
     
     """
-    
+    pass
+
+
+class DonorCollection:
+    """
+    hold all the donor objects, but also methods to add a new donor, search for a given donor, etc. If you want a way to save and re-load your data, this class would have that too
+
+    hold the code that generates reports about multiple donors
+    """
+    pass
