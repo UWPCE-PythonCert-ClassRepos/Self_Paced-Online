88f9175b7151f71388e6ea7ffe41b57da7e70da5
Thomas Horn <hortw1991@gmail.com>
2018-05-15 16:20:20 -0500

    Initial commit - lesson 9

---
+++ students/thorn/lesson09/mailroom_oo.py
@@ -0,0 +1,15 @@
+"""
+Thomas Horn
+
+Object Oriented refactor of mailroom_oo.py
+"""
+
+import os
+
+class Donor:
+    """
+    Contains all donor information.
+
+    
+    """
+    
---
+++ students/thorn/lesson09/test_mailroom_oo.py
@@ -0,0 +1,6 @@
+'''
+Tests for mailroom_oo.py
+'''
+
+
+
