4475d08d69b14bfd6aef04a9fe8f8509dc96203e
csdotson <csdotson1@gmail.com>
2018-05-16 07:16:41 -0700

    Refactor mailroom program, add test file

--- students/csdotson/lesson06/mailroom_4.py
+++ students/csdotson/lesson06/mailroom_4.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # Lesson 6 - Mailroom_4 (Unit testing)
 
-### Function Definitions ###
 def prompt(prompt, menu):
-    # Create menu based on arguments
+    """ Create menu based on arguments """
     while True:
         response = input(prompt)
         try:
@@ -14,12 +13,12 @@ def prompt(prompt, menu):
 
 
 def send_thank_you():
-    # Create sub_prompt for creating thank you's and adding new donors/donations
+    """ Create sub_prompt """
     prompt(thank_you_prompt, thank_you_menu)
 
 
 def add_donation():
-    # Append donation to existing donor, or add donor/donation to existing list
+    """ Add donor/donation to existing list of donations """
     try:
         donor_name = input("\nPlease enter a donor name: ")
         new_donation = float(input(f"Please enter a donation amount for {donor_name}: "))
@@ -35,7 +34,9 @@ def add_donation():
 
 
 def write_letters():
-    # For all donors, generate a thank you letter, and write it to disk as a text file
+    """
+    For all donors, generate a thank you letter, and write it to disk as a text file
+    """
     for donor in donations:
         file_name = '_'.join(donor.split()) + ".txt"
         with open(file_name, 'w') as f:
@@ -49,39 +50,71 @@ def write_letters():
             print(f"\nCreated letter for {donor}!")
 
 
-def print_email(donor, amount):
-    # Print formatted email thanking donor
-    letter_details = {'name': donor, 'donation_amount': amount}
-    letter = "\nDear {name},\n\nThank you so very much for your kind donation of ${donation_amount}. We can assure you that it will be put to great use.\n\nBest,\nChris".format(**letter_details)
-    print(letter)
+def compose_letter(donations):
+    pass
 
 
-def create_report():
-    # Print formatted report showing donors and donation stats
+
+####  TESTED  ####
+def create_report_header():
+    """ Generate formatted header for report """
     header = '{:20}|{:^15}|{:^15}|{:>15}'.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
-    print()
-    print(header)
-    print("-" * len(header))
+    header += '\n'
+    header += ("-" * len(header))
+    return header
 
+
+def create_report_data(donations):
+    """ Generate data needed for report """
+    data = []
     for donor, donor_data in donations.items():
         total_given = sum(donor_data)
         num_gifts = len(donor_data)
         avg_gift = total_given / num_gifts
-        print('{:21}{:>15.2f}{:>16}{:>16.2f}'.format(donor, total_given, num_gifts, avg_gift))
-    print()
+        data.append([donor, total_given, num_gifts, avg_gift])
+    return data
 
 
-def list_donors():
-    # Formatted print of donor names
-    print("\nList of donors:")
+def create_report_rows(data):
+    """ Create formatted data rows for report """
+    rows = ''
+    for donor, total_given, num_gifts, avg_gift in data:
+        rows += '{:21}{:>15.2f}{:>16}{:>16.2f}'.format(donor, total_given, num_gifts, avg_gift) + '\n'
+    return rows
+
+
+def print_report():
+    print(create_report_header())
+    data = create_report_data(donations)
+    print(create_report_rows(data))
+
+
+def create_email(donor, donation):
+    """ Create formatted email thanking donor """
+    letter_details = {'name': donor, 'donation_amount': donation}
+    letter = "\nDear {name},\n\nThank you so very much for your kind donation of ${donation_amount}. We can assure you that it will be put to great use.\n\nBest,\nChris".format(**letter_details)
+    return letter
+
+
+def print_email(donor, donation):
+    print(create_email(donor, donation))
+
+
+def list_donors(donations):
+    """ Create formatted list of donor names """
+    donor_names = "\nList of donors:\n"
     for donor in donations:
-        print(donor)
-    print()
+        donor_names += (donor) + "\n"
+    return donor_names
+
+
+def print_donors():
+    print(list_donors(donations))
 
 
 def quit_menu():
-    # Quit current menu
-    return("quit")
+    """ Quit current menu """
+    return "quit"
 
 
 ### Parameters ###
@@ -94,14 +127,14 @@ donations = {"Bill Gates": [10.50, 123.45, 1111.11],
 # Dictionaries for menu control flow
 main_menu = {
     "1": send_thank_you,
-    "2": create_report,
+    "2": print_report,
     "3": write_letters,
     "q": quit_menu,
 }
 
 thank_you_menu = {
     "1": add_donation,
-    "2": list_donors,
+    "2": print_donors,
     "q": quit_menu,
 }
 
--- students/csdotson/lesson06/test_mailroom_4.py
+++ students/csdotson/lesson06/test_mailroom_4.py
@@ -4,13 +4,6 @@ Unit testing for mailroom
 
 import mailroom_4 as mr
 import pytest
-import sys
-
-donations = {"Bill Gates": [10.50, 123.45, 1111.11],
-             "Jeff Bezos": [7.65, 1000.00],
-             "Paul Allen": [145.90],
-             "John Nordstrom": [45.67, 6519.65],
-             "Mark Zuck": [789.12]}
 
 def test_main_menu_prompt():
     assert mr.main_prompt == "\nWelcome to the Main Menu! What would you like to do?\n1 - Send a Thank You\n2 - Create a Report\n3 - Send letters to everyone\nq - Quit and exit\n--> "
@@ -21,7 +14,7 @@ def test_thank_you_prompt():
 def test_main_menu():
     assert mr.main_menu == {
         "1": mr.send_thank_you,
-        "2": mr.create_report,
+        "2": mr.print_report,
         "3": mr.write_letters,
         "q": mr.quit_menu,
     }
@@ -29,25 +22,32 @@ def test_main_menu():
 def test_thank_you_menu():
     assert mr.thank_you_menu == {
         "1": mr.add_donation,
-        "2": mr.list_donors,
+        "2": mr.print_donors,
         "q": mr.quit_menu,
     }
 
-def test_send_thank_you_prompt(capfd):
-    mr.send_thank_you()
-    out, err = capfd.readouterr()
-    assert out == "\nPlease choose one of the following:\n1 - Add a donation and send thank you message\n2 - Display list of current donors\nq - Return to main menu\n--> "
+def test_donor_list():
+    donations = {"Chris": 100.00, "Joe": 10.99}
+    assert mr.list_donors(donations) == '\nList of donors:\nChris\nJoe\n'
 
+def test_create_donor_email():
+    assert mr.create_email("Chris", 100.00) == '\nDear Chris,\n\nThank you so very much for your kind donation of $100.0. We can assure you that it will be put to great use.\n\nBest,\nChris'
 
-# def test_prompt():
-#     mr.prompt(mr.main_prompt, mr.main_menu)
+def test_create_report_header():
+    assert mr.create_report_header() == 'Donor Name          |  Total Given  |   Num Gifts   |   Average Gift\n---------------------------------------------------------------------'
 
+def test_create_report_data():
+    donations = {"Chris": [10.00, 20.00, 100.00]}
+    data = mr.create_report_data(donations)
+    assert data[0][0] == "Chris"
+    assert data[0][1] == sum(donations["Chris"])
+    assert data[0][2] == len(donations["Chris"])
+    assert data[0][3] == data[0][1] / data[0][2]
 
-# def test_list_donors_function(capfd):
-#     mr.list_donors()
-#     out, err = capfd.readouterr()
-
-
+def test_create_report_rows():
+    donations = {"Chris": [10.00, 20.00, 100.00]}
+    data = mr.create_report_data(donations)
+    assert mr.create_report_rows(data) == 'Chris                         130.00               3           43.33\n'
 
 def test_quit_menu_function():
     assert mr.quit_menu() == "quit"
