3c2b897077b71659e4c9d0aa9c2c641fb9c496c5
Matt Hudgins <mhudgin1@uw.edu>
2018-05-15 20:34:17 -0700

    Starting the kata asignment

---
+++ students/Matt_Hudgins/Lesson04/kata.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# chmod +x kata.py needs to be performed before executable
+
+'''
+    File Name: kata.py
+    Author: Matt Hudgins
+    Date created: 5/15/18
+    Date last modified: 5/15/18
+    Python Version 3.6.4
+'''
+
+import sys
+
+# Starting the kata fourteen lesson
