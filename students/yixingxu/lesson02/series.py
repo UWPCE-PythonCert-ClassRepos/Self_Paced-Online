8360e40f2982f72327c9528b3b9aa649474e472b
Yixing Xu <xuyixing2005@gmail.com>
2018-05-17 17:57:42 -0700

    create series.py to calculate fibonacci number

---
+++ students/yixingxu/lesson02/series.py
@@ -0,0 +1,11 @@
+def fibonacci(arg1):
+	"""
+	Return nth fibonacci number
+	:param arg1: the n th number
+	"""
+	if arg1 == 0
+		return 0
+	elif arg1 ==1
+		return 1
+	elif type(arg1) == int and arg1 >1
+		return fibonacci(arg1-1) + fibonacci(arg1-2)
