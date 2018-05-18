e03722b41c812c2b886428286ac12dbe27cced2e
Micah Braun <mcbraun@uw.edu>
2018-05-18 14:12:31 -0700

    Final edits for list_lab.py

--- students/MicahBraun/Lesson 03/list_lab.py
+++ students/MicahBraun/Lesson 03/list_lab.py
@@ -1,60 +1,39 @@
 # ------------------------------------------------------------------------
 # NAME: MICAH BRAUN
 # PROJECT: list_lab.py
-# PURPOSE: 
-# DATE: 05/15/2018
+# PURPOSE: To manipulate data in a list
+# DATE: 05/17/2018
 #
-# DESCRIPTION:
-# 
-# 
-# 
-# 
+# DESCRIPTION: Sequence to move through item(s) in a list and to perform
+#              actions on item(s) to alter the makeup of the list.
 #
 # ------------------------------------------------------------------------
-'''
 
-    - Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
-    - Display the list (plain old print() is fine…).
-    - Ask the user for another fruit and add it to the end of the list.
-    - Display the list.
-    - Ask the user for a number and display the number back to the user and the fruit
-      corresponding to that number (on a 1-is-first basis). Remember that Python uses
-      zero-based indexing, so you will need to correct.
-    - Add another fruit to the beginning of the list using “+” and display the list.
-    - Add another fruit to the beginning of the list using insert() and display the list.
-    - Display all the fruits that begin with “P”, using a for loop.
-
-'''
 # Data -------------------------------------------------------------------
 
 fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]  # instantiate list
 
-
 # -------------------------------------------------------------------------
-
-
-# Processing --------------------------------------------------------------
-
-
-# ---------------------------------------------------------------------------
-
 
 # Display -------------------------------------------------------------------
 
 # SECTION I
 
+print("Section I")
 print(fruit_list)                          # print out items in list
 print()
 
+sec1_list = fruit_list
 add_new = str(input("Enter a new fruit to add to the list: ")).capitalize()  # new item to be added to list
 print()
 
-fruit_list.append(add_new)                 # append item to existing list
-count = len(fruit_list)                    # adjust len(list) to including new entry
+sec1_list.append(add_new)                 # append item to existing list
+count = len(sec1_list)                    # adjust len(list) to including new entry
 
-print(fruit_list)                          # display list
+print(sec1_list)                          # display list
 print()
                                            # solicit user's input for index
+
 item_at_index = int(input("Enter a number between 1 and " + str(count) + " print "
                           "index value at that index: "))
 
@@ -66,20 +45,94 @@ elif item_at_index <= 0:                   # we are indexing at 1 - onward, omit
     print("Indexing starts at 1!")
 else:
     print("Value at index " + str(item_at_index) + ": ", end="")    # print index input to search
-    print(fruit_list[item_at_index - 1])   # search list for index -1 (to account for list indexing-
-    print()                                # -at 0 and this program indexing at 1)
+    print(sec1_list[item_at_index - 1])   # search list for index -1 (to account for list indexing-
+    print()                               # -at 0 and this program indexing at 1)
 
-add_new_plus = "Plum"                      # new value to be added to list
-fruit_list = [add_new_plus] + fruit_list   # add value to beginning of list using '+'
-print(fruit_list)                          # display updated list
+add_new_plus = "Plums"                   # new value to be added to list
+sec1_list = [add_new_plus] + sec1_list   # add value to beginning of list using '+'
+print(sec1_list)                         # display updated list
 print()
 
-add_new_insert = "Strawberry"              # new value to be added to list
-fruit_list.insert(0,add_new_insert)        # add value to beginning of list using .insert()
-print(fruit_list)                          # display updated list
+add_new_insert = "Strawberries"           # new value to be added to list
+sec1_list.insert(0,add_new_insert)        # add value to beginning of list using .insert()
+print(sec1_list)                          # display updated list
 print()
 
-for i in fruit_list:                       # print only items in list that begin with "P"
-    if i[:1] == "P":
-        print(i)
+for element in sec1_list:                 # print only items in list that begin with "P"
+    if element[:1] == "P":
+        print(element)
+print()
 
+# SECTION II
+
+print()
+print("Section II")
+print()
+
+print(sec1_list)                   # display original list
+print()
+
+
+sec1_list.pop()                    # remove last item from list
+print(sec1_list)                   # display list
+print()
+
+val_delete = str(input("Enter a fruit you want to remove from the list: (string value) ")).capitalize()  # entry to delete
+print()
+
+
+if val_delete in sec1_list:            # search list for entry to delete
+    print("Item removed!")
+    sec1_list.remove(val_delete)       # delete entry
+else:
+    print("Value not found in list.")   # if not found, print message
+
+print()
+print(sec1_list)                       # display updated list
+
+# SECTION III
+print()
+print("Section III")
+print()
+
+liked_fruits = []                       # storage container for list vals that are "yes"
+
+for fruit in sec1_list:                # for each item in fruit_list
+    while True:                         # keep looping until list ended
+
+        user_resp = input("Do you like " + str(fruit) + "? ").lower()    # solicit user input
+        print()
+
+        if user_resp != "yes" and user_resp != "no":                # if user doesn't enter "yes" or "no" error msg
+            print("Please only answer with 'yes' or 'no'.")
+            print()
+            continue                          # stay on current item until valid entry
+
+        elif user_resp == "yes":              # if user enters "yes", append value to new list
+            liked_fruits.append(fruit)
+            break                             # break to next item
+
+        else:                                 # if answer is "no", break to next item without saving
+            break
+
+sec1_list = liked_fruits                      # overwrite existing list with liked_fruits
+
+print(sec1_list)                              # print out to user
+
+
+
+# SECTION IV
+
+print()
+print("Section IV")
+print()
+
+reverse_list = []                       # container to hold copy of reversed strings
+
+for item in sec1_list:                  # for item in original list, do this
+    reverse_list.append(item[::-1])     # append reversed strings to container
+
+sec1_list.pop()                         # remove last item from non-reversed list
+print(sec1_list)                        # print regular list
+
+print(reverse_list)                     # print reversed list
