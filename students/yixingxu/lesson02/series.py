79dc2edf1aa54ec0d5a2e350f981bd3246e3b6de
Yixing Xu <xuyixing2005@gmail.com>
2018-05-17 18:15:02 -0700

    finished and tested fibonacci

--- students/yixingxu/lesson02/series.py
+++ students/yixingxu/lesson02/series.py
@@ -3,9 +3,14 @@ def fibonacci(arg1):
 	Return nth fibonacci number
 	:param arg1: the n th number
 	"""
-	if arg1 == 0
+	if arg1 == 0:
 		return 0
-	elif arg1 ==1
+	elif arg1 ==1:
 		return 1
-	elif type(arg1) == int and arg1 >1
+	elif type(arg1) == int and arg1 >1:
 		return fibonacci(arg1-1) + fibonacci(arg1-2)
+	else: print('Please enter valid integer number')
+		
+if __name__ == '__main__' :
+	for i in range(10):
+		print(fibonacci(i))
