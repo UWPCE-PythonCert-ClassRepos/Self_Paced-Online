c2203bf3fe40298741fcb53e8cad66e060f45cdb
Yixing Xu <xuyixing2005@gmail.com>
2018-05-17 17:36:52 -0700

    change & to and

--- students/yixingxu/lesson02/Fizz-Buzz.py
+++ students/yixingxu/lesson02/Fizz-Buzz.py
@@ -1,9 +1,9 @@
 for i in range(1,100):
-	if i%3 ==0 & i%5 ==0:
+	if i%3 ==0 and i%5 ==0:
 		print('FizzBuzz')
-	elif i%5 ==0:
-		print('Buzz')
 	elif i%3 ==0:
 		print('Fizz')
+	elif i%5 ==0:
+		print('Buzz')
 	else: print(i)
 		
