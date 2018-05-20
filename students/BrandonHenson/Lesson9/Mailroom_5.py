36d6e08d8c9aab7875420a72bff3295645fe1d85
Brandon_Henson <Bigbrandonh@gmail.com>
2018-05-20 02:17:13 -0700

    Addressed the issues that were commented on.(I hope)

--- students/BrandonHenson/Lesson9/Mailroom_5.py
+++ students/BrandonHenson/Lesson9/Mailroom_5.py
@@ -1,138 +1,151 @@
 # Brandon Henson
-# 5/17/18
+# 5/20/18
 # Lesson 09 mailroom_5.py
-# Refactoring
+# Fixing issues:Refactoring
+# !/usr/bin/env python3
 
 
-class Donor(object):
+class Donor():
+    def __init__(self, name=None):
+        self._name = name
+        self._donations = []
 
-    donor_history = {'Brandon Henson': [1005.49, 3116.72, 5200],
-                        'Alicia Henson': [21.47, 1500],
-                        'Michael Green': [2400.54],
-                        'Brandon Henson Jr': [355.42, 579.31],
-                        'Kaiya Henson': [636.9, 850.13, 125.23]}
+    @property
+    def name(self):
+        return self._name
 
-    name_list = [i for i in donor_history]
+    @name.setter
+    def name(self, val):
+        self._name = val
 
-    def __init__(self, name, *donation):
-        self.donations = []
-        self.name = name
-        self.donation = donation
-        for i in self.donation:
-            self.donations.append(i)
-        Donor.name_list.append(self.name)
-        Donor.donor_history[self.name] = self.donation
+    @property
+    def donations(self):
+        return self._donations
+
+    @donations.setter
+    def donations(self, val):
+        self._donations = val
 
     @property
     def total(self):
         return sum(self.donations)
 
-    @property
-    def average(self):
-        return self.total/self.num_of_donations
+
+class DonorAll(dict):
+    def __init__(self, donors):
+        self._donors = donors
 
     @property
-    def num_of_donations(self):
-        return len(self.donations)
+    def donors(self):
+        return self._donors
 
+    @donors.setter
+    def donors(self, val):
+        self._donors = val
 
-class All(Donor):
-    def __init__(self):
-        pass
+    def add_donor(self, donor):
+        self.donors[donor.name] = donor.donations
 
-    def all_total(self):
-        list_of_sums = []
-        count = 0
-        for k, v in Donor.donor_history.items():
-            for i in v:
-                count = count+1
-            total = sum(v)
-            list_of_sums.append(total)
-        gt = (sum(list_of_sums))
-        av = gt/count
-        gt = str(round(gt, 2))
-        av = str(round(av, 2))
-        print('The grand total of all donations is ${}.\
- The average amount per donation is ${}.'.format(gt, av))
+    def donate(self, name, donation):
+        if name not in self.donors.keys():
+            new_donor = Donor(name)
+            self.add_donor(new_donor)
+        else:
+            self.donors[name].append(int(donation))
 
-
-def main_menu():
-    arg_dict = {
-        '1': menu_1,
-        '2': menu_2,
-        '3': email_all,
-        '4': exit
-    }
-
-    prompt = '\nSelect an option:\n' \
-             '[1] Send a Thank You\n' \
-             '[2] Create a Report\n' \
-             '[3] Send letters to everyone\n' \
-             '[4] Exit\n' \
-
-    menu_selection(prompt, arg_dict)
-
-
-def menu_selection(prompt, arg_dict):
-    try:
-        while True:
-            response = input(prompt)
-            arg_dict[response]()
-    except KeyError:
-        print("Enter 1,2,3, or 4")
-
-
-def menu_1():
-    try:
-        fullname = input("Enter a full name or 'list' to view all\n")
-        if fullname.lower() == 'list':
-            print(Donor.name_list)
-
-        elif str(fullname) in Donor.donor_history:
-            amount = int(input("Donation amount? \n"))
-            newvalue = Donor.donor_history.get(fullname) + [amount]
-            donor_history[fullname] = newvalue
-            print("Dear {}\nThank you for your generous donation of ${}\nIt will \
-     be put to good use.\n\n Sincerely,\n\
-            -The Team".format(fullname, amount))
-        elif str(fullname) not in Donor.donor_history:
-            amount = int(input("Donation amount? \n"))
-            Donor.donor_history[fullname] = amount
-            print("Dear {}\nThank you for your generous donation\
-     of ${}\nIt will be put to good use.\n\n Sincerely,\n\
-            -The Team".format(fullname, amount))
-    except ValueError:
-        print("Please enter a number for the amount.")
-
-
-def menu_2():
-    try:
-        print("\nDonor Name         |  Total Given | Num Gifts | Average Gift")
-        print("------------------------------------------------------------\n")
-        report = list()
-        for donor, total in Donor.donor_history.items():
-            report.append([donor, sum(total), len(total), sum(total)/len(total)])
-        sorted_report = sorted(report, key=lambda x: -x[1])
-
-        for i in sorted_report:
-            print("{:<20}   ${:>8} {:>8}        ${:<1}".format(
-                i[0], i[1], i[2], i[3]))
-        print(All.all_total(self=All))
-    except TypeError:
-        print()
-
-
-def email_all():
-    for key, values in Donor.donor_history.items():
-        filename = str(key)+'.txt'
-        with open(filename, 'w') as fileobj:
-            total = sum(values)
-            numgifts = len(values)
-            fileobj.write("Dear {}\nThank you for your {} generous donations \
+    def createfile(self, name):
+        for key, values in donor_history.items():
+            filename = str(key)+'.txt'
+            with open(filename, 'w') as fileobj:
+                total = sum(values)
+                numgifts = len(values)
+                fileobj.write("Dear {}\nThank you for your {} generous donations \
 totaling ${}\nThe money will be put to good use.\n\n\
-        Sincerely, \n                -The Team".format(key, numgifts, total))
-        fileobj.close()
+            Sincerely, \n                -The Team".format(key, numgifts, total))
+            fileobj.close()
 
+    def menu_2(self):
+        try:
+            print(f'{"Donor Name":18s} {"|  Total Given":12s} \
+    {"|  Num Gifts  |":14s} {"Average Gift":18s}')
+            print(f'{"-"*62}')
+            [print(f'{name:18s} ${sum(self.donors[name]):10.2f} {len(self.donors[name]):8d}        \
+    ${sum(self.donors[name])/len(self.donors[name]):10.2f}')
+    for name in self.donors.keys()]
+        except ZeroDivisionError:
+            pass
+
+
+def menu_1(donorhistory):
+    while True:
+        name = input("Enter a full name or 'list' to view all\n")
+        if name.lower() == 'list':
+            print(donorhistory.donors.keys())
+        else:
+            break
+
+    success = False
+    while not success:
+        donation = int(input("Donation amount? \n"))
+        try:
+            donorhistory.donate(name, donation)
+            success = True
+        except Exception as e:
+            print(e.args[0])
+            success = False
+    print("Dear {},\nThank you for your generous donation\
+ of ${}\nIt will be put to good use.\n\n Sincerely,\n\
+                -The Team".format(name, donation))
+
+
+def report(donorhistory):
+    donorhistory.menu_2()
+
+
+def menu_1_all(donorhistory):
+    [donorhistory.createfile(name) for name in donorhistory.donors.keys()]
 
 if __name__ == '__main__':
-    main_menu()
+    donor_history = {}
+    donor_history['Brandon Henson'] = [1005.49, 3116.72, 5200]
+    donor_history['Alicia Henson'] = [21.47, 1500]
+    donor_history['Michael Green'] = [2400.54]
+    donor_history['Brandon Henson Jr'] = [355.42, 579.31]
+    donor_history['Kaiya Henson'] = [636.9, 850.13, 125.23]
+
+    DonorDict = DonorAll(donor_history)
+
+    response = input('\nSelect an option:\n'
+             '[1] Send a Thank You\n'
+             '[2] Create a Report\n'
+             '[3] Send letters to everyone\n'
+             '[4] Exit\n')
+    try:
+        int(response)
+    except:
+        while not response.isdigit():
+            print("Enter 1,2,3, or 4.")
+            print()
+            response = input('\nSelect an option:\n'
+                             '[1] Send a Thank You\n'
+                             '[2] Create a Report\n'
+                             '[3] Send letters to everyone\n'
+                             '[4] Exit\n')
+    arg_dict = {1: menu_1, 2: report, 3: menu_1_all}
+    while int(response) != 4:
+        try:
+            arg_dict.get(int(response))(DonorDict)
+        except (TypeError, ValueError):
+            print('Enter 1,2,3, or 4')
+            print()
+            response = input('\nSelect an option:\n'
+             '[1] Send a Thank You\n'
+             '[2] Create a Report\n'
+             '[3] Send letters to everyone\n'
+             '[4] Exit\n')
+        else:
+            response = input('\nSelect an option:\n'
+             '[1] Send a Thank You\n'
+             '[2] Create a Report\n'
+             '[3] Send letters to everyone\n'
+             '[4] Exit\n')
