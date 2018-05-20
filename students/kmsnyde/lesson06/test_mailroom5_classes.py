01b6c9fee0d0017909c6db6d1e9d85c5993f6e1b
kmsnyde <kmsnyder2@verizon.net>
2018-05-20 14:09:39 -0400

    lesson09 mailroom with classes

---
+++ students/kmsnyde/lesson06/test_mailroom5_classes.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 17:58:03 2018
+
+@author: Karl M. Snyder
+"""
+
+import mailroom5_classes
+
+
+
+
+
+
+
+if __name__ = "__main__":
+    #Default names.
+    donor_dict = defaultdict(list, {'Karl Stick': [10, 20, 30],
+              'Kate Stam': [5, 30],
+              'Christine Goose': [21],
+              'Matt Hen':  [40, 5, 11],
+              'Zumi Was': [32]})
+    
+    #Start user input.
+    print('\n', 'Please select a number from the following choices:\n')
+    
