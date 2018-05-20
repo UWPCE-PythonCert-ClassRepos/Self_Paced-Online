cbd65d6730d822249e2f3268b4fcdf4cd4b5d9ba
seaneightysix <sean.eightysix@GMAIL.com>
2018-05-20 15:57:10 -0700

    modified mailroom_part3.py Need help with a few problems

--- students/Sean_Tasaki/Lesson05/mailroom_part3.py
+++ students/Sean_Tasaki/Lesson05/mailroom_part3.py
@@ -1,13 +1,12 @@
 """
 Sean Tasaki
-5/18/2018
+5/20/2018
 Lesson05.mailroom_part_3
 """
-
-donor_full_list = [['Bob Dylan', 120000.00, 3, 40000.00], ['Leonard Cohen', 600.00, 2, 300.00], ['Soren Kierkegaard', 1000.00, 2, 500.00], ['Italo Calvino', 20000.00, 4, 5000.00], ['Florence Feist', 300.00, 3, 100.00]]
+donor_full_dict = {'Bob Dylan': [20000.00, 3, 40000.00], 'Leonard Cohen' : [600.00, 2, 300.00], 'Soren Kierkegaard' : [1000.00, 2, 500.00], 'Italo Calvino' : [20000.00, 4, 5000.00], 'Florence Feist' : [300.00, 3, 100.00]}
 donor_name_list = []
-for x in donor_full_list:
-    donor_name_list.append(x[0])
+for donor in donor_full_dict:
+    donor_name_list.append(donor)
 
 
 def main_menu():
@@ -36,15 +35,15 @@ def thank_you():
             main_menu()
         
         elif name.title() in donor_name_list:
-            for donor in donor_full_list:
-                if donor[0].casefold() == name.casefold():
+            for donor in donor_full_dict:
+                if donor.casefold() == name.casefold():
                     amount = input(f"{name.title()} is a previous donor. Enter the donation amount.\n>> ")
                     if amount.lower() == 'q':
                         thank_you()
                     else:
-                        donor[1] += float(amount)
-                        donor[2] += 1
-                        donor[3] = float(donor[1]/donor[2])
+                        donor_full_dict[donor][0] += float(amount)
+                        donor_full_dict[donor][1] += 1
+                        donor_full_dict[donor][2] = float(donor[1]/donor[2])
                         print(f"Thank you {name.title()} for your genereous donation of ${float(amount):.2f}.")
                         thank_you()
                
@@ -53,7 +52,7 @@ def thank_you():
             if amount.lower() == 'q':
                 thank_you()
             else:            
-                donor_full_list.append([name.title(), float(amount), 1, float(amount)])
+                donor_full_dict.update({name.title() : [float(amount), 1, float(amount)]})
                 donor_name_list.append(name.title())
                 print(f"Thank you {name.title()} for your genereous donation of ${float(amount):.2f}.")
                 thank_you()
@@ -67,20 +66,24 @@ def quit():
     return "exit menu"
 
 def create_report():
-    donor_full_list.sort(key = lambda x: x[1], reverse = True)
+    lis = []
+    for value in donor_full_dict.values():
+        lis.append(value)
+    lis.sort(key = lambda x: x[0], reverse = True)
+    #lis.sort(key = lambda x: x[0], reverse = True)
     print("Donor Name           |   Total Given   |   Num Gifts  |    Average Gift")
     print("-----------------------------------------------------------------------")
-    for x in donor_full_list:
-        print(f"{x[0]:20} ${x[1]:>17.2f}    {x[2]:>6}     ${x[3]:>16.2f}")  
+    for donor in donor_full_dict:
+        print(f"{donor:20} ${donor_full_dict[donor][0]:>17.2f}    {donor_full_dict[donor][1]:>6}     ${donor_full_dict[donor][2]:>16.2f}")  
     main_menu()
 
 def create_thank_you_letters():
     # Creates a letter for each donor that gets a file in the working dir based on donor's name.
     try:
         # Separate donors based on whether they gave multiple donations or a single donation
-        file_donor_names=  [(donor[0] + '.txt') for donor in donor_full_list]
+        file_donor_names=  [(donor + '.txt') for donor in donor_full_dict]
         file_object_for_donations = [open(file, 'w') for file in file_donor_names]
-        for count, donor in enumerate(donor_full_list):
+        for count, donor in enumerate(donor_full_dict):
             file_object_for_donations[count].write(thank_you_letter_template(donor)) 
         
 
@@ -92,11 +95,11 @@ def create_thank_you_letters():
     finally: 
         for file in file_object_for_donations:
             file.close()
-def thank_you_letter_template(list):
-    if list[2] > 1:
-        return "Dear {},\nThank you for your {} generous donations of ${:.2f}. Your support helps our charity stay in business.\n\nSincerely,\n-The Team".format(list[0], list[2], list[1])
+def thank_you_letter_template(key):
+    if donor_full_dict[key][1] > 1:
+        return "Dear {},\nThank you for your {} generous donations of ${:.2f}. Your support helps our charity stay in business.\n\nSincerely,\n-The Team".format(key, donor_full_dict[key][1], donor_full_dict[key][0])
     else:
-        return "Dear {},\nThank you for your generous donation of ${:.2f}. Your support helps our charity stay in business.\n\nSincerely,\n-The Team".format(list[0], list[2], list[1])
+        return "Dear {},\nThank you for your generous donation of ${:.2f}. Your support helps our charity stay in business.\n\nSincerely,\n-The Team".format(key, donor_full_dict[key][1], donor_full_dict[key][0])
 
 if __name__ == '__main__':
     main_menu()
