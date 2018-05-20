10ea3a916ee7fd8a7e8cd5c596abfb8bb2d77e68
= <=>
2018-05-20 11:29:15 -0500

    tests written for step 2

--- students/TrkCat/Lesson07/test_html_render.py
+++ students/TrkCat/Lesson07/test_html_render.py
@@ -23,3 +23,29 @@ def test_step1():
     f = StringIO()
     el.render(f, 1)
     assert f.getvalue() == ('    <html>\n        hi\n        bye\n    </html>')
+
+def test_step2();
+    html_el = Html()
+    body_el = Body()
+    p_el = P()
+    assert html_el.tag = 'html'
+    assert body_el.tag = 'body'
+    assert p_el.tag = 'p'
+    
+    html_el.append('hi')
+    f = StringIO()
+    el.render(f)
+    assert f.getvalue() == ('<html>\n    hi\n</html>')
+    
+    page = hr.Html()
+    body = hr.Body()
+        body.append(hr.P("Some text."))
+        body.append(hr.P("More text."))
+    page.append(body)
+    f = StringIO()
+    page.render(f)
+    expected = ('<hmtl>\n    <body>\n        <p>\n            Some text.\n'
+                '            More text.\n        </p>\n    </body>\n    '
+                '</html>'
+                )
+    assert f.getvalue() == expected
