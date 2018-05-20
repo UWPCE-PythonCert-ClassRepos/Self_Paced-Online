caae3c1512e1e66e2bc2bba1f60b564dc40d8ca6
= <=>
2018-05-20 11:34:28 -0500

    corrected syntax and formatting issues for step 2 tests

--- students/TrkCat/Lesson07/test_html_render.py
+++ students/TrkCat/Lesson07/test_html_render.py
@@ -1,4 +1,4 @@
-#Test file for html_render.pytest
+# Test file for html_render.pytest
 
 import pytest
 from io import StringIO
@@ -10,37 +10,38 @@ def test_step1():
     assert el.tag == 'html'
     assert el.indent == 0
     assert el.content == []
-    
+
     el.append('hi')
     assert el.content == ['hi']
     el.append('bye')
     assert el.content == ['hi', 'bye']
-    
+
     f = StringIO()
     el.render(f)
     assert f.getvalue() == ('<html>\n    hi\n    bye\n</html>')
-    
+
     f = StringIO()
     el.render(f, 1)
     assert f.getvalue() == ('    <html>\n        hi\n        bye\n    </html>')
 
-def test_step2();
+
+def test_step2():
     html_el = Html()
     body_el = Body()
     p_el = P()
     assert html_el.tag = 'html'
     assert body_el.tag = 'body'
     assert p_el.tag = 'p'
-    
+
     html_el.append('hi')
     f = StringIO()
     el.render(f)
     assert f.getvalue() == ('<html>\n    hi\n</html>')
-    
+
     page = hr.Html()
     body = hr.Body()
-        body.append(hr.P("Some text."))
-        body.append(hr.P("More text."))
+    body.append(hr.P("Some text."))
+    body.append(hr.P("More text."))
     page.append(body)
     f = StringIO()
     page.render(f)
