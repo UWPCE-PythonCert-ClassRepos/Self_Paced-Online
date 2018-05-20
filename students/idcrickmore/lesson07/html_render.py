357129bb619f60fbd6869f0b7494fa8d39df838e
Ian David Crickmore <idcrickmore@gmail.com>
2018-05-20 13:37:31 -0700

    comleted lesson 07 including unit testing

--- students/idcrickmore/lesson07/html_render.py
+++ students/idcrickmore/lesson07/html_render.py
@@ -1,20 +1,21 @@
 """renders some simple HTML code"""
 
-class Element():
 
+class Element():
     tag_name = "html"
 
-    def __init__(self, content: str=None, **kwargs):
+    def __init__(self, content: str = None, **kwargs):
         self.content = [content] if content else []
         self.attributes = kwargs
 
     def append(self, content):
-        #append method that can add another string to the content
+        # append method that can add another string to the content
         self.content.append(content)
 
     def render(self, file_out, cur_ind=""):
         if self.attributes != {}:
-            attributes = " ".join([' %s="%s"' % (k, v) for k, v in self.attributes.items()])
+            attributes = " ".join([' %s="%s"' % (k, v)
+                                  for k, v in self.attributes.items()])
             open_tag = f"<{self.tag_name}{attributes}>"
             close_tag = f"</{self.tag_name}>"
             print(open_tag)
@@ -42,77 +43,67 @@ class OneLineTag(Element):
                 file_out.write(f"<{self.tag_name}>")
                 file_out.write(item + "\n")
                 file_out.write(f"</{self.tag_name}>")
-            
-        
+
+
 class SelfClosingTag(Element):
-    #To Do: raise some kind of exception for content
-    
     def render(self, file_out, cur_ind=""):
         tag_close = "/"
         print(f"<{self.tag_name}{tag_close}>")
         file_out.write(f"<{self.tag_name}{tag_close}>")
-        
-        
+
+
 class Html(Element):
     tag = 'html'
-
 
     def render(self, file_out, cur_ind=""):
         file_out.write("<!DOCTYPE html>\n")
         Element.render(self, file_out, cur_ind="")
-    
-    
+
+
 class Body(Element):
     tag_name = "body"
-    
+
 
 class P(Element):
     tag_name = "p"
 
-    
+
 class Head(Element):
     tag_name = "head"
 
-    
+
 class Title(OneLineTag):
     tag_name = "title"
 
-    
+
 class Hr(SelfClosingTag):
     tag_name = 'hr'
-   
-   
+
+
 class Br(SelfClosingTag):
     tag_name = 'br'
 
 
 class A(Element):
     tag_name = 'a'
-    
+
     def __init__(self, link, content):
         super().__init__(content, href=link)
 
 
 class Ul(Element):
     tag_name = 'ul'
-    
-    
-class  Li(Element):
+
+
+class Li(Element):
     tag_name = 'li'
-    
-    
+
+
 class H(OneLineTag):
-    def __init__(self, level_number, content = None):
+    def __init__(self, level_number, content=None):
         super().__init__(content)
         self.tag_name = "h%s" % level_number
 
+
 class Meta(SelfClosingTag):
     tag = 'meta'
-
-
-    
-
-
-
-        
-        
--- students/idcrickmore/lesson07/test_html_render.py
+++ students/idcrickmore/lesson07/test_html_render.py
@@ -1,12 +1,38 @@
 """tests the html_render.py file"""
 
-from html_render import Element
+import html_render as hr
+from io import StringIO
+import unittest
 
 
-def test_append():
-	if Element.append:
-        return True
+class TestCalc(unittest.TestCase):
 
-def test_render():
-    if Element.render:
-        return True
+    def test_element(self):
+        test = hr.Element('test content', keyword='value')
+        self.assertEqual(test.content, ['test content'])
+        self.assertEqual(test.attributes, {'keyword': 'value'})
+
+    def test_append(self):
+        test = hr.Element('Some content.')
+        test.append('Some more content.')
+        self.assertEqual(test.content, ['Some content.', 'Some more content.'])
+
+    def test_render(self):
+        test = hr.Element('Some content.')
+        f = StringIO()
+        test.render(f)
+        self.assertEqual(f.getvalue(), '<html>Some content.\n</html>')
+
+    def test_OneLineTag(self):
+        test = hr.OneLineTag('Some content.')
+        test.tag_name = 'title'
+        f = StringIO()
+        test.render(f)
+        self.assertEqual(f.getvalue(), '<title>Some content.\n</title>')
+
+    def test_SelfClosingTag(self):
+        test = hr.SelfClosingTag('Some content.')
+        test.tag_name = 'hr'
+        f = StringIO()
+        test.render(f)
+        self.assertEqual(f.getvalue(), '<hr/>')
