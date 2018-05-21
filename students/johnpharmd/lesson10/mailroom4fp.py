abd5fe627b2a4fee843230d2f9c964cec0c2c5f6
johnpharmd <jhumphre@uw.edu>
2018-05-20 17:17:28 -0700

    added run_projection() stub

--- students/johnpharmd/lesson10/mailroom4fp.py
+++ students/johnpharmd/lesson10/mailroom4fp.py
@@ -174,15 +174,15 @@ def challenge(donations, factor):
     return donations * factor
 
 
-def challenge_map(*args, **min_and_max):
-    factor = input('Enter a factor: ')
-    if not min_and_max:
-        min_and_max_text1 = """
-Enter a minimum donation to match: """
-        min_and_max_text2 = """
-Now a maximum donation to match: """
-        min_and_max = {'min_donation': int(input(min_and_max_text1)),
-                       'max_donation': int(input(min_and_max_text2))}
+def challenge_map(factor, **min_and_max):
+    # factor = input('Enter a factor: ')
+    #     if not min_and_max:
+    #         min_and_max_text1 = """
+    # Enter a minimum donation to match: """
+    #         min_and_max_text2 = """
+    # Now a maximum donation to match: """
+    #         min_and_max = {'min_donation': int(input(min_and_max_text1)),
+    #                        'max_donation': int(input(min_and_max_text2))}
     copy_donors_amts = dict(donors_amts)
     donors_list = []
     donations_list = []
@@ -207,18 +207,23 @@ Now a maximum donation to match: """
     new_donors_amts_zip = zip([l[1] for l in amts_donors_list], donations_map)
     for donor_tuple in new_donors_amts_zip:
         copy_donors_amts[donor_tuple[0]]['donations'] = donor_tuple[1]
-    print(copy_donors_amts)
+    return copy_donors_amts
+
+
+def run_projection(donor, contrib_ceiling=None, contrib_floor=None, factor=1):
+    pass
 
 
 def program_run():
-    menu_dict = {'1': prepare_ty, '2': get_report,
-                 '3': send_letters, '4': challenge_map, 'q': sys.exit}
+    menu_dict = {'1': prepare_ty, '2': get_report, '3': send_letters,
+                 '4': challenge_map, '5': run_projection, 'q': sys.exit}
     main_text = '\n'.join((
         'Choose from the following:',
         '"1" - Send a "Thank You",',
         '"2" - Create a Report,',
         '"3" - Send Letters to All Donors,',
-        '"4" - Donation Match Challenge, or',
+        '"4" - Donation Match Challenge,',
+        '"5" - Run Projection, or'
         '"q" to Quit: '
       ))
     while True:
