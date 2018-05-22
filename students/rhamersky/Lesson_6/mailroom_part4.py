ec7034446df2df1dd5320549759c5f5ccacf4fe9
Ryan Hamersky <ryan.hamersky@gmail.com>
2018-05-21 22:40:59 -0700

    Updated Lesson 6. Ready for review.

--- students/rhamersky/Lesson_6/mailroom_part4.py
+++ students/rhamersky/Lesson_6/mailroom_part4.py
@@ -9,44 +9,40 @@
 #      Reduced the amount of unnecessary print() statements.
 #
 # Rev: B - 05/19/2018 updated code to reflect comments made by the instructor in mailroom_part3.py.
+# Rev: C - 05/21/2018 updated code to reflect comments made on 05/20/2018.
+#      Changed tuple data set to dictionary data set. Refactored program.
 
 # -----Data Section-----
 import os
 str_name = str()    # --> User's name
-int_option = int()  # --> User's option.
-tup_donor_names = (["William Gates, III",1,10000.0],["Mark Zuckerberg",1,20000.0],
-                   ["Jeff Bezos",1,1200.0],["Paul Allen",1,5000.0],
-                   ["Mike Scott",1,25.0])  # --> Tuple that stores all donor's information
+int_user_input = int()  # --> User's option.
+dic_donors = {"William Gates, III":[10000.0, 1], "Mark Zuckerberg":[20000.0, 1],
+                   "Jeff Bezos":[1200.0,1],"Paul Allen":[5000.0,1],
+                   "Mike Scott":[25.0,1]} # --> Dic that stores all donor's information
 str_salutation = ""   # --> Email salutation
 str_body = ""         # --> Email body
 str_valediction = ""  # --> Email valediction
 
 
 # -----Process Section-----
-#  Methods defined in the menu class are related to general menu checks or options based on user inputs.
-
-class Menu():
-    @staticmethod
-    def menu_options():
-        '''
-        Try and except block used on User's option choice to check for integers.
-        :return: Returns int data (1-4) which is used to determine what menu option will be executed.
-        '''
-        while True:
-            try:
-                int_choice = int(input(str_name + ", please select an option above. "))
-            except ValueError:
-                print(str_name + ", not a valid option. \n")
-            # Verify that the user selected an option 1 through option 4.
+def menu_options():
+    '''
+    Try and except block used on User's option choice to check for integers.
+    :return: Returns int data (1-4) which is used to determine what menu option will be executed.
+    '''
+    while True:
+        try:
+            int_choice = int(input(str_name + ", please select an option above. "))
+        except ValueError:
+            print(str_name + ", not a valid option. \n")
+        # Verify that the user selected an option 1 through option 4.
+        else:
+            if 1 <= int_choice <= 4:
+                break
             else:
-                if int_choice in menu_dispatch:
-                    break
-                else:
-                    print("Integer selected is not a valid option. "
-                          "Please, enter an integer and corresponds to the options given. \n")
-        return int_choice
-############## END OF MENU CLASS ######################
-
+                print("Integer selected is not a valid option. "
+                      "Please, enter an integer and corresponds to the options given. \n")
+    return int_choice
 
 # Methods in the sub_menu class will execute code based off of the user's sub-menu choice.
 
@@ -55,94 +51,75 @@ class SubMenu():
     @staticmethod
     def send_thankyou():
         '''
-        This method will create a list of donors, allow new donors to be added, store donations, and print a thank you
+        This method will  allow new donors to be added, store donations, and print a thank you
         statement to send to the donor by mail.
         :return:
         '''
-        bool_flag = True  # --> Used as a flag.
         donor_name = input("Please, enter full name of donor. ").title()
+        global dic_donors
 
-        # Appends the donor list.
-        if donor_name == "List":
-            # --> Verifies that there is at least a donor is stored as a list row in the table tuple.
-            if len(tup_donor_names) > 0:
-                donor_list_creation()  # --> Creates a list of donors
-                bool_flag = False  # --> Is used as a flag on the while loop below
-            else:
-                print("\n"+"There are no donors in list.")  # --> Let's the user know that a donor list does not exist.
-                bool_flag = False  # --> Is used as a flag on the while loop below.
 
         # Gathers donation information from the user and prints a thank-you message for the donor.
-        while bool_flag:
+        while True:
+            # Appends the donor list.
+            if donor_name == "List":
+                # --> Verifies that there is at least a donor is stored as a list row in the table tuple.
+                if len(dic_donors) > 0:
+                    print_donor_names()  # --> Prints a list of donors
+                    break
+                else:
+                    print(
+                        "\n" + "There are no donors in list.")  # --> Let's the user know that a donor list does not exist.
+                    break
+
             try:
                 flo_donation_amount = float(input(str_name + ", please enter donation amount. "))
 
                 # Updates donor tuple
-                update_donor(donor_name, flo_donation_amount)
+                dic_donors = update_donor(donor_name, flo_donation_amount)
 
                 # String formatting for a brief thank-you to donor.
                 thankyou_message = "\n"+"{}, thank you for donating ${:,.2f}.".format(donor_name, flo_donation_amount)
                 print(thankyou_message)
+                break
 
             except ValueError:  # --> Allows the program to continue if the user does not enter the right data type.
                 print(str_name + ", not a valid amount. \n")
                 continue
 
-            else:
-                break
-
     @staticmethod
     def create_report():
         '''
-        This method takes the table tuple and creates a summary report for the user (name, number of gifts,
-        total lifetime amount in gifts, and the average amount given. The code sorts the list rows into
-         the largest amount amount give to the least amount given.
+        This method will create a report for the user with the following columns:
+        donor's name, number of gifts, average gift amount, and lifetime donations.
         :return:
         '''
-        lifetime_donations = int()
-        donation_count = int()
-        lst_table = []
-        # Creates a new list [name, number of gifts, average amount given, total lifetime amount in gifts]
-        # to be used to create the summary chart.
-        for row in tup_donor_names:
-            for value in range(2, len(row)):
-                lifetime_donations += row[value]
-                donation_count += 1
-            lst_table.append([row[0], row[1], lifetime_donations/donation_count, lifetime_donations])
-            lifetime_donations = 0
-            donation_count = 0
-        # Reverses the list
-        [row.reverse() for row in lst_table]  # --> Added list comprehension
-
-        # Shorts the list form largest to smallest on the first element of each row.
-        lst_table.sort(reverse=True)
-
-        index = 0
-        # Formatting the money elements and changing all int() & float() data to str() data
-        # to use for creating the chart.
-        for row in range(len(lst_table)):
-            lst_table[row][index] = "${:,.2f}".format(lst_table[row][index])  # --> Total amount for gifts
-            lst_table[row][index + 1] = "${:,.2f}".format(lst_table[row][index + 1])  # --> Avg. gift amount
-            lst_table[row][index + 2] = str(lst_table[row][index + 2])  # --> Number of gifts
-        # Reverses list
-        [row.reverse() for row in lst_table]  # --> Added list comprehension
+        longest_entry = float()
+        # Finds the max amount of characters for all items stored in dic_donors.
+        for name in dic_donors:
+            entry_length = len(name)
+            if longest_entry < entry_length:
+                longest_entry = entry_length
+            for index in range(0,1):
+                entry_length = len(str(dic_donors[name][index]))
+                if longest_entry < entry_length:
+                    longest_entry = entry_length
 
         lst_headers = ["Donor_Name", "Num_Gifts", "Average_Gift", "Total_Given"]  # --> List of headers for the chart.
-        # Calculating the max length of all the elements.
-        length1 = max(len(word) for word in lst_headers) + 10  # --> Max length for the header list
-        length2 = max(len(word) for row in lst_table for word in row)  # --> Max length for the donor table
-        # Calculates the max between the header list and the donor list
-        max_length = max(length1, length2)
+
         # Creates the header for the chart
         for heading in lst_headers:
-            print(("{0:{padding}}".format(heading, padding=max_length)), end=" ")
+            print(("{0:{padding}}".format(heading, padding=longest_entry)), end=" ")
         print()
+
         # Creates the donor rows for the chart.
-        for row in lst_table:
-            print(("{0:{padding}}".format(row[0], padding=max_length)), end=" ")
-            print(("{0:{padding}}".format(row[1], padding=max_length)), end=" ")
-            print(("{0:{padding}}".format(row[2], padding=max_length)), end=" ")
-            print(("{0:{padding}}".format(row[3], padding=max_length)), end=" ")
+        sorted_dic = dic_sort()
+        for name in sorted_dic:
+            donation_avg = dic_donors[name][0] / dic_donors[name][1]
+            print(("{0:{padding}}".format(name, padding=longest_entry)), end=" ")
+            print(("{0:{padding}}".format("{}".format(dic_donors[name][1]), padding=longest_entry)), end=" ")
+            print(("{0:{padding}}".format("${:,.2f}".format(donation_avg), padding=longest_entry)), end=" ")
+            print(("{0:{padding}}".format("${:,.2f}".format(dic_donors[name][0]), padding=longest_entry)), end=" ")
             print()
 
 ########### END OF SUB_MENU CLASS###############
@@ -154,34 +131,46 @@ class Files():
     def write_email(donors):
         '''
         Writes emails to the donors and saves them to text files.
-        :param donors: Tuple table that stores all the donor's donation information.
+        :param donors: Dictionary that stores all the donor's donation information.
         :return:
         '''
         for donor_name in donors:
             # Open text file and assign a mode to the file.
-            new_file = open(donor_name[0] + ".txt", mode="w")
-            new_file.write(Files.donor_email(donor_name[0], lifetime_donations(tup_donor_names)))
+            new_file = open(donor_name + ".txt", mode="w")
+            new_file.write(Files.donor_email(donor_name))
             new_file.close()
-            print(donor_name[0] + " saved to "+donor_name[0]+".txt.")
+            print(donor_name + " saved to "+donor_name+".txt.")
 
 
     @staticmethod
-    def donor_email(donor, dictionary):
+    def donor_email(donor):
         '''
         Formats the email with donor's name and total lifetime donations.
-        :param donor: Tuple table that stores all the donor's donation information.
+        :param donor: Dictionary that stores all the donor's donation information.
         :param dictionary: A dictionary that stores the donor as the 'key' and the lifetime donation as the 'value'.
         :return: The concatenation how the three strings.
         '''
         str_salutation = "Dear {},".format(donor)
         str_body = "\n\n"+"Thank you for generosity! " \
                    "\n\n"+"This donation will be put to great use!" \
-                   "\n\n"+"Your current lifetime donation is "+dictionary[donor]+"."
+                   "\n\n"+"Your current lifetime donation is "+"${:,.02f}".format(dic_donors[donor][0]) + "."
         str_valediction = "\n\n"+"Sincerely," \
                           "\n\n"+"The Team"
         thankyou_message = str_salutation+str_body+str_valediction
         return thankyou_message
 ############# END OF FILE CLASS #################
+
+def dic_sort():
+    '''
+    Takes a dictionary and creates a sorted tuple using the first element of the value list.
+    It then puts the sorted data into a new dictionary.
+    :return:
+    '''
+    sorted_tuple = sorted(dic_donors.items(), key=lambda x: x[1], reverse=True)
+    new_dic = {}
+    for items in sorted_tuple:
+        new_dic[items[0]] = [items[1][0],items[1][1]]
+    return new_dic
 
 def update_donor(donor_name, donation_amount):
     '''
@@ -190,100 +179,35 @@ def update_donor(donor_name, donation_amount):
     :param donation_amount: The amount of money the donor donated.
     :return:
     '''
-    global tup_donor_names
     # Creates a donor name list
-    lst_donor_name = [tup_donor_names[row][0] for row in range(len(tup_donor_names))]  # --> Added list comprehension
+    #lst_donor_name = [dic_donors[row][0] for row in range(len(dic_donors))]  # --> Added list comprehension
     # Checks to see if donor has given before and updates history
-    if donor_name in lst_donor_name:
-        index = lst_donor_name.index(donor_name)  # --> Finds index of donor in  list
-        tup_donor_names[index][1] = tup_donor_names[index][
+    dic_new_donor_names = dic_donors
+    if donor_name in dic_donors:
+        dic_new_donor_names[donor_name][1] = dic_donors[donor_name][
                                           1] + 1  # --> Increase amount of times donor donated
-        tup_donor_names[index].append(donation_amount)  # --> Adds donation to history
+        dic_new_donor_names[donor_name][0] = dic_donors[donor_name][0] + donation_amount  # --> Adds donation to history
     # Adds new donor to the donor list
     else:
-        tup_new_donor = ([donor_name, 1, donation_amount],)
-        tup_donor_names += tup_new_donor
+        dic_new_donor_names[donor_name] = [donation_amount, 1]
+    return dic_new_donor_names
 
-def donor_list_creation():
+def print_donor_names():
     '''
-    Creates a donor list if the user types "list" the the program prompts the user to input a donor.
+    Prints donors names if the user types "list" when the program prompts the user to input a donor.
     :return:
     '''
     # Creates a list just of donor names
-    lst_donors = [row[0] for row in tup_donor_names]  # --> Added list comprehension
-    lst_donors.sort()  # --> Sorts donors alphabetically by first name
-    lst_headers = ["Donor_Name"]  # --> List for header
-    length1 = max(len(word) for word in lst_headers) + 10  # --> Max length for header plus 10 (for buffer)
-    length2 = max(len(word) for word in lst_donors)  # --> Max length for donor name
-    max_length = max(length1, length2)  # --> Max length
-    # Creating the header for the list
-    for heading in lst_headers:
-        print("\n"+("{0:{padding}}".format(heading, padding=max_length)), end=" ")
-    # Creating the donor list
-    for row in lst_donors:
-        print("\n"+("{0:{padding}}".format(row, padding=max_length)), end=" ")
-        print()
-
-def send_thankyou_option():
-    '''
-    Calls a method to add donors and/or donations to the master donor tuple table when the user inputs '1'.
-    :return:
-    '''
-    # User selects option 1. Can display a list of the donors and where the user can add donors and donations.
-    print()  # --> Formatting
-    SubMenu.send_thankyou()
-
-def create_report_option():
-    '''
-    Calls a method to create a summary report for the user when the user inputs '2'.
-    :return:
-    '''
-    # User selects option 2. Creates a donation summary for the user.
-    print()  # --> Formatting
-    try:
-        SubMenu.create_report()
-    except ValueError:
-        print("There are no donors on the list to create a report.")
-
-def send_letters_option():
-    '''
-    Calls a method to write and save emails to donors when the user inputs '3'.
-    :return: This function does not return anything. Return is just used to clearly show where this function ends.
-    '''
-    print()  # --> Formatting
-    Files.write_email(tup_donor_names)
-
-def exit_program_option():
-    '''
-    Exits the program when the user inputs '4'.
-    :return: Returns 'exit' which is the flag word to breakout of the program.
-    '''
-    print()  # --> Formatting
-    return "exit"
-
-def lifetime_donations(donors):
-    '''
-    Creates a dictionary that stores the donor's name as the 'key' and the lifetime donation as the 'value'.
-    :param donors: Tuple table that stores all the donor's donation information.
-    :return: Returns the newly created dictionary with the donor's name:lifetime donation
-    '''
-    # Local variables used for this function.
-    lifetime_donations = int()
-    dict_donor_donations = {}
-    # Creates a dictionary (donor's name:lifetime donation)
-    for donor in donors:
-        for donation in range(2, len(donor)):
-            lifetime_donations += donor[donation]
-        lifetime_donations = "${:,.2f}".format(lifetime_donations)  # --> Formats the total amount for gifts
-        dict_donor_donations[donor[0]] = lifetime_donations
-        lifetime_donations = int()  # --> Resets lifetime donations.
-    return dict_donor_donations
+    print("\n"+"Donor")
+    for name in dic_donors:
+        print(name)
 
 def main():
     '''
     This is the main function. This will run the full program.
     :return:
     '''
+    global dic_donors
     while True:
         # User menu to update the priority list.
         print()  # --> Formatting
@@ -292,17 +216,31 @@ def main():
               "\t\t2) Create a Report\n\n"
               "\t\t3) Send letters to everyone\n\n"
               "\t\t4) Quit\n")
+
         # Error handles user's option choice.
-        int_option = Menu.menu_options()
-        if menu_dispatch[int_option]() == "exit":
+        int_user_input = menu_options()
+
+        # User selects option 1. Can display a list of the donors and where the user can add donors and donations.
+        if int_user_input == 1:
+            print()  # --> Formatting
+            SubMenu.send_thankyou()
+            continue
+
+        # User selects option 2. Creates a donation summary for the user.
+        elif int_user_input == 2:
+            print()  # --> Formatting
+            SubMenu.create_report()
+
+        # User selects option 3. Creates text files for thank-yous to donors.
+        elif int_user_input == 3:
+            Files.write_email(dic_donors)
+            print()  # --> Formatting
+
+        # User selects option 4. Allows the user to exit the program.
+        elif int_user_input == 4:
+            print()  # --> Formatting
             break
 
-# -----Dispatch Dictionary Section-----
-menu_dispatch = {1 : send_thankyou_option,
-                 2 : create_report_option,
-                 3 : send_letters_option,
-                 4 : exit_program_option
-                 }
 
 # -----Presentation Section-----
 # The main block checks to see the program is not being imported then runs the program.
--- students/rhamersky/Lesson_6/test_mailroom_part4.py
+++ students/rhamersky/Lesson_6/test_mailroom_part4.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
 import unittest
-from mailroom_part4 import tup_donor_names, Menu, SubMenu, Files, update_donor, donor_list_creation,send_thankyou_option, \
-    create_report_option, exit_program_option,lifetime_donations,menu_dispatch
+from mailroom_part4 import Files, update_donor, dic_sort, menu_options
 
 class MyTestCase(unittest.TestCase):
-    def test_creatReport(self):
-        self.assertIsNone(SubMenu.create_report())
 
-    def test_donorEmail(self):
-        str_salutation = "Dear John Banks,"
+    def test_donor_email(self):
+        str_salutation = "Dear Paul Allen,"
         str_body = "\n\n" + "Thank you for generosity! " \
                             "\n\n" + "This donation will be put to great use!" \
-                                     "\n\n" + "Your current lifetime donation is $300.00."
+                                     "\n\n" + "Your current lifetime donation is $5,000.00."
         str_valediction = "\n\n" + "Sincerely," \
                                    "\n\n" + "The Team"
-        self.assertMultiLineEqual(Files.donor_email("John Banks",{"John Banks":"$300.00"}),
+        self.assertMultiLineEqual(Files.donor_email("Paul Allen"),
                                   str_salutation+str_body+str_valediction)
 
-    def test_updateDonor(self):
-        self.assertIsNone(update_donor("John Banks",300.00))
+    def test_update_donor(self):
+        self.assertEqual(update_donor("Jeff Bezos",300.00),{"William Gates, III":[10000.0, 1],
+                                                            "Mark Zuckerberg":[20000.0, 1],
+                                                            "Jeff Bezos":[1500.0, 2],
+                                                            "Paul Allen":[5000.0, 1],
+                                                            "Mike Scott":[25.0, 1]})
 
-    def test_donorListCreation(self):
-        self.assertIsNone(donor_list_creation())
+        self.assertEqual(update_donor("John Banks",300.00),{"William Gates, III":[10000.0, 1],
+                                                            "Mark Zuckerberg":[20000.0, 1],
+                                                            "Jeff Bezos":[1500.0, 2],
+                                                            "Paul Allen":[5000.0, 1],
+                                                            "Mike Scott":[25.0, 1],
+                                                            "John Banks":[300.0, 1]})
 
-    def test_sendThankyouOption(self):
-        self.assertIsNone(donor_list_creation())
-
-    def test_createReportOption(self):
-        self.assertIsNone(create_report_option())
-
-    def test_exitProgram(self):
-        self.assertEqual(exit_program_option(),"exit")
-
-    def test_lifetimeDonations(self):
-        self.assertDictEqual(lifetime_donations(tup_donor_names), lifetime_donations(tup_donor_names))
-
-    def test_menu_dispatch(self):
-        self.assertDictEqual(menu_dispatch,menu_dispatch)
+    def test_dic_sort(self):
+        self.assertEqual(dic_sort(), {"William Gates, III": [10000.0, 1],
+                                                            "Mark Zuckerberg": [20000.0, 1],
+                                                            "Jeff Bezos": [1200.0, 1],
+                                                            "Paul Allen": [5000.0, 1],
+                                                            "Mike Scott": [25.0, 1]})
 
 
 if __name__ == '__main__':
