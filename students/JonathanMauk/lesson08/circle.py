96404b9a4ffe3b870f8091262dc08533a71563b7
Jonathan Mauk <jorauk@uw.edu>
2018-05-22 14:33:33 -0700

    Wrote tests for area, diameter, str, repr, and adding/multiplying, then wrote Circle code for each.

--- students/JonathanMauk/lesson08/circle.py
+++ students/JonathanMauk/lesson08/circle.py
@@ -1,3 +1,6 @@
+from math import pi
+
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
@@ -9,3 +12,23 @@ class Circle:
     @diameter.setter
     def diameter(self, val):
         self.radius = val / 2
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
+
+    @classmethod
+    def from_diameter(cls, val):
+        return cls(val / 2)
+
+    def __str__(self):
+        return "Circle with radius: {}".format(self.radius)
+
+    def __repr__(self):
+        return "Circle({})".format(self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
