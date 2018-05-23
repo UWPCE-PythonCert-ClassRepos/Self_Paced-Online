c02151452b4d08087c4f5830767ac4c5f259195f
= <=>
2018-05-22 23:32:05 -0500

    added diameter setter and area (steps 3 and 4) and associated test

--- students/TrkCat/Lesson08/circle.py
+++ students/TrkCat/Lesson08/circle.py
@@ -1,7 +1,18 @@
+from math import pi
+
+
 class Circle():
     def __init__(self, radius):
         self.radius = radius
-       
+
     @property
     def diameter(self):
         return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, val):
+        self.radius = val / 2
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
--- students/TrkCat/Lesson08/test_circle.py
+++ students/TrkCat/Lesson08/test_circle.py
@@ -1,10 +1,27 @@
+from math import pi
 from circle import Circle
+import pytest
+
 
 def test_step1():
     c = Circle(5)
     assert c.radius == 5
-    
+
 
 def test_step2():
     c = Circle(5)
     assert c.diameter == 10
+
+
+def test_step3():
+    c = Circle(5)
+    c.diameter = 20
+    assert c.diameter == 20
+    assert c.radius == 10
+
+
+def test_step4():
+    c = Circle(2)
+    assert c.area == pi * 2 ** 2
+    with pytest.raises(AttributeError):
+        c.area = 10
