18f05ed65dc5d41fe27dd3981d9506f87aca3690
= <=>
2018-05-26 20:02:18 -0500

    added lt and eq operators, enabling sorting. wrote applicable tests.

--- students/TrkCat/Lesson08/test_circle.py
+++ students/TrkCat/Lesson08/test_circle.py
@@ -37,3 +37,29 @@ def test_step6():
     c = Circle(4)
     assert str(c) == 'Circle with radius: 4'
     assert repr(c) == 'Circle(4)'
+
+
+def test_step7():
+    c1 = Circle(1)
+    c2 = Circle(2)
+    assert c1 + c2 == Circle(3)
+    assert c2 - c1 == Circle(1)
+    with pytest.raises(ValueError):
+        c1 - c2
+    assert c1 * 3 == Circle(3)
+    assert 3 * c1 == Circle(3)
+    assert c2 / 2 == Circle(1)
+
+
+def test_step8():
+    c1 = Circle(1)
+    c2 = Circle(2)
+    circles = [Circle(6), Circle(7), Circle(0), Circle(1), Circle(0.5)]
+    assert c1 < c2
+    c2 = Circle(1)
+    assert c1 == c2
+
+    sorted_cir = [Circle(0), Circle(0.5), Circle(1), Circle(6), Circle(7)]
+    assert sorted(circles) == sorted_cir
+    circles.sort(key=Circle.sort_key)
+    assert circles == sorted_cir
