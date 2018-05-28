36ece33844390bb9b8c467772f8a3783bc4ef1a4
Robert Pagliarini <rgpag@umich.edu>
2018-05-28 13:38:22 -0700

    updated mailroom.py for lesson05

--- students/rgpag/lesson05/mailroom.py
+++ students/rgpag/lesson05/mailroom.py
@@ -20,38 +20,49 @@ Dime for Donuts\n
 """
 
 
+def don_in():
+    while True:
+        try:
+            don_amt = int(input('What was the donation amount?'))
+        except ValueError:
+            print('input must be an integer, try again')
+        else:
+            return don_amt
+
+
 def new_donor(new_name):
-    don_amt = int(input('What was the donation amount?'))
+    don_amt = don_in()
     donors[new_name] = [don_amt]
     return new_name, 1, don_amt
 
 
 def update_don(ret_don):
-    don_amt = int(input('What was the donation amount?'))
+    # prompt user for donation amount
+    don_amt = don_in()
     donors[ret_don].append(don_amt)
     count = len(donors[ret_don])
     total_amt = sum(donors[ret_don])
     return ret_don, count, total_amt
 
 
-def get_key(item):
-    return item[1]
-
-
 def menu_sel_1():
-    name = input('Please provide full name ')
-    if name == 'quit':
+    name_in = input('Please provide full name ')
+    if name_in == 'quit':
         q = 1
         return q
-    elif name == 'list':
-        for z in donors:
-            print(z)
-    elif name not in donors:
-        msg_vars = new_donor(name)
+    elif name_in == 'list':
+        for donor in donors:
+            print(donor)
+    elif name_in not in donors:
+        msg_vars = new_donor(name_in)
         print(msg.format(*msg_vars))
     else:
-        msg_vars = update_don(name)
+        msg_vars = update_don(name_in)
         print(msg.format(*msg_vars))
+
+
+def get_key(item):
+    return item[1]
 
 
 def menu_sel_2():
@@ -60,25 +71,26 @@ def menu_sel_2():
           'Total Given', 'Num Gifts', 'Avg Gift'))
     print('-'*63)
     s = '{:<20} ${:>15}  {:^10} ${:>12.2f}'
-    for x in (donors):
-        amt = sum(donors[x])
-        count = len(donors[x])
+    for donor in (donors):
+        amt = sum(donors[donor])
+        count = len(donors[donor])
         avg = amt/count
-        db.append([x, amt, count, avg])
+        db.append([donor, amt, count, avg])
     db_sort = sorted(db, key=get_key, reverse=True)
+    # prints donor name, amount, count, average sorted by amount
     for i in range(len(db_sort)):
         print(s.format(db_sort[i][0], db_sort[i][1], db_sort[i][2],
                        db_sort[i][3]))
 
 
 def menu_sel_3():
-    y = datetime.datetime.now()
-    for i in donors:
-        file_name = '{}_{}_{}_{}.txt'.format(i, y.month, y.day, y.year)
-        new_file = open(file_name, 'w')
-        msg_vars = (i, len(donors[i]), sum(donors[i]))
-        new_file.write(msg.format(*msg_vars))
-        new_file.close()
+    date = datetime.datetime.now()
+    for donor in donors:
+        file_name = '{}_{}_{}_{}.txt'.format(donor, date.month, date.day,
+                                             date.year)
+        msg_vars = (donor, len(donors[donor]), sum(donors[donor]))
+        with open(file_name, 'w') as new_file:
+            new_file.write(msg.format(*msg_vars))
 
 
 menu_switch_dict = {
