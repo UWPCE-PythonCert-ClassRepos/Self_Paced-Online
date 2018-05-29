5de2e55491adcd970e031671f5ed4686fae2ca2f
Matt Hudgins <mhudgin1@uw.edu>
2018-05-28 22:02:50 -0700

    Starting the unit test for the html_render.py

---
+++ students/Matt_Hudgins/Lesson07/test_html_render.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# chmod +x test_html_render.py needs to be performed before executable
+
+'''
+    File Name: test_html_render.py
+    Author: Matt Hudgins
+    Date created: 5/28/18
+    Date last modified: 5/28/18
+    Python Version 3.6.4
+'''
+
