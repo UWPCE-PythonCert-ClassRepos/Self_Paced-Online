a8a08299971350fddeadb32814db669646654a57
Matt Hudgins <mhudgin1@uw.edu>
2018-05-28 22:03:49 -0700

    Starting the HTML Render Assignment

---
+++ students/Matt_Hudgins/Lesson07/html_render.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# chmod +x html_render.py needs to be performed before executable
+
+'''
+    File Name: html_render.py
+    Author: Matt Hudgins
+    Date created: 5/28/18
+    Date last modified: 5/28/18
+    Python Version 3.6.4
+'''
+
+
+
