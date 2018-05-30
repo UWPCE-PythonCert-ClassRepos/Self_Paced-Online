96db15c347e40c2a298cf6ea7fe0cea12cad9b46
Yixing Xu <xuyixing2005@gmail.com>
2018-05-29 17:57:32 -0700

    finished step 3

--- students/yixingxu/lesson07/html_render.py
+++ students/yixingxu/lesson07/html_render.py
@@ -28,3 +28,19 @@ class Body(Element):
 
 class P(Element):
     tag = 'p'
+
+class Head(Element):
+    tag = 'head'
+
+class OneLineTag(Element):
+    def render(self, file_out, cur_ind=''):
+        file_out.write(cur_ind + '<{}>'.format(self.tag))
+        for item in self.content:
+            try:
+                item.render(file_out)
+            except AttributeError:
+                file_out.write(str(item))
+        file_out.write('</{}>\n'.format(self.tag))
+
+class Title(OneLineTag):
+    tag = 'title'
