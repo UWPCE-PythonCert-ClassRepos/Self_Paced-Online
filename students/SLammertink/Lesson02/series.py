cecb53ea09ed58ad1c587d9746e3d47c7e4c0757
SLammertink <seb.lammertink@gmail.com>
2018-06-04 19:16:37 +0400

    Added the series.py script

---
+++ students/SLammertink/Lesson02/series.py
@@ -0,0 +1,9 @@
+# Author: SLammertink
+# Series excercise Lesson 2
+# Run as python3 series.py
+
+def fibonacci(N):
+    ''' Shows the N'th number in the Fibonacci sequence
+    '''
+    fib_list = [0]
+    next_number = 1
