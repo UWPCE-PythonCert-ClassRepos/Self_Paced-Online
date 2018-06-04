95dc4a642be047d3dfc16efb1816513bb2554221
SLammertink <seb.lammertink@gmail.com>
2018-06-04 20:07:40 +0400

    Added series.py

---
+++ students/SLammertink/Lesson02/series.py
@@ -0,0 +1,8 @@
+# Author: SLammertink
+# Lesson 02 series exercise
+# Run it in terminal python series.py
+
+def fibonacci(N):
+    ''' a function that gives you the N'th
+    number in a Fibonacci sequesnce '''
+
