29e6b9e36209d668de25891cfa21b45ade7f4065
idcrickmore <idcrickmore@gmail.com>
2018-06-07 15:59:16 -0700

    added functionality to mailroom and test_mailroom

--- students/idcrickmore/lesson09/mailroom_pt5.py
+++ students/idcrickmore/lesson09/mailroom_pt5.py
@@ -13,68 +13,58 @@ class Donor:
         self.ave = self.total/len(self.donation) if donation else 0
         self.num_donations = len(self.donation)
         self.last = self.donation[-1]
+
         
-    def __str__(self):
-        return '{:<15}{:<15,.2f}{:<15}'.format(self.total, self.ave, self.num_donations)
-
-
 donor_data = {
               "Galileo Galilei": [348, 8377, 123],
               "Giovanni Cassini": [209],
               "Christiaan Huygens": [9135, 39],
               "Edmond Halley": [399, 1100, 357],
               "Edwin Hubble": [1899],
+              "Jill": [50, 200, 100, 250]
               }
 
 # stores and updates donar data, makes reports 
-class Donor_operations:
+class Donor_ops:
 
-    # 'append' function here
+    # 'append' function
     def donor_append(name, don):
         try:
             donor_data[name].append(don)
         except KeyError:
             donor_data.setdefault((name), []).append(don)
             
-    # calls input class and returns donor object    
-    def donation_query():
-        text = ["\nEnter the name of the donar\nor"
-                "'list' for a list of donors\n"
-                "or 'menu' for the main menu\n->"
-                , "Enter the donation amount\n->"]
-        donor = user_input.donation_query(text)
-        return donor
-
-    # 'thank you' function calls 'donation_query()'
-    def thank_you():
-        query = Donor_operations.donation_query()
+    # 'thank you' function'
+    def thank_you(query):
         if query != 'list' and query != 'menu':
             donation_info = Donor(donor_data[query])
-            print(donation_info.last)
-            print(f"""\n---------------- Thank You Letter ----------------\n
+            thank_you_text = str(f"""\n---------------- Thank You ----------------\n
 Dear {query},\n
 You rock. Your fat contribution of ${donation_info.last:,.2f}
 will go a long way to lining my pockets.\n
 Sincerely,
 Scrooge McDuck""")
+            Donor_ops.print_function(thank_you_text)
                   
         elif query == 'list':
-            Donor_operations.list_donors()
+            Donor_ops.list_donors()
 
     # List donors
     def list_donors():
-        print("\n---------------- List of Donors ----------------\n")
+        list_text = str("\n---------------- List of Donors ----------------\n\n")
         for donor in donor_data:
-            print(donor)
+            list_text += (donor + '\n')
+        Donor_ops.print_function(list_text)
             
     def report():
         columns = ["DONOR", "TOTAL", "AVERAGE", "NUMBER"]
-        print("\n--------------- Report -------------")
-        print('{:<30}{:<15}{:<15}{:<15}'.format(*columns))
+        report_text = str("\n--------------- Report -------------\n\n")
+        report_text += str('{:<30}{:<15}{:<15}{:<15}\n'.format(*columns))
         
         for name in donor_data:
-            print(f'{name:<30}', end="")
-            print(Donor(donor_data[name]))
+            donation_info = Donor(donor_data[name])
+            report_text += str('{:<30}{:<15}{:<15,.2f}{:<15}\n'.format(name, donation_info.total, donation_info.ave, donation_info.num_donations))
+        Donor_ops.print_function(report_text)
             
     def letters():
         print("\n--------- Exporting Letters to Everyone ---------\n")
@@ -91,39 +81,44 @@ Sincerely,
 Scrooge McDuck"""
             with open(str(name) + "_" + str(datetime.date.today()) + '.txt', 'w') as out_file:
                 out_file.write(letter)
+        
+    def print_function(text):
+        print(text)
     
 
 class user_input:
     # user input for menu selection
     def menu_render(text, key):
-
-        print("\n---------------Main Menu-------------")
+        print("\n---------------Main Menu-------------\n")
         print("Enter the number coresponding to your choice\n")
         for line in text:
-            print(line)                
+            print(line)
         choice = input("-> ")
-        try:
-            key[choice]()
-        except (KeyError):
-            print("\nPlease enter one of the following options...")
-            for item in key:
-                print(f'"{item}",', end=" ")
-            print("\n")
+        if key[choice] == Donor_ops.thank_you:
+            key[choice](user_input.donation_query())
+        else:
+            try:
+                key[choice]()
+            except (KeyError):
+                print("\nPlease enter one of the following options...")
+                for item in key:
+                    print(f'"{item}",', end=" ")
+                print("\n")
                 
     # user input for new donations
-    def donation_query(text):
-        new_donor = input(text[0])
-        if new_donor == 'list':
+    def donation_query():
+        query = input("\nEnter the name of the donor\nor 'list' or 'menu'\n->")
+        if query == 'list':
             return 'list'
-        elif new_donor == 'menu':
+        elif query == 'menu':
             return 'menu'
         try:
-            donation_amount = float(input(text[1]))
+            donation_amount = float(input("Enter the donation amount\n->"))
         except(ValueError):
             print('entry not valid')
             user_input.menu_render(main_menu_text, main_menu_key)
-        Donor_operations.donor_append(new_donor,donation_amount)
-        return new_donor
+        Donor_ops.donor_append(query,donation_amount)
+        return query
 
 
 def Simple_print():
@@ -142,7 +137,7 @@ def main():
         
         
 main_menu_text = ["1. Donor Thank You", "2. Report", "3. Letters", "4. Quit"]
-main_menu_key = {'1': Donor_operations.thank_you, '2': Donor_operations.report, '3': Donor_operations.letters, '4': Ex}
+main_menu_key = {'1': Donor_ops.thank_you, '2': Donor_ops.report, '3': Donor_ops.letters, '4': Ex}
 
   
 if __name__ == '__main__':
--- students/idcrickmore/lesson09/test_mailroom_pt5.py
+++ students/idcrickmore/lesson09/test_mailroom_pt5.py
@@ -1,37 +1,50 @@
 import mailroom_pt5 as m
 import unittest
 
-data = [50, 200, 100, 250]
+donor_data = m.donor_data
 
         
 class test_Donor(unittest.TestCase):
 
     def test_donations(self):
-        test = m.Donor(data)
+        test = m.Donor(donor_data['Jill'])
         self.assertEqual(test.donation, [50, 200, 100, 250])
         
     def test_total(self):
-        test = m.Donor(data)
+        test = m.Donor(donor_data['Jill'])
         self.assertEqual(test.total, 600)
         
     def test_ave(self):
-        test = m.Donor(data)
+        test = m.Donor(donor_data['Jill'])
         self.assertEqual(test.ave, 150)
         
     def test_num(self):
-        test = m.Donor(data)
+        test = m.Donor(donor_data['Jill'])
         self.assertEqual(test.num_donations, 4)
         
     def test_last(self):
-        test = m.Donor(data)
+        test = m.Donor(donor_data['Jill'])
         self.assertEqual(test.last, 250)
         
-    def test_str(self):
-        test = m.Donor(data)
-        self.assertEqual(str(test), "600            150.00         4              ")
 
-# class test_Donor_oporations(unittest.TestCase):
+
+class test_Donor_oporations(unittest.TestCase):
+
+    def test_donor_append(self):
+        m.Donor_ops.donor_append('Jill', 99)
+        self.assertEqual(donor_data['Jill'], [50, 200, 100, 250, 99])
     
+    def test_thank_you_self(self):
+        expected = (
+                    "Dear Jill,\n\n"
+                    "You rock. Your fat contribution of $99.00\n"
+                    "will go a long way to lining my pockets.\n\n"
+                    "Sincerely,\n"
+                    "Scrooge McDuck")
+        actual = m.Donor_ops.thank_you('Jill')
+        
+        # assert expected == actual
+        self.assertEqual(m.Donor_ops.thank_you('Jill'), expected)
     
 
         
