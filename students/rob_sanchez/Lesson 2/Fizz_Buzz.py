9b281fae9598f97d648da37cf4aa9487f929ae2c
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-06-08 22:57:36 -0700

    Completed Fizz Buzz exercise.

--- students/rob_sanchez/Lesson 2/Fizz_Buzz.py
+++ students/rob_sanchez/Lesson 2/Fizz_Buzz.py
@@ -1,1 +1,16 @@
-print("+----+")
+def FizzBuzz():
+    #variables
+    num1=3
+    num2=5
+    range_start=1
+    range_end=101
+
+    for i in xrange(range_start,range_end):
+        if i%num1==0 and i%num2==0:
+            print('FizzBuzz')
+        elif i%num1==0:
+            print('Fizz')
+        elif i%num2==0:
+            print('Buzz')
+        else:
+            print(i)
