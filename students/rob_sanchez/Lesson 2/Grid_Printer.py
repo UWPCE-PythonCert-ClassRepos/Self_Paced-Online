d1ea6ca064bed6b08bfed22a0fbacdd0bde44923
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-06-08 20:16:09 -0700

    Completed print_grid function.

--- students/rob_sanchez/Lesson 2/Grid_Printer.py
+++ students/rob_sanchez/Lesson 2/Grid_Printer.py
@@ -1,1 +1,28 @@
-print("+----+")
+#Grid Printer exercise
+def print_grid(n):
+    if n%2 == 0:
+        row2_size=n+1
+    else: row2_size=n
+
+    plus='+ '
+    minus='- '
+    y='|'
+    z=' '
+
+
+    row=plus + minus*(n/2) + plus + minus*(n/2) + plus
+    row2=y + z*(row2_size) + y + z*(row2_size) + y
+
+    print(row)
+
+    for x in xrange(n/2):
+        print(row2)
+
+    print(row)
+
+    for x in xrange(n/2):
+        print(row2)
+
+    print(row)
+
+print_grid(3)
