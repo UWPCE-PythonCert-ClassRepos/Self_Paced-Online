81890f24bf6a44cc5a9cd0aeae58b656f5ffef0c
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-06-08 21:30:31 -0700

    Added print_grid2 function.

--- students/rob_sanchez/Lesson 2/Grid_Printer.py
+++ students/rob_sanchez/Lesson 2/Grid_Printer.py
@@ -9,7 +9,6 @@ def print_grid(n):
     y='|'
     z=' '
 
-
     row=plus + minus*(n/2) + plus + minus*(n/2) + plus
     row2=y + z*(row2_size) + y + z*(row2_size) + y
 
@@ -25,4 +24,23 @@ def print_grid(n):
 
     print(row)
 
-print_grid(3)
+
+def print_grid2(grid,size):
+
+    plus='+ '
+    minus='- '
+    bar='| '
+    z='  '
+
+    row=minus*(size) + plus
+
+    row2= z*size + bar
+
+    for x in xrange(grid):
+        print(plus+row*grid)
+        for y in xrange(size):
+            print(bar + row2*grid)
+    print(plus+row*grid)
+
+#print_grid(3)
+#print_grid2(10,5)
