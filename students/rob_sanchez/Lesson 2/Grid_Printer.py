64be3245116efeb387706c03b803b6ffa282fd19
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-06-08 22:21:21 -0700

    Cleaned up the for loop for the print_grid function.

--- students/rob_sanchez/Lesson 2/Grid_Printer.py
+++ students/rob_sanchez/Lesson 2/Grid_Printer.py
@@ -1,32 +1,30 @@
 #Grid Printer exercise
 def print_grid(n):
+    #variables
+    grid_size=2
+
     if n%2 == 0:
         row2_size=n+1
     else: row2_size=n
 
+    size=n/2
+
     plus='+ '
     minus='- '
-    y='|'
+    bar='|'
     z=' '
 
-    row=plus + minus*(n/2) + plus + minus*(n/2) + plus
-    row2=y + z*(row2_size) + y + z*(row2_size) + y
+    row=plus + minus*(size) + plus + minus*(size) + plus
+    row2=bar + z*(row2_size) + bar + z*(row2_size) + bar
 
+    for x in xrange(grid_size):
+        print(plus + minus*(size) + plus + minus*(size) + plus)
+        for y in xrange(size):
+            print(bar + z*row2_size + bar + z*row2_size + bar)
     print(row)
-
-    for x in xrange(n/2):
-        print(row2)
-
-    print(row)
-
-    for x in xrange(n/2):
-        print(row2)
-
-    print(row)
-
 
 def print_grid2(grid,size):
-
+    #variables
     plus='+ '
     minus='- '
     bar='| '
@@ -41,6 +39,3 @@ def print_grid2(grid,size):
         for y in xrange(size):
             print(bar + row2*grid)
     print(plus+row*grid)
-
-#print_grid(3)
-#print_grid2(10,5)
