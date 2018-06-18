29a0cde35680e8c9893f7b11780c635837922663
mmsunday1 <matt.m.sunday@gmail.com>
2018-06-17 17:31:50 -0700

    Steps 1, 2, and 3 of exercise are complete

--- students/M_Sunday/lesson10/mailroom_fp.py
+++ students/M_Sunday/lesson10/mailroom_fp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 from operator import itemgetter
+from functools import reduce
 
 # Lesson 10, Mailroom Part 6 Assignment (functional programming)
-# Starting from lesson06 (Mailroom_Part_4)
+# StartedS from lesson06 (Mailroom_Part_4)
 
 
 # ------- Global Assignments -------
@@ -28,7 +29,9 @@ def menu_display():
           "1: Send a Thank You\n"
           "2: Create a Report\n"
           "3: Send letters to everyone\n"
-          "4: Quit\n")
+          "4: Philanthropic Challenge\n"
+          "5: Project Philanthropic Contribution\n"
+          "6: Quit\n")
     menu_sel_ = get_menu_option()
     return menu_sel_
 
@@ -92,8 +95,8 @@ def thank_you_input():
     thank_you(donor_name_)
 
 
-def calc_data():
-    summary_data = list(map(calc_data_2, donor_data.values(), donor_data.keys()))
+def calc_data(data_for_donors):
+    summary_data = list(map(calc_data_2, data_for_donors.values(), data_for_donors.keys()))
     return summary_data
 
 
@@ -104,13 +107,13 @@ def calc_data_2(donation_data, donor):
     return [donor, donations_quantity, donations_value, donations_average]
 
 
-def report():
-    summary_data = calc_data()
+def report(db=donor_data):
+    summary_data = calc_data(db)
     list_details(summary_data)
 
 
 def send_all_letters():
-    summary_data = calc_data()
+    summary_data = calc_data(donor_data)
     list(map(send_letter, summary_data))
     print("\nAll thank you letters drafted")
 
@@ -124,12 +127,69 @@ def send_letter(item):
     thank_you_text.close()
 
 
+def philanthropic(report_option=1):
+    factor = get_challenge_factor()
+    min_threshold = get_minimum_threshold()
+    max_threshold = get_maximum_threshold()
+    donor_data_philanthropic_list = list(map(challenge, donor_data.keys(), [factor] * len(donor_data),
+                                             donor_data.values(), [min_threshold] * len(donor_data),
+                                             [max_threshold] * len(donor_data)))
+    donor_data_philanthropic_dict = {}
+    for entry in donor_data_philanthropic_list:
+        if entry[1]:
+            donor_data_philanthropic_dict[entry[0]] = entry[1]
+    if report_option:
+        report(donor_data_philanthropic_dict)
+    else:
+        projection_report(donor_data_philanthropic_dict, factor, min_threshold, max_threshold)
+
+
+def challenge(donor, factor, data, min_th, max_th):
+    data = list(filter(lambda x: filtering(x, min_th, max_th), data))
+    return donor, list(map(challenge_m, [factor]*len(data), data))
+
+
+def challenge_m(factor, val):
+    return factor * val
+
+
+def filtering(val, minimum=0, maximum=100000):
+    return minimum < val < maximum
+
+
+def get_challenge_factor():
+    return int(input("\nBy what factor do you want to multiply all donations? "))
+
+
+def get_minimum_threshold():
+    return int(input("\nWhat is the minimum threshold for donations you want to multiply? "))
+
+
+def get_maximum_threshold():
+    return int(input("\nWhat is the maximum threshold for donations you want to multiply? "))
+
+
+def project_philanthropic():
+    philanthropic(report_option=0)
+
+
+def projection_report(data, fact, min_, max_):
+    total_donations = sum(list(map(summer, data.values())))
+    print("\nBy matching all donations greater than ${} and less than ${}, by a factor of {}, your total donation "
+          "comes to ${}".format(min_, max_, fact, total_donations))
+
+
+def summer(val):
+    return sum(val)
+
+
 def exit_program():
     exit()
 
 
 def main_exec():
-    menu_map = {'1': thank_you_input, '2': report, '3': send_all_letters, '4': exit_program}
+    menu_map = {'1': thank_you_input, '2': report, '3': send_all_letters, '4': philanthropic, '5': project_philanthropic
+                , '6': exit_program}
 
     while True:
         try:
@@ -144,4 +204,3 @@ def main_exec():
 if __name__ == "__main__":
 
     main_exec()
-
