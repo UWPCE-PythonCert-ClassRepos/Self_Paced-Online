52926df003a7d5552fa2ee26a28a51417f8fcd3b
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-06-18 21:06:35 -0700

    Completed Series 3.

--- students/rob_sanchez/Lesson 3/list_lab.py
+++ students/rob_sanchez/Lesson 3/list_lab.py
@@ -5,24 +5,36 @@ import time
 
 # Returns a list of fruits based on different scenarios
 def series_1():
+    # List of fruits
     fruits = ["Apples", "Pears", "Oranges", "Peaches"]
 
     print("*** Series 1 ***")
-    print("List of fruits:"), fruits
+    print()
+
+    # Print list of fruits
+    print("List of fruits: " + ", ".join(["{}"] * len(fruits)).format(*fruits))
+    print()
+    time.sleep(1)
+
+    # Prompt user for new fruit
     new_fruit = input('Enter name of new fruit > ')
     fruits.append(new_fruit)
     print (fruits)
     print()
+
+    # Display index of fruit based on user input
     number = input('Enter a number > ')
-    print('You selected: {} - {}...'.format(number, fruits[int(number)-1]))
+    print('You selected: {}...'.format(fruits[int(number)-1]))
     print()
     time.sleep(1)
-    # Adding a new fruit to the list using '+'
+
+    # Add a new fruit to the list using '+'
     new_list = ["Plums"] + fruits
     print(new_list[0], "have been added to the beginning of the list...")
     print(new_list)
     time.sleep(1)
-    # Adding a new fruit to the beginnning of the list using insert()
+
+    # Add a new fruit to the beginnning of the list using insert()
     yet_another_fruit = "Pineapples"
     new_list.insert(0, yet_another_fruit)
     print()
@@ -30,25 +42,33 @@ def series_1():
     print(new_list)
     time.sleep(1)
     print()
+
     # Filter all fruits from the list that begin with the letter "P"
     length = len(new_list)
-    filtered_items = get_items_with_p(new_list)
+    filtered_items = get_items_beginning_with_p(new_list)
 
     print("These {} fruits begin with 'P':".format(length))
     print(filtered_items)
 
 
 def series_2():
-    # Print initial list
+    # List of fruits
     fruits = ["Apples", "Pears", "Oranges", "Peaches"]
-    print(fruits)
+
+    print("*** Series 2 ***")
+    print()
+
+    # Print list of fruits
+    print("List of fruits: " + ", ".join(["{}"] * len(fruits)).format(*fruits))
     print()
     time.sleep(1)
+
     # Remove the last fruit from the list
     print('Removing last item ({})...'.format(fruits.pop()))
     print(fruits)
     print()
     time.sleep(1)
+
     # Remove fruit based on user input
     fruit_to_remove = input('Enter name of fruit to be removed > ')
     print()
@@ -80,9 +100,41 @@ def series_2():
 
 
 def series_3():
-    # Print initial list
-    fruits = "Apples", "Pears", "Oranges", "Peaches"
-    print (fruits)
+    # List of fruits
+    fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+
+    print("*** Series 3 ***")
+    print()
+
+    # Print list of fruits
+    print("List of fruits: " + ", ".join(["{}"] * len(fruits)).format(*fruits))
+    print()
+
+    new_list = []
+
+    for item in fruits:
+        # Prompt user if they like fruit from list
+        index = fruits.index(item)
+        user_preference = input('Do you like {}? (Yes/No) > '.format(fruits[index].lower()))
+
+        # Keep prompting user until acceptable answer is returned
+        acceptable_answers = ('Yes', 'yes', 'No', 'no')
+        while user_preference not in acceptable_answers:
+            print()
+            user_preference = input('Do you like {}? (Yes/No) > '.format(fruits[index].lower()))
+
+        # Return a new list based on the user's preferences
+        if user_preference in ('Yes', 'yes'):
+            new_list.append(fruits[index])
+
+    print()
+
+    # Print list of fruit the user likes
+    list_lenght = len(new_list)
+    if list_lenght == 0:
+        print("List of fruits is empty.")
+    else:
+        print("Fruits you like: " + ", ".join(["{}"] * list_lenght).format(*new_list))
 
 
 def series_4():
