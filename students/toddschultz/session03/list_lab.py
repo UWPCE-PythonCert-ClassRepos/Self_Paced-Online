5c7496487b029e9fe1db0a2e36bb91034518628e
toddschultz <todd@wetfoot.net>
2018-07-02 11:12:46 -0700

    all assignments are complete. Need to do one more double check to make sure I did all the assignment items, plus add a loop to the end of Series 2.

--- students/toddschultz/session03/list_lab.py
+++ students/toddschultz/session03/list_lab.py
@@ -2,7 +2,7 @@
 
 #SERIES 1
 print ("~~~~~~~~~~~~~~~SERIES 1~~~~~~~~~~~~~~~")
-fruit = ["Apple", "Pears", "Oranges", "Peaches"]
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 print (fruit)
 another = input("What other fruit would you like: ")
 fruit.append(another)
@@ -10,7 +10,7 @@ print(fruit)
 a = int(input("What number fruit would you like? "))
 if a > 5:
 	a = int(input("Please provide a number between 1 and 5: "))
-print (fruit[a - 1])
+print (str(a), fruit[a - 1])
 fruit = fruit + ["Kiwi"]
 print (fruit)
 fruit.insert(0, "Dragon")
@@ -21,8 +21,9 @@ for i in fruit:
 
 #SERIES 2
 print ("~~~~~~~~~~~~~~~SERIES 2~~~~~~~~~~~~~~~")
-print (fruit)
-del fruit [0]
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruit)
+del fruit [-1]
 print (fruit)
 b = input("Which fruit would you like to delete? ")
 fruit.remove(b)
@@ -34,3 +35,16 @@ for i in fruit:
 	if i == c:
 		fruit.remove(c)
 print(fruit)
+
+#SERIES 3
+print ("~~~~~~~~~~~~~~~SERIES 3~~~~~~~~~~~~~~~")
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+fruit2 = ["Apples", "Pears", "Oranges", "Peaches"]
+for elem in fruit:
+	response = input("Would you like some " + elem + "? ")
+	while response != "no" and response != "yes":
+		response = input("Please enter yes or no: ")
+	if response == "no":
+		fruit2.remove(elem)
+print (fruit2)
+
