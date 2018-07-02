ec1e4aaf0331573f4fe18c235e18be33b173d640
toddschultz <todd@wetfoot.net>
2018-07-02 10:49:26 -0700

    Added asserts per instructor feedback, somehow missed them the first time through. Thanks again for Natasha's assistance in Slack.

--- students/toddschultz/session03/slicing_lab_exercise.py
+++ students/toddschultz/session03/slicing_lab_exercise.py
@@ -11,12 +11,23 @@ def remove_fours_every_other(seq):
     return seq[4:-4:2] 
 
 #with the elements reversed (just with slicing).
-def reversed(seq):
+def reverse(seq):
     return seq[::-1] 
 
 #with the middle third, then last third, then the first third in the new order.
 def thirds_list(seq):
     return seq[3:6] + seq[6:9] + seq[0:3]
-
 def thirds_string_len(seq):
     return seq[int((len(seq) * .34)):int((len(seq) * .67))] + seq[int((len(seq) * .67)):len(seq)] + seq[:int((len(seq) * .34))]
+
+#asserts
+assert exchanged("abcdefghijklmnopqrstuvwxyz") == "zbcdefghijklmnopqrstuvwxya"
+assert every_other("123456789") == ("13579")
+assert remove_fours_every_other("----123456789___") == "1357"
+assert reverse("123456789") == "987654321"
+assert thirds_list("123456789") == "456789123"
+
+
+
+
+
