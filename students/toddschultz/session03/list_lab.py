8959996d139ffe4b7b60cc46626a9aea505db4ad
toddschultz <todd@wetfoot.net>
2018-07-01 18:02:20 -0700

    I think this technically meets the first challenge, but it isn't pretty, just seems like a list of commands.

--- students/toddschultz/session03/list_lab.py
+++ students/toddschultz/session03/list_lab.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-print("hey mundo")
+fruit = ["Apple", "Pears", "Oranges", "Peaches"]
+print (fruit)
+another = input("What other fruit would you like: ")
+fruit.append(another)
+print(fruit)
+a = int(input("What number fruit would you like? "))
+if a > 5:
+	a = int(input("Please provide a number between 1 and 5: "))
+print (fruit[a - 1])
+fruit = fruit + ["Kiwi"]
+print (fruit)
+fruit.insert(0, "Dragon")
+print(fruit)
+for i in fruit:
+	if i[0] == "P":
+		print(i)
