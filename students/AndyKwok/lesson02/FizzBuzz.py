c56913a16469767d854968500326ee476a6202bf
Andy Kwok <andykhkwok@gmail.com>
2018-07-02 22:37:12 -0700

    Updated per comment

--- students/AndyKwok/lesson02/FizzBuzz.py
+++ students/AndyKwok/lesson02/FizzBuzz.py
@@ -5,12 +5,13 @@
 #			v1.0 - Initialization
 
 for n in range(1, 101):
-	#prints out Fizz/Buzz/FizzBuzz accordingly by if the value can be divided equally by assigned values 
-	if n%3 is 0 and n%5 is 0:
-		print('FizzBuzz')
-	elif n%3 is 0:
-		print('Fizz')
-	elif n%5 is 0:
-		print('Buzz')
-	else:
-		print(n)
+    #prints out Fizz/Buzz/FizzBuzz accordingly by if the value can be divided equally by assigned values 
+    if n % 3 is 0 and n % 5 is 0:
+        print('FizzBuzz')
+    elif n % 3 is 0:
+        print('Fizz')
+    elif n % 5 is 0:
+        print('Buzz')
+    else:
+        print(n)
+        
