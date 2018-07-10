dfedf8d5fcce88625e1cf719c498c5b9c7983b59
Micah Braun <mcbraun@uw.edu>
2018-07-10 14:54:43 -0700

    Test file upload

---
+++ students/MicahBraun/Lesson 06/Tests/test_menu.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------
+# NAME: MICAH BRAUN
+# PROJECT: 
+# PURPOSE: 
+# DATE: 
+#
+# DESCRIPTION:
+# ------------------------------------------------------------------------
+import magicmock
+from Tasks import mailroom_3_UPDATE
+
+
+def test_menu():
+    assert mailroom_3_UPDATE.menu() is function()
