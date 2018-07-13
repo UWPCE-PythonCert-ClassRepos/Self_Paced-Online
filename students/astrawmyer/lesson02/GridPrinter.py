29e600853b39ee3f42d09eec1a0bdf27de12f0b4
Adam Strawmyer <adam@strawmyer.com>
2018-07-12 20:57:48 -0500

    Initial draft of part one of lesson 2 grid printer

---
+++ students/astrawmyer/lesson02/GridPrinter.py
@@ -0,0 +1,17 @@
+def Part1():
+    Row = '+ ' + '- ' * 4 + '+ ' + '- ' * 4 + '+'
+    Col = '|' + ' ' * 9 + '|' + ' ' * 9 + '|'
+    print(Row)
+    print(Col)
+    print(Col)
+    print(Col)
+    print(Col)
+    print(Row)
+    print(Col)
+    print(Col)
+    print(Col)
+    print(Col)
+    print(Row)
+    return
+
+Part1()
