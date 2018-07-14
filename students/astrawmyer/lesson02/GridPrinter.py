2ed8c14df1fb74350d2186ed606481e222bd23a1
Adam Strawmyer <adam@strawmyer.com>
2018-07-14 14:41:10 -0500

    Completion of Part 2 of lesson. Also improved part 2

--- students/astrawmyer/lesson02/GridPrinter.py
+++ students/astrawmyer/lesson02/GridPrinter.py
@@ -18,16 +18,33 @@ def Part1():
 
 #Part1()
 
-#Function for Part 2 of assignment
+# Function for Part 2 of assignment
 # Prints a 2x2 grid with customized sizes based on input to function
 def print_grid(n):
-    Row = "+ " + '- '*n + '+ ' + '- '*n + '+'
-    Col = '|' + ' ' * (2*n+1) + '|' + ' ' * (2*n+1) + '|'
+    Row = ("+ " + '- '*n)*2 + '+'
+    Col = ('|' + ' ' * (2*n+1))*2 + '|'
     for i in range(2):
         print(Row)
         for j in range(n):
             print(Col)
     print(Row)
+    return
 
 
 print_grid(15)
+
+# Function for part 3 of assignment
+# Prints grid with specificized number of rows and columns and size of cells.
+# m is grid size
+# n is cell size
+def print_grid2(m,n):
+    Row = ("+ " + '- '*n)*m + '+'
+    Col = ('|' + ' ' * (2*n+1))*m + '|'
+    for i in range(m):
+        print(Row)
+        for j in range(n):
+            print(Col)
+    print(Row)
+    return
+    
+print_grid2(5,3)
