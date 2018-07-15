d6903ce1c6243668f29144fd46f4ac9eba7857e4
Adam Strawmyer <adam@strawmyer.com>
2018-07-15 15:18:19 -0500

    have working functions for all parts of series exercise. no tests yet

--- students/astrawmyer/lesson02/series.py
+++ students/astrawmyer/lesson02/series.py
@@ -1,26 +1,56 @@
 def fibonacci(n):
-    #add a doc string
+    """add a doc string"""
     a=0
     b=1
-    for i in range(n-2):
-        c=a+b
-        a=b
-        b=c
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a=0
+        b=1
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
         #print(i,c)
     print(c)
 
-fibonacci(8)
+#fibonacci(8)
 
 
 def lucas(n):
-    #add a doc string
-    a=2
-    b=1
-    for i in range(n-2):
-        c=a+b
-        a=b
-        b=c
+    """add a doc string"""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a=2
+        b=1
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
         #print(i,c)
     print(c)
 
-lucas(8)
+#lucas(8)
+
+def sum_series(n, a=0, b=1):
+    #add a doc string
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        #a=2
+        #b=1
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+        #print(i,c)
+    print(c)
+
+sum_series(8)
