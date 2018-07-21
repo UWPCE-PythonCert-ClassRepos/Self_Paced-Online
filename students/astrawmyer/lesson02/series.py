c17351d89b546f56d2fe779676cef5d30df542ac
Adam Strawmyer <adam@strawmyer.com>
2018-07-21 17:18:02 -0500

    cleaned up variables and added tests to series.py

--- students/astrawmyer/lesson02/series.py
+++ students/astrawmyer/lesson02/series.py
@@ -2,55 +2,74 @@ def fibonacci(n):
     """add a doc string"""
     a=0
     b=1
+    c=0
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        a=0
-        b=1
-        for i in range(n-2):
+        for i in range(n-1):
             c=a+b
             a=b
             b=c
-        #print(i,c)
-    print(c)
+        return c
 
-#fibonacci(8)
+
 
 
 def lucas(n):
     """add a doc string"""
+    a=2
+    b=1
+    c=0
     if n == 0:
-        return 0
+        return 2
     elif n == 1:
         return 1
     else:
-        a=2
-        b=1
-        for i in range(n-2):
+        for i in range(n-1):
             c=a+b
             a=b
             b=c
-        #print(i,c)
-    print(c)
+        return c
 
-#lucas(8)
+
 
 def sum_series(n, a=0, b=1):
-    #add a doc string
+    """add a doc string"""
+    c=0
     if n == 0:
-        return 0
+        return a
     elif n == 1:
-        return 1
+        return b
     else:
-        #a=2
-        #b=1
-        for i in range(n-2):
+        for i in range(n-1):
             c=a+b
             a=b
             b=c
-        #print(i,c)
-    print(c)
+        return c
 
-sum_series(8)
+#sum_series(8)
+
+
+#add tests here
+
+assert fibonacci(0) == 0
+assert fibonacci(1) == 1
+assert fibonacci(2) == 1
+assert fibonacci(5) == 5
+
+assert lucas(0) == 2
+assert lucas(1) == 1
+assert lucas(2) == 3
+assert lucas(5) == 11
+
+assert sum_series(0) == fibonacci(0)
+assert sum_series(1) == fibonacci(1)
+assert sum_series(2) == fibonacci(2)
+assert sum_series(5) == fibonacci(5)
+
+assert sum_series(0,2,1) == lucas(0)
+assert sum_series(1,2,1) == lucas(1)
+assert sum_series(2,2,1) == lucas(2)
+assert sum_series(5,2,1) == lucas(5)
