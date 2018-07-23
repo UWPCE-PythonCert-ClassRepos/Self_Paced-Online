2855c1a7a12facfb6fadf981cbb1e4e5334e293e
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-07-23 16:09:04 -0700

    Finished second bullet point.

--- students/toddschultz/session04/file_lab.py
+++ students/toddschultz/session04/file_lab.py
@@ -6,4 +6,10 @@ def full_path():
 	for file in files:
 		print(path, file)
 
-full_path()
+def file_copy():
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/lorem.txt', 'r') as f1:
+		data = f1.read()
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy.txt', 'w') as f2:
+		f2.write(data)
+
+file_copy()
