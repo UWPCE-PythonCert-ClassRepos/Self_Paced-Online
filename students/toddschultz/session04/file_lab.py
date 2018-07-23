4261f170f21ca90b19665f9340ae4d37f81f20fb
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-07-23 16:21:09 -0700

    Complete first sub-bullet point.

--- students/toddschultz/session04/file_lab.py
+++ students/toddschultz/session04/file_lab.py
@@ -12,4 +12,13 @@ def file_copy():
 	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy.txt', 'w') as f2:
 		f2.write(data)
 
+def advanced_copy():
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/lorem2.txt', 'r') as f3:
+		while True:
+			line = f3.readline()
+			if not line:
+				break
+			with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy2.txt', 'w') as f4:
+				f4.write(line)
 file_copy()
+advanced_copy()
