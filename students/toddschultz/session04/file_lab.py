d15337ca67b03306da70a7c25b87364a0d309104
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-07-23 17:12:44 -0700

    Done!   :-)

--- students/toddschultz/session04/file_lab.py
+++ students/toddschultz/session04/file_lab.py
@@ -13,12 +13,25 @@ def file_copy():
 		f2.write(data)
 
 def advanced_copy():
-	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/lorem2.txt', 'r') as f3:
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/lorem.txt', 'r') as f3:
 		while True:
 			line = f3.readline()
 			if not line:
 				break
 			with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy2.txt', 'w') as f4:
 				f4.write(line)
-file_copy()
-advanced_copy()
+
+def binary_copy():
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/jeep.jpg', 'rb') as f5:
+		with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy3.jpg', 'wb') as f6:
+			f6.write(f5.read())		
+
+def byte_binary_copy():
+	with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/jeep.jpg', 'rb') as f7:
+		with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/new_copy4.jpg', 'wb') as f8:	
+			while True:
+				buf = f7.read(1)
+				if buf:
+					f8.write(buf)
+				else:
+					break
