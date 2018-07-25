38de53f803f279c4f17595d0ae6a591522ac9ecf
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-07-24 17:27:51 -0700

    Completed the new file writing portion of mailroom2.py. Now I just need to add a few dicts and a bit more formatting with .format.

--- students/toddschultz/session04/mailroom2.py
+++ students/toddschultz/session04/mailroom2.py
@@ -4,15 +4,17 @@ global donors
 donors = [["baby huey", 1123.00, 456.00, 1789.00], ["mighty mouse", 99.99], ["fred flintstone", 5550.00, 5555.00], ["road runner", 199999.00], ["papa smurf", 1001.00, 1002.00, 1003.00]]
 
 def prompt():
-    action = (input("Would you like to (S)end a Thank You, (C)reate a Report or (Q)uit? ").lower())
-    if action == "s":
+    action = (input("Choose an action:\n1 - Send a Thank You\n2 - Create a Report\n3 - Send letters to everyone\n4 - Quit\n>>>> ").lower())
+    if action == "1":
         thank_you()
-    elif action == "c":
+    elif action == "2":
         create_report()
+    elif action == "3":
+        letters()
     elif action == "q" or action == "quit":
         print("Have a nice day!")
     else:
-        print("Please enter S, C, or Q.")
+        print("Please enter 1, 2, 3, or 4\n")
         prompt()
 
 def thank_you():
@@ -65,14 +67,32 @@ def create_report():
     print("\n")
     prompt()
 
+def letters():
+    donors_list = []
+    z = 0
+    for i in donors:
+        donors_list.append([i[0]]) #name
+        donors_list[z].append(sum(i[1:])) #total
+        donors_list[z].append(len(i)) #number + 1
+        aa=sum(i[1:])
+        donors_list[z].append(aa/len(i)) #average
+        z += 1
+    for i in donors_list:
+        name = (i[0])
+        total = (i[1]) 
+        with open('/Users/toddschultz/Projects/UW/Self_Paced-Online/Students/toddschultz/session04/letters/' + name + ".txt", 'w') as f:
+            f.write("\n\n" + name + ":\n\tThank you very much for your generous donation. ")
+            f.write(f"your donation total of ${total:.2f} is awesome ")
+            f.write("Our charity would not exist without your support.\n")
+            f.write("Sincerely:\n\nLeadership Team at Charity X.\n\n")
+    prompt()
+    print("Letters Complete!\n")
+    prompt()
+
+
+
+
 if __name__ == '__main__':
     prompt() 
 
-'''
-Instructor Feedback:
-DONE!  you could add lower() at the end to avoid calling it each time: action = input("").lower()
-a better alternative to calling prompt() all over the place is to have a while True loop so that it forever loops until program is terminated.
-DONE! it would be easier (code-wise) to have a list of lists here like: donors  = [["baby huey", 1123, 456], ["mighty mouse", 99, 100]]
-don't forget this report is supposed to be sorted (check requirements)
-'''
 
