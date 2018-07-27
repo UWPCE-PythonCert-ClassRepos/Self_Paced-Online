86fc02af537b2316e0b2886e46d367a13f0d2e08
Toni Lugosan <tlugosan@hotmail.com>
2018-07-27 00:33:46 -0700

    FizzBuzz implementation

---
+++ students/tlugosan/lesson02/FizzBuzz.py
@@ -0,0 +1,22 @@
+# Print numbers from 1 to 100
+# but for multiple of 3 print Fizz
+# for multiple of 5 print BUzz
+# for multiple of both 3 and 5 print FizzBuzz
+
+def FizzBuzz():
+
+    for i in range(1,101):
+        output =""
+        if(i%3==0):
+            output += "Fizz"
+
+        if(i%5==0):
+            output += "Buzz"
+
+        if(output==""):
+            output += str(i)
+
+        print(output)
+
+if __name__ == '__main__':
+    FizzBuzz()
