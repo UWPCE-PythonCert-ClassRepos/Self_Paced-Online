bc5b24af21df43134605f8866170f033621ab43f
Adam Strawmyer <adam@strawmyer.com>
2018-07-26 19:29:00 -0500

    function 3 for lesson 3 slicing lab added

--- students/astrawmyer/lesson03/SlicingLab.py
+++ students/astrawmyer/lesson03/SlicingLab.py
@@ -11,5 +11,12 @@ def remove_every_other(seq):
     new_seq = seq[0:-1:2]
     return new_seq
 
-seqtup= [2, 54, 13, 12, 5, 32]
-print(remove_every_other(seqtup))
+seqtup= (2, 54, 13, 12, 5, 32)
+#print(remove_every_other(seqtup))
+
+def remove_4(seq):
+    new_seq = seq[0:3] + seq[4:-4]
+    return new_seq
+
+seqtup= (2, 54, 13, 12, 5, 32, 32, 1, 2, 3, 4)
+print(remove_4(seqtup))
