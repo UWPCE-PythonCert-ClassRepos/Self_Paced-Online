3421f3e6e5ddc0c086790012de8bfdc1ab87e99a
Adam Strawmyer <adam@strawmyer.com>
2018-07-26 19:18:32 -0500

    second function of slicing lab lesson 3

--- students/astrawmyer/lesson03/SlicingLab.py
+++ students/astrawmyer/lesson03/SlicingLab.py
@@ -5,4 +5,11 @@ def exchange_first_last(seq):
     return new_seq
 
 seqtup= (2, 54, 13, 12, 5, 32)
-print(exchange_first_last(seqtup))
+#print(exchange_first_last(seqtup))
+
+def remove_every_other(seq):
+    new_seq = seq[0:-1:2]
+    return new_seq
+
+seqtup= [2, 54, 13, 12, 5, 32]
+print(remove_every_other(seqtup))
