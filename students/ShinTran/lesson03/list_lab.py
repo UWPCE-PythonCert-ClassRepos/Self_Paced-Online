aaef266a3b450fabde2c1dbaf0802c5d7767a25d
Swt16 <swtran21@gmail.com>
2018-07-27 19:29:00 -0700

    Syntax update to list_lab

--- students/ShinTran/lesson03/list_lab.py
+++ students/ShinTran/lesson03/list_lab.py
@@ -26,10 +26,10 @@ series1 = ["Strawberries"] + series1
 print(series1)
 series1.insert(0,"Blueberries")
 print(series1)
-for i in range(0,len(series1)):
-	if (series1[i][0] == "P"):
-		print(series1[i])
-
+for fruit in series1:
+    if (fruit[0] == "P"):
+        print(fruit)
+print()
 
 '''
 Using the list created in series 1 above:
@@ -46,12 +46,12 @@ print(series2)
 series2 = series1 * 2
 print(series2)
 response2 = ''
-newSeries2 = series2[:]
+new_series2 = series2[:]
 while (response2 not in series2):
-	response2 = input("Type a fruit to remove from the list > ")
-	newSeries2[:] = (value for value in newSeries2 if value != response2)
-print(newSeries2)
-
+    response2 = input("Type a fruit to remove from the list > ")
+    new_series2[:] = (value for value in new_series2 if value != response2)
+print(new_series2)
+print()
 
 '''
 Again, using the list from series 1:
@@ -61,15 +61,15 @@ For any answer that is not “yes” or “no”, prompt the user to answer with
 Display the list.
 '''
 series3 = series1[:]
-newSeries3 = series3[:]
-for j in range(0,len(series3)):
-	response3 = input("Do you like {} (yes/no)? ".format(series3[j]))
-	while (response3 not in ["yes","no"]):
-		response3 = input("Do you like {} (yes/no)? ".format(series3[j]))
-	if response3 == "no":
-		newSeries3.remove(series3[j])
-print(newSeries3)
-
+new_series3 = series3[:]
+for fruit in series3:
+    response3 = input("Do you like {} (yes/no)? ".format(fruit))
+    while (response3 not in ["yes","no"]):
+        response3 = input("Do you like {} (yes/no)? ".format(fruit))
+    if response3 == "no":
+        new_series3.remove(fruit)
+print(new_series3)
+print()
 
 '''
 Once more, using the list from series 1:
@@ -77,12 +77,12 @@ Make a copy of the list and reverse the letters in each fruit in the copy.
 Delete the last item of the original list. Display the original list and the copy.
 '''
 series4 = series1[:]
-newSeries4 = series4[:]
-for k in range(0,len(newSeries4)):
-	newSeries4[k] = newSeries4[k][::-1]
+new_series4 = []
+for fruit in series4:
+    new_series4.append(fruit[::-1])
 series4.pop()
 print(series4)
-print(newSeries4)
+print(new_series4)
 
 
 
