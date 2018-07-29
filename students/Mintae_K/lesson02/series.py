1cb8aef18bf5f100ce557158a0ba66e8af7e0188
Mintae <mtwow789@gmail.com>
2018-07-28 23:19:35 -0700

    added asser test

--- students/Mintae_K/lesson02/series.py
+++ students/Mintae_K/lesson02/series.py
@@ -5,6 +5,7 @@ def fibonacci(n):
             x = x + [x[i-2] + x[i-1]]
     print (x)
     print(x[i])
+    return x[i]
 
 def lucas(n):
     x=[2, 1]
@@ -13,6 +14,7 @@ def lucas(n):
             x = x + [x[i-2] + x[i-1]]
     print (x)
     print(x[i])
+    return x[i]
 
 def sum_series(n,first=0,second=1):
     x=[first, second]
@@ -21,3 +23,13 @@ def sum_series(n,first=0,second=1):
             x = x + [x[i-2] + x[i-1]]
     print (x)
     print(x[i])
+    return x[i]
+
+#test to see 10th number in lucas series is equal to 77
+assert lucas(10) == 76
+
+#test to see 10th number in fib series is equal to 77
+assert fibonacci(10) == 34
+
+#test to see 10th number in fib series is equal to 10th in sum_series
+assert fibonacci(10) == sum_series(10)
