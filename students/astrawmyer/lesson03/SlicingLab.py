0d0b46b55c430ca470a676222cb1b2ca12648371
Adam Strawmyer <adam@strawmyer.com>
2018-07-29 18:54:03 -0500

    4th and 5th functions of lesson 3 slicing lab

--- students/astrawmyer/lesson03/SlicingLab.py
+++ students/astrawmyer/lesson03/SlicingLab.py
@@ -4,19 +4,30 @@ def exchange_first_last(seq):
     new_seq = seq[-1:] + seq[1:-1] + seq[:1]
     return new_seq
 
-seqtup= (2, 54, 13, 12, 5, 32)
+
 #print(exchange_first_last(seqtup))
 
 def remove_every_other(seq):
     new_seq = seq[0:-1:2]
     return new_seq
 
-seqtup= (2, 54, 13, 12, 5, 32)
+
 #print(remove_every_other(seqtup))
 
 def remove_4(seq):
     new_seq = seq[0:3] + seq[4:-4]
     return new_seq
 
-seqtup= (2, 54, 13, 12, 5, 32, 32, 1, 2, 3, 4)
-print(remove_4(seqtup))
+
+def reverse(seq):
+    new_seq = seq[::-1]
+    return new_seq
+
+
+def thirds(seq):
+    length = len(seq)
+    new_seq = seq[int(length/3):] + seq[0:int(length/3)]
+    return new_seq
+
+seqtup= (2, 54, 13, 12, 5, 32, 32, 1, 2, 3, 4, 5)
+print(thirds(seqtup))
