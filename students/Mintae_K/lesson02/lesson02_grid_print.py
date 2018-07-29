4944c3f3054679fa70d70793561771fb3a582d9c
Mintae <mtwow789@gmail.com>
2018-07-28 22:24:16 -0700

    Grid Print solution ready for submittal

--- students/Mintae_K/lesson02/lesson02_grid_print.py
+++ students/Mintae_K/lesson02/lesson02_grid_print.py
@@ -1,1 +1,25 @@
-print("this is test")
+def print_grid(n):
+    plus="+ "
+    dash="- "
+    row_1=plus+dash*n
+    row=row_1*2+plus
+    column_1="| "+"  "*n
+    column=column_1*3
+    print(row)
+    for i in range(2):
+        for i in range(n):
+            print(column)
+        print(row)
+
+def print_grid2(x,y):
+    plus="+ "
+    dash="- "
+    row_1=plus+dash*y
+    row=row_1*x+plus
+    column_1="| "+"  "*y
+    column=column_1*(x+1)
+    print(row)
+    for i in range(x):
+        for i in range(y):
+            print(column)
+        print(row)
