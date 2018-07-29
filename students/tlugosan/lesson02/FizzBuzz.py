7ac8782954252e5e5176cbc1af98454a8e122982
Toni Lugosan <tlugosan@hotmail.com>
2018-07-29 14:51:31 -0700

    fixed more coding styles to adder to pep8 rules

--- students/tlugosan/lesson02/FizzBuzz.py
+++ students/tlugosan/lesson02/FizzBuzz.py
@@ -1,22 +1,24 @@
-# Print numbers from 1 to 100
-# but for multiple of 3 print Fizz
-# for multiple of 5 print BUzz
-# for multiple of both 3 and 5 print FizzBuzz
+""" Print numbers from 1 to 100
+but for multiple of 3 print Fizz
+ for multiple of 5 print BUzz
+ for multiple of both 3 and 5 print FizzBuzz   """
 
 def fizz_buzz():
 
-    for i in range(1,101):
-        output =""
-        if(i%3==0):
+    for i in range(1, 101):
+        output = ""
+        if i % 3 == 0:
             output += "Fizz"
 
-        if(i%5==0):
+        if i % 5 == 0:
             output += "Buzz"
 
-        if(output==""):
+        if output == "":
             output += str(i)
 
         print(output)
 
+
 if __name__ == '__main__':
+
     fizz_buzz()
