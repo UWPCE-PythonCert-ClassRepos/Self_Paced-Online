29af383b69444aacbc3461d3f5ca9cf2b8b1f5b8
Adam Strawmyer <adam@strawmyer.com>
2018-07-29 19:39:57 -0500

    sequence 2 list lab lesson 3

--- students/astrawmyer/lesson03/list_lab.py
+++ students/astrawmyer/lesson03/list_lab.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #Series 1
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+""" fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 response = input("Add another fruit: ")
 fruit.append(response)
 print(fruit)
@@ -23,4 +23,13 @@ for i in fruit:
     if fruit[j][0]=="P":
         p_fruit.append(fruit[j])
     j=j+1
-print(p_fruit)
+print(p_fruit) """
+
+#Series 2
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruit)
+fruit.pop()
+print(fruit)
+response = input("Type a fruit to delete:")
+fruit.remove(response)
+print(fruit)
