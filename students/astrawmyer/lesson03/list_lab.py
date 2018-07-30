00730be2559046c1bf3216bc3c4f92eed48c6c35
Adam Strawmyer <adam@strawmyer.com>
2018-07-29 19:51:49 -0500

    incomplete series 3 for lesson 3 list lab

--- students/astrawmyer/lesson03/list_lab.py
+++ students/astrawmyer/lesson03/list_lab.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Series 1
+# Series 1
 """ fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 response = input("Add another fruit: ")
 fruit.append(response)
@@ -23,13 +23,25 @@ for i in fruit:
     if fruit[j][0]=="P":
         p_fruit.append(fruit[j])
     j=j+1
-print(p_fruit) """
+print(p_fruit)
 
-#Series 2
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-print(fruit)
-fruit.pop()
-print(fruit)
+# Series 2
+# Made fruit2 so list from first sequence unchanged for series 3 and 4.
+fruit2 = fruit
+print(fruit2)
+fruit2.pop()
+print(fruit2)
 response = input("Type a fruit to delete:")
-fruit.remove(response)
-print(fruit)
+fruit2.remove(response)
+print(fruit2) """
+
+# Series 3
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+fruit3 = fruit
+for i in fruit3:
+    response = input("Do you like " + i.lower() + "?")
+    if response == "no":
+        fruit3.remove(i)
+print(fruit3)
+
+# look into how enumerate woorks to fix this/make it easier.
