32f049e8428ddfae8c94befe3f41a19667a679b2
Toni Lugosan <tlugosan@hotmail.com>
2018-08-01 22:11:07 -0700

    fixed bugs in series1 and series 2, added implementation for series3 and series4

--- students/tlugosan/lesson03/list_lab.py
+++ students/tlugosan/lesson03/list_lab.py
@@ -1,6 +1,6 @@
 #! python
 
-# region SERIES1
+# region series1
 """Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”"""
 fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
 
@@ -15,16 +15,14 @@ fruit_list.append(response1.capitalize())
 print(fruit_list)
 print()
 
-"""Ask for a number and display the number and the corespnding fruit on a 1-is basis."""
-response2 = int(
-    input("Please provide a number between 1 and %s: " % len(fruit_list)))
+"""Ask for a number and display the number and the corresponding fruit on a 1-is basis."""
+response2 = input("Please provide a number between 1 and %s: " % len(fruit_list))
 while True:
-    if response2 < 1 or response2 > len(fruit_list):
-        response2 = int(
-            input("Try again a number between 1 and %s: " % len(fruit_list)))
+    if not response2.isnumeric() or int(response2) < 1 or int(response2) > len(fruit_list):
+        response2 = input("Try again a number between 1 and %s: " % len(fruit_list))
     else:
         break
-print("%d : %s" % (response2, fruit_list[response2 - 1]))
+print("%d : %s" % (int(response2), fruit_list[int(response2) - 1]))
 print()
 
 """Add another fruit at the beginning of the list using +  then print the list"""
@@ -44,16 +42,16 @@ print("Fruit that start with letter P: ", end=" ")
 for item in fruit_list:
     if item.startswith("P"):
         print(item, end=" ")
-
 print()
+print("---------------------------------")
 # endregion
 
-# region SERIES2
+# region series2
 """Display the fruit list."""
 print(fruit_list)
 
 """Remove the last fruit from the list."""
-fruit_list.remove(fruit_list[len(fruit_list) - 1])
+fruit_list.pop(len(fruit_list) - 1)
 print("Last fruit from the list was removed")
 
 """Display list."""
@@ -79,5 +77,44 @@ while True:
     else:
         response6 = input("This fruit doesn't exist in the list. Please try again: ")
 print(fruit_list)
+print("---------------------------------")
+# endregion
 
+# region series3
+"""Given a fruit list ask the user if they like it or not.
+ For no remove the fruit from the list then display the leftover list."""
+
+fruit_list3 = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruit_list3)
+YES = "Yes"
+NO = "No"
+temp_fruit_list = []
+for item in fruit_list3:
+    response7 = input("Do you like %s? Please answer with yes or no: " % item.lower())
+    while not (response7.lower() == YES.lower() or response7.lower() == NO.lower()):
+            response7 = input("Please answer only with yes or no: ")
+    if response7.lower() == YES.lower():
+        temp_fruit_list.append(item)
+fruit_list3 = temp_fruit_list
+print("The fruit that you like are: %s" % fruit_list3)
+print("---------------------------------")
+# endregion
+
+# region series4
+"""Make a copy of the list and reverse the letters in each fruit in the copy.
+Delete the last item of the original list. Display the original list and the copy."""
+
+fruit_list4 = ["Apples", "Pears", "Oranges", "Peaches"]
+print("Original list: %s" % fruit_list4)
+print()
+
+reversed_fruit_list = []
+for item in fruit_list4:
+    reversed_fruit_list.append(item[::-1])
+print("Copy of reversed items list: %s" % reversed_fruit_list)
+print()
+
+fruit_list4.pop(len(fruit_list4) -1)
+print("Original list once the last item was removed: %s" % fruit_list4)
+print("Copy of reversed items list: %s" % reversed_fruit_list)
 # endregion
