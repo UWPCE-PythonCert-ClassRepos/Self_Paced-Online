785d201afe872bbd1e784f848a3d2b8ea61488ac
mxwllndrsn <mxwllndrsn@gmail.com>
2018-08-03 08:22:56 -0700

    task 1 complete

---
+++ students/mxwllndrsn/lesson03/strformat_lab.py
@@ -0,0 +1,11 @@
+#uw python 210
+#lesson 03
+#max anderson
+
+#string lab
+
+def str_format():
+
+    atuple = (2, 123.4567, 10000, 12345.67)
+
+    print('file_00{:d}: {:.2f}, {:.2e}, {:.2e}'.format(*atuple))
