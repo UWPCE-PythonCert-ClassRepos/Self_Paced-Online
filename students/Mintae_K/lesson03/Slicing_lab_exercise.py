dad03eeb6ebbe6549e73280431fce9b2acb62124
Mintae <mtwow789@gmail.com>
2018-08-03 23:13:52 -0700

    works for str and tuple

--- students/Mintae_K/lesson03/Slicing_lab_exercise.py
+++ students/Mintae_K/lesson03/Slicing_lab_exercise.py
@@ -1,3 +1,18 @@
 def slicing_exercise(x):
-    s= x[-1:]+x[1:-1]+x[0]
-    print ("with the first and last items exchanged",s)
+    print(x)
+    s= x[-1:] + x[1:-1] + x[0:1]
+    print ("with the first and last items exchanged:",s)
+    #next
+    s = x[::2]
+    print ("with every other item removed:",s)
+    #next
+    s = x[4:-4:2]
+    print ("first 4, last 4 removed, and every other item in between:",s)
+    #next
+    s = x[::-1]
+    print ("with the elements reversed (just with slicing):",s)
+    #next
+    third = int(len(x)/3)
+    s = x[third:-third] + x[-third:] + x[:third]
+    print ("with the middle third, then last third, then first third:",s)
+    return
