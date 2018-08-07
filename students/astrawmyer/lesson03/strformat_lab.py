792753bcd3cf8c726f427fc33f40b8e2acbcf46d
Adam Strawmyer <adam@strawmyer.com>
2018-08-06 20:15:19 -0500

    lesson03 format lab part 6

--- students/astrawmyer/lesson03/strformat_lab.py
+++ students/astrawmyer/lesson03/strformat_lab.py
@@ -32,9 +32,26 @@ four = (4, 30, 2017, 2, 27)
 out_four = '{3:0>2} {4} {2} {0:0>2} {1}'.format(*four)
 #print(out_four)
 
-#Task FIve
+
+#Task Five
 
 five = ['oranges', 1.3, 'lemons', 1.1]
-
 five_out = f"The weight of an {five[0][:-1]} is {five[1]} and the weight of a {five[2][:-1]} is {five[3]}"
-print(five_out)
+#print(five_out)
+
+#Task Six
+
+six_one = ("Manny", 26, "$34062.00")
+six_two = ("Chris", 32, "$2300.00")
+six_three = ("Adam", 33, "$1733.33")
+six_four = ("Dylan", 25, "$164.26")
+
+six = (six_one, six_two, six_three, six_four)
+
+for i in six:
+    print("{:<10}{:<4} {:>10}".format(*i))
+
+#Task Six Part Two
+
+six_part_two = (0,1,2,3,4,5,6,7,8,9)
+print(("{:<5}"*10).format(*six_part_two))
