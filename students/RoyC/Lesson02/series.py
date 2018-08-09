03c4c8655b7b09699514b82f20b9a5ea51d0be10
Roy Crowley <roybillups@comcast.net>
2018-08-08 18:35:21 -0700

    Lesson 2, series.py, complete with assert tests in module.

--- students/RoyC/Lesson02/series.py
+++ students/RoyC/Lesson02/series.py
@@ -2,7 +2,9 @@ def fibonacci(n):
     """
     Returns the nth member of the Fibonacci series
     Function is naturally recursive, calling itself for all but the first two members.
-    :param n: the index of the Lucas series member to be returned
+
+    Arguments:
+        n: the index of the Lucas series member to be returned
     """
     return sum_series(n)
         
@@ -10,7 +12,9 @@ def lucas(n):
     """
     Returns the nth member of the Lucas series
     Function is naturally recursive, calling itself for all but the first two members.
-    :param n: the index of the Lucas series member to be returned
+
+    Arguments:
+        n: the index of the Lucas series member to be returned
     """
     return sum_series(n, 2, 1)
         
@@ -20,9 +24,11 @@ def sum_series(n, num0=0, num1=1):
     series is the sum of the previous two numbers. The optional two arguments, num0 and num1,
     are the first two numbers in the series, the defaults of 0 and 1 represent the Fibonacci series.
     Function is naturally recursive, calling itself for all but the first two members.
-    :param n: the index of the Lucas series member to be returned
-    :param num0: the first number in the series
-    :param num1: the second number in the series
+    
+    Arguments:
+        n: the index of the Lucas series member to be returned
+        num0: the first number in the series (default 0)
+        num1: the second number in the series (default 1)
     """
     if n == 0:
         return num0
@@ -30,3 +36,32 @@ def sum_series(n, num0=0, num1=1):
         return num1
     else:
         return sum_series(n-2, num0, num1) + sum_series(n-1, num0, num1)
+        
+if __name__ == "__main__":
+
+    # Test random values in the Fibonacci series
+    assert(fibonacci(0) == 0)
+    assert(fibonacci(1) == 1)
+    assert(fibonacci(2) == 1)
+    assert(fibonacci(3) == 2)
+    assert(fibonacci(4) == 3)
+    assert(fibonacci(8) == 21)
+    assert(fibonacci(10) == 55)
+    assert(fibonacci(12) == 144)
+    
+    # Test random values in the Lucas series
+    assert(lucas(0) == 2)
+    assert(lucas(1) == 1)
+    assert(lucas(2) == 3)
+    assert(lucas(3) == 4)
+    assert(lucas(4) == 7)
+    assert(lucas(8) == 47)
+    assert(lucas(10) == 123)
+    assert(lucas(12) == 322)
+
+    # Test random values in sum_series (though above tests were also tests)
+    assert(sum_series(4, 5, 3) == 19)
+    assert(sum_series(3, 2, 4) == 10)
+    assert(sum_series(6, 1, 5) == 45)
+    assert(sum_series(4, 4, 4) == 20)
+    assert(sum_series(6, 12, 12) == 156)
