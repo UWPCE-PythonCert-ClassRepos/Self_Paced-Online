7dd9712361a2be08377a4c8c5bce6a7fd1c29b15
Parzival-X <39395936+Parzival-X@users.noreply.github.com>
2018-08-08 23:04:18 -0700

    Completed steps 3 and 4.

--- students/rob_sanchez/Lesson 8/circle_test.py
+++ students/rob_sanchez/Lesson 8/circle_test.py
@@ -13,9 +13,21 @@ class circle_tests(unittest.TestCase):
         c = Circle(5)
         self.assertEqual(5, c.radius)
 
+    # Test step 2 - get circle diameter
     def test_diameter_property(self):
         c = Circle(4)
         self.assertEqual(8, c.diameter)
+
+    # Test step 3 - set diameter property
+    def test_diameter_set(self):
+        c = Circle(4)
+        c.diameter = 2
+        self.assertEqual(1, c.radius)
+
+    # Test step 4 - get area property
+    def test_area_set(self):
+        c = Circle(2)
+        self.assertEqual(12.566370, c.area)
 
 
 if __name__ == '__main__':
