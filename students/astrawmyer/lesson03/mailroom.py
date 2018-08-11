05a0be131c6f7c670fbcc9c633a2dea7a153f5f7
Adam Strawmyer <adam@strawmyer.com>
2018-08-11 16:36:45 -0500

    lesson03 mailroom completed thank you letter module

--- students/astrawmyer/lesson03/mailroom.py
+++ students/astrawmyer/lesson03/mailroom.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-donors = [["Manny Machado",[1,2,3]],["Adam Jones",[1,2,3]]]
+donors = [["Manny Machado",[1.2,2.51,3.20]],["Adam Jones",[1.14,22.21,323.45]]]
 names = ["manny", "adam"]
 
 # Thank you Module
@@ -10,40 +10,40 @@ def displaylist():
         print(sublist[0])
     return
 
-def enterdonation():
+""" def enterdonation(index,amount):
+    print(donors[i][1])
+    return """
+
+def writeletter(name,amount):
+    print('Dear {},'.format(name))
+    print("Thank you for donating ${} to the Human Fund. Your money will be used appropriately.".format(amount))
     return
 
 #function to send a thank you
 def thankyou():
-    
-
     loop_trigger = True
-
     while loop_trigger == True:
         input_name = input("Enter full name: ")
-
         for i, donor_row in enumerate(donors):
-
             if donor_row[0] == input_name:
+                #print(donor_row[0],input_name)
                 #this section for if name typed is correct.
                 #ask for number and add it to list of donations.
                 # print(i,donor_row)
                 donation = float(input("Enter donation amount:"))
                 donors[i][1].append(int(donation))
-                # print(donors)
-                #need to add to original donors structure. need to index???
-                #loop_trigger = False
+                #enterdonation(i,donation)
+                print(donors)
+                writeletter(donors[i][0],donors[i][1][len(donors[i][1])-1])
+                loop_trigger = False
                 break
             elif input_name == 'list':
                 displaylist()
                 break
-            else:
-                break
 
-# to do: make the add to donation into a separate function.
-# make a function to write a letter.
-# these need a way to keep track of which name and which value is new.
 
+
+#def createreport():
 
 
 
@@ -60,6 +60,8 @@ print("What do you want to do?")
 response = input("1. Send a Thank You, 2. Create a Report, 3. Quit")
 if response == '1':
     thankyou()
+if response == '2':
+    createreport()
 
 
 
