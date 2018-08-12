968cda9f8a1553d2d199de567331af502fb0b3ad
Adam Strawmyer <adam@strawmyer.com>
2018-08-11 20:59:25 -0500

    lesson03 mailroom complete. added name=main

--- students/astrawmyer/lesson03/mailroom.py
+++ students/astrawmyer/lesson03/mailroom.py
@@ -2,21 +2,14 @@
 
 donors = [["Manny Machado",[12.2,2.51,3.20]],["Adam Jones",[1024.14,22.21,323.45]],["Chris Davis",[3.2,5.55,4.20]]]
 
-# Thank you Module
 
 def displaylist():    
     for sublist in donors:
         print(sublist[0])
 
-
-""" def enterdonation(index,amount):
-    print(donors[i][1])
-    return """
-
 def writeletter(name,amount):
     print('Dear {},'.format(name))
     print("Thank you for donating ${:.2f} to the Human Fund. Your money will be used appropriately.".format(amount))
-
 
 def writereport(donors):
     print('Donor Name                | Total Given | Num Gifts | Average Gift')
@@ -25,7 +18,7 @@ def writereport(donors):
         print('{1:27}${0:11.2f}{2:12}  ${3:12.2f}'.format(*i))
 
 
-#function to send a thank you
+# Function to send a thank you letter.
 def thankyou():
     loop_trigger = True
     while loop_trigger == True:
@@ -43,34 +36,29 @@ def thankyou():
                 break
 
 
-
-
+# Function to write a report of the donors.
 def createreport():
     donors_report = []
-    
     for i, name in enumerate(donors):
         sum_donation = 0
         avg_donation = 0
-        
         for j in donors[i][1]:
             sum_donation = sum_donation + j
             num_donation = len(donors[i][1])
         avg_donation = sum_donation/num_donation
         donors_report.append([sum_donation, name[0], num_donation, avg_donation])
     donors_report.sort(reverse=True)
-    print("")
     writereport(donors_report)
-    print("")
 
 
 
 
-
-print("What do you want to do?")
-response = input("1. Send a Thank You, 2. Create a Report, 3. Quit: ")
-if response == '1':
-    thankyou()
-if response == '2':
-    createreport()
-if response == '3':
-    exit()
+if __name__ == "__main__":
+    print("What do you want to do?")
+    response = input("1. Send a Thank You, 2. Create a Report, 3. Quit: ")
+    if response == '1':
+        thankyou()
+    if response == '2':
+        createreport()
+    if response == '3':
+        exit()
