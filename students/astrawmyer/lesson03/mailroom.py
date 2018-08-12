0d7202d7c5c4fd1fd418fa54520fe0ca76261a01
Adam Strawmyer <adam@strawmyer.com>
2018-08-11 20:52:12 -0500

    lesson 3 mailroom completed all steps. need main block

--- students/astrawmyer/lesson03/mailroom.py
+++ students/astrawmyer/lesson03/mailroom.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-donors = [["Manny Machado",[12.2,2.51,3.20]],["Adam Jones",[1024.14,22.21,323.45]]]
-names = ["manny", "adam"]
+donors = [["Manny Machado",[12.2,2.51,3.20]],["Adam Jones",[1024.14,22.21,323.45]],["Chris Davis",[3.2,5.55,4.20]]]
 
 # Thank you Module
 
 def displaylist():    
     for sublist in donors:
         print(sublist[0])
-    return
+
 
 """ def enterdonation(index,amount):
     print(donors[i][1])
@@ -16,8 +15,15 @@ def displaylist():
 
 def writeletter(name,amount):
     print('Dear {},'.format(name))
-    print("Thank you for donating ${} to the Human Fund. Your money will be used appropriately.".format(amount))
-    return
+    print("Thank you for donating ${:.2f} to the Human Fund. Your money will be used appropriately.".format(amount))
+
+
+def writereport(donors):
+    print('Donor Name                | Total Given | Num Gifts | Average Gift')
+    print('-'*67)
+    for i in donors:
+        print('{1:27}${0:11.2f}{2:12}  ${3:12.2f}'.format(*i))
+
 
 #function to send a thank you
 def thankyou():
@@ -26,13 +32,8 @@ def thankyou():
         input_name = input("Enter full name: ")
         for i, donor_row in enumerate(donors):
             if donor_row[0] == input_name:
-                #print(donor_row[0],input_name)
-                #this section for if name typed is correct.
-                #ask for number and add it to list of donations.
-                # print(i,donor_row)
                 donation = float(input("Enter donation amount:"))
-                donors[i][1].append(int(donation))
-                #enterdonation(i,donation)
+                donors[i][1].append(float(donation))
                 print(donors)
                 writeletter(donors[i][0],donors[i][1][len(donors[i][1])-1])
                 loop_trigger = False
@@ -40,81 +41,36 @@ def thankyou():
             elif input_name == 'list':
                 displaylist()
                 break
-    return
+
 
 
 
 def createreport():
     donors_report = []
-    sum_donation = 0
+    
     for i, name in enumerate(donors):
-        for j in donors[0][1]:
+        sum_donation = 0
+        avg_donation = 0
+        
+        for j in donors[i][1]:
             sum_donation = sum_donation + j
-        num_donation = len(donors[i][1])
+            num_donation = len(donors[i][1])
         avg_donation = sum_donation/num_donation
-        donors_report.append([name[0],sum_donation, num_donation,avg_donation])
-        print(name[0],sum_donation, num_donation,avg_donation)
-        print(donors_report)
-    return
-
-
-
-
-
-
-
+        donors_report.append([sum_donation, name[0], num_donation, avg_donation])
+    donors_report.sort(reverse=True)
+    print("")
+    writereport(donors_report)
+    print("")
 
 
 
 
 
 print("What do you want to do?")
-response = input("1. Send a Thank You, 2. Create a Report, 3. Quit")
+response = input("1. Send a Thank You, 2. Create a Report, 3. Quit: ")
 if response == '1':
     thankyou()
 if response == '2':
     createreport()
-
-
-
-
-
-
-
-
-""" loop_trigger = True
-
-while loop_trigger == True:
-    input_name = input("Enter full name: ")
-
-    #This block checks if the input name is in the list of donors.
-    for i in donors:
-        if i[0] == input_name:
-            #this section for if name typed is correct.
-            #ask for number and add it to list of donations.
-            donation = float(input("Enter donation amount:"))
-            #need to add to original donors structure. need to index???
-            loop_trigger = False
-            break
-        elif input_name == 'list':
-            #Prints a list of donors.
-            for j in donors:
-                print(j[0])
-        else:
-            #Back to requesting input name.
-            break
-     """
-    
-
-
-
-#        for i in donors:
-#            print(i[0])
-
-#    for i in fruit3:
-#    response = input("Do you like " + i.lower() + "? ")
-#    while response not in ['no','yes']:
-#        response = input("Respond with only 'yes' or 'no': ")
-#    if response == "yes":
-#        new_fruit.append(i) 
-# print(new_fruit)
+if response == '3':
+    exit()
