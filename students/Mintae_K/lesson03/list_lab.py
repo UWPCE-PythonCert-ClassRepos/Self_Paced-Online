333218fde38880c36109af4dbefe1b5c89115da4
Mintae <mtwow789@gmail.com>
2018-08-11 17:06:32 -0700

    added series2

--- students/Mintae_K/lesson03/list_lab.py
+++ students/Mintae_K/lesson03/list_lab.py
@@ -11,3 +11,21 @@ print(a_list)
 response = input("Tell me another fruit to add to the beginning > ")
 a_list.insert(0,response)
 print(a_list)
+print("All the fruits that begin with P or p are:")
+for x in range(len(a_list)):
+    if a_list[x][0] is 'P':
+        print(a_list[x])
+    elif a_list[x][0] is 'p':
+        print(a_list[x])
+#Series2
+print(a_list)
+a_list.pop()
+print(a_list)
+a_list = a_list * 2
+response = input("Tell me a fruit to delete > ")
+while response not in a_list:
+    response = input("Tell me a fruit to delete > ")
+while response in a_list:
+    a_list.remove(response)
+print(a_list)
+#Series 3
