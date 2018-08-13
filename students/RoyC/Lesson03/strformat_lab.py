78149eeebe5b56a63015e815a7300e06dfc2384c
Roy Crowley <roybillups@comcast.net>
2018-08-12 22:43:03 -0700

    Lesson 3, part 3 string formatting, Task Two complete

--- students/RoyC/Lesson03/strformat_lab.py
+++ students/RoyC/Lesson03/strformat_lab.py
@@ -4,3 +4,7 @@
 # Task One
 in_tuple = ( 2, 123.4567, 10000, 12345.67)
 print("file_{:03d}: {:.2f}, {:.2e}, {:.2e}".format(*in_tuple))
+
+# Task Two
+print(f"file_{in_tuple[0]:03}: {in_tuple[1]:.2f}, {in_tuple[2]:.2e}, {in_tuple[3]:.2e}")
+
