b907ded785424003eb4d5e2931c1ae3acb1e4ae1
Roy Crowley <roybillups@comcast.net>
2018-08-13 20:12:55 -0700

    Lesson 3, part 3 string formatting, Task Three complete

--- students/RoyC/Lesson03/strformat_lab.py
+++ students/RoyC/Lesson03/strformat_lab.py
@@ -8,3 +8,15 @@ print("file_{:03d}: {:.2f}, {:.2e}, {:.2e}".format(*in_tuple))
 # Task Two
 print(f"file_{in_tuple[0]:03}: {in_tuple[1]:.2f}, {in_tuple[2]:.2e}, {in_tuple[3]:.2e}")
 
+# Task Three
+def formatter(in_tuple):
+    """
+    Return formatted string with the integer values in the given tuple
+    """
+    out_string = "The {:d} numbers are " + ("{:d},"*len(in_tuple))
+    # print out formatted string, minus last extranneous comma
+    return out_string.format(len(in_tuple), *in_tuple)[:-1]
+    
+print(formatter((1, 4, 2, 11, 9)))
+print(formatter((5, 1, 0)))
+print(formatter((3, 6, 9, 12, 15, 18)))
