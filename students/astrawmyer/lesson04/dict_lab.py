2e82663ecbb1e658fe19cfa68349df5fd1116098
Adam Strawmyer <adam@strawmyer.com>
2018-08-19 15:00:58 -0500

    lessson 4 dict lab start

---
+++ students/astrawmyer/lesson04/dict_lab.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+dict1 = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+print(dict1)
+dict1.pop('cake')
+print(dict1)
+dict1['fruit'] = 'Mango'
+print(dict1.keys())
+print(dict1.values())
