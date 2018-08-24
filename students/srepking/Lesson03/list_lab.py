54d41df24b79acf920ffbcea391638a378d1bad6
srepking <shanerepking@gmail.com>
2018-08-24 09:48:06 -0700

    Updates on Series 1 through 3

--- students/srepking/Lesson03/list_lab.py
+++ students/srepking/Lesson03/list_lab.py
@@ -10,9 +10,11 @@ print('\n'"Nice Choice! You chose {number}, which corresponds to the {fruit}"'\n
 number=response,fruit=fruits[int(response)-1]))
 #Add another to the beginning using '+'
 fruits=['tomato']+fruits
+print('\n''We added tomatoes to the beginning of the list')
 print(fruits)
 #Add another to the beginning using 'insert'
 fruits.insert(0,'kiwi')
+print('\n''We added kiwis to the beginning of the list')
 print (fruits)
 
 #Display all the fruits that begin with P, using a for loop
@@ -20,6 +22,7 @@ new_list=[]
 for i in fruits:
      if i[0]=='P':
         new_list+=[i]
+print('\n''These are the fruits that start with the letter P ')
 print(new_list)
   
   
@@ -29,29 +32,31 @@ print(fruits)
 
 #Remove the last fruit
 del fruits[-1:]
+print('\n''Now we remove the last fruit')
 print(fruits)
 #Now we will ask a user which fruit to remove. First double the fruit list.
-fruits=fruits*2
-print(fruits)
+fruits2=fruits*2
+print('\n''We Double our fruit list')
+print(fruits2)
 response=input('\n''Which fruit would you like to remove?')
-for i in fruits:
+for i in fruits2:
     try:
-        del fruits[fruits.index(response)]
+        del fruits2[fruits2.index(response)]
     except ValueError as error:
         break
-
-print (fruits)
+print('\n''These are the fruits left in your basket')
+print (fruits2)
 
 #Start of Series 3
-fruits=['Apples', 'Pears', 'Oranges', 'Peaches']
+
 
 new_list=fruits[:]
 for i in fruits:
     while True:
         response=input("Do you like {fruit}? yes or no > ".format(fruit=i.lower()))
-        if 'no' in response or 'yes' in response:
-            break
-    if 'no' in response:
+        if response == 'no' or response == 'yes':
+            break        
+    if response == 'no':
         del new_list[new_list.index(i)]
  
 print(new_list) 
