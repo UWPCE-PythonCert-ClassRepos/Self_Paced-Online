d69dc8d265f7d3adadf03d4be9f8687a21b1bb1d
Shane Repking <shanerepking@gmail.com>
2018-08-26 11:46:53 -0700

    Ready to Submit

--- students/srepking/Lesson03/Slicing.py
+++ students/srepking/Lesson03/Slicing.py
@@ -3,11 +3,18 @@ def first_to_last(x):
     new_list = x[-1:] + x[1:-1] + x[0:1]
     return new_list
 
+print('Swap first to last')
+print(first_to_last([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 14, 15, 16]))
+
 
 def remove_every_other(x):
     """Remove every other item in list"""
     new_list = x[0::2]
     return new_list
+
+
+print('Remove Every Other')
+print(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 14, 15, 16]))
 
 
 def four_and_four(x):
@@ -16,9 +23,17 @@ def four_and_four(x):
     return x[4:-4:2]
 
 
+print('Trim first four and last four, then return every other')
+print(four_and_four([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 14, 15, 16]))
+
+
 def reverse_list(x):
     """Returns the list in reverse order."""
     return x[::-1]
+
+
+print('Return the reverse list')
+print(reverse_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 14, 15, 16]))
 
 
 def thirds(x):
@@ -37,4 +52,8 @@ def thirds(x):
     else:
         print('This list is not divisible by three')
     return
+
+
+print('Creates a list with the middle third and last third, then the first third in the new order')
+print(thirds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 14, 15, 16, 17, 18]))
 
