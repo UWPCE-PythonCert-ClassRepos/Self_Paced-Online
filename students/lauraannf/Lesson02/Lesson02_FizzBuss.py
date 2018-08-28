7bfdd09ebb6666ea9074c63804ab5dedaf685970
lauraannf <lauraannf@gmail.com>
2018-08-28 14:59:19 -0400

    changed 6 to 5 (whoops); added fizzbuzz component

--- students/lauraannf/Lesson02/Lesson02_FizzBuss.py
+++ students/lauraannf/Lesson02/Lesson02_FizzBuss.py
@@ -1,7 +1,9 @@
 for it in range(1, 101):
-    if it % 3 == 0 and it % 6 != 0:
+    if it % 3 == 0 and it % 5 != 0:
         print('Fizz')
-    elif it % 6 == 0:
+    elif it % 5 == 0 and it % 3 != 0:
         print('Buzz')
+    elif it % 5 == 0 and it % 3 == 0:
+        print('FizzBuzz')
     else:
         print(it)
