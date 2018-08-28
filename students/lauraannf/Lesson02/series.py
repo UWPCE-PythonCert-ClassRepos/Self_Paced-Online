3fceb89ffcf99f03c43e8b09660abee7763ded79
lauraannf <lauraannf@gmail.com>
2018-08-28 17:05:14 -0400

    added sum_series function

--- students/lauraannf/Lesson02/series.py
+++ students/lauraannf/Lesson02/series.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-    """return the nth value of the Fibonacci series"""
+    """Return the nth value of the Fibonacci series."""
     if n == 0:
         return 0
     elif n == 1:
@@ -15,7 +15,7 @@ def fibonacci(n):
 
 
 def lucas(n):
-    """return the nth value of the Lucas series"""
+    """Return the nth value of the Lucas series."""
     if n == 0:
         return 2
     elif n == 1:
@@ -28,4 +28,24 @@ def lucas(n):
             luc1 = luc2
             luc2 = luc
         return luc
- 
+
+
+def sum_series(n, val1=0, val2=1):
+    """
+    Return the nth value of a series f, such that f(n)=f(n-2)+f(n-1).
+    :param n: The value of the series requested
+    :param val1: First optional parameter.
+                 The first value of the series (Default 0)
+    :param val2: Second optional parameter.
+                 The second value of the series (Default is 1)
+    """
+    if n == 0:
+        return val1
+    elif n == 1:
+        return val2
+    else:
+        for it in range(1, n):
+            valn = val1 + val2
+            val1 = val2
+            val2 = valn
+        return valn
