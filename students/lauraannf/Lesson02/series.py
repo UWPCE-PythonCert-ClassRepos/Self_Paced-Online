ff89e4483b1f4d3a35c535f1e42abdf08516d1b9
lauraannf <lauraannf@gmail.com>
2018-08-28 15:43:17 -0400

    series.py

---
+++ students/lauraannf/Lesson02/series.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib1 = 0
+        fib2 = 1
+        for it in range(1, n):
+            fib = fib1 + fib2
+            fib1 = fib2
+            fib2 = fib
+        return fib
