2484d9a684bfbaa20da6d98d349a3ff418e638a4
lauraannf <lauraannf@gmail.com>
2018-08-28 16:33:20 -0400

    added lucas function

--- students/lauraannf/Lesson02/series.py
+++ students/lauraannf/Lesson02/series.py
@@ -14,4 +14,18 @@ def fibonacci(n):
         return fib
 
 
-def 
+def lucas(n):
+    """return the nth value of the Lucas series"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        luc1 = 2
+        luc2 = 1
+        for it in range(1, n):
+            luc = luc1 + luc2
+            luc1 = luc2
+            luc2 = luc
+        return luc
+ 
