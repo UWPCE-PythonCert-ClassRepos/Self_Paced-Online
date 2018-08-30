9e67a9c16bd0a0b4fd49f9d4d646fc7ddae170a7
srepking <shanerepking@gmail.com>
2018-08-29 20:20:09 -0700

    mailroom2.py and kata.py are ready for review.

--- students/srepking/Lesson04/mailroom2.py
+++ students/srepking/Lesson04/mailroom2.py
@@ -1,11 +1,12 @@
 def thank_you():
     print('\n''Ok, you chose 1, lets see what we can do.')
     while True:
-        name = input('\n''Type the full name to add entry, \'list\' to see list of names, or \'e\' to exit: > ')
+        name = input('\n''Type the full name to add entry, '
+                     '\'list\' to see list of names, or \'e\' to exit: > ')
         # See if they want a list of names.
         if name != 'list':
             break
-        for x in donors:    
+        for x in donors:
             print(x)
     # Add Name to list if not in list
     if name not in donors and name != 'e':
@@ -16,84 +17,101 @@ def thank_you():
         if amount != 'e':
             new_amount = [float(amount)]
             old_amount = donors[name]
-            donors[name] = old_amount+new_amount
-            print('Thank you so much for the generous gift of ${0:.2f}, {1}!'.format(float(amount),name))
+            donors[name] = old_amount + new_amount
+            print('Thank you so much for the generous gift of ${0:.2f}, {1}!'
+                  .format(float(amount), name))
 
 
 def report():
-        print('\n''A summary of your donors donations:')
-        # Create a new dictionary with Total, number of donations, and average donation amount
-        donors_f = {}
-        for name, donations in donors.items():
-            # Calculate total donation amount
-            total = 0
-            count = 0
-            average = 0
-            i = 0
-            for i in donations:
-                total = total+i
-            # Calculate total number of donations, not counting $0
-            for i in donations:
-                if i != 0:
-                    count = count+1
-            #Calculate total donation amount
-            if count > 0:
-                average = total/count
-            donors_f[name] = [total, count, average]
-        print('\n')
-        # Find the longest name in the list to format column width
-        name_list = list(donors_f.keys())#creates a list of keys
-        name_wi = 11
-        for i in name_list:
-            if len(i) > name_wi:
-                name_wi = (len(i))  # width of name column
-        # Find the longest donation amount
-        tot_wi = 12
-        ave_wi = 12
-        num_wi = 12
-        for name, summary in donors_f.items():
-            if len(str(summary[0])) > tot_wi:
-                tot_wi = (len(str(summary[0])))+3# width of total column
-            if len(str(summary[1])) > num_wi:
-                num_wi = (len(str(summary[1])))+3 #width of number of donations column
-            if len(str(int(summary[2]))) > ave_wi:
-                ave_wi = (len(str(int(summary[2]))))+3 #width of total average column
-        list_sorted = sorted(donors_f,key = donors_f.__getitem__, reverse = True)
-        #Print the Table
-        print(f"{'Donor Name':{name_wi}}| {'Total Given':^{tot_wi}}| {'Num Gifts':^{num_wi}}| {'Average Gift':^{ave_wi}}")
-        print(f"{'-':-^{(name_wi+tot_wi+ave_wi+num_wi+8)}}")
-        #print("-")
-        for key in list_sorted:
-            temp=donors_f[key]
-            print(f"{key:{name_wi}}${temp[0]:{tot_wi}}{temp[1]:^{num_wi}}   ${temp[2]:>{ave_wi}.2f}")
+    print('\n''A summary of your donors donations:')
+    # Create a new dictionary with Total, number of donations,
+    # and average donation amount
+    donors_f = {}
+    for name, donations in donors.items():
+        # Calculate total donation amount
+        total = 0
+        count = 0
+        average = 0
+        i = 0
+        for i in donations:
+            total = total + i
+        # Calculate total number of donations, not counting $0
+        for i in donations:
+            if i != 0:
+                count = count + 1
+        # Calculate total donation amount
+        if count > 0:
+            average = total / count
+        donors_f[name] = [total, count, average]
+    print('\n')
+    # Find the longest name in the list to format column width
+    name_list = list(donors_f.keys())  # creates a list of keys
+    name_wi = 11
+    for i in name_list:
+        if len(i) > name_wi:
+            name_wi = (len(i))  # width of name column
+    # Find the longest donation amount
+    tot_wi = 12
+    ave_wi = 12
+    num_wi = 12
+    for name, summary in donors_f.items():
+        if len(str(summary[0])) > tot_wi:
+            # width of total column
+            tot_wi = (len(str(summary[0]))) + 3
+            # width of number of donations column
+        if len(str(summary[1])) > num_wi:
+            num_wi = (len(str(summary[1]))) + 3
+            # width of total average column
+        if len(str(int(summary[2]))) > ave_wi:
+            ave_wi = (len(str(int(summary[2])))) + 3
+    list_sorted = sorted(donors_f, key=donors_f.__getitem__, reverse=True)
+    # Print the Table
+    print(f"{'Donor Name':{name_wi}}| {'Total Given':^{tot_wi}}| "
+          f"{'Num Gifts':^{num_wi}}| {'Average Gift':^{ave_wi}}")
+    print(f"{'-':-^{(name_wi+tot_wi+ave_wi+num_wi+8)}}")
+    # print("-")
+    for key in list_sorted:
+        temp = donors_f[key]
+        print(f"{key:{name_wi}}${temp[0]:{tot_wi}}{temp[1]:^{num_wi}}   "
+              f"${temp[2]:>{ave_wi}.2f}")
 
-        print('\n')
-        return donors_f
+    print('\n')
+    return donors_f
+
+
 def letters_for_all():
     path_letters = os.getcwd()
     print(f"You chose to send letters for everyone. "
-          f"The letters have been completed and you can find them here: {path_letters}")
+          f"The letters have been completed and you "
+          f"can find them here: {path_letters}")
     donors_f = report()
     for donor, donation in donors.items():
         donation_summary = donors_f[donor]
-        letter = f'Dear {donor}, thank you so much for your last contribution of ${donation[-1]:.2f}!' \
-                 f' You have contributed a total of ${donation_summary[0]:.2f}, and we appreciate your support!'
+        letter = f'Dear {donor}, thank you so much for your last ' \
+                 f'contribution of ${donation[-1]:.2f}! ' \
+                 f'You have contributed a total of ${donation_summary[0]:.2f}, ' \
+                 f'and we appreciate your support!'
         # Write the letter to a destination
-        with open(donor+'.txt', 'w') as to_file:
+        with open(donor + '.txt', 'w') as to_file:
             to_file.write(letter)
+
+
 def goodbye():
     pass
 
 
 if __name__ == '__main__':
     import os
-    donors= {'Joe Edgar Allen Poe the Third ':[1,4,200],'Jack':[4,5], 'Jill':[4], 'Jake':[.30],'Jim':[1,2,1.04]}
+
+    donors = {'Joe Edgar Allen Poe the Third ': [1, 4, 200],
+              'Jack': [4, 5], 'Jill': [4], 'Jake': [.30],
+              'Jim': [1, 2, 1.04]}
     while True:
         response = input(
             '\nChoose an Action:\n'
             '1 - Send a Thank You\n'
             '2 - Create a Report\n'
-        
+
             '3 - Send letters to everyone\n'
             '4 - Quit\n'
             '>>')
@@ -102,7 +120,3 @@ if __name__ == '__main__':
         if response == '4':
             print('Goodbye!')
             break
-
-
-        
-
