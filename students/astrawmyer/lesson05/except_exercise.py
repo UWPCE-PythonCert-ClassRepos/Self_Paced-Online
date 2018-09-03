0bb1e3630f7b91afc87fc1750e156c4a5663dc38
Adam Strawmyer <adam@strawmyer.com>
2018-09-03 14:03:20 -0500

    L5 except block for first error

--- students/astrawmyer/lesson05/except_exercise.py
+++ students/astrawmyer/lesson05/except_exercise.py
@@ -14,7 +14,11 @@ from except_test import fun, more_fun, last_fun
 # in that catch block, try again with the second item in the list
 first_try = ['spam', 'cheese', 'mr death']
 
-joke = fun(first_try[0])
+try:
+    joke = fun(first_try[0])
+except NameError:
+    print("No joke for {}".format(first_try[0]))
+    joke = fun(first_try[1])
 
 # Here is a try/except block. Add an else that prints not_joke
 try:
