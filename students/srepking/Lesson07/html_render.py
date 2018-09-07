723674525cf357ddadbf5d3a05e8e83ce2ba1412
srepking <shanerepking@gmail.com>
2018-09-07 14:02:27 -0700

    Three tests are passing.

--- students/srepking/Lesson07/html_render.py
+++ students/srepking/Lesson07/html_render.py
@@ -1,15 +1,22 @@
-class Element():
+class Element:
     """Creates html code for given inputs"""
+    tag = 'html'
 
-
-    def __init__(self, content = ''):
-        self.content = content
+    def __init__(self, content = None):
+        if content == None:
+            self.contents = []
+        else:
+            self.contents = [content]
 
     def append(self, some_string):
-        self.content = self.content + some_string
+        self.contents.append(some_string)
+
+    def render(self, file_out):
+            file_out.write("<{}>\n".format(self.tag))
+            for content in self.contents:
+                file_out.write(content)
+                file_out.write('\n')
+            file_out.write("<\{}>\n".format(self.tag))
 
 
-#page = Element()
-#page.append("some test text ")
-#page.append("add some more")
-#print(page.content)
+
--- students/srepking/Lesson07/test_html_render.py
+++ students/srepking/Lesson07/test_html_render.py
@@ -1,6 +1,6 @@
 import unittest
 import html_render as hr
-
+from io import StringIO
 
 class TestHTML(unittest.TestCase):
 
@@ -8,19 +8,50 @@ class TestHTML(unittest.TestCase):
 # The html code is output to "test_html_output1.html". Read in the page data
 # and test.
     def test_append(self):
-        expected_text = "Here is a paragraph of text -- there could be more of them, " \
-                      "but this is enough  to show that we can do some text." \
-                      "And here is another piece of text -- you should be able to " \
-                      "add any number"
+        expected_list = ['Here is a paragraph of text.', 'Here is some more.']
 
         page = hr.Element()
 
-        page.append("Here is a paragraph of text -- there could be more of them, "
-                    "but this is enough  to show that we can do some text.")
+        page.append("Here is a paragraph of text.")
 
-        page.append("And here is another piece of text -- you should be able to add any number")
+        page.append("Here is some more.")
 
-        self.assertEqual(page.content, expected_text)
+        self.assertEqual(page.contents, expected_list)
+
+    def test_render_1(self):
+        #The render function will take in a filename and indent,
+        # and write html formatted content to the file that looks
+        # like this:
+        test_text = "<html>\n" \
+                    "Some content.\n" \
+                    "Some more content.\n" \
+                    "<\html>\n"
+
+        page = hr.Element()
+        page.append("Some content.")
+        page.append("Some more content.")
+        #And the render will wrap these two pieces of text with html tags, and output to this file.
+
+        f = StringIO()
+        page.render(f)
+        self.assertEqual(f.getvalue(), test_text)
+
+    def test_render_2(self):
+        # Same as test above, but checking that we can pass text through the Element() method:
+        test_text = "<html>\n" \
+                    "Start with....\n" \
+                    "Some content.\n" \
+                    "Some more content.\n" \
+                    "<\html>\n"
+
+        page = hr.Element("Start with....")
+        page.append("Some content.")
+        page.append("Some more content.")
+        #And the render will wrap these two pieces of text with html tags, and output to this file.
+
+        f = StringIO()
+        page.render(f)
+        self.assertEqual(f.getvalue(), test_text)
 
 if __name__ == '__main__':
     unittest.main()
