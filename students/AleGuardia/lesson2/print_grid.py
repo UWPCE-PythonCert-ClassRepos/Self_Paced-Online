d6b3cf23ba712eb659a6e49ddbd93e82510e0523
aguardia <alejandro.guardia@gmail.com>
2018-09-10 14:35:03 -0600

    first exercise code simplified

--- students/AleGuardia/lesson2/print_grid.py
+++ students/AleGuardia/lesson2/print_grid.py
@@ -2,21 +2,20 @@
 
 
 def print_grid_std():
-# prints a standard grid, takes no argument
-	joint = "+"
-	row_marks =" - "
-	colum_marks="|"
-	print(joint+row_marks*4+joint+row_marks*4+joint)
-	print()
-	for i in range(4):
-	    print(colum_marks+" "*12+colum_marks+" "*12+colum_marks)
-	    print()
-	print(joint+row_marks*4+joint+row_marks*4+joint)
-	print()
-	for i in range(4):
-	    print(colum_marks+" "*12+colum_marks+" "*12+colum_marks)
-	    print()
-	print(joint+row_marks*4+joint+row_marks*4+joint)
-	
+    # prints a standard grid, takes no arguments
+    q_rows = 11
+    joint = "+"
+    row_marks = " - "
+    colum_marks="|"
+    row = joint+row_marks*4+joint+row_marks*4+joint
+    colum = colum_marks+" "*12+colum_marks+" "*12+colum_marks
+    for i in range(q_rows):
+        if i == 0 or i == q_rows//2 or i == q_rows-1:
+            print(row)
+            print()
+            continue
+        print(colum)
+        print()
+
+
 print_grid_std()
-      
