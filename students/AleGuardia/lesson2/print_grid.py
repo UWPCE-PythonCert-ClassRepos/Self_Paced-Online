16a07ee0d903bf43d17514a662077315c837d71d
aguardia <alejandro.guardia@gmail.com>
2018-09-10 15:12:18 -0600

    added function to customize size of grid

--- students/AleGuardia/lesson2/print_grid.py
+++ students/AleGuardia/lesson2/print_grid.py
@@ -6,16 +6,39 @@ def print_grid_std():
     q_rows = 11
     joint = "+"
     row_marks = " - "
-    colum_marks="|"
+    column_marks="|"
     row = joint+row_marks*4+joint+row_marks*4+joint
-    colum = colum_marks+" "*12+colum_marks+" "*12+colum_marks
+    column = column_marks+" "*12+column_marks+" "*12+column_marks
     for i in range(q_rows):
         if i == 0 or i == q_rows//2 or i == q_rows-1:
             print(row)
             print()
             continue
-        print(colum)
+        print(column)
         print()
 
 
 print_grid_std()
+
+
+def print_grid_custom(n):
+    # takes a grid size value and prints the grid
+
+    q_marks = (n//2)
+    q_rows = q_marks*2 + 3
+    joint = "+"
+    row_marks = " - "
+    column_marks = "|"
+    blank = len(row_marks)*q_marks*" "
+    row = joint + row_marks * q_marks + joint + row_marks * q_marks + joint
+    column = column_marks + blank + column_marks + blank + column_marks
+    for i in range(q_rows):
+        if i == 0 or i == q_rows // 2 or i == q_rows - 1:
+            print(row)
+            print()
+            continue
+        print(column)
+        print()
+
+
+print_grid_custom(3)
