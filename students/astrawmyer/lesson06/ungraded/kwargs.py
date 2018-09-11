70fa6dab94e22a620c05c53e4ba78a75ffb72c36
Adam Strawmyer <adam@strawmyer.com>
2018-09-10 20:59:20 -0500

    lesson 6 kwargs lab

--- students/astrawmyer/lesson06/ungraded/kwargs.py
+++ students/astrawmyer/lesson06/ungraded/kwargs.py
@@ -9,3 +9,21 @@ print(keywords('purple', link_color='red', back_color='blue'))
 regular = ('red', 'blue')
 links = {'link_color': 'chartreuse'}
 print(keywords(*regular, **links))
+
+def keywords2(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return args, kwargs
+
+print("keywords 2")
+
+print(keywords2('red', 'blue', 'yellow', 'chartreuse'))
+print('next')
+print(keywords2(link_color='red', back_color='blue'))
+print('next')
+print(keywords2('purple', link_color='red', back_color='blue'))
+print('next')
+
+regular = ('red', 'blue')
+links = {'link_color': 'chartreuse'}
+print(keywords2(*regular, **links))
