b43e490983faff416864c9f09482a65dfcc372c0
aguardia <alejandro.guardia@gmail.com>
2018-09-12 15:29:14 -0600

    added task one and two to strformat_lab

--- students/AleGuardia/Lesson03/strformat_lab.py
+++ students/AleGuardia/Lesson03/strformat_lab.py
@@ -2,7 +2,10 @@
 
 # Task One
 
+print('file_{:0>3d}: {:.2f}, {:.2e}, {:.2e}'.format(2, 123.4567, 10000, 12345.67))
+
+# Task Two
+
 element_tuple = (2, 123.4567, 10000, 12345.67)
-
 print('file_{:0>3d}: {:.2f}, {:.2e}, {:.2e}'.format(*element_tuple))
-
+print(f'file_{element_tuple[0]:0>3d}: {element_tuple[1]:.2f}, {element_tuple[2]:.2e}, {element_tuple[3]:.2e}')
