17a3bde059225a79a842dd2ad79486361c69214f
Adam Strawmyer <adam@strawmyer.com>
2018-09-11 20:00:06 -0500

    lesson06 partial test of display list

--- students/astrawmyer/lesson06/test_mailroom.py
+++ students/astrawmyer/lesson06/test_mailroom.py
@@ -2,7 +2,20 @@
 
 import mailroom as m
 import pytest
+import sys
+
+
+
+def test_display_list(capsys):
+    result = m.display_list()
+    out, err = capsys.readouterr()
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+    assert out.startswith("Man")
 
 def test_write_letter():
-    expected = "Dear Manny,\nThank you for donating $8 to the Human Fund. Your money will be used appropriately."
-    assert write_letter("Manny",8) == expected
+    expected = "Dear Manny,\nThank you for donating $8.00 to the Human Fund. " \
+            "Your money will be used appropriately."
+    assert m.write_letter("Manny",8) == expected
+
+#m.display_list()
