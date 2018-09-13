874aeb4d782e29d339694a3c6c42f5108f771a40
aguardia <alejandro.guardia@gmail.com>
2018-09-13 14:20:01 -0600

    added main menu and quit function

--- students/AleGuardia/Lesson03/mailroom.py
+++ students/AleGuardia/Lesson03/mailroom.py
@@ -6,6 +6,8 @@ donations = [[['William Gates, III'], [1000000, 585000, 5750000]],
              [['Paul Allen'], [25000,1000]],
              [['Elon Musk'], [30000,3499]]]
 
+responses = ["Send a Thank You","Create a Report", "quit"]
+
 
 def return_average(seq):
     return sum(seq)/len(seq)
@@ -25,7 +27,24 @@ def return_report(data):
     return report
 
 
+def initial_prompt():
+    resp = input(f"Please choose one of the following: "
+                 f"'{responses[0]}', '{responses[1]}', or '{responses[2]}' :")
+    return resp
+
+
 if __name__ == '__main__':
-    print(return_report(donations))
+    response = initial_prompt()
+    while response != responses[-1]:
+        if response == responses[0]:
+            print("Thank you")
+        elif response == responses[1]:
+            print(return_report(donations))
+        response = initial_prompt()
+
+
+
+
+
 
 
