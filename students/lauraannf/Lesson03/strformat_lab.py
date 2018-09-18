c4febacd57b3a933d450b85f9e260a78e797c4f3
lauraannf <lauraanng@gmail.com>
2018-09-18 17:25:53 -0400

    lesson 3 files

--- students/lauraannf/Lesson03/strformat_lab.py
+++ students/lauraannf/Lesson03/strformat_lab.py
@@ -4,4 +4,9 @@ Created on Tue Sep 18 17:00:52 2018
 
 @author: Laura.Fiorentino
 """
+print('----------Task One--------')
+testtuple = (2, 123.4567, 10000, 12345.67)
+files = 'file_{:03d} :   {:.2f}, {:.2e}, {:.3g}'.format(testtuple[0], testtuple[1], testtuple[2], testtuple[3])
+#files = 'file_{:03d} :   {}, {}, {}'.format(testtuple)
+print(files)
 
