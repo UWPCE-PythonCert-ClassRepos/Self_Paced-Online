ccc3f7bd50bfde8f10dc68d6bd2c661ec4026c60
Adam Strawmyer <adam@strawmyer.com>
2018-09-17 19:20:40 -0500

    l06 file test

--- students/astrawmyer/lesson06/test_mailroom.py
+++ students/astrawmyer/lesson06/test_mailroom.py
@@ -3,6 +3,7 @@
 import mailroom as m
 import pytest
 import sys
+import os
 
 
 def test_display_list(capsys):
@@ -49,13 +50,25 @@ def test_thank_you_new_donor():
         return input_val.pop(0)
     m.input = mock_input
     m.thank_you()
-    assert m.ddonors == {"Manny Machado": [12.2,2.51,3.20],
+    assert m.ddonors == {"Manny Machado": [12.2,2.51,3.20,2131],
                         "Adam Jones": [1024.14,22.21,323.45], 
                         "Chris Davis": [3.2,5.55,4.20],
                         "Robert Wickens": [9000.1]}
 
-#find way to reset ddonors variable
-#def test_create_report():
 
+def test_create_report(capsys):
+    m.create_report()
+    expected_1 = 'Donor Name                | Total Given | Num Gifts | Average Gift\n'
+    expected_2 = '-------------------------------------------------------------------\n'
+    expected_3 = 'Robert Wickens             $    9000.10           1  $     9000.10\n'
+    expected = expected_1 + expected_2 + expected_3
+    report_input = [[1369.80,'Adam Jones',3,456.60]]
+    m.write_report(report_input)
+    out, err = capsys.readouterr()
+    assert out.startswith(expected)
 
-
+def test_all_letters():
+    m.all_letters()
+    assert os.path.isfile('Manny Machado.txt')
+    assert os.path.isfile('Chris Davis.txt')
+    assert os.path.isfile('Adam Jones.txt')
