38dc8de4b040c547e87e2da990a3138e3e2950e3
Mintae <mtwow789@hotmail.com>
2018-09-18 19:30:38 -0700

    added file read commands

--- students/Mintae_K/lesson04/dict_lab.py
+++ students/Mintae_K/lesson04/dict_lab.py
@@ -14,3 +14,34 @@ d2['name'] = d['name'].count('t')
 d2['city'] = d['city'].count('t')
 d2['cake'] = d['cake'].count('t')
 print(d2)
+
+s2 = set()
+s3 = set()
+s4 = set()
+for i in range(21):
+    if i % 2 == 0:
+        s2.update([i])
+    if i % 3 == 0:
+        s3.update([i])
+    if i % 4 == 0:
+        s4.update([i])
+print("s2 is ", s2)
+print("s3 is ", s3)
+print("s4 is ", s4)
+print(s3.issubset(s2))
+print(s4.issubset(s2))
+
+s2 = set()
+for i in "Python":
+    s2.update([i])
+s2.update(['i'])
+print(s2)
+
+fs1 = frozenset(('m', 'a', 'r', 'a', 't', 'h', 'o', 'n'))
+print(s2.union(fs1))
+print(s2.intersection(fs1))
+
+import os
+place = os.getcwd()
+for root, dirs, files in os.walk(".", topdown=False):
+    print (place, '\\', files[0])
