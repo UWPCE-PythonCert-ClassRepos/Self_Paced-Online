d8f1a5d9884d10090b4c1b927fc02f0bc7dc7411
aguardia <alejandro.guardia@gmail.com>
2018-09-21 12:18:30 -0600

    Dictionary 2 exercise

--- students/AleGuardia/Lesson04/dict_lab.py
+++ students/AleGuardia/Lesson04/dict_lab.py
@@ -2,7 +2,7 @@
 
 dict_example = {"name":"Chris","city":"Seattle","cake":"Chocolate"}
 
-#Dictionary 1
+# Dictionary 1
 
 print(dict_example)
 dict_example.pop("cake")
@@ -11,4 +11,15 @@ dict_example.keys()
 dict_example.values()
 "cake" in dict_example
 "Mango" in dict_example.values()
+
+
+# Dictionary 2
+
+# Using the dictionary from item 1: Make a dictionary
+# using the same keys but with the number of ‘t’s in each value as the value
+# (consider upper and lower case?).
+
+dict_example2 = {}
+for i in dict_example:
+    dict_example2[i] = dict_example[i].lower().count('t')
 
