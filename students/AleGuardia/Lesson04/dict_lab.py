fc51a9f48992757603a3eaadf45074aae407a08f
aguardia <alejandro.guardia@gmail.com>
2018-09-21 11:33:21 -0600

    Dictionary 1 exercises

---
+++ students/AleGuardia/Lesson04/dict_lab.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+dict_example = {"name":"Chris","city":"Seattle","cake":"Chocolate"}
+
+#Dictionary 1
+
+print(dict_example)
+dict_example.pop("cake")
+dict_example['fruit'] = "Mango"
+dict_example.keys()
+dict_example.values()
+"cake" in dict_example
+"Mango" in dict_example.values()
+
