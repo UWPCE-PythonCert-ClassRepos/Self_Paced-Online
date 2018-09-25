365f398f689475da8f1ff139be29e5d40896bac5
Adam Strawmyer <adam@strawmyer.com>
2018-09-24 20:46:27 -0500

    L07 preliminary rendering

--- students/astrawmyer/lesson07/html_render.py
+++ students/astrawmyer/lesson07/html_render.py
@@ -8,5 +8,14 @@ class Element(object):
     
     def append(self, new_content):
         self.content.append(new_content)
+    
+    def render(self,file_out,cur_ind=""):
+        file_out.write(cur_ind + "<html>")
+        for item in self.content:
+            file_out.write('{}</html>\n'.format(item))
 
 
+""" test_element = Element()
+test_element.append("This is content.")
+test_element.append("This is more content.")
+render(test_element,"test_html_output1.html") """
--- students/astrawmyer/lesson07/test_html_render.py
+++ students/astrawmyer/lesson07/test_html_render.py
@@ -7,3 +7,4 @@ def test_element():
     test_element.append("This is more content.")
     assert test_element.content[0] == "This is content."
     assert test_element.content[1] == "This is more content."
+
