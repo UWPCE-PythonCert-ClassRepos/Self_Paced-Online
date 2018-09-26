62ce1b53d64b02571348bc7f7d99c90ceff567ed
pythonwa <tonyamy111@yahoo.com>
2018-09-25 22:34:44 -0700

    adding FizzBuzz and Series.py

--- students/tonylee/FizzBuzz.py
+++ students/tonylee/FizzBuzz.py
@@ -1,13 +1,12 @@
-def printFizzBuzz():
-	for number in range(1, 100):
-		if number%3 == 0 and number%5 == 0:
-			print "FizzBuzz"
-		
-		elif number%3 == 0:
-			print "Fizz"
-		elif number%5 == 0:
-			print "Buzz"
-		else:
-			print number
-			
-printFizzBuzz()
+def print_fizz_buzz():
+    for number in range(1, 100):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+            
+print_fizz_buzz()
