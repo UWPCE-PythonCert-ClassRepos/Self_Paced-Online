da550b845f6c6c2beacbeb2459565f61cf715a2a
Roy Crowley <roybillups@comcast.net>
2018-09-26 21:20:47 -0700

    Step 2 done

--- students/RoyC/Lesson07/html_render.py
+++ students/RoyC/Lesson07/html_render.py
@@ -2,20 +2,47 @@
 
 class Element:
 
-    tag = "html"
+    tag = ""
     indent = 4
     
     def __init__(self, content=None):
         self.content = []
         if not content is None:
-            self.content.append(content)
+            self.append(content)
         
     def append(self, content):
-        self.content.append(content)
+        if isinstance(content, str):
+            self.content.append(TextEntry(content))
+        else:
+            self.content.append(content)
         
     def render(self, file_out, cur_ind=""):
         file_out.write(cur_ind + "<{}>\n".format(self.tag))
         sub_ind = cur_ind + (Element.indent * ' ')
         for entry in self.content:
-            file_out.write(sub_ind + entry + "\n")
-        file_out.write(cur_ind + "<\{}>".format(self.tag))
+            entry.render(file_out, cur_ind + self.indent * " ")
+        file_out.write(cur_ind + "<\{}>\n".format(self.tag))
+        
+class Html(Element):
+    tag = "html"
+
+class Body(Element):
+    tag = "body"
+    
+class P(Element):
+    tag = "p"
+ 
+class TextEntry(Element):
+
+    def __init__(self, text):
+        self.text = text
+        
+    def append(self, content):
+        pass
+        
+    def render(self, file_out, cur_ind=""):
+        file_out.write(cur_ind + self.text + "\n")
+    
+
+    
+
--- students/RoyC/Lesson07/test_html_render.py
+++ students/RoyC/Lesson07/test_html_render.py
@@ -4,23 +4,34 @@ import html_render as hr
 
 from io import StringIO
 
-def test_element():
-    # test output with no content
-    e = hr.Element()
+def test_html():
+    """ test Html output with no content """
+    h = hr.Html()
     f = StringIO()
-    e.render(f)
-    assert(f.getvalue() == "<html>\n<\html>")
+    h.render(f)
+    assert(f.getvalue() == "<html>\n<\\html>\n")
     
-    # test output with 1 line of content
-    e = hr.Element("test1")
+def test_body():
+    """ test Body output with 1 line of content """
+    b = hr.Body("test1")
     f = StringIO()
-    e.render(f)
-    assert(f.getvalue() == "<html>\n    test1\n<\html>")
-    
-    # test output with 2 lines of content on construct and a current indent
-    e = hr.Element()
-    e.append("test1")
-    e.append("test2")
+    b.render(f)
+    assert(f.getvalue() == "<body>\n    test1\n<\\body>\n")
+
+def test_p():    
+    """ test P output with 2 lines of content """
+    p = hr.P()
+    p.append("test1")
+    p.append("test2")
     f = StringIO()
-    e.render(f, "   ")
-    assert(f.getvalue() == "   <html>\n       test1\n       test2\n   <\html>")
+    p.render(f, "   ")
+    assert(f.getvalue() == "   <p>\n       test1\n       test2\n   <\\p>\n")
+
+def test_nested():
+    """ test a nested structure of elements """
+    p = hr.P("test1")
+    b = hr.Body(p)
+    h = hr.Html(b)
+    f = StringIO()
+    h.render(f)
+    assert(f.getvalue() == "<html>\n    <body>\n        <p>\n            test1\n        <\\p>\n    <\\body>\n<\\html>\n")
