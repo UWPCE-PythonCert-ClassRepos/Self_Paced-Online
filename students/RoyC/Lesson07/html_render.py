02f2505fbc43420bcc6926fc136a3f87316845a7
Roy Crowley <roybillups@comcast.net>
2018-09-26 21:29:23 -0700

    added comments

--- students/RoyC/Lesson07/html_render.py
+++ students/RoyC/Lesson07/html_render.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
 
 class Element:
-
+    """
+    An HTML element represents one level of tag, which can contain more nested elements
+    """
+    
     tag = ""
     indent = 4
     
     def __init__(self, content=None):
+        """
+        Optional content is the next element nested under this one
+        """
         self.content = []
         if not content is None:
             self.append(content)
         
     def append(self, content):
+        """
+        Content to be appended is either an Element or a string, which will be used
+        to append a new TextElement
+        """
         if isinstance(content, str):
             self.content.append(TextEntry(content))
         else:
             self.content.append(content)
         
     def render(self, file_out, cur_ind=""):
+        """
+        Renders opening tag, followed by nested elements and closing tag
+        """
         file_out.write(cur_ind + "<{}>\n".format(self.tag))
         sub_ind = cur_ind + (Element.indent * ' ')
         for entry in self.content:
@@ -33,12 +46,16 @@ class P(Element):
     tag = "p"
  
 class TextEntry(Element):
+    """
+    Lowest level of Element, used for plain text line entry
+    """
 
     def __init__(self, text):
         self.text = text
         
     def append(self, content):
-        pass
+        # this is the lowest level Element, append is not applicable
+        raise TypeError("TextEntry element does not support lower-level elements")
         
     def render(self, file_out, cur_ind=""):
         file_out.write(cur_ind + self.text + "\n")
