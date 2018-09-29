e2f3919b5bf7b8f50074efd356a742dcdd9be173
Josh Hoff <jhoff1985@hotmail.com>
2018-09-28 22:41:56 -0700

    added Lucas Numbers function to file

--- students/Josh_HOff/lesson02/series.py
+++ students/Josh_HOff/lesson02/series.py
@@ -9,3 +9,18 @@ def fibonacci(n):
         else:
             num1 = num0 + num1
     return num1 + num0
+def lucas(n):
+    if (n == 0):
+        return 0
+    if (n == 1):
+        return 2
+    if (n == 2):
+        return 1
+    num0 = 2
+    num1 = 1
+    for x in range(2, n - 1):
+        if (x % 2 == 0):
+            num0 = num0 + num1
+        else:
+            num1 = num0 + num1
+    return num1 + num0
