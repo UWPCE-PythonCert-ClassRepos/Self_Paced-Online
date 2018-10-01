4e8a7d9bce5e9fba31b3bb8805b1ab4a1e230abe
Adam Strawmyer <adam@strawmyer.com>
2018-09-30 19:09:58 -0500

    L07 part 6 (link)

--- students/astrawmyer/lesson07/html_render.py
+++ students/astrawmyer/lesson07/html_render.py
@@ -29,8 +29,6 @@ class Element(object):
         # closing tag
         file_out.write('{}</{}>\n'.format(cur_ind,self.tag))
 
-
-
 #subclasses for part 2
 class Html(Element):
     tag = "html"
@@ -49,16 +47,35 @@ class Head(Element):
 class OneLineTag(Element):
     def render(self,file_out,cur_ind=""):
         # opening tag
-        file_out.write('{}<{}>'.format(cur_ind,self.tag))
+        file_out.write('{}<{}'.format(cur_ind,self.tag))
+        for key, value in self.attrs.items():
+            file_out.write(" {}=\"{}\"".format(key, value))
+        file_out.write('>')
         # content
         for item in self.content:
             # Checks if a sub element is here. If so, performs recursive render.
             if isinstance(item, Element):
-                item.render(file_out, cur_ind="    ")
+                item.render(file_out, cur_ind="")
             else:
-                file_out.write("{}".format("    " + item))
+                file_out.write("{}".format("" + item))
         # closing tag
-        file_out.write('{}</{}>\n'.format(cur_ind,self.tag))
+        file_out.write('{}</{}>\n'.format("",self.tag))
 
 class Title(OneLineTag):
     tag = "title"
+
+class SelfClosingTag(Element):
+    def render(self,file_out,cur_ind=""):
+        file_out.write("{}<{} /> \n".format(cur_ind,self.tag))
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+
+class Br(SelfClosingTag):
+    tag = 'br'
+
+class A(OneLineTag):
+    tag = 'a'
+
+    def __init__(self, link, content):
+        super().__init__(content, href = link)
