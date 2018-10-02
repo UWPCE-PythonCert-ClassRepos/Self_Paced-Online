51ecea4144b95d2570404c936d8141ca04b6e3ab
Roy Crowley <roybillups@comcast.net>
2018-10-01 20:55:44 -0700

    done through step 5

--- students/RoyC/Lesson07/html_render.py
+++ students/RoyC/Lesson07/html_render.py
@@ -8,13 +8,14 @@ class Element:
     tag = ""
     indent = 4
     
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         """
         Optional content is the next element nested under this one
         """
         self.content = []
         if not content is None:
             self.append(content)
+        self.attr = kwargs
         
     def append(self, content):
         """
@@ -30,11 +31,16 @@ class Element:
         """
         Renders opening tag, followed by nested elements and closing tag
         """
-        file_out.write(cur_ind + "<{}>\n".format(self.tag))
+        file_out.write(cur_ind + "<{}".format(self.tag))
+        for key, val in self.attr.items():
+            file_out.write(" {}=\"{}\"".format(key, val))
+        file_out.write("}")
         sub_ind = cur_ind + (Element.indent * ' ')
         for entry in self.content:
+            file_out.write("\n")
             entry.render(file_out, cur_ind + self.indent * " ")
-        file_out.write(cur_ind + "<\{}>\n".format(self.tag))
+        file_out.write("\n")
+        file_out.write(cur_ind + "<\{}>".format(self.tag))
         
 class Html(Element):
     tag = "html"
@@ -44,6 +50,27 @@ class Body(Element):
     
 class P(Element):
     tag = "p"
+    
+class Head(Element):
+    tag = "head"
+    
+class OneLineTag(Element):
+    """
+    HTML element that renders on one line
+    """
+    
+    def render(self, file_out, cur_ind=""):
+        """
+        Same render as Element without line feeds
+        """
+        file_out.write(cur_ind + "<{}>".format(self.tag))
+        sub_ind = cur_ind + (Element.indent * ' ')
+        for entry in self.content:
+            entry.render(file_out, cur_ind + self.indent * " ")
+        file_out.write(cur_ind + "<\{}>".format(self.tag))
+        
+class Title(OneLineTag):
+    tag = "title"
  
 class TextEntry(Element):
     """
@@ -58,7 +85,7 @@ class TextEntry(Element):
         raise TypeError("TextEntry element does not support lower-level elements")
         
     def render(self, file_out, cur_ind=""):
-        file_out.write(cur_ind + self.text + "\n")
+        file_out.write(cur_ind + self.text)
     
 
     
