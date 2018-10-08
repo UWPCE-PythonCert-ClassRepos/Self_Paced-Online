39424bd6694ec1177a80af7091d0fe6c62769ff2
Paul Jurek <pjurek3@gmail.com>
2018-10-07 21:57:25 -0700

    added logic to check for divisible by 5 and divisible by both.  Easier to implement it all at this point so just added all steps.  Tested and looks good.

--- students/paul_jurek/lesson2/fizz_buzz.py
+++ students/paul_jurek/lesson2/fizz_buzz.py
@@ -6,7 +6,11 @@ For the multiples of five print “Buzz” instead of the number.
 For numbers which are multiples of both three and five print “FizzBuzz” instead."""
 
 for i in range (1,101):
-	if i%3==0:
+	if (i%3==0) & (i%5==0):
+		print('FizzBuzz')
+	elif i%3==0:
 		print('Fizz')
+	elif i%5==0:
+		print('Buzz')
 	else:
 		print(i)
