c2321edc6f88ff0988449f7b5a7fe362f6aae75b
Josh Hoff <jhoff1985@hotmail.com>
2018-10-09 12:36:22 -0700

    Finished series #1

--- students/Josh_HOff/lesson03/list_lab.py
+++ students/Josh_HOff/lesson03/list_lab.py
@@ -1,7 +1,15 @@
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
-def series1(fruits)
+def series1(fruits):
     print(fruits)
-    response = input('Please add a fruit:')
+    response = input('Please add a fruit: ')
     fruits.append(response)
     print(fruits)
-    response = input('Please indicate 
+    response = input('Please enter a number:')
+    print(response + ' {}'.format(fruits[int(response) - 1]))
+    fruits = ['Pomegranates'] + fruits
+    print(fruits)
+    fruits.insert(0,'Blueberries')
+    print(fruits)
+    for i in fruits:
+        if i[0] == 'P':
+            print(i)
