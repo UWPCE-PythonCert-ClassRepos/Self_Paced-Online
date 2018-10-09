95860bb603dcfcfad05d24b736b8a623c05ede57
Adam Strawmyer <adam@strawmyer.com>
2018-10-08 23:17:11 -0500

    L08

--- students/astrawmyer/lesson08/circle.py
+++ students/astrawmyer/lesson08/circle.py
@@ -1,16 +1,25 @@
+import math
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
-        self._diameter = 2*radius
+
         
     @property
     def diameter(self):
-        return self._diameter
+        return 2 * self.radius
 
     @diameter.setter
-    def diameter(self, diameter):
-        self._diameter = diameter
-        self.radius = diameter/2
+    def diameter(self, val):
+        self.radius = val / 2
+
+    @property
+    def area(self):
+        return (self.radius**2) * math.pi
+    
+"""     @classmethod
+    def from_diameter(cls, diameter): """
+
 
 c = Circle(5)
 print(c.radius)
--- students/astrawmyer/lesson08/test_circle.py
+++ students/astrawmyer/lesson08/test_circle.py
@@ -1,5 +1,6 @@
 import pytest
 import circle
+import math
 
 def test_radius():
     c = circle.Circle(5)
@@ -14,3 +15,7 @@ def test_input_diameter():
     c.diameter = 50
     assert c.diameter == 50
     assert c.radius == 25
+
+def test_area():
+    c = circle.Circle(5)
+    assert c.area == math.pi*25
