06807a65478724c54de975157d05f448e875218c
Paul Jurek <pjurek3@gmail.com>
2018-10-09 20:25:14 -0700

    completed intial fibonacci function.  Ran some manual tests and it looks good.  Also linted with flake8 based off previous feedback

--- students/paul_jurek/lesson2/series.py
+++ students/paul_jurek/lesson2/series.py
@@ -2,11 +2,30 @@
 
 
 def fibonacci(n: int):
-    """return nth value of fibonacci sequesnce
+    """return nth value of fibonacci sequence starting with index 0
     args:
         n: positive integer for indicating sequence number to return
+
 
     returns:
         fib number at nth position
     """
     pass
+
+    if n < 0:
+        raise ValueError('input must be greater than or equal to 0')
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        counter = 2
+        previous = 0
+        current = 1
+
+        while counter <= n:
+            next = previous + current
+            previous, current = current, next
+            counter += 1
+        return next
