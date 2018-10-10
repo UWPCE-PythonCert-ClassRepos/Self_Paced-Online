73fad4ff687e99f9929f3818aba2596f580a4f5c
Paul Jurek <pjurek3@gmail.com>
2018-10-09 19:27:16 -0700

    created new module series and added fibonacci function

---
+++ students/paul_jurek/lesson2/series.py
@@ -0,0 +1,12 @@
+"""module to answer Fibonacci Series Exercise in Lesson 2"""
+
+
+def fibonacci(n: int):
+    """return nth value of fibonacci sequesnce
+    args:
+        n: positive integer for indicating sequence number to return
+
+    returns:
+        fib number at nth position
+    """
+    pass
