c92f80335414f328afc0409ff3cb70cdf4d84d1f
Paul Jurek <pjurek3@gmail.com>
2018-10-09 20:30:56 -0700

    added lucas function.  Just copied fibonacci and changed starting numbers

--- students/paul_jurek/lesson2/series.py
+++ students/paul_jurek/lesson2/series.py
@@ -29,3 +29,33 @@ def fibonacci(n: int):
             previous, current = current, next
             counter += 1
         return next
+
+
+def lucas(n: int):
+    """return nth value of lucas sequence starting with index 0
+    args:
+        n: positive integer for indicating sequence number to return
+
+
+    returns:
+        lucas number at nth position
+    """
+    pass
+
+    if n < 0:
+        raise ValueError('input must be greater than or equal to 0')
+
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        counter = 2
+        previous = 2
+        current = 1
+
+        while counter <= n:
+            next = previous + current
+            previous, current = current, next
+            counter += 1
+        return next
