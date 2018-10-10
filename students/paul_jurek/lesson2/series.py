282ab33f5f9bdd0ed810a60d8863d514bf539beb
Paul Jurek <pjurek3@gmail.com>
2018-10-09 20:45:39 -0700

    updated lucas and fibonaccci functions to reference sum_series

--- students/paul_jurek/lesson2/series.py
+++ students/paul_jurek/lesson2/series.py
@@ -4,61 +4,23 @@
 def fibonacci(n: int):
     """return nth value of fibonacci sequence starting with index 0
     args:
-        n: positive integer for indicating sequence number to return
-
+        n: non-negative integer for indicating sequence number to return
 
     returns:
         fib number at nth position
     """
-    pass
-
-    if n < 0:
-        raise ValueError('input must be greater than or equal to 0')
-
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        counter = 2
-        previous = 0
-        current = 1
-
-        while counter <= n:
-            next = previous + current
-            previous, current = current, next
-            counter += 1
-        return next
+    return sum_series(n=n,index0=0,index1=1)
 
 
 def lucas(n: int):
     """return nth value of lucas sequence starting with index 0
     args:
-        n: positive integer for indicating sequence number to return
-
+        n: non-negative integer for indicating sequence number to return
 
     returns:
         lucas number at nth position
     """
-    pass
-
-    if n < 0:
-        raise ValueError('input must be greater than or equal to 0')
-
-    if n == 0:
-        return 2
-    elif n == 1:
-        return 1
-    else:
-        counter = 2
-        previous = 2
-        current = 1
-
-        while counter <= n:
-            next = previous + current
-            previous, current = current, next
-            counter += 1
-        return next
+    return sum_series(n=n,index0=2,index1=1)
 
 
 def sum_series(n:int, index0:int=0, index1:int=1):
@@ -70,4 +32,4 @@ def sum_series(n:int, index0:int=0, index1:int=1):
         index1: number in second index of series 
     returns:
         value at nth position in summing series"""
-    pass
+    
