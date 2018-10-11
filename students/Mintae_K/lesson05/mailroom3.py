75cbce1b187c2457034f61ee9f22c8ff6629b48a
Mintae <mtwow789@hotmail.com>
2018-10-10 19:09:58 -0700

    added error handling on main menu

--- students/Mintae_K/lesson05/mailroom3.py
+++ students/Mintae_K/lesson05/mailroom3.py
@@ -1,3 +1,15 @@
+# Error Handling function
+def check_error(f_name, s):
+    while True:
+        amount = input(s.format(f_name))
+        try:
+            checked_amount = float(amount)
+            break
+        except ValueError:
+            print('\nValue entered is not a number. Only number allowed.\n')
+    return checked_amount
+
+
 # Sending a Thank you
 def sending_thank():
     f_name = input('Please type the Full Name or list> ')
@@ -9,17 +21,10 @@ def sending_thank():
         f_name = input('Please type the Full Name or list> ')
     if donors.get(f_name):
         s = 'Existing donor, please type the donation amount from {}> '
-        while True:
-            amount = input(s.format(f_name))
-            try:
-                donors[f_name].append(float(amount))
-                break
-            except ValueError:
-                print('\nValue entered is not a number. Only number allowed.')
+        donors[f_name].append(check_error(f_name, s))
     else:
         s = 'New donor, please type the donation amount from {}> '
-        amount = input(s.format(f_name))
-        donors[f_name] = [float(amount)]
+        donors[f_name] = [check_error(f_name, s)]
     print('\n', "Thank you {} for the generous donation!".format(f_name), '\n')
 
 
@@ -28,10 +33,7 @@ def create_report():
     s = '\n, {:<20}| {:<10} | {:<10}| {:<20}'
     print(s.format('Donor Name', 'Total Given', "Num Gift", "Average Gift"))
     print('-' * 60)
-    total = []
-    for key in donors:
-        num_gift = len(donors[key])
-        total.append([sum(donors[key]), num_gift, key])
+    total = [[sum(donors[key]), len(donors[key]), key] for key in donors]
     total.sort(reverse=True)
     s = '{:<20}  $ {:<10.2f}  {:^12} $ {:<20.2f}'
     for i in range(len(total)):
@@ -60,14 +62,17 @@ def main():
     response = '0'
     # menu_prompt
     switch_func_dict = {'1': sending_thank, '2': create_report, '3': send_letters, '4': just_quit}
-    while not (response == '4'):
+    while not response == '4':
         print('Menu')
         print('1. Send a Thank You')
         print('2. Create a Report')
         print('3. Send letters to everyone')
         print('4. Quit')
         response = input("Please type number from menu > ")
-        switch_func_dict.get(response)()
+        try:
+            switch_func_dict.get(response)()
+        except TypeError:
+            print('\n Please type a number from 1 to 4.\n')
 
 
 if __name__ == "__main__":
