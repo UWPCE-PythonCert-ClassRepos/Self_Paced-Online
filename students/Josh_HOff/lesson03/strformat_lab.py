ca351db2787aca381b5ccc9021ba32502faf7303
Josh Hoff <jhoff1985@hotmail.com>
2018-10-11 12:40:45 -0700

    Finished task 6 and bonus question

--- students/Josh_HOff/lesson03/strformat_lab.py
+++ students/Josh_HOff/lesson03/strformat_lab.py
@@ -19,3 +19,17 @@ e = ['oranges', 1.3, 'lemons', 1.1]
 print(f'the weight of an {e[0][:-1]} is {e[1]} and the weight of a {e[2][:-1]} is {e[3]}')
 
 #task 6
+rows = (('Josh', 27, 9001.27), ('Tatsiana', 30, 20009.29), ('Ralph', 50, 40.378934), ('Joey', 24, 12000985.49834))
+
+for i in rows:
+    print(f'{i[0]:<8} {i[1]} ${i[2]}')
+    
+t = (100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
+for i in t:
+    print(f'{i:<5}', end='')
+
+#print(rows[0][0], rows[0][1])
+#for i in rows:
+#for i in rows:
+#    print(i)
+#    print('{:20}{:18}{:20}{:18}'.format(*i))
