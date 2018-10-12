80f022dd0da7cfce19ba351f26fc4f495e92c8bb
Laura Denney <Laura.A.Denney@gmail.com>
2018-10-11 22:37:08 -0700

    added mailroom.py file

---
+++ students/laura_denney/lesson03/mailroom.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------#
+# Title: Mail Room Part 1
+# Dev:   LDenney
+# Date:  October 11, 2018
+# ChangeLog: (Who, When, What)
+#   Laura Denney, 10/11/18, Created File
+#-------------------------------------------------#
+
+tabl = [["John Doe", 100, 200], ["Laura Denney", 5],
+        ["Bill Gates", 4000], ["Samuel Jackson", 1, 2, 3],
+        ["Mr. Bean", 500, 100]]
