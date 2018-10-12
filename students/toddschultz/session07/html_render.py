ef7f238da3867b3899f7a7a104a6737751b9080a
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-10-12 16:02:00 -0700

    Added docstrings

--- students/toddschultz/session07/html_render.py
+++ students/toddschultz/session07/html_render.py
@@ -27,12 +27,12 @@ class Element:
         file_out.write(f"{cur_ind}{self.indent}</{self.tag}>\n")
 
 class P(Element):
-    ''' create the p tag '''
+    ''' create the p tag from Element '''
     tag = 'p'
     indent = "    "
 
 class Html(Element):
-    ''' html tag '''
+    ''' create the html tag from Element, with custom render() '''
     tag = 'html'
 
     def render(self, file_out, cur_indent=''):
@@ -40,18 +40,17 @@ class Html(Element):
         super().render(file_out, cur_indent)
 
 class Body(Element):
-    ''' create the body tag '''
+    ''' create the body tag from Element '''
     tag = 'body'
     indent = "    "
 
 class Head(Element):
-    ''' create the head tag '''
+    ''' create the head tag from Element '''
     tag = 'head'
     indent = "    "
 
 class OneLineTag(Element):
-    ''' for items all on one line '''
-
+    ''' Custom Render for items all on one line '''
     def render(self, file_out, cur_ind = ""):
         tagstring = (f"{cur_ind}{self.indent}<{self.tag}")
         if self.attributes:
@@ -73,7 +72,7 @@ class Title(OneLineTag):
     indent = "        "
 
 class SelfClosingTag(Element):
-
+    ''' create self closing tags with a custom render() '''
     def render(self, file_out, cur_ind = ""):    
         tagstring = (f"{cur_ind}{self.indent}<{self.tag}")
         if self.attributes:
@@ -89,35 +88,39 @@ class SelfClosingTag(Element):
                 file_out.write(f"{cur_ind}{self.indent}{item}\n")
 
 class Hr(SelfClosingTag):
+    ''' create the hr tag from SelfClosingTag() '''
     tag = 'hr'
     indent = "    "
 
 class Br(SelfClosingTag):
+    ''' create the br tag from OneLineTag '''
     tag = 'br'
     indent = "    "
 
 class A(Element):
+    ''' create the a tag for links, with a custom __init__ '''
     tag = 'a'
     indent = "    "
     def __init__(self, link, content):
         super().__init__(content, href=link)
 
 class Ul(Element):
+    ''' create the ul tag from Element '''
     tag = 'ul'
     indent = "    "
 
 class Li(Element):
+    ''' create the li tag from Element '''
     tag = 'li'
     indent = "        "
 
 class H(OneLineTag):
+    ''' create the h tag from OneLineTag with custom __init__ '''
     tag = 'h'
     def __init__(self, level, text):
         super().__init__()
 
 class Meta(SelfClosingTag):
+    ''' create the meta tag from SelfClosingTag() '''
     tag = 'meta'
     indent = "        "
-# Tech Debt
-#render function
-#tests
