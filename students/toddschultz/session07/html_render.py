269c73a03f37eb0b648bf208dcbc29686180ca4e
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-10-12 15:53:15 -0700

    Oh MY, I think I'm done ... only about 6 weeks after I started.

--- students/toddschultz/session07/html_render.py
+++ students/toddschultz/session07/html_render.py
@@ -2,17 +2,22 @@ class Element:
     tag = 'html'
     indent = ""
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.content = [content] if content else []
+        self.attributes = kwargs
  
     def append(self, item):
         """ Appends lines to the list of verbiage"""
         self.content.append(item)
-        
 
     def render(self, file_out, cur_ind = ""):
         """Writes verbiage and appropriate tags to the file"""
-        file_out.write(f"{cur_ind}{self.indent}<{self.tag}>\n")
+        tagstring = (f"{cur_ind}{self.indent}<{self.tag}")
+        if self.attributes:
+            for key, value in self.attributes.items():
+                tagstring += " {}=\"{}\"".format(key, value)
+        tagstring += ">\n"
+        file_out.write(tagstring)
 
         for item in self.content:
             if hasattr(item, 'render'):
@@ -24,17 +29,95 @@ class Element:
 class P(Element):
     ''' create the p tag '''
     tag = 'p'
-    indent = "        "
+    indent = "    "
 
 class Html(Element):
     ''' html tag '''
     tag = 'html'
 
-class Head(Element):
-    ''' create the head tag '''
-    tag = 'head'
+    def render(self, file_out, cur_indent=''):
+        file_out.write(cur_indent + "<!DOCTYPE html>\n")
+        super().render(file_out, cur_indent)
 
 class Body(Element):
     ''' create the body tag '''
     tag = 'body'
     indent = "    "
+
+class Head(Element):
+    ''' create the head tag '''
+    tag = 'head'
+    indent = "    "
+
+class OneLineTag(Element):
+    ''' for items all on one line '''
+
+    def render(self, file_out, cur_ind = ""):
+        tagstring = (f"{cur_ind}{self.indent}<{self.tag}")
+        if self.attributes:
+            for key, value in self.attributes.items():
+                tagstring += " {}=\"{}\"".format(key, value)
+        tagstring += ">"
+        file_out.write(tagstring)
+        
+        for item in self.content:
+            if hasattr(item, 'render'):
+                item.render(file_out, cur_ind=cur_ind)
+            else:
+                file_out.write(f"{cur_ind}{self.indent}{item}")
+        file_out.write(f"{cur_ind}{self.indent}</{self.tag}>\n")
+
+class Title(OneLineTag):
+    ''' create the title tag from OneLineTag '''
+    tag = 'title'
+    indent = "        "
+
+class SelfClosingTag(Element):
+
+    def render(self, file_out, cur_ind = ""):    
+        tagstring = (f"{cur_ind}{self.indent}<{self.tag}")
+        if self.attributes:
+            for key, value in self.attributes.items():
+                tagstring += " {}=\"{}\"".format(key, value)
+        tagstring += ">\n"
+        file_out.write(tagstring)
+
+        for item in self.content:
+            if hasattr(item, 'render'):
+                item.render(file_out, cur_ind=cur_ind)
+            else:
+                file_out.write(f"{cur_ind}{self.indent}{item}\n")
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+    indent = "    "
+
+class Br(SelfClosingTag):
+    tag = 'br'
+    indent = "    "
+
+class A(Element):
+    tag = 'a'
+    indent = "    "
+    def __init__(self, link, content):
+        super().__init__(content, href=link)
+
+class Ul(Element):
+    tag = 'ul'
+    indent = "    "
+
+class Li(Element):
+    tag = 'li'
+    indent = "        "
+
+class H(OneLineTag):
+    tag = 'h'
+    def __init__(self, level, text):
+        super().__init__()
+
+class Meta(SelfClosingTag):
+    tag = 'meta'
+    indent = "        "
+# Tech Debt
+#render function
+#tests
