77fe163ab9734726ddeec5f7347841f64163505a
lauraannf <lauraanng@gmail.com>
2018-10-12 11:47:58 -0400

    lesson 04

--- students/lauraannf/Lesson04/write_a_book.py
+++ students/lauraannf/Lesson04/write_a_book.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 10 12:35:18 2018
-
-@author: Laura.Fiorentino
-"""
 import random
-file = open('hound_small.txt', 'r', encoding='utf8')
+file = open('hound.txt', 'r', encoding='utf8')
 # file = open('testbook.txt', 'r')
 lines = file.readlines()
 del lines[:38]
@@ -22,13 +17,24 @@ for it in range(len(word_list)-2):
         word_dict[word_list[it] + ' ' + word_list[it+1]] = [word_list[it+2]]
 word_list_new = ['Holmes', 'leaned']
 word_key = word_list_new[0] + ' ' + word_list_new[1]
-n=1
+n = 1
 while True:
     if word_key not in word_dict:
         break
     word_list_new.append(random.choice(word_dict[word_key]))
-    word_key =  word_list_new[n] + ' ' + word_list_new[n+1]
-    n=n + 1
+    word_key = word_list_new[n] + ' ' + word_list_new[n+1]
+    n = n + 1
 file.close()
 
-new_book = ' '.join(word_list_new)
+file_new = open('new_hound.txt', 'w')
+n_period = 0
+for it in range(len(word_list_new)):
+    # for it in range(300):
+    file_new.write(word_list_new[it] + ' ')
+    if word_list_new[it][-1] is '.' and len(word_list_new[it]) > 2 and \
+            word_list_new[it] not in ['Mr.', 'Dr.', 'Mrs.']:
+        n_period = n_period + 1
+    if n_period == 5:
+        file_new.write('\n     ')
+        n_period = 0
+file_new.close()
