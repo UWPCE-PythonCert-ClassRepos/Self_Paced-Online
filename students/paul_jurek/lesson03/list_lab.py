b82b00845ef26e6f775810dab76b69a8bd65314c
Paul Jurek <pjurek3@gmail.com>
2018-10-14 14:06:09 -0700

    create list_lab module and filled in with instructions.  Made each step comment and will fill in as I go.

---
+++ students/paul_jurek/lesson03/list_lab.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+When the script is run, it should accomplish the following four series of actions:
+Series 1
+Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+Display the list (plain old print() is fine…).
+Ask the user for another fruit and add it to the end of the list.
+Display the list.
+Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis). Remember that Python uses zero-based indexing, so you will need to correct.
+Add another fruit to the beginning of the list using “+” and display the list.
+Add another fruit to the beginning of the list using insert() and display the list.
+Display all the fruits that begin with “P”, using a for loop.
+Series 2
+Using the list created in series 1 above:
+Display the list.
+Remove the last fruit from the list.
+Display the list.
+Ask the user for a fruit to delete, find it and delete it.
+(Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+Series 3
+Again, using the list from series 1:
+Ask the user for input displaying a line like “Do you like apples?” for each fruit in the list (making the fruit all lowercase).
+For each “no”, delete that fruit from the list.
+For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values (a while loop is good here)
+Display the list.
+Series 4
+Once more, using the list from series 1:
+Make a copy of the list and reverse the letters in each fruit in the copy.
+Delete the last item of the original list. Display the original list and the copy."""
+
+#Series 1
+#Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
+
+#Display the list (plain old print() is fine…).
+print(fruits)
+
+#Ask the user for another fruit and add it to the end of the list.
+#Display the list.
+#Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis). Remember that Python uses zero-based indexing, so you will need to correct.
+#Add another fruit to the beginning of the list using “+” and display the list.
+# Add another fruit to the beginning of the list using insert() and display the list.
+# Display all the fruits that begin with “P”, using a for loop.
+# Series 2
+# Using the list created in series 1 above:
+# Display the list.
+# Remove the last fruit from the list.
+# Display the list.
+# Ask the user for a fruit to delete, find it and delete it.
+# (Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+# Series 3
+# Again, using the list from series 1:
+# Ask the user for input displaying a line like “Do you like apples?” for each fruit in the list (making the fruit all lowercase).
+# For each “no”, delete that fruit from the list.
+# For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values (a while loop is good here)
+# Display the list.
+# Series 4
+# Once more, using the list from series 1:
+# Make a copy of the list and reverse the letters in each fruit in the copy.
+# Delete the last item of the original list. Display the original list and the copy.
