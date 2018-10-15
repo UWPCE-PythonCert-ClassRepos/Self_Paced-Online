1be3945f55080372abb83640170126b6e445ea56
Paul Jurek <pjurek3@gmail.com>
2018-10-15 05:36:37 -0700

    completed task2.  Utilized .format as alternative method.  Very simliar as formatting indicators stay in same spot but we just remove variable placement.  I like Fstrings better though.

--- students/paul_jurek/lesson03/strformat_lab.py
+++ students/paul_jurek/lesson03/strformat_lab.py
@@ -5,6 +5,14 @@
 ( 2, 123.4567, 10000, 12345.67)
 and produce:
 'file_002 :   123.46, 1.00e+04, 1.23e+04'"""
-input = ( 2, 123.4567, 10000, 12345.67)
+input = (2, 123.4567, 10000, 12345.67)
 output = f'file_{input[0]:0=3} :   {input[1]:.2f}, {input[2]:.2E}, {input[3]:.3g}'
 print(output)
+
+# Task 2
+# using standard format to compare with f-strings
+output_format = 'file_{:0=3} :   {:.2f}, {:.2E}, {:.3g}'.format(input[0],
+                                                                input[1],
+                                                                input[2],
+                                                                input[3])
+print(output_format)
