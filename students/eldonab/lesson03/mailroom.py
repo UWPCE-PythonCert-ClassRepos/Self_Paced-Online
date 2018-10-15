d50d0e074d540cf6106d1fd89208fe21f1593e17
Elnura Donabedian <elnurab@gmail.com>
2018-10-15 16:44:37 +0200

    Corrected indentation in create_report func and added docstrings to functions. Thank you for the feedback!

--- students/eldonab/lesson03/mailroom.py
+++ students/eldonab/lesson03/mailroom.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 #create a list of at least 5 donors.
 donors_list = [["John Smith", 50000, 600000], ["Bill Gates", 100000, 400, 7000], ["Andy Jones", 400000, 600000], ["Hannah Jones", 30000, 40000], ["Anna Allen", 80000,90000, 1000]] 
+
+
 # to add a new donation amount to an existing donor's name.
 def add_donation_to_list(res_name):
+    """Add a new donation amount to an existing donor's name."""
     amount = float(input("How much is the new donation amount? "))
     for donor in donors_list:
         if res_name in donor:
             donor.append(amount)
+
+
 # to add a new donor name&donation amount:
 def add_new_donor(new_donor):
+    """Add a new donor name and donation amount."""
     amount = float(input("How much is the new donation amount? "))
     donors_list.append([new_donor, amount])
     
+
 # to check whether donor already exists in the list.
 def is__in_list(donor_name):
+    """Check whether donor already exists in the list of donors."""
     for donor in donors_list:
         if donor_name in donor:
             return True
 
+
 # to show existing list of donor names:
 def show_list():
+    """Show existing list of donor names"""
     for donor in donors_list:
         print(donor[0])
 
@@ -43,6 +53,7 @@ def thank_you_note():
 # to print donation statistics for each donor:
 
 def stat_donors():
+    """Print donation statistics for each donor"""
     for donor in donors_list:
         a_donor = donor[0]
         total_don = sum(donor[1:])
@@ -56,9 +67,10 @@ def stat_donors():
 # to print a list of donors sorted by name, total donated amount, number of donations, and average donation amount:
 
 def create_report():
-              print("{0:<20}{1:>12}{2:>12}{3:>15}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift"))
-              print("--------------------------------------------------------------------------------------------")
-              stat_donors()
+    """Print a list of donors sorted by name, total donated amount, number of donation, and average donation amount"""
+    print("{0:<20}{1:>12}{2:>12}{3:>15}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift"))
+    print("--------------------------------------------------------------------------------------------")
+    stat_donors()
 
 
 
