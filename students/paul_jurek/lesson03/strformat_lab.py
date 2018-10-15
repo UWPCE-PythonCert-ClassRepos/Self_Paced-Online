329f973f7924278723e960999a7904de586dbc60
Paul Jurek <pjurek3@gmail.com>
2018-10-15 05:31:34 -0700

    completed task 1 with formatting.  Utilized fstrings and referenced documenation on string formatting heavily to understand this.

--- students/paul_jurek/lesson03/strformat_lab.py
+++ students/paul_jurek/lesson03/strformat_lab.py
@@ -6,5 +6,5 @@
 and produce:
 'file_002 :   123.46, 1.00e+04, 1.23e+04'"""
 input = ( 2, 123.4567, 10000, 12345.67)
-output = f'file_{input[0]} :   {input[1]}, {input[2]}, {input[3]}'
+output = f'file_{input[0]:0=3} :   {input[1]:.2f}, {input[2]:.2E}, {input[3]:.3g}'
 print(output)
