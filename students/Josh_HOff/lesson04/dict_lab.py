5840af08b1e011b6cee6ca8675bc6f6caba36209
Josh Hoff <jhoff1985@hotmail.com>
2018-10-16 12:38:54 -0700

    Super confused on how to copy a file without using shutil or os copy command

--- students/Josh_HOff/lesson04/dict_lab.py
+++ students/Josh_HOff/lesson04/dict_lab.py
@@ -11,3 +11,26 @@ def dictionary1():
     print('Mango' in personal_details.values())
     
 def dictionary2():
+    personal_details = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+    for i in personal_details:
+        personal_details[i] = personal_details[i].lower().count('t')
+    print(personal_details)
+    
+def sets1():
+    s2 = set(range(0, 21, 2))
+    print(s2)
+    s3 = set(range(0, 21, 3))
+    print(s3)
+    s4 = set(range(0, 21, 4))
+    print(s4)
+    print(s3.issubset(s2))
+    print(s4.issubset(s2))
+    
+def sets2():
+    letters = set('Python')
+    print(letters)
+    letters.add('i')
+    print(letters)
+    letters2 = frozenset('marathon')
+    print(letters.union(letters2))
+    print(letters.intersection(letters2))
