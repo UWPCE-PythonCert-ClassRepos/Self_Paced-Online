7c33d1e309079fcbbf0547aad6d73f8f76766a91
Josh Hoff <jhoff1985@hotmail.com>
2018-10-17 15:46:18 -0700

    Finished mailroom2, still trying to figure out how to save the txt files in a new folder, and how to ask the user for a path/folder

--- students/Josh_HOff/lesson04/mailroom2.py
+++ students/Josh_HOff/lesson04/mailroom2.py
@@ -1,13 +1,12 @@
+import pathlib
+import os
 donors_list = {'Ralph Anders': [5, 10], 'Andrei Wasinski': [101, 151, 75], 'Stalk Holmes': [40], 'Traci Johnston': [20], 'James Hendrick': [60], 'Angelica Kisel': [45, 25, 55.60]}
 donor = ''
 donation = 0
+tab = '    '
 
-def send_email(donor):
-    donation = sum(donors_list.get(donor))
-    print(f'Hello, {donor}! Thank you very much for your generous donation of ${donation}! Your contribution is essential and will be well utilized.')
-
+#this function gets a donor's name and donation amount and adds them both to the donor list.
 def send_thank_you():
-    print('thank you')
     while True:
         print('')
         donor = input('Type a Full Name: ')
@@ -23,47 +22,51 @@ def send_thank_you():
                 donors_list[donor] += [donation]
             else:
                 donors_list[donor] = [donation]
-            print(donors_list)
             print('')
-            print(f'Hello, {donor}! Thank you very much for your generous donation of ${donation}! Your contribution is essential and will be well utilized.')
+            print(f'Hello, {donor}! Thank you very much for your generous donation of ${donation:.2f}! Your contribution is essential and will be well utilized.')
             
-            
+#this function prints out a list of the donors in the donor list.            
 def print_list():
     print('')
     for i in donors_list:
         print(i)
     return True
-    
+
+#this function prints out a report on the prompt screen.    
 def create_report():
-    print('created report')
     print('')
     y = '|'
     print(f'Donor Name{y:>14} Total Given {y} Num Gifts {y} Average Gift')
     print('-' * 63)
-    
-    
-    donation = sum(donors_list.get(donor))
+    temp_set = donors_list.copy()
+    for i, val in donors_list.items():
+        temp_set[i] = sum(val)        
+    temp_set = dict(sorted(temp_set.items(), key=lambda kv: kv[1], reverse=True))
+    for i, val in temp_set.items():
+        gift = len(donors_list[i])
+        average = ( val / gift)
+        print(f'{i:<23} $ {val:>11.2f} {gift:>11} {average:>11.2f}')
+    print('')    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+#this function quits the previous menu.    
 def quitting():
-    print('time to quit')
     return False
-    
+
+#this function creates files for all donors in the donor list.
 def letters_to_everyone():
-    print('create txt files with thank you letters in each one')
-    
+#    pth = pathlib.Path('./donations/')
+#    p = pth.absolute()
+#    print(p)
+    for i, val in donors_list.items():
+        outfile = open(f'{i}.txt', 'w')
+        donation = sum(val)
+        outfile.write(f'Dear {i}, \n\n{tab}Thank you very much for your most recent donation \
+of ${val[-1]:.2f}! \n\n{tab}You have now donated a total of ${donation:.2f}. \n\n{tab}Your support \
+is essential to our success and will be well utilized. \n\n{tab*2}Sincerely, \n{tab*3}-The Company')
+        outfile.close()
+
+#this function will continue the previous while loop and prevents errors if a user enters an unexpected input.
 def continue_func():
-    print('continuing the function')
     return True
     
 switch_func_dict = {'1':send_thank_you, '2':create_report, '3':letters_to_everyone, '4':quitting, 'quit':quitting, 'list':print_list}
@@ -71,6 +74,6 @@ switch_func_dict = {'1':send_thank_you, '2':create_report, '3':letters_to_everyo
 if __name__ == "__main__":
     while True:
         send_thank_you_set = set(donors_list)
-        a = input('1: Send a Thank You \n2: Create a Report \n3: Send Letters to Everyone \n4: Quit \n Choose an Option: ')
+        a = input('1: Send a Thank You \n2: Create a Report \n3: Send Letters to Everyone \n4: Quit \nChoose an Option: ')
         if switch_func_dict.get(a, continue_func)() == False:
             break
