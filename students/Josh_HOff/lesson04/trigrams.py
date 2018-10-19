85ee29184b778177d0f8b2c7fe0a87318e16a794
Josh Hoff <jhoff1985@hotmail.com>
2018-10-19 15:24:19 -0700

    working on trigrams, trying to get it to arrange words correctly

--- students/Josh_HOff/lesson04/trigrams.py
+++ students/Josh_HOff/lesson04/trigrams.py
@@ -1,7 +1,9 @@
 import pathlib
 import os
+import random
 newdict = {}
 words = ''
+b = ''
 #main function
 if __name__ == "__main__":
     with open('words.txt', "r") as word_list:
@@ -9,8 +11,31 @@ if __name__ == "__main__":
         print(words)
         for x in range(len(words) - 2):
             key = f'{words[x]} {words[x+1]}'
+#            print(key)
             if key in newdict:
                 newdict[key] += [words[x + 2]]
             else:
                 newdict[key] = [words[x + 2]]
         print(newdict)
+#        while True:        
+        a = random.choice(list(newdict.keys()))
+        while True:
+#            b = 
+#            if b not in newdict:
+#                break
+            print(a)
+#            print(newdict[a])
+#            print(len(newdict[a]))
+            a += f' {newdict[a][0]}'
+            print(a)
+            print(a.split(' '))
+            b = a.split(' ')
+            key2 = f'{b[-2]} {b[-1]}'
+            print(key2)
+            if key2 not in newdict:
+                print('WOW')
+                break
+            else:
+                print(newdict[key2])
+                a += f' {newdict[key2][0]}'
+#                print(a)
