4e2b5bae28fc20884d9ef60efd7e6cab89562ce8
Paul Jurek <pjurek3@gmail.com>
2018-10-20 10:28:53 -0700

    mailroom: setup mailroom module with initial donor list in dictionary and added if __name__ == '__main__' block.  Next I will fill in blank functions.

--- students/paul_jurek/lesson03/mailroom.py
+++ students/paul_jurek/lesson03/mailroom.py
@@ -1,2 +1,10 @@
 """runs the command line app for the mailroom as part of lesson 3"""
 
+# setup initial donor list
+donors = {'Bill Gates': [100000, 5, 3000000],
+          'Paul Allen': [10, 1000000],
+          'Warren Buffet': [300000000],
+          }
+
+if __name__ == '__main__':
+    pass
