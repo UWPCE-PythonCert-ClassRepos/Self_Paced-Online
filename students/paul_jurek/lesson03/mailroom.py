b771f132d792cd66b97e1b62f3cafa69076e38d0
Paul Jurek <pjurek3@gmail.com>
2018-10-20 10:42:20 -0700

    setup while loop to allow interaction with user.  Also added cleaning of input to make more robust. This allows front/back spaces to be removed and the inputs to be non-case sensitive

--- students/paul_jurek/lesson03/mailroom.py
+++ students/paul_jurek/lesson03/mailroom.py
@@ -7,8 +7,16 @@ donors = {'Bill Gates': [100000, 5, 3000000],
           }
 
 if __name__ == '__main__':
-    user_input = input("Options:\n"
-        '\tSend a Thank You\n'
-        '\tCreate a Report\n'
-        '\tquit\n'
-        'Please input option: ')
+    # initial placeholder for input
+    user_input = None
+
+    # run until user specifies to get out
+    while user_input != 'quit':
+        user_input = input('Options:\n'
+                           '\tSend a Thank You\n'
+                           '\tCreate a Report\n'
+                           '\tquit\n'
+                           'Please input option: ')
+
+        # cleans up user input to make more robust.
+        user_input = user_input.lower().strip()
