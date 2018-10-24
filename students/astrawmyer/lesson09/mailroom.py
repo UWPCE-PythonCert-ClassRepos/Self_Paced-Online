8b7321637a370d33ff9c3776e6978f5cfd33fa87
Adam Strawmyer <adam@strawmyer.com>
2018-10-23 20:32:35 -0500

    L09 completed classes

--- students/astrawmyer/lesson09/mailroom.py
+++ students/astrawmyer/lesson09/mailroom.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
 
 # new file for lesson 9 work to incorporate classes.
-""" 
-    need a donor class
-    includes info about donor
-    stuff to provide access too donot specific information
-"""
 
-""" 
-    class that handles the donor collection. subclass of ^?
-    methods to add donor
-    search for donor
-    sum stuff
-    generate report
-"""
 
 class Donor:
     def __init__(self, name, *donation):
@@ -40,13 +28,12 @@ class Donor:
 class Donors:
     def __init__(self):
         self.data = {}
-
     
 
     def new_donor(self,donor):
         #currently allows to add one individual donor with list of donations
         self.data[donor.name] = donor.donations
-        print(self.data)
+        #print(self.data)
 
 
     def display_list(self):
@@ -57,7 +44,7 @@ class Donors:
     def add_donation(self,name,amount):
         #add doination to existing donor within Donors
         self.data[name].append(amount)
-        print(self.data)
+        #print(self.data)
 
 
     def write_letter(self,name,amount):
@@ -82,25 +69,71 @@ class Donors:
         print('-'*67)
         for i in donors_report:
             print('{1:27}${0:11.2f}{2:12}  ${3:12.2f}'.format(*i))
+    
+
+    def letter_files(self):
+        for name in self.data:
+            donation = ddonors[name][0]
+            with open('{}.txt'.format(name), 'w') as f:
+                f.write(self.write_letter(name, donation))
+        print("Letter files created.")
+
+
+########################Classes above here
 
 
 
+def thank_you():
+    """Function to send a thank you letter."""
+    while True:
+        input_name = input("Enter full name: ")
+        if input_name in donor_set.data.keys():
+            while True:
+                try:
+                    donation = float(input("Enter donation amount:"))
+                except ValueError:
+                    print("Donation needs to be a number.")
+                else:
+                    break
+            donor_set.add_donation(input_name,donation)
+            #ddonors[input_name].append(donation)
+            print(donor_set.write_letter(input_name,donation))
+            break
+        elif input_name == 'list':
+            donor_set.display_list()
+            
+        else:
+            print("Adding {} to donor database".format(input_name))
+            while True:
+                try:
+                    donation = float(input("Enter donation amount:"))
+                except ValueError:
+                    print("Donation needs to be a number.")
+                else:
+                    break
+            new_guy = Donor(input_name,[donation])
+            donor_set.new_donor(new_guy)
+            print(donor_set.write_letter(input_name,donation))
+            #print(ddonors)
+            break
 
 
 
-ddonors = {"Manny Machado": [12.2,2.51,3.20],
-            "Adam Jones": [1024.14,22.21,323.45],
-            "Chris Davis": [3.2,5.55,4.20]}
-
-a = Donor('Adam', [1])
-b = Donor("Yasiel", [3])
-a.new_donation(8)
-print(a.donations)
-
-don = Donors()
-don.new_donor(a)
-don.new_donor(b)
-don.display_list()
-don.add_donation("Adam",55)
-print(don.write_letter("Adam",54))
-don.write_report()
+ 
+if __name__ == "__main__":
+    # Adding this to import preset data.
+    a = Donor("James Hinchcliffe", [12.2,2.51,3.20])
+    b = Donor("Robert Wickens", [1024.14,22.21,323.45])
+    c = Donor("Sam Schmidt", [3.2,5.55,4.20])
+    donor_set = Donors()
+    donor_set.new_donor(a)
+    donor_set.new_donor(b)
+    donor_set.new_donor(c)
+    main_switch_function = {"1": thank_you, "2": donor_set.write_report, "3": donor_set.letter_files, "4": exit}
+    while True:
+        print("What do you want to do?")
+        response = input("1. Send a Thank You, 2. Create a Report, 3. Send all letters, 4. Quit: ")
+        try:
+            main_switch_function.get(response)()
+        except TypeError:
+            print("Not a valid input.")
