50dfb4f69f52e568194509ca422dbb2125611cd3
Paul Jurek <pjurek3@gmail.com>
2018-10-24 11:54:22 -0700

    completed initial steps to copy files and advanced step to copy file.  In this iteration of copying file i used pathlib.rename.  This is wrong though as it actually moves the file.  I am going back to replace this section with the more advanced method for copying conents.

--- students/paul_jurek/lesson04/file_lab.py
+++ students/paul_jurek/lesson04/file_lab.py
@@ -11,6 +11,26 @@ for f in pth.iterdir():
     print(f)
 
 # Write a program which copies a file from a source, to a destination (without using shutil, or the OS copy command). 
+# we will copy lesson 3 file to lesson 4 for this
+original_file = pathlib.Path('../lesson03/list_lab.py')
+new_file = pathlib.Path('list_lab_copy.py')
+original_file.rename(new_file)
+
 # Advanced: make it work for any size file: i.e. don’t read the entire contents of the file into memory at once.
+def copy_file(old_file, new_file):
+    """does content copy of files from old file to new"""
+    
+    with open(old_file, 'rb') as infile, open(new_file, 'wb') as outfile:
+        while True:
+            line = infile.readline()
+            if not line:
+                break
+            outfile.write(line)
+
+# testing this script
+original_file = '../lesson03/slicing_lab.py'
+new_file = 'slicing_lab_copy2.py'
+copy_file(old_file=original_file, new_file=new_file)
+
 # This should work for any kind of file, so you need to open the files in binary mode: open(filename, 'rb') (or 'wb' for writing). Note that for binary files, you can’t use readline() – lines don’t have any meaning for binary files.
 # Test it with both text and binary files (maybe jpeg or something of your chosing).
