c5553f34e2e1d0c261a7155e6f9f32baad9d7fa6
Laura Denney <Laura.A.Denney@gmail.com>
2018-10-24 20:55:08 -0700

    Completed work on kata_fourteen.py

--- students/laura_denney/lesson04/kata_fourteen.py
+++ students/laura_denney/lesson04/kata_fourteen.py
@@ -9,7 +9,6 @@
 
 import random
 
-test = ["I", "wish", "I", "may", "I", "wish", "I", "might"]
 source = "sherlock_holmes.txt"
 
 def read_from_source(source= source):
@@ -17,21 +16,9 @@ def read_from_source(source= source):
         book = infile.read()
     return book
 
-
-    #book = book.replace("\n", " ")
-    #book = book.replace("\\", " ")
-    #book = book.replace("\\'", "'")
-    #book = book.replace("\\\'", "'")
-    #book = book.replace('"\\\'', '"\'')
-    #book = book.replace('\"\\\'', '"\'')
-    #words = book.split(" ")
-    #return words
-
 def format_book(book):
     book = book.replace("\n", " ")
     book = book.replace("\\", " ")
-    # book = book.replace("\'", "'")
-    # book = book.replace("\'", "'")
     words = book.split(" ")
     while "" in words:
         words.remove("")
@@ -40,43 +27,74 @@ def format_book(book):
             words[i] = words[i].replace(" ", "")
         while "\\" in words[i]:
             words[i] = words[i].replace("\\", "")
-        while "\'" in words[i]:
+        while "\\\'" in words[i]:
             words[i] = words[i].replace("\\\'", "'")
     return words
 
 
 
 def make_dictionary(lst):
-    dict = {}
+    dic = {}
     for i in range(len(lst)- 2):
         two_words = lst[i] + " " + lst[i + 1]
         value = lst[i+2]
-        if two_words not in dict:
-            dict[two_words] = [value,]
+        if two_words not in dic:
+            dic[two_words] = [value,]
         else:
-            dict[two_words].append(value)
-    return dict
+            dic[two_words].append(value)
+    return dic
+
+def make_a_sentence(dic):
+    sentence = []
+    while True:
+        if len(sentence) == 0:
+            two_words = random.choice(list(dic.keys()))
+            two_word_lst = two_words.split(" ")
+            if "." in two_words:
+                continue
+            else:
+                two_word_lst[0] = two_word_lst[0].capitalize()
+            word = random.choice(dic[two_words])
+            new_two_word = two_word_lst[1] + " " + word
+            if new_two_word in dic:
+                sentence.append(two_word_lst[0])
+                sentence.append(two_word_lst[1])
+                sentence.append(word)
+        else:
+            two_word_lst = sentence[-2:]
+            two_words = " ".join(two_word_lst)
+            word = random.choice(dic[two_words])
+            new_two_word = two_word_lst[1] + " " + word
+            if new_two_word in dic:
+                sentence.append(word)
+        if "." in sentence[-1]:
+            break
+    return " ".join(sentence)
+
+def make_a_paragraph(dic):
+    paragraph = []
+    while len(paragraph) < 5:
+        para = make_a_sentence(dic)
+        paragraph.append(para)
+    return " ".join(paragraph)
 
 
+def create_literature(dic, paras = 4):
+    entire_book = []
+    while len(entire_book) < paras:
+        para = make_a_paragraph(dic)
+        entire_book.append(para)
 
-def create_literature(dict):
-    lst = []
-    while len(lst) < 5:
-        two_words = random.choice(list(dict.keys()))
-        two_word_lst = two_words.split(" ")
-        print(two_word_lst)
-        word = random.choice(dict[two_words])
-        print(word)
-        new_two_word = two_word_lst[1] + " " + word
-        print(new_two_word)
-        if new_two_word in dict:
-            lst.append(two_word_lst[0])
-            lst.append(two_word_lst[1])
-            lst.append(word)
-        else: continue
-    return lst
+    return "\n\n".join(entire_book)
+
+
 
 if __name__ == "__main__":
     book = read_from_source()
     words = format_book(book)
-    print(words)
+    dic = make_dictionary(words)
+    paras = int(input("How many paragraphs would you like your 'book' to have?: "))
+    new_book = create_literature(dic, paras)
+    print("\nAnd thus verily is a new book as follows: ")
+    print()
+    print(new_book)
