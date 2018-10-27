93616474f20f82b5d3a4767d9de83648950ac577
april301975 <april301975@gmail.com>
2018-10-27 07:45:23 -0700

    fixed the order of the if/else to get correct fizzbuzz

--- students/Thai_H/lesson2/fizzbuzz.py
+++ students/Thai_H/lesson2/fizzbuzz.py
@@ -1,12 +1,9 @@
 for n in range(1, 101):
-    if n % 3 == 0:
+    if n % 3 == 0 and n % 5 == 0:
+        print('FizzBuzz')
+    elif n % 3 == 0:
         print('Fizz')
     elif n % 5 == 0:
         print('Buzz')
-    elif n % 3 == 0 and n % 5 == 0:
-        print('FizzBuzz')
     else:
         print(n)
-
-
-
