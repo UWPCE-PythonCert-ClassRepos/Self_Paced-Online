d7721af17add7ca25f6cc4ef316c15c02f4d367f
unknown <Administrator@CRIMELAB.redmond.corp.microsoft.com>
2018-10-30 18:12:46 -0700

    1 box called via a function with two parameters...

--- students/MichaelM/lesson2/Lesson2_Ex1.py
+++ students/MichaelM/lesson2/Lesson2_Ex1.py
@@ -1,18 +1,23 @@
 # cd C:\Users\Administrator\Dropbox\UW_Python\git\Self_Paced-Online\students\MichaelM\lesson2
+# git status
+# git add Lesson2_Ex1.py
+# git commit
+# goto https://github.com/geekwriter2/Self_Paced-Online/tree/master/students
+# click Pull request > new pull request
 # import name_main
 # run Lesson2_Ex1.py
 
-def horizontalLine():
-    print('+' + '-'*4 + '+' + '-'*4 + '+')
-def vertSpacer(cols):
-    print(('|' + '    ')*cols)
-    print(('|' + '    ')*cols)
-    print(('|' + '    ')*cols)
-    print(('|' + '    ')*cols)
+def horizontalLine(rows):
+    print('+' + '-' * rows + '+' + '-' * rows + '+')
+def vertSpacer(cols, rows):
+    print((('|' + (' ' * rows)) * cols + '\n') * rows, end='')
+def box(cols, rows):
+    horizontalLine(rows)
+    vertSpacer(cols, rows)
+    horizontalLine(rows)
+  
 
 if __name__ == "__main__":
-    horizontalLine()
-    vertSpacer(3)
-    horizontalLine()
-    vertSpacer(3)
-    horizontalLine()
+    box(3, 10)
+
+  
