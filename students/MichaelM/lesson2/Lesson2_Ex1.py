4990d3d1cb2412b28390cfa0054a4f9ca9796cf1
geekwriter2 <geekwriter@hotmail.com>
2018-10-31 19:16:58 -0700

    3rd commit for lesson 2. No grade needed yet, thx -

--- students/MichaelM/lesson2/Lesson2_Ex1.py
+++ students/MichaelM/lesson2/Lesson2_Ex1.py
@@ -1,23 +1,26 @@
-# cd C:\Users\Administrator\Dropbox\UW_Python\git\Self_Paced-Online\students\MichaelM\lesson2
+# cd C:\Users\v-micmcd.Redmond\Dropbox\UW_Python\git\Self_Paced-Online\students\MichaelM\lesson2
 # git status
 # git add Lesson2_Ex1.py
 # git commit
+# git push
 # goto https://github.com/geekwriter2/Self_Paced-Online/tree/master/students
 # click Pull request > new pull request
 # import name_main
 # run Lesson2_Ex1.py
 
-def horizontalLine(rows):
-    print('+' + '-' * rows + '+' + '-' * rows + '+')
-def vertSpacer(cols, rows):
-    print((('|' + (' ' * rows)) * cols + '\n') * rows, end='')
-def box(cols, rows):
-    horizontalLine(rows)
-    vertSpacer(cols, rows)
-    horizontalLine(rows)
+def horizontalLine(boxCnt):
+    print((('+' + '- - - - -') * boxCnt ), end='+' + '\n')
+def vertLine(boxCnt):
+    cols = boxCnt + 1
+    print(((('|' + '         ') * cols)+ '\n') * 4, end='') 
+    
+def box(boxCnt):
+    for _ in range(boxCnt):
+        horizontalLine(boxCnt)
+        vertLine(boxCnt)
+    horizontalLine(boxCnt)
   
 
 if __name__ == "__main__":
-    box(3, 10)
-
-  
+    box(10)
+    print("done...")
