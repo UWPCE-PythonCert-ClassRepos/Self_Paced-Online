401b6f6ffdd6794ff32c1c1eecf91d345146e95f
lauraannf <lauraanng@gmail.com>
2018-11-02 15:59:24 -0400

    'lesson6

--- students/lauraannf/Lesson06/test_mailroom_4.py
+++ students/lauraannf/Lesson06/test_mailroom_4.py
@@ -14,3 +14,7 @@ def test_add_donation():
     assert donor_list['Frank Reynolds'] == [10, 20, 50, 1000]
 
 
+def test_create_folder():
+    now = datetime.datetime.now()
+    new_path = os.getcwd() + '\\' + 'letters_' + now.strftime('%d%m%Y')
+    assert os.path.exists(new_path) is True
