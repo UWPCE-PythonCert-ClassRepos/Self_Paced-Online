ab020e246885f8ba38e2f9cd8edc0b0873a59855
Paul Jurek <pjurek3@gmail.com>
2018-11-01 20:05:32 -0700

    refactored to have seperate function to split string
    * this allows easy modification in future to change what splits are (eg. punctuations
    * added tests for both trigram generator and split string

--- students/paul_jurek/lesson04/trigram.py
+++ students/paul_jurek/lesson04/trigram.py
@@ -1,8 +1,39 @@
 """implementin trigram problem in python"""
 
 
-def build_trigram_dict(raw_text):
-    pass
+def split_text(raw_text: str):
+    """splits text to prepare for building trigram
+
+    Takes in raw text string and splits into list in order.
+    args:
+        raw_text: string to be parsed
+
+    returns
+        list of word"""
+    return raw_text.split()
+
+
+def build_trigram_dict(split_text: list, output: dict={}):
+    """using recursive function to build trigram dict
+    output will utilize sets to capture results as values
+
+    args:
+        split_text: list of strings in order from original text
+        output: dict of trigrams
+    returns:
+        updated dict of trigrams"""
+
+    # mean we ran into end of string and cannot build trigram
+    if len(split_text) < 3:
+        return output
+    else:
+        new_key = " ".join([split_text[0], split_text[1]])
+        if output.get(new_key):
+            output.get(new_key).add(split_text[2])
+        else:
+            output[new_key] = set([split_text[2]])
+        return build_trigram_dict(split_text[1:], output)
+
 
 def test_building_dict():
         """given the example text tests building the dict"""
@@ -12,6 +43,15 @@ def test_building_dict():
                            "may I": {"wish"},
                            "I may": {"I"},
                            }
+        test_split_text = split_text(example_text)
 
-        assert build_trigram_dict(example_text) == expected_result
+        assert build_trigram_dict(test_split_text) == expected_result
 
+
+def test_split_text():
+    """given a raw text string,
+    when function to split is run
+    correct split text is returned"""
+    example_text = 'I wish I may I wish I might'
+
+    assert split_text(example_text) == ['I', "wish", "I", "may", "I", "wish", "I", "might"]
