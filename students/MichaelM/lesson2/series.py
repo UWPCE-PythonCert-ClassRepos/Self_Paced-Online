e2baedd1d030c5e838623d31f658cc45fade2e7d
Michael McDonald (Fiveby Solutions Inc) <v-micmcd@microsoft.com>
2018-11-06 10:31:16 -0800

    see q on Lesson2_Ex3_Series.py

--- students/MichaelM/lesson2/series.py
+++ students/MichaelM/lesson2/series.py
@@ -1,64 +1,58 @@
-def fibonacci(n):
-    """ 
-    Returns a fibonacci value given a specific term.
-
-    {Extended description} 
-
-    Parameters: 
-    n (int): The fibonacci sequence term  
-
-    Returns: 
-    int: fibonacci value for the term provided
+def sum_series(n, n_minus2=0, n_minus1=1):
     """
+    Returns a value given a specific term. Either the Fibonacci, Lucas or any other value defined in the term provided.
+
+    {Extended description}
+
+    Parameters:
+    n (int): The sequence term
+    n_minus2 (int) (opt): The starting point of the sequence
+    n_minus1 (int) (opt): The next integer in the sequence
+
+    Returns:
+    int: Either the Fibonacci, Lucas or any other value defined in the term provided.
+
+    """
+
     loop_counter = 1
-    result = 0
-    n_minus2 = 0
-    n_minus1 = 1
-    for i in range(1, n):
-        i2 = i+1
-        # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 //first 16
-        # cheat for the first iteration...
-        if loop_counter == 1:
-            result = 0
-        # add the last two results
-        result = n_minus2 + n_minus1
-        # print("Fibonacci iteration: " + str(loop_counter) + ", result: " + str(result) + ",n-2=" + str(n_minus2) + ", n-1=" + str(n_minus1))
-        # swap out new values for next loop
+    # 0, 1 Fib
+    # 2, 1 Luc
+    # 1, 1 undefined
+    term_values = [n_minus2, n_minus1]
+    for i in range(1, n - 1):
+        new_value = term_values[loop_counter] + term_values[loop_counter - 1]
+        term_values.append(new_value)
         loop_counter += 1
-        n_minus1 = n_minus2
-        n_minus2 = result
+    result = max(term_values)
     return result
 
 
-def lucas(n):
-    """ 
-    Returns a lucas value given a specific term.
-
-    {Extended description} 
-
-    Parameters: 
-    n (int): The lucas sequence term  
-
-    Returns: 
-    int: lucas value for the term provided
+def assert_correct_input(n, n_minus2, n_minus1):
     """
-    loop_counter = 1
-    result = 0
-    n_minus2 = 2
-    n_minus1 = 0
-    for i in range(0, n):
-        # 2, 1, 3, 4, 7, 11, 18, 29 //first 8
-        # cheat for the first 2 iterations...
-        if loop_counter == 1:
-            result = 2
-        if loop_counter == 2:
-            result = 1
-        # add the last two results for every other lucas number
-        else:
-            result = n_minus2 + n_minus1
-            # print("Loop iteration: " + str(loop_counter) + ", result: " + str(result) + ",n-2=" + str(n_minus2) + ", n-1=" + str(n_minus1))
-        # swap out new values for next loop
-        loop_counter += 1
-        n_minus1 = n_minus2
-        n_minus2 = result
+    Check inputs for whole numbers.
+
+    sum_series requires whole numbers to correctly compute Fibonacci and Lucas series'
+
+    Parameters:
+    n: The sequence term
+    n_minus2: The starting point of the sequence
+    n_minus1: The next integer in the sequence
+
+    Returns:
+    string: Results as to whether the inputs all passed or all failed.
+
+    """
+    try:
+        assert (n % 1) == 0
+        assert (n_minus2 % 1) == 0
+        assert (n_minus1 % 1) == 0
+    except AssertionError:
+        result = "int_assertion_failed"
+    else:
+        result = "ok"
     return result
+
+# 1  2  3  4  5   6   7   8,  9 // term
+# 0, 1, 1, 2, 3,  5,  8, 13, 21 // fibonacci
+# 2, 1, 3, 4, 7, 11, 18, 29, 47 // lucas
+# 1, 1, 2, 3, 5,  8, 13, 21, 34 // undefined
