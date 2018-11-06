50a659b036bfe0ba7f38091df23fb88bbf45fda2
Michael McDonald (Fiveby Solutions Inc) <v-micmcd@microsoft.com>
2018-11-06 10:28:12 -0800

    for this assignment ok to remove the Fibonacci and Lucas functions althogether in favor of generalizing the entire program in sum_series.py?

--- students/MichaelM/lesson2/Lesson2_Ex3_Series.py
+++ students/MichaelM/lesson2/Lesson2_Ex3_Series.py
@@ -4,6 +4,7 @@
 # run Lesson2_Ex3_Series.py
 # git status
 # git add Lesson2_Ex3_Series.py
+# git add series.py
 # git commit
 # git push
 # goto https://github.com/geekwriter2/Self_Paced-Online/tree/master/students/MichaelM/lesson2/
@@ -17,12 +18,24 @@ importlib.reload(series)
 
 if __name__ == "__main__":
     n = randint(0, 9)
-    fib_calc = series.fibonacci(n)
-    lucas_calc = series.lucas(n)
-    checkwork_fib = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610"
-    checkwork_Luc = "2, 1, 3, 4, 7, 11, 18, 29 //first 8"
-    result = "The fibonacci and lucas sequence term requested is {:d}. The Fibonacci value is: {:d}. The Lucas value is: {:d}".format(
-        n, fib_calc, lucas_calc)
-    print(result)
-    print(checkwork_fib)
-    print(checkwork_Luc)
+    n_minus2 = randint(0, 2)
+    n_minus1 = 1
+    # assertions test
+    # n_minus2 = 1.5
+    if n_minus2 == 0:
+        calc_type = "Fibonacci"
+    elif n_minus2 == 2:
+        calc_type = "Lucas"
+    else:
+        calc_type = "currently undefined"
+    check_inputs = series.assert_correct_input(n, n_minus2, n_minus1)
+    if check_inputs != "ok":
+        error_statement = "Incorrect input. Did you enter whole numbers only? ({:s}).".format(check_inputs)
+        print(error_statement)
+    else:
+        series.sum_series(n, n_minus2=0, n_minus1=1)
+        series_value = series.sum_series(n, n_minus2, 1)
+        result = "The {:s} sequence term requested is {:d}. The value is: {}.".format(calc_type, n, series_value)
+        print(result)
+
+
