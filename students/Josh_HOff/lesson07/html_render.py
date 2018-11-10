15cf153c344e08ac54664e72f4ef89d30c3c3aa4
Josh Hoff <jhoff1985@hotmail.com>
2018-11-09 16:40:22 -0800

    halfway through lesson7, currently on step 4

--- students/Josh_HOff/lesson07/html_render.py
+++ students/Josh_HOff/lesson07/html_render.py
@@ -8,11 +8,54 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
-    def __init__(self, content=None):
-        pass
+    tag = 'html'
+
+    def __init__(self, content=None, **kwargs):
+        if content is None:
+            self.contents = []
+        else:
+            self.contents = [content]
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write(f'<{self.tag}>\n')
+        for content in self.contents:
+#            out_file.write(content)
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write('\n')
+        out_file.write(f'</{self.tag}>\n')
+        
+        
+class Body(Element):
+    tag = 'body'
+    
+    
+class Html(Element):
+    tag = 'html'
+    
+    
+class P(Element):
+    tag = 'p'
+    
+
+class Head(Element):
+    tag = 'head'
+    
+    
+class OneLineTag(Element):
+    def render(self, out_file):
+        out_file.write(f'<{self.tag}>')        
+        out_file.write(self.contents[0])
+        out_file.write(f'</{self.tag}>\n')
+    
+    def append(self, content):
+        raise NotImplementedError
+    
+    
+class Title(OneLineTag):
+    tag = 'title'
