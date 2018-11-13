fb62f807889cdf89ae89dfa07213277fa370e116
Laura Denney <Laura.A.Denney@gmail.com>
2018-11-12 21:36:02 -0800

    finished work on mailroom part 4

--- students/laura_denney/lesson06/mailroom_part4.py
+++ students/laura_denney/lesson06/mailroom_part4.py
@@ -11,8 +11,8 @@
 #   Laura Denney, 11/7/18, Started MailRoom Part 4
 #-------------------------------------------------#
 
-donor_list = {"john doe":[100, 200],
-        "laura denney":[5],
+donor_list = {"john doe":[100.50, 200.00],
+        "laura denney":[5.00],
         "bill gates":[4000],
         "samuel jackson":[x for x in range(1,4)],
         "mr. bean": [500, 100]}
@@ -39,22 +39,21 @@ def quit():
 def send_letters():
     print("\nYou have chosen to send letters to everyone.")
     total_donor = sort_by_amount()
+    total_letters_sent = 0
     for each_donor in total_donor:
         amount = each_donor[0]
         donor = each_donor[1].title()
         #Name formatting for one word names like 'Cher' vs normal full names
-
         first_last = donor.split(" ")
-
         if len(first_last) == 1:
             first = first_last[0]
             last = ""
         else:
             first = first_last[0]
             last = first_last[1]
-        send_email(donor, amount, "{}_{}.txt".format(first, last))
-    print("\nLetters have been successfully sent to every donor.")
-    print("A copy of that letter is now saved in your current directory.")
+        total_letters_sent += send_email(donor, amount, "{}_{}.txt".format(first, last))
+    print(f"\nA total of {total_letters_sent} letters have been successfully sent to our donors.")
+    print("A copy of those letters are now saved in your current directory.")
 
 #creates a table of donors sorted by total amount donated
 def sort_by_amount():
@@ -73,26 +72,43 @@ def send_thank_you():
 
 #adding list comprehension here
 def print_list():
+    # names = [name.title() for name in donor_list.keys()]
+    # strformat = "\nWe have {} current donors: " + ", ".join(["{}"] * len(names))
+    # print(strformat.format(len(names), *names))
+    print(get_list())
+
+def get_list():
     names = [name.title() for name in donor_list.keys()]
     strformat = "\nWe have {} current donors: " + ", ".join(["{}"] * len(names))
-    print(strformat.format(len(names), *names))
+    return strformat.format(len(names), *names)
 
-def check_list():
+def thank_a_donor():
     name = input("\nPlease enter the full name of the donor you would like to thank: ").lower()
     donation = input("How much money did they donate? (type 100 for $100) >> ")
+    donation = validate_donation(donation)
+    print(check_current_donor(name, donation))
+    print("The email you are sending is as follows:")
+    print(send_email(name.title(), donation))
+    print("You have successfully sent a Thank You to {}.".format(name.title()))
+
+def check_current_donor(name, donation):
+    if name in donor_list:
+        donor_list[name].append(donation)
+        return "\n{} is a current donor, we will update their donations.".format(name.title())
+
+    else:
+        donor_list[name] = [donation]
+        return "\n{} is not a current donor, we will add them to our system.".format(name.title())
+
+
+def validate_donation(donation):
     #validate donation amount
     try:
         num = float(donation)
     except ValueError:
         print("\nERROR: Invalid donation amount entered, marking donation as $0.")
         num = 0.0
-    if name in donor_list:
-        print("\n{} is a current donor, we will update their donations.".format(name.title()))
-        donor_list[name].append(num)
-    else:
-        print("\n{} is not a current donor, we will add them to our system.".format(name.title()))
-        donor_list[name] = [num]
-    send_email(name.title(), num)
+    return num
 
 #modified to either print to screen or disk
 def send_email(donor, amount, dest = 0):
@@ -106,26 +122,30 @@ def send_email(donor, amount, dest = 0):
     <3 The MailRoom
     '''
     if not dest:
-        print("The email you are sending is as follows:")
-        print(fstring)
-        print("You have successfully sent a Thank You to {}.".format(donor))
+
+        return fstring
+
     else:
         with open(dest, 'w') as outfile:
             outfile.write(fstring)
+        return 1
 
 def create_report():
     report_list = sort_by_amount()
     print("\nYou have chosen to Create a Report.")
-    header = '''
+    print(create_string_report(report_list))
+
+def create_string_report(report_list = []):
+    report = '''
 Donor Name                | Total Given | Num Gifts | Average Gift
 ------------------------------------------------------------------'''
-    print(header)
-    strformat = '{:<26}${:>13}{:^12}${:>13}'
+    strformat = '\n{:<26}${:>13.2f}{:^12}${:>13.2f}'
     for donor in report_list:
-        total_donations = format(donor[0], '.2f')
+        total_donations = donor[0]
         number_donations = len(donor_list[donor[1]])
-        average = format(total_donations/number_donations, '.2f')
-        print(strformat.format(donor[1].title(),total_donations, number_donations, average))
+        average = total_donations/number_donations
+        report +=  strformat.format(donor[1].title(),total_donations, number_donations, average)
+    return report
 
 #Main Menu options for user
 main_choice_dict = {
@@ -138,7 +158,7 @@ main_choice_dict = {
 #Sub Menu options for user
 sub_choice_dict = {
     "1": print_list,
-    "2": check_list,
+    "2": thank_a_donor,
     "3": quit
 }
 
--- students/laura_denney/lesson06/test_mailroom_part4.py
+++ students/laura_denney/lesson06/test_mailroom_part4.py
@@ -4,13 +4,52 @@
 # Date:  November 7, 2018
 # ChangeLog: (Who, When, What)
 #   Laura Denney, 11/7/18, Started MailRoom Part 4
+#   Laura Denney, 11/12/18, Completed work on MailRoom Part 4
 #-------------------------------------------------#
 
 from mailroom_part4 import *
 
+#tests sort_by_amount
 def test_1():
     assert len(sort_by_amount()) >= 5
     assert type(sort_by_amount()) is list
 
+#tests the quit function
 def test_2():
     assert quit() == 'quit'
+
+def test_3():
+    assert get_list() == '\nWe have 5 current donors: John Doe, Laura Denney, Bill Gates, Samuel Jackson, Mr. Bean'
+
+#tests the validation of donation amounts
+def test_4():
+    assert validate_donation(400) == 400.0
+
+def test_4_5():
+    assert validate_donation('four hundred') == 0.0
+
+#tests the function to create a string for report
+def test_7():
+    assert create_string_report() == '\nDonor Name                | Total Given | Num Gifts | Average Gift\n---------------------------------------------------\
+---------------'
+
+def test_7_5():
+    assert create_string_report(sort_by_amount()) > create_string_report()
+
+
+#tests checking if entered donor is current donor or not
+def test_5():
+    assert check_current_donor("laura denney", 5.0) == '\nLaura Denney is a current donor, we will update their donations.'
+
+def test_6():
+    assert check_current_donor("chuck norris", 500.00) == '\nChuck Norris is not a current donor, we will add them to our system.'
+
+
+
+#tests whether to print to screen or save in txt file
+def test_8():
+    assert send_email('Laura Denney', 500) == '\n    Dear Laura Denney,\n\n    We would like to thank you for your generous donation\n    of $500.00\
+. It will be put to great use!\n\n    Thank you!\n    <3 The MailRoom\n    '
+
+def test_9():
+    assert send_email('Laura Denney', 500, "home.txt") == 1
