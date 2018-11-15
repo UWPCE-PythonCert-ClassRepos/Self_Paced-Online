d23872a3ef8444d7d186279108e6876554f93ddd
lauraannf <lauraanng@gmail.com>
2018-11-15 18:06:48 -0500

    lesson07

--- students/lauraannf/Lesson07/html_render.py
+++ students/lauraannf/Lesson07/html_render.py
@@ -7,20 +7,19 @@ Created on Sat Nov 10 17:01:24 2018
 
 
 class Element():
-    tag = 'html'
-    indentation = '     '
-
-    def __init__(self, content=None):
+    def __init__(self, tag='html', indentation='     ', content=None):
+        self.tag = tag
+        self.indentation = indentation
         if content is None:
             self.content = []
         else:
             self.content = [content]
 
-    def append_element(self, content):
-        self.content.append_element(content)
+    def append(self, new_content):
+        self.content.append(new_content)
 
-    def render_element(self, file_out, cur_ind=''):
+    def render(self, file_out, cur_ind=''):
         file_out.write('{}<{}>\n'.format(cur_ind, self.tag))
         for item in self.content:
-            file_out.write(self.content)
-        file_out.write('{}/<{}>'.format(cur_ind, self.tag))
+            file_out.write(item)
+        file_out.write('\n{}</{}>'.format(cur_ind, self.tag))
