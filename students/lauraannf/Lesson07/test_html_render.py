4b7da08b0ad3b0c81de944516719039605bb4760
lauraannf <lauraanng@gmail.com>
2018-11-15 18:07:16 -0500

    lesson07

--- students/lauraannf/Lesson07/test_html_render.py
+++ students/lauraannf/Lesson07/test_html_render.py
@@ -7,9 +7,23 @@ Created on Sat Nov 10 17:02:49 2018
 
 
 import html_render
+from io import StringIO
 
-class Element():
-    def test_append(self):
-        test1 = html_render.Element('test1')
-        self.append_element(test1, 'test2')
-        assert test2 == ['test1' 'test2']
+
+def test_element():
+    tag_test = html_render.Element()
+    assert tag_test.tag == 'html'
+    assert tag_test.indentation == '     '
+
+
+def test_append():
+    tag_test = html_render.Element(content='test1')
+    tag_test.append('test2')
+    assert tag_test.content == ['test1', 'test2']
+
+
+def test_render():
+    render_test = html_render.Element(content='test1')
+    f = StringIO()
+    render_test.render(f)
+    assert f.getvalue() == '<html>\ntest1\n</html>'
