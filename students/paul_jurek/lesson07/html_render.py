14fbcdc8d7f41fc1ee4cecd9238bfa3ee345a7a3
Paul Jurek <pjurek3@gmail.com>
2018-11-19 06:18:01 -0800

    updated render function to be recursive call Due to interface on test_html_render which doesn't allow content list to be passed back, I created helper static function which does recurssive processing of content list

--- students/paul_jurek/lesson07/html_render.py
+++ students/paul_jurek/lesson07/html_render.py
@@ -3,21 +3,32 @@
 class Element():
     """creates HTML element and context"""
     tag = ''
-    _start_tag = f'<{tag}>'
-    _end_tag = f'</{tag}>'
 
     def __init__(self, content=None):
         self.content = []
         if content:
             self.content.append(content)
+        self._start_tag = f'<{self.tag}>'
+        self._end_tag = f'</{self.tag}>'
 
     def append(self, content):
         self.content.append(content)
 
     def render(self, file_out, cur_ind = ""):
-        for entry in self.content:
-            file_out.write(entry + '\n')
-        #file_out.writelines(self.content)
+        """initiaties the processing of element tree"""
+        Element.process_content(self.content, file_out=file_out, cur_ind=cur_ind)
+
+
+    @staticmethod
+    def process_content(content, file_out, cur_ind=""):
+        """helper function for render() which allows us to do
+        recursive function on content list"""
+        for entry in content:
+            if issubclass(type(entry), Element):
+                Element.process_content(entry.content, file_out=file_out)
+            else:
+                file_out.write(entry + '\n')
+
 
 class Html(Element):
     """html element tag"""
