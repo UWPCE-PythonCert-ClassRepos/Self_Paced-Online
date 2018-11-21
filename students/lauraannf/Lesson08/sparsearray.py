7c183941d27b0d29bff1eb03163e0f3279b46563
lauraannf <lauraanng@gmail.com>
2018-11-21 16:10:41 -0500

    '8'

---
+++ students/lauraannf/Lesson08/sparsearray.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 16:00:24 2018
+
+@author: Laura.Fiorentino
+"""
+
+
+class SparseArray:
+    def __init__(self, array):
+        
+ for item, it in zip(A, range(len(A))):
+   if item != 0:
+     B[it]=item
