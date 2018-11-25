587470fc9a119d29204ff6bf08cb9750fc8bbf5f
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 15:31:19 -0800

    noticed that output was not correct, was printing fizzbuzz but then going to bottom else and printing the number anyways. reworked test to elif instead of if. Also realized fizzbuzz test is not requiring both true. Researching operands as what i have is incorrect.

--- students/jstrauss123/lesson02/fizzbuzz.py
+++ students/jstrauss123/lesson02/fizzbuzz.py
@@ -3,17 +3,17 @@
 # iterate from 1 - 100
 for x in range(1, 51):
 	# test if value is divisible by 3 and 5 with no remainder
-	if x % 3 == 0 or x % 5 == 0:
+	if x % 3 == 0 AND x % 5 == 0:
 		print("fizzbuzz")
 	# else if value is divisible by 3
-	if x % 3 == "0":
+	elif x % 3 == "0":
 		print("fizz")
 	# else if value is divisible by 5
-	if x % 5 == "0":
+	elif x % 5 == "0":
 		print("buzz")
 	# else print value
 	else:
-		print("falling through", x)
+		print(x)
 # end
 
 	
