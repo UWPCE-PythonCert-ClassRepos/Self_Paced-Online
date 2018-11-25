52cb27bced1a5ce134ad1d0fbebc93bbf9da9591
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 15:11:57 -0800

    testing iteration, need to add one to ensure last number is listed, modified psuedo code for first test from or to and

--- students/jstrauss123/lesson02/fizzbuzz.py
+++ students/jstrauss123/lesson02/fizzbuzz.py
@@ -1,15 +1,15 @@
 # fizzbuzz - loop through 1 to 100, if # dvisible by 3 print fizz, if divisible by 5 print buzz if both, print fizzbuzz
 
 # iterate from 1 - 100
-
-	# test if value is divisible by 3 or 5
+for x in range(1, 21):
+	# test if value is divisible by 3 and 5
 	
 	# else if value is divisible by 3
 	
 	# else if value is divisible by 5
 	
 	# else print value
-
+	print(x)
 # end
 
 	
