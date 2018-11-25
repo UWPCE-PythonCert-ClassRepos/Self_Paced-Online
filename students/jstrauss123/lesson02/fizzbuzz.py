33f7108ba78923f96c894231bbcf41d8a8c13c20
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 15:20:36 -0800

    math statement incorrect... doing wrong thing. researching / thinking...

--- students/jstrauss123/lesson02/fizzbuzz.py
+++ students/jstrauss123/lesson02/fizzbuzz.py
@@ -1,9 +1,9 @@
 # fizzbuzz - loop through 1 to 100, if # dvisible by 3 print fizz, if divisible by 5 print buzz if both, print fizzbuzz
 
 # iterate from 1 - 100
-for x in range(1, 21):
-	# test if value is divisible by 3 and 5
-	if x == "3" OR x == "5":
+for x in range(1, 51):
+	# test if value is divisible by 3 and 5 with no remainder
+	if x / 3 == "1" or x / 5 == "1":
 		print("fizz")
 	# else if value is divisible by 3
 	
