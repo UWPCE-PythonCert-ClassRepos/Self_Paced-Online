16f16616852f4b740c1097ff541f08d5caa72be2
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 15:09:24 -0800

    Created psuedo code outline, begin working on code

---
+++ students/jstrauss123/lesson02/fizzbuzz.py
@@ -0,0 +1,15 @@
+# fizzbuzz - loop through 1 to 100, if # dvisible by 3 print fizz, if divisible by 5 print buzz if both, print fizzbuzz
+
+# iterate from 1 - 100
+
+	# test if value is divisible by 3 or 5
+	
+	# else if value is divisible by 3
+	
+	# else if value is divisible by 5
+	
+	# else print value
+
+# end
+
+	
