769a632d7e4f9ef60f5977e623056e5d964fb8eb
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 17:17:57 -0800

    the sum_series func is basically a copy / paste of the other two with some minor tweaks. Also modified the loop to run one greater than the input number. Looking into asserts now.

--- students/jstrauss123/lesson02/series.py
+++ students/jstrauss123/lesson02/series.py
@@ -5,7 +5,8 @@ def fibonacci(n):
 	fibvaln1 = 1
 	fibvaln2 = 0
 	nextval = fibvaln1 + fibvaln2
-	while count < 50:
+	# loop through series stopping at input + 1
+	while count < n + 1:
 		if count == n:
 			print("The ", n," fibonacci series value is: ", nextval)
 		#print("fibvaln1 = ", fibvaln1, "fibvaln2 = ", fibvaln2, "nextval = ", nextval)
@@ -20,19 +21,35 @@ def lucas(n):
 	lucvaln1 = 1
 	lucvaln2 = 2
 	nextval = lucvaln1 + lucvaln2
-	while count < 50:
+	# loop through series stopping at input + 1
+	while count < n + 1:
 		if count == n:
 			print("The ", n," lucas series value is: ", nextval)
-		#print("lucvaln1 = ", lucvaln1, "fibvaln2 = ", lucvaln2, "nextval = ", nextval)
 		# add lucvaln1 and lucvaln2 for next number in series 
 		lucvaln2 = lucvaln1
 		lucvaln1 = nextval
 		nextval = lucvaln1 + lucvaln2
 		count += 1
 
-#def sum_series(x, y, z):
-
+def sum_series(n, y=0, z=1):
+	count = 3
+	valn1 = z
+	valn2 = y
+	nextval = valn1 + valn2
+	# loop through series stopping at input + 1
+	while count < n + 1:
+		if count == n:
+			print("The ", n," series value is: ", nextval)
+		# add valn1 and valn2 for next number in series 
+		valn2 = valn1
+		valn1 = nextval
+		nextval = valn1 + valn2
+		count += 1
+	# add assert tests.. ?
 
 fibonacci(5)
 lucas(5)
+sum_series(5)
+sum_series(5, 5, 10)
+
 
