3da1b9aca6d36596f1a99c72157cc2eda77d300f
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 17:07:07 -0800

    lucas func: cleaned it up with same changes as fibonacci func noted that sequence was not right, realized my two values were reversed. also counter needs to start at 3 not 1. now working properly returning appropriate "nth" value fibonacci func: modified count to start at 3 and changed the value assignment order now returning appropriate nth value in series

--- students/jstrauss123/lesson02/series.py
+++ students/jstrauss123/lesson02/series.py
@@ -1,9 +1,9 @@
 # series function - compute fibonacci and lucas series
 
 def fibonacci(n):
-	count = 2
-	fibvaln1 = 0
-	fibvaln2 = 1
+	count = 3
+	fibvaln1 = 1
+	fibvaln2 = 0
 	nextval = fibvaln1 + fibvaln2
 	while count < 50:
 		if count == n:
@@ -16,16 +16,14 @@ def fibonacci(n):
 		count += 1
 	
 def lucas(n):
-	count = 0
-	l0 = 2
-	l1 = 1
-	lucvaln1 = l0 + l1
-	lucvaln2 = l1
+	count = 3
+	lucvaln1 = 1
+	lucvaln2 = 2
 	nextval = lucvaln1 + lucvaln2
 	while count < 50:
 		if count == n:
 			print("The ", n," lucas series value is: ", nextval)
-		print("lucvaln1 = ", lucvaln1, "fibvaln2 = ", lucvaln2, "nextval = ", nextval)
+		#print("lucvaln1 = ", lucvaln1, "fibvaln2 = ", lucvaln2, "nextval = ", nextval)
 		# add lucvaln1 and lucvaln2 for next number in series 
 		lucvaln2 = lucvaln1
 		lucvaln1 = nextval
@@ -35,6 +33,6 @@ def lucas(n):
 #def sum_series(x, y, z):
 
 
-fibonacci(6)
-#lucas(5)
+fibonacci(5)
+lucas(5)
 
