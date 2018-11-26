874aaa0cc77b9cb20728ef9a5ee1ce4b276b6e7d
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 16:34:31 -0800

    working on proper calculation for fibonacci series and will then add loop to go through small number to see if I can get this working properly Moved code to the proper loop

--- students/jstrauss123/lesson02/series.py
+++ students/jstrauss123/lesson02/series.py
@@ -1,13 +1,20 @@
 # series function - compute fibonacci and lucas series
 
 def fibonacci(n):
-	
-	
-#def lucas(n):
+	count = 0
 	f0 = 0
 	f1 = 1
-	fibval = f0 + f1
-	print(fibval)
+	fibvaln1 = f0 + f1
+	fibvaln2 = f1
+	nextval = fibvaln1 + fibvaln2
+	print("fibvaln1 = ", fibvaln1, "fibvaln2 = ", fibvaln2, "nextval = ", nextval)
+	#while count < 1000:
+	#	fibval = f0 + f1
+	#	nextval = fibval +
+	#	print(fibval)
+	
+#def lucas(n):
+	
 
 #def sum_series(x, y, z):
 
