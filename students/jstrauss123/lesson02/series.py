4ce9054d73ef246cf0797289b096fb7bea50db90
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 16:59:44 -0800

    lucas func: basically a copy/paste of fibonacci func with some name changes. however... fibonacci func: while copy / paste I realized some changes I needed to make. Removed the initial value set (f1 & f2) as there is no need for them. outside of loop, did the first calculation of the series then moved into loop. Maxing at 50. Counter set to 2 to take into account the first two numbers in the series. Now back to lucas loop and clean it up as well.

--- students/jstrauss123/lesson02/series.py
+++ students/jstrauss123/lesson02/series.py
@@ -1,31 +1,40 @@
 # series function - compute fibonacci and lucas series
 
 def fibonacci(n):
-	count = 0
-	f0 = 0
-	f1 = 1
-	fibvaln1 = f0 + f1
-	fibvaln2 = f1
+	count = 2
+	fibvaln1 = 0
+	fibvaln2 = 1
 	nextval = fibvaln1 + fibvaln2
 	while count < 50:
 		if count == n:
-			#print("this is the nth iteration, number is: ", n)
-			#print("fibvaln1 = ", fibvaln1, "fibvaln2 = ", fibvaln2, "nextval = ", nextval)
 			print("The ", n," fibonacci series value is: ", nextval)
-		# add fibn1 and fibn2 for next number in series 
+		#print("fibvaln1 = ", fibvaln1, "fibvaln2 = ", fibvaln2, "nextval = ", nextval)
+		# add fibvaln1 and fibvaln2 for next number in series 
 		fibvaln2 = fibvaln1
 		fibvaln1 = nextval
 		nextval = fibvaln1 + fibvaln2
 		count += 1
-	#	fibval = f0 + f1
-	#	nextval = fibval +
-	#	print(fibval)
 	
-#def lucas(n):
-	
+def lucas(n):
+	count = 0
+	l0 = 2
+	l1 = 1
+	lucvaln1 = l0 + l1
+	lucvaln2 = l1
+	nextval = lucvaln1 + lucvaln2
+	while count < 50:
+		if count == n:
+			print("The ", n," lucas series value is: ", nextval)
+		print("lucvaln1 = ", lucvaln1, "fibvaln2 = ", lucvaln2, "nextval = ", nextval)
+		# add lucvaln1 and lucvaln2 for next number in series 
+		lucvaln2 = lucvaln1
+		lucvaln1 = nextval
+		nextval = lucvaln1 + lucvaln2
+		count += 1
 
 #def sum_series(x, y, z):
 
 
+fibonacci(6)
+#lucas(5)
 
-fibonacci(11)
