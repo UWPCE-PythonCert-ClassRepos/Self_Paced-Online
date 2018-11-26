d274035902947c2d5dd85d045d8cba2608af88be
jstrauss123 <jstrauss123@gmail.com>
2018-11-25 16:24:33 -0800

    putting together the outline, doing some initial testing with simple code for fib func

---
+++ students/jstrauss123/lesson02/series.py
@@ -0,0 +1,16 @@
+# series function - compute fibonacci and lucas series
+
+def fibonacci(n):
+	
+	
+#def lucas(n):
+	f0 = 0
+	f1 = 1
+	fibval = f0 + f1
+	print(fibval)
+
+#def sum_series(x, y, z):
+
+
+
+fibonacci(1)
