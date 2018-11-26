e1bc8589524d5e82ef7a36864d85d346f56e97e8
Dennis Coffey <denniscoffey787@yahoo.com>
2018-11-25 20:46:53 -0800

    Lesson 4 trigram file updated to clean commas and periods from input story

--- students/Dennis_Coffey/lesson04/trigram.py
+++ students/Dennis_Coffey/lesson04/trigram.py
@@ -19,16 +19,18 @@ import random
 def story_list(input_file):
     with open(input_file) as f:
         story_str = f.read()
+        # Clean out periods and commas from story
+        story_str = story_str.replace('.','')
+        story_str = story_str.replace(',','')
+        # Create list of words in story
         story_words = story_str.split()
     return story_words
     
 # Convert list into trigram dictionary
 def create_trigram_dict(storylist):
     trigram_dict = {}
-    print(len(storylist))
     for i in range(len(storylist)-2):
         trigram_dict[storylist[i] + ' ' + storylist[i+1]] = storylist[i+2]
-#    print(trigram_dict)
     return trigram_dict
 
 # Create trigram output
@@ -41,13 +43,11 @@ def create_trigram_output(input_file, trigram_output, start_wordpair):
         f.write(filter_string + ' ')
         # Perform trigram process up to 500 times to create kata of 500 or so words
         for i in range(500):
-            print(i)
             # Test that not at end of file
             if not d.get(filter_string):
-                print('break')
                 break
             # Randomly choose dictionary value based on key (filter_string)
-            random_value = random.choice([(d[filter_string])])
+            random_value = random.choice([d.get(filter_string)])
             f.write(random_value + ' ')
             key_words = filter_string.split(' ')
             # Generate new key to filter by combining last word of prior key
@@ -55,7 +55,10 @@ def create_trigram_output(input_file, trigram_output, start_wordpair):
             filter_string = str(key_words[1]) + " " + random_value
 
 
+# Kata example for sherlock_small.txt, outputting to trigram_out_small.txt,
+# starting with key of 'I was'            
+create_trigram_output('sherlock_small.txt', 'trigram_out_small.txt', 'I was')    
 
-#create_trigram_output('sherlock_small.txt', 'trigram_out.txt', 'I was')    
-create_trigram_output('sherlock.txt', 'trigram_out.txt', 'nature when')    
-# print(create_trigram_dict(story_list('sherlock_small.txt')))
+# Kata example for sherlock.txt, outputting to trigram_out.txt,
+# starting with key of 'with this'            
+create_trigram_output('sherlock.txt', 'trigram_out.txt', 'with this')    
