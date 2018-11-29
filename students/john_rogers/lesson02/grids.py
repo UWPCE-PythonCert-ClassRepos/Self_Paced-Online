0cb54d4761c066bf031a70f7d8dc14c15d1bf006
john r <johnroge@outlook.com>
2018-11-29 14:22:47 -0800

    Updates to grids.py, still need to complete third exercise.

--- students/john_rogers/lesson02/grids.py
+++ students/john_rogers/lesson02/grids.py
@@ -3,19 +3,59 @@
 # Author: JohnR
 
 
+def g():
+    """Print a grid with no user input"""
+    horizontal_line = '+ - + - +'
+    vertical_line = '|   |   |'
+    print(horizontal_line)
+    print(vertical_line)
+    print(horizontal_line)
+    print(vertical_line)
+    print(horizontal_line)
+
+
+g()
+
+
 def grid(num):
-    """Simple grid with single input"""
+    """Simple grid with single input, the size of the boxes"""
+    if num >= 2:
+        num = round(num / 2)
+    else:
+        num = 1
     print('+', '-' * num, '+', '-' * num, '+')
     for i in range(num):
-        print('|', " " * num, '|', " " *num, '|')
+        print('|', " " * num, '|', " " * num, '|')
     print('+', '-' * num, '+', '-' * num, '+')
     for i in range(num):
-        print('|', " " * num, '|', " " *num, '|')
+        print('|', " " * num, '|', " " * num, '|')
     print('+', '-' * num, '+', '-' * num, '+')
 
 
-grid(8)
+grid(15)
 
 
-def multi_grid(boxes, size):
-    
+# TODO: Fix this ugly mess
+def multiple_grids(boxes, size):
+    """ take in two values, number of boxes and size of each box"""
+    plus = '+ '
+    dash = '- '
+    wall = '|'
+
+    horizontal = (plus + dash * size) * boxes + plus
+    vertical = (wall + ((' ' * size) + wall) * boxes)
+    print(horizontal)
+    for i in range(size):
+        print((vertical + '\n') * size + horizontal)
+
+
+multiple_grids(2, 6)
+
+
+
+
+
+
+
+
+
