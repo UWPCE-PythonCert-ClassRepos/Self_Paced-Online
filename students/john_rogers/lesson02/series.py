21db2a7d453ab0d0db694d7288b86fbcd5f395e7
john r <johnroge@outlook.com>
2018-11-30 11:01:00 -0800

    Adding series.py, empty framework

---
+++ students/john_rogers/lesson02/series.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Create a new module series.py in the lesson02 folder in your student folder.
+In it, add a function called fibonacci.
+The function should have one parameter, n.
+The function should return the nth value in the fibonacci series (starting
+with zero index)
+Ensure that your function has a well-formed docstring
+Author: JohnR
+"""
+
+
+def main():
+    pass
+
+
+def fibonacci(n):
+    """
+    Create a fibonacci series wtih a single function
+    :param n:
+    :return: return the nth value starting with zero
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
