df790b8014bc8c2200a88b52979457864bd7a1d9
john r <johnroge@outlook.com>
2018-11-30 13:08:45 -0800

    Adding lucas function to series.py

--- students/john_rogers/lesson02/series.py
+++ students/john_rogers/lesson02/series.py
@@ -15,29 +15,38 @@ def main():
     Main script logic
     :return: Call each function and print the results to screen
     """
-    print(fibonacci(7))
+    print(fibonacci(9))
+    print(lucas(9))
 
 
 def fibonacci(n):
     """
     Create a fibonacci series starting at 0 and 1
-    :param n: any integer (presumably, not tested)
+    :param n: any positive integer
     :return: return fibonacci value for input n
     """
     if n < 0:
         print('invalid input')
     elif n == 0 or n == 1:
         return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
     """
     Create a Lucas series that start at 2 and 1
-    :param n: any integer (presumably)
+    :param n: any positive integer
     :return: return Lucas value for input n
     """
+    if n < 0:
+        print('invalid input')
+    elif n == 0:
+        return 2
+    elif n == 1:
+        return 1
+
+    return lucas(n - 1) + lucas(n - 2)
 
 
 if __name__ == '__main__':
