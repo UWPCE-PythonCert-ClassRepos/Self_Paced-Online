5f3375c13aac117fff2b4b1251e44626b473650a
Paul Jurek <pjurek3@gmail.com>
2018-11-30 21:36:37 -0800

    completed step 1 * setup tests for circle * created circle class

--- students/paul_jurek/lesson08/circle.py
+++ students/paul_jurek/lesson08/circle.py
@@ -1,4 +1,12 @@
 """Circle generation Class
 This is part of Lesson 8 for UW Python 201 course
-This generates a circle and related methods to 
-practice OOP and TDD"""
+This generates a circle and related methods to
+practice OOP and TDD
+reference: https://startlearning.uw.edu/courses/course-v1:UW+PYTHON210+2018_Winter/courseware/0e928204424a407eac492ebcd2f69adb/d57639f9f0a8498f8578550886710867/?child=first"""
+
+
+class Circle:
+    """circle geometry object"""
+
+    def __init__(self, radius):
+        self.radius = radius
--- students/paul_jurek/lesson08/test_circle.py
+++ students/paul_jurek/lesson08/test_circle.py
@@ -1,1 +1,8 @@
 """test suite for circle class"""
+from circle import Circle
+
+def test_cirle_creation():
+    """when user initiates a circle with no keyword
+    and object is created with equivalent radius"""
+    c = Circle(4)
+    assert c.radius == 4
