f9dcc4ad6cbeb9cf2f40bf4d4a6132d415b647d4
Paul Jurek <pjurek3@gmail.com>
2018-11-30 21:47:48 -0800

    completed step 3 * built test for modiying diameter * no changes to circle class as setter created in step 2

--- students/paul_jurek/lesson08/test_circle.py
+++ students/paul_jurek/lesson08/test_circle.py
@@ -1,12 +1,16 @@
 """test suite for circle class"""
+
+import pytest
 from circle import Circle
 
+@pytest.fixture
+def example_circle():
+    return Circle(4)
 
-def test_cirle_creation():
+def test_cirle_creation(example_circle):
     """when user initiates a circle with no keyword
     and object is created with equivalent radius"""
-    c = Circle(4)
-    assert c.radius == 4
+    assert example_circle.radius == 4
 
 
 def test_circle_diameter_exists():
@@ -16,3 +20,14 @@ def test_circle_diameter_exists():
     c = Circle(radius)
     assert c.diameter == 2*radius
     assert c.diameter == 2*c.radius
+
+
+def test_user_can_change_diameter(example_circle):
+    """when user changes a circles diameter
+    the radius is updated"""
+    new_diameter = 10
+    new_radius = new_diameter/2
+    assert new_diameter != example_circle.diameter
+    example_circle.diameter = new_diameter
+    assert example_circle.diameter == new_diameter
+    assert example_circle.radius == new_radius
