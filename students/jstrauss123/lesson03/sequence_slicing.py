5efa5da040bf908ea481adfd69f6ca76863c220a
jstrauss123 <jstrauss123@gmail.com>
2018-12-02 18:58:31 -0800

    first validation, returning the proper first and last value

--- students/jstrauss123/lesson03/sequence_slicing.py
+++ students/jstrauss123/lesson03/sequence_slicing.py
@@ -6,10 +6,13 @@ a_tuple = (2, 54, 13, 12, 5, 32)
 
 # define function - accept a sequence and return a copy of sequence with first and last items exchanged
 def exchange_first_last(seq):
+    first = seq[0]
+    last = seq[-1]
+    print("first is: ", first, " last is: ", last)
     
     
 
-#exchange_first_last(a_string)
+exchange_first_last(a_string)
 #exchange_first_last(a_tuple)
 
 
@@ -17,11 +20,11 @@ def exchange_first_last(seq):
 
 
 # assert tests
-if __name__ == "__main__":
-	# this runs only if run as a script
-	print("Running validation tests")
-	# validate exchange_first_last function returning appropriate value
-	assert exchange_first_last(a_string) == "ghis is a strint"
-    assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
-	
-    print("validation tests passed")
+#if __name__ == "__main__":
+#   # this runs only if run as a script
+#   print("Running validation tests")
+#   # validate exchange_first_last function returning appropriate value
+#   assert exchange_first_last(a_string) == "ghis is a strint"
+#   assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+#
+#    print("validation tests passed")
