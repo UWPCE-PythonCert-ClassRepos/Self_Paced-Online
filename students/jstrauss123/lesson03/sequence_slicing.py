664ffa2446bdad88b2f7285d0548d7f838ed7451
jstrauss123 <jstrauss123@gmail.com>
2018-12-02 20:19:40 -0800

    Second function: remove_every_other_item. Fairly quick and easy change out of recoding first function to step through sequence by 2. assert tests pass

--- students/jstrauss123/lesson03/sequence_slicing.py
+++ students/jstrauss123/lesson03/sequence_slicing.py
@@ -4,7 +4,7 @@
 a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
 
-# define function - accept a sequence and return a copy of sequence with first and last items exchanged
+# function - accept a sequence and return a copy of sequence with first and last items exchanged
 def exchange_first_last(seq):
     # assign first and last value
     first = seq[0]
@@ -24,6 +24,19 @@ def exchange_first_last(seq):
         outseq = work_list
     return(outseq)
     
+# function - accept a sequence and return every other item in sequence
+def  remove_every_other_item(seq):
+    work_list = list(seq)
+    work_list = work_list[0:-1:2]
+    #print(work_list)
+    if type(seq) == str:
+        # convert list back to string
+        outseq = str("".join(work_list))
+    elif type(seq) == tuple:
+        outseq = tuple(work_list)
+    else:
+        outseq = work_list
+    return(outseq)
     
     
     
@@ -32,6 +45,8 @@ def exchange_first_last(seq):
 
 #exchange_first_last(a_string)
 #exchange_first_last(a_tuple)
+remove_every_other_item(a_string)
+remove_every_other_item(a_tuple)
 
 
 
@@ -44,5 +59,7 @@ if __name__ == "__main__":
    # validate exchange_first_last function returning appropriate value
    assert exchange_first_last(a_string) == "ghis is a strint"
    assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+   assert remove_every_other_item(a_string) == "ti sasrn"
+   assert remove_every_other_item(a_tuple) == (2, 13, 5)
 
    print("validation tests passed")
