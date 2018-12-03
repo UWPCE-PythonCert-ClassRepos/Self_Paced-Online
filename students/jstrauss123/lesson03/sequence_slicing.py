331dfd0f56a027a5a7b6c06339f4acb32c7ebfaa
jstrauss123 <jstrauss123@gmail.com>
2018-12-02 21:14:02 -0800

    lesson3 - sequence_slicing - complete and all assert tests successful

--- students/jstrauss123/lesson03/sequence_slicing.py
+++ students/jstrauss123/lesson03/sequence_slicing.py
@@ -68,32 +68,28 @@ def reverse_items(seq):
 # function - accept a sequence, return in new order: middle third/last third/first third
 def  return_in_thirds(seq):    
     work_list = list(seq)
-    print("orig list", work_list)
     # determine length of list and divide by 3
     list_len = (len(work_list) // 3)
     # assign first third to list1 and remove from orig list
     work_list1 = work_list[0:list_len]
     work_list = work_list[list_len:]
-    print("list after first third", work_list)
     # assign next third to list2 and remove from orig list
     work_list2 = work_list[0:list_len]
     work_list = work_list[list_len:]
-    print("list with last third", work_list)
     # assign the remainder to list3
     work_list3 = work_list
-    print(work_list)
     # assign thirds by middle (2), last (3) and first (1)
-    outseq = work_list2 + work_list3 + work_list1
-    print(outseq)
+    work_list = work_list2 + work_list3 + work_list1
+    outseq = work_list
     if type(seq) == str:
         # convert list back to string
         outseq = str("".join(work_list))
     elif type(seq) == tuple:
+        # convert list back to tuple
         outseq = tuple(work_list)
     else:
         outseq = work_list
     return(outseq)
-    print(outseq)
     
 # create string and tuple values
 a_string = "this is a string"
@@ -108,8 +104,8 @@ a_tuple = (2, 54, 13, 12, 5, 32)
 #first_last_four(a_tuple)
 #reverse_items(a_string)
 #reverse_items(a_tuple)
-return_in_thirds(a_string)
-return_in_thirds(a_tuple)
+#return_in_thirds(a_string)
+#return_in_thirds(a_tuple)
 
 
 
@@ -127,5 +123,7 @@ if __name__ == "__main__":
    assert first_last_four(a_tuple) == ()
    assert reverse_items(a_string) == "gnirts a si siht"
    assert reverse_items(a_tuple) == (32, 5, 12, 13, 54, 2)
+   assert return_in_thirds(a_string) == "is a stringthis "
+   assert return_in_thirds(a_tuple) == (13, 12, 5, 32, 2, 54)
 
    print("validation tests passed")
