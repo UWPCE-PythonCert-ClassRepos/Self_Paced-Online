8150b64f7344db038791e6f0bb8a5b04e89a6975
jstrauss123 <jstrauss123@gmail.com>
2018-12-04 22:07:28 -0800

    task one working, string formatted properly, moving on to task two

--- students/jstrauss123/lesson03/strformat_lab.py
+++ students/jstrauss123/lesson03/strformat_lab.py
@@ -4,17 +4,15 @@
 tuple1 = (2, 123.4567, 10000, 12345.67)
 
 # first value
-#print("the number is {:03.0f}".format(tuple1[0]))
-print("the number is {:0>3d}".format(tuple1[0]))
+#print("the number is {:0>3d}".format(tuple1[0]))
 
 # second value
-#print("the number is {:06.2f}".format(tuple1[1]))
-print("the number is {:.2f}".format(tuple1[1]))
-
+#print("the number is {:.2f}".format(tuple1[1]))
 
 # third value
-print("the number is {:.2e}".format(tuple1[2]))
-
+#print("the number is {:.2e}".format(tuple1[2]))
 
 # fourth value
-print("the number is {:.3e}".format(tuple1[3]))
+#print("the number is {:.3e}".format(tuple1[3]))
+
+print("{}{:0>3d}: {:.2f}, {:.2e}, {:.3e}".format("file_",tuple1[0],tuple1[1],tuple1[2], tuple1[3]))
