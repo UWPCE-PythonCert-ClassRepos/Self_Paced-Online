acfa8d2c1358a2709b1f16cc62039b0efa742366
jstrauss123 <jstrauss123@gmail.com>
2018-12-06 21:51:13 -0800

    creating outline of code for mailroom

---
+++ students/jstrauss123/lesson03/mailroom.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# function - create report
+def fun_report():
+
+
+# function - send thank you
+def fun_sendty():
+
+
+
+
+# main
+if __name__ == "__main__":
+donor_list = [[Mickey Mouse, 100, 150, 100],[Minnie Mouse, 50, 50], [Ron Jones, 100]]
+
+
+
