823ba8dfcd658562f380e87373f8dcdef6ebd73f
jstrauss123 <jstrauss123@gmail.com>
2018-12-08 09:49:08 -0800

    adding structure to create_report function

--- students/jstrauss123/lesson03/mailroom.py
+++ students/jstrauss123/lesson03/mailroom.py
@@ -3,7 +3,14 @@
 # function - create report
 def fun_report():
     yum = ''
-
+    response = input("Please provide full name of donor: ")
+    while response == "list":
+        print(donor_list)
+        
+    #if response == "list"
+    #    print(donor_list)
+    #if response in donor_list:
+        
 # function - send thank you
 def fun_sendty():
     yum1 = ''
@@ -12,7 +19,7 @@ def fun_sendty():
 
 # main
 if __name__ == "__main__":
-    donor_list = [["Mickey Mouse", 100, 150, 100], ["Minnie Mouse", 50, 50], ["Ron Jones", 100]]
+    donor_list = [["Mickey Mouse", 100, 150, 100], ["Minnie Mouse", 50, 50], ["Ron Jones", 100],["Donald Duck", "25"], ["Busy Bear", "10", "10", "10"]]
 
     response = input("Please select: 1 to Send a Thank you, 2 to Create a report or q to quit : ")
     print(response)
@@ -26,5 +33,5 @@ if __name__ == "__main__":
         else:
             print("exiting mailroom")
             break
-        
+    print("Good-bye")
 
