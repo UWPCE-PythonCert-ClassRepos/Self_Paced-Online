18899d3bb0907b756c507644161e9919d6b4e0ca
john r <johnroge@outlook.com>
2018-12-08 14:56:34 -0800

    Created template for list_lab.py, series 1.

---
+++ students/john_rogers/lesson03/list_lab.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+
+Author: JohnR
+Version: .1
+Date: 12/8/2018
+Notes: Core logic for part one
+"""
+
+
+def main():
+    """
+    TODO: Create list apples/pears/organges/peaches
+        Display list - print to screen
+        Ask user for another fruit & add it to the end of the list
+        Display list
+        Input number, display number along with fruit from list (1 is first)
+        Add fruit to beginning of list using +
+        Display list
+        Add another fruit using insert
+        Display list
+        Display all fruits that begin with P
+    :return:
+    """
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()
