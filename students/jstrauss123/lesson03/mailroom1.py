90621ad76daa9faabcec29c6ab61040e3d1a5d98
jstrauss123 <jstrauss123@gmail.com>
2018-12-08 19:41:23 -0800

    code working, print working -- successful coding using list in list. creating push to upload. I need to replace the code that was submitted on website, sent email to python1

--- students/jstrauss123/lesson03/mailroom1.py
+++ students/jstrauss123/lesson03/mailroom1.py
@@ -36,15 +36,24 @@ def fun_sendty(donorl):
         
 # function - create report
 def fun_report():
+    sum = 0
     donor_len = len(donor_list)
     print("donor_len = ", donor_len)
     count1 = 0
     print("")
+    # print header line
     print("{:15} |{:>15} |{:>2} |{:>15}".format("Donor Name","Total Given","Num Gifts","Average Gifts" ))
-    while count1 < donor_len:
-        print("")
-        #print("{:15} ${:>15} {:>2} ${:>15}".format(donor_list[count1], 25000, 2, 12000))
-        count1 += 1
+    # loop through donors and contributions and print detail line
+    for donor in donor_list:
+        sum = 0
+        num_of_donations = 0
+        #str1 = ""
+        num_of_donations = len(donor)-1
+        contrib_amts = donor[1:]
+        for elem in contrib_amts:
+            sum = sum + int(elem)
+        avg_contribution_amt = sum / num_of_donations
+        print("{:15} ${:>15.2f} {:>2} ${:>15.2f}".format(donor[0], sum, num_of_donations, avg_contribution_amt))
     print("")
         
 
@@ -53,7 +62,7 @@ if __name__ == "__main__":
     donor_list = [["Mickey Mouse", "100", "150", "100"], ["Minnie Mouse", "50", "50"], ["Ron Jones", "100"],["Donald Duck", "25"], ["Busy Bear", "10", "10", "10"]]
     response = ""
     while response != "q":
-        response = input("Please select: 1 to Send a Thank you, 2 to Create a report or q to quit : ")
+        response = input("Please select:\n 1 to Send a Thank you \n 2 to Create a report \n or q to quit : ")
         if response == "1":
             updated_list = fun_sendty(donor_list)
             donor_list = updated_list
