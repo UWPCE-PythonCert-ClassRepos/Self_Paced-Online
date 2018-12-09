2354a6b5e94a1929875b59a04b930491073824ec
john r <johnroge@outlook.com>
2018-12-08 16:17:44 -0800

    Completed series 1 of list_lab.py in Lesson03.

--- students/john_rogers/lesson03/list_lab.py
+++ students/john_rogers/lesson03/list_lab.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """
+        Add fruit to beginning of list using +
+        Display list
+        Add another fruit using insert
+        Display list
+        Display all fruits that begin with P
 
 Author: JohnR
 Version: .1
@@ -9,23 +14,36 @@ Notes: Core logic for part one
 
 
 def main():
-    """
-    TODO: Create list apples/pears/organges/peaches
-        Display list - print to screen
-        Ask user for another fruit & add it to the end of the list
-        Display list
-        Input number, display number along with fruit from list (1 is first)
-        Add fruit to beginning of list using +
-        Display list
-        Add another fruit using insert
-        Display list
-        Display all fruits that begin with P
-    :return:
-    """
-    pass
+    fruit_list = ['Apples',
+                  'Pears',
+                  'Oranges',
+                  'Peaches',
+                ]
+    print(fruit_list)
 
+    new_fruit = input('Please enter name of a fruit: ')
+    new_fruit = new_fruit.capitalize()
+    fruit_list.append(new_fruit)
 
+    print(fruit_list)
 
+    number = input('Please enter a number between 1 and 5: ')
+    number = int(number)
+    print(str(number) + ' is ' + fruit_list[number - 1])
+
+    second_fruit = input('Enter another fruit: ')
+    second_fruit = second_fruit.capitalize()
+    fruit_list = [second_fruit] + fruit_list
+    print(fruit_list)
+
+    third_fruit = input('Enter a third fruit: ')
+    third_fruit = third_fruit.capitalize()
+    fruit_list.insert(0, third_fruit)
+    print(fruit_list)
+
+    for fruit in fruit_list:
+        if fruit[0] == 'P':
+            print(fruit)
 
 
 if __name__ == '__main__':
