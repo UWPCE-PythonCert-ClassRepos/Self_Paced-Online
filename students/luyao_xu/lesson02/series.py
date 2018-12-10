540053eaf7915b478d2c69a255e08e07529b2a39
Luyao Xu <xuluyao19931213@gmail.com>
2018-12-09 20:05:25 -0800

    add fibonacci and lucas functions

---
+++ students/luyao_xu/lesson02/series.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+
+
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-2) + lucas(n-1)
