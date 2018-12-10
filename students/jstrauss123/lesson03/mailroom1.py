3c52f448e6ea2e508174e8531ee98d61b1daed78
jstrauss123 <jstrauss123@gmail.com>
2018-12-09 17:12:55 -0800

    changing data structure to format in mailroom tutorial.. testing with code to try to access, will require major rewrite tutorial unseen until today

--- students/jstrauss123/lesson03/mailroom1.py
+++ students/jstrauss123/lesson03/mailroom1.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python3
 
 # function - send thank you
-def fun_sendty(donorl):
+def send_thankyou(donorl):
     name_list = list()
     local_donor_list = donorl
+    # create list of donor names
     for donor in local_donor_list:
             str1 = ""
             for elem in donor[0]:
                 str1 = str1 + elem
             name_list.append(str1)
+    # prompt for donor name
     full_name = input("Please provide full name of donor: ")
+    # while input value is "list" continue to provide list of donors
     while full_name == "list":
-        name_list = list()
-        for donor in local_donor_list:
-            str1 = ""
-            for elem in donor[0]:
-                str1 = str1 + elem
-            name_list.append(str1)
+        # create updated donor list of names after new add.
+        #name_list = list()
+        #for donor in local_donor_list:
+        #    str1 = ""
+        #    for elem in donor[0]:
+        #        str1 = str1 + elem
+        #    name_list.append(str1)
         print(name_list)
         full_name = input("Please provide full name of donor: ")
-        
+    
+    # reworking this section to ask for contribution amount from existing donor. I missed this in home work requirements.
+    # also the code could be simpler so only one mail message statement exists.
+    # if donor does not exist add to donor list, ask for contribution, add to list, mail thank you, return to main menu
+    if full_name not in name_list:
+        # donor does not exist, add to donor_list
+        new_donor_list = [[full_name]]
+        print("new list contains: ", new_donor_list)
+        local_donor_list = local_donor_list + new_donor_list
+        print("updated local_donor_list: ", local_donor_list)
+    
+    # continue with prompt for contribution amount and add to list
+    contrib_amt = input("Enter donation amount for donor: ")
+    new_contrib_amt = [[contrib_amt]]
+    print("new list contains: ", new_donor_list)
+    local_donor_list = local_donor_list+new_donor_list
+    print("updated donor list: ", local_donor_list)
+    print("Dear {}, Thank you so much for your generous contribution".format(full_name))
+    print("")
+    
+    
+    """    
     if full_name in name_list:
         # donor exists - send thank you email
         print("Dear {}, Thank you so much for your generous contribution".format(full_name))
@@ -33,9 +58,10 @@ def fun_sendty(donorl):
         print("Dear {}, Thank you so much for your generous contribution".format(full_name))
         print("")
     return local_donor_list
-        
+    """    
+    return local_donor_list
 # function - create report
-def fun_report():
+def create_report():
     sum = 0
     donor_len = len(donor_list)
     print("donor_len = ", donor_len)
@@ -59,15 +85,18 @@ def fun_report():
 
 # main
 if __name__ == "__main__":
-    donor_list = [["Mickey Mouse", "100", "150", "100"], ["Minnie Mouse", "50", "50"], ["Ron Jones", "100"],["Donald Duck", "25"], ["Busy Bear", "10", "10", "10"]]
+    #donor_list = [["Mickey Mouse", "100", "150", "100"], ["Minnie Mouse", "50", "50"], ["Ron Jones", "100"],["Donald Duck", "25"], ["Busy Bear", "10", "10", "10"]]
+    # trying to use the structure described in mailroom tutorial
+    donor_list = [("Mickey Mouse", ["100", "150", "100"]), ("Minnie Mouse", ["50", "50"]), ("Ron Jones", ["100"]), ("Donald Duck", ["25"]), ("Busy Bear", ["10", "10", "10"])]
     response = ""
     while response != "q":
+        print(donor_list)
         response = input("Please select:\n 1 to Send a Thank you \n 2 to Create a report \n or q to quit : ")
         if response == "1":
-            updated_list = fun_sendty(donor_list)
-            donor_list = updated_list
+            donor_list = send_thankyou(donor_list)
+            #donor_list = updated_list
         elif response == "2":
-            fun_report()
+            create_report()
         elif response == "q":
             print("q selected - Good-bye")
         else:
