b185a32299ec7a41d652662c3e73e8860d62358e
Joshua Bone <joshua.bone@hotmail.com>
2018-12-09 20:25:57 -0800

    add assignment description

---
+++ students/joshua-bone/lesson2/grid-printer.py
@@ -0,0 +1,17 @@
+#Joshua Bone - UW Python 210 - Lesson 2
+#Grid Printer Exercise
+
+#Write a function that draws a grid like the following:
+#
+#+ - - - - + - - - - +
+#|         |         |
+#|         |         |
+#|         |         |
+#|         |         |
+#+ - - - - + - - - - +
+#|         |         |
+#|         |         |
+#|         |         |
+#|         |         |
+#+ - - - - + - - - - +
+
