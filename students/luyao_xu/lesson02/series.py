687fa43871dbee018cc4f6420c760305b7827f8f
Luyao Xu <xuluyao19931213@gmail.com>
2018-12-09 20:05:59 -0800

    add `sum_series` function to `series.py`

--- students/luyao_xu/lesson02/series.py
+++ students/luyao_xu/lesson02/series.py
@@ -14,3 +14,13 @@ def lucas(n):
         return 1
     else:
         return lucas(n-2) + lucas(n-1)
+
+
+def sum_series(n, first=0, second=1):
+    if n == 0:
+        return first
+    elif n == 1:
+        return second
+    else:
+        return sum_series(n-2, first, second) + sum_series(n-1, first, second)
+
