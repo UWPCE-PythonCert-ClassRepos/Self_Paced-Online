f0eb14fe3c5d06fb9876b082c6ae0501428185a9
joshua-bone <joshua.bone@hotmail.com>
2018-12-09 21:02:50 -0800

    answer part 1

--- students/joshua-bone/lesson2/grid-printer.py
+++ students/joshua-bone/lesson2/grid-printer.py
@@ -15,3 +15,11 @@
 #|         |         |
 #+ - - - - + - - - - +
 
+# Prints a 2x2 grid having a side length of n
+def print_grid(n):
+  a=('-'*(n-1)).join('+++')+'\n'
+  b=(' '*(2*n-1)).join('|||')+'\n'
+  print((b*(n-1)).join([' '.join(a)]*3))
+
+if __name__ == "__main__":
+  print_grid(5)
