33abb463a08058603138109c1f52f901acaa7615
LouReis <lou.reis@gmx.com>
2018-12-10 19:03:00 -0700

    added additional comments for functions and overview of the code

--- lesson02/grid_part_final.py
+++ lesson02/grid_part_final.py
@@ -1,6 +1,14 @@
 # grid_part_final.py
 #
-# Displays a grid of small boxes based on user input for square size & dimensions.
+# This is the assignment for Lesson2, each part of the exercise is included
+# As indicated in the exercise, "Do something reasonable", I attempted to meet
+# the objectives of the exercise utilizing the demonstrated elements for
+# string manipulation that were provided.  The end result is a grid that is
+# printed out with modular functions to aid in the printing & gathering of
+# user input.  I allowed for the user to indicate the cell size, to indicate
+# the number of columns in the grid, and to also indicate the number of rows.
+#
+# This code isplays a grid of small boxes based on user input for square size & dimensions.
 # Where the user enters a value for x and y, the grid of boxes is x by y in grid size.
 #
 # Coded by LouReis
@@ -44,4 +52,5 @@ def user_input():
   print('Here is a',columns,'by',rows,'grid:')
   variable_size_grid(size,columns,rows)
 
+#Call the function to prompt the user for the 3 parameters.
 user_input()
