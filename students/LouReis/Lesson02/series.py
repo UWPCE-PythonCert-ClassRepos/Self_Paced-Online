3bc6e39ff11de361631fff562a2235b05863ec14
LouReis <lou.reis@gmx.com>
2018-12-11 17:53:01 -0700

    New Lesson 2 exercises

---
+++ students/LouReis/Lesson02/series.py
@@ -0,0 +1,13 @@
+# series.py
+# Fibonacci assignment
+# Coded by LouReis
+#
+# Note that the fibinacci series is naturally recursive –
+# the value is defined by previous values:
+# fib(n) = fib(n-2) + fib(n-1)
+#
+
+def fibonacci.py(n):
+    x=0
+    y=1
+    for x in range(0,n):
