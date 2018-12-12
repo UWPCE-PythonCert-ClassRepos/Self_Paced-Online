29d65b898efd23c43990e0bc0f36eb4798c2fe6c
LouReis <lou.reis@gmx.com>
2018-12-11 18:21:19 -0700

    Added code to prompt user for the value of n

--- students/LouReis/Lesson02/series.py
+++ students/LouReis/Lesson02/series.py
@@ -7,7 +7,16 @@
 # fib(n) = fib(n-2) + fib(n-1)
 #
 
-def fibonacci.py(n):
-    x=0
-    y=1
+def fibonacci(n):
     for x in range(0,n):
+        n=(n-2) + (n-1)
+    print(n)
+
+def user_input():
+#This function prompts the user for the square size & grid dimensions.
+  print ('This function returns the nth value in the fibonacci series.')
+  n=int(input('Enter the value for n: '))
+  print('The',n,'th value is:')
+  fibonacci(n)
+
+user_input()
