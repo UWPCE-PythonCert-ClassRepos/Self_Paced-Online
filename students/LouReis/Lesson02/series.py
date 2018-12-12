aeaff418a11516cf7424a45444be88097d8c51e6
LouReis <lou.reis@gmx.com>
2018-12-12 13:43:30 -0700

    Added the lucas function

--- students/LouReis/Lesson02/series.py
+++ students/LouReis/Lesson02/series.py
@@ -6,7 +6,7 @@
 # the value is defined by previous values:
 # fib(n) = fib(n-2) + fib(n-1)
 
-# This function caclulates the nth value in the Fibonacci Series.
+# This function calculates the nth value in the Fibonacci Series.
 # This is a recursive funtion.
 def fibonacci(n):
     if n <= 1:
@@ -14,11 +14,25 @@ def fibonacci(n):
     else:
         return (fibonacci(n-2) + fibonacci(n-1))
 
+# This function calculates the nth value of the Lucas Series.
+# The first numbers are 2 & 1 is the difference from the Fibonacci Series.
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return (lucas(n-2) + lucas(n-1))
+
 def user_input():
 #This function prompts the user for the square size & grid dimensions.
-    print ('This function returns the nth value in the fibonacci series.')
+    print ('This function returns the nth value in the Fibonacci series.')
     n=int(input('Enter a number for n to compute the nth value: '))
     print('The value is: ',end="")
     print (fibonacci(n))
+    print ('This function returns the nth value in the Lucas series.')
+    n=int(input('Enter a number for n to compute the nth value: '))
+    print('The value is: ',end="")
+    print (lucas(n))
 
 user_input()
