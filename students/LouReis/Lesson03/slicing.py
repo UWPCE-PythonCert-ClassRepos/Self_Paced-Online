35a60cdca31a26d9582f04b3e93752647ed26d1b
LouReis <lou.reis@gmx.com>
2018-12-13 17:15:52 -0700

    Added more functions

--- students/LouReis/Lesson03/slicing.py
+++ students/LouReis/Lesson03/slicing.py
@@ -1,15 +1,20 @@
 # slicing.py
 #
 #
-#
-
-def slice_sequence()
-
-def exchange_first_last(seq):
-    return a_new_sequence
+# Coder: LouReis
 
 def copy_sequence(seq):
+    """Return a copy of the sequence entered"""
     return seq
 
 def copy_first_last(seq):
+    """Return the first and last items of the sequence entered"""
     return seq[0],seq[-1]
+
+def copy_every_other(seq):
+    """Return every other item in the sequence entered"""
+    return seq[::2]
+
+def copy_rem_first_4_last_4(seq):
+    """Return the remaining every other items after removing first 4 & last 4."""
+    return seq[4:-4:2]
