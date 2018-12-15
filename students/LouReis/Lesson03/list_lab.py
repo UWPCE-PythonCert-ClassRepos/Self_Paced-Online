c0200b598552e71e5298692476433e0a0bfd2734
LouReis <lou.reis@gmx.com>
2018-12-15 16:08:14 -0700

    Added and tested loop to print only fruit that start with the letter P

--- students/LouReis/Lesson03/list_lab.py
+++ students/LouReis/Lesson03/list_lab.py
@@ -15,3 +15,9 @@ fruit_list = ['Bananas'] + fruit_list
 print(fruit_list)
 fruit_list.insert(0, 'Kiwi')
 print(fruit_list)
+num_fruit = len(fruit_list)
+print("The below fruit in the list start with the letter P:")
+for x in range(0,num_fruit):
+    pick=fruit_list[x]
+    if pick[:1] == 'P' or pick[:1] == 'p':
+        print(pick)
