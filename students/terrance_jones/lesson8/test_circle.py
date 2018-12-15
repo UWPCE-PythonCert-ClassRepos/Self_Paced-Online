d548ff721e5d0b00f82aab34dccd731696476056
Jay Jones <jayjones@Jays-MacBook-Pro.local>
2018-12-15 06:11:36 -0800

    adding lesson 8

---
+++ students/terrance_jones/lesson8/test_circle.py
@@ -0,0 +1,56 @@
+from circle import *
+from math import pi
+import pytest
+
+
+c = Circle(10)
+d = Circle(5)
+
+
+def test_circle():
+	assert isinstance(c, Circle)
+	assert isinstance(d, Circle)
+
+	assert c.__str__() == "Circle with radius: 10"
+	assert c.__repr__() == "Circle(10)"
+
+def test_radius():
+	assert c.radius == 10
+
+def test_diameter():
+	assert c.diameter == 20
+
+def test_area():
+	assert c.area ==  math.pi * c.radius**2
+
+def test__mul__():
+	c = Circle(10)
+	d = Circle(5)
+	assert c * d == 50
+	assert c * 5 == 50
+
+def test__add__():
+	assert c + d == 15
+	assert d + 10 == 15
+
+def test_comparissons():
+	assert c > d
+	assert d < c
+	assert d <= c
+	assert c >= d
+	assert c != d
+
+def test_sphere():
+	sp = Sphere(50)
+	assert isinstance(sp, Sphere)
+	assert sp.area == (4 * math.pi * 50**2)
+	assert sp.volume == ((4/3) * math.pi * 50**3)
+
+	assert sp.__str__() == "Sphere with radius: 50"
+	assert sp.__repr__() == "Sphere(50)"
+
+
+
+
+
+
