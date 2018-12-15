a158711f6823fe0041b26859fc16e13fbe825dd6
LouReis <lou.reis@gmx.com>
2018-12-14 20:37:07 -0700

    Added insert list lines

--- students/LouReis/Lesson03/list_lab.py
+++ students/LouReis/Lesson03/list_lab.py
@@ -6,4 +6,12 @@ print(fruit_list)
 response = [input("Enter the name of another fruit> ")]
 fruit_list = fruit_list + response
 print(fruit_list)
-fruit_list.append(response)
+# fruit_list.append(response)
+pick = input("Enter a number to pick a fruit in the list> ")
+pick_int = int(pick)
+pick_int = pick_int - 1
+print(fruit_list[pick_int])
+fruit_list = ['Bananas'] + fruit_list
+print(fruit_list)
+fruit_list.insert(0, 'Kiwi')
+print(fruit_list)
