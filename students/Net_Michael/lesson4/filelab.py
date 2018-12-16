f8c013f72769c985fa60eb908720fa585382ce6d
Net Michael <nettaimam@gmail.com>
2018-12-15 19:38:31 -0800

    add the filelab file

--- students/Net_Michael/lesson4/filelab.py
+++ students/Net_Michael/lesson4/filelab.py
@@ -3,28 +3,15 @@
 
 # In[1]:
 
-
 import os
 import io
 import pathlib
 import shutil
 
-
-# In[3]:
-
-
 #Write a program which prints the full path for all files in the current 
 #directory, one per line
 
-
-# In[2]:
-
-
 [os.path.join(os.getcwd(),file) for file in os.listdir()]
-
-
-# In[3]:
-
 
 # copy file 
 source = os.getcwd()
@@ -32,10 +19,6 @@ destn = os.path.split(os.getcwd())[0]
 srcname = os.path.join( source, 'testfile.txt')
 dstname = os.path.join( destn, 'session04\\testfile1.txt')
 shutil.copyfile(srcname, dstname)
-
-
-# In[4]:
-
 
 
 src_students = os.path.join(destn,'session04\\students.txt')
@@ -50,15 +33,7 @@ for line in students:
     prgr_lang = prgr_lang.union(nickname_lang[1:])
     nick_names = nick_names.union(nickname_lang[:1])   
 
-
-# In[7]:
-
-
-(prgr_lang)
-
-
-# In[6]:
-
+print(prgr_lang)
 
 nicknames = (nick_names.difference(prgr_lang))
-
+print(nicknames)
