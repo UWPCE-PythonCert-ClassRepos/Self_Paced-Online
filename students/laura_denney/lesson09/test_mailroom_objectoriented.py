4f25b407ef985beb09b85555284396e3237ec2cf
Laura Denney <Laura.A.Denney@gmail.com>
2018-12-16 20:46:36 -0800

    adding lesson09 files to repository, starting work on assignment

---
+++ students/laura_denney/lesson09/test_mailroom_objectoriented.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------#
+# Title: Test Object Oriented Mail Room
+# Dev:   LDenney
+# Date:  December 13, 2018
+# ChangeLog: (Who, When, What)
+#   Laura Denney, 10/11/18, Created MailRoom Part 1 File
+#   Laura Denney, 10/12/18, Modified MailRoom Part 1 File
+#   Laura Denney, 10/12/18, Started Part 2
+#   Laura Denney, 10/12/18, Finished Part 2
+#   Laura Denney, 10/31/18, Started MailRoom Part 3
+#   Laura Denney, 11/7/18, Started MailRoom Part 4
+#   Laura Denney, 11/7/18, Started Object Oriented Mail Room
+#-------------------------------------------------#
+
+from mailroom_objectoriented import *
+
+def test_args():
+    a = Donor("Laura Denney", 5, 6, 7)
+    assert repr(a) == "'Laura Denney, 18'"
+
+
+def test_sort():
+    a = Donor("Bob")
+    a.append(5)
+    a.append(6)
+    c = Donor("Charles")
+    c.append(500)
+    e = Donor("Lewis")
+    e.append(11)
+
+    f = [a, c, e]
+    f.sort(reverse = True)
+    assert repr(f) == "['Charles, 500', 'Lewis, 11', 'Bob, 11']"
