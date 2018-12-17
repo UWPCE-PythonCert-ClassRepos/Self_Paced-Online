2a58b0cf7a66b550d8cb23d91ecf1c7ed009700b
Dennis Coffey <denniscoffey787@yahoo.com>
2018-12-16 21:24:54 -0800

    Commit test_html_render to include tests for each step.  Tests will all fail now that indents have been added to html_render

--- students/Dennis_Coffey/lesson07/test_html_render.py
+++ students/Dennis_Coffey/lesson07/test_html_render.py
@@ -17,8 +17,8 @@ page.render(f)
 
 
 # Test for Step 1 of run_html_render
-#def test_Element():
-#    assert f.getvalue() == 'Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some textAnd here is another piece of text -- you should be able to add any number'
+def test_Element():
+    assert f.getvalue() == 'Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some textAnd here is another piece of text -- you should be able to add any number'
 
 # Funtion to compare desired vs generated result for each step
 def compare_files(desired_file, generated_file):
@@ -56,5 +56,5 @@ def test_step7():
     compare_files('test_html_output7.htm', 'test_html_output7.html')        
 
 # Test for Step 8 of run_html_render
-#def test_step8():
-#    compare_files('test_html_output8.htm', 'test_html_output8.html')        
+def test_step8():
+    compare_files('test_html_output8.htm', 'test_html_output8.html')        
