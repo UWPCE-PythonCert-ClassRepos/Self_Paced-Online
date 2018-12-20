79b1c52d07e792dc63ff053ffe82cdf77f9faf28
jeanruggiero <jeanruggiero@gmail.com>
2018-12-19 17:52:10 -0800

    Improved docstrings.

--- students/jeanruggiero/Lesson03/slice_of_py.py
+++ students/jeanruggiero/Lesson03/slice_of_py.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
 def exchange_first_last(seq):
-    """This function exchanges the first and last items in a sequence."""
+    """Return a copy of the sequence with first and last items switched."""
     if len(seq) <= 1:
         return seq
     else:
         return seq[-1:] + seq[1:-1] + seq[:1]
 
 def rm_every_other_item(seq):
-    """This function returns every other item in a sequence."""
+    """Return a copy of the sequence containing every other item."""
     return seq[::2]
 
 def rm_four_every_other(seq):
-    """This function removes the first 4 and last 4 items in a sequence and returns every other item in between."""
+    """Return a copy of the sequence with first 4 and last 4 items removed,
+    and then every other item in between.
+    """
     return seq[4:-4:2]
 
 def reverse(seq):
-    """This function reverses the order of the items in a sequence."""
+    """Return a copy of the sequence with the elements reversed."""
     return seq[::-1]
 
 def thirds_reorder(seq):
-    """This function moves the first one third of items to the end of the sequence."""
+    """Return a yoda-ified copy of the sequence.
+    (Return a copy of the sequence with the first one third of items moved to the end.)"""
     third = len(seq)//3;
     return seq[third:] + seq[:third]
 
@@ -56,7 +59,7 @@ assert reverse(('some', 5, 6, [7, 8])) == ([7, 8], 6, 5, 'some')
 assert reverse([3, 2, 1]) == [1, 2, 3]
 
 # Test thirds_reorder on all sequence types including empty sequences
-assert thirds_reorder('123456789') == '456789123'
+assert thirds_reorder('you are a strong jedi ') == ' a strong jedi you are'
 assert thirds_reorder('1') == '1'
 assert thirds_reorder('') == ''
 assert thirds_reorder((5, 6, 7, 8, 9, 10)) == (7, 8, 9, 10, 5, 6)
