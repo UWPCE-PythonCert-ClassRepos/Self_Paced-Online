f636fbc88906348daed03f4cef55fdcef25a40ca
jeanruggiero <jeanruggiero@gmail.com>
2018-12-20 21:56:25 -0800

    Tasks 1 through 4 complete.

--- students/jeanruggiero/Lesson03/strformat_lab.py
+++ students/jeanruggiero/Lesson03/strformat_lab.py
@@ -11,8 +11,27 @@ file_format = 'file_{:03d} :  {:.2f}, {:.2e}, {:.3G}'
 file_disp = file_format.format(*file_info)
 print(file_disp)
 
-##################################TASK 1#######################################
+##################################TASK 2#######################################
 
+# Create a dictionary defining the file info
 file_info2 = {'name': 2, 'size': 123.4567, 'use': 10000, 'key': 12345.67}
-file_disp2 = "file_{name} :  {size}, {use}, {key}".format(**file_info2)
+
+# Create a format string that uses the dictionary keywords and apply it
+file_disp2 = "file_{name:03d} :  {size:.2f}, {use:.2e}, {key:.3G}".format(**file_info2)
 print(file_disp2)
+
+
+##################################TASK 3#######################################
+
+def formatter(in_tuple):
+    """Prints the numbers in a tuple of arbitrary length."""
+    # Build a format string
+    form_string = 'the {:d} numbers are: '+'{:d}, '*(len(in_tuple)-1) + '{:d}'
+    # Apply format string and print results
+    return form_string.format(len(in_tuple), *in_tuple)
+
+##################################TASK 4#######################################
+
+tuple_four = ( 4, 30, 2017, 2, 27)
+# Create format string using tuple indices, apply to tuple, and print
+print('{3:02d} {4:d} {2:d} {0:02d} {1:d}'.format(*tuple_four))
