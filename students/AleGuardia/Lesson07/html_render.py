f797ada146208e6e426e9c59e13eb83c1ca6593b
aguardia <alejandro.guardia@gmail.com>
2018-12-21 15:48:37 -0600

    Lesson 7 assignment step 1 to 4

---
+++ students/AleGuardia/Lesson07/html_render.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element(object):
+
+    tag = "html"
+
+    def __init__(self, content=None, **kwargs):
+        self.contents = []
+        self.attributes = kwargs
+        if content is not None:
+            self.contents.append(content)
+
+    def append(self, new_content):
+        self.contents.append(new_content)
+
+    def render(self, out_file):
+        # out_file.write("<{}>\n".format(self.tag))
+        open_tag = ["<{}".format(self.tag)]
+        for key, value in self.attributes.items():
+            open_tag.append(' {}="{}"'.format(key,value))
+        open_tag.append(">\n")
+        out_file.write("".join(open_tag))
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
+
+
+class Html(Element):
+    tag = 'html'
+
+
+class Body(Element):
+    tag = 'body'
+
+
+class P(Element):
+    tag = "p"
+
+
+class Head(Element):
+    tag = "head"
+
+
+class OneLineTag(Element):
+    def render(self, out_file):
+        out_file.write("<{}>".format(self.tag))
+        out_file.write(self.contents[0])
+        out_file.write("</{}>\n".format(self.tag))
+
+    def append(self, content):
+        raise NotImplementedError
+
+
+class Title(OneLineTag):
+    tag = "title"
+
+class SelfClosingTag(Element):
+    pass
