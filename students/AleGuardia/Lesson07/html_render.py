62e4220edfc0a2feea281506f1523b0fc523092d
aguardia <alejandro.guardia@gmail.com>
2018-12-22 12:11:23 -0600

    Fixed extra newlines

--- students/AleGuardia/Lesson07/html_render.py
+++ students/AleGuardia/Lesson07/html_render.py
@@ -40,6 +40,7 @@ class Element(object):
                 content.render(out_file)
             except AttributeError:
                 out_file.write(content)
+        out_file.write("\n")
         out_file.write(self._close_tag())
         out_file.write("\n")
 
@@ -63,11 +64,10 @@ class Head(Element):
 class OneLineTag(Element):
 
     def render(self, out_file):
-        # out_file.write("<{}>".format(self.tag))
         out_file.write(self._open_tag())
         out_file.write(self.contents[0])
-        # out_file.write(self.tag))
         out_file.write(self._close_tag())
+        out_file.write("\n")
 
     def append(self, content):
         raise NotImplementedError
