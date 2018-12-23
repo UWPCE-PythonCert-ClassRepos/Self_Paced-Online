4efec67c2e748264aeffb9891b0c4e5c8400c438
Ian Sahlberg <iansahlberg@gmail.com>
2018-12-22 20:35:31 -0800

    Added lucas function.

--- students/Sahlberg/Lesson2/series.py
+++ students/Sahlberg/Lesson2/series.py
@@ -6,3 +6,14 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 2) + fibonacci(n - 1)
+		
+def lucas(n):
+    """Returns the nth value of a fibonacci series starting at 2,1..."""
+
+    while n >= 0:
+        if n == 0:
+            return 2
+        elif n == 1:
+            return 1
+        else:
+            return lucas(n - 2) + lucas(n - 1)
