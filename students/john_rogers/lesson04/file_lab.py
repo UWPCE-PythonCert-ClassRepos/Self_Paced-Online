169b91f16aa78104738de196259385a94703b1e7
john r <johnroge@outlook.com>
2018-12-28 08:55:11 -0800

    skeleton created for file_lab.py exercise, lesson04.

---
+++ students/john_rogers/lesson04/file_lab.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+ungraded lab work for working with files
+Author: JohnR
+Version: .1
+Date: 12/28/2018
+Notes:
+"""
+
+
+def main():
+    """
+    1) print the full path for all files in the current directory, 1/line.
+    2) copy a file from a source to destination (no shutil or os copy)
+    3) make it work for any file size (don't read all into memory)
+    4) make it wor for any file type; use binary mode and a jpg file
+        NOTE: readline() has no meaning for binary files
+    5) test with both text files and binary files
+    :return:
+    """
+
+
+def dir_contents():
+    """
+    print out contents of current directory, one per line (full path)
+    :return: none
+    """
+
+
+def file_copy():
+    """
+    copy a file from source to destination
+    test with any size or file type
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    main()
