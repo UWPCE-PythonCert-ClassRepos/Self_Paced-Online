c43445bae04cc6fe7bf2658d9700a102b44358a6
John Rogers <johnroge@outlook.com>
2018-12-27 18:51:34 -0800

    Completed dict_lab.py in lesson04.

--- students/john_rogers/lesson04/dict_lab.py
+++ students/john_rogers/lesson04/dict_lab.py
@@ -2,7 +2,7 @@
 """
 Ungraded practice assignment for dictionaries and sets.
 Author: JohnR
-Version: .1
+Version: 1.0
 Date: 12/27/2018
 Notes:
 """
@@ -13,6 +13,19 @@ def main():
     Core script logic, global variables, various function calls
     :return:
     """
+
+    my_dictionary = dict_1()
+    print()
+    print('results of the first dictionary: ')
+    print(my_dictionary)
+    new_dict = dict_2(my_dictionary)
+    print()
+    print('second dictionary looks like this: ')
+    print(new_dict)
+    set_1()
+
+    print()
+    set_2()
 
 
 def dict_1():
@@ -27,10 +40,35 @@ def dict_1():
     Display whether or not mango is a value in the dict
     :return: Dictionary
     """
-    pass
+    db = {
+        'name': 'chris',
+        'city': 'seattle',
+        'cake': 'chocolate',
+        'car': 'Toyota',
+        'book': 'catcher in the rye',
+    }
+
+    print(db)
+    del db['cake']
+    print(db)
+    db['fruit'] = 'mango'
+    print(db)
+    print(db.keys())
+    print(db.values())
+    if 'cake' in db.keys():
+        print('yes, cake is in the dictionary')
+    else:
+        print('sorry, no cake in this dictionary')
+
+    if 'mango' in db.values():
+        print('si, tengo mango hoy')
+    else:
+        print('no tengo mango mi amigo')
+
+    return db
 
 
-def dict_2():
+def dict_2(data):
     """
     Use dictionary from dict_1 using the same keys but with the number
     of 't's in each value as the value; remember to check for upper
@@ -38,19 +76,51 @@ def dict_2():
     Display new dictionary.
     :return: New dictionary with updated values
     """
-    pass
+    db2 = {}
+    for k, v in data.items():
+        t_count = v.lower().count('t')
+        db2[k] = t_count
+
+    return db2
 
 
 def set_1():
     """
-    Create sets s2, s3 and s4 that contain numbers from 0-20, divisible
+    Create sets s2, s3 and s4 that contain numbers from 0-40, divisible
     by 2, 3 and 4.
     Display the sets.
     Display if s3 is a subset of s2 (false)
     Display if s4 is a subset of s2 (true)
     :return: None
     """
-    pass
+    s2 = set()
+    for i in range(1, 21):
+        if i % 2 == 0:
+            s2.update([i])
+
+    s3 = set()
+    for i in range(1, 21):
+        if i % 3 == 0:
+            s3.update([i])
+
+    s4 = set()
+    for i in range(1, 21):
+        if i % 4 == 0:
+            s4.update([i])
+
+    print(s2)
+    print(s3)
+    print(s4)
+
+    if s3.issubset(s2):
+        print('True')
+    else:
+        print('false')
+
+    if s4.issubset(s2):
+        print('Yes, s4 is in s2.')
+    else:
+        print('negative')
 
 
 def set_2():
@@ -60,7 +130,25 @@ def set_2():
     Display the union and intersection of the two sets.
     :return:
     """
-    pass
+    sp = set()
+    for c in 'python':
+        sp.update(c)
+    print(sp)
+    sp.add('i')
+    print()
+    print(sp)
+
+    fr_set = set()
+    for c in 'marathon':
+        fr_set.update(c)
+
+    print(fr_set)
+
+    foo = fr_set.intersection(sp)
+    print(foo)
+
+    bar = sp.union(fr_set)
+    print(bar)
 
 
 if __name__ == '__main__':
