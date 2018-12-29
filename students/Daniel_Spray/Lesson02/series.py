97cf4db9cb54aba11ae2f91837c9d25daedcd51a
dfspray <daniel_spray@comcast.net>
2018-12-28 17:32:47 -0800

    Created a common function for both lucas and fibonacci functions

--- students/Daniel_Spray/Lesson02/series.py
+++ students/Daniel_Spray/Lesson02/series.py
@@ -1,13 +1,10 @@
 def fibonacci(n):
     l = [0,1]
-    m=1
-    while len(l)<n:
-        y=l[m-1]+l[m]
-        l.append(y)
-        m+=1
-    print(l[n-1])
+    commonfunction(l,n)
 def lucas(n):
     l = [2,1]
+    commonfunction(l,n)
+def commonfunction(l,n):
     m=1
     while len(l)<n:
         y=l[m-1]+l[m]
