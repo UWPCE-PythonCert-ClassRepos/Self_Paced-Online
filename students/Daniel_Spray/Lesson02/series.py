ed62f58134301ebd1b383ba5653571d4553a6853
dfspray <daniel_spray@comcast.net>
2018-12-28 17:27:43 -0800

    Added lucas function to series.py

--- students/Daniel_Spray/Lesson02/series.py
+++ students/Daniel_Spray/Lesson02/series.py
@@ -6,3 +6,11 @@ def fibonacci(n):
         l.append(y)
         m+=1
     print(l[n-1])
+def lucas(n):
+    l = [2,1]
+    m=1
+    while len(l)<n:
+        y=l[m-1]+l[m]
+        l.append(y)
+        m+=1
+    print(l[n-1])
