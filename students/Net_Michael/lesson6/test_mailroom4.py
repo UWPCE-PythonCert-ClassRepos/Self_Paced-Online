cab237841c80de9efcd90b0757739a4809389c98
Marie Curie <Nettaimam@gmail.com>
2018-12-30 09:51:34 -0800

    test_mailroom4.py - tests all the donors information

--- students/Net_Michael/lesson6/test_mailroom4.py
+++ students/Net_Michael/lesson6/test_mailroom4.py
@@ -7,22 +7,41 @@ from mailroom4 import *
 import pytest
 
 
-def test_one():
+def test_exit():
     with pytest.raises(SystemExit):
         quit_sel()
 
-# in this computer donor_list is the file that contains the donors list and their
-# contribution in text file. given that folder name, from any working directory
-# test if this function can find it.
+# test if directory_path can find the directory path of donor_list (
+# folder containing all donor text files)
+def test_directory_path():
+    test_path = directory_path("donor_list")
+    directories = test_path.split("\\")
+    assert "donor_list" == directories[-1]
 
-def test_two():
-    test_values = donor_list_dir("donor_list").split("\\")
-    assert "donor_list" == test_values[len(test_values)-1]
-
-# tests if open_donor_file can open the donations by checking the his first
-# donation which is 300
-
-def test_three():
-    test_path = os.path.join(donor_list_dir("donor_list"), "Paul_Allen.txt")
-    test_donations = open_donor_file(test_path).split(", ")
+# test if open_donor_file can open and extract list of donations form a donor
+def test_open_donor_file():
+    dir_path = directory_path("donor_list")
+    file_path = os.path.join(dir_path, "Paul_Allen.txt")
+    donation_info = open_donor_file(file_path)
+    test_donations = donation_info.split(", ")
     assert eval(test_donations[0]) == 300
+
+
+# given folder name holding the text files of donors, test if directory_path can find the whole path in the machine
+
+def test_one_donor_info():
+# get directory information
+    dir_path = directory_path("donor_list")
+    file_name = "Paul_Allen.txt"
+    donor_dict = one_donor_info(file_name = file_name, donor_folder = dir_path)
+    donor_first_name = list(donor_dict.values())[0][0]
+    assert donor_first_name == "Paul"
+
+
+
+def test_all_donors_info():
+    # get directory information
+    dir_path = directory_path("donor_list")
+    all_donors = all_donors_info(dir_path)
+    donor_list = list(all_donors.keys())
+    assert "Paul Allen" in donor_list
