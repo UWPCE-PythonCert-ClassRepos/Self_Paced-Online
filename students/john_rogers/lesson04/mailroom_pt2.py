e2f789e8e23c352f644f81a0a19760b6a65a137b
John Rogers <johnroge@outlook.com>
2018-12-29 17:57:29 -0800

    Updates to mailroom_pt2.py in lesson04; working menu in place.

--- students/john_rogers/lesson04/mailroom_pt2.py
+++ students/john_rogers/lesson04/mailroom_pt2.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 """
-mailroom_pt2.py -
-    1) Update to use dictionaries and a menu system
-    Try to use a dict and the .format() method to do the letter as
-    one big template rather than building up a big string in parts.
-    2) Write a letter to each person in a seperate file on disk.
-         1 - Send a Thank You
-         2 - Create a Report
-         3 - Send letters to everyone
-         4 - Quit
-    The letters should each get a unique file name – derived from the
-    donor’s name, and maybe a date.
-    After running the “send letters to everyone” option, you should get
-    a bunch of new files in the working dir – one for each donor.
+mailroom.py -
+    1) prompt user for 3 actions - Send thank you, create report or quit
 Author: JohnR
-Version: .1
-Last updated: 12/28/2018
-Notes: lesson 4 re-write using menus, dict and writing to files
+Version: 1.3
+Last updated: 12/29/2018
+Notes: Menu is currently working, but is part of main()
+
+  v2.0 requirements:
+    1) use dict where appropriate
+    2) create a dict-based menu
+    3) create a single template using .format(**d) method for the letter
+    4) write a full set of letters to everyone on disk in individual files
+    5) add a function that goes through all donors, generates a thank you
+        letter, and writes it to disk as a text file.
+        A) use donor name and date for file name
 """
-# TODO: Use original mailroom.py to create new skeleton
 
 
 def main():
@@ -28,8 +25,6 @@ def main():
     :return: none
     """
 
-    # Nested dictionaries seems like the most intuitive data structure
-    # for this type of record keeping
     db = {'sting': {'d1': 13.45,
                     'd2': 214.34,
                     'd3': 453.23,
@@ -52,38 +47,40 @@ def main():
                     'd4': 456.23,
                     },
          }
+    main_prompt = (
+        "\nWelcome to the main menu!\n"
+        "Please pick a number from the following:\n"
+        "1: exit the program\n"
+        "2: check donor list and become a donor\n"
+        "3: create an interactive report of current donors\n"
+        "4: write a thank you note for each donor and write to file\n"
+        ">>>\n"
+    )
 
+    main_dispatch = {
+        '1': exit_menu,
+        '2': thank_you,
+        '3': create_report,
+        '4': write_report,
+    }
+
+    # Call the main menu - currently uses a static variable, db
     while True:
-        choice = get_input()
+        selection = input(main_prompt)
 
-        if choice == 1:
-            print('Exiting program.')
-            exit()
-        elif choice == 2:
-            thank_you(db)
-        elif choice == 3:
-            create_report(db)
-
-
-def get_input():
-    """
-    Get user input
-    :return: User choice from menu
-    """
-    print()
-    print('Please choose from the following menu: ')
-    print('1 Quit')
-    print('2 Send a thank you')
-    print('3 Create a report')
-
-    # TODO: Time permitting, check if string
-    while True:
-        cmd = input('Please pick a number between 1 and 3: ')
-        cmd = int(cmd)
-        if cmd not in range(1, 4):
-            break
+        if selection in main_dispatch:
+            main_dispatch.get(selection)(db)
         else:
-            return cmd
+            print('Please enter a number between 1 and 4')
+
+
+def write_report():
+    pass
+
+
+def exit_menu():
+    print('Exiting program -')
+    raise SystemExit
 
 
 def thank_you(names):
@@ -138,20 +135,28 @@ def create_report(data):
     List donor name, number of donations and average donation amount.
     :return: none
     """
-    print()
-    print('Donor Name       | Total Given | Num Gifts | Avg Gift Amount')
-    print('-' * 60)
 
-    # Not the prettiest formatting, but works well enough for v1
+    # Create a list from the donor dictionary so we can sort by size
+    d_list = []
     for donor in data:
         total_amount = round(float(sum(data[donor].values())), 2)
         num_donations = round(len(data[donor].values()), 2)
         avg_donation = total_amount / num_donations
         avg_donation = round(avg_donation, 2)
-        dollar = '$'
 
-        print(f'{donor.capitalize():20} {dollar:1} {total_amount:3n}'
-              f' {num_donations:12n} {avg_donation:12n}')
+        d_list.append([[donor], [total_amount, num_donations, avg_donation]])
+
+    # Sort the list from largest to smallest donation
+    d_list.sort(key=lambda x: x[1])
+    d_list.reverse()
+
+    # Print the sorted list in a nice format
+    print()
+    print('Donor Name       | Total Given | Num Gifts | Avg Gift Amount')
+    print('-' * 60)
+
+    for d in d_list:
+        print(f'{d[0][0]:<17} ${d[1][0]:^15} {d[1][1]:<10} ${d[1][2]}')
 
 
 if __name__ == '__main__':
