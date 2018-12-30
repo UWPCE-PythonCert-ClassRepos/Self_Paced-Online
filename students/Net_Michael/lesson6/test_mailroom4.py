e8691b9446419d498e7e1de4fa74907c607a5389
Marie Curie <Nettaimam@gmail.com>
2018-12-29 22:26:18 -0800

    test_mailroom4.py - test open_donor_file - given Paul Allen text file path test if his first donation was 300

--- students/Net_Michael/lesson6/test_mailroom4.py
+++ students/Net_Michael/lesson6/test_mailroom4.py
@@ -11,7 +11,18 @@ def test_one():
     with pytest.raises(SystemExit):
         quit_sel()
 
+# in this computer donor_list is the file that contains the donors list and their
+# contribution in text file. given that folder name, from any working directory
+# test if this function can find it.
 
 def test_two():
     test_values = donor_list_dir("donor_list").split("\\")
     assert "donor_list" == test_values[len(test_values)-1]
+
+# tests if open_donor_file can open the donations by checking the his first
+# donation which is 300
+
+def test_three():
+    test_path = os.path.join(donor_list_dir("donor_list"), "Paul_Allen.txt")
+    test_donations = open_donor_file(test_path).split(", ")
+    assert eval(test_donations[0]) == 300
