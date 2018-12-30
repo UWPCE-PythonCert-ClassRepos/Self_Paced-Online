89218fb893f895e5b50ae44f3e24643030a0e343
Marie Curie <Nettaimam@gmail.com>
2018-12-29 21:50:22 -0800

    test_mailroom4.py - test donor_list_dir - given folder name, in this case donor_list, containing donor txt files can find its directory.

--- students/Net_Michael/lesson6/test_mailroom4.py
+++ students/Net_Michael/lesson6/test_mailroom4.py
@@ -10,3 +10,8 @@ import pytest
 def test_one():
     with pytest.raises(SystemExit):
         quit_sel()
+
+
+def test_two():
+    test_values = donor_list_dir("donor_list").split("\\")
+    assert "donor_list" == test_values[len(test_values)-1]
