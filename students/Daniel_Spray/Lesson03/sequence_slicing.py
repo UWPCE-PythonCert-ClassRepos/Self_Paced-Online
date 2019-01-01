653292c4b531bafbc84150c5e370254b3852ab9e
dfspray <daniel_spray@comcast.net>
2018-12-31 21:28:43 -0800

    Added the rearranged thirds ffunction to the sequence_slicing.py file

--- students/Daniel_Spray/Lesson03/sequence_slicing.py
+++ students/Daniel_Spray/Lesson03/sequence_slicing.py
@@ -14,8 +14,10 @@ def reversed(seq):
     a_new_sequence4 = seq[::-1]
     return a_new_sequence4
 
-#def thirds(seq):
-    #return a_new_sequence5
+def thirds(seq):
+    a=int(len(seq)/3)
+    a_new_sequence5 = seq[a:]+seq[:a]
+    return a_new_sequence5
 
 a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
@@ -28,5 +30,5 @@ assert middle_every_other(a_string) == " sas"
 assert middle_every_other(a_tuple) == ()
 assert reversed(a_string) == "gnirts a si siht"
 assert reversed(a_tuple) == (32, 5, 12, 13, 54, 2)
-assert exchange_first_last(a_string) == "ghis is a strint"
-assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+assert thirds(a_string) == "is a stringthis "
+assert thirds(a_tuple) == (13, 12, 5, 32, 2, 54)
