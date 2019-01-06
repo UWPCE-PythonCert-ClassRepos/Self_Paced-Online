d27cd70fa7b51d821cf6a83261fbbd7d4e5b1be9
Ian Sahlberg <iansahlberg@gmail.com>
2019-01-06 14:02:49 -0800

    Finalizing edits.

--- students/Sahlberg/Lesson3/mailroom.py
+++ students/Sahlberg/Lesson3/mailroom.py
@@ -12,44 +12,74 @@ donor = {'bob johnson': [150.00],
          'jon jacob':[5000.00, 5.00]
          }
 
-def donate():
+def donate(name, dictionary):
     """Appends a donation amount to a given donor."""
     amount = int(input('How much did they donate?'))
-    return amount, donor[option].append(amount)
+    return amount, dictionary[name].append(amount)
 
-def print_list():
+def print_list(dictionary):
     """Prints our the donor list"""
-    return print(list(donor.keys()))
+    return print(list(dictionary.keys()))
 
 def user_options():
     """Main user options menu choices."""
     user_action = input('Please choose from the following options: Send a Thank You, Create a Report, quit')
     return user_action
 
-def email():
-    return print(f'\n\nThank you {option} for the generous donation of $ {amount}. We appreciate your generosity.\n\nSincerely, \n\nThe Helping R Us Team\n\n')
+def email(name, amount):
+    return print(f'\n\nThank you {name.title()} for the generous donation of $ {amount:.2f}. We appreciate your generosity.\n\nSincerely, \n\nThe Helping R Us Team\n\n')
 
-print(list(donor.keys()))
+def quit():
+    return print('Until next time!')
 
-user_action = user_options()
+def sort_and_stats_dict(dictionary):
+    pre_sort = {k: [sum(v), len(v), round((sum(v) / len(v)), 0)] for k, v in dictionary.items()}
+    print(pre_sort)
+    donor_sorted = sorted(list(pre_sort.items()), key=lambda x: x[1], reverse=True)
+    return donor_sorted
 
-while user_action == 'Send a Thank You':
-    option = input('Enter an option: List (to give a list of names), A donors name')
-    if option.lower() == 'list':
-        print_list()
-    else:
-        if option.lower() in donor:
-            amount = donate()[0]
-            print(donor)
-            email()
+def print_report(dictionary):
+    print('Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations')
+    print('----------------------------------------------------------------------------------------')
+    count = 0
+    for donor in dictionary:
+        print(
+            f'{dictionary[count][0]:<20}$    {dictionary[count][1][0]:<23.2f}{dictionary[count][1][1]:<15}    ${dictionary[count][1][2]:>12.0f}')
+        count += 1
 
+if __name__== '__main__':
+    user_action = user_options()
+
+    if user_action.lower() == 'quit':
+        quit()
+
+    while user_action == 'Send a Thank You':
+        option = input('Enter an option: List (to give a list of names), A donors name, \"quit\" , to quit.')
+        if option.lower() =='quit':
+            print('Bye!')
+            break
+        if option.lower() == 'list':
+            print_list(donor)
         else:
-            donor[option] = []
-            amount = donate()[0]
-            print(donor)
-            email()
+            if option.lower() in donor:
+                amount = int(donate(option,donor)[0])
+                print(donor)
+                email(option,amount)
 
+            else:
+                donor[option] = []
+                amount = int(donate(option,donor)[0])
+                print(donor)
+                email(option,amount)
 
+    while user_action == 'Create a Report':
+        donor_sorted = sort_and_stats_dict(donor)
+        print('Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations')
+        print('----------------------------------------------------------------------------------------')
+        count = 0
+        for donor in donor_sorted:
+            print(
+                f'{donor_sorted[count][0]:<20}$    {donor_sorted[count][1][0]:<23.2f}{donor_sorted[count][1][1]:<15}    ${donor_sorted[count][1][2]:>12.0f}')
+            count += 1
+        break
 
-
-print(donor)
