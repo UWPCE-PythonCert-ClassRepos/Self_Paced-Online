5c659ee792b348e2d23e3496c14206fe3beab457
LouReis <lou.reis@gmx.com>
2019-01-10 13:40:02 -0700

    Initial draft of circle class, created basic class to calculate area of circle, tested and works

---
+++ students/LouReis/Lesson08/test_circle_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.7
+# circle_class.py
+# Coded by LouReis
+
+class rectangle():
+    def __init__(self,breadth,length):
+        self.breadth=breadth
+        self.length=length
+    def area(self):
+        return self.breadth*self.length
+a=int(input("Enter length of rectangle: "))
+b=int(input("Enter breadth of rectangle: "))
+obj=rectangle(a,b)
+print("Area of rectangle:",obj.area())
+
+print()
