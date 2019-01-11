5d0df05945115b76f909138c482ddea016f15b99
dfspray <daniel_spray@comcast.net>
2019-01-11 12:04:12 -0800

    cleaned up strformat_lab.py and created the table for mailroom.py

---
+++ students/Daniel_Spray/Lesson03/mailroom.py
@@ -0,0 +1,26 @@
+def table(a):
+    print("{:<19}{:<1}{:^9}{:<1}{:^9}{:<1}{:^9}".format('Donor Name','|','Total Given','|','Num Gifts','|','Average Gift')
+    for row in a:
+        print("{:<20}{:<1}{:>9}{:>9}{:<1}{:>9}".format(*row))
+
+def main():
+    average_gift
+    
+    donors = ("William Gates, III","Mark Zuckerberg","Jeff Bezos","Paul Allen")
+    
+    latest_donation = (653784.49,16396.10,877.33,708.42)
+    
+    total_donated = 
+    
+    number of donations = 
+    
+    data = (["William Gates, III","$",latest_donation,num_gifts,"$",average_gift],
+    ["Mark Zuckerberg","$",latest_donation,num_gifts,"$",average_gift],
+    ["Jeff Bezos","$",latest_donation,num_gifts,"$",average_gift],
+    ["Paul Allen","$",latest_donation,num_gifts,"$",average_gift])
+    
+    print("This program will hopefully help you send some useless messages",
+    "Type the corresponding number to select from the following list:")
+    
+    
+    table(Data)
--- students/Daniel_Spray/Lesson03/strformat_lab.py
+++ students/Daniel_Spray/Lesson03/strformat_lab.py
@@ -1,32 +1,40 @@
-def task1(inputs):
+def task1(inputs=(2,123.4567,10000,12345.67)):
     a=str(inputs[0]).zfill(3)
     b=round(inputs[1],2)
     c="{0:.2e}".format(inputs[2])
     d="{0:.2e}".format(inputs[3])
     e=a,b,c,d
+    print("file_{}: {}, {}, {}".format(a,b,c,d))
     return e
-    #print("file_{}: {}, {}, {}".format(a,b,c,d))
 
 def task2(e):
     string=f"file_{e[0]}: {e[1]}, {e[2]}, {e[3]}"
     print(string)
     return string
 
-def task3(f):
+def task3(f=(1,2,3)):
     g="{:d}, "*(len(f)-1)+"{:d}"
     h=g.format(*f)
     string = "the {:d} numbers are: {}".format(len(f), h)
-    return string
     print(string)
+    return string
 
-def task4(i):
+def task4(i=(4, 30, 2017, 2, 27)):
     j=str(i[3]).zfill(2)+" "+str(i[4]).zfill(2)+" "+str(i[2]).zfill(4)+" "+str(i[0]).zfill(2)+" "+str(i[1]).zfill(2)
     print(j)
     return j
 
-def task5(k):
+def task5(k=['oranges', 1.3, 'lemons', 1.1]):
     string=f"The weight of an {k[0][:-1]} is {k[1]} and the weight of a {k[2][:-1]} is {k[3]}"
     string2=f"The weight of an {(k[0][:-1]).upper()} is {k[1]*1.2} and the weight of a {(k[2][:-1]).upper()} is {k[3]*1.2}"
     print(string)
     print(string2)
-    return(string)
+    return string
+
+
+def task61(l=(['Daniel', '55', '$88.09'],['Alex','27', '$624.26'],['James','35', '$55555.01'],['Myles','15', '$753354.34'])):
+    for row in l:
+        print("{:<20}{:<20}{:<20}".format(*row))
+
+def task62(m=(1,2,3,4,5,6,7,8,9,0)):
+    print(("{:<5}"*len(m)).format(*m))
