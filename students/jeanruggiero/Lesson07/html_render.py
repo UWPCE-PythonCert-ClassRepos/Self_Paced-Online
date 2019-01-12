7d1363ef30f85f34fc1ca55fe25ff47d8a749055
jeanruggiero <jeanruggiero@gmail.com>
2019-01-11 19:44:43 -0800

    Done with step 2.

--- students/jeanruggiero/Lesson07/html_render.py
+++ students/jeanruggiero/Lesson07/html_render.py
@@ -3,31 +3,76 @@
 class Element():
 
     indent_size = 4
-    tag_types = ['html', 'head', 'meta', 'title']
+    tag_types = ['html', 'body', 'p', 'head', 'meta', 'title']
 
-    def __init__(self, content=[], tag_type=0):
-        if not content:
+    tag_type = 0
+
+    def __init__(self, content=None):
+        if content is None:
             self.content = []
         else:
             self.content = [content]
-        self.tag_type = tag_type
 
     def __str__(self):
-        return ''.join([self.tag(c) for c in self.content])
+        return self.tag()
 
     def append(self,newcontent):
-        """Append new string to element's content list."""
+        """Append new string or element to element's content list."""
         self.content.append(newcontent)
 
     def render(self, out_file, cur_ind=0):
         """
         Apply appropriate tags and indentation and output Element content to
-        an open writable file-like object.
+        an open writable object.
         """
-        out_file.write(''.join([self.tag(c,cur_ind) for c in self.content]))
+        out_file.write(self.tag(cur_ind))
 
-    def tag(self, c, cur_ind=0):
-        """Apply tags and in-tag indentation to a string."""
-        t = self.tag_types[self.tag_type]
-        ind = self.indent_size
-        return '<' + t + '>\n' + ' '*(ind) + c + '\n<\\' + t + '>\n'
+    def apply_tag(self, cur_ind=0, tlist=None):
+        """Return a list of html-tagged blocks of text. Apply tags and in-tag
+        indentation to a string or Element object."""
+        if tlist is None:
+            tlist = []
+
+        tlist.append(self.opentag(cur_ind))
+
+        for c in self.content:
+            if isinstance(c, Element):
+                c.apply_tag(cur_ind+1, tlist)
+            else:
+                tlist.append(self.line(c,cur_ind))
+        tlist.append(self.closetag(cur_ind))
+        return tlist
+
+    def tag(self, cur_ind=0):
+        """Returns a string of html-tagged text. Wrapper on apply_tag
+        that returns a string instead of a list."""
+        return ''.join(self.apply_tag(cur_ind))
+
+    def opentag(self, ci):
+        """Returns an open tag with indentation ci."""
+        return ci * self.indent_size * ' ' + '<' + \
+            self.tag_types[self.tag_type] + '>\n'
+
+    def closetag(self, ci):
+        """Returns a close tag with indentation ci."""
+        return ci * self.indent_size * ' ' + '</' + \
+            self.tag_types[self.tag_type] + '>\n'
+
+    def line(self, c, ci):
+        """Returns a block of text with indentation and newlines."""
+        return (ci + 1)*self.indent_size * ' ' + c + '\n'
+
+
+
+class Html(Element):
+
+    tag_type = 0
+
+
+class Body(Element):
+
+    tag_type = 1
+
+class P(Element):
+
+    tag_type = 2
--- students/jeanruggiero/Lesson07/test_html_render.py
+++ students/jeanruggiero/Lesson07/test_html_render.py
@@ -3,14 +3,22 @@
 import html_render as hr
 from io import StringIO
 
-element = hr.Element('some text')
+element = hr.Element()
+element.append('some text')
+# body = hr.Body()
+# body.append('body text')
+# p1 = hr.P('para1')
+# body.append(p1)
+# body.append(hr.P('para2'))
+# element.append(body)
 
 def test_init():
     assert element.content == ['some text']
     assert element.tag_type == 0
 
 def test_str():
-    assert element.__str__() == '<html>\n    some text\n<\\html>\n'
+    assert element.__str__() == '<html>\n' + element.indent_size*' ' + \
+        'some text\n</html>\n'
 
 def test_append():
     element.append('stuff')
@@ -19,8 +27,32 @@ def test_append():
 def test_render():
     f = StringIO()
     element.render(f)
-    assert f.getvalue() == '<html>\n    some text\n<\\html>\n' + \
-                           '<html>\n    stuff\n<\\html>\n'
+    print(element)
+    assert f.getvalue() == '<html>\n' + element.indent_size*' ' + \
+        'some text\n' + element.indent_size*' ' + 'stuff\n</html>\n'
+
+def test_apply_tag():
+    assert element.apply_tag() == [
+        '<html>\n',
+        element.indent_size*' ' + 'some text\n',
+        element.indent_size*' ' + 'stuff\n',
+        '</html>\n'
+        ]
 
 def test_tag():
-    assert element.tag('line') == '<html>\n    line\n<\\html>\n'
+    assert element.tag() == '<html>\n' + element.indent_size*' ' + \
+        'some text\n' + element.indent_size*' ' + 'stuff\n</html>\n'
+
+def test_opentag():
+    assert element.opentag(0) == '<html>\n'
+    assert element.opentag(1) == element.indent_size*' ' + '<html>\n'
+
+def test_closetag():
+    assert element.closetag(0) == '</html>\n'
+    assert element.closetag(1) == element.indent_size*' ' + '</html>\n'
+
+def test_line():
+    assert element.line('text line',0) == (1)*element.indent_size * ' ' + \
+        'text line\n'
+    assert element.line('text line',1) == (2)*element.indent_size * ' ' + \
+        'text line\n'
