d0cb46642e40a86845e155df8585672113fbf62c
jeanruggiero <jeanruggiero@gmail.com>
2019-01-15 00:05:10 -0800

    Ready to submit circle assignment

--- students/jeanruggiero/Lesson08/circle.py
+++ students/jeanruggiero/Lesson08/circle.py
@@ -1,14 +1,70 @@
 #!/usr/bin/env python3
 
 import math
+from functools import total_ordering
 
+class RadiusError(Exception): pass
+
+@total_ordering
 class Circle():
     """
     Circle class represents a simple circle.
     """
 
     def __init__(self, radius):
+        # Check provided radius is numeric and positive
+        try:
+            int(radius)
+        except ValueError:
+            raise RadiusError("Radius must be a number!")
+        if radius  < 0:
+            raise RadiusError("Radius can't be negative!")
         self.radius = radius
+
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
+
+    def __str__(self):
+        return 'Circle of radius: {:.6f}'.format(self.radius)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __iadd__(self, other):
+        self.radius += other.radius
+        return self
+
+    def __sub__(self,other):
+        # Check radius remains positive
+        if self.radius - other.radius < 0:
+            raise RadiusError("Radius can't be negative!")
+        return Circle(self.radius - other.radius)
+
+    def __isub__(self,other):
+        # Check radius remains positive
+        if self.radius - other.radius < 0:
+            raise RadiusError("Radius can't be negative!")
+        self.radius -= other.radius
+        return self
+
+    def __mul__(self, value):
+        return Circle(self.radius*value)
+
+    def __imul__(self, value):
+        self.radius *= value
+        return self
+
+    def __rmul__(self,value):
+        return self*value
+
+    def __truediv__(self,value):
+        return Circle(self.radius/value)
 
     @property
     def radius(self):
@@ -29,3 +85,8 @@ class Circle():
     @property
     def area(self):
         return math.pi * self.radius**2
+
+    @classmethod
+    def from_diameter(cls, value):
+        """Create instance of circle from diamater."""
+        return cls(value/2)
--- students/jeanruggiero/Lesson08/test_circle.py
+++ students/jeanruggiero/Lesson08/test_circle.py
@@ -6,9 +6,79 @@ import math
 import circle
 
 c1 = circle.Circle(5)
+c2 = circle.Circle(2)
+c3 = circle.Circle(2)
+
+circles = [circle.Circle(2), circle.Circle(1), circle.Circle(4),
+    circle.Circle(8), circle.Circle(0.5)]
 
 def test_init():
     assert c1.radius == 5
+
+    with pytest.raises(circle.RadiusError):
+        circle.Circle(-5)
+
+    with pytest.raises(circle.RadiusError):
+        circle.Circle('not a number')
+
+def test_repr():
+    assert c1.__repr__() == 'Circle(5)'
+
+def test_str():
+    assert c1.__str__() == 'Circle of radius: 5.000000'
+
+def test_eq():
+    assert c2 == c3
+    assert c2 != c1
+
+def test_lt():
+    assert c2 < c1
+    assert c2 <= c3
+    assert c2 >= c2
+    assert c1 > c3
+    circles.sort()
+    assert circles == [circle.Circle(0.5), circle.Circle(1), circle.Circle(2),
+        circle.Circle(4), circle.Circle(8)]
+
+def test_add():
+    assert c1 + c2 == circle.Circle(7)
+
+def test_iadd():
+    c4 = circle.Circle(5)
+    c5 = circle.Circle(2)
+    c4 += c5
+    assert c4 == circle.Circle(7)
+
+def test_sub():
+    assert c1 - c2 == circle.Circle(3)
+
+    with pytest.raises(circle.RadiusError):
+        c2 - c1
+
+def test_isub():
+    c4 = circle.Circle(5)
+    c5 = circle.Circle(2)
+    c4 -= c5
+    assert c4 == circle.Circle(3)
+
+    c6 = circle.Circle(5)
+    c7 = circle.Circle(2)
+    with pytest.raises(circle.RadiusError):
+        c7 -= c6
+    assert c7 == circle.Circle(2)
+
+def test_mul():
+    assert c1*2 == circle.Circle(10)
+    assert 3*c2 == circle.Circle(6)
+    assert 4*c1 == c1*4
+
+def test_imul():
+    c5 = circle.Circle(2)
+    c5 *= 2
+    assert c5 == circle.Circle(4)
+
+def test_truediv():
+    assert c2/2 == circle.Circle(1)
 
 def test_properties():
     assert c1.area == 25*math.pi
