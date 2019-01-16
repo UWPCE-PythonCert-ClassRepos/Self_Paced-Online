ee684216662c063b16a9803d102b9e9f31a96d28
Victor A Medina <vmedina1014@gmail.com>
2019-01-16 14:29:01 -0800

    Update mailroom.py
    changed format of mailroom.py to have individual functions for better usability.

--- students/V_Medina/lesson_03/mailroom.py
+++ students/V_Medina/lesson_03/mailroom.py
@@ -4,47 +4,67 @@ Victor Medina
 1/12/2019
 Lesson 3: String Formatting
 """
-Donors = [
-    ['Victor', 100, 20, 30],
-    ['John', 12],
-    ['Kevin', 91, 32],
-    ['Kelly', 5, 21],
-    ['Matt', 75, 20],
-    ['Josh', 31, 3],
-    ['Micah', 120]
-]
 
-response = input('Do you want to: Send a Thank you, Create a Report or quit? ')
 
-# If response is send a thank you
-if response == 'send a thank you':
+def send_thank_you(Donors):
+    print(Donors)
     name = input("What's the name? ")
     if name == 'list':
         for donor in Donors:
             print(donor[0])
         name = input("What's the name? ")
-    if any(name not in donor for donor in Donors):
-        Donors.append([name])
+    if any(name in donor_info for donor_info in Donors):
         donor = name
     else:
+        Donors.append([name])
         donor = name
+
     donation_amount = int(input('Donation Amount? '))
     for index, person in enumerate(Donors):
         if person[0] == donor:
             Donors[index].append(donation_amount)
     print(Donors)
-    print('Thank you {} for your generous donation of {} dollars!'.format(donor,donation_amount))
-elif response == 'create a report':
-    Donor_report = []
+    print('Thank you {} for your generous donation of {} dollars!'.format(donor, donation_amount))
+    return Donors
+
+
+def create_report(Donors):
+    donor_report = []
     for donor in Donors:
-        Donor_total = 0
+        donor_total = 0
         amount_count = 0
         for amount in donor:
             if type(amount) == str:
                 pass
             else:
-                Donor_total += int(amount)
+                donor_total += int(amount)
                 amount_count += 1
-        avg_donation = Donor_total / amount_count
-        Donor_report.append([donor[0], Donor_total, amount_count, avg_donation])
-    print(Donor_report)
+        avg_donation = donor_total / amount_count
+        donor_report.append([donor[0], donor_total, amount_count, avg_donation])
+    print('Donor Name      | Total Given |   Num Gifts  | Average Gift')
+    print('------------------------------------------------------------')
+    for donor in donor_report:
+        print('{:<15} ${:>13} {:>14} ${:>13,.2f}'.format(*donor))
+    return None
+
+
+if __name__ == "__main__":
+    Donors = [
+        ['Victor', 100, 20, 30],
+        ['John', 12],
+        ['Kevin', 91, 32],
+        ['Kelly', 5, 21],
+        ['Matt', 75, 20],
+        ['Josh', 31, 3],
+        ['Micah', 120]]
+    # initial request
+    response = input('Do you want to: "Send a Thank you", "Create a Report" or "quit"? ')
+    while response.lower() != 'quit':
+        # while loop to allow for multiple inquiries
+        if response.lower() == 'send a thank you':
+            send_thank_you(Donors)
+        elif response.lower() == 'create a report':
+            create_report(Donors)
+        else:
+            print('Wrong input')
+        response = input('Do you want to: Send a Thank you, Create a Report or quit? ')
