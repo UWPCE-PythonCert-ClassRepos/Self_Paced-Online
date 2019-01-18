189254f9ca7366ee98daed49f0fed0360208f335
Alex Filson <alexdfilson@gmail.com>
2019-01-18 10:37:16 -0800

    Added lucas series function and print statement.

--- students/alheadstrong/lesson_2/series.py
+++ students/alheadstrong/lesson_2/series.py
@@ -11,3 +11,16 @@ def fibonacci(n):
     else:
         return(fibonacci(n-1)+fibonacci(n-2))
 
+def lucas(n):
+    '''recursive function that returns nth number in fibonacci sequence'''
+    if n == 0:
+        return(2)
+    if n == 1:
+        return(1)
+    else:
+        return(lucas(n-1)+lucas(n-2))
+
+for i in range(20):
+    print(fibonacci(i))
+for i in range(20):
+    print(lucas(i))
