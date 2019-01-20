2554af32fba13a4d0400be17b8d6a5ca82026d63
Alex Filson <alexdfilson@gmail.com>
2019-01-19 16:27:31 -0800

    Added the remaining series, organized each series under a function, corrected syntax errors and changed each function call to create a sliced version of orignal list

--- students/alheadstrong/lesson_3/list_lab.py
+++ students/alheadstrong/lesson_3/list_lab.py
@@ -4,33 +4,86 @@ List Lab for Lesson 3
 Py210, Online Self-Paced
 '''
 
-fruit = ['Apples','Pears','Oranges','Peaches']
+def series1(fruit):
+    '''Display and add to a list of fruit based on user input'''
+    seriesheader("Series 1")
+    print(fruit)
+    newfruit = input('Enter the name of a fruit to be added:')
+    fruit.append(newfruit) #Add item to end of list
+    print(fruit)
+    num = int(input('Enter a number:'))-1
+    print('The fruit at number {} is {}.'.format(num+1,fruit[num]))
 
-print(fruit)
+    newfruit = input('Add a fruit to the beginning of the list:')
+    newfruitlist = [newfruit] #Seeing an type error when on one line.
+    fruit = newfruitlist + fruit
+    print(fruit)
 
-newfruit = input('Enter the name of a fruit to be added:')
+    newfruit = input('Add another fruit to the beginning of the list:')
+    fruit.insert(0,newfruit)
+    print(fruit)
 
-fruit.append(newfruit)
+    print("These are all of the fruit that begin with 'P':")
+    for i in fruit:
+        if i[0].lower() == 'p':
+            print (i)
 
-print(fruit)
+def series2(fruit):
+    '''Display and remove items from list based on user input'''
+    seriesheader("Series 2")
+    print(fruit)
+    fruit.pop() #Removes last item
+    print(fruit)
+    dfruit = input("Select a fruit to remove:")
+    fruit.remove(dfruit) # No exception handling, see below
+    fruit = fruit*2
+    length = len(fruit)
+    while length == len(fruit):
+        dfruit = input("Select a fruit to remove:")
+        for f in fruit:
+            if f == dfruit:
+                fruit.remove(dfruit)
+        if length == len(fruit):
+            print("Fruit not found!")
+            print("Current fruit: {}".format(fruit))
+    print(fruit)
 
-num = input('Enter a number:')-1
 
-print('The fruit at number {} is {}.'.format(num+1,fruit[num]))
 
-newfruit = input('Add a fruit to the top of the list:')
+def series3(fruit):
+    '''Copy list, reverse letters in each item. Remove last item of
+    original list, and displahy both lists'''
+    seriesheader("Series 3")
+    for f in fruit[:]:
+        ans = input("Do you like {}? (yes or no)".format(f.lower()))
+        while ans != 'yes' and  ans != 'no':
+            ans = input("Do you like {}? (yes or no)".format(f.lower()))
+            if ans is not "yes" and ans is not "no":
+                print("Please respond with 'yes' or 'no'.")
+        if ans == "no":
+            fruit.remove(f)
+    print("Some fruits you like: {}".format(fruit))
 
-fruit = newfruit + fruit
+def series4(fruit):
+    seriesheader("Series 4")
+    fruitcopy = []
+    for f in fruit:
+        fruitcopy.append(f[::-1])
+    fruit = fruit[:-1]
 
-print(fruit)
+    print("Original list, minus last item: {}".format(fruit))
+    print("Modified list with reversed letters: {}".format(fruitcopy))
 
-newfruit = input('Add another fruit to the top of the list:')
+def seriesheader(seriesname):
+    '''Print some text to indicate new series to user.'''
+    print()
+    print('{}{}{}'.format('*'*10,seriesname,'*'*10))
+    print()
 
-fruit.insert(newfruit)
+if __name__ == '__main__':
+    fruitlist = ['Apples','Pears','Oranges','Peaches']
+    series1(fruitlist[:])
+    series2(fruitlist[:])
+    series3(fruitlist[:])
+    series4(fruitlist[:])
 
-print(fruit)
-
-print("These are all of the fruit that begin with 'P':")
-for i in fruit:
-    if lowercase(i[0]) == 'p':
-        print i
