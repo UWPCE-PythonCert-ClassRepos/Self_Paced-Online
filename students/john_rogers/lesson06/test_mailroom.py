4345b4cc3f06d4e3524223058015e59703aa5995
john r <johnroge@outlook.com>
2019-01-22 18:05:30 -0800

    Added working example for test_mailroom.py

--- students/john_rogers/lesson06/test_mailroom.py
+++ students/john_rogers/lesson06/test_mailroom.py
@@ -3,17 +3,22 @@
 Test the various functions in mailroom_L6.py
 """
 
+import pytest
 from mailroom_L6 import thank_all
 
 
-db = {'sting': [13.45, 214.34, 123.45, 1433.23, 1243.13],
+@pytest.fixture
+def data():
+    db = {'sting': [13.45, 214.34, 123.45, 1433.23, 1243.13],
           'bono': [7843.34, 35.55, 732.34],
           'oprah': [66.34, 32.23, 632.21, 66.67],
           'yoko': [34.34, 4.34],
           'santa': [5334.00, 254.34, 64324.23, 2345.23, 5342.24],
           }
+    return db
 
 
-def test_thank_all(db):
-    assert thank_all(db) == 
+def test_thank_all(data):
+    assert thank_all(data) is None
+
 
