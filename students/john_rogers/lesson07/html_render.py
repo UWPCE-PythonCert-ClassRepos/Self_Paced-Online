7b6f3e4bf763d5051d76c30688f874dd2944dd97
John Rogers <johnroge@outlook.com>
2019-01-27 13:46:28 -0800

    creating templates needed for lesson07 - html_render.py.

---
+++ students/john_rogers/lesson07/html_render.py
@@ -0,0 +1,29 @@
+
+
+class Element:
+    pass
+
+
+class Html(Element):
+    pass
+
+
+class Body(Element):
+    pass
+
+
+class Head(Element):
+    pass
+
+
+class Hr(Element):
+    pass
+
+
+class Li(Element):
+    pass
+
+
+class Ul(Element):
+    pass
+
---
+++ students/john_rogers/lesson07/test_html_render.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# unit testing for module html_render.py
+
+import pytest
+
+
+def test_element():
+    pass
+
