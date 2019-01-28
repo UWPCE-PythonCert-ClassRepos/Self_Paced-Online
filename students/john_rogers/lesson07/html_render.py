457a6b8c7ff7a61352a92495a484a72408baee1f
john r <johnroge@outlook.com>
2019-01-28 14:07:31 -0800

    Still working on html_render.py template for lesson07.

--- students/john_rogers/lesson07/html_render.py
+++ students/john_rogers/lesson07/html_render.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
+"""
+html_render.py: using classes to render HTML
+Author: JohnR
+Version: .1
+Last updated: 1/28/2019
+Notes:
+"""
 
 
 class Element:
+    """
+    List out what we want from the base class
+    """
     pass
 
 
