b89425fb4b5edbeb7483623582cb0beb2689fd98
guntur <ramakanth.gunturu@gmail.com>
2019-01-29 11:23:09 -0800

    Modified the fib, luc and gen functions and added the fizzbuzz file

--- students/g_rama/lesson02/series.py
+++ students/g_rama/lesson02/series.py
@@ -1,62 +1,48 @@
 def fibonacci(n):
-    """ The function prints the fibonacci series for giving number"""
-    if n<= 1:
-        fib = [0, 1]
-        print((fib[n]))
-    else:
-        fib = [0, 1]
-        for _ in range(n):
-            print(fib)
-            fib.append(fib[_]+fib[_+1])
-        print(fib[n])
+    """ The function prints the fibonacci series and prints the nth fibonacci number"""
+    fib = [0, 1]
+    for _ in range(n):
+        #print(fib)
+        fib.append(fib[_] + fib[_ + 1])
+    return fib[n]
 
 
-fibonacci(0)
-fibonacci(1)
-fibonacci(2)
-fibonacci(3)
-fibonacci(8)
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(3))
+print(fibonacci(8))
 
 
 def lucas(n):
-    """ The function prints the lucas series for giving number"""
-    if n<= 1:
-        luc = [2, 1]
-        print((luc[n]))
-    else:
-        luc = [2, 1]
-        for _ in range(n):
-            print(luc)
-            luc.append(luc[_]+luc[_+1])
-        print(luc[n])
+    """ The function prints the lucas series for giving number and prints the nth lucas number"""
+    luc = [2, 1]
+    for _ in range(n):
+        luc.append(luc[_] + luc[_ + 1])
+    return luc[n]
 
 
-lucas(0)
-lucas(1)
-lucas(2)
-lucas(3)
-lucas(8)
+print(lucas(0))
+print(lucas(1))
+print(lucas(2))
+print(lucas(3))
+print(lucas(8))
 
 
 def sum_series(n,g1=0,g2=1):
     """ The function prints a generic sequence number for given arguments
     usage: g1 and g2 defaults to fibonacci if no arguments are given
     """
-    if n <= 1:
-        gen = [g1, g2]
-        print((gen[n]))
-    else:
-        gen = [g1, g2]
-        for _ in range(n):
-            print(gen)
-            gen.append(gen[_] + gen[_ + 1])
-        print(gen[n])
+    gen = [g1, g2]
+    for _ in range(n):
+        gen.append(gen[_] + gen[_ + 1])
+    return gen[n]
 
 
-sum_series(8)
-sum_series(8,2,1)
-sum_series(8,0,1)
-sum_series(8,3,1)
+print(sum_series(8))
+print(sum_series(8,2,1))
+print(sum_series(8,0,1))
+print(sum_series(8,3,1))
 
 
 def testing():
@@ -67,3 +53,4 @@ def testing():
     assert fibonacci(0) != 21
 
 
+testing()
