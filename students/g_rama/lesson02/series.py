901521915e2111f9c5a34d1845908c352e7575b6
guntur <ramakanth.gunturu@gmail.com>
2019-01-29 00:23:02 -0800

    Fibonacci,lucas and general seq functions

---
+++ students/g_rama/lesson02/series.py
@@ -0,0 +1,56 @@
+def fibonacci(n):
+    """ The function prints the fibonacci series for giving number"""
+    if n<= 1:
+        fib = [0, 1]
+        print((fib[n]))
+    else:
+        fib = [0, 1]
+        for _ in range(n):
+            print(fib)
+            fib.append(fib[_]+fib[_+1])
+        print(fib[n])
+
+
+fibonacci(0)
+fibonacci(1)
+fibonacci(2)
+fibonacci(3)
+fibonacci(8)
+
+
+def lucas(n):
+    """ The function prints the lucas series for giving number"""
+    if n<= 1:
+        luc = [2, 1]
+        print((luc[n]))
+    else:
+        luc = [2, 1]
+        for _ in range(n):
+            print(luc)
+            luc.append(luc[_]+luc[_+1])
+        print(luc[n])
+
+
+lucas(0)
+lucas(1)
+lucas(2)
+lucas(3)
+lucas(8)
+
+
+def sum_series(n,g1=0,g2=1):
+        if n <= 1:
+            gen = [g1, g2]
+            print((gen[n]))
+        else:
+            gen = [g1, g2]
+            for _ in range(n):
+                print(gen)
+                gen.append(gen[_] + gen[_ + 1])
+            print(gen[n])
+
+
+sum_series(8)
+sum_series(8,2,1)
+sum_series(8,0,1)
+sum_series(8,3,1)
