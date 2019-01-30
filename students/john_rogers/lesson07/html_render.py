4dfa5ba57b780f201182e057d62ba016219d2b8b
john r <johnroge@outlook.com>
2019-01-30 14:41:06 -0800

    part 4 of html_render.py working in lesson07.

--- students/john_rogers/lesson07/html_render.py
+++ students/john_rogers/lesson07/html_render.py
@@ -15,14 +15,19 @@ class Element(object):
     tag = 'html'
     indent = ' ' * 4
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.content = [content] if content else []
+        self.kwargs = kwargs
 
     def append(self, data):
         self.content.append(data)
 
     def render(self, file_out, cur_ind=''):
-        file_out.write(cur_ind + f'<{self.tag}>\n')
+        file_out.write(cur_ind + f'<{self.tag}')
+
+        for key, value in self.kwargs.items():
+            file_out.write(f' {key}="{value}"')
+        file_out.write('>\n')
 
         for item in self.content:
             try:
@@ -36,7 +41,11 @@ class Element(object):
 class OneLineTag(Element):
 
     def render(self, file_out, cur_ind=''):
-        file_out.write(cur_ind + f'<{self.tag}>')
+        file_out.write(cur_ind + f'<{self.tag}')
+
+        for key, value in self.kwargs.items():
+            file_out.write(f'{key}="{value}"')
+        file_out.write('>')
 
         for item in self.content:
             try:
