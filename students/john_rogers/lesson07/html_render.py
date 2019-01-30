edc521cbc0b5cfcb62148fc4deb25cafec1461e1
john r <johnroge@outlook.com>
2019-01-29 16:15:08 -0800

    Initial work on the base class near completion, lesson07.

--- students/john_rogers/lesson07/html_render.py
+++ students/john_rogers/lesson07/html_render.py
@@ -3,23 +3,34 @@
 html_render.py: using classes to render HTML
 Author: JohnR
 Version: .1
-Last updated: 1/28/2019
+Last updated: 1/29/2019
 Notes:
 """
 
 
-class Element:
+class Element(object):
     """
-    List out what we want from the base class
+    create base class for adding html tags and text strings to a file
     """
-    def __init__(self):
-        pass
+    tag = 'html'
+    indent = ' ' * 4
+
+    def __init__(self, content=None):
+        self.content = [content] if content else []
 
     def append(self, data):
-        pass
+        self.content.append(data)
 
-    def render(self, data):
-        pass
+    def render(self, file_out, cur_ind=''):
+        file_out.write(cur_ind + f'<{self.tag}>\n')
+
+        for item in self.content:
+            try:
+                file_out.write(cur_ind + self.indent + item + '\n')
+            except Exception as e:
+                print(e)
+
+        file_out.write(cur_ind + f'</{self.tag}>\n')
 
 
 class Html(Element):
