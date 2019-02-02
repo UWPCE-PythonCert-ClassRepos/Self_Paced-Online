f36917583a48bb4dd33be0ae5077dd2eacf25129
guntur <ramakanth.gunturu@gmail.com>
2019-02-01 18:03:47 -0800

    Listlab upto series 3

--- students/g_rama/lesson03/slicing.py
+++ students/g_rama/lesson03/slicing.py
@@ -1,27 +1,18 @@
-# Write some functions that take a sequence as an argument, and return a copy of that sequence:
-#
-#     with the first and last items exchanged.
-#     with every other item removed.
-#     with the first 4 and the last 4 items removed, and then every other item in between.
-#     with the elements reversed (just with slicing).
-#     with the middle third, then last third, then the first third in the new order.
-#
-
-
+# Slicing LAB
 def exchange_first_last(seq):
     """Invert the first and last element from a sequence"""
     if len(seq) > 1:
         if type(seq) is tuple:
-            last_first = (seq[-1], *seq[1:-2], seq[0])
-            print(last_first)
+            last_first = (seq[-1], *seq[1:-1], seq[0])
+            return last_first
         elif type(seq) is str:
-            last_first = seq[-1] + seq[1:-2] + seq[0]
-            print(last_first)
+            last_first = seq[-1] + seq[1:-1] + seq[0]
+            return last_first
         elif type(seq) is list:
-            last_first = [seq[-1], *seq[1:-2], seq[0]]
-            print(last_first)
+            last_first = [seq[-1], *seq[1:-1], seq[0]]
+            return last_first
     else:
-        print(seq)
+        return seq
 
 
 def every_other_remove(seq):
@@ -30,14 +21,17 @@ def every_other_remove(seq):
         if type(seq) is tuple:
             last_first = (seq[::2])
             print(last_first)
+            return last_first
         elif type(seq) is str:
             last_first = seq[::2]
             print(last_first)
+            return last_first
         elif type(seq) is list:
             last_first = seq[::2]
             print(last_first)
+            return last_first
     else:
-        print(seq)
+        return seq
 
 
 def f4_l4_every_other_remove(seq):
@@ -46,64 +40,85 @@ def f4_l4_every_other_remove(seq):
         if type(seq) is tuple:
             f4_l4 = (seq[4:-4:2])
             print(f4_l4)
+            return f4_l4
         elif type(seq) is str:
             f4_l4 = seq[4:-4:2]
             print(f4_l4)
+            return f4_l4
         elif type(seq) is list:
             f4_l4 = [seq[4:-4:2]]
             print(f4_l4)
+            return f4_l4
     else:
         print(seq)
+        return seq
 
 
-def every_other_remove(seq):
-    """Remove every other element from a sequence"""
+def element_reverse(seq):
+    """Reverse the elements"""
     if len(seq) > 1:
         if type(seq) is tuple:
-            every_other = (seq[::-1])
-            print(every_other)
+            reverse = (seq[::-1])
+            print(reverse)
+            return reverse
         elif type(seq) is str:
-            every_other = seq[::-1]
-            print(every_other)
+            reverse = seq[::-1]
+            print(reverse)
+            return reverse
         elif type(seq) is list:
-            every_other = seq[::-1]
-            print(every_other)
+            reverse = seq[::-1]
+            print(reverse)
+            return reverse
     else:
         print(seq)
+        return seq
 
 
 def mid_last_first(seq):
     """Middle 3 + Last 3 + first 3"""
     if len(seq) > 1:
         if type(seq) is tuple:
-            mid_last_first = (*seq[int(len(seq)/3):int((len(seq)/3)*2):1],*seq[-int(len(seq)/3)::1],*seq[:int(len(seq)/3):1])
-            print(mid_last_first)
+            mid_last_first_v = (*seq[int(len(seq)/3):int((len(seq)/3)*2):1], *seq[-int(len(seq)/3)::1],
+                                *seq[:int(len(seq)/3):1])
+            print(mid_last_first_v)
+            return mid_last_first_v
         elif type(seq) is str:
-            mid_last_first = seq[int(len(seq)/3):int((len(seq)/3)*2):1] + seq[-int(len(seq)/3)::1] + seq[:int(len(seq)/3):1]
-            print(mid_last_first)
+            mid_last_first_v = seq[int(len(seq)/3):int((len(seq)/3)*2):1] + seq[-int(len(seq)/3)::1]\
+                               + seq[:int(len(seq)/3):1]
+            print(mid_last_first_v)
+            return mid_last_first_v
         elif type(seq) is list:
-            mid_last_first = [*seq[int(len(seq)/3):int((len(seq)/3)*2):],*seq[-int(len(seq)/3)::],*seq[:int(len(seq)/3):]]
-            print(mid_last_first)
+            mid_last_first_v = [*seq[int(len(seq)/3):int((len(seq)/3)*2):], *seq[-int(len(seq)/3)::],
+                                *seq[:int(len(seq)/3):]]
+            print(mid_last_first_v)
+            return mid_last_first_v
     else:
         print(seq)
+        return seq
 
 
-exchange_first_last([1,4])
-exchange_first_last([1,2,3,4])
-exchange_first_last((1,2,3,4,5))
+exchange_first_last([1, 4])
+exchange_first_last([1, 2, 3, 4])
+exchange_first_last((1, 2, 3, 4, 5))
 exchange_first_last('This is a test string for functions')
 exchange_first_last((1,5))
 exchange_first_last((1,))
 exchange_first_last('m')
-every_other_remove((1,2,3,4))
-every_other_remove([1,2,3,4])
+every_other_remove((1, 2, 3, 4))
+every_other_remove([1, 2, 3, 4])
 every_other_remove('This is a test string for functions')
-f4_l4_every_other_remove((1,2,3,4,5,6,7,8,9,10,11,12,13,13))
-f4_l4_every_other_remove([1,2,3,4,5,6,7,8,9,10,11,12,13,13])
+f4_l4_every_other_remove((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13))
 f4_l4_every_other_remove('This is a test string for functions')
-every_other_remove((1,2,3,4,5,6,7,8,9,10,11,12,13,13))
+element_reverse((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13))
 mid_last_first((2, 54, 13, 12, 5, 32))
 mid_last_first('This is a test string for functions')
-mid_last_first([1,2,3,4])
+mid_last_first([1, 2, 3, 4])
 
+a_string = "this is a string"
+a_tuple = (2, 54, 13, 12, 5, 32)
+
+assert exchange_first_last(a_string) == "ghis is a strint"
+assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+assert element_reverse((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13)) == (13, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2,
+                                                                            1)
 
