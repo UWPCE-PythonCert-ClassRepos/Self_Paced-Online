1e9d09b89ca51ecbd3d2ec706cbc600662015d67
Alex Filson <alexdfilson@gmail.com>
2019-02-03 12:50:52 -0800

    Added send letters to all, fixed PEP8 issues, broke out letter creation into separate function.

--- students/alheadstrong/lesson_4/mailroom_part2.py
+++ students/alheadstrong/lesson_4/mailroom_part2.py
@@ -1,26 +1,27 @@
-def thank_you(donor_dict):
+import os
+
+def new_thank_you(donor_dict):
     """Add donation amount to new or existing donor and generate a thank you text."""
     while 1:
-        donor_name = input("Enter donor name:")
-        if donor_name == 'list':
+        current_dict = {'donor_name': input("Enter donor name, or type 'list' for current donor list:")}
+        if current_dict['donor_name'] == 'list':
             for d in donor_dict:  # display all donor names on user input 'list'
-                print(d[0])
+                print(d + ': ', donor_dict[d])
                 continue
         else:
-            for d in range(len(donor_dict)):
-                if donor_dict[d][0] == donor_name:
+            for key in donor_dict.keys():
+                if key == current_dict['donor_name']:
                     print("Donor name found in database.")
-                    donation = int(input('Enter donation amount:'))
-                    donor_dict[d].append(donation)
+                    current_dict['donation'] = float(input('Enter new donation amount:'))
+                    donor_dict[key].append(current_dict['donation'])
                     break
             else:
-                print(f"Donor name not found. Will add {donor_name} to database at position {len(donor_dict)+1}.")
-                donation = int(input('Enter donation amount:'))
-                donor_dict += [[donor_name, donation]]
-                print(donor_dict)
+                print(f"Donor name not found. Will add {current_dict['donor_name']} to database.")
+                current_dict['donation'] = float(input('Enter donation amount:'))
+                donor_dict[current_dict['donor_name']] = [current_dict['donation']]
             break
-    print('Dear {},\nThank you for your generous gift of ${}\n'
-          'Sincerely,\nLocal Charity.'''.format(donor_name, donation))
+
+    print('\n\n',create_letter(current_dict))
     return donor_dict
 
 
@@ -30,18 +31,19 @@ def report(donor_dict):
 
     report_table = []
 
-    for key in donor_dict.keys():
+    for key in donor_dict.keys():  # read dict values into 2D list to be displayed.
         total_given = sum(donor_dict[key])
         num_gifts = len(donor_dict[key])
         average_gift = total_given/num_gifts
         new_report_line = (key, '$' + str(total_given), str(num_gifts), '$' + str(average_gift))
-        if not report_table:
-            report_table += [new_report_line]
+
+        for i in range(len(report_table)):
+            if total_given > float(report_table[i][1][1:]):
+                report_table = report_table[:i]+[new_report_line]+report_table[i:]
+                break
         else:
-            for i in range(len(report_table)):
-                if average_gift > float(report_table[i][3][1:]):
-                    report_table = report_table[:i]+[new_report_line]+report_table[i:]
-                    break
+            report_table = report_table + [new_report_line]
+
     report_table = report_table_header + report_table
 
     mtl = []  # list for the longest characters in each column
@@ -57,29 +59,67 @@ def report(donor_dict):
         form_string.append('{:' + str(i) + '}')
     form_string = ''.join(form_string)
 
-    print(form_string.format(*report_table[0]))
-    print('{}'.format('-'*sum(mtl)))
-    for row in range(len(report_table)-1):
-        print(form_string.format(*report_table[row+1]))
+    print('\n\n')  # Begin printing out table
+    print(form_string.format(*report_table.pop(0)))
+    print('{}'.format('-'*sum(mtl)))  # spacer line between header and body of table
+    for row in report_table:
+        print(form_string.format(*row))
     return donor_dict
 
-def goodbye(arg):
-    print("goodbye!")
 
+def letters_to_all(donor_dict):
+    user_input = input("Would you like save to current directory,{}? >(y,n)".format(os.getcwd()))
+    if user_input == 'n':
+        path = input("Enter full path to desired directory.")
+        os.chdir(path)
+
+    for key in donor_dict.keys():
+        print(key)
+        current_dict = {'donor_name': key, 'donation': donor_dict[key][-1]}
+        print("current_dict: ", current_dict)
+        key = key.replace(' ', '_')
+        with open('{}.txt'.format(key), 'w') as f:
+            f.write(create_letter(current_dict))
+
+
+def create_letter(letter_dict):
+    letter_dict['fleebs_floobed'] = round(letter_dict['donation']/20)
+    return ('Dear {donor_name},'
+            '\n\n\tThank you for your generous gift of ${donation:,.2f}.'
+            '\n\tYour contribution will keep {fleebs_floobed} Fleebs floobing for an entire year.'
+            '\n\tWe truly could not do this important work without you.'
+            '\n\nSincerely,'
+            '\nFleeb Freedom Now'.format(**letter_dict))
+
+
+def goodbye(arg):
+    """Do nothing with arg, return 'quit' to exit main loop."""
+    print("goodbye!")
+    return 'quit'
 
 
 def main():
     """Establish initial donor database and initiate top user menu. """
-    ddb = {'Archie Bunker': [20, 100],
+    ddb = {'Archie Bunker': [20, 100, 75, 98],
            'Beyonce Knowles': [2000000, 50000000],
            'Charlie Kauffman': [12345],
            'David Sedaris': [23000, 1200, 2000],
            'Edvard Munch': [1, 2, 3]}
 
-    while ddb != quit:
-        user_input = input('\n\nMENU:\n1 - Send a Thank You\n2 - Create a Report\n3 - Quit\nPlease enter 1-3>')
-        user_menu = {'1': thank_you, '2': report, '3': goodbye}
-        ddb = user_menu.get(user_input,'Option not found. Please enter the numeral 1,2 or 3')(ddb)
+    while ddb != 'quit':
+        user_input = input('\n\nMENU:\n'
+                           '1 - Send a Thank You for a New Donation\n'
+                           '2 - Create a Report\n'
+                           '3 - Send letters to everyone\n'
+                           '4 - Quit\n'
+                           'Please enter 1-4>')
+        user_menu = {'1': new_thank_you, '2': report, '3': letters_to_all, '4': goodbye}
+        user_options = ('1', '2', '3', '4')
+        if user_input in user_options:
+            ddb = user_menu.get(user_input)(ddb)
+        else:
+            print('Option not found. Please enter the numeral 1,2,3 or 4')
+
 
 if __name__ == '__main__':
     main()
