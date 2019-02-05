47564ad27a6b2bafd68ae5bca0b4b5ecec2c2ff2
dfspray <daniel_spray@comcast.net>
2019-02-04 16:25:01 -0800

    Fixed step 2 render function to match output

--- students/Daniel_Spray/Lesson07/html_render.py
+++ students/Daniel_Spray/Lesson07/html_render.py
@@ -12,23 +12,20 @@ class Element:
 
     def render(self,file_out,cur_ind=''):
         file_out.write(cur_ind+'<'+self.tag+'>\n')
-        str_only=[]
         for text in self.content:
             if isinstance(text,str):
-                str_only.append(str(text))
+                file_out.write(text+'.\n')
             else:
                 text.render(file_out)
-        content_string = ('.\n'+cur_ind+self.indent).join(str_only)
-        file_out.write(cur_ind+self.indent+content_string+'\n'+cur_ind+'</'+self.tag+'>\n')
+        file_out.write(cur_ind+'</'+self.tag+'>\n')
 
 class Html(Element):
     tag='html'
-    def render(self,file_out,cur_ind=''):
-        file_out.write('<!DOCTYPE html>\n')
-        Element.render(self,file_out,cur_ind='')
 
 class Body(Element):
     tag='body'
 
 class P(Element):
     tag='p'
+
+
