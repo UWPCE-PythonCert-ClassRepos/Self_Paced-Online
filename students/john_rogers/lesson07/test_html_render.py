36f73295767a6bdd9b3fa98ecdfe5d47d3423dfd
John Rogers <johnroge@outlook.com>
2019-02-04 18:49:28 -0800

    cleaning up variable names in test_html_render.py

--- students/john_rogers/lesson07/test_html_render.py
+++ students/john_rogers/lesson07/test_html_render.py
@@ -26,46 +26,45 @@ def test_element(data):
                                     ' font-style: oblique;'}
 
 
-def test_append():
-    test02 = hr.Element('test02')
-    test02.append('test append method')
-    assert test02.content == ['test02', 'test append method']
+def test_append(data):
+    data.append('test append method')
+    assert data.content == ['text foo', 'test append method']
 
 
 def test_render():
     test03 = hr.Element('test03')
     test03.tag = 'html'
-    foo = sio()
-    test03.render(foo)
-    assert foo.getvalue() == '<html>\ntest03\n</html>\n'
+    file_out = sio()
+    test03.render(file_out)
+    assert file_out.getvalue() == '<html>\ntest03\n</html>\n'
 
 
 def test_onelinetag():
     test04 = hr.OneLineTag('test04')
     test04.tag = 'title'
-    foo = sio()
-    test04.render(foo)
-    assert foo.getvalue() == '<title>test04</title>\n'
+    file_out = sio()
+    test04.render(file_out)
+    assert file_out.getvalue() == '<title>test04</title>\n'
 
 
 def test_add_items():
     test05 = hr.Element('test05')
-    foo = sio()
-    test05.add_items(foo)
-    assert foo.getvalue() == 'test05\n'
+    file_out = sio()
+    test05.add_items(file_out)
+    assert file_out.getvalue() == 'test05\n'
 
 
 def test_add_items_no_line():
     test06 = hr.Element('test06')
-    foo = sio()
-    test06.add_items_no_line(foo)
-    assert foo.getvalue() == 'test06'
+    file_out = sio()
+    test06.add_items_no_line(file_out)
+    assert file_out.getvalue() == 'test06'
 
 
 def test_add_values(data):
-    foo = sio()
-    data.add_values(foo)
-    assert foo.getvalue() == ' style="text-align: center; font-style: oblique;"'
+    file_out = sio()
+    data.add_values(file_out)
+    assert file_out.getvalue() == ' style="text-align: center; font-style: oblique;"'
 
 
 
