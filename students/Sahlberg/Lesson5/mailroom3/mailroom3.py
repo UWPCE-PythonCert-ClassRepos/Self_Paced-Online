7ef8ca00265ab949906d3eb3972fbdb9083c5c8e
Ian Sahlberg <iansahlberg@gmail.com>
2019-02-13 21:55:26 -0800

    Refactored code.

--- students/Sahlberg/Lesson5/mailroom3/mailroom3.py
+++ students/Sahlberg/Lesson5/mailroom3/mailroom3.py
@@ -5,9 +5,8 @@ Python 210
 
 #for file dating
 import datetime as dt
-
 #Dictionary of donors name, dollar amount
-donor = {'bob johnson': [150.00],
+donors = {'bob johnson': [150.00],
          'susan skoosan': [2000.00, 550.00],
          'tim tam':[10.50],
          'roxanne raffle':[13.00, 75.00, 123.00],
@@ -15,35 +14,24 @@ donor = {'bob johnson': [150.00],
          }
 
 #Dictionary of user options
-user_select = {0:'1 - Send a Thank You', 1:'2 - Create a Report', 2:'4 - quit', 3:'3 - Send letters to everyone', 4:'Enter an option: List (to give a list of names), A donors name, \"quit\" , to quit.', 5:'How much did they donate?'}
+user_select = {0:'1 - Send a Thank You', 1:'2 - Create a Report', 2:'4 - quit', 3:'3 - Send letters to everyone', 4:'Enter an option: List (to give a list of names), A donors full name, \"Menu\" to return to main menu.', 5:'How much did they donate?'}
 
-def donate(name, dictionary):
+def donate():
+
     """Appends a donation amount to a given donor."""
-    amount = int(input(user_select.get(5)))
-    return amount#, dictionary[name].append(amount)
+    try:
+        amount = int(input(user_select.get(5)))
+        return amount
+    except ValueError:
+        return print('Please enter a dollar amount.')
+
 
 def amount_append(name, dictionary, amount):
     return dictionary[name].append(amount)
 
 def print_list(dictionary):
     """Prints our the donor list"""
-    return print(list(dictionary.keys()))
-
-def user_options(dict):
-    """Main user options menu choices."""
-    user_select = dict
-    x = True
-    while x:
-        user_action = int(input('Please choose from the following options:  \n'+ user_select.get(0) + '\n'+ user_select.get(1) + '\n'+ user_select.get(3)+ '\n'+ user_select.get(2)))
-
-        if user_action in (1,2,3,4):
-            x = False
-            return user_action
-        else:
-            print('Not a valid option, please try again.')
-            continue
-
-    return user_action
+    return list(dictionary.keys())
 
 def email(name, amount):
     """Create email for specific donor and amount donated."""
@@ -51,79 +39,117 @@ def email(name, amount):
 
 def quit():
     """Quit the program"""
-    return print('Until next time!')
+    print('Until next time!')
+    return 'Until next time!'
 
 def sort_and_stats_dict(dictionary):
     """Create a stats dictionary from a donor dictionary and sort by total donated."""
-    pre_sort = {k: [sum(v), len(v), round((sum(v) / len(v)), 0)] for k, v in dictionary.items()}
-    print(pre_sort)
-    donor_sorted = sorted(list(pre_sort.items()), key=lambda x: x[1], reverse=True)
-    return donor_sorted
+    try:
+        pre_sort = {k: [sum(v), len(v), round((sum(v) / len(v)), 0)] for k, v in dictionary.items()}
+        donor_sorted = sorted(list(pre_sort.items()), key=lambda x: x[1], reverse=True)
+        return donor_sorted
+    except (TypeError, AttributeError):
+        print('Please use a dictionary.')
 
-def print_report(dictionary):
+    except NameError as ne:
+        print('You used an unknown reference. Please use an existing dictionary.')
+
+
+def print_report(stats_dictionary):
     """Print out a report of donors and amounts donated, including stats (total donated, number of times donated, average donation)."""
-    print('Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations')
-    print('----------------------------------------------------------------------------------------')
-    count = 0
-    for donor in dictionary:
-        print(
-            f'{dictionary[count][0]:<20}$    {dictionary[count][1][0]:<23.2f}{dictionary[count][1][1]:<15}    ${dictionary[count][1][2]:>12.0f}')
-        count += 1
+    # access indexed results in stats dictionary
+    print_out = 'Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations\n,----------------------------------------------------------------------------------------\n'
+    for name, donor in stats_dictionary:
+        print_out += f'{name:<20}$    {donor[0]:<23.2f}{donor[1]:<15}    ${donor[2]:>12.0f}\n'
+    return print_out
+
 
 def email_file(dictionary):
     """Generates a thank you letter and writes to disk for all donors."""
-    count = 0
-    count_file = 0
-    for name in dictionary.keys():
-        amount = [sum(v) for v in dictionary.values()][count]
-        file_name = '{}_{}.txt'.format(name, dt.date.today())
-        count += 1
-        with open(file_name, 'w+') as file:
-            e_text = str(email(name, amount))
-            count_file += 1
-            file.write(e_text)
-    return print('Emails written to file.')
+    import datetime as dt
+    try:
+        count = 0
+        for name, donations in dictionary.items():
+            amount = sum(donations)
+            file_name = '{}_{}.txt'.format(name, dt.date.today())
+            count += 1
+            try:
+                with open(file_name, 'w+') as file:
+                    e_text = email(name, amount)
+                    file.write(e_text)
+
+            except IOError as err:
+                print('There was an error:', err)
+        return print('Emails written to file.')
+    #except TypeError as err:
+        #print('Wrong type of object.', err)
+    except AttributeError as err:
+        print('Wrong object attribute:', err)
+
+
+def user_action1(dict):
+    "Runs through donors and donor amount options. Appends donation amounts to the donors list"
+    option = input(user_select.get(4))
+    if option.lower() == 'menu':
+        return 'menu'
+
+
+    if option.lower() == 'list':
+        print(print_list(dict))
+        return user_action1(dict)
+
+    else:
+        if option.lower() in dict:
+            amount = donate()
+            amount_append(option, dict, amount)
+            print(email(option, amount))
+            return user_action1(dict)
+
+        else:
+            dict[option] = []
+            try:
+                amount = donate()
+                amount_append(option, donors, amount)
+                print(email(option, amount))
+                return user_action1(dict)
+            except ValueError as err:
+                print('Something is not right.')
+                return print(err)
+
+
+def user_action2(dict):
+    #Prints a report of donors and stats details"
+    try:
+        donor_sorted = sort_and_stats_dict(dict)
+        return print_report(donor_sorted)
+    except NameError:
+        print('Unknown reference')
+        user_action2(dict)
+    except TypeError:
+        print('Cannot iterate over function objects')
+        user_action2(dict)
+
+switchitdic = {1 : lambda : user_action1(donors), 2 : lambda :print(user_action2(donors)), 3 : lambda : email_file(donors), 4 : lambda : quit()}
 
 #---------------------------------------------------------------
 #Work Zone
-if __name__== '__main__':
 
+x = True
+while x:
     try:
-        user_action = str(user_options(user_select))
-    except ValueError:
-        print('Please choose options 1 thur 4.')
-        user_action = str(user_options(user_select))
+        option1 = int(input('Please choose from the following options: \n1 - Send a Thank You \n2 - Create a Report \n3 - Send letters to everyone \n4 - quit'))
+        y = switchitdic.get(option1)()
+
+    except TypeError as err:
+        print('Please choose an option 1 thru 4.', err)
+        continue
+    if y == 'Until next time!':
+        x = False
+    elif y == 'menu':
+        continue
 
 
-    if user_action.lower() == 'quit':
-        quit()
 
-    while user_action == '1':
-        option = input(user_select.get(4))
 
-        if option.lower() == 'quit':
-            print('Bye!')
-            break
 
-        if option.lower() == 'list':
-            print_list(donor)
-        else:
-            if option.lower() in donor:
-                amount = donate(option,donor)
-                amount_append(option,donor,amount)
-                email(option,amount)
 
-            else:
-                donor[option] = []
-                amount = donate(option,donor)
-                amount_append(option, donor, amount)
-                email(option,amount)
-
-    while user_action == '2':
-        donor_sorted = sort_and_stats_dict(donor)
-        print_report(donor_sorted)
-        break
-
-    while user_action == '3':
-        email_file(donor)
-        break
