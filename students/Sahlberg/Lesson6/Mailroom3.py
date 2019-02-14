e598e0a95331d5976a2cb49b06e3761dea867def
Ian Sahlberg <iansahlberg@gmail.com>
2019-02-13 22:00:20 -0800

    Unit testing files.

---
+++ students/Sahlberg/Lesson6/Mailroom3.py
@@ -0,0 +1,165 @@
+"""Ian Sahlberg
+Assignment 5 mailroom3
+Python 210
+01/14/2019"""
+
+
+
+#for file testing only
+if __name__ == "__main__":
+    print('this is main')
+
+
+
+else:
+
+    #for file dating
+    import datetime as dt
+    #Dictionary of donors name, dollar amount
+    donors = {'bob johnson': [150.00],
+             'susan skoosan': [2000.00, 550.00],
+             'tim tam':[10.50],
+             'roxanne raffle':[13.00, 75.00, 123.00],
+             'jon jacob':[5000.00, 5.00]
+             }
+
+    #Dictionary of user options
+    user_select = {0:'1 - Send a Thank You', 1:'2 - Create a Report', 2:'4 - quit', 3:'3 - Send letters to everyone', 4:'Enter an option: List (to give a list of names), A donors full name, \"Menu\" to return to main menu.', 5:'How much did they donate?'}
+
+    def donate():
+
+        """Appends a donation amount to a given donor."""
+        try:
+            amount = int(input(user_select.get(5)))
+            return amount
+        except ValueError:
+            return print('Please enter a dollar amount.')
+
+
+    def amount_append(name, dictionary, amount):
+        return dictionary[name].append(amount)
+
+    def print_list(dictionary):
+        """Prints our the donor list"""
+        return list(dictionary.keys())
+
+    def email(name, amount):
+        """Create email for specific donor and amount donated."""
+        return 'Thank you {} for the generous donation of $ {:.2f}. We appreciate your generosity.\n\nSincerely, \n\nThe Helping R Us Team\n\n'.format(name.title(), amount)
+
+    def quit(self):
+        """Quit the program"""
+        print('Until next time!')
+
+        return 'Until next time!'
+
+    def sort_and_stats_dict(dictionary):
+        """Create a stats dictionary from a donor dictionary and sort by total donated."""
+        try:
+            pre_sort = {k: [sum(v), len(v), round((sum(v) / len(v)), 0)] for k, v in dictionary.items()}
+            donor_sorted = sorted(list(pre_sort.items()), key=lambda x: x[1], reverse=True)
+            return donor_sorted
+        except (TypeError, AttributeError):
+            print('Please use a dictionary.')
+
+        except NameError as ne:
+            print('You used an unknown reference. Please use an existing dictionary.')
+
+
+    def print_report(stats_dictionary):
+        """Print out a report of donors and amounts donated, including stats (total donated, number of times donated, average donation)."""
+        # access indexed results in stats dictionary
+        print_out = 'Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations\n,----------------------------------------------------------------------------------------\n'
+        for name, donor in stats_dictionary:
+            print_out += f'{name:<20}$    {donor[0]:<23.2f}{donor[1]:<15}    ${donor[2]:>12.0f}\n'
+        return print_out
+
+
+    def email_file(dictionary):
+        """Generates a thank you letter and writes to disk for all donors."""
+        import datetime as dt
+        try:
+            count = 0
+            for name, donations in dictionary.items():
+                amount = sum(donations)
+                file_name = '{}_{}.txt'.format(name, dt.date.today())
+                count += 1
+                try:
+                    with open(file_name, 'w+') as file:
+                        e_text = email(name, amount)
+                        file.write(e_text)
+
+                except IOError as err:
+                    print('There was an error:', err)
+            return print('Emails written to file.')
+        #except TypeError as err:
+            #print('Wrong type of object.', err)
+        except AttributeError as err:
+            print('Wrong object attribute:', err)
+
+
+    def user_action1(dict):
+        "Runs through donors and donor amount options. Appends donation amounts to the donors list"
+        option = input(user_select.get(4))
+        if option.lower() == 'menu':
+            return 'menu'
+
+
+        if option.lower() == 'list':
+            print(print_list(dict))
+            return user_action1(dict)
+
+        else:
+            if option.lower() in dict:
+                amount = donate()
+                amount_append(option, dict, amount)
+                print(email(option, amount))
+                return user_action1(dict)
+
+            else:
+                dict[option] = []
+                try:
+                    amount = donate()
+                    amount_append(option, donors, amount)
+                    print(email(option, amount))
+                    return user_action1(dict)
+                except ValueError as err:
+                    print('Something is not right.')
+                    return print(err)
+
+
+    def user_action2(dict):
+        #Prints a report of donors and stats details"
+        try:
+            donor_sorted = sort_and_stats_dict(dict)
+            return print_report(donor_sorted)
+        except NameError:
+            print('Unknown reference')
+            user_action2(dict)
+        except TypeError:
+            print('Cannot iterate over function objects')
+            user_action2(dict)
+
+    switchitdic = {1 : lambda : user_action1(donors), 2 : lambda :print(user_action2(donors)), 3 : lambda : email_file(donors), 4 : lambda : quit()}
+
+    #---------------------------------------------------------------
+    #Work Zone
+
+"""
+    x = True
+    while x:
+        try:
+            option1 = int(input('Please choose from the following options: \n1 - Send a Thank You \n2 - Create a Report \n3 - Send letters to everyone \n4 - quit'))
+            y = switchitdic.get(option1)()
+
+        except TypeError:
+            print('1')
+            print('Please choose an option 1 thru 4.')
+            continue
+        if y == 'Until next time!':
+            x = False
+        elif y == 'menu':
+            continue
+"""
+
+
---
+++ students/Sahlberg/Lesson6/UnitTest.py
@@ -0,0 +1,89 @@
+import unittest as u
+
+import Mailroom3 as m
+from Mailroom3 import amount_append
+from Mailroom3 import donate
+from Mailroom3 import print_report
+from Mailroom3 import quit
+
+
+
+class tests(u.TestCase):
+
+    """
+    def test_donate(self): #Requires test input = amount below (100)
+        test_dict = {'bugs mouse': [32, 5], 'mickey bunny': {678}}
+        amount_donated = 100
+        expected = 100
+        actual = donate()
+        self.assertEqual(expected,actual)
+    """
+
+    def test_amount_append(self):
+        dict = {'Roberto' : []}
+        name = 'Roberto'
+        amount = 23.00
+        expected = {name : [amount]}
+        m.amount_append(name, dict, amount)
+        self.assertDictEqual(expected, dict)
+
+
+    def test_print_list(self):
+        dict = {'Bob': 1, 'Mary': 2}
+        expected = ['Bob', 'Mary']
+        actual = m.print_list(dict)
+        self.assertEqual(expected, actual)
+
+    def test_email(self):
+        name = 'Bibs'
+        amount = 666.00
+        expected = 'Thank you {} for the generous donation of $ {:.2f}. We appreciate your generosity.\n\nSincerely, \n\nThe Helping R Us Team\n\n'.format(name.title(), amount)
+        actual = m.email(name, amount)
+        self.assertEqual(expected, actual)
+
+    def test_quit(self):
+        actual = quit(self)
+        expected = 'Until next time!'
+        self.assertEqual(expected, actual)
+
+
+    def test_sort_stats(self):
+        dict = {'bob johnson': [150.00],
+         'susan skoosan': [2000.00, 550.00]}
+        expected = [('susan skoosan', [2550.00, 2, 1275.00]), ('bob johnson', [150.00, 1, 150.00])]
+        actual = m.sort_and_stats_dict(dict)
+        self.assertListEqual(expected, actual)
+
+    def test_print_report(self):
+
+        dict = {'bob johnson': [150,1,150],
+             'susan skoosan': [2550,2,1275],
+             'tim tam':[10.50,1,10],
+             'roxanne raffle':[211,3,70],
+             'jon jacob':[5005,2,2502]}
+        dict_2 = sorted(list(dict.items()), key=lambda x: x[1], reverse=True)
+        actual = print_report(dict_2)
+        expected = """Donor Name       |      Donor Total    |  Count of Donations    |  Average of Donations
+,----------------------------------------------------------------------------------------
+jon jacob           $    5005.00                2                  $        2502
+susan skoosan       $    2550.00                2                  $        1275
+roxanne raffle      $    211.00                 3                  $          70
+bob johnson         $    150.00                 1                  $         150
+tim tam             $    10.50                  1                  $          10"""
+
+        self.assertEqual(expected,str(actual).rstrip())
+
+
+    def test_email_file(self):
+        import datetime as dt
+        dict = ({'Haggus': [4.00]})
+        expected = 'Thank you Haggus for the generous donation of $ 4.00. We appreciate your generosity.Sincerely, The Helping R Us Team'
+        a = m.email_file(dict)
+        with open ('Haggus_{}.txt'.format(dt.date.today()), 'r') as file:
+            x = file.read()
+            actual = "".join(x.splitlines())
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    u.main()
