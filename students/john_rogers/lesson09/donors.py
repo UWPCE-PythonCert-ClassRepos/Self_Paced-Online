bc06f477d2bb440da46c94c3d7f398f2f63fb4b0
John Rogers <johnroge@outlook.com>
2019-02-16 18:41:47 -0800

    Creating base templates for lesson09 assignment.

---
+++ students/john_rogers/lesson09/donors.py
@@ -0,0 +1,27 @@
+"""
+donors classes for mailroom_L9.py
+Author: JohnR
+Version: .1 (Lesson 09)
+Last updated: 2/16/2019
+Notes: Guidelines:
+    If a function affects a single donor, use Donor class
+    If a function affects multiple donors, use Collection class
+
+"""
+
+
+class Donor(object):
+    pass
+
+
+class Collection(Donor):
+    pass
+
+    def add_donor(self):
+        pass
+
+    def search_donor(self):
+        pass
+
+    def save_donor(self):
+        pass
