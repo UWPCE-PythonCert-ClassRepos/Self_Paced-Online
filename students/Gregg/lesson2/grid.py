e73957d20438443312efdac6d249bbc47dc935e2
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-02-17 19:28:22 -0800

    Complete grid part 2 a function that varies the grid side length

--- students/Gregg/lesson2/grid.py
+++ students/Gregg/lesson2/grid.py
@@ -18,9 +18,33 @@ def grid_no_inputs():
     #I only print it in the script
     return grid_string
 
+def grid2x2(side_length):
+    """Produce a 2x2 grid with sides of length side_length"""
+    repeats = abs(side_length//2)
+    corner = "+ "
+    h_side = "- "*repeats
+    h_spacer = " "*(repeats*2)
+    v_side = "| "
+    edge_row = corner+h_side+corner+h_side+corner
+    spacer_row = v_side + h_spacer + v_side +h_spacer + v_side
+    row_list = []
+    row_list.append(edge_row)
+    for i in range(2):
+        for i in range(repeats):
+            row_list.append(spacer_row)
+        row_list.append(edge_row)
+    grid_string = ('\n').join(row_list)
+    #print(grid_string) - seems to be more in keeping with a good funciton if
+    #I only print it in the script
+    return grid_string
 
 
 if __name__ == "__main__":
     print('Lesson 2: Grid Exercise')
     print('Lesson 2: Grid Exercise, Part 1 - a grid with no inputs')
     print(grid_no_inputs())
+    print('Lesson 2: Grid Exercise, Part 2 - grid side length from input')
+    print('With input = 3 - grid2x2(3):')
+    print(grid2x2(3))
+    print('With input = 15 - grid2x2(15):')
+    print(grid2x2(15))
