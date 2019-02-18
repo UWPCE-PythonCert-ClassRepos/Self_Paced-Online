52f42f2fbeb803702f929725d75e841b7118748c
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-02-17 19:38:17 -0800

    Complete grid part 3 a function that varies the grid size and side length

--- students/Gregg/lesson2/grid.py
+++ students/Gregg/lesson2/grid.py
@@ -19,7 +19,7 @@ def grid_no_inputs():
     return grid_string
 
 def grid2x2(side_length):
-    """Produce a 2x2 grid with sides of length side_length"""
+    """Produce a 2x2 grid with sides of length side_length//2"""
     repeats = abs(side_length//2)
     corner = "+ "
     h_side = "- "*repeats
@@ -34,9 +34,31 @@ def grid2x2(side_length):
             row_list.append(spacer_row)
         row_list.append(edge_row)
     grid_string = ('\n').join(row_list)
-    #print(grid_string) - seems to be more in keeping with a good funciton if
-    #I only print it in the script
     return grid_string
+
+def gridYxY(Y = 2, side_length = 4):
+    """Produce an YxY grid with sides of length side_length"""
+    side_length = abs(side_length//1)
+    Y = Y//1
+    corner = "+ "
+    h_side = "- "*side_length
+    h_spacer = " "*(side_length*2)
+    v_side = "| "
+    edge_row = corner
+    spacer_row = v_side
+    for i in range(Y):
+        edge_row = edge_row+h_side+corner
+        spacer_row = spacer_row+h_spacer+v_side
+    row_list = []
+    row_list.append(edge_row)
+    for i in range(Y):
+        for i in range(side_length):
+            row_list.append(spacer_row)
+        row_list.append(edge_row)
+    grid_string = ('\n').join(row_list)
+    return grid_string
+
+
 
 
 if __name__ == "__main__":
@@ -48,3 +70,9 @@ if __name__ == "__main__":
     print(grid2x2(3))
     print('With input = 15 - grid2x2(15):')
     print(grid2x2(15))
+    print('Lesson 2: Grid Exercise, Part 3 - grid with size and side length from input')
+    print('With input = (3,4) - gridYxY(3,4):')
+    print(gridYxY(3,4))
+    print('With input = (5,3) - gridYxY(5,3):')
+    print(gridYxY(5,3))
+
