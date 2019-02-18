3a7318b3a32c1bc89c722ce52d1266e16d4e5b22
dfspray <daniel_spray@comcast.net>
2019-02-18 10:38:39 -0800

    Put together non-operational organization of OO mailroom

--- students/Daniel_Spray/Lesson09/mailroom5.py
+++ students/Daniel_Spray/Lesson09/mailroom5.py
@@ -4,20 +4,23 @@ class Donor:
     """Create a class that sets up one donor"""
 
     def __init__(self,full_name,donation_history = None):
-        self.name = full_name
-        self.donations = donation_history if donation_history else []
+        self.full_name = full_name
+        if donation_history is None:
+            self.donation_history = []
+        else:
+            self.donation_history = donation_history
 
     @property
     def name(self):
-        return self.name
+        return self.full_name
 
     @name.setter
-    def name(self,key):
-        self.name = key
+    def name(self,new_name):
+        self.full_name = new_name
 		
     @property
     def donations(self):
-        return self.donations
+        return self.donation_history
 
     def num_gifts(self):
         return self.donations.len()
@@ -30,44 +33,73 @@ class Donor:
 
     def add_donation(self, new_donation):
         self.donations.append(new_donation)
+
+    def letter(self):
+        """Format a letter for one donor and donation"""	
+        content = """ 
+Dear {},
+
+Thank you for your generous donation of ${:.2f}
+
+Sincerely,
+The Charity
+""".format(str(self.full_name), float(self.donation_history[-1]))
+        print(content)
+        return(content)
  
 class Collection:
     """Create a class that manages a collection of donors"""
 
-    def __init__(self, names = None)
-        self.names = names if names else []
+    def __init__(self, full_names = None):
+        if full_names == None:
+            self.full_names = []
+        else:
+            self.full_names = full_names
 
     @property
     def names(self):
-        return self.names
+        return self.full_names
 
     def add_new(self,new_donor):
         self.names.append(new_donor)
 
     def list_all(self):
-        return " ,".join(self.names)
-   
+        return" ,".join(self.names)
 
-        
+    def create_report(self):
+        """Build a report"""
+        result = collection.calculation
+        collection.table(result)
+        return
 
+    def table(self,result):
+        """Make a formatted table from the sorted calculation data output"""
+        table_output = ["{:<24}{:<1}{:^13}{:<1}{:^13}{:<1}{:^17}".format('Donor Name','|','Total Given','|','Num Gifts','|','Average Gift')+"\n"+"-"*67]
+        for row in result:
+            table_output.append("{:<25}{:<1}{:>12.2f}{:>14}{:>2}{:>13.2f}".format(*row))
+        table_string = "\n"+"\n".join(table_output)+"\n"
+        print(table_string)
+        return table_string
 
+    def use_total(amounts):
+        return amounts[2]
 
+    def calculation(self):
+        """Calculate averages and return sorted data for each donor"""
+        data = []
 
+        for donor in self.full_names:
+            data.append(donor.name, donor.total_given, donor.num_gifts, donor.average)
 
+        sorted_data = sorted(data,key=Collection.use_total,reverse=True)
+        return sorted_data
 
-
-
-
-
-
-
-"""Establish donor data dictionary"""
-donation_data = {
-'William Gates, III': [100000.00,553784.49],
-'Mark Zuckerberg': [5000.00,5000.00,6396.10],
-'Jeff Bezos': [877.33],
-'Paul Allen': [100.00,100.00,508.42]
-}
+    def send_all(self):
+        """Write letters to all donors in text documents"""
+        for donor in self.full_names:
+            with open(donor.replace(' ','_')+'.txt','w') as f:
+                f.write(donor.letter())
+        print("Done!")
 
 def menu():
     """Create a selection menu"""
@@ -82,8 +114,8 @@ Type the corresponding number to select from the following list:
 
     switch_menu = {
         '1': send_thank_you,
-        '2': create_report,
-        '3': send_all,
+        '2': collection.create_report,
+        '3': collection.send_all,
         '4': quit
     }
     
@@ -93,89 +125,47 @@ Type the corresponding number to select from the following list:
         print("Sorry, I didn't recognize that command")
         return
 
-def quit():
-    """Quit the program"""
-    sys.exit()
-
 def send_thank_you():
     """Prompt inputs for new donation data"""
     name = input("Please enter a full name > ")
 	
     while name == "list":
-        list_of_donors = "{}, "*(len(donation_data)-1)+"{}"
-        print(list_of_donors.format(*donation_data))
+        print(Collection.list_all)
         name = input("Please enter a full name > ")
 
     if name.lower() == "quit":
         return
     
     while True:
+        donor = Donor(name)
+        if donor not in Collection().names:
+            Collection().add_new(name)
         donation = input("Donation Amount? > ")
         if donation.lower() == "quit":
             return
         try:
-            donation_data.setdefault(name,[]).append(float(donation))
+            donor.add_donation(donation)
             break
         except ValueError:
             print("That's not a valid donation")
 		
     print("Data added!")
-    letter_dictionary = {'donor':name,'amount':round(float(donation),2)}
-    letter(letter_dictionary)
-
-    return donation_data
-	
-def letter(letter_dictionary):
-    """Format a letter for one donor and donation"""	
-    content = """
-Dear {donor},
-
-Thank you for your generous donation of ${amount:.2f}
-
-Sincerely,
-The Charity
-""".format(**letter_dictionary)
-    print(content)
-    return(content)
-
-def create_report():
-    """Build a report"""
-    result = calculation()
-    table(result)
+    donor.letter()
     return
 
-def table(result):
-    """Make a formatted table from the sorted calculation data output"""
-    table_output = ["{:<24}{:<1}{:^13}{:<1}{:^13}{:<1}{:^17}".format('Donor Name','|','Total Given','|','Num Gifts','|','Average Gift')+"\n"+"-"*67]
-    for row in result:
-        table_output.append("{:<25}{:<1}{:>12.2f}{:>14}{:>2}{:>13.2f}".format(*row))
-    table_string = "\n"+"\n".join(table_output)+"\n"
-    print(table_string)
-    return table_string
+def quit():
+    """Quit the program"""
+    sys.exit()
 
-def use_total(amounts):
-    return amounts[2]
+"""Add the default data to classes"""
+Donor('William Gates, III', [100000.00,553784.49])
+Donor('Mark Zuckerberg', [5000.00,5000.00,6396.10])
+Donor('Jeff Bezos', [877.33])
+Donor('Paul Allen', [100.00,100.00,508.42])
 
-def calculation():
-    """Calculate averages and return sorted data for each donor"""
-    data = []
-
-    for donor, donations in donation_data.items():
-        total_given = round(sum(donations),2)
-        num_gifts = len(donations)
-        average = round(float(total_given)/float(num_gifts),2)
-        data.append([donor, "$", total_given, num_gifts, "$", average])
-
-    sorted_data = sorted(data,key=use_total,reverse=True)
-    return sorted_data
-
-def send_all():
-    """Write letters to all donors in text documents"""
-    for person in donation_data:
-        with open(person.replace(' ','_')+'.txt','w') as f:
-            f.write(letter({'donor':person,'amount':donation_data[person][-1]}))
-    print("Done!")
+collection = Collection(['William Gates, III','Mark Zuckerberg','Jeff Bezos','Paul Allen'])
 
 if __name__ == '__main__':
     while True:
         menu()
+
