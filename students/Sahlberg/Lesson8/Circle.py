65a16e464e801bd7cfc3717acfbb7d21bdf42e5c
Ian Sahlberg <iansahlberg@gmail.com>
2019-02-26 20:30:18 -0800

    File testing. Finished Assingnment.

--- students/Sahlberg/Lesson8/Circle.py
+++ students/Sahlberg/Lesson8/Circle.py
@@ -4,20 +4,24 @@
 class Circle(object):
     import math as m
 
-    def __init__(self,radius):
-        self.radius = radius
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
 
     @property
     def diameter(self):
-         return self.radius*2
+         return self._radius*2
 
     @property
     def area(self):
-        return float(f'{Circle.m.pi * self.radius**2:.4f}')
+        return float(f'{Circle.m.pi * self._radius**2:.4f}')
 
     @diameter.setter
     def diameter(self, diameter):
-        self.radius = diameter/2
+        self._radius = _diameter/2
 
     @classmethod
     def from_diameter(cls, diameter):
@@ -25,20 +29,33 @@ class Circle(object):
         return cls(radius)
 
     def __str__(self):
-        return f'Circle with radius: {self.radius:.6f}'
+        return f'Circle with radius: {self._radius:.6f}'
 
     def __repr__(self):
-        return f'Circle({self.radius:.0f})'
+        return f'Circle({self._radius:.0f})'
 
     def __add__(self,circle_x):
-        new_circle = Circle(self.radius + circle_x.radius)
+        new_circle = Circle(self._radius + circle_x.radius)
         return new_circle
 
+    def __mul__(self, other):
+        if isinstance(other, Circle):
+            return Circle(self._radius * other._radius)
+        else:
+            return Circle(self._radius * other)
 
-c = Circle(10)
-print(c)
+    __rmul__ = __mul__
 
-print(repr(c))
-print(c.radius)
-print(c.diameter)
-print(c.area)
+    def __lt__(self, other):
+        return self._radius < other.radius
+
+    def __gt__(self, other):
+        return self._radius > other.radius
+
+    def __eq__(self, other):
+        return self._radius == other.radius
+
+
+
+circles = [Circle(6), Circle(7), Circle(8), Circle(4), Circle(0), Circle(2), Circle(3), Circle(5), Circle(9), Circle(1)]
+
--- students/Sahlberg/Lesson8/circle_test.py
+++ students/Sahlberg/Lesson8/circle_test.py
@@ -1,13 +1,11 @@
 import unittest as u
-
 from Circle import *
-
+from io import StringIO
 c = Circle(4)
 
 print(c.radius)
 
 class test_circle(u.TestCase):
-    from io import StringIO
 
     def test_radius(self):
         c = Circle(4)
@@ -26,6 +24,31 @@ class test_circle(u.TestCase):
         d = Circle.from_diameter(10)
         self.assertEqual(10,d.diameter)
         self.assertEqual(5,d.radius)
+
+    def test_string(self):
+        self.assertEqual(f'Circle with radius: {6:.6f}', str(Circle(6)))
+
+    def test_rep(self):
+        self.assertEqual(f'Circle({6:.0f})', repr(Circle(6)))
+
+    def test_addition(self):
+        self.assertEqual(Circle(6), Circle(2) + Circle(4))
+
+    def test_multiply(self):
+        self.assertEqual((Circle(6) * 2), Circle(12))
+        self.assertEqual((2 * Circle(6)), Circle(12))
+
+    def test_less_than(self):
+        self.assertEqual(Circle(2) < Circle(66), True)
+        self.assertEqual(Circle(2) > Circle(66), False)
+
+    def test_great_than(self):
+        self.assertEqual(Circle(22) > Circle(6), True)
+        self.assertEqual(Circle(22) > Circle(66), False)
+    def test_equality(self):
+        self.assertEqual(True, True)
+        self.assertEqual(Circle(6), Circle(6))
+        self.assertEqual(Circle(6), (Circle(2) + Circle(4)))
 
 if __name__ == '__main__':
     u.main()
