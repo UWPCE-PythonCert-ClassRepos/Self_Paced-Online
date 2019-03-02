76887bebd91594a5401a8ee759652eef400b0817
HawkeyeUW <virtuej@outlook.com>
2019-03-02 13:38:27 -0500

    Refactor main menu and added pytest unit test cases

--- students/jvirtue/lesson06/mailroom_ut.py
+++ students/jvirtue/lesson06/mailroom_ut.py
@@ -3,6 +3,9 @@
 #Jason Virtue 02/25/2019
 #UW Self Paced Python Course
 
+#Data Set
+donors_db = {'Fred Flintstone': [100,200],'Wilma Flintstone': [300],'Bamm-Bamm Rubble': [50,30,40],'Barney Rubble' : [75],'Pebbles Flintstone': [500]}
+
 #Option menu 1 -- input donors and amounts into DICT
 def send_thanks(db_name):
     print("-"*40)
@@ -38,14 +41,13 @@ def thanks_file(db_name):
     for data in db_name:
         filename = data.replace(" ", "_") + ".txt"
         total_donation = sum(db_name[data])
-        letter = ('Thank you {} for you generous contributions totalling {:.2f}!'.format(data, total_donation))
+        letter = ('Thank you {} for you generous contributions totaling {:.2f}!'.format(data, total_donation))
         open(filename, 'w').write(letter)
         print(f"{data}'s letter has been saved to " + filename)
 
 #Build list of donors, amount, count of contributions and average gift amount
 def crunch_numbers(db_name):
     crunch_numbers = [[donor,sum(db_name[donor]),len(db_name[donor]),sum(db_name[donor])/len(db_name[donor])] for donor in db_name] #changed to list comprehension
-    crunch_numbers.sort(key=sort_key,reverse=True)
     return crunch_numbers
 
 #Sort key for donor list.  Sort by donation amount
@@ -60,12 +62,12 @@ def create_report(db_name):
     sort_table = crunch_numbers(db_name)
     [print(f'{item[0]:<20}{" ":<1}{"$":<1}{item[1]:>14}{" ":<1}{item[2]:>15}{" ":<1}{"$":<1}{item[3]:>14}') for item in sort_table]
 
+#Main menu quit program
 def quit(db_name):
     return "Quit"
 
-#Options Menu
-if __name__ == '__main__':
-    donors_db = {'Fred Flintstone': [100,200],'Wilma Flintstone': [300],'Bamm-Bamm Rubble': [50,30,40],'Barney Rubble' : [75],'Pebbles Flintstone': [500]}
+#Main menu function
+def main_menu():
     menu_dict = {1:send_thanks,2:create_report,3:thanks_file,4:quit}
     while True:
         print("\n","-"*40,"\nMailroom Main Menu;\n","1: Send a Thank You\n","2: Create a Report\n","3: Send letters to everyone\n","4: Quit\n","-"*40,sep="")
@@ -79,3 +81,7 @@ if __name__ == '__main__':
                 break         
         except KeyError:
             print("-"*40,"\nPlease enter a valid number between 1 to 4.\n","-"*40,sep="")
+
+#Options Menu
+if __name__ == '__main__':
+    main_menu()
