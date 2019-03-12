aeaf543c555fd0e47e3c5e231b7f1870c338ab4c
Muhammad Z. Khan <mzkhan2@uw.edu>
2019-03-12 16:41:53 -0400

    removed the line .close() when open a file using with

--- students/MzKhan/lesson04/trigram.py
+++ students/MzKhan/lesson04/trigram.py
@@ -9,22 +9,21 @@ import random
 import re
 
 def read_text(filename):
-    '''
+    """
     The method reads the data from the text file.
-    parm: text file
-    return: text
-    '''
+    parm: file  a text file
+    return: string     a string of text data.
+    """
     with open(filename, 'r') as f:
         text = f.read()
-        f.close()
     return text
 
 def write_text(filename, text_to_write):
-    '''
-    This method writes the data to an output file.
+    """
+    This method writes the text data to an output file.
     parm: file
-    parm: text data
-    '''
+    parm: text
+    """
     filename = filename.replace('.txt','_trigram_story.txt')
     text = text_to_write.split()
     with open(filename,'w') as out:
@@ -37,9 +36,13 @@ def write_text(filename, text_to_write):
             max += 15
             if max > len(text):
                 out.write(' '.join(text[max:]))
-        out.close()
 
 def create_filtered_list(filename):
+    """
+    This method filters the data in the text file from all non numeric characters.
+    parm: file
+    return: list
+    """
     text = read_text(filename)
     pattern = re.compile(r'[\r\n\t\--]')
     text = re.sub(pattern,' ',text)
@@ -50,6 +53,11 @@ def create_filtered_list(filename):
     return text_list
 
 def create_trigram(text_list):
+    """
+    Convert the text in the list to a dictionary.
+    parm: list
+    return: dict
+    """
     trigram= {}
     for i in range(0,len(text_list)-2):
         key1,key2, value = text_list[i], text_list[i+1], text_list[i+2]
@@ -57,16 +65,17 @@ def create_trigram(text_list):
     return trigram
 
 def get_random_pair(trigram):
-    '''
-    This method returns a random key pair from the trigram dictionary
-    return: tuple
-    '''
+    """
+    return a random key pair from the trigram dictionary
+    parm: dict
+    """
     return random.choice(list(trigram.keys()))
 
 def generate_new_text(trigram, number_of_words=500):
     """
     This method generates a trigram of words.
     parm: dict
+    parm: kwarg1
     return: string
     """
     new_text = list(get_random_pair(trigram))
@@ -83,9 +92,10 @@ def generate_new_text(trigram, number_of_words=500):
     return ' '.join(new_text)
 
 def trigram_story(filename,number_of_words=500):
-    """ This method generates a trigram story
+    """
+    This method generates a trigram story
     parm: string
-    parm: int
+    parm: int (kwarg)
     """
     text_list= create_filtered_list(filename)
     trigram_dict = create_trigram(text_list)
