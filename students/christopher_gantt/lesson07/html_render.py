ab400275a1df0186041b6b698dbf8564c06a7d7e
ganttArt <gantt.art@gmail.com>
2019-03-25 09:26:45 -0700

    commiting html renderer through step 3

---
+++ students/christopher_gantt/lesson07/html_render.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element(object):
+    tag = "html"
+
+    def __init__(self, content=None):
+        if content is None:
+            self.contents = []
+        else:
+            self.contents = [content]
+
+    def append(self, new_content):
+        self.contents.append(new_content)
+
+    def render(self, out_file):
+        out_file.write("<{}>\n".format(self.tag))
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
+
+
+class Html(Element):
+    tag = "html"
+
+class Body(Element):
+    tag = "body"
+
+class P(Element):
+    tag = "p"
+
+class Head(Element):
+    tag = 'head'
+
+class OneLineTag(Element):
+    def render(self, out_file):
+        out_file.write("<{}>".format(self.tag))
+        out_file.write(self.contents[0])
+        out_file.write("</{}>\n".format(self.tag))
+
+    def append(self, content):
+        raise NotImplementedError
+
+class Title(OneLineTag):
+    tag = "title"
+
+
