238cba391094bd5acdc1cd53c5053a47fd16db72
guntur <ramakanth.gunturu@gmail.com>
2019-03-24 23:04:32 -0700

    Added the projected function for single donor

--- students/g_rama/lesson10/mailroom_fp.py
+++ students/g_rama/lesson10/mailroom_fp.py
@@ -35,16 +35,28 @@ if __name__ == '__main__':
         max_donation = CLI.max_donation_input()
         donors.multiply_factor(factor, min_donation, max_donation, **donors.donors_collection_data)
 
+    def projections():
+        fullname = CLI.fullname_input()
+        if fullname in donors.donors_collection_data.keys():
+            projected_donor = {fullname: donors.donors_collection_data[fullname]}
+            factor = CLI.mulfactor_input()
+            min_donation = CLI.min_donation_input()
+            max_donation = CLI.max_donation_input()
+            donors.multiply_factor(factor, min_donation, max_donation, **projected_donor)
+
+
     main_prompt = ("1 - Send a Thank You\n"
                    "2 - Create a Report\n"
                    "3 - Send letters to everyone\n"
                    "4 - Challenge factor\n"
-                   "5 - Quit\n")
+                   "5 - Projections with factors\n"
+                   "6 - Quit\n")
     main_dispatch = {"1": thank_you,
                      "2": donor.create_report,
                      "3": donors.send_letters,
                      "4": challenge_factor,
-                     "5": cli.exit_menu,
+                     "5": projections,
+                     "6": cli.exit_menu,
                      }
     cli.menu_selection(main_prompt, main_dispatch)
 
