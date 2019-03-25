5a49265450715051ace449c67160bac9590f6419
guntur <ramakanth.gunturu@gmail.com>
2019-03-24 20:16:13 -0700

    cleaning up DonorCollection class

--- students/g_rama/lesson10/DonorCollection.py
+++ students/g_rama/lesson10/DonorCollection.py
@@ -47,27 +47,3 @@ class DonorCollection:
                                                   donation_values))
             data[name] = list(map(lambda amount: amount * factor, min_max_donation_values))
         self.donor_details(**data)
-
-
-
-
-
-
-
-
-
-# ages = [5, 12, 17, 18, 24, 32]
-#
-# def myFunc(x):
-#   if x < 18:
-#     return False
-#   else:
-#     return True
-#
-# adults = filter(myFunc, ages)
-#
-# for x in adults:
-#   print(x)
-
-
-
