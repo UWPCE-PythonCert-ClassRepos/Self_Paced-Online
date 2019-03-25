766cd6b3e38c0b88e9c2e8d7ef75a5093ddc33d5
guntur <ramakanth.gunturu@gmail.com>
2019-03-24 18:16:51 -0700

    Moved the cli interaction from donr to mailroom_fp class

--- students/g_rama/lesson10/Donor.py
+++ students/g_rama/lesson10/Donor.py
@@ -9,23 +9,7 @@ class Donor(DonorCollection):
             super().donors_collection_data.update({fullname: [amount]})
             self.thank_you_letter(fullname, amount)
 
-    def thank_you(self):
-        """Send a thank you letter"""
-        fullname = CLI.fullname_input()
-        if fullname.isalpha():
-            if fullname == str("list"):
-                super().display_donors()
-            elif fullname in super().donors_collection_data.keys():
-                amount = CLI.amount_input()
-                self.update_data_print_thanks(amount, fullname)
-            else:
-                try:
-                    amount = CLI.amount_input()
-                    self.add_data_print_thanks(amount, fullname)
-                except ValueError:
-                    print("Enter the correct amount in integer")
-        else:
-            print("Enter the donor name correctly")
+
 
     def amount_validate(self, amount):
         if amount < 0:
