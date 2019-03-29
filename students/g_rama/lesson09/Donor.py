51e9e65f14bd543cd7c2a5f862286f5592fd9f4e
guntur <ramakanth.gunturu@gmail.com>
2019-03-28 22:46:07 -0700

    Reworked to make Donor class more object oriented

--- students/g_rama/lesson09/Donor.py
+++ students/g_rama/lesson09/Donor.py
@@ -4,26 +4,29 @@ from CLI import CLI
 
 class Donor(DonorCollection):
 
-    def add_data_print_thanks(self, amount, fullname):
-        if self.amount_validate(amount):
-            super().donors_collection_data.update({fullname: [amount]})
-            self.thank_you_letter(fullname, amount)
+    def __init__(self, fullname, amount):
+        self.fullname = fullname
+        self.amount = amount
 
+    def thank_you_letter(self):
+        """Send a thank you letter"""
+        print(f"Thank you {self.fullname} for donating {self.amount} dollars generously.")
+        return f"Thank you {self.fullname} for donating {self.amount} dollars generously."
 
-    def amount_validate(self, amount):
-        if amount < 0:
+    def add_data_print_thanks(self):
+        if self.amount_validate():
+            super().donors_collection_data.update({self.fullname: [self.amount]})
+            self.thank_you_letter()
+
+    def amount_validate(self):
+        if self.amount < 0:
             print("Enter the correct amount with out negitives")
         else:
             return True
 
-    def thank_you_letter(self, donor_name, amount):
-        """Send a thank you letter"""
-        print(f"Thank you {donor_name} for donating {amount} dollars generously.")
-        return f"Thank you {donor_name} for donating {amount} dollars generously."
-
-    def update_data_print_thanks(self, amount, fullname):
-        if self.amount_validate(amount):
-            super().donors_collection_data[fullname].append(amount)
-            self.thank_you_letter(fullname, amount)
+    def update_data_print_thanks(self):
+        if self.amount_validate():
+            super().donors_collection_data[self.fullname].append(self.amount)
+            self.thank_you_letter()
 
 
