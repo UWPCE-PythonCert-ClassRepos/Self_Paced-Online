20d2e87234d4bc073dc4d91faff11bd1e89047fc
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-03-31 13:59:25 -0700

    Began work in dict lab

---
+++ students/Gregg/lesson4/dict_lab.py
@@ -0,0 +1,21 @@
+"""Copy this so your ecxercises are nice and neat"""
+
+
+def function(input):
+    """Reorder sequence switching the first and last elements"""
+
+def tests():
+    """Test the ________ functions"""
+    pass
+
+def exercise_demo(func_in):
+    print(f'LessonX: _______ Exercise - {func_in.__doc__}')
+    print(f'Demo of {func_in()}')
+    print('')
+
+if __name__ == "__main__":
+    tests()
+    print('LessonX: _______ Exercise')
+    exercise_demo(funtion)
+
+
