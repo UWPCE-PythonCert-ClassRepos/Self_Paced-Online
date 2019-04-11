efa8c3713b4c2f0f860e70ef62e725c49dd40c07
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2019-04-10 21:06:01 -0700

    Finished all except mid_last_first and need to add docstrings.

--- students/Ramkumar_Rajanbabu/Lesson03/Slicing_Lab.py
+++ students/Ramkumar_Rajanbabu/Lesson03/Slicing_Lab.py
@@ -1,13 +1,23 @@
 #Slicing Lab
 
-seq = "this is a string"
-tup = (2, 54, 13, 12, 5, 32)
+s = "this is a string"
+t = (2, 54, 13, 12, 5, 32)
+l = [2, 54, 13, 12, 5, 32]
 
 def exchange_first_last(seq):
-    first = seq[-1] 
-    mid = seq[1:-1]
-    last = seq[0]
-    new_seq = first + mid + last 
+    if seq == tuple(seq):
+        first = seq[0],
+        mid = seq[1:-1]
+        last = seq[-1],
+    elif seq == list(seq):
+        first = [seq[0]]
+        mid = seq[1:-1]
+        last = [seq[-1]]
+    else:
+        first = seq[0]
+        mid = seq[1:-1]
+        last = seq[-1]
+    new_seq = last + mid + first 
     return new_seq
 
 def other_remove(seq):
@@ -26,17 +36,23 @@ def mid_last_first(seq):
     pass
 
 if __name__ == "__main__":
-   #Run some tests
-   assert exchange_first_last(seq) == "ghis is a strint"
-   assert other_remove(seq) == "ti sasrn"
-   assert first_last_other_remove(seq) == " sas"
-   assert reverse(seq) == "gnirts a si siht"
-   #assert mid_last_first(seq) == "is a stringthis "
-   
-   #assert exchange_first_last(tup) == (32, 54, 13, 12, 5, 2)
-   assert other_remove(tup) == (2, 13, 5)
-   assert first_last_other_remove(tup) == ()
-   assert reverse(tup) == (32, 5, 12, 13, 54, 2)
-   #assert mid_last_first(tup) == (13, 12, 5, 32, 2, 54)
-   
-   print("Tests passed!")
+    #Run some tests
+    assert exchange_first_last(s) == "ghis is a strint"
+    assert other_remove(s) == "ti sasrn"
+    assert first_last_other_remove(s) == " sas"
+    assert reverse(s) == "gnirts a si siht"
+    #assert mid_last_first(s) == "is a sthis "
+    
+    assert exchange_first_last(t) == (32, 54, 13, 12, 5, 2)
+    assert other_remove(t) == (2, 13, 5)
+    assert first_last_other_remove(t) == ()
+    assert reverse(t) == (32, 5, 12, 13, 54, 2)
+    #assert mid_last_first(t) == (13, 12, 5, 32, 2, 54)
+
+    assert exchange_first_last(l) == [32, 54, 13, 12, 5, 2]
+    assert other_remove(l) == [2, 13, 5]
+    assert first_last_other_remove(l) == []
+    assert reverse(l) == [32, 5, 12, 13, 54, 2]
+    #assert mid_last_first(l) == [13, 12, 5, 32, 2, 54]    
+    
+    print("Tests passed!")
