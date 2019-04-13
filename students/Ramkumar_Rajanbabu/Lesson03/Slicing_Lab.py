7d0f6217a6ab62a63b470335420c043a3c6d670f
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2019-04-13 12:24:47 -0700

    Finished only Slicing_Lab, still working on other assignments.

--- students/Ramkumar_Rajanbabu/Lesson03/Slicing_Lab.py
+++ students/Ramkumar_Rajanbabu/Lesson03/Slicing_Lab.py
@@ -5,6 +5,15 @@ t = (2, 54, 13, 12, 5, 32, 24, 78, 89, 64, 4)
 l = [2, 54, 13, 12, 5, 32, 24, 78, 89, 64, 4]
 
 def exchange_first_last(seq):
+    """Exchange the first and last items in a sequence. 
+    
+    Args:
+        seq: a string, tuple or list.
+    Returns: 
+        new_seq: a string, tuple or list with first and last items exchanged.
+        
+    """
+    
     if seq == tuple(seq):
         first = seq[0],
         mid = seq[1:-1]
@@ -14,25 +23,63 @@ def exchange_first_last(seq):
         mid = seq[1:-1]
         last = [seq[-1]]
     else:
-        first = seq[0]
+        first = seq[0] 
         mid = seq[1:-1]
         last = seq[-1]
     new_seq = last + mid + first 
     return new_seq
 
 def other_remove(seq):
+    """Remove every other item in a sequence. 
+    
+    Args:
+        seq: a string, tuple or list.
+    Returns: 
+        new_seq: a string, tuple or list with every other item removed.
+        
+    """
+    
     new_seq = seq[::2]
     return new_seq
 
 def first_last_other_remove(seq):
+    """Remove the first and last 4 items, then remove every other item in a sequence. 
+    
+    Args:
+        seq: a string, tuple or list.
+    Returns: 
+        new_seq: a string, tuple or list with first and last 4 items removed,
+        then every other item removed.
+        
+    """
+        
     new_seq = seq[4:-4:2]
     return new_seq
 
 def reverse(seq):
+    """Reverse items in a sequence. 
+    
+    Args:
+        seq: a string, tuple or list.
+    Returns: 
+        new_seq: a string, tuple or list with the items reversed.
+        
+    """
+    
     new_seq = seq[::-1]
     return new_seq
 
 def last_first_mid(seq):
+    """Display in order of the last third, first third and middle third of a sequence. 
+    
+    Args:
+        seq: a string, tuple or list.
+    Returns: 
+        new_seq: a string, tuple or list in a new order with the last third, first third
+        and middle third. 
+        
+    """
+    
     seq_len = int(len(seq) / 3)
     
     first = seq[0:seq_len]
