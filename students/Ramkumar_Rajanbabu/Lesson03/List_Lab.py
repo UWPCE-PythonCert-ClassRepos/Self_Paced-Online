f6d302cfa543d59ceb3fd30a0da2598ac7cb03cb
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2019-04-14 17:57:51 -0700

    Finished series1,2,4. Still need to do series 3 and docstrings.

--- students/Ramkumar_Rajanbabu/Lesson03/List_Lab.py
+++ students/Ramkumar_Rajanbabu/Lesson03/List_Lab.py
@@ -1,82 +1,90 @@
 #List Lab
 
-#Series 1
 list1 = ["Apples", "Pears", "Oranges", "Peaches"] #Create a list
-print(list1) #Display list
 
-fruit = str(input("Enter the fruit to add to the list: ")) #Ask user to add fruit to list
-list1.append(fruit) #Add fruit to end of list
-print(list1) #Display list 
+#All functions for Series 1-4
+def current_list():
+    list1 = ["Apples", "Pears", "Oranges", "Peaches"] #Create a list
+    print("Current List:", list1, "\n") #Display list and then a new line
 
-num = int(input("Enter the number to choose the fruit from the list: ")) #Ask user to choose number for fruit
-num -= 1 #Using -1 since items in list is chosen from index of 0
-if num < 0 : 
-    print("Please enter a number of 1 or above.") #User can't choose 0 as an option
-else:
-    print(list1[num]) #Display list
-    
-list2 = ["Pineapple"] #Creating a new list to add item to existing list
-list1 = list2 + list1 #Adding item to the beginning of the list
-print(list1) #Display list
+def add_fruit():
+    fruit = str(input("Enter the fruit to add to the list: ")) #Ask user to add fruit to list
+    list1.append(fruit) #Add fruit to end of list
+    print("New List:", list1, "\n") #Display list and then a new line
 
-list1.insert(0, "Watermelon") #Using insert to add item to beginning of list
-list1 #Display list
-
-for fruit in list1: #For every fruit in the list
-    if fruit.startswith("P"): #If statement looks for fruits that start with P
-        print(fruit) #Prints the fruit from the list
-        
-#Series 2
-list1 = ["Apples", "Pears", "Oranges", "Peaches"] #Create a list
-print(list1) #Display list
-
-list1.pop() #removes last item in list
-print(list1)
-
-answer = str(input("Enter the name of the fruit to delete: "))
-if answer == "Apples":
-    list1.remove("Apples")
-elif answer == "Pears":
-    list1.remove("Pears")
-elif answer == "Oranges":
-    list1.remove("Oranges")
-elif answer == "Peaches":
-    list1.remove("Peaches")
-else:
-    print("Please choose a fruit to delete that is within the list.")
-print(list1)
-
-#Series 3
-list1 = ["Apples", "Pears", "Oranges", "Peaches"] #Create a list
-print(list1) #Display list
-
-while True:
-    answer = str(input("Do you like apples? "))
-    if answer == "no":
-        list1.remove("Apples")
-        print(list1)
-
-    answer = str(input("Do you like pears? "))
-    if answer == "no":
-        list1.remove("Pears")
-        print(list1)
-
-    answer = str(input("Do you like oranges? "))
-    if answer == "no":
-        list1.remove("Oranges")
-        print(list1)
-
-    answer = str(input("Do you like peaches? "))
-    if answer == "no":
-        list1.remove("Peaches")
-        print(list1)
+def choose_fruit():
+    num = int(input("Enter the number to choose the fruit from the list: ")) #Ask user to choose number for fruit
+    num -= 1 #Using -1 since items in list is chosen from index of 0
+    if num < 0 : 
+        print("Please enter a number of 1 or above.\n", ) #User can't choose 0 as an option
     else:
-        print("Please answer between yes or no.")
-        
-#Series 4
-list1 = ["Apples", "Pears", "Oranges", "Peaches"] #Create a list
-print(list1) #Display list
+        print("Chosen Fruit:", list1[num], "\n") #Display list and then a new line
+        #print(list1[num]) #Display list
 
-reverse = (list1[0][::-1], list1[1][::-1], list1[2][::-1], list1[3][::-1])
-print(reverse)
+def add_fruit_plus(list1):
+    list2 = ["Pineapple"] #Creating a new list to add item to existing list
+    list1 = list2 + list1 #Adding item to the beginning of the list
+    print("New List with Plus Method:", list1, "\n") #Display list and then a new line
+    
+def add_fruit_insert():
+    list1.insert(0, "Watermelon") #Using insert to add item to beginning of list
+    print("New List with Insert Method:", list1, "\n") #Display list and then a new line
+    
+def p_fruit():
+    print("Fruits that start with P:")
+    for fruit in list1: #For every fruit in the list
+        if fruit.startswith("P"): #If statement looks for fruits that start with P
+            print(fruit, end="\n") #Prints the fruit from the list
+    print()
 
+def remove_last_fruit():
+    list1.pop() #removes last item in list
+    print("New List:", list1, "\n") #Display list and then a new line
+
+def del_fruit():
+    answer = str(input("Enter the name of the fruit to delete: "))
+    if answer == "Apples":
+        list1.remove("Apples")
+    elif answer == "Pears":
+        list1.remove("Pears")
+    elif answer == "Oranges":
+        list1.remove("Oranges")
+    elif answer == "Peaches":
+        list1.remove("Peaches")
+    else:
+        print("Please choose a fruit to delete that is within the list.\n")
+    print("New List:", list1, "\n") #Display list and then a new line
+
+#Series 1-4
+def series_1(list1):
+    current_list()
+    add_fruit()
+    choose_fruit()
+    add_fruit_plus(list1)
+    add_fruit_insert()
+    p_fruit()
+
+def series_2():
+    current_list()
+    remove_last_fruit()
+    del_fruit()
+    
+def series_4():
+    current_list()
+    fruits = [item[::-1] for item in list1] #Reverse each item in the list but order stays the same
+    list1.pop()
+    print("New List in Reverse:", fruits, "\n") #Display list and then a new line
+    print("Original List:", list1, "\n") #Display list and then a new line
+    
+if __name__ == "__main__":
+    #Perform series 1-4
+    print("***************************************************************************")
+    print("Series 1: ")
+    series_1(list1)
+    print("***************************************************************************")
+    print("Series 2: ")
+    series_2()
+    print("***************************************************************************")
+    print("Series 4: ")
+    series_4()
+    print("***************************************************************************")
