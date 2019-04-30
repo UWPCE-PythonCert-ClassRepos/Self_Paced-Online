5992b3fb49dbc84f8cbbea7e09c797e90797605e
smckellips <sean.mckellips@gmail.com>
2019-04-29 22:26:09 -0700

    initial mailroom.py

---
+++ students/smckellips/lesson03/mailroom.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+donor_db = [("William Gates, III", [653772.32, 12.17]),
+            ("Jeff Bezos", [877.33]),
+            ("Paul Allen", [663.23, 43.87, 1.32]),
+            ("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]),
+            ]
+
+donors = [x[0] for x in donor_db]
+prompt = "\n".join(("Welcome to the mailroom!",
+          "Please choose from below options:",
+          "1 - Send a thank you",
+          "2 - Create a report",
+          "3 - Exit",
+          ">>> "))
+
+def thank_you():
+    # donor = input("Name of the donor? ").title()
+    donor = "Paul Allen"
+    donor_db = donor_db
+    if donor == "List":
+        print("\n".join(x[0] for x in donor_db))
+        thank_you()
+    else:
+        if donor in donors:
+
+            # donor_name = [x[0] for x in donor_db if x[0] == donor]
+            # donor_donations = [x[1] for x in donor_db if x[0] == donor]
+
+            donor_e = [(x[0],x[1]) for x in donor_db if x[0] == donor]
+            donor_name = donor_e[0]
+            donor_donations = donor_e[1]
+        else:
+            donor_name = donor
+            donor_donations = []
+        donation_amt = float(input("Amount of the donation? "))
+        donor_donations.append(donation_amt)
+
+        donor_db = [d for d in donor_db if d[0] != donor]
+        donor_db.append((donor_name,donor_donations))        
+        print()
+def main():
+    while True:
+        response = input(prompt)  # continuously collect user selection
+        # now redirect to feature functions based on the user selection
+        if response == "1":
+            thank_you()
+        elif response == "2":
+            add_fruit()
+        elif response == "3":
+            remove_fruit()
+        elif response == "4":
+            exit_program()
+        else:
+            print("Not a valid option!")
+
+
+if __name__ == "__main__":
+    main()
