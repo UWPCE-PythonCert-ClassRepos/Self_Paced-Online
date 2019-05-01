f8be5468cc5984aec42c3f4d8ee30a25137fcef9
smckellips <sean.mckellips@gmail.com>
2019-04-30 20:01:00 -0700

    Adding create_report

--- students/smckellips/lesson03/mailroom.py
+++ students/smckellips/lesson03/mailroom.py
@@ -13,13 +13,12 @@ prompt = "\n".join(("Welcome to the mailroom!",
           "3 - Exit",
           ">>> "))
 
-def thank_you():
-    # donor = input("Name of the donor? ").title()
-    donor = "Paul Allen"
-    donor_db = donor_db
+def thank_you(donor_db):
+    donor = input("Name of the donor? ").title()
+    # donor = "Paul Allen"
     if donor == "List":
         print("\n".join(x[0] for x in donor_db))
-        thank_you()
+        thank_you(donor_db)
     else:
         if donor in donors:
 
@@ -27,8 +26,8 @@ def thank_you():
             # donor_donations = [x[1] for x in donor_db if x[0] == donor]
 
             donor_e = [(x[0],x[1]) for x in donor_db if x[0] == donor]
-            donor_name = donor_e[0]
-            donor_donations = donor_e[1]
+            donor_name = donor_e[0][0]
+            donor_donations = donor_e[0][1]
         else:
             donor_name = donor
             donor_donations = []
@@ -36,20 +35,28 @@ def thank_you():
         donor_donations.append(donation_amt)
 
         donor_db = [d for d in donor_db if d[0] != donor]
-        donor_db.append((donor_name,donor_donations))        
-        print()
+        donor_db.append((donor_name,donor_donations))
+
+        letter = "Thank you, {}, for your kind donation of ${:,.2f}"
+        print(letter.format(donor_name,donation_amt))
+
+
+def create_report():
+    print("Donor Name                | Total Given | Num Gifts | Average Gift" )
+    print("-" * 65)
+    for donor in donor_db:
+        print("{:<26} $ {:>,.2f} )
+
 def main():
     while True:
         response = input(prompt)  # continuously collect user selection
         # now redirect to feature functions based on the user selection
         if response == "1":
-            thank_you()
+            thank_you(donor_db)
         elif response == "2":
-            add_fruit()
+            create_report()
         elif response == "3":
-            remove_fruit()
-        elif response == "4":
-            exit_program()
+            exit()
         else:
             print("Not a valid option!")
 
