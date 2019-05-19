a4f1a5a249636d37db2da398ba35da080f693165
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-05-19 00:02:10 -0700

    Now our circle has a radius

--- students/Gregg/lesson8/circle.py
+++ students/Gregg/lesson8/circle.py
@@ -3,10 +3,26 @@
 
 class Circle():
     """A circle object that has all the attributes and methods of a circle"""
-
+    _radius = None
 
     def __init__(self, radius):
         self.radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius_in):
+        if radius_in > 0:
+            self._radius = radius_in
+        else:
+            raise(ValueError('The radius must be a positive number'))
+
+
+    @property
+    def diameter(self):
+        return 2*self.radius
 
     def __eq__(self, other):
         return self.radius == other.radius
--- students/Gregg/lesson8/test_circle.py
+++ students/Gregg/lesson8/test_circle.py
@@ -5,7 +5,12 @@ from circle import *
 def test_circle_radius():
     assert(4 == Circle(4).radius)
     assert(2 == Circle(2).radius)
+    with pytest.raises(ValueError):
+        Circle(-1)
 
+def test_circle_diameter():
+    assert(8 == Circle(4).diameter)
+    assert(4 == Circle(2).diameter)
 
 def test_repr():
     assert(eval( repr( Circle(4))) == Circle(4))
