472a7a5e6a6ea08de3a69da4e233286a48a6fa92
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-05-20 08:14:57 -0700

    started adding functional elements to mailroom

---
+++ students/Gregg/lesson10/mailroom.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python3
+"""Use current knowledge to set up an interactive program"""
+
+import datetime
+from collections import defaultdict
+
+donations_init = {
+    "William Gates, III": [3, 5, 7],
+    "Mark Zuckerberg": [4.50, 8, 2],
+    "Jeff Bezos": [7.77],
+    "Paul Allen": [3.6, 4.5],
+    "bob": [.01]
+}
+#donations = defaultdict(list, donations)
+# todo
+# add file writing
+
+class Donor():
+
+
+    def __init__(self, name_in, donations=None):
+        self.name = name_in
+        if donations is not None:
+            try:
+                self._donation_list = list(donations)
+            except TypeError as E:
+                self._donation_list = [donations]
+        else:
+            self._donation_list = []
+
+    def add_donation(self, donation_amount):
+        """Add a dontation to to the database for donor"""
+        donation_amount = float(donation_amount)
+        if donation_amount <= 0:
+            raise(ValueError)
+        self._donation_list.append(donation_amount)
+
+    def send_thank_you(self, this_thx_string):
+        """send the thank you to the donor"""
+        # leaving this as a sperate function because at some point I expect
+        # to actually do things other than printing here
+        print(f"Heres the thank you that should be sent to {self.name}:")
+        print(this_thx_string)
+        print('')
+
+    def new_donation_handler(self, donation_amount):
+        """add a donation to the list for given donor in donations dict"""
+        try:
+            self.add_donation(donation_amount)
+        except (ValueError, TypeError) as E:
+            print(f'{donation_amount} is not a valid donation amount, please enter a number greater than 0')
+            raise(E)
+        this_thx_string = self.last_don_thx_string
+        self.send_thank_you(this_thx_string)
+
+    @property
+    def stats(self):
+        """retrieve the statistics for a given donor"""
+        donors_donations = self.all_donations
+        total = sum(donors_donations)
+        num = len(donors_donations)
+        try:
+            avg = total / num
+        except ZeroDivisionError as E:
+            avg = 0
+        return self.name, total, num, avg
+
+    @property
+    def all_donations(self):
+        """Return a list of all amounts made by donor ordered chronologically"""
+        return self._donation_list
+
+    line1 = 'Dear {},\n\n'
+    line2 = '\tThank you for you generous donation of ${:.2f}.\n\n'
+    line3 = '\tIt will be put to good use.\n\n'
+    line4 = 'Sincerely,\n'
+    line5 = '-The team'
+
+    thankyou_string = (
+        '{}{}{}{:>40}{:>45}'.format(line1, line2, line3, line4, line5).format
+    )
+
+    @property
+    def last_don_thx_string(self):
+        return self.thankyou_string(self.name, self.last_donation)
+
+    def send_letter(self, letter_filename):
+        """Send a thank you letter to a donor for the most recent donation"""
+        try:
+            donor_thx_string = self.last_don_thx_string
+        except TypeError as E:
+            print((
+                "{0} is in our database but has not made a donation.\n"
+                "No thank you was saved for {0}"
+            ).format(self.name)
+            )
+        else:
+            with open(letter_filename, 'w') as letter:
+                letter.write(donor_thx_string)
+            print(f'Sent letter to {self.name}')
+
+    @property
+    def last_donation(self):
+        """Return the value of the last donation made by donor"""
+        try:
+            last_donation = self.all_donations[-1]
+        except IndexError as E:
+            last_donation = None
+        # This is currently pointelss because the default dict took care of the
+        # the situation where a donor would need to be added seperatley from their
+        # donation. I will leave it in anyway in case it comes again in the future
+        return last_donation
+
+
+
+class Donations_db():
+
+
+    def __init__(self, donor_dict = None):
+        self._donors = []
+        if donor_dict is not None:
+            for name, donations in donor_dict.items():
+                self.add_donor(name, donations)
+
+    def add_donor(self, name, donations=None):
+            donor = Donor(name, donations)
+            self._donors.append(donor)
+
+    def list_donors(self):
+        """print all the donors currently as keys of donations dictionary"""
+        self.print_list(self.names_in_database, 'donors')
+
+
+    def donor_from_name(self, name):
+        for donor in self._donors:
+            if donor.name == name:
+                return donor
+
+    @property
+    def names_in_database(self):
+        """Retunr the donors in the database"""
+        # putting this in in case the data strict changes
+        name_list = []
+        for donor in self._donors:
+            name_list.append(donor.name)
+        return name_list
+
+    def send_letters(self, filename_sufffix = None):
+        """Send letters to the donors in the DB that have made donations"""
+        if filename_sufffix is None:
+            filename_sufffix = datetime.datetime.now().strftime("%m.%d.%Y.%H.%M.%S")
+        for donor in self._donors:
+            letter_filename = "{}_{}.txt".format(donor.name, filename_sufffix)
+            donor.send_letter(letter_filename)
+
+    @property
+    def donation_report(self):
+        """Create a report of summarry statistics for the current database
+
+        The columns are name, total, number and average
+        """
+        row_list = []
+        headers = 'Donor name', 'Total Given', 'Num Gifts', 'Average Gift'
+        format_column_header = "{:<26}|{:^13}|{:^13}|{:^13}".format
+        row_list.append(format_column_header(*headers))
+        row_list.append('_' * 68)
+
+        donor_rows_list = [donor.stats for donor in self._donors]
+        donor_rows_list.sort(key=self.key1, reverse=True)
+
+        format_row = "{:<27}${:>12.2f} {:>12}  ${:>12.2f}".format
+        row_list.extend([
+            format_row(*row)
+            for row in donor_rows_list
+        ])
+        report_string = ('\n').join(row_list)
+        return report_string
+
+    def print_report(self):
+        """Print report to prompt"""
+        print(self.donation_report)
+
+    def new_donation_handler(self, name, donation_amount):
+        donor = self.donor_from_name(name)
+        donor.new_donation_handler(donation_amount)
+
+
+    def scaled_database(self, scale_factor, min_donation = 0, max_donation = None):
+
+
+        return scaled_db
+
+
+    def matching_projection(self, scale_factor, min_donation = 0, max_donation = None)
+        scaled_db = self.scaled_database(self, scale_factor, min_donation, max_donation)
+
+    @staticmethod
+    def total_donations(any_donation_db)
+        total_donations = sum([
+            sum(donation_list)
+            for donation_list in any_donation_db.donors.values()
+        ])
+        return total_donations
+
+    @staticmethod
+    def key1(iterable):
+        """Sort key for creating report"""
+        return iterable[1]
+
+    @staticmethod
+    def print_list(list_in, descriptor_str):
+            """prints the elements of the list with some nice UI text"""
+            # makes it more flexible so I could print other lists if I wanted to
+            # such as the donations for a given donor
+            print("These are the {} currently in the database:".format(descriptor_str))
+            print('\n'.join(list_in))
+            print('')
+
+donation_db = Donations_db(donations_init)
+
+def menu(prompt, menu_dict, quit_string='q'):
+    """Continues prompting with prompt until the quit_string is returned
+
+    Prompt handler is set up to return values based on the functions in
+    action_list, so one of these functions should return the quit_string
+    otherwise there is no way to quit
+    """
+    def menu_function(key_in):
+        menu_handler(key_in, menu_dict)
+
+    while(True):
+        result = quitable_function_prompt(prompt, menu_function, quit_string)
+        if result is not None:
+            break
+
+
+def menu_handler(key_in, menu_dict):
+    """use a switch dict to choose route the program based on input
+
+    includes nice handling of invlaid options
+    """
+    try:
+        selected_action = menu_dict[key_in]
+    except KeyError as E:
+        print("That isn't one of the menu options")
+        raise(E)
+    else:
+        selected_action()
+
+
+def quitable_function_prompt(prompt, function_in, quit_string='q'):
+    """Runs function with user input unless input is quit string
+
+    Will repeat if the input is invalid
+    """
+    while True:
+        result = input(prompt_formatter(prompt))
+        if result == quit_string:
+            return result
+        try:
+            function_in(result.upper())
+            return None
+        except (KeyError, ValueError) as E:
+            # I feel like maybe these should be custom errors?
+            # made dbugging a bit of a pain
+            print('Unable to continue, please try again')
+
+
+prompt_formatter = '{}\nInput: '.format
+
+
+def primary_menu():
+    """Guide user through the mailroom UI"""
+    menu(primary_prompt, primary_switch_dict, quit_string='Q')
+
+
+primary_prompt = (
+    "\nWould you like to Send a Thank You (enter TY), "
+    "Create a Report (enter CR), send thank you letters to everyone (enter SL)"
+    ", run a projection for matching contributions (enter MP)"
+    " or quit (enter Q).\n"
+    "At any point enter q to return to this prompt"
+)
+
+
+def prompt_thank_you():
+    """Guide user to create a thank you for a new donation"""
+    quitable_function_prompt(thank_you_prompt, thank_you)
+
+
+def thank_you(donor):
+    """Prompt the necessary actions to send thankyou to donor"""
+    #print(donor)
+    if donor.lower() == 'list':
+        donation_db.list_donors()
+        prompt_thank_you()
+    else:
+        add_donation_prompt = f"How much did {donor} donate today?"
+        if not(donor in donation_db.names_in_database):
+            print(f'{donor} is not in our database yet, lets add them.\n')
+            donation_db.add_donor(donor)
+        def add_donation_for_donor(donation_amount):
+            donation_db.new_donation_handler(donor, donation_amount)
+        quitable_function_prompt(add_donation_prompt, add_donation_for_donor)
+
+
+thank_you_prompt = (
+    "Enter a full name to send them a thank you\n"
+    "Enter 'list' to see current donors"
+)
+
+
+primary_switch_dict = {
+    'TY': prompt_thank_you,
+    'CR': donation_db.print_report,
+    'SL': donation_db.send_letters
+    'MP': donation_db.matching_projection
+}
+
+
+
+if __name__ == "__main__":
+
+    print("Welcome to Gregg's Mailroom 1.3\n")
+    primary_menu()
---
+++ students/Gregg/lesson10/test_mailroom.py
@@ -0,0 +1,246 @@
+from mailroom import *
+import pytest
+import os
+import copy
+
+initial_donations = {
+    "William Gates, III": [3, 5, 7],
+    "Mark Zuckerberg": [4.50, 8, 2],
+    "Jeff Bezos": [7.77],
+    "Paul Allen": [3.6, 4.5],
+    "bob": [.01],
+    "bill": []
+}
+
+donations = copy.deepcopy(initial_donations)
+
+donations = defaultdict(list, donations)
+
+donation_db = Donations_db(initial_donations)
+
+donors = [Donor(name, donation_list) for name, donation_list in initial_donations.items()]
+
+def test_menu():
+    pass
+
+def test_total_donations():
+    assert(donation_db.total_donations == 45.38)
+
+
+def test_menu_handler(capsys):
+    flag = False
+
+    def a(input = flag):
+        print('Ran func A')
+
+    def b(input = flag):
+        print('Ran func B')
+
+    mock_menu = {'A': a, 'B': b}
+    menu_handler('A', mock_menu)
+    captured = capsys.readouterr()
+    assert('Ran func A' in captured.out)
+    menu_handler('B', mock_menu)
+    captured = capsys.readouterr()
+    assert('Ran func B' in captured.out)
+    with pytest.raises(KeyError):
+        menu_handler(2, mock_menu)
+
+
+def test_quitbale_function_prompt():
+    # input_dict =
+    # def mock_input():
+    pass
+    # monkeypatch.setattr('builtins.input', lambda x: "Mark")
+    # requires mocking input
+    # - can we do this consecutively for a full functional test?
+
+
+def test_thank_you():
+    # this is really just an instance of print list, I don't need to test both right?
+    donor = 'list'
+    # we just want to make sure it loops back
+    dono = 'bob'
+    # should print the next piece?
+
+
+donors_list = [
+    "William Gates, III",
+    "Mark Zuckerberg",
+    "Jeff Bezos",
+    "Paul Allen",
+    "bob",
+    "bill"
+]
+
+
+def test_list_donors(capsys):
+    desc = 'donors'
+    donation_db.list_donors()
+    captured = capsys.readouterr()
+    assert('These are the donors currently in the database:' in captured.out)
+
+
+def test_print_list(capsys):
+    desc = 'descriptor'
+    donation_db.print_list(donors_list, desc)
+    captured = capsys.readouterr()
+    assert(desc in captured.out)
+    for donor in donors_list:
+        assert(donor in captured.out)
+
+
+def test_donors_in_database():
+    assert(donation_db.names_in_database == donors_list)
+
+
+name = 'cary'
+donation_amounts = [-5, 'cow', 300]
+donations_raise = [ValueError, ValueError, False]
+
+
+def test_new_doation_handler(capsys):
+    donation_add_db = Donations_db(initial_donations)
+    for idx, donation in enumerate(donation_amounts):
+        E = donations_raise[idx]
+        if E:
+            with pytest.raises(E):
+                donation_add_db.new_donation_handler(name, donation)
+        else:
+            donation_add_db.new_donation_handler(name, donation)
+            assert(donation_add_db.donor_from_name(name).last_donation == donation)
+            captured = capsys.readouterr()
+            assert name in captured.out
+            assert '$'+str(donation) in captured.out
+
+
+def test_add_donation_to_database():
+    donor = Donor(name)
+    for idx, donation in enumerate(donation_amounts):
+        if donations_raise[idx]:
+            with pytest.raises(ValueError):
+                donor.add_donation(donation)
+        else:
+            donor.add_donation(donation)
+            assert(donor.last_donation == donation)
+
+name = 'bob'
+test_donations = {
+    5.0935: [name, '$5.09', 'Thank you'],
+    50: [name, '$50', 'Thank you'],
+    5.1: [name, '$5.10', 'Thank you'],
+}
+
+
+def test_thankyou_string():
+    donor = Donor(name)
+    for test_donation, should_contain in test_donations.items():
+        thx_str = donor.thankyou_string(donor.name, test_donation)
+        for string in should_contain:
+            assert(string in thx_str)
+
+
+def test_send_thank_you(capsys):
+    test_string = 'test_string'
+    donor = Donor('bob')
+    donor.send_thank_you('test_string')
+    captured = capsys.readouterr()
+    assert(test_string in captured.out)
+        # for string in should_contain:
+        #    assert(string in captured.out)
+
+
+donation_report_string = (
+    "Donor name                | Total Given |  Num Gifts  |Average Gift \n"
+    "____________________________________________________________________\n"
+    "William Gates, III         $       15.00            3  $        5.00\n"
+    "Mark Zuckerberg            $       14.50            3  $        4.83\n"
+    "Paul Allen                 $        8.10            2  $        4.05\n"
+    "Jeff Bezos                 $        7.77            1  $        7.77\n"
+    "bob                        $        0.01            1  $        0.01\n"
+    "bill                       $        0.00            0  $        0.00"
+)
+
+
+def test_create_donation_report():
+    donation_report = donation_db.donation_report
+    assert(donation_report_string == donation_report)
+
+
+def test_print_report(capsys):
+    #fix this
+    pass
+
+
+def test_key1():
+    assert(donation_db.key1([1, 2, 3]) == 2)
+
+
+def test_get_row():
+    rows = {
+        "William Gates, III": ("William Gates, III", 15, 3, 5),
+        "Mark Zuckerberg": ("Mark Zuckerberg", 14.5, 3, 14.5/3),
+        "Jeff Bezos": ("Jeff Bezos", 7.77, 1, 7.77),
+        "Paul Allen": ("Paul Allen", 8.10, 2, 4.05),
+        "bob": ("bob", .01, 1, .01),
+        "bill": ("bill", 0, 0, 0)
+    }
+    for donor in donors:
+        row = donor.stats
+        assert(row == rows[donor.name])
+        print(row)
+
+
+def test_donations_from():
+    for donor in donors:
+        assert(donor.all_donations == donations[donor.name])
+
+
+def test_send_letters():
+    donation_db = Donations_db(initial_donations)
+    filename_sufffix = 'test'
+    donation_db.send_letters(filename_sufffix)
+    for name in donations:
+        test_name = "{}_{}.txt".format(name, filename_sufffix)
+        if name == 'bill':
+            assert(not(os.path.isfile(test_name)))
+        else:
+            donor = donation_db.donor_from_name(name)
+            with open(test_name, 'r') as letter_file:
+                letter = letter_file.read()
+                las_don = donor.last_donation
+                assert(letter == donor.thankyou_string(donor.name, las_don))
+
+
+def test_send_letter(capsys):
+    donor = Donor('bill')
+    donor.send_letter('test1.txt')
+    captured = capsys.readouterr()
+    assert("No thank you was saved for bill" in captured.out)
+    test_name = 'test2.txt'
+    donor = Donor('bob', .01)
+    donor.send_letter(test_name)
+    with open(test_name, 'r') as letter_file:
+        letter = letter_file.read()
+        las_don = .01
+        thx_str = donor.thankyou_string(donor.name, las_don)
+        print(thx_str)
+        print(letter)
+        assert(thx_str in letter)
+
+
+last_donations = {
+    "William Gates, III": 7,
+    "Mark Zuckerberg": 2,
+    "Jeff Bezos": 7.77,
+    "Paul Allen": 4.5,
+    "bob": .01,
+    "bill": None
+}
+
+
+def test_last_donation():
+    for donor in donors:
+        assert(donor.last_donation == last_donations[donor.name])
+
+
