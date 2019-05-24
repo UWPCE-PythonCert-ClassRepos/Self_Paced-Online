e2f5a4840ba5851ff34d9292ab17c6093f41fdc1
smckellips <sean.mckellips@gmail.com>
2019-05-24 13:43:05 -0700

    Trigram with text loaded

--- students/smckellips/lesson04/trigram.py
+++ students/smckellips/lesson04/trigram.py
@@ -1,16 +1,29 @@
 #! /usr/bin/env python
+import string
 
 example_text = "I wish I may I wish I might"
 
+def load_file(file):
+    # TODO: Strip not text, ie TOC, Intros, title lines, etc.
+    with open(file, 'r') as f:
+        text = f.read().replace('\n', ' ')
+    return text
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation))).lower()
+
 
 def build_trigram(text):
+    
     words = text.split()
     word_dict = {}
 
     for i in range(len(words) - 2):
-        key = "{} {}".format(words[i],words[i+1])
+        key = (words[i],words[i+1])
         word_dict.setdefault(key,[]).append(words[i+2])
     return word_dict
 
 if __name__ == "__main__":
-    trigram = build_trigram(example_text)    
+    # file = './students/smckellips/lesson04/sherlock_small.txt'
+    file = './students/smckellips/lesson04/sherlock.txt'
+    trigram = build_trigram(remove_punctuation(load_file(file)))    
